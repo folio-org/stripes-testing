b50836cb1948c616e80fcfe3b7245deec0def277
Wolfram Schneider <wosch@indexdata.com>
2017-03-19 16:45:43 +0100

    a working trivial example

---
+++ test/trivial.js
@@ -0,0 +1,39 @@
+const Nightmare = require('nightmare')
+const assert = require('assert')
+
+describe('Using the App', function () {
+  this.timeout('10s')
+
+  let nightmare = null
+  beforeEach(() => {
+    // show true lets you see wth is actually happening :)
+    nightmare = new Nightmare({ show: true })
+  })
+
+  describe('signing up and finishing setup', () => {
+    let username = process.env.FOLIO_UI_USERNAME
+    let password = process.env.FOLIO_UI_PASSWORD
+    
+    it('should work without timing out', done => {
+      nightmare
+      .goto('http://localhost:3000')
+      .type('input[name=username]', username)
+      .type('input[name=password]', password)
+      .click('button[type=submit]')
+      .wait('h3')
+      
+      .click('a[title=Trivial] > span') // Hi Kurt
+      .type("input[id=g]", "Ho")
+      .type("input[id=n]", "Karl")
+      .click('button[type=submit]')
+      
+      .wait('h3')
+      //.wait(20000)
+      .end()
+      .then(result => { done() })
+      .catch(done)
+    })
+  })
+})
+
+
