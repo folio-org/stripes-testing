bbe501a4821019add327aa6a40e9e452be91ab52
Wolfram Schneider <wosch@indexdata.com>
2017-03-19 15:49:25 +0100

    read username/password from environment variables FOLIO_UI_USERNAME/FOLIO_UI_PASSWORD

--- test/110-auth-success.js
+++ test/110-auth-success.js
@@ -2,20 +2,23 @@ const Nightmare = require('nightmare')
 const assert = require('assert')
 
 describe('Using the App', function () {
-  this.timeout('60s')
+  this.timeout('6s')
 
   let nightmare = null
   beforeEach(() => {
     // show true lets you see wth is actually happening :)
-    nightmare = new Nightmare({ show: false })
+    nightmare = new Nightmare({ show: true })
   })
 
   describe('signing up and finishing setup', () => {
+    let username = process.env.FOLIO_UI_USERNAME
+    let password = process.env.FOLIO_UI_PASSWORD
+    
     it('should work without timing out', done => {
       nightmare
       .goto('http://localhost:3000')
-      .type('input[name=username]', 'diku_admin')
-      .type('input[name=password]', 'admin')
+      .type('input[name=username]', username)
+      .type('input[name=password]', password)
       .click('button[type=submit]')
       .wait('h3')
       .end()
