19968125422f4cd077edad8b0990ccacbaba7bf5
Wolfram Schneider <wosch@indexdata.com>
2017-03-19 16:51:17 +0100

    /scan example

---
+++ test/scan.js
@@ -0,0 +1,35 @@
+const Nightmare = require('nightmare')
+const assert = require('assert')
+
+describe('Using the App', function () {
+  this.timeout('10s')
+
+  let nightmare = null
+  beforeEach(() => {
+    // show true lets you see wth is actually happening :)
+    nightmare = new Nightmare({ show: true })
+  })
+
+  describe('signing up and finishing setup', () => {
+    let username = process.env.FOLIO_UI_USERNAME
+    let password = process.env.FOLIO_UI_PASSWORD
+    
+    it('should work without timing out', done => {
+      nightmare
+      .goto('http://localhost:3000')
+      .type('input[name=username]', username)
+      .type('input[name=password]', password)
+      .click('button[type=submit]')
+      .wait('h3')
+      
+      .click('a[title=Scan] > span')
+      //.wait('h1')
+      //.wait(2000)
+      .end()
+      .then(result => { done() })
+      .catch(done)
+    })
+  })
+})
+
+
