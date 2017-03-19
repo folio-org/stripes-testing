5d9c2fa74e47a1174e372c94f65a3a87605997f0
Wolfram Schneider <wosch@indexdata.com>
2017-03-19 15:04:11 +0100

    no unicode characters in comments

---
+++ test/startpage.js
@@ -0,0 +1,22 @@
+const Nightmare = require('nightmare')
+const assert = require('assert')
+
+describe('Load a Page', function() {
+  // Recommended: 5s locally, 10s to remote server, 30s from airplane
+  this.timeout('30s')
+
+  let nightmare = null
+  beforeEach(() => {
+    nightmare = new Nightmare()
+  })
+
+  describe('/ (Home Page)', () => {
+    it('should load without error', done => {
+      // your actual testing urls will likely be `http://localhost:port/path`
+      nightmare.goto('http://localhost:3000')
+        .end()
+        .then(function (result) { done() })
+        .catch(done)
+    })
+  })
+})
