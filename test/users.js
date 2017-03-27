22e63b95a6fc8d5d4b7b4a93c711bec505bf9b85
Wolfram Schneider <wosch@indexdata.com>
2017-03-27 16:00:50 +0200

    add campuse filter

--- test/users.js
+++ test/users.js
@@ -24,13 +24,16 @@ describe('Using the App Folio UI App /users', function () {
       .click('a[title=Users] > span')
       .wait('em')
       
+      .click('input[id="pg.On-campus-ItemFilter"]')
+      .wait('em')
+      
       .type('input[placeholder=Search]', "da")
       .wait('em')
       
       .click('input[id="active.Inactive-ItemFilter"]')
       .wait('em')
       
-      .wait(4000) // debugging
+      .wait(parseInt(process.env.FOLIO_UI_DEBUG) ? parseInt(config.debug_sleep) : 0) // debugging
       
       .end()
       .then(result => { done() })
