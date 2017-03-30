f4e9623f30f17a2eb1132756cca2b90c0487a8bd
Wolfram Schneider <wosch@indexdata.com>
2017-03-30 11:33:49 +0200

    speedup login by faster typing

--- folio-ui.config.js
+++ folio-ui.config.js
@@ -8,6 +8,7 @@ module.exports = {
     debug_sleep: process.env.FOLIO_UI_DEBUG_SLEEP || 4000,
     
     nightmare: process.env.FOLIO_UI_DEBUG == 2 ? {
+        typeInterval: 40,
         openDevTools: {
             mode: 'detach'
         },
@@ -15,6 +16,7 @@ module.exports = {
         height: 600,
         show: true
     } : process.env.FOLIO_UI_DEBUG == 1 ? {
+        typeInterval: 20,
         show: true
-    } : {}
+    } : { typeInterval: 5 }
 }
