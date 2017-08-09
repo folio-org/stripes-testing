6acbe849dbdbae669e0c1b9649e22910fae471f5
Charles Ledvina <charles@indexdata.com>
2017-08-09 12:57:07 -0500

    Added random address generator.

--- namegen.js
+++ namegen.js
@@ -2,8 +2,13 @@ module.exports.namegen = function() {
   var ts = new Date().valueOf()
   var fn = ['Emma','Olivia','Ava','Sophia','Isabella','Liam','Noah','Mason','Lucas','Oliver']
   var ln = ['Smith','Johnson','Williams','Brown','Jones','Miller','Davis','Garcia','Rodriquez','Wilson']
+  var ad = [{city: 'Chicago',zip: '60609',address: '5 Wacker Dr', country: 'USA',state: 'IL', phone:'13125552121'},
+            {city: 'New York',zip: '10002-2341',address: '425 Bowery', country: 'USA',state: 'NY', phone:'12125554444'},
+            {city: 'San Francisco',zip: '94016',address: '39 Green Street', country: 'USA',state: 'CA', phone:'12153338888'},
+            {city: 'København',zip: '1208',address: 'Nybrogade 30', country: 'Denmark',state: 'K', phone:'453332229999'}]
   var fnpos = Math.floor(Math.random() * fn.length)
   var lnpos = Math.floor(Math.random() * ln.length)
+  var adpos = Math.floor(Math.random() * ad.length)
   var firstname = fn[fnpos]
   var lastname = ln[lnpos]
   return {
@@ -12,6 +17,7 @@ module.exports.namegen = function() {
     lastname: lastname,
     email: firstname.toLowerCase() + '.' + lastname.toLowerCase() + '@someserver.org',
     barcode: ts,
-    password: 'P@$$w0rd23'
+    password: 'P@$$w0rd23',
+    address: ad[adpos]
   }
 }
