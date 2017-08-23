4546bc107492bc74df777382731811655c1e12c8
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-08-23 19:43:47 +0200

    Add scripts for running modules' own tests. FOLIO-782.

---
+++ test-module.js
@@ -0,0 +1,27 @@
+const Nightmare = require('./xnightmare.js')
+const minimist = require('minimist')
+const config = require('./folio-ui.config.js')
+const auth = require('./auth.js');
+const names = require('./namegen.js')
+
+const app = minimist(process.argv.slice(2)).app;
+const appArray = app.split(',');
+
+const nightmare = new Nightmare(config.nightmare);
+
+var appTest = null;
+for (var i=0; i< appArray.length; i++) {
+  try {
+    var scripts = '@folio/'+appArray[i]+'/test/nightmare/test.js'
+    console.log("Test: ", scripts);
+    appTest = require(scripts);
+    try {
+      appTest.test({ nightmare, config, utils: { auth, names }});
+    } catch (e) {
+      console.log("Could not run tests for module ", appArray[i], e);
+    }
+  } catch (e) {
+    console.log('Module or test not found: ', appArray[i], e);
+  }
+}
+
