94670f6d8e59d33d31dfc80aa07714b8e6e574b6
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-08-24 10:08:48 +0200

    Rename folder holding ui modules' own tests. FOLIO-801.

--- test-module.js
+++ test-module.js
@@ -3,7 +3,6 @@ const minimist = require('minimist')
 const config = require('./folio-ui.config.js')
 const auth = require('./auth.js');
 const names = require('./namegen.js')
-
 const app = minimist(process.argv.slice(2)).app;
 const appArray = app.split(',');
 
@@ -12,7 +11,8 @@ const nightmare = new Nightmare(config.nightmare);
 var appTest = null;
 for (var i=0; i< appArray.length; i++) {
   try {
-    var scripts = '@folio/'+appArray[i]+'/test/nightmare/test.js'
+
+    var scripts = '@folio/'+appArray[i]+'/test/ui-testing/test.js'
     console.log("Test: ", scripts);
     appTest = require(scripts);
     try {
