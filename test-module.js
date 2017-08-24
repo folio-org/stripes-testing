25496a609248d5fd131d98ed6eb793022f2956c1
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-08-24 19:13:23 +0200

    Provide syntax for running individual script(s) within a module. FOLIO-801.
       Run main tests (test.js) for Users and CheckOut: app=users/checkout
       Run a named test within Users:                   app=users:new_user
       Multiple tests for Users, main for Checkout:     app=users:new_user,patron_group/checkout
    
       For instance:
    
        yarn test-module -- -o --app=users:new_user,patron_groups/checkout/items:new_item

--- test-module.js
+++ test-module.js
@@ -6,22 +6,47 @@ const names = require('./namegen.js')
 
 config.nightmare.gotoTimeout = 90000;
 
-const app = minimist(process.argv.slice(2)).app;
-const appArray = app.split(',');
-var appTest = null;
-console.log("Running module tests. ")
-for (var i=0; i< appArray.length; i++) {
-  try {
-    var scripts = '@folio/'+appArray[i]+'/test/ui-testing/test.js'
-    console.log("Module: ", scripts);
-    appTest = require(scripts);
-    try {
-      appTest.test({ config, utils: { auth, names }});
-    } catch (e) {
-      console.log("Could not run tests for module ", appArray[i], e);
-    }
-  } catch (e) {
-    console.log('Module or test not found: ', appArray[i], e);
-  }
-}
+let app = minimist(process.argv.slice(2)).app;
 
+if (app) {
+  app = app.replace(/\s/g, '');
+
+  const appArray = app.split('/');
+  var appTest = null;
+  console.log("Running module tests: ")
+  for (var i=0; i< appArray.length; i++) {
+    if (appArray[i].length>0) {
+      let app = "";
+      let scriptArray = [];
+      if (appArray[i].indexOf(":") == -1) {
+        app = appArray[i];
+        scriptArray.push("test");
+      } else {
+        app = appArray[i].substring(0,appArray[i].indexOf(":"));
+        scriptArray = appArray[i].substring(appArray[i].indexOf(":")+1).split(',');
+        if (scriptArray[0].length === 0) {
+          scriptArray[0] = "test";
+        }
+      }
+      for (var j=0; j<scriptArray.length; j++) {
+        if (scriptArray[j].length>0) {
+          try {
+            var script = scriptArray[j];
+            var testscript = '@folio/'+app+'/test/ui-testing/'+script+'.js'
+            console.log(' '+app+(script != "test" ? '/'+script : ''));
+            appTest = require(testscript);
+            try {
+              appTest.test({ config, utils: { auth, names }});
+            } catch (e) {
+              console.log("Could not run tests for module ", app, e);
+            }
+          } catch (e) {
+            console.log('Module or test not found: ', app, e);
+          }
+        }
+      }
+    }
+  }
+} else {
+  console.log("Found no modules or tests to run.");
+}
