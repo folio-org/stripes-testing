ff58ad768c6748c2db0a5609b95b44bb31d2cc97
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-08-28 09:10:51 +0200

    Change module-test arg name to 'run'. Add help page.

--- test-module.js
+++ test-module.js
@@ -6,47 +6,82 @@ const names = require('./namegen.js')
 
 config.nightmare.gotoTimeout = 90000;
 
-let app = minimist(process.argv.slice(2)).app;
+let run = minimist(process.argv.slice(2)).run;
 
-if (app) {
-  app = app.replace(/\s/g, '');
+if (run) {
+  run = run.replace(/\s/g, '');
 
-  const appArray = app.split('/');
-  var appTest = null;
-  console.log("Running module tests against "+config.url+": ")
-  for (var i=0; i< appArray.length; i++) {
-    if (appArray[i].length>0) {
+  const apps = run.split('/'); // find modules to test
+
+  for (var a=0; a< apps.length; a++) { // for each module in argv
+    let appscripts = apps[a];
+
+    if (appscripts.length>0) {
+
       let app = "";
-      let scriptArray = [];
-      if (appArray[i].indexOf(":") == -1) {
-        app = appArray[i];
-        scriptArray.push("test");
-      } else {
-        app = appArray[i].substring(0,appArray[i].indexOf(":"));
-        scriptArray = appArray[i].substring(appArray[i].indexOf(":")+1).split(',');
-        if (scriptArray[0].length === 0) {
-          scriptArray[0] = "test";
-        }
-      }
-      for (var j=0; j<scriptArray.length; j++) {
-        if (scriptArray[j].length>0) {
-          try {
-            var script = scriptArray[j];
-            var testscript = '@folio/'+app+'/test/ui-testing/'+script+'.js'
-            console.log(' '+app+(script != "test" ? '/'+script : ''));
-            appTest = require(testscript);
-            try {
-              appTest.test({ config, utils: { auth, names }});
-            } catch (e) {
-              console.log("Could not run tests for module ", app, e);
-            }
-          } catch (e) {
-            console.log('Module or test not found: ', app, e);
+      let scripts = [];
+      if (appscripts.indexOf(":") == -1) { // no scripts requested, run main ("test.js")
+        app = appscripts;
+        scripts.push("test");
+      } else {                             // get scripts
+        app = appscripts.substring(0, appscripts.indexOf(":"));
+        scripts = appscripts.substring(appscripts.indexOf(":")+1).split(',');
+        let run_main = true;
+        for (var s=0; s<scripts.length; s++) {
+          if (scripts[s].length>0) {
+            run_main=false;
           }
         }
+        if (run_main) {
+          console.log('No scripts found after script indicator ":", running main ("test.js") for module "'+app+'"');
+          scripts[0] = 'test';
+        }
+      }
+
+      let emptyScriptArg = false;
+      for (var j=0; j<scripts.length; j++) { // for each test script requested from the module
+        let script = scripts[j];
+        if (script) {
+          try {
+            let tests = require('@folio/'+app+'/test/ui-testing/'+script+'.js');
+            try {
+              tests.test({ config, utils: { auth, names }});
+            } catch (e) {
+              console.log('Could not run tests for module "'+app+'"\n', e);
+            }
+          } catch (e) {
+            console.log('Module or test script not found: "'+app+ (script=='test' ? '' : ':'+ script) +'"\n', e.message);
+          }
+        } else {
+          emptyScriptArg = true;
+        }
+      }
+      if (emptyScriptArg) {
+        console.log('Found empty script argument(s) for "'+app+'". Skipped.');
       }
     }
   }
 } else {
-  console.log("Found no modules or tests to run.");
+  console.log("test-module:   Missing 'run' argument. Found no modules or tests to run.");
+  console.log("");
+  console.log("");
+  console.log("Usage:");
+  console.log("");
+  console.log("  yarn test-module -- -o --run=module[:script[,script]][/module[:script[,script]]]");
+  console.log("");
+  console.log("Examples:");
+  console.log("");
+  console.log("  yarn test-module -- -o --run=requests                     Runs main tests (test.js) for UI module 'requests'")
+  console.log("  yarn test-module -- -o --run=users/items                  Runs tests (test.js) for multiple UI modules");
+  console.log("  yarn test-module -- -o --run=users:new_user,patron_group  Runs specific tests for 'users'");
+  console.log("");
+  console.log("Info:");
+  console.log("");
+  console.log("  The script will run a UI module's Nightmare tests, provided that the module has tests defined and ");
+  console.log("");
+  console.log("   a) is included as a dependency in /ui-testing/package.json ");
+  console.log("   b) runs on the FOLIO web application targeted by this test (see URL settings in /ui-testing/folio-ui-config.js)");
+  console.log("");
+  console.log("  The npm version of the test suite should match the version of the live module being tested.");
+  console.log("");
 }
