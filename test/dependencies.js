7c3f47cc45dcbb1ab09aeebd70c6e0e8864c1e48
Charles Ledvina <charles@indexdata.com>
2017-10-27 14:27:58 -0500

    Only log warnings when dependency check fails.

--- test/dependencies.js
+++ test/dependencies.js
@@ -42,36 +42,49 @@ describe('Checking for dependency issues on FOLIO UI App /about ("test-dependenc
       .then(result => { done() })
       .catch(done)
     })
-    it('should find no "red" errors', done => {
+    it('should check for "red" errors', done => {
       nightmare
       .evaluate(function() {
         var elements = document.querySelectorAll('li[style*=" red"]')
-      var msgs = new Array();
-      for (x = 0; x < elements.length; x++) {
-        msgs.push(elements[x].textContent)  
-      }
-      if (msgs.length > 0) {
-        throw new Error("Interfaces that are required but absent:\n" + msgs.join("\n"))
-      }
-      })
-      .wait(parseInt(process.env.FOLIO_UI_DEBUG) ? parseInt(config.debug_sleep) : 555) // debugging
-      .then(result => { done() })
-      .catch(done)
-    })
-    it('should find no "orange" errors', done => {
-      nightmare
-      .evaluate(function() {
-        var elements = document.querySelectorAll('li[style*="orange"]')
         var msgs = new Array();
         for (x = 0; x < elements.length; x++) {
           msgs.push(elements[x].textContent)  
         }
         if (msgs.length > 0) {
-          throw new Error("Interfaces that are required but present only in an incompatible version:\n" + msgs.join("\n"))
+          msgs.push("Interfaces that are required but absent:")
         }
       })
-      .wait(parseInt(process.env.FOLIO_UI_DEBUG) ? parseInt(config.debug_sleep) : 555) // debugging
-      .then(result => { done() })
+      .then(result => { 
+        done()
+	if (result !== null) {
+          for (x = 0; x < result.length; x++) {
+	    console.log('          WARN:', result[x])
+	  }
+	}
+      })
+      .catch(done)
+    })
+    it('should check for "orange" errors', done => {
+      nightmare
+      .evaluate(function() {
+        var elements = document.querySelectorAll('li[style*="orange"]')
+        var msgs = new Array();
+        for (x = 0; x < elements.length; x++) {
+          msgs.push('* ' + elements[x].textContent)  
+        }
+        if (msgs.length > 0) {
+	  msgs.unshift("Interfaces that are required but present only in an incompatible version:")
+	  return msgs
+        }
+      })
+      .then(result => { 
+        done()
+	if (result !== null) {
+          for (x = 0; x < result.length; x++) {
+	    console.log('          WARN:', result[x])
+	  }
+	}
+      })
       .catch(done)
     })
     flogout();
