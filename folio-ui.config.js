0a065bf9b5e00e37137d4e8b35fe17356caa2629
Charles Ledvina <charles@indexdata.com>
2018-02-21 10:14:38 -0600

    Fix lint errors

--- folio-ui.config.js
+++ folio-ui.config.js
@@ -1,13 +1,14 @@
 //
 // global debug options for tests
 //
+/* eslint-disable eqeqeq */
 module.exports = {
   url: process.env.FOLIO_UI_URL || 'http://folio-testing.aws.indexdata.com',
   username: process.env.FOLIO_UI_USERNAME || 'diku_admin',
   password: process.env.FOLIO_UI_PASSWORD || 'admin',
-  debug_sleep: parseInt(process.env.FOLIO_UI_DEBUG_SLEEP) || 4000,
-  login_wait: parseInt(process.env.FOLIO_UI_LOGIN_WAIT) || 1000,
-  test_timeout: parseInt(process.env.FOLIO_UI_TEST_TIMEOUT) || 40000,
+  debug_sleep: parseInt(process.env.FOLIO_UI_DEBUG_SLEEP, 10) || 4000,
+  login_wait: parseInt(process.env.FOLIO_UI_LOGIN_WAIT, 10) || 1000,
+  test_timeout: parseInt(process.env.FOLIO_UI_TEST_TIMEOUT, 10) || 40000,
   select: {
     username: 'input[name=username]',
     password: 'input[name=password]',
@@ -22,23 +23,23 @@ module.exports = {
     width: 800,
     height: 600,
     show: true,
-    typeInterval: parseInt(process.env.FOLIO_UI_TYPE_INTERVAL) || 75,
-    gotoTimeout: parseInt(process.env.FOLIO_UI_GOTO_TIMEOUT) || 90000,
-    waitTimeout: parseInt(process.env.FOLIO_UI_WAIT_TIMEOUT) || 30000,
-    executionTimeout: parseInt(process.env.FOLIO_UI_EXECUTION_TIMEOUT) || 30000,
+    typeInterval: parseInt(process.env.FOLIO_UI_TYPE_INTERVAL, 10) || 75,
+    gotoTimeout: parseInt(process.env.FOLIO_UI_GOTO_TIMEOUT, 10) || 90000,
+    waitTimeout: parseInt(process.env.FOLIO_UI_WAIT_TIMEOUT, 10) || 30000,
+    executionTimeout: parseInt(process.env.FOLIO_UI_EXECUTION_TIMEOUT, 10) || 30000,
   } : process.env.FOLIO_UI_DEBUG == 1 ? {
     width: 1600,
     height: 1200,
     show: true,
-    typeInterval: parseInt(process.env.FOLIO_UI_TYPE_INTERVAL) || 75,
-    gotoTimeout: parseInt(process.env.FOLIO_UI_GOTO_TIMEOUT) || 90000,
-    waitTimeout: parseInt(process.env.FOLIO_UI_WAIT_TIMEOUT) || 30000,
-    executionTimeout: parseInt(process.env.FOLIO_UI_EXECUTION_TIMEOUT) || 30000,
+    typeInterval: parseInt(process.env.FOLIO_UI_TYPE_INTERVAL, 10) || 75,
+    gotoTimeout: parseInt(process.env.FOLIO_UI_GOTO_TIMEOUT, 10) || 90000,
+    waitTimeout: parseInt(process.env.FOLIO_UI_WAIT_TIMEOUT, 10) || 30000,
+    executionTimeout: parseInt(process.env.FOLIO_UI_EXECUTION_TIMEOUT, 10) || 30000,
   } : {
-    typeInterval: parseInt(process.env.FOLIO_UI_TYPE_INTERVAL) || 75,
-    gotoTimeout: parseInt(process.env.FOLIO_UI_GOTO_TIMEOUT) || 90000,
-    waitTimeout: parseInt(process.env.FOLIO_UI_WAIT_TIMEOUT) || 30000,
-    executionTimeout: parseInt(process.env.FOLIO_UI_EXECUTION_TIMEOUT) || 30000,
+    typeInterval: parseInt(process.env.FOLIO_UI_TYPE_INTERVAL, 10) || 75,
+    gotoTimeout: parseInt(process.env.FOLIO_UI_GOTO_TIMEOUT, 10) || 90000,
+    waitTimeout: parseInt(process.env.FOLIO_UI_WAIT_TIMEOUT, 10) || 30000,
+    executionTimeout: parseInt(process.env.FOLIO_UI_EXECUTION_TIMEOUT, 10) || 30000,
   },
 };
 if (module.exports.url.match(/snapshot/)) {
