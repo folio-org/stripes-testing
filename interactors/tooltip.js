bae8387057a61aeaa808956cdc82eaf9f572b1d6
John Coburn <jcoburn@ebsco.com>
2020-12-01 12:09:32 -0600

    update tooltip

--- interactors/tooltip.js
+++ interactors/tooltip.js
@@ -2,8 +2,10 @@ import { createInteractor, perform } from '@bigtest/interactor';
 import { isVisible } from 'element-is-visible';
 
 export const Tooltip = createInteractor('tooltip')({
-  selector: '[class^=tooltip], [id^=text][role=tooltip]',
-  locator: (el) => el.querySelector('[class^=text]').textContent,
+  selector: '[class^=tooltip], .sr-only',
+  locator: (el) => {
+    return el.querySelector('[class^=text], span[role=tooltip]').textContent
+  },
   filters: {
     text: (el) => el.querySelector('[class^=text]').textContent,
     sub: (el) => el.querySelector('[class^=sub]').textContent,
@@ -11,7 +13,7 @@ export const Tooltip = createInteractor('tooltip')({
     id: (el) => el.id,
     proximity: {
       apply: (el) => {
-        el.getAttribute('role') === 'tooltip'
+        return el.querySelector('span[role=tooltip]')?.getAttribute('role') === 'tooltip'
       },
       default: false,
     }
