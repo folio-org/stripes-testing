48bdd8f27cfbef6065eb28ef35fe7b88be939446
Jacob Bolda <me@jacobbolda.com>
2020-12-09 16:28:04 -0600

    interactors update: radio button

--- interactors/index.js
+++ interactors/index.js
@@ -2,5 +2,6 @@ export { default as IconButton } from './icon-button';
 export { default as KeyValue } from './key-value';
 export { Tooltip, TooltipProximity } from './tooltip';
 export { default as converge } from './converge';
+export { default as RadioButton } from './radio-button';
 export { default as Select } from './select';
 export { default as Label } from './label';
