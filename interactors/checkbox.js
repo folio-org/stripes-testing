8772c1c639be5142c8d859ff35048d635eb8bd7c
Jacob Bolda <me@jacobbolda.com>
2020-12-11 07:07:12 -0600

    find input element with built-in checkbox interactor

--- interactors/checkbox.js
+++ interactors/checkbox.js
@@ -14,13 +14,14 @@ export default createInteractor('checkbox')({
     feedbackText: (el) => el.querySelector('[role=alert]').textContent,
     hasWarning: (el) => !!el.className.match(/hasWarning/),
     hasError: (el) => !!el.className.match(/hasError/),
-    disabled: (el) => el.disabled // we have cases where it is undefined and then what?
+    disabled: (el) => el.disabled
   },
   actions: {
-    ...CheckBox().specification.actions,
     focus: perform((el) => el.querySelector('label').focus()),
     click: perform((el) => el.querySelector('label').click()),
-    clickInput: perform((el) => el.querySelector('input').click()),
+    // the input is actually transparent (opacity: 0) to opt for showing
+    // an svg element instead, it is still clickable, but in reality not visible
+    clickInput: (interactor) => interactor.find(CheckBox({visible: false})).click(),
     clickAndBlur: perform((el) => {
       el.querySelector('label').click();
       el.querySelector('input').blur();
