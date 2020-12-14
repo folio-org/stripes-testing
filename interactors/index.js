9cfa8a0e70e99e71363cfcc261e68e69b1c32ac6
Jonas Nicklas <jonas@jnicklas.com>
2020-12-14 23:09:11 +0100

    Button interactor
    Adds an interactor for stripes buttons

--- interactors/index.js
+++ interactors/index.js
@@ -4,3 +4,4 @@ export { Tooltip, TooltipProximity } from './tooltip';
 export { default as converge } from './converge';
 export { default as Select } from './select';
 export { default as Label } from './label';
+export { default as Button } from './button';
