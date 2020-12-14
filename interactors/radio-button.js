24c5de44dd797d7163a6b6643511b59cea000951
Jacob Bolda <me@jacobbolda.com>
2020-12-14 15:19:35 -0600

    remove spread and explicitly include built-in RadioButton

--- interactors/radio-button.js
+++ interactors/radio-button.js
@@ -4,20 +4,27 @@ export default createInteractor('radio button')({
   selector: 'div[class^=radioButton]',
   locator: (el) => el.querySelector('[class^=labelText]')?.textContent,
   filters: {
-    ...RadioButton().specification.filters,
+    title: (el) => el.querySelector('input')?.title,
     id: (el) => el.querySelector('input')?.id,
+    valid: (el) => el.querySelector('input')?.validity.valid,
     checked: (el) => el.querySelector('input')?.checked,
     value: (el) => el.querySelector('input')?.value,
     label: (el) => el.textContent,
     feedbackText: (el) => el.querySelector('[class^=radioFeedback]').textContent,
     hasWarning: (el) => !!el.className.match(/hasWarning/),
     hasError: (el) => !!el.className.match(/hasError/),
-    disabled: (el) => el.disabled // we have cases where it is undefined and then what?
+    disabled: {
+      apply: (el) => el.querySelector('input').disabled,
+      default: false
+    },
   },
   actions: {
-    ...RadioButton().specification.actions,
     click: perform((el) => { el.querySelector('label').click(); }),
     focus: perform((el) => { el.querySelector('input').focus(); }),
     blur: perform((el) => { el.querySelector('input').blur(); }),
+    // TODO: when @bigtest/interactors version gets bumped, we can find
+    // the built-in radio button interactor instead
+    // focus: (interactor) => { interactor.find(RadioButton()).focus(); },
+    // blur: (interactor) => { interactor.find(RadioButton()).blur(); },
   }
 });
