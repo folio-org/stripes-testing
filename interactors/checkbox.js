cb827064382068afca2b2d743f6f37d5e1c76b12
Charles Lowell <cowboyd@frontside.io>
2020-12-17 15:52:26 -0600

    Fix linting errors on recently merged code

--- interactors/checkbox.js
+++ interactors/checkbox.js
@@ -7,15 +7,15 @@ const { title, visible } = CheckBox().specification.filters;
 
 export default createInteractor('checkbox')({
   selector: 'div[class^=checkbox-]',
-  locator: (el) => el.querySelector('[class^=labelText]')?.textContent,
+  locator: (el) => el.querySelector('[class^=labelText]').textContent,
   filters: {
-    id: (el) => el.querySelector('input')?.id,
-    checked: (el) => el.querySelector('input')?.checked,
-    valid: (el) => el.querySelector('input')?.validity.valid,
-    value: (el) => el.querySelector('input')?.value,
+    id: (el) => el.querySelector('input').id,
+    checked: (el) => el.querySelector('input').checked,
+    valid: (el) => el.querySelector('input').validity.valid,
+    value: (el) => el.querySelector('input').value,
     label: (el) => el.textContent,
-    ariaLabel: (el) => el.querySelector('input')?.ariaLabel,
-    ariaInvalid: (el) => el.querySelector('input')?.getAttribute('aria-invalid') === 'true',
+    ariaLabel: (el) => el.querySelector('input').ariaLabel,
+    ariaInvalid: (el) => el.querySelector('input').getAttribute('aria-invalid') === 'true',
     feedbackText: (el) => el.querySelector('[role=alert]').textContent,
     hasWarning: (el) => !!el.className.match(/hasWarning/),
     hasError: (el) => !!el.className.match(/hasError/),
@@ -29,7 +29,7 @@ export default createInteractor('checkbox')({
     click: perform((el) => el.querySelector('label').click()),
     // the input is actually transparent (opacity: 0) to opt for showing
     // an svg element instead, it is still clickable, but in reality not visible
-    clickInput: (interactor) => interactor.find(CheckBox({visible: false})).click(),
+    clickInput: (interactor) => interactor.find(CheckBox({ visible: false })).click(),
     clickAndBlur: perform((el) => {
       el.querySelector('label').click();
       el.querySelector('input').blur();
