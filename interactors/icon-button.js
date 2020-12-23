d9fbf77d34119759025ab866155886eaddc34009
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2020-12-23 13:33:06 -0400

    add 'ariaLabel' filter

--- interactors/icon-button.js
+++ interactors/icon-button.js
@@ -1,16 +1,19 @@
 import { createInteractor, focused, perform } from '@bigtest/interactor';
 
+const ariaLabel = el => el.ariaLabel;
+
 export default createInteractor('icon button')({
   selector: '[class^=iconButton]',
-  locator: (el) => el.ariaLabel,
+  locator: ariaLabel,
   filters: {
-    focused,
-    button: (el) => el.tagName.toLowerCase() === 'button',
-    anchor: (el) => el.tagName.toLowerCase() === 'a',
     id: (el) => el.id,
     href: (el) => el.getAttribute('href'),
     hash: (el) => el.hash,
     icon: (el) => el.getAttribute('icon'),
+    button: (el) => el.tagName.toLowerCase() === 'button',
+    anchor: (el) => el.tagName.toLowerCase() === 'a',
+    ariaLabel,
+    focused,
   },
   actions: {
     focus: perform((el) => el.focus()),
