9df1970e36d7bd0b457bcaca4367c1832e6ce356
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2020-12-28 19:41:32 -0400

    add idempotent dropdown actions

--- interactors/dropdown.js
+++ interactors/dropdown.js
@@ -16,19 +16,32 @@ const DropdownMenu = createInteractor('dropdown menu')({
   }
 });
 
+const open = el => el.querySelector('[aria-haspopup]').getAttribute('aria-expanded') === 'true';
+
+const control = (shouldOpen = true) => async interactor => {
+  let isOpen;
+  await interactor.perform(el => { isOpen = open(el); });
+  if (isOpen !== shouldOpen) {
+    await interactor.toggle();
+  }
+};
+
 export default createInteractor('dropdown')({
   selector: 'div[class*=dropdown]',
   locator: el => el.querySelector('[aria-haspopup]').textContent,
   filters: {
-    open: el => el.querySelector('[aria-haspopup]').getAttribute('aria-expanded') === 'true',
+    open,
     visible: el => [el, el.querySelector(['[aria-haspopup]'])].every(isVisible),
   },
   actions: {
     focus: interactor => interactor.find(DropdownTrigger()).focus(),
     toggle: interactor => interactor.find(DropdownTrigger()).click(),
+    open: control(true),
+    close: control(false),
     choose: async (interactor, value) => {
-      await interactor.find(DropdownTrigger()).click();
+      await interactor.open();
       await DropdownMenu({ visible: true }).find(ButtonInteractor(value)).click();
+      await interactor.close();
     },
   }
 });
