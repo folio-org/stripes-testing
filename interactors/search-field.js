144babf7ea77bff9c8bf1a444cb1f51dc1a6137b
Elrick Ryan <elrick@frontside.com>
2020-12-29 13:34:49 -0500

    Expose only one disabled filter

--- interactors/search-field.js
+++ interactors/search-field.js
@@ -7,11 +7,10 @@ export default createInteractor('search field')({
     readOnly: el => el.querySelector('input').hasAttribute('readOnly'),
     value: el => el.querySelector('input').value,
     placeholder: el => el.querySelector('input').placeholder,
-    isDisabledIndex: el => el.querySelector('select').disabled,
-    isDisabled: el => el.querySelector('input').disabled,
+    isDisabled: el => el.querySelector('select').disabled,
   },
   actions: {
     fillIn: (interactor, value) => interactor.find(TextField()).fillIn(value),
-    selectIndex: (interctor, value) => interctor.find(Select()).choose(value),
+    selectIndex: (interactor, value) => interactor.find(Select()).choose(value),
   }
 });
