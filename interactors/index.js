e453536bf2ca5684cda856ceac9e286e65b14699
Charles Lowell <cowboyd@frontside.io>
2021-01-17 10:50:05 -0600

    Remove unused field `input` that was a vestige of rebasing

--- interactors/index.js
+++ interactors/index.js
@@ -3,7 +3,6 @@ export { default as Button } from './button';
 export { default as Checkbox } from './checkbox';
 export { default as HTML } from './html';
 export { default as IconButton } from './icon-button';
-export { default as Input } from './input';
 export { default as KeyValue } from './key-value';
 export { default as Keyboard } from './keyboard';
 export { default as Label } from './label';
