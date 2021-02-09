c06c305cf6bf3811bdda987769ff93b8a0d134db
Charles Lowell <cowboyd@frontside.io>
2021-02-09 09:57:40 +0200

    update button interactor to new builder syntax

--- interactors/button.js
+++ interactors/button.js
@@ -1,23 +1,12 @@
-import { Button as ButtonInteractor, createInteractor, perform, focused } from '@bigtest/interactor';
-import { isVisible } from 'element-is-visible';
+import { HTML } from '@bigtest/interactor';
 
-
-export default createInteractor('button')({
-  selector: 'a[href],button,input[type=button],input[type=submit],input[type=reset],input[type=image]',
-  filters: {
-    id: (el) => el.id,
+export default HTML.extend('button')
+  .selector('a[href],button,input[type=button],input[type=submit],input[type=reset],input[type=image]')
+  .filters({
     text: (el) => el.textContent,
     href: (el) => el.getAttribute('href'),
     button: (el) => el.tagName === 'BUTTON',
     anchor: (el) => el.tagName === 'A',
     default: (el) => el.classList.contains('default'),
     ariaLabel: (el) => el.ariaLabel,
-    visible: isVisible,
-    focused,
-  },
-  actions: {
-    click: perform((el) => el.click()),
-    focus: perform((el) => el.focus()),
-    blur: perform((el) => el.blur())
-  }
-});
+  });
