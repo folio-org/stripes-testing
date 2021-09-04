e7f2da11701967d4514746ff4640e71ef9509e34
Aleksey Poflankov <87149297+poflankov@users.noreply.github.com>
2021-09-04 13:01:46 +0300

    Use `including` for Pane assertion

--- cypress/integration/users/create-users.spec.js
+++ cypress/integration/users/create-users.spec.js
@@ -1,5 +1,6 @@
 import {
   Pane,
+  including,
 } from '../../../interactors';
 
 describe('Creating user', () => {
@@ -17,7 +18,7 @@ describe('Creating user', () => {
 
     cy.visit('/users/create');
     cy.createUser('Test123', userGroupOption, 'test@folio.org');
-    cy.expect(Pane('Test123 ').is({ visible: true, index: 2 }));
+    cy.expect(Pane(including('Test123')).is({ visible: true, index: 2 }));
 
     cy.get('#pane-userdetails [data-pane-header-actions-dropdown="true"]').click();
     cy.get('#clickable-checkdeleteuser').click();
