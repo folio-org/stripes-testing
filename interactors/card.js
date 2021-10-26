571ef144815e9ef9b082e487fd32982c466d9486
John Coburn <JCoburn@EBSCO.COM>
2021-10-26 08:57:44 -0500

    change name of filter to 'headerStart' vs 'headingStart'

--- interactors/card.js
+++ interactors/card.js
@@ -10,8 +10,8 @@ export default HTML.extend('card')
   .selector('[class^=card-]')
   .locator((el) => el.querySelector('[class^=body]').textContent)
   .filters({
-    headingStart: (el) => el.querySelector('[class^=headerStart-]').textContent,
-    headingEnd: (el) => el.querySelector('[class^=headerEnd-]').textContent,
+    headerStart: (el) => el.querySelector('[class^=headerStart-]').textContent,
+    headerEnd: (el) => el.querySelector('[class^=headerEnd-]').textContent,
     style: (el) => cardStyles.keys()
       .filter(s => el.className.includes(s))[0]
   });
