db8b739afb18fbc1ed39ec658fad80ccf3074f2e
Maksim_Terekhov <maksim_terekhov@epam.com>
2021-11-03 16:12:50 +0300

    FAT-733 added test for action menu

---
+++ cypress/integration/inventory/actions-menu.spec.js
@@ -0,0 +1,28 @@
+import { Button } from '../../../interactors';
+
+describe('ui-inventory: actions', () => {
+  beforeEach('navigates to Inventory', () => {
+    cy.login('diku_admin', 'admin');
+    cy.visit('/inventory');
+  });
+
+  it('verifies action menu options before any search is conducted', () => {
+    cy.do(Button('Actions').click());
+
+    cy.get('#dropdown-clickable-get-items-uiids')
+      .invoke('prop', 'disabled')
+      .should('eq', true);
+
+    cy.get('#dropdown-clickable-get-cql-query')
+      .invoke('prop', 'disabled')
+      .should('eq', true);
+
+    cy.get('#dropdown-clickable-export-marc')
+      .invoke('prop', 'disabled')
+      .should('eq', true);
+
+    cy.get('#dropdown-clickable-show-selected-records')
+      .invoke('prop', 'disabled')
+      .should('eq', true);
+  });
+});
