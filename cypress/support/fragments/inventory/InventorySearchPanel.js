bfbae8cb863770ea172c056a4d3414d962186867
Maksim_Terekhov <maksim_terekhov@epam.com>
2021-11-15 16:24:35 +0300

    FAT-734 added improvements for inventory fragments

---
+++ cypress/support/fragments/inventory/InventorySearchPanel.js
@@ -0,0 +1,8 @@
+import { MultiSelect } from '../../../../interactors';
+
+export default class InventorySearchPanel {
+  static effectiveLocationInput = MultiSelect({ 'id': 'multiselect-6' });
+  static effectiveLocationValues = {
+    'mainLibrary': 'Main Library'
+  }
+}
--- cypress/support/fragments/inventory/actions.js
+++ cypress/support/fragments/inventory/actions.js
@@ -1,15 +1,14 @@
-import {Button} from "../../../../interactors";
+import {Button} from '../../../../interactors';
 
 export default class Actions {
-  static #actions = Button('Actions');
+  static actionsBtn = Button('Actions');
   static saveUUIDOption = '#dropdown-clickable-get-items-uiids';
   static saveCQLQueryOption = '#dropdown-clickable-get-cql-query';
   static exportMARCOption = '#dropdown-clickable-export-marc';
   static showSelectedRecordsOption = '#dropdown-clickable-show-selected-records';
 
-
   static openActions() {
-    cy.do(this.#actions.click());
+    cy.do(this.actionsBtn.click());
   }
 
   static optionIsDisabled(selector, disabled) {
