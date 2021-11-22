f7eeee3171f9061d76fd1e12315175275cbe41a0
Maksim_Terekhov <maksim_terekhov@epam.com>
2021-11-22 16:39:28 +0300

    FAT-734 improve fragments with interactors

--- cypress/integration/inventory/actions-menu.spec.js
+++ cypress/integration/inventory/actions-menu.spec.js
@@ -3,36 +3,34 @@ import Actions from '../../support/fragments/inventory/actions';
 import InventorySearchPanel from '../../support/fragments/inventory/inventorySearchPanel';
 
 describe('ui-inventory: actions', () => {
-  beforeEach('navigates to Actions', () => {
+  beforeEach('navigates to actions', () => {
     cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
     cy.visit(TopMenu.inventoryPath);
   });
 
   it('C196752 verifies action menu options before any search is conducted', () => {
-    const optionsShouldDisabled = [
-      Actions.saveUUIDOption,
-      Actions.saveCQLQueryOption,
-      Actions.exportMARCOption,
-      Actions.showSelectedRecordsOption
-    ];
+    cy.do(Actions.open());
 
-    Actions.open()
-      .then(() => {
-        optionsShouldDisabled.forEach(option => Actions.optionIsDisabled(option, true));
-      });
+    cy.expect([
+      Actions.saveUUIDsOption().is({ disabled: true }),
+      Actions.saveCQLQueryOption().is({ disabled: true }),
+      Actions.exportMARCOption().is({ disabled: true }),
+      Actions.showSelectedRecordsOption().is({ disabled: true }),
+    ]);
   });
 
   it('C196753 verifies action menu options after searching and selecting result', () => {
-    const optionsShouldEnabled = [
-      Actions.saveUUIDOption,
-      Actions.saveCQLQueryOption,
-      Actions.exportMARCOption,
-      Actions.showSelectedRecordsOption,
-    ];
+    cy.do([
+      InventorySearchPanel.effectiveLocationInput.select([InventorySearchPanel.effectiveLocationValues.mainLibrary]),
+      InventorySearchPanel.firstResultCheckbox().click(),
+      Actions.open()
+    ]);
 
-    cy.do(InventorySearchPanel.effectiveLocationInput.select([InventorySearchPanel.effectiveLocationValues.mainLibrary]))
-      .then(() => InventorySearchPanel.selectAllCheckboxResult())
-      .then(() => Actions.open())
-      .then(() => optionsShouldEnabled.forEach(option => Actions.optionIsDisabled(option, false)));
+    cy.expect([
+      Actions.saveUUIDsOption().is({ disabled: false }),
+      Actions.saveCQLQueryOption().is({ disabled: false }),
+      Actions.exportMARCOption().is({ disabled: false }),
+      Actions.showSelectedRecordsOption().is({ disabled: false }),
+    ]);
   });
 });
