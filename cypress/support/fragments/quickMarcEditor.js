cb0755d26ed827b0f1f491068dda7328fbc4e8ca
dmitriyfolio <dmitrii_vagin@epam.com>
2021-12-01 12:44:56 +0300

    initial correct filters to quickmarc editor

--- cypress/support/fragments/quickMarcEditor.js
+++ cypress/support/fragments/quickMarcEditor.js
@@ -1,28 +1,22 @@
 import { read } from '@interactors/html';
-import { QuickMarkEditor } from '../../../interactors';
+import { QuickMarkEditor, QuickMarkEditorRow } from '../../../interactors';
 
 
 export default class QuickMarcEditor {
   static addNewLine() {
-  // cy.then(read(QuickMarkEditor(), 'rowsCount'))//.rowsCount())
-  //   .then(rowsCount => {
+    // example of checking of records presense
+    // cy.expect(QuickMarkEditor().has({ rowsCount: 10 }));
 
+    //--------
+    // Может лучше экшон написать
 
-    //     //cy.log(rowsCount);
+    cy.do(QuickMarkEditor()
+      .find(QuickMarkEditorRow({ index: 1 }))
+      .click());
 
-    //     //cy.pause();
-    //   });
-    // cy.then(QuickMarkEditor().rows())
-    //   .then((rowsCount) => {
-    //     cy.log(rowsCount);
-    //   });
+    cy.pause();
 
-    //cy.expect(QuickMarkEditor().has({ rowsCount: 10 }));
-    //cy.do(QuickMarkEditor().rows()[10].click());
-    cy.do(QuickMarkEditor().rows()[10].click());
-
-
-
+    //----------------
   // QuickMarkEditor().find()
   }
 }
