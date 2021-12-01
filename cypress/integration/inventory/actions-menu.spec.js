fb519d906e3e420cb4306df4c84a0af4dd457724
Maksim_Terekhov <maksim_terekhov@epam.com>
2021-12-01 14:26:05 +0300

    fixed namings

--- cypress/integration/inventory/actions-menu.spec.js
+++ cypress/integration/inventory/actions-menu.spec.js
@@ -1,6 +1,6 @@
 import TopMenu from '../../support/fragments/topMenu';
-import inventorySearch from '../../support/fragments/inventory/inventorySearch';
-import inventoryActions from '../../support/fragments/inventory/inventoryActions';
+import InventorySearch from '../../support/fragments/inventory/inventorySearch';
+import InventoryActions from '../../support/fragments/inventory/inventoryActions';
 import { Checkbox } from '../../../interactors';
 
 describe('ui-inventory: actions', () => {
@@ -10,29 +10,29 @@ describe('ui-inventory: actions', () => {
   });
 
   it('C196752 verifies action menu options before any search is conducted', () => {
-    cy.do(inventoryActions.open());
+    cy.do(InventoryActions.open());
 
-    cy.expect(inventorySearch.getAllSearchResults().absent());
-    inventoryActions.optionsIsDisabled([
-      inventoryActions.options.saveUUIDs,
-      inventoryActions.options.saveCQLQuery,
-      inventoryActions.options.exportMARC,
-      inventoryActions.options.showSelectedRecords
+    cy.expect(InventorySearch.getAllSearchResults().absent());
+    InventoryActions.optionsIsDisabled([
+      InventoryActions.options.saveUUIDs,
+      InventoryActions.options.saveCQLQuery,
+      InventoryActions.options.exportMARC,
+      InventoryActions.options.showSelectedRecords
     ]);
   });
 
   it('C196753 verifies action menu options after searching and selecting result', () => {
     cy.do([
-      inventorySearch.searchByEffectiveLocation(),
-      inventorySearch.getSearchResult().find(Checkbox()).click(),
-      inventoryActions.open()
+      InventorySearch.searchByEffectiveLocation(),
+      InventorySearch.getSearchResult().find(Checkbox()).click(),
+      InventoryActions.open()
     ]);
 
-    inventoryActions.optionsIsEnabled([
-      inventoryActions.options.saveUUIDs,
-      inventoryActions.options.saveCQLQuery,
-      inventoryActions.options.exportMARC,
-      inventoryActions.options.showSelectedRecords
+    InventoryActions.optionsIsEnabled([
+      InventoryActions.options.saveUUIDs,
+      InventoryActions.options.saveCQLQuery,
+      InventoryActions.options.exportMARC,
+      InventoryActions.options.showSelectedRecords
     ]);
   });
 });
