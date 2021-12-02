81dfbdf0e5e604e1cca1f6ee67be5f800aede316
dmitriyfolio <dmitrii_vagin@epam.com>
2021-12-02 17:34:01 +0300

    a few todos and small scenario refactring

--- cypress/integration/data_import/upload-file-create.spec.js
+++ cypress/integration/data_import/upload-file-create.spec.js
@@ -1,38 +1,27 @@
 /// <reference types="cypress" />
-
-import { set } from 'lodash';
+// TODO: recheck that classes has the same name as js files
 import fieldMappingProfiles from '../../support/fragments/data_import/fieldMappingProfiles';
 import NewMappingProfile from '../../support/fragments/data_import/newMappingProfile';
 import TopMenu from '../../support/fragments/topMenu';
 
 
 describe('ui-data-import: MARC file import with creating of the new instance, holding and item', () => {
+  const collectionOfMappingProfiles = [NewMappingProfile.defaultMappingProfile.folioRecordType];
 
-    const collectionOfMappingProfiles = [NewMappingProfile.defaultMappingProfile.folioRecordType];
+  collectionOfMappingProfiles.forEach(mappingProfile => {
+    // TODO: add testid from Testrail
+    it(`create a mapping profile for ${mappingProfile}`, () => {
+      const specialMappingProfile = { ...NewMappingProfile.defaultMappingProfile };
+      specialMappingProfile.folioRecordType = mappingProfile;
 
-    collectionOfMappingProfiles.forEach(mappingProfile => {
-        it('create a mapping profile for Instance', () => {
-//
-            const specialMappingProfile = {... NewMappingProfile.defaultMappingProfile};
-            specialMappingProfile.folioRecordType = mappingProfile;
-            
+      // TODO: move to beforeEach
+      cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+      cy.visit(TopMenu.settingsDataImportPath);
 
-            cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
-            cy.visit(TopMenu.settingsDataImportPath);
-
-            fieldMappingProfiles.clickActionButton();
-            fieldMappingProfiles.createNewMappingProfile();
-            NewMappingProfile.fill(specialMappingProfile);
-            NewMappingProfile.saveAndClose();
-
-
-
-
-
-        });
+      fieldMappingProfiles.clickActionButton();
+      fieldMappingProfiles.createNewMappingProfile();
+      NewMappingProfile.fill(specialMappingProfile);
+      NewMappingProfile.saveAndClose();
     });
-
-
-
-
+  });
 });
