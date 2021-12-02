a3cddd396e6cb516f15f86ff66a65e97b1de7280
Kyrylo Brener <Kyrylo_Brener@epam.com>
2021-12-02 15:41:43 +0200

    minor changes

--- cypress/support/fragments/finance/ledgers/Ledgers.js
+++ cypress/support/fragments/finance/ledgers/Ledgers.js
@@ -28,7 +28,6 @@ export default {
   },
 
   createDefaultLedger(defaultLedger) {
-    cy.expect(newButton.exists());
     cy.do(newButton.click());
     NewLedger.waitLoading();
     NewLedger.fillMandatoryFields(defaultLedger);
