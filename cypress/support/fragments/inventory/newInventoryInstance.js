1bcc69984be6d1ec8ae546aa7f192e735f37093a
dmitriyfolio <dmitrii_vagin@epam.com>
2021-12-02 19:42:41 +0300

    redesign from class to object

--- cypress/support/fragments/inventory/newInventoryInstance.js
+++ cypress/support/fragments/inventory/newInventoryInstance.js
@@ -1,34 +1,29 @@
 import { MultiColumnList, HTML, including, Button } from '../../../../interactors';
 import { getLongDelay } from '../../utils/cypressTools';
 
-export default class NewInventoryInstance {
-    static #validOCLC = { id:'176116217',
-    // TODO: hardcoded count related with interactors getters issue. Redesign to cy.then(QuickMarkEditor().rowsCount()).then(rowsCount => {...}
-      getLastRowNumber:() => 31 };
-
-    static #rootCss = 'section[id=pane-instancedetails]';
-    // TODO: rewrite to interactor with filter by id
-    static #actionsCss = `${this.#rootCss} button[class*=actionMenuToggle]`;
-    static #identifiers = MultiColumnList({ id:'list-identifiers' });
-    static #editMARCBibRecordButton = Button({ id:'edit-instance-marc' });
-    static #viewSourceButton = Button({ id:'clickable-view-source' });
+const rootCss = 'section[id=pane-instancedetails]';
+// TODO: rewrite to interactor with filter by id
+const actionsCss = `${rootCss} button[class*=actionMenuToggle]`;
+const identifiers = MultiColumnList({ id:'list-identifiers' });
+const editMARCBibRecordButton = Button({ id:'edit-instance-marc' });
+const viewSourceButton = Button({ id:'clickable-view-source' });
 
 
-    static get validOCLC() {
-      return this.#validOCLC;
-    }
+export default {
+  // TODO: hardcoded count related with interactors getters issue. Redesign to cy.then(QuickMarkEditor().rowsCount()).then(rowsCount => {...}
+  validOCLC : { id:'176116217',
+    getLastRowNumber:() => 31 },
+  checkExpectedOCLCPresence: (OCLCNumber) => {
+    cy.expect(identifiers.find(HTML(including(OCLCNumber))).exists());
+  },
 
-    static checkExpectedOCLCPresence(OCLCNumber) {
-      cy.expect(this.#identifiers.find(HTML(including(OCLCNumber))).exists());
-    }
+  goToEditMARCBiblRecord:() => {
+    cy.get(actionsCss, getLongDelay()).click();
+    cy.do(editMARCBibRecordButton.click());
+  },
 
-    static goToEditMARCBiblRecord() {
-      cy.get(this.#actionsCss, getLongDelay()).click();
-      cy.do(this.#editMARCBibRecordButton.click());
-    }
-
-    static viewSource() {
-      cy.get(this.#actionsCss, getLongDelay()).click();
-      cy.do(this.#viewSourceButton.click());
-    }
-}
+  viewSource: () => {
+    cy.get(actionsCss, getLongDelay()).click();
+    cy.do(viewSourceButton.click());
+  }
+};
