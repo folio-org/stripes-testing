096c5b7f95b41985d619412f6cd68848fbfe1fd7
dmitriyfolio <dmitrii_vagin@epam.com>
2021-12-03 16:41:26 +0300

    resolve merge conflicts

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,33 @@
-// eslint-disable-next-line no-unused-vars
+const globby = require('globby');
+const { rmdir } = require('fs');
+
 module.exports = (on, config) => {
+  on('task', {
+    // a task to find files matching the given mask
+    async findFiles(mask) {
+      if (!mask) {
+        throw new Error('Missing a file mask to search');
+      }
+
+      const list = await globby(mask);
+
+      if (!list.length) {
+        return null;
+      }
+
+      return list;
+    },
+
+    deleteFolder(folderName) {
+      return new Promise((resolve, reject) => {
+        rmdir(folderName, { maxRetries: 10, recursive: true }, (err) => {
+          if (err && err.code !== 'ENOENT') {
+            return reject(err);
+          }
+
+          resolve(null);
+        });
+      });
+    }
+  });
 };
---
+++ cypress/support/utils/fileManager.js
@@ -0,0 +1,17 @@
+// downloadsFolder - cypress variable, it's value = "cypress/downloads"
+const downloadsFolder = Cypress.config('downloadsFolder');
+
+export default {
+  findDownloadedFilesByMask(mask) {
+    // more about masks: https://github.com/isaacs/minimatch#usage
+    return cy.task('findFiles', `${downloadsFolder}/${mask}`);
+  },
+
+  deleteFolder(pathToFolder) {
+    return cy.task('deleteFolder', pathToFolder);
+  },
+
+  readFile(pathToFile) {
+    return cy.readFile(pathToFile);
+  }
+};
