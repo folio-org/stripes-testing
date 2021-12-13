8aa4793733552d87b56e54b88486f1be21c6ae4b
Maksim_Terekhov <maksim_terekhov@epam.com>
2021-12-13 15:10:24 +0300

    FAT-737 improve file manager

--- cypress/support/utils/fileManager.js
+++ cypress/support/utils/fileManager.js
@@ -3,7 +3,13 @@ const downloadsFolder = Cypress.config('downloadsFolder');
 export default {
   findDownloadedFilesByMask(mask) {
     // more about masks: https://github.com/isaacs/minimatch#usage
+    // returns array with all files matched to mask
     return cy.task('findFiles', `${downloadsFolder}/${mask}`);
+  },
+
+  getLastDownloadedFileName(downloadedFilenames) {
+    // returns only one last downloaded file from array of files
+    return downloadedFilenames.sort()[downloadedFilenames.length - 1];
   },
 
   deleteFolder(pathToFolder) {
