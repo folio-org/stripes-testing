6fc7127135e1a3bef69b72dae5efcc00eeac0c46
dmitriyfolio <dmitrii_vagin@epam.com>
2021-12-22 19:42:01 +0300

    C16994- current state

--- cypress/integration/eholdings/eholdings-titles.spec.js
+++ cypress/integration/eholdings/eholdings-titles.spec.js
@@ -1,20 +1,26 @@
 /// <reference types="cypress" />
 
 import eHoldingSearch from '../../support/fragments/eholdings/eHoldingsSearch';
+import eHoldingsTitle from '../../support/fragments/eholdings/eHoldingsTitle';
 import eHoldingsTitles from '../../support/fragments/eholdings/eHoldingsTitles';
 import TopMenu from '../../support/fragments/topMenu';
 import { testType, feature } from '../../support/utils/tagTools';
 
 describe('eHoldings titles management', () => {
-  // TODO: specify new user with permissions eHoldings: eHoldings: Can view providers, packages, titles detail records
-  it('C684 Title Search: Search titles for chemical engineering. Then filter results to journals.', { tags:  [testType.smoke, feature.eHoldings] }, () => {
-    // TODO: add support of special permissions in special account
+  beforeEach(() => {
+    // TODO: specify new user with permissions eHoldings: eHoldings: Can view providers, packages, titles detail records
     cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
     cy.visit(TopMenu.eholding);
     eHoldingSearch.switchToTitles();
     eHoldingSearch.bySubject('chemical engineering');
     eHoldingSearch.byPublicationType('Journal');
+  });
+  it('C684 Title Search: Search titles for chemical engineering. Then filter results to journals.', { tags:  [testType.smoke, feature.eHoldings] }, () => {
     eHoldingsTitles.openTitle();
     // TODO: the issue with filtering by subject, continue after issue UIEH-1225 solving
+  });
+  it.only('C16994 Add a title in a package to holdings', { tags:  [testType.smoke, feature.eHoldings] }, () => {
+    eHoldingsTitles.openTitle();
+    eHoldingsTitle.filterPackages();
   });
 });
--- cypress/support/fragments/eholdings/eHoldingsTitle.js
+++ cypress/support/fragments/eholdings/eHoldingsTitle.js
@@ -1,7 +1,26 @@
-import { Section } from '../../../../interactors';
+import { Accordion, Button, Modal, Section, RadioButton, including } from '../../../../interactors';
+
+const packagesSection = Section({ id: 'titleShowPackages' });
+const packageFilterModal = Modal({ id : 'package-filter-modal' });
+
+const filterPackagesStatuses = { all: 'All',
+  selected: 'Selected',
+  notSelected: 'Not selected' };
 
 export default {
   waitLoading: (specialTitle) => {
     cy.expect(Section({ id : specialTitle.replaceAll(' ', '-').toLowerCase() }).exists());
+  },
+
+  filterPackagesStatuses,
+
+  filterPackages: (selectionStatus) => {
+    const selectionStatusAccordion = packageFilterModal.find(Accordion({ id: 'filter-packages-selected' }));
+    cy.do(packagesSection.find(Button({ icon: 'search' })).click());
+    cy.expect(packageFilterModal.exists());
+    cy.do(selectionStatusAccordion
+      .find(RadioButton(selectionStatus ?? filterPackagesStatuses.notSelected)).click());
+    cy.do(packageFilterModal.find(Button('Search')).click());
+    cy.expect(packageFilterModal.absent());
   }
 };
