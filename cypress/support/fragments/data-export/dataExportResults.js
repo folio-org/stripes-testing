6b57c0e054cc6045144ac1baa85f8a1b4c16e43e
Maksim_Terekhov <maksim_terekhov@epam.com>
2021-12-28 16:03:18 +0300

    FAT-740 replace cy.get to interactor

--- cypress/support/fragments/data-export/dataExportResults.js
+++ cypress/support/fragments/data-export/dataExportResults.js
@@ -1,12 +1,18 @@
+import InventorySearch from '../inventory/inventorySearch';
+
 const defaultJobProfile = 'Default instances export job profile';
 const quickExportFileNameMask = /quick-export-\d{1,3}.mrc/gm;
 
+
 export default {
   verifyQuickExportResult() {
-    // TODO: need table interactor with rows (mclRowContainer)
-    cy.get('[data-row-index="row-0"]').then(elem => {
-      expect(elem.text()).to.match(quickExportFileNameMask);
-      expect(elem.text()).to.have.string(defaultJobProfile);
-    });
+    cy.do([
+      InventorySearch.getSearchResult(0, 0).perform(element => {
+        expect(element.innerText).to.match(quickExportFileNameMask);
+      }),
+      InventorySearch.getSearchResult(0, 4).perform(element => {
+        expect(element.innerText).to.equals(defaultJobProfile);
+      }),
+    ]);
   }
 };
