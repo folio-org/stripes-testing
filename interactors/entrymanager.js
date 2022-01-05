75ab7d7bca646f295d67ae557a6ee9e18a25d931
John Coburn <JCoburn@EBSCO.COM>
2022-01-05 16:58:04 -0600

    add/export entrymanager and navlist interactors

---
+++ interactors/entrymanager.js
@@ -0,0 +1,18 @@
+import { Link, Button } from '@interactors/html';
+import HTML from './baseHTML';
+
+export const EntryManager = HTML.extend('Entry Manager')
+  .selector('[data-test-entry-manager]')
+  .actions({
+    navTo: ({ find }, linkText) => find(Link(linkText)).click(),
+    createEntry: ({ find }) => find(Button({ id: 'clickable-create-entry' })).click(),
+  });
+
+export const EntryForm = HTML.extend('Entry Form')
+  .selector('[data-test-entry-form]')
+  .actions({
+    save: ({ find }) => find(Button('Save')).click()
+  });
+
+export const EntryDetails = HTML.extend('Entry Details')
+  .selector('[data-test-detail-section]');
