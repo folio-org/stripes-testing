a6e92005796b6231d66207290458417aefe7d133
Maxim Terekhov <maksim_terekhov@epam.com>
2022-01-12 11:00:17 +0300

    FAT-739 test added (#181)

--- cypress/integration/inventory/selected-records.spec.js
+++ cypress/integration/inventory/selected-records.spec.js
@@ -3,7 +3,7 @@ import InventorySearch from '../../support/fragments/inventory/inventorySearch';
 import InventoryModals from '../../support/fragments/inventory/inventoryModals';
 import { testType } from '../../support/utils/tagTools';
 
-describe('inventory: selecting records', () => {
+describe('inventory: selecting / changing records', () => {
   beforeEach('navigates to Inventory', () => {
     cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
     cy.visit(TopMenu.inventoryPath);
@@ -22,10 +22,25 @@ describe('inventory: selecting records', () => {
 
     InventorySearch.byEffectiveLocation();
     InventorySearch.selectResultCheckboxes(selectedRecords);
+    InventorySearch.verifySelectedRecords(selectedRecords);
     InventorySearch.showSelectedRecords();
 
     InventoryModals.verifySelectedRecords(selectedRecords);
     InventoryModals.verifySelectedRecordsCount(selectedRecords);
     InventoryModals.verifyButtons();
+  });
+
+  it('C196756 verify selected records after changing selection', () => {
+    const selectedRecords = 3;
+    const unselectedRecords = 1;
+
+    InventorySearch.byEffectiveLocation();
+    InventorySearch.selectResultCheckboxes(selectedRecords);
+    InventorySearch.verifySelectedRecords(selectedRecords);
+    InventorySearch.showSelectedRecords();
+
+    InventoryModals.clickOnCheckboxes(unselectedRecords);
+    InventoryModals.save();
+    InventorySearch.verifySelectedRecords(selectedRecords - unselectedRecords);
   });
 });
