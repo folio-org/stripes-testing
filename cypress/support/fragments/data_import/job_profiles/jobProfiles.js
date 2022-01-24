e8d65ca6152652a3c4ad1a42c8bea32ad55f864c
OleksandrHladchenko1 <85172747+OleksandrHladchenko1@users.noreply.github.com>
2022-01-24 11:02:35 +0200

    Fat-756: Implement e-2-e automation of test case C17044 (#195)
    * Refactor changes
    
    * Remove comments
    
    * Fix instanceHRID problem
    
    * Stable working version
    
    * Fix after review
    
    * Update CHANGELOG.md
    
    * Fix match profile fields
    
    * Add tag with test type

--- cypress/support/fragments/data_import/job_profiles/jobProfiles.js
+++ cypress/support/fragments/data_import/job_profiles/jobProfiles.js
@@ -1,7 +1,21 @@
 import { Button, TextField, MultiColumnListCell, Modal } from '../../../../../interactors';
 import { getLongDelay } from '../../../utils/cypressTools';
+import NewJobProfile from './newJobProfile';
 
 const actionsButton = Button('Actions');
+
+const openNewJobProfileForm = () => {
+  cy.do([
+    actionsButton.click(),
+    Button('New job profile').click(),
+  ]);
+};
+
+const waitLoadingList = () => {
+  cy.get('[id="job-profiles-list"]', getLongDelay())
+    .should('be.visible');
+  cy.expect(actionsButton.exists());
+};
 
 const deleteJobProfile = (profileName) => {
   // get all job profiles
@@ -33,19 +47,6 @@ const deleteJobProfile = (profileName) => {
 };
 
 export default {
-  openNewJobProfileForm: () => {
-    cy.do([
-      actionsButton.click(),
-      Button('New job profile').click(),
-    ]);
-  },
-
-  waitLoadingList:() => {
-    cy.get('[id="job-profiles-list"]', getLongDelay())
-      .should('be.visible');
-    cy.expect(actionsButton.exists());
-  },
-
   checkJobProfilePresented:(jobProfileTitle) => {
     cy.get('[id="job-profiles-list"]')
       .should('contains.text', jobProfileTitle);
@@ -69,4 +70,16 @@ export default {
   },
 
   deleteJobProfile,
+
+  createJobProfile: (jobProfile, actionProfileName, matchProfileName) => {
+    openNewJobProfileForm();
+    NewJobProfile.fillJobProfile(jobProfile);
+    if (!matchProfileName) {
+      NewJobProfile.linkActionProfile(actionProfileName);
+    } else {
+      NewJobProfile.linkMatchAndActionProfiles(matchProfileName, actionProfileName);
+    }
+    NewJobProfile.clickSaveAndCloseButton();
+    waitLoadingList();
+  },
 };
