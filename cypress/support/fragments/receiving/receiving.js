98db5de6cbda0bf71f7a8b6355f4e201ef5bde6d
Kyrylo Brener <58509534+KyryloBrener@users.noreply.github.com>
2022-01-31 15:34:02 +0200

    C10925 added (#200)

--- cypress/support/fragments/receiving/receiving.js
+++ cypress/support/fragments/receiving/receiving.js
@@ -10,14 +10,17 @@ import Helper from '../finance/financeHelper';
 
 const actionsButton = Button('Actions');
 const receivingSuccessful = 'Receiving successful';
+const unreceivingSuccessful = 'Unreceiving successful';
+const expectedPiecesAccordionId = 'expected';
+const receivedPiecesAccordionId = 'received';
 
 export default {
 
   receivePiece: (rowNumber, caption, barcode) => {
     const recievingFieldName = `receivedItems[${rowNumber}]`;
-    cy.expect(Accordion({ id: 'expected' }).exists());
+    cy.expect(Accordion({ id: expectedPiecesAccordionId }).exists());
     cy.do([
-      Accordion({ id: 'expected' }).find(actionsButton).click(),
+      Accordion({ id: expectedPiecesAccordionId }).find(actionsButton).click(),
       Button('Receive').click(),
       Checkbox({ name: `${recievingFieldName}.checked` }).clickInput(),
       TextField({ name: `${recievingFieldName}.caption` }).fillIn(caption),
@@ -29,11 +32,31 @@ export default {
 
   checkReceivedPiece: (rowNumber, caption, barcode) => {
     cy.do([
-      cy.expect(Accordion({ id: 'received' })
+      cy.expect(Accordion({ id: receivedPiecesAccordionId })
         .find(MultiColumnListRow({ index: rowNumber }))
         .find(MultiColumnListCell({ content: barcode })).exists()),
 
-      cy.expect(Accordion({ id: 'received' })
+      cy.expect(Accordion({ id: receivedPiecesAccordionId })
+        .find(MultiColumnListRow({ index: rowNumber }))
+        .find(MultiColumnListCell({ content: caption })).exists())
+    ]);
+  },
+
+  unreceivePiece: (rowNumber = 0) => {
+    const recievingFieldName = `receivedItems[${rowNumber}]`;
+    cy.expect(Accordion({ id: receivedPiecesAccordionId }).exists());
+    cy.do([
+      Accordion({ id: receivedPiecesAccordionId }).find(actionsButton).click(),
+      Button('Unreceive').click(),
+      Checkbox({ name: `${recievingFieldName}.checked` }).clickInput(),
+      Button('Unreceive').click(),
+    ]);
+    Helper.checkCalloutMessage(unreceivingSuccessful, 'success');
+  },
+
+  checkUnreceivedPiece: (rowNumber = 0, caption) => {
+    cy.do([
+      cy.expect(Accordion({ id: expectedPiecesAccordionId })
         .find(MultiColumnListRow({ index: rowNumber }))
         .find(MultiColumnListCell({ content: caption })).exists())
     ]);
