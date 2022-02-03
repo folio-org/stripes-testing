3c5d0ea5eae6aa949145550a5aad67b4e2add1ef
Kyrylo Brener <Kyrylo_Brener@epam.com>
2022-02-03 14:26:57 +0200

    C2326 added

---
+++ cypress/support/fragments/invoices/newInvoiceLine.js
@@ -0,0 +1,9 @@
+import getRandomPostfix from '../../utils/stringTools';
+
+export default {
+  defaultUiInvoiceLine : {
+    description: `autotest description ${getRandomPostfix()}`,
+    quantity: 5,
+    subTotal: 2
+  }
+};
