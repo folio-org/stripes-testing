60eda59eeff8b55698e81a3dbd0733174b3acae6
dmitriyfolio <dmitrii_vagin@epam.com>
2022-02-07 16:54:43 +0300

    checking of delete button

--- cypress/support/fragments/inventory/InventoryInstanceEdit.js
+++ cypress/support/fragments/inventory/InventoryInstanceEdit.js
@@ -1,4 +1,4 @@
-import { Accordion, Button, Section, Select, TextArea, TextField, TextInput, FieldSet } from '../../../../interactors';
+import { Accordion, Button, Section, Select, TextArea, TextField, TextInput, FieldSet, Checkbox, FieldInFieldset } from '../../../../interactors';
 
 const closeButton = Button({ icon: 'times' });
 const rootSection = Section({ id: 'instance-form' });
@@ -9,6 +9,7 @@ export default {
   waitLoading:() => cy.expect(Section({ id: 'instance-form' }).exists()),
   // related with Actions->Overlay
   checkReadOnlyFields() {
+    const deleteInFieldsetButton = Button({ ariaLabel: 'remove fields for ' });
     function getRegularElements() {
       return Object.values(this).filter(element => typeof element !== 'function');
     }
@@ -19,7 +20,8 @@ export default {
     };
 
     const readonlyButtons = {
-      alernativeTitles: rootSection.find(Button('Add alternative title')),
+      // already checked in scope of fieldset alternativeTitles checking
+      // alernativeTitles: rootSection.find(Button('Add alternative title')),
       series: rootSection.find(Button('Add series')),
       editions: rootSection.find(Button('Add edition')),
       getRegularElements
@@ -46,17 +48,20 @@ export default {
       classifications: rootSection.find(FieldSet('Classification')),
       precedingTitles: rootSection.find(FieldSet('Preceding titles')),
       succeedingTitles: rootSection.find(FieldSet('Succeeding titles')),
+      // related with fields Type and Alternative title
+      alternativeTitles: rootSection.find(FieldSet('Alternative titles')),
       getRegularElements
     };
 
     const readonlyTextAreas = {
-      resourceTitle: rootSection.find(TextArea('Resource title')),
+      resourceTitle: rootSection.find(TextArea('Resource title*')),
       indexTitle: rootSection.find(TextArea('Index title')),
       getRegularElements
     };
 
     const readonlySelects = {
-      type:rootSection.find(Select('Type*')),
+      // already checked in scope of fieldset alternativeTitles checking
+      // type:rootSection.find(FieldSet('Alternative titles')).find(Select('Type*')),
       modeOfIssuance:rootSection.find(Select('Mode of issuance')),
       getRegularElements
     };
@@ -67,7 +72,6 @@ export default {
     readonlyButtons.getRegularElements().forEach(element => {
       cy.expect(element.has({ disabled: true }));
     });
-    // TODO: add button Delete
     readonlyAccordions.getRegularElements().forEach(accordion => {
       // check textareas
       cy.then(() => accordion.textareaNames()).then(textareaNames => {
@@ -78,11 +82,6 @@ export default {
           selectNames.forEach(selectName => cy.expect(accordion.find(Select({ name: selectName })).has({ disabled: true })));
         })
       // check buttons
-      // // TODO: add special unique attribute to each delete button
-      // .then(() => accordion.buttonAriaLabels())
-        // .then(buttonAriaLabels => {
-        //   buttonAriaLabels.forEach(buttonAriaLabel => cy.expect(accordion.find(Button({ ariaLabel: buttonAriaLabel })).has({ disabled: true })));
-        // })
         .then(() => accordion.buttonIds())
         .then(buttonIds => {
           buttonIds.forEach(buttonId => cy.expect(accordion.find(Button({ id: buttonId })).has({ disabled: true })));
@@ -91,14 +90,19 @@ export default {
         .then(buttonTexts => {
           buttonTexts.forEach(buttonText => cy.expect(accordion.find(Button({ text : buttonText })).has({ disabled: true })));
         })
+        // check state of button delete in each field of fieldset into each accordion
+        .then(() => accordion.find(FieldSet()).feildsCount())
+        .then(fieldsCount => {
+          for (let i = 0; i < fieldsCount; i++) {
+            accordion.find(FieldSet()).find(FieldInFieldset({ fieldByIndex: i })).find(deleteInFieldsetButton).has({ disabled: true });
+          }
+        })
       // check textfields
         .then(() => accordion.inputNames())
         .then(inputNames => {
           inputNames.forEach(inputName => cy.expect(accordion.find(TextField({ name: inputName })).has({ disabled: true })));
         });
     });
-    // TODO: add checkboxes
-    // TODO: add button delete
     readonlyFieldsets.getRegularElements().forEach(fieldset => {
       // check textfields
       cy.then(() => fieldset.inputNames())
@@ -110,19 +114,34 @@ export default {
           selectNames.forEach(selectName => cy.expect(fieldset.find(Select({ name: selectName })).has({ disabled: true })));
         })
       // check buttons
-      // // TODO: add special unique attribute to each delete button
-      // .then(() => accordion.buttonAriaLabels())
-        // .then(buttonAriaLabels => {
-        //   buttonAriaLabels.forEach(buttonAriaLabel => cy.expect(accordion.find(Button({ ariaLabel: buttonAriaLabel })).has({ disabled: true })));
-        // })
+      // check state of button delete in each field of fieldset into each accordion
+        .then(() => fieldset.feildsCount())
+        .then(fieldsCount => {
+          for (let i = 0; i < fieldsCount; i++) {
+            fieldset.find(FieldInFieldset({ fieldByIndex: i })).find(deleteInFieldsetButton).has({ disabled: true });
+          }
+        })
         .then(() => fieldset.buttonIds())
         .then(buttonIds => {
           buttonIds.forEach(buttonId => cy.expect(fieldset.find(Button({ id: buttonId })).has({ disabled: true })));
-        });
+        })
       // check textareas
-      cy.then(() => fieldset.textareaNames()).then(textareaNames => {
-        textareaNames.forEach(textareaName => cy.expect(fieldset.find(TextArea({ name: textareaName })).has({ disabled: true })));
-      });
+        .then(() => fieldset.textareaNames())
+        .then(textareaNames => {
+          textareaNames.forEach(textareaName => cy.expect(fieldset.find(TextArea({ name: textareaName })).has({ disabled: true })));
+        })
+        // check checkboxes
+        .then(() => fieldset.checkboxLabels())
+        .then(checkboxLabels => {
+          checkboxLabels.forEach(checkboxLabel => cy.expect(fieldset.find(Checkbox(checkboxLabel)).has({ disabled: true })));
+        });
+    });
+    readonlyTextAreas.getRegularElements().forEach(textArea => {
+      cy.expect(textArea.has({ disabled: true }));
+    });
+
+    readonlySelects.getRegularElements().forEach(select => {
+      cy.expect(select.has({ disabled: true }));
     });
   }
 };
--- interactors/fieldset.js
+++ interactors/fieldset.js
@@ -1,21 +1,27 @@
 import HTML from './baseHTML';
 
 
-function label(element) {
-  const labelEl = element.querySelector('legend');
-  return labelEl ? labelEl.textContent.trim() : '';
+function legend(element) {
+  const legendEl = element.querySelector('legend');
+  return legendEl ? legendEl.textContent.trim() : '';
 }
 
-export default HTML.extend('fieldset')
+export const FieldSet = HTML.extend('fieldset')
   .selector('fieldset')
-  .locator(label)
+  .locator(legend)
   .filters({
     // related with common checks of accordion elements(disabled for example)
     textareaNames: el => [...el.querySelectorAll('textarea')].map(textarea => textarea.getAttribute('name')),
     selectNames: el => [...el.querySelectorAll('select')].map(select => select.getAttribute('name')),
-    // TODO: add special unique attribute to each delete button
-    // buttonAriaLabels: el => [...el.querySelectorAll('div[class^=content-] button')].map(button => button.getAttribute('aria-label')),
     buttonIds: el => [...el.querySelectorAll('div[class^=content-] button')].map(button => button.getAttribute('id')).filter(id => id),
-    //buttonContainsText: el => [...el.querySelectorAll('div[class^=content-] button')].map(button => button.textContent).filter(id => id),
     inputNames: el => [...el.querySelectorAll('input:not([type=checkbox])')].map(input => input.getAttribute('name')),
+    checkboxLabels: el => [...el.querySelectorAll('div[class^=checkbox-]>label')].map(input => input.textContent),
+    feildsCount: el => [...el.children].length,
+  });
+
+
+export const FieldInFieldset = HTML.extend('fieldInfieldset')
+  .selector('fieldset>div')
+  .filters({
+    fieldByIndex: el => [...el.parentElement.children].indexOf(el),
   });
