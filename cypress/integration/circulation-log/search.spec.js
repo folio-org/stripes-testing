570645aba00cad8f2bb9954b7c9976483f4ca79f
Maksim_Terekhov <maksim_terekhov@epam.com>
2022-02-07 16:23:18 +0300

    FAT-1192 test added

---
+++ cypress/integration/circulation-log/search.spec.js
@@ -0,0 +1,29 @@
+import TopMenu from '../../support/fragments/topMenu';
+import TestTypes from '../../support/dictionary/testTypes';
+import SearchPane from '../../support/fragments/circulation-log/searchPane';
+import { MultiColumnListCell } from '../../../interactors';
+
+
+describe('navigate to circulation log', () => {
+  beforeEach('login', () => {
+    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+    cy.visit(TopMenu.circulationLogPath);
+  });
+
+
+  it('C15484 Filter circulation log on item barcode', { tags: [TestTypes.smoke] }, () => {
+    SearchPane.searchByLoan();
+
+    cy.do(
+      // get existing item barcode from results
+      MultiColumnListCell({ row: 0, columnIndex: 1 }).perform(element => {
+        const itemBarcode = element.textContent;
+
+        SearchPane.resetFilters();
+        SearchPane.searchByItemBarcode(itemBarcode);
+      })
+    );
+
+    SearchPane.verifyResultCells();
+  });
+});
