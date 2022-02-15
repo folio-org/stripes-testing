55a056f0e2d413805e8c079f7156c255c24e6ecd
Maksim_Terekhov <maksim_terekhov@epam.com>
2022-02-15 16:51:01 +0300

    FAT-1196 test added

---
+++ cypress/integration/settings/remote-storage/configure-remote-storage.spec.js
@@ -0,0 +1,27 @@
+import TopMenu from '../../../support/fragments/topMenu';
+import RemoteStorageHelper from '../../../support/fragments/settings/remote-storage/configuration';
+import getRandomPostfix from '../../../support/utils/stringTools';
+
+describe('export instance records', () => {
+  beforeEach('login', () => {
+    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+    cy.visit(TopMenu.remoteStorageConfigurationPath);
+  });
+
+  // parametrized providers
+  [
+    RemoteStorageHelper.providers.CaiaSoft,
+    RemoteStorageHelper.providers.DematicEMS,
+    RemoteStorageHelper.providers.DematicStagingDirector
+  ].forEach(provider => {
+    it('C163919 configure remote storage', () => {
+      const name = 'TestName' + getRandomPostfix();
+
+      provider.create(name);
+      RemoteStorageHelper.verifyCreatedRemoteStorage(name, provider);
+      RemoteStorageHelper.tryToEditRemoteStorage(name);
+      RemoteStorageHelper.verifyCreatedRemoteStorage(name, provider);
+      provider.delete(name);
+    });
+  });
+});
