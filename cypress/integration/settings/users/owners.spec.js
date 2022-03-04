5fbcdfc4c4a33185bf0e25b1c78f63f1ca681d0b
dmitriyfolio <dmitrii_vagin@epam.com>
2022-03-04 10:32:55 +0300

    current state(not debugged)

--- cypress/integration/settings/users/owners.spec.js
+++ cypress/integration/settings/users/owners.spec.js
@@ -4,21 +4,32 @@ import UsersOwners from '../../../support/fragments/settings/users/usersOwners';
 // import Permissions from '../../../support/dictionary/permissions';
 
 describe('Management of n fee/fine owners and service points', () => {
-  let userId = '';
+  const users = [];
   beforeEach(() => {
-    cy.createTempUser(['Settings (Users): Can create, edit and remove owners']).then(userProperties => {
-      userId = userProperties.userId;
-      cy.login(userProperties.username, userProperties.password);
+    cy.createTempUser(['Settings (Users): Can create, edit and remove owners',
+      'Users: Can assign and unassign service points to users']).then(firstUserProperties => {
+      users.add({ userId:firstUserProperties.userId,
+        userName: firstUserProperties.username });
+      cy.login(firstUserProperties.userme, firstUserProperties.password);
       cy.visit(SettingsMenu.usersOwnersPath);
       UsersOwners.waitLoading();
     });
-  });
 
-  it('C6530 Create notice policy', { tags: [TestType.smoke] }, () => {
-    
+    it('C441  Only one Fee/Fine Owner is allowed to be associated to a Service Point', { tags: [TestType.smoke] }, () => {
+      UsersOwners.addNewLine();
+      UsersOwners.getUsedServicePoints().then(usedServicePoints => {
+        const freeServicePoint = usedServicePoints.filter(servicePoint => !usedServicePoints.contains(servicePoint))[0];
+        UsersOwners.fill(users[0].userName, UsersOwners.defaultServicePoints.filter(freeServicePoint));
+        UsersOwners.save();
+      });
+      // cy.createTempUser(['Settings (Users): Can create, edit and remove owners',
+      //   'Users: Can assign and unassign service points to users']).then(secondUserProperties => {
+      //   userIds.add(secondUserProperties.userId);
+      // });
+    });
   });
 
   afterEach(() => {
-    cy.deleteUser(userId);
+    // cy.deleteUser(userId);
   });
 });
--- cypress/support/fragments/settings/users/usersOwners.js
+++ cypress/support/fragments/settings/users/usersOwners.js
@@ -1,7 +1,29 @@
-import { PaneHeader } from '../../../../../interactors';
+import { PaneHeader, Button, MultiColumnListCell, MultiColumnListRow, MultiColumnList, TextField, MultiSelect, PaneSet } from '../../../../../interactors';
 
+const rootPaneset = PaneSet({ id:'settings-owners' });
+const addButton = rootPaneset.find(Button({ id:'clickable-add-settings-owners' }));
 
+const defaultServicePoints = ['Circ Desk 1',
+  'Circ Desk 2',
+  'Online'];
 
 export default {
-  waitLoading:() => cy.expect(PaneHeader({id: ''})),
+  waitLoading:() => cy.expect(PaneHeader('Fee/fine: Owners').exists()),
+  addNewLine: () => cy.do(addButton.click()),
+  defaultServicePoints,
+  getUsedServicePoints: () => {
+    cy.then(rootPaneset.find(MultiColumnList()).rowCount()).then(rowsCount => {
+      const usedServicePoints = [];
+      for (let i = 0; i < rowsCount; i++) {
+        cy.then(rootPaneset.find(MultiColumnListCell({ index:2, row:i })).content())
+          .then(servicePointName => usedServicePoints.add(servicePointName));
+      }
+      return usedServicePoints;
+    });
+  },
+  fill: (userName, servicePoint) => {
+    cy.do(rootPaneset.find(TextField({ name:'items[0].owner' })).fillIn(userName));
+    cy.do(rootPaneset.find(MultiSelect('owner-service-point-label')).select(servicePoint));
+  },
+  save:(rowNumber = 0) => cy.do(rootPaneset.find(Button({ id:`clickable-save-settings-owners-${rowNumber}` })).click())
 };
