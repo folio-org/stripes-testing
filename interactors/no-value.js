1dac26607f51ed1ac4c8ab1c1cb1fc156d41bcfb
Zak Burke <zburke@ebsco.com>
2022-03-08 09:42:07 -0500

    omit unused locator

--- interactors/no-value.js
+++ interactors/no-value.js
@@ -2,7 +2,6 @@ import { createInteractor } from '@interactors/html';
 
 export default createInteractor('no value')
   .selector('[data-test-no-value]')
-  // .locator((el) => el.querySelector('[data-test-no-value]').textContent)
   .filters({
     ariaValue: (el) => el.querySelector('span[class^="sr-only"]').textContent,
     emptyScreenValue: (el) => el.querySelector('span[aria-hidden]').textContent === '-',
