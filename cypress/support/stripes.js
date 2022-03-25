30d57bb5c53b65021c547e8590d5183e3613fb4e
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2022-03-25 10:59:39 +0200

    greening-C347615 (#290)
    * added timeout
    
    * changed waiter and env
    
    * added waiter
    
    * added variable to cypress com getToken
    
    * added variable to getToken command
    
    * resolved conflicts
    
    * added permission
    
    * deleted path
    
    * changed variables

--- cypress/support/stripes.js
+++ cypress/support/stripes.js
@@ -9,13 +9,14 @@ Cypress.Commands.add('okapiRequest', ({ method = 'GET',
   body,
   isDefaultSearchParamsRequired = true }) => {
   const initialParams = new URLSearchParams({ ...searchParams });
+  const cypressEnvPath = `${Cypress.env('OKAPI_HOST')}/${path}`;
   if (isDefaultSearchParamsRequired) {
     Object.entries(DEFAULT_SEARCH_PARAMS).forEach(([key, value]) => initialParams.append(key, value));
   }
   const queryString = (initialParams).toString();
   cy.request({
     method,
-    url: `${Cypress.env('OKAPI_HOST')}/${path}?${queryString}`,
+    url: queryString ? cypressEnvPath`?${queryString}` : cypressEnvPath,
     headers: {
       'x-okapi-tenant': Cypress.env('OKAPI_TENANT'),
       'x-okapi-token': Cypress.env('token'),
