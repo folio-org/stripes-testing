8e4ace5482038b62fc43f389e4ed86f738c18120
dmitriyfolio <dmitrii_vagin@epam.com>
2022-03-30 19:51:11 +0300

    add link to issue

--- cypress/support/fragments/eholdings/eHoldingsPackages.js
+++ cypress/support/fragments/eholdings/eHoldingsPackages.js
@@ -46,7 +46,7 @@ export default {
     return cy.get('@customePackageName');
   },
   getNotCustomSelectedPackageIdViaApi:() => {
-    // TODO: issue related with filter[selected] https://issues.folio.org/browse/MODKBEKBJ-630
+    // TODO: issue related with filter[custom] https://issues.folio.org/browse/MODKBEKBJ-630
     cy.okapiRequest({ path: 'eholdings/packages',
       searchParams: { 'filter[selected]':true, count:100, pageSize:100 },
       isDefaultSearchParamsRequired : false }).then(({ body }) => {
@@ -61,7 +61,7 @@ export default {
     return cy.get('@packageId');
   },
   getNotSelectedPackageIdViaApi:() => {
-    // TODO: add issue related with filter[selected]
+    // TODO: issue related with filter[custom] https://issues.folio.org/browse/MODKBEKBJ-630
     cy.okapiRequest({ path: 'eholdings/packages',
       searchParams: { 'filter[selected]':false, count:100, pageSize:100 },
       isDefaultSearchParamsRequired : false }).then(({ body }) => {
