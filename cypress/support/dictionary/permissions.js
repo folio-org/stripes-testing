8f988e5892251f200cbf6bc4e72ca8af2ea4cd45
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2022-03-31 11:45:30 +0300

    Implement e-2-e automation of test case C505 (#302)
    * added timeout
    
    * changed waiter and env
    
    * added waiter
    
    * added variable to cypress com getToken
    
    * added waiters
    
    * added waiter 2 min
    
    * added waiter 150
    
    * added waiter and fix interractor
    
    * added test create edit delete material type
    
    * added delete user
    
    * fixed commit
    
    * added deleted permission
    
    * changed according comment
    
    * changed according to comments
    
    * changed according comment
    
    Co-authored-by: dmitriyfolio <93538670+dmitriyfolio@users.noreply.github.com>

--- cypress/support/dictionary/permissions.js
+++ cypress/support/dictionary/permissions.js
@@ -25,6 +25,7 @@ export default {
   inventoryAll: { internal: ['inventory.all', 'ui-inventory.all-permissions.TEMPORARY'], gui: 'Inventory: All permissions' },
   uiInventorySingleRecordImport: { internal: 'ui-inventory.single-record-import', gui: 'Inventory: Import single bibliographic records' },
   uiInventorySettingsFastAdd: { internal: 'ui-inventory.settings.fast-add', gui: 'Settings (Inventory): Edit fast add settings' },
+  uiCreateEditDeleteMaterialTypes: { internal: 'ui-inventory.settings.materialtypes', gui: 'Settings (Inventory): Create, edit, delete material types' },
   // Tags
   uiTagsPermissionAll: { internal: 'ui-tags.permission.all', gui:'Tags: All permissions' },
   // Settings->Owners
