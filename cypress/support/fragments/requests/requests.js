9f1cd7a9cccc2eb0b2b87e58dfd866c5e27eaaa4
Jakhongir Abdukhamidov <100832608+jakhongirabdukhamdiov@users.noreply.github.com>
2022-03-31 17:51:35 +0500

    FAT 891 (#306)
    * Click column headers (including Request Date) to verify that they sort and reverse sort
    
    * when adding requests use different types

--- cypress/support/fragments/requests/requests.js
+++ cypress/support/fragments/requests/requests.js
@@ -2,6 +2,7 @@ import uuid from 'uuid';
 import {
   Button,
   MultiColumnListCell,
+  MultiColumnListHeader,
   MultiSelect,
   Pane,
   IconButton,
@@ -229,4 +230,141 @@ export default {
     }).as('getTags');
     cy.wait('@getTags');
   },
+
+  sortingColumns: [
+    {
+      title: 'Title',
+      id: 'title',
+      columnIndex: 2,
+    },
+    {
+      title: 'Type',
+      id: 'type',
+      columnIndex: 5,
+    },
+    {
+      title: 'Item barcode',
+      id: 'itembarcode',
+      columnIndex: 4,
+    },
+    {
+      title: 'Requester',
+      id: 'requester',
+      columnIndex: 8,
+    },
+    {
+      title: 'Requester Barcode',
+      id: 'requesterbarcode',
+      columnIndex: 9,
+    },
+  ],
+
+  requestTypes: ['Page', 'Hold', 'Recall'],
+
+  checkAllRequestTypes() {
+    cy.get('#clickable-filter-requestType-hold').check();
+    cy.get('#clickable-filter-requestType-page').check();
+    cy.get('#clickable-filter-requestType-recall').check();
+  },
+
+  validateRequestTypes() {
+    cy.expect(Checkbox({ name: 'Recall' }).checked);
+    cy.expect(Checkbox({ name: 'Page' }).checked);
+    cy.expect(Checkbox({ name: 'Hold' }).checked);
+  },
+
+  validateNumsAscendingOrder(prev) {
+    const itemsClone = [...prev];
+    itemsClone.sort((a, b) => a - b);
+    cy.expect(itemsClone).to.deep.equal(prev);
+  },
+
+  validateNumsDescendingOrder(prev) {
+    const itemsClone = [...prev];
+    itemsClone.sort((a, b) => b - a);
+    cy.expect(itemsClone).to.deep.equal(prev);
+  },
+
+  validateStringsAscendingOrder(prev) {
+    const itemsClone = [...prev];
+
+    itemsClone.sort((a, b) => {
+      // when sorting move falsy values to the end and localeCompare truthy values
+      if (!a) return 1;
+      if (!b) return -1;
+      return a.localeCompare(b);
+    });
+
+    expect(prev).to.deep.equal(itemsClone);
+  },
+
+  validateStringsDescendingOrder(prev) {
+    const itemsClone = [...prev];
+    // when sorting move falsy values to the beginning and localeCompare truthy values
+    itemsClone.sort((a, b) => {
+      if (!a) return -1;
+      if (!b) return 1;
+      return b.localeCompare(a);
+    });
+    expect(prev).to.deep.equal(itemsClone);
+  },
+
+  // TODO: redesign to interactors
+  getMultiColumnListCellsValues(cell) {
+    const cells = [];
+    // get MultiColumnList rows and loop over
+    return cy.get('[data-row-index]').each($row => {
+      // from each row, choose specific cell
+      cy.get(`[class*="mclCell-"]:nth-child(${cell})`, { withinSubject: $row })
+      // extract its text content
+        .invoke('text')
+        .then(cellValue => {
+          cells.push(cellValue);
+        });
+    })
+      .then(() => cells);
+  },
+
+  getSortOrder(headerId) {
+    let order;
+    return cy.do(MultiColumnListHeader({ id: 'list-column-' + headerId }).perform(el => {
+      order = el.attributes.getNamedItem('aria-sort').value;
+    })).then(() => order);
+  },
+
+  validateRequestsDateSortingOrder() {
+    this.getSortOrder('requestdate').then(order => {
+      this.getMultiColumnListCellsValues(1).then(cells => {
+        const dates = cells.map(cell => new Date(cell));
+        if (order === 'ascending') this.validateNumsAscendingOrder(dates);
+        else if (order === 'descending') this.validateNumsDescendingOrder(dates);
+      });
+    });
+  },
+
+  validateRequestsSortingOrder({ headerId, columnIndex }) {
+    this.waitLoadingRequests();
+
+    this.getSortOrder(headerId).then(order => {
+      this.getMultiColumnListCellsValues(columnIndex).then(cells => {
+        if (order === 'ascending') this.validateStringsAscendingOrder(cells);
+        else if (order === 'descending') this.validateStringsDescendingOrder(cells);
+      });
+    });
+  },
+
+  waitLoadingRequests() {
+    cy.wait('@getRequests');
+    /*
+      ***
+      - REASON: cy.wait(300)
+      It awaits for the api to resolve but as previous MultiColumnCellValues are already present
+      It does not wait for UI to update cellValues and some test fails randomly.
+      with cy.wait(300) It awaits UI for ui to update and tests work.
+      ***
+    */
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(300);
+  }
 };
