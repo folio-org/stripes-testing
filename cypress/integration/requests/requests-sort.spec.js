3567b1cc3b0a80443a8bf8b64706a4be7b17c7d3
Jakhongir Abdukhamidov <100832608+jakhongirabdukhamdiov@users.noreply.github.com>
2022-04-04 16:13:01 +0500

    FAT 891 (#309)
    * FAT-891 Implement e-2-e automation of test case C2379: Test Request app sorting
    
    * FAT-891: Click column headers (including Request Date) to verify that they sort and reverse sort
    
    * FAT-891: when adding requests use different types
    
    * FAT-891: fix
    
    * FAT-891 refactor code
    
    * FAT-891: Delete created requests after each test
    
    Co-authored-by: Javokhir Rajabboev <89512426+Javokhir12@users.noreply.github.com>

---
+++ cypress/integration/requests/requests-sort.spec.js
@@ -0,0 +1,69 @@
+import testType from '../../support/dictionary/testTypes';
+import TopMenu from '../../support/fragments/topMenu';
+import Requests from '../../support/fragments/requests/requests';
+import { MultiColumnListHeader } from '../../../interactors';
+
+describe('ui-requests: Sort requests', () => {
+  const userIds = [];
+  const requests = [];
+  const instances = [];
+
+  beforeEach(() => {
+    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+    cy.getToken('diku_admin', 'admin');
+
+    Object.values(Requests.requestTypes).forEach((requestType) => {
+      const itemStatus = requestType === 'Page' ? 'Available' : 'Checked out';
+      Requests.createRequestApi(itemStatus, requestType).then(({
+        instanceRecordData,
+        createdRequest,
+        createdUser
+      }) => {
+        userIds.push(createdUser.id);
+        instances.push(instanceRecordData);
+        requests.push(createdRequest);
+      });
+    });
+  });
+
+  afterEach(() => {
+    instances.forEach(instance => {
+      cy.deleteItem(instance.itemId);
+      cy.deleteHoldingRecord(instance.holdingId);
+      cy.deleteInstanceApi(instance.instanceId);
+    });
+    requests.forEach(request => {
+      Requests.deleteRequestApi(request.id);
+    });
+    userIds.forEach(id => {
+      cy.deleteUser(id);
+    });
+  });
+
+  it('should implement e-2-e automation of test case C2379: Test Request app sorting', { tags: [testType.smoke] }, () => {
+    cy.visit(TopMenu.requestsPath);
+
+    cy.intercept('GET', '/circulation/requests?*').as('getRequests');
+
+    Requests.checkAllRequestTypes();
+    Requests.validateRequestTypesChecked();
+
+    // Validate that the requests are sorted by Request date (oldest at top) by default
+    Requests.validateRequestsDateSortingOrder('ascending');
+
+    // Click column header Request Date to verify that they reverse sort
+    cy.do(MultiColumnListHeader('Request Date').click());
+    Requests.waitLoadingRequests();
+    Requests.validateRequestsDateSortingOrder('descending');
+
+    Requests.sortingColumns.forEach(column => {
+      // Validate sort
+      cy.do(MultiColumnListHeader(column.title).click());
+      Requests.validateRequestsSortingOrder({ headerId: column.id, columnIndex: column.columnIndex });
+
+      // Validate reverse sorting
+      cy.do(MultiColumnListHeader(column.title).click());
+      Requests.validateRequestsSortingOrder({ headerId: column.id, columnIndex: column.columnIndex });
+    });
+  });
+});
