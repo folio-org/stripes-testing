e25927f4e61bb62ed6007030c6cd2ba9a184f659
Maksim_Terekhov <maksim_terekhov@epam.com>
2022-04-07 14:48:54 +0300

    FAT-1643 fixed tests

--- cypress/integration/inventory/moving-items.spec.js
+++ cypress/integration/inventory/moving-items.spec.js
@@ -9,29 +9,95 @@ import HoldingsRecordView from '../../support/fragments/inventory/holdingsRecord
 import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
 import InventoryViewSource from '../../support/fragments/inventory/inventoryViewSource';
 import Features from '../../support/dictionary/features';
+import permissions from '../../support/dictionary/permissions';
+import getRandomPostfix from '../../support/utils/stringTools';
 
 const successCalloutMessage = '1 item has been successfully moved.';
-
+const ITEM_BARCODE = `123${getRandomPostfix()}`;
+let userId = '';
+let firstHolding = '';
+let secondHolding = '';
 
 describe('ui-inventory: moving items', () => {
-  beforeEach('navigates to Inventory', () => {
-    // TODO: replace this user with all permissions to user with only needed permission for moving items
-    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
-    cy.visit(TopMenu.inventoryPath);
+  before('create inventory instance', () => {
+    cy.createTempUser([
+      permissions.inventoryAll.gui,
+      permissions.uiInventoryMoveItems.gui
+    ])
+      .then(userProperties => {
+        userId = userProperties.userId;
+        cy.login(userProperties.username, userProperties.password);
+        cy.visit(TopMenu.inventoryPath);
+        cy.getToken(Cypress.env('diku_login'), Cypress.env('diku_password'))
+          .then(() => {
+            cy.getLoanTypes({ limit: 1 });
+            cy.getMaterialTypes({ limit: 1 });
+            cy.getLocations({ limit: 2 });
+            cy.getHoldingTypes({ limit: 2 });
+            cy.getHoldingSources({ limit: 2 });
+            cy.getInstanceTypes({ limit: 1 });
+            cy.getServicePointsApi({ limit: 1, query: 'pickupLocation=="true"' });
+            cy.getUsers({
+              limit: 1,
+              query: `"personal.lastName"="${userProperties.username}" and "active"="true"`
+            });
+          })
+          .then(() => {
+            firstHolding = Cypress.env('locations')[0].name;
+            secondHolding = Cypress.env('locations')[1].name;
+            cy.createInstance({
+              instance: {
+                instanceTypeId: Cypress.env('instanceTypes')[0].id,
+                title: `Barcode search test ${Number(new Date())}`,
+              },
+              holdings: [
+                {
+                  holdingsTypeId: Cypress.env('holdingsTypes')[0].id,
+                  permanentLocationId: Cypress.env('locations')[0].id,
+                  sourceId: Cypress.env('holdingSources')[0].id,
+                },
+                {
+                  holdingsTypeId: Cypress.env('holdingsTypes')[1].id,
+                  permanentLocationId: Cypress.env('locations')[1].id,
+                  sourceId: Cypress.env('holdingSources')[1].id,
+                }],
+              items: [
+                [{
+                  barcode: ITEM_BARCODE,
+                  missingPieces: '3',
+                  numberOfMissingPieces: '3',
+                  status: { name: 'Available' },
+                  permanentLoanType: { id: Cypress.env('loanTypes')[0].id },
+                  materialType: { id: Cypress.env('materialTypes')[0].id },
+                }],
+              ],
+            });
+          });
+      }).then(() => { console.log(Cypress.env('locations')); console.log(ITEM_BARCODE); });
   });
 
-  // TODO: redesign test, add test data generation/delete
-  it('C15185 Move multiple items from one holdings to another holdings within an instance', { tags: [TestTypes.smoke] }, () => {
-    const secondHolding = 'Annex';
+  after('Delete all data', () => {
+    cy.getInstances({ limit: 1, expandAll: true, query: `"items.barcode"=="${ITEM_BARCODE}"` })
+      .then(() => {
+        cy.deleteItem(Cypress.env('instances')[0].items[0].id);
+        cy.deleteHoldingRecord(Cypress.env('instances')[0].holdings[0].id);
+        cy.deleteHoldingRecord(Cypress.env('instances')[0].holdings[1].id);
+        cy.deleteInstanceApi(Cypress.env('instances')[0].id);
+      });
+    cy.deleteUser(userId);
+  });
 
-    InventorySearch.byEffectiveLocation();
+
+  it('C15185 Move multiple items from one holdings to another holdings within an instance', { tags: [TestTypes.smoke] }, () => {
+    InventorySearch.switchToItem();
+    InventorySearch.searchByParameter('Barcode', ITEM_BARCODE);
     InventorySearch.selectSearchResultItem();
     InventoryInstance.openMoveItemsWithinAnInstance();
 
-    InventoryInstance.moveItemToAnotherHolding(InventorySearch.getEffectiveLocation().name, secondHolding);
+    InventoryInstance.moveItemToAnotherHolding(firstHolding, secondHolding);
     InteractorsTools.checkCalloutMessage(successCalloutMessage);
 
-    InventoryInstance.returnItemToFirstHolding(InventorySearch.getEffectiveLocation().name, secondHolding);
+    InventoryInstance.returnItemToFirstHolding(firstHolding, secondHolding);
     InteractorsTools.checkCalloutMessage(successCalloutMessage);
   });
 
