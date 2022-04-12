a0398c4ff3045362c5d275d4df31576444742d65
Tetiana_Paranich <tetiana_paranich@epam.com>
2022-04-12 14:33:12 +0300

    created test

---
+++ cypress/integration/inventory/Item-status-date-updates.spec.js
@@ -0,0 +1,16 @@
+import TestTypes from '../../support/dictionary/testTypes';
+
+
+describe('ui-inventory: Item status date updates', () => {
+  before(() => {
+
+  });
+
+  after(() => {
+
+  });
+
+  it('C9200 Item status date updates', { tags: [TestTypes.smoke] }, () => {
+
+  });
+});
