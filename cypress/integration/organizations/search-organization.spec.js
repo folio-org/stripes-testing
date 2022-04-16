15b3c188440f994128234f1582e61f4a1ccf691d
Ihor Bohdan <ihor_bohdan@epam.com>
2022-04-16 12:36:09 +0300

    C6712: Test the Organizations app searche

--- cypress/integration/organizations/search-organization.spec.js
+++ cypress/integration/organizations/search-organization.spec.js
@@ -19,28 +19,20 @@ describe('ui-organizations: Search organization', () => {
       });
   });
 
-  it('C6712 Test the Organizations app searches', { tags: [TestType.smoke] }, () => {
-    cy.visit(TopMenu.organizationsPath);
-    Organizations.searchByParameters('All', organization.name);
-    Organizations.checkSearchResults(organization);
-    Organizations.resetFilters();
-    Organizations.searchByParameters('Name', organization.name);
-    Organizations.checkSearchResults(organization);
-    Organizations.resetFilters();
-    Organizations.searchByParameters('Code', organization.code);
-    Organizations.checkSearchResults(organization);
-    Organizations.resetFilters();
-    Organizations.searchByParameters('Language', organization.language);
-    Organizations.checkSearchResults(organization);
-    Organizations.resetFilters();
-    Organizations.searchByParameters('Alias', organization.aliases[0].value);
-    Organizations.checkSearchResults(organization);
-    Organizations.resetFilters();
-    Organizations.searchByParameters('Accounting code', organization.erpCode);
-    Organizations.checkSearchResults(organization);
-    Organizations.resetFilters();
-    Organizations.searchByParameters('Tax ID', organization.taxId);
-    Organizations.checkSearchResults(organization);
-    Organizations.resetFilters();
+  [
+    { parameter: 'All', value: organization.name },
+    { parameter: 'Name', value: organization.name },
+    { parameter: 'Code', value: organization.code },
+    { parameter: 'Language', value: organization.language },
+    { parameter: 'Alias', value: organization.aliases[0].value },
+    { parameter: 'Accounting code', value: organization.erpCode },
+    { parameter: 'Tax ID', value: organization.taxId },
+  ].forEach((searcher) => {
+    it('C6712 Test the Organizations app searches', { tags: [TestType.smoke] }, () => {
+      cy.visit(TopMenu.organizationsPath);
+      Organizations.searchByParameters(searcher.parameter, searcher.value);
+      Organizations.checkSearchResults(organization);
+      Organizations.resetFilters();
+    });
   });
 });
