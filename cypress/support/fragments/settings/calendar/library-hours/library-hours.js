51672126a9bbea39b3988d6aee9b84ffe719e866
Artem Blazhko <artem_blazhko@epam.com>
2022-04-27 11:17:18 +0300

    Settings Calendar create, view, edit, delete events (#349)

---
+++ cypress/support/fragments/settings/calendar/library-hours/library-hours.js
@@ -0,0 +1,90 @@
+import { including } from '@interactors/html';
+import {
+  Button,
+  IconButton,
+  TextField,
+  Calendar,
+  PaneHeader,
+  Modal,
+  KeyValue,
+} from '../../../../../../interactors';
+import settingsMenu from '../../../settingsMenu';
+import dateTools from '../../../../utils/dateTools';
+
+const currentDate = dateTools.getCurrentDay();
+const initialEventName = 'Initial event name';
+const editedEventName = 'Edited event name';
+const nameAttributeValue = 'periodName';
+const currentEventLabel = 'Current:';
+
+const iconsSet = {
+  calendar: 'calendar',
+  edit: 'edit',
+};
+const selectors = {
+  calendarFirstCell: '.rbc-day-bg:first-child',
+  servicePoint: '[class^=navListSectionControl] [href^="/settings/calendar/library-hours/"]',
+};
+const pageHeaders = {
+  createEvent: 'New: Regular Library Hours Validity Period',
+  modifyEvent: 'Modify: Regular Library Hours Validity Period',
+  deleteEvent: 'Delete regular library hours',
+};
+const buttonLabels = {
+  new: 'New',
+  saveAndClose: 'Save & close',
+  delete: 'Delete',
+};
+const fieldLabels = {
+  from: 'Valid From:*',
+  to: 'Valid To:*',
+};
+
+export default {
+  createCalendarEvent() {
+    cy
+      .visit(settingsMenu.calendarLibraryHoursPath)
+      .get(selectors.servicePoint).first().click()
+      .do(Button(buttonLabels.new).click())
+      .expect(PaneHeader(pageHeaders.createEvent).exists());
+
+    cy
+      .do([
+        TextField(fieldLabels.from).find(Button({ icon: iconsSet.calendar })).click(),
+        Calendar().clickActiveDay(currentDate),
+        TextField(fieldLabels.to).find(Button({ icon: iconsSet.calendar })).click(),
+        Calendar().clickActiveDay(currentDate),
+        TextField({ name: nameAttributeValue }).fillIn(initialEventName)
+      ]);
+
+    cy
+      .get(selectors.calendarFirstCell).click()
+      .do(Button(buttonLabels.saveAndClose).click())
+      .expect(KeyValue(currentEventLabel, { value: including(initialEventName) }).exists());
+  },
+
+  editCalendarEvent() {
+    cy
+      .do(IconButton({ icon: iconsSet.edit }).click())
+      .expect(PaneHeader(pageHeaders.modifyEvent).exists());
+
+    cy
+      .do([
+        TextField({ name: nameAttributeValue }).fillIn(editedEventName),
+        Button(buttonLabels.saveAndClose).click()
+      ])
+      .expect(KeyValue(currentEventLabel, { value: including(editedEventName) }).exists());
+  },
+
+  deleteCalendarEvent() {
+    cy
+      .do(IconButton({ icon: iconsSet.edit }).click())
+      .expect(PaneHeader(pageHeaders.modifyEvent).exists());
+
+    cy
+      .do(Button(buttonLabels.delete).click())
+      .expect(Modal(pageHeaders.deleteEvent).exists());
+
+    cy.do(Button({ id: 'clickable-delete-confirmation-confirm' }).click());
+  },
+};
