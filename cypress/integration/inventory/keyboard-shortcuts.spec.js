a9c4f96003692b4614e9e3dfc0ab56a4f882842e
Maksim_Terekhov <maksim_terekhov@epam.com>
2022-05-06 15:57:59 +0300

    FAT-1657 fix comments

--- cypress/integration/inventory/keyboard-shortcuts.spec.js
+++ cypress/integration/inventory/keyboard-shortcuts.spec.js
@@ -1,11 +1,24 @@
 import TopMenu from '../../support/fragments/topMenu';
 import testTypes from '../../support/dictionary/testTypes';
 import InventoryMainButton from '../../support/fragments/inventory/inventoryMainButton';
+import permissions from '../../support/dictionary/permissions';
+
+let userId = '';
 
 describe('ui-inventory: keyboard shortcut', () => {
   beforeEach('navigate to inventory', () => {
-    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+    cy.createTempUser([
+      permissions.inventoryAll.gui
+    ])
+      .then(userProperties => {
+        userId = userProperties.userId;
+        cy.login(userProperties.username, userProperties.password);
+      });
     cy.visit(TopMenu.inventoryPath);
+  });
+
+  after('Delete all data', () => {
+    cy.deleteUser(userId);
   });
 
   it('C345297 Keyboard Shortcut. Access to drop down menu', { tags: [testTypes.smoke] }, () => {
@@ -15,7 +28,7 @@ describe('ui-inventory: keyboard shortcut', () => {
     InventoryMainButton.verifyInventoryDropdownIsShown('true');
     InventoryMainButton.openShortcuts();
 
-    InventoryMainButton.verifyInventoryModalName('Keyboard shortcuts');
+    InventoryMainButton.waitModalLoading('Keyboard shortcuts');
     InventoryMainButton.verifyInventoryDropdownIsShown('false');
   });
 });
--- cypress/support/fragments/inventory/inventoryMainButton.js
+++ cypress/support/fragments/inventory/inventoryMainButton.js
@@ -1,20 +1,19 @@
 import { Button, Dropdown, Modal } from '../../../../interactors';
 
+const dropDown = Dropdown('InventoryApplication context dropdown');
+
 export default {
   verifyInventoryDropdownIsShown(isOpen) {
     // isOpen: string with bool value
-    cy.do(Dropdown('InventoryApplication context dropdown').find(Button('Inventory\nApplication context dropdown'))
-      .perform(element => {
-        expect(element).to.have.attr('aria-expanded', isOpen);
-      }));
+    cy.expect(dropDown.find(Button('Inventory\nApplication context dropdown')).has({ ariaExpanded: isOpen }));
   },
   openInventoryMenu() {
-    cy.do(Dropdown('InventoryApplication context dropdown').open());
+    cy.do(dropDown.open());
   },
   openShortcuts() {
-    cy.do(Dropdown('InventoryApplication context dropdown').choose('Keyboard shortcuts'));
+    cy.do(dropDown.choose('Keyboard shortcuts'));
   },
-  verifyInventoryModalName(title) {
-    cy.expect(Modal().has({ title }));
-  }
+  waitModalLoading(modalName) {
+    cy.expect(Modal(modalName).exists());
+  },
 };
