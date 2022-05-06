a2d493acf25e002cc8b8089ad97a680e8fa1155f
Maksim_Terekhov <maksim_terekhov@epam.com>
2022-05-06 16:16:24 +0300

    FAT-1657 fix namings

--- cypress/support/fragments/inventory/inventoryMainButton.js
+++ cypress/support/fragments/inventory/inventoryMainButton.js
@@ -1,17 +1,18 @@
 import { Button, Dropdown, Modal } from '../../../../interactors';
 
-const dropDown = Dropdown('InventoryApplication context dropdown');
+const inventoryApplicationContextDropdown = Dropdown('InventoryApplication context dropdown');
 
 export default {
   verifyInventoryDropdownIsShown(isOpen) {
     // isOpen: string with bool value
-    cy.expect(dropDown.find(Button('Inventory\nApplication context dropdown')).has({ ariaExpanded: isOpen }));
+    cy.expect(inventoryApplicationContextDropdown.find(Button('Inventory\nApplication context dropdown'))
+      .has({ ariaExpanded: isOpen }));
   },
   openInventoryMenu() {
-    cy.do(dropDown.open());
+    cy.do(inventoryApplicationContextDropdown.open());
   },
   openShortcuts() {
-    cy.do(dropDown.choose('Keyboard shortcuts'));
+    cy.do(inventoryApplicationContextDropdown.choose('Keyboard shortcuts'));
   },
   waitModalLoading(modalName) {
     cy.expect(Modal(modalName).exists());
