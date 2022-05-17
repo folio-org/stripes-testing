a0c793f92738dc4cf4e07b9557b2a115d09483cc
Tetiana_Paranich <tetiana_paranich@epam.com>
2022-05-17 20:45:47 +0300

    added consts

--- cypress/support/fragments/users/loans/userLoans.js
+++ cypress/support/fragments/users/loans/userLoans.js
@@ -3,25 +3,27 @@ import { MultiColumnListRow, HTML, including, Button } from '../../../../../inte
 const declaredLostButton = Button('Declare lost');
 const itemDetailsButton = Button('Item details');
 const renewButton = Button('Renew');
+const ellipsisButton = Button({ icon:'ellipsis' });
+const rowInList = MultiColumnListRow({ indexRow: 'row-0' });
 
 export default {
   selectLoan:(barcode) => {
-    cy.do(MultiColumnListRow({ indexRow: 'row-0' }).find(HTML(including(barcode))).click());
+    cy.do(rowInList.find(HTML(including(barcode))).click());
   },
   declareLoanLost:() => {
-    cy.do(Button({ icon:'ellipsis' }).click());
+    cy.do(ellipsisButton.click());
     cy.expect(declaredLostButton.exists());
     cy.do(declaredLostButton.click());
   },
   openItemRecordInInventory:(barcode) => {
-    cy.expect(MultiColumnListRow({ indexRow: 'row-0' }).find(HTML(including(barcode))).exists());
-    cy.do(Button({ icon:'ellipsis' }).click());
+    cy.expect(rowInList.find(HTML(including(barcode))).exists());
+    cy.do(ellipsisButton.click());
     cy.expect(itemDetailsButton.exists());
     cy.do(itemDetailsButton.click());
   },
   renewItem:(barcode) => {
-    cy.expect(MultiColumnListRow({ indexRow: 'row-0' }).find(HTML(including(barcode))).exists());
-    cy.do(Button({ icon:'ellipsis' }).click());
+    cy.expect(rowInList.find(HTML(including(barcode))).exists());
+    cy.do(ellipsisButton.click());
     cy.expect(renewButton.exists());
     cy.do(renewButton.click());
   }
