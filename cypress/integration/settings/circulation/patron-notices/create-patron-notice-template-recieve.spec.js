d96276f7a107cc405f98b39ead165d3b7e514632
ekaterina_kim@epam.com <ekaterina_kim@epam.com>
2022-05-23 07:38:44 +0500

    debuging the wtf button

--- cypress/integration/settings/circulation/patron-notices/create-patron-notice-template-recieve.spec.js
+++ cypress/integration/settings/circulation/patron-notices/create-patron-notice-template-recieve.spec.js
@@ -1,8 +1,10 @@
 import NewPatronNoticeTemplate from '../../../../support/fragments/circulation/newPatronNoticeTemplate'
 import SettingsMenu from '../../../../support/fragments/settingsMenu'
 import TestType from '../../../../support/dictionary/testTypes'
+import NewPatronNoticePolicies from '../../../../support/fragments/circulation/newPatronNoticePolicies'
 describe('ui-circulation-settings: create and recieve patron notice', () => {
     const patronNoticeTemplate = { ...NewPatronNoticeTemplate.defaultUiPatronNoticeTemplate}
+    const patronNoticePolicy ={ ...NewPatronNoticePolicies.defaultUiPatronNoticePolicies}
 
     beforeEach('login', () => {
         cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'))
@@ -12,6 +14,15 @@ describe('ui-circulation-settings: create and recieve patron notice', () => {
     it('C347621Check that user can receive notice with multiple items after finishing the session "Check out" by clicking the End Session button.', { tags: [TestType.smoke] }, () => {
         NewPatronNoticeTemplate.createTemplate(patronNoticeTemplate)
         NewPatronNoticeTemplate.checkTemplate(patronNoticeTemplate);
-        NewPatronNoticeTemplate.deleteTemplate()
+        patronNoticePolicy.template = patronNoticeTemplate.name
+        NewPatronNoticePolicies.getTemplate(patronNoticePolicy, 'template')
+        cy.log(`patron notice template name: ${patronNoticePolicy.template}`)
+
+        cy.visit(`${SettingsMenu.circulationPatronNoticePoliciesPath}`)
+        NewPatronNoticePolicies.createPolicy(patronNoticePolicy)
+        NewPatronNoticePolicies.fillPolicyTemplate(patronNoticePolicy)
+
+         // NewPatronNoticeTemplate.deleteTemplate()
+        //  NewPatronNoticePolicies.deletePolicy()
     })
 })
--- cypress/support/fragments/circulation/newPatronNoticePolicies.js
+++ cypress/support/fragments/circulation/newPatronNoticePolicies.js
@@ -1,18 +1,37 @@
 import getRandomPostfix from '../../utils/stringTools';
-import { Button, TextField, TextArea, NavListItem, Checkbox } from '../../../../interactors';
+import { Button, TextField, TextArea, NavListItem, Checkbox, Select } from '../../../../interactors';
 
 const actionsButton = Button('Actions');
+const addNoticeButton = Button('Add notice')
 const nameField = TextField({ id: 'notice_policy_name' });
+const templateSelect = Select({ id: 'select-45'})
 
 export default {
   defaultUiPatronNoticePolicies : {
     name: `Test_notice_${getRandomPostfix()}`,
     description: 'Created by autotest team',
+    template: null,
   },
+
+  getTemplate(patronNoticePolicy,  template) {
+    return { ...patronNoticePolicy, template }
+  },
+
   createPolicy(patronNoticePolicy) {
     cy.do(Button({ id: 'clickable-create-entry' }).click());
+    addNoticeButton.click()
     this.fillGeneralInformation(patronNoticePolicy);
   },
+
+  fillPolicyTemplate(patronNoticePolicy) {
+    cy.do([
+      addNoticeButton.click(),
+      cy.log('should find button "Add notice"'),
+      cy.log(patronNoticePolicy.template),
+      templateSelect.select( ` ${patronNoticePolicy.template} `)
+    ])
+    },
+
   fillGeneralInformation: (patronNoticePolicy) => {
     cy.do([
       nameField.fillIn(patronNoticePolicy.name),
@@ -20,18 +39,22 @@ export default {
       TextArea({ id: 'notice_policy_description' }).fillIn(patronNoticePolicy.description),
     ]);
   },
+
   savePolicy: () => {
     cy.do([
       Button({ id: 'footer-save-entity' }).click(),
       Button({ icon: 'times' }).click(),
     ]);
   },
+
   checkPolicy: (patronNoticePolicyName) => {
     cy.expect(NavListItem(patronNoticePolicyName).exists());
   },
+
   choosePolicy: (patronNoticePolicy) => {
     cy.do(NavListItem(patronNoticePolicy.name).click());
   },
+
   editPolicy(patronNoticePolicy) {
     cy.do([
       NavListItem(patronNoticePolicy.name).click(),
@@ -40,6 +63,7 @@ export default {
     ]);
     this.fillGeneralInformation(patronNoticePolicy);
   },
+
   duplicatePolicy: () => {
     cy.do([
       actionsButton.click(),
@@ -48,6 +72,7 @@ export default {
       Button({ id: 'footer-save-entity' }).click(),
     ]);
   },
+
   deletePolicy: () => {
     cy.do([
       actionsButton.click(),
