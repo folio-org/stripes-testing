62b0da483a2d84c4ab5f1f1b35333f92e95c7504
dmitriyfolio <dmitrii_vagin@epam.com>
2022-05-30 23:55:53 +0300

    conditions processing

--- cypress/integration/settings/users/patronGroups.spec.js
+++ cypress/integration/settings/users/patronGroups.spec.js
@@ -3,6 +3,7 @@ import Features from '../../../support/dictionary/features';
 import PatronGroups from '../../../support/fragments/settings/users/patronGroups';
 import getRandomPostfix from '../../../support/utils/stringTools';
 import Users from '../../../support/fragments/users/users';
+import Conditions from '../../../support/fragments/settings/users/conditions';
 
 describe('Patron blocks relations with users, conditions', () => {
   it("C11020 Verify user information display when automated patron block 'Maximum outstanding fee/fine balance' exists for patron", { tags: [TestType.smoke, Features.patronBlocks] }, () => {
@@ -11,16 +12,18 @@ describe('Patron blocks relations with users, conditions', () => {
     const patronGroupName = `auttestPatronGroup${getRandomPostfix()}`;
     PatronGroups.createViaApi(patronGroupName).then(patronGroupId => {
       Users.createUserApi({ ...Users.defaultUser, patronGroup: patronGroupId }).then(userId => {
-        console.log(patronGroupId);
-        console.log(userId);
+        Conditions.getConditionsViaApi().then(patronBlockConditions => {
+          const testCondition = Conditions.defaultConditions.defaultMaximumOustandingFeeFineBalance;
+          const testConditionId = patronBlockConditions.filter(conditionProperty => conditionProperty.name === testCondition.name)[0].id;
+          Conditions.updateViaApi({ ...testCondition, id: testConditionId });
 
 
 
-
-
-        // test data clearing
-        Users.deleteViaApi(userId);
-        PatronGroups.deleteViaApi(patronGroupId);
+          // test data clearing
+          Conditions.resetConditionViaApi(testConditionId, testCondition.name);
+          Users.deleteViaApi(userId);
+          PatronGroups.deleteViaApi(patronGroupId);
+        });
       });
     });
   });
