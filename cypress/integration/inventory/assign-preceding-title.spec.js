83409531fb80e03b5fe18d171d4e5ab3417cdba5
dmitriyfolio <dmitrii_vagin@epam.com>
2022-06-02 14:10:17 +0300

    fix import issue

--- cypress/integration/inventory/assign-preceding-title.spec.js
+++ cypress/integration/inventory/assign-preceding-title.spec.js
@@ -4,6 +4,7 @@ import InventorySearch from '../../support/fragments/inventory/inventorySearch';
 import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
 import InventoryInstance from '../../support/fragments/inventory/inventoryInstance';
 import InventoryInstanceEdit from '../../support/fragments/inventory/InventoryInstanceEdit';
+import TestTypes from '../../support/dictionary/testTypes';
 
 describe('ui-inventory: Assign a Preceding title for an instance', () => {
   const instanceIds = [];
@@ -51,7 +52,7 @@ describe('ui-inventory: Assign a Preceding title for an instance', () => {
       });
   });
 
-  it('C9215 In Accordion Title --> Test assigning a Preceding title', { tags:  [testTypes.smoke] }, () => {
+  it('C9215 In Accordion Title --> Test assigning a Preceding title', { tags:  [TestTypes.smoke] }, () => {
     InventorySearch.searchByParameter('Title (all)', instanceTitle);
     InventoryInstances.selectInstance();
     InventoryInstance.editInstance();
