572ad54079afd6d18448f9ce4c141c2e7bdc1aaa
Tetiana_Paranich <tetiana_paranich@epam.com>
2022-06-17 17:15:16 +0300

    changes

--- cypress/integration/data-import/match-on-pol-and-update-items.spec.js
+++ cypress/integration/data-import/match-on-pol-and-update-items.spec.js
@@ -1,10 +1,39 @@
 import TestTypes from '../../support/dictionary/testTypes';
 import permissions from '../../support/dictionary/permissions';
+import BasicOrderLine from '../../support/fragments/orders/basicOrderLine';
+import NewOrder from '../../support/fragments/orders/newOrder';
 
 describe('ui-users:', () => {
+  const instanceTitle = `autotestTitle ${Helper.getRandomBarcode()}`;
+  const itemQuantity = '1';
+  let vendorId;
+  let locationId;
+  const orderLine = { ...BasicOrderLine.defaultOrderLine };
+  let orderNumber;
   let user = {};
 
   beforeEach(() => {
+    cy.getAdminToken();
+    cy.getOrganizationApi({ query: 'name="GOBI Library Solutions"' })
+      .then(organization => {
+        vendorId = organization.id;
+      });
+    cy.getLocations({ query: `name="${OrdersHelper.mainLibraryLocation}"` })
+      .then(location => { 
+        locationId = location.id; });
+    cy.getMaterialTypes({ query: 'name="book"' })
+      .then(materialType => { orderLine.physical.materialType = materialType.id; });
+      .then(() => {
+            Orders.createOrderWithOrderLineViaApi(
+              NewOrder.getDefaultOrder({vendor: vendorId})),
+              BasicOrderLine.getDefaultOrderLine(itemQuantity, instanceTitle, effectiveLocation.id)
+            )
+              .then(order => {
+                orderNumber = order;
+              });
+          });
+      });
+
     cy.createTempUser([
       permissions.createOrdersAndOrderLines.gui,
       permissions.editOrdersAndOrderLines.gui,
--- cypress/support/fragments/orders/newOrder.js
+++ cypress/support/fragments/orders/newOrder.js
@@ -1,9 +1,27 @@
 import uuid from 'uuid';
+import NewOrganization from '../organizations/newOrganization';
+
+const getDefaultOrder = (vendorId) => {
+  const defaultOrder = {
+    id: uuid(),
+    vendor: vendorId,
+    orderType: 'One-Time'
+  };
+  if (!vendorId) {
+    NewOrganization.createViaApi(NewOrganization.getDefaultOrganization())
+      .then(newOrganization => {
+        defaultOrder.vendor = newOrganization.id;
+      });
+  }
+  return defaultOrder;
+};
 
 export default {
   defaultOrder: {
     id: uuid(),
     vendor: '',
     orderType: 'One-Time'
-  }
+  },
+
+  getDefaultOrder,
 };
