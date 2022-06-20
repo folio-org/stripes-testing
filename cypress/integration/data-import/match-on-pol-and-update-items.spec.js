c8713bfecbf63cc1e0a5f40d651a408b7204dd23
Tetiana_Paranich <tetiana_paranich@epam.com>
2022-06-20 13:46:01 +0300

    changed po number

--- cypress/integration/data-import/match-on-pol-and-update-items.spec.js
+++ cypress/integration/data-import/match-on-pol-and-update-items.spec.js
@@ -2,36 +2,36 @@ import TestTypes from '../../support/dictionary/testTypes';
 import permissions from '../../support/dictionary/permissions';
 import BasicOrderLine from '../../support/fragments/orders/basicOrderLine';
 import NewOrder from '../../support/fragments/orders/newOrder';
+import OrdersHelper from '../../support/fragments/orders/ordersHelper';
+import Orders from '../../support/fragments/orders/orders';
+import Helper from '../../support/fragments/finance/financeHelper';
+import PoNumberEdit from '../../support/fragments/settings/orders/poNumberEdit';
 
 describe('ui-users:', () => {
   const instanceTitle = `autotestTitle ${Helper.getRandomBarcode()}`;
   const itemQuantity = '1';
   let vendorId;
   let locationId;
-  const orderLine = { ...BasicOrderLine.defaultOrderLine };
-  let orderNumber;
+  let materialTypeId;
+  const firstOrderNumber = 'autotestOrder99999';
   let user = {};
 
   beforeEach(() => {
-    cy.getAdminToken();
-    cy.getOrganizationApi({ query: 'name="GOBI Library Solutions"' })
-      .then(organization => {
-        vendorId = organization.id;
-      });
-    cy.getLocations({ query: `name="${OrdersHelper.mainLibraryLocation}"` })
-      .then(location => { 
-        locationId = location.id; });
-    cy.getMaterialTypes({ query: 'name="book"' })
-      .then(materialType => { orderLine.physical.materialType = materialType.id; });
+    cy.getAdminToken()
       .then(() => {
-            Orders.createOrderWithOrderLineViaApi(
-              NewOrder.getDefaultOrder({vendor: vendorId})),
-              BasicOrderLine.getDefaultOrderLine(itemQuantity, instanceTitle, effectiveLocation.id)
-            )
-              .then(order => {
-                orderNumber = order;
-              });
-          });
+        PoNumberEdit.viaApi();
+        cy.getOrganizationApi({ query: 'name="GOBI Library Solutions"' })
+          .then(organization => { vendorId = organization.id; });
+        cy.getLocations({ query: `name="${OrdersHelper.mainLibraryLocation}"` })
+          .then(location => { locationId = location.id; });
+        cy.getMaterialTypes({ query: 'name="book"' })
+          .then(materialType => { materialTypeId = materialType.id; });
+      })
+      .then(() => {
+        Orders.createOrderWithOrderLineViaApi(
+          NewOrder.getDefaultOrder(vendorId, firstOrderNumber),
+          BasicOrderLine.getDefaultOrderLine(itemQuantity, instanceTitle, locationId, materialTypeId)
+        );
       });
 
     cy.createTempUser([
@@ -56,4 +56,3 @@ describe('ui-users:', () => {
 
   });
 });
-
