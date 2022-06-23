f4a4dc58ef34b14d0457920e5b836b9756045b3a
Ihor Bohdan <ihor_bohdan@epam.com>
2022-06-23 16:46:46 +0300

    FAT-1730:C350758: Verify if a User can set/edit EDI convention in Organization Integration

---
+++ cypress/integration/organizations/EDI-convention-organization-integration.spec.js
@@ -0,0 +1,69 @@
+import TopMenu from '../../support/fragments/topMenu';
+import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
+import searchInventory from '../../support/fragments/data_import/searchInventory';
+import { MultiColumnListCell } from '../../../interactors';
+import permissions from '../../support/dictionary/permissions';
+import InventoryInstance from '../../support/fragments/inventory/inventoryInstance';
+import HoldingsRecordView from '../../support/fragments/inventory/holdingsRecordView';
+import getRandomPostfix from '../../support/utils/stringTools';
+import TestTypes from '../../support/dictionary/testTypes';
+import users from '../../support/fragments/users/users';
+import newOrganization from '../../support/fragments/organizations/newOrganization';
+import Organizations from '../../support/fragments/organizations/organizations';
+import InteractorsTools from '../../support/utils/interactorsTools';
+
+
+describe('ui-organizations: EDI convention in Organization Integration', () => {
+  let userId = '';
+  const organization = {
+    ...newOrganization.defaultUiOrganizations,
+    accounts: [{
+      accountNo: '1234',
+      accountStatus: 'Active',
+      acqUnitIds: [],
+      appSystemNo: '',
+      contactInfo: 'customerservice@alexanderstreet.com',
+      description: 'Main library account',
+      libraryCode: 'COB',
+      libraryEdiCode: `${getRandomPostfix()}`,
+      name: 'Library account',
+      notes: '',
+      paymentMethod: 'Physical Check',
+    }]
+  };
+
+  beforeEach(() => {
+    cy.createTempUser([permissions.OrganizationviewCreateEdit.gui])
+      .then(userProperties => {
+        userId = userProperties.userId;
+        cy.login(userProperties.username, userProperties.password);
+        // cy.getAdminToken();
+      });
+    Organizations.createOrganizationApi(organization)
+      .then(response => {
+        organization.id = response;
+      });
+    cy.visit(TopMenu.organizationsPath);
+  });
+
+  afterEach(() => {
+    users.deleteViaApi(userId);
+    Organizations.deleteOrganizationApi(organization.id);
+  });
+
+  it('C350758: Verify if a User can set/edit EDI convention in Organization Integration', { tags: [TestTypes.smoke] }, () => {
+    // Find created Organization
+    Organizations.searchByParameters('Name', organization.name);
+    Organizations.checkSearchResults(organization);
+    Organizations.chooseOrganizationFromList(organization);
+
+    Organizations.addIntegration();
+    Organizations.fillIntegrationInformation();
+    // InteractorsTools.checkCalloutMessage('Integration was saved');
+
+    Organizations.selectIntegration();
+    Organizations.editIntegrationInformation();
+    InteractorsTools.checkCalloutMessage('Integration was saved');
+    cy.pause();
+  });
+});
