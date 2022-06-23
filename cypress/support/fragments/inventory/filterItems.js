be33e80db5f6f950af6f9ea56fde76cdb38f50e7
Tetiana_Paranich <tetiana_paranich@epam.com>
2022-06-23 16:13:47 +0300

    fixed test

--- cypress/support/fragments/inventory/filterItems.js
+++ cypress/support/fragments/inventory/filterItems.js
@@ -5,6 +5,9 @@ export default {
 
   toggleStatus(statusName) {
     cy.do(TextField({ label: 'itemStatus-field' }).fillIn(statusName));
+    // need to wait until status is uploaded
+    cy.wait(500);
+    cy.do(TextField({ label: 'itemStatus-field' }).focus());
     cy.do(Checkbox(statusName).click());
   },
 
@@ -25,7 +28,7 @@ export default {
   },
 
   waitItemsLoading() {
-    cy.wait(['@getItems', '@getTitles', '@getHoldings']);
+    cy.wait(['@getItems', '@getHoldings']);
   },
 };
 
