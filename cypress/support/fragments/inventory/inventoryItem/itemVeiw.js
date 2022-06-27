0213fd6f1225e024f2e323d4881584b1609cce87
Tetiana_Paranich <tetiana_paranich@epam.com>
2022-06-27 15:53:09 +0300

    added comment

--- cypress/support/fragments/inventory/inventoryItem/itemVeiw.js
+++ cypress/support/fragments/inventory/inventoryItem/itemVeiw.js
@@ -30,6 +30,7 @@ export default {
       const rawDate = element.innerText;
       const parsedDate = Date.parse(rawDate.match(/\d{1,2}\/\d{1,2}\/\d{4},\s\d{1,2}:\d{1,2}\s\w{2}/gm)[0]);
       // For local run it needs to add 18000000
+      // The time on the server and the time on the yuai differ by 3 hours. It was experimentally found that it is necessary to add 18000000 sec
       dateTools.verifyDate(parsedDate, 18000000);
     }));
   },
