7e3b6ba6d0405334d6b836d86fbf6760f40f74d6
Diana Holik <Diana_Holik@epam.com>
2022-07-25 23:10:32 +0300

    The autotest C2268: Add notice policy to circulation rules - is fixed

--- cypress/integration/settings/circulation/general/edit-circulation-rules.spec.js
+++ cypress/integration/settings/circulation/general/edit-circulation-rules.spec.js
@@ -44,7 +44,7 @@ describe('ui-circulation-settings: Edit circulation rules', () => {
       });
       NewMaterialType.createViaApi(NewMaterialType.getDefaultMaterialType());
       NoticePolicy.createApi();
-      LoanPolicy.createApi();
+      LoanPolicy.createLoanableNotRenewableLoanPolicyApi(defaultLoanPolicy);
       RequestPolicy.createApi();
       LostItemFeePolicy.createViaApi();
       OverdueFinePolicy.createApi();
@@ -66,7 +66,7 @@ describe('ui-circulation-settings: Edit circulation rules', () => {
     Users.deleteViaApi(newUserId);
   });
 
-  it('C2268: Add notice policy to circulation rules (vega)', { tags: [TestType.smoke, TestType.broken] }, () => {
+  it('C2268: Add notice policy to circulation rules (vega)', { tags: [TestType.smoke] }, () => {
     CirculationRules.clearCirculationRules();
     CirculationRules.fillInPriority();
 
