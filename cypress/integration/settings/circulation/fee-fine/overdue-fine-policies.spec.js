23cdeb99c2a12c00a8b3199eb16a309cb25dbc80
ekaterina kim <ekaterina_kim@epam.com>
2022-08-09 10:47:19 +0500

    uppercase fix

--- cypress/integration/settings/circulation/fee-fine/overdue-fine-policies.spec.js
+++ cypress/integration/settings/circulation/fee-fine/overdue-fine-policies.spec.js
@@ -1,27 +1,27 @@
-import overdueFinePolicies from '../../../../support/fragments/settings/circulation/fee-fine/overdueFinePolicies';
-import settingsMenu from '../../../../support/fragments/settingsMenu';
+import OverdueFinePolicies from '../../../../support/fragments/settings/circulation/fee-fine/overdueFinePolicies';
+import SettingsMenu from '../../../../support/fragments/settingsMenu';
 
 describe('ui-circulation-settings: overdue fine policies management', () => {
   beforeEach('Log in', () => {
-    cy.loginAsAdmin({ path: settingsMenu.circulationoVerdueFinePoliciesPath, waiter: overdueFinePolicies.waitLoading });
+    cy.loginAsAdmin({ path: SettingsMenu.circulationoVerdueFinePoliciesPath, waiter: OverdueFinePolicies.waitLoading });
   });
 
   it('C5557: Verify that you can create/edit/delete overdue fine policies', () => {
     // TODO add check that name is unique
-    overdueFinePolicies.openCreatingForm();
-    overdueFinePolicies.checkCreatingForm();
-    overdueFinePolicies.checkOverDueFineInCreating();
-    overdueFinePolicies.fillGeneralInformation('1.00', '2.00', '3.00', '4.00');
-    overdueFinePolicies.save();
-    overdueFinePolicies.checkAfterSave('1.00', '2.00', '3.00', '4.00');
+    OverdueFinePolicies.openCreatingForm();
+    OverdueFinePolicies.checkCreatingForm();
+    OverdueFinePolicies.checkOverDueFineInCreating();
+    OverdueFinePolicies.fillGeneralInformation('1.00', '2.00', '3.00', '4.00');
+    OverdueFinePolicies.save();
+    OverdueFinePolicies.checkAfterSave('1.00', '2.00', '3.00', '4.00');
 
-    overdueFinePolicies.openEditingForm();
-    overdueFinePolicies.checkEditingForm('1.00', '2.00', '3.00', '4.00');
-    overdueFinePolicies.fillGeneralInformation('5.00', '6.00', '7.00', '8.00');
-    overdueFinePolicies.save();
-    overdueFinePolicies.checkAfterSave('5.00', '6.00', '7.00', '8.00');
+    OverdueFinePolicies.openEditingForm();
+    OverdueFinePolicies.checkEditingForm('1.00', '2.00', '3.00', '4.00');
+    OverdueFinePolicies.fillGeneralInformation('5.00', '6.00', '7.00', '8.00');
+    OverdueFinePolicies.save();
+    OverdueFinePolicies.checkAfterSave('5.00', '6.00', '7.00', '8.00');
 
-    overdueFinePolicies.delete();
-    overdueFinePolicies.checkAfterDelete();
+    OverdueFinePolicies.delete();
+    OverdueFinePolicies.checkAfterDelete();
   });
 });
