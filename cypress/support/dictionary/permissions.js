d205e4f7d596b6a53c7c4d22df41e08eb89f2b60
Ihor Bohdan <ihor_bohdan@epam.com>
2022-08-17 09:38:14 +0300

    FAT-780

--- cypress/support/dictionary/permissions.js
+++ cypress/support/dictionary/permissions.js
@@ -79,11 +79,12 @@ export default {
   uiUsersEditProfile: { internal: 'ui-users.edit', gui: 'Users: Can edit user profile' },
   uiUsersViewProfile: { internal: 'ui-users.view', gui: 'Users: Can view user profile' },
   uiUsersPermissions: { internal: 'ui-users.editperms', gui: 'Users: Can assign and unassign permissions to users' },
-  uiUsersfeefinesCRUD: { internal:'ui-users.feesfines.actions.all', gui: 'Users: Can create, edit and remove fees/fines' },
-  uiUsersfeefinesView: { internal: 'ui-users.feesfines.view', gui: 'Users: Can view fees/fines and loans' },
-  uiUsersManualCharge: { internal: 'ui-users.manual_charge', gui: 'Fees/Fines: Can charge' },
-  uiUsersManualPay: { internal: 'ui-users.manual_pay', gui: 'Fees/Fines: Can pay' },
-  uiUsersViewServicePoints: { internal: 'ui-users.viewuserservicepoints', gui: 'Users: Can view service points assigned to users' },
+  uiUsersfeefinesCRUD: { internal:'ui-users.feesfines.actions.all', gui:'Users: Can create, edit and remove fees/fines' },
+  uiUsersPatronBlocks: { internal: 'ui-users.patron_blocks', gui: 'Users: Can create, edit and remove patron blocks' },
+  uiUsersCreatePatronTamplate: { internal: 'ui-users.settings.patron-block-templates', gui: 'Settings (Users): Can create, edit and remove patron blocks templates' },
+  uiUsersCreatePatronGroups: { internal: 'ui-users.settings.usergroups', gui: 'Settings (Users): Can create, edit and remove patron groups' },
+  uiUsersCreatePatronLimits: { internal: 'ui-users.settings.limits', gui: 'Settings (Users): Can create, edit and remove patron blocks limits' },
+  uiUsersCreatePatronConditions: { internal: 'ui-users.settings.conditions', gui: 'Settings (Users): Can view and edit patron blocks conditions' },
   // Remote Storage
   remoteStorageCRUD: { internal: '', gui: 'Settings (Remote storage): Can create, edit, delete remote storage settings' },
   // Requests
