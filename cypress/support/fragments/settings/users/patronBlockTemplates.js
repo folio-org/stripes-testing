cd015cd4665388a574817f0532710762350473d1
Ihor Bohdan <ihor_bohdan@epam.com>
2022-08-17 09:58:26 +0300

    FAT-780

--- cypress/support/fragments/settings/users/patronBlockTemplates.js
+++ cypress/support/fragments/settings/users/patronBlockTemplates.js
@@ -1,14 +1,4 @@
-import { including } from 'bigtest';
-import { Button, MultiColumnListCell, MultiColumnListRow, PaneHeader, Section, TextField, Modal, MultiColumnListHeader, TextArea, Checkbox, NavListItem } from '../../../../../interactors';
-
-const rootSection = Section({ id: 'controlled-vocab-pane' });
-const newButton = rootSection.find(Button({ id: 'clickable-add-settings-waives' }));
-const saveButton = rootSection.find(Button({ id: including('clickable-save-settings-waives-') }));
-const reasonTextField = rootSection.find(TextField({ placeholder: 'nameReason' }));
-const descriptionTextField = TextField({ placeholder: 'description' });
-
-const getRowByReason = (reason) => cy.then(() => rootSection.find(MultiColumnListCell(reason)).row());
-const getDescriptionColumnIdex = () => cy.then(() => rootSection.find(MultiColumnListHeader('Description')).index());
+import { Button, Section, TextField, TextArea, Checkbox, NavListItem } from '../../../../../interactors';
 
 export default {
 
