cb552c5173d5df0b888b9bd588f6a74899f960a6
IhorBohdan <99126301+IhorBohdan@users.noreply.github.com>
2022-08-23 14:14:23 +0300

    FAT-1320: C15857 Create edit and delete Expense classes (#554)
    * FAT-1320: C15857 Create edit and delete Expense classes
    
    * FAT-1320: C15857 Create edit and delete Expense classes

---
+++ cypress/integration/settings/finance/expense-classes-create.spec.js
@@ -0,0 +1,28 @@
+import NewExpenceClass from '../../../support/fragments/settings/finance/newExpenseClass';
+import TestType from '../../../support/dictionary/testTypes';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import devTeams from '../../../support/dictionary/devTeams';
+import SettingsFinance from '../../../support/fragments/settings/finance/settingsFinance';
+
+describe('ui-invoices-settings: Batch Group creation', () => {
+  const expenseClass = { ...NewExpenceClass.defaultUiBatchGroup };
+  const newExpenseClass = { ...NewExpenceClass.defaultUiBatchGroup };
+  before(() => {
+    cy.getAdminToken();
+    cy.visit(`${SettingsMenu.expenseClassesPath}`);
+  });
+
+  it('C15857: Create edit and delete Expense classes (thunderjet)', { tags: [TestType.criticalPath, devTeams.thunderjet] }, () => {
+    SettingsFinance.waitExpenseClassesLoading();
+    SettingsFinance.createNewExpenseClass(expenseClass);
+    SettingsFinance.checkExpenseClass(expenseClass);
+
+    newExpenseClass.name += 'updated';
+    newExpenseClass.code += 'updated';
+
+    SettingsFinance.editExpenseClass(newExpenseClass, expenseClass.name);
+    SettingsFinance.checkExpenseClass(newExpenseClass);
+
+    SettingsFinance.deleteExpenseClass(newExpenseClass);
+  });
+});
---
+++ cypress/support/fragments/settings/finance/newExpenseClass.js
@@ -0,0 +1,9 @@
+import getRandomPostfix from '../../../utils/stringTools';
+
+export default {
+  defaultUiBatchGroup : {
+    name: `test_class_${getRandomPostfix()}`,
+    code: getRandomPostfix(),
+    externalAccountNumber: getRandomPostfix(),
+  }
+};
