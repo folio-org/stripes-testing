661656d61c9e41e3e246f471e8e25d12e5856ac1
Kseniia Dubniak <kseniia_dubniak@epam.com>
2022-08-26 17:17:46 +0300

    Added testrailReporter module to plugins

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -5,11 +5,13 @@ const { rmdir, unlink } = require('fs');
 const cypressGrep = require('cypress-grep/src/plugin');
 const { downloadFile } = require('cypress-downloadfile/lib/addPlugin');
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const testrailReporter = require('cypress-testrail-simple/src/plugin');
 // import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   cypressGrep(config);
+  testrailReporter(on, config);
 
   on('task', {
     // a task to find files matching the given mask
