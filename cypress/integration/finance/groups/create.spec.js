6f08c0aa9ab13939435f3a72eb1df8a8002cfd77
IhorBohdan <99126301+IhorBohdan@users.noreply.github.com>
2022-09-22 16:49:12 +0300

    FAT-1313:  C4054 Create a new fund group (#587)
    * FAT-1313:  C4054 Create a new fund group
    
    * FAT-1313:  final commit
    
    * FAT-1313:  final commit
    
    * FAT-1313:  final commit

---
+++ cypress/integration/finance/groups/create.spec.js
@@ -0,0 +1,24 @@
+import testType from '../../../support/dictionary/testTypes';
+import devTeams from '../../../support/dictionary/devTeams';
+import Groups from '../../../support/fragments/finance/groups/groups';
+import TopMenu from '../../../support/fragments/topMenu';
+import NewGroup from '../../../support/fragments/finance/groups/newGroup';
+
+describe('ui-finance: Group creation', () => {
+  const defaultGroup = { ...NewGroup.defaultGroup };
+
+  before(() => {
+    cy.getAdminToken();
+    cy.visit(TopMenu.groupsPath);
+  });
+
+  it('C4054 Create a new fund group (thunderjet)', { tags: [testType.criticalPath, devTeams.thunderjet] }, () => {
+    Groups.createDefaultGroup(defaultGroup);
+    Groups.checkCreatedGroup(defaultGroup);
+    Groups.deleteGroupViaActions(defaultGroup);
+
+    // should not create new ledger if mandatory fields are not filled
+    const testGroupName = 'autotest_group_';
+    Groups.tryToCreateGroupWithoutMandatoryFields(testGroupName);
+  });
+});
--- cypress/support/fragments/finance/groups/groups.js
+++ cypress/support/fragments/finance/groups/groups.js
@@ -1,4 +1,9 @@
 import getRandomPostfix from '../../../utils/stringTools';
+import { Button, Accordion, TextField, Section, KeyValue } from '../../../../../interactors';
+
+const newButton = Button('New');
+const nameField = TextField('Name*');
+const codeField = TextField('Code*');
 
 export default {
 
@@ -18,6 +23,48 @@ export default {
       .then((response) => {
         return response.body;
       });
+  },
+
+  createDefaultGroup(defaultGroup) {
+    cy.do([
+      newButton.click(),
+      nameField.fillIn(defaultGroup.name),
+      codeField.fillIn(defaultGroup.code),
+      Button('Save & Close').click()
+    ]);
+    this.waitForGroupDetailsLoading();
+  },
+
+  waitForGroupDetailsLoading : () => {
+    cy.do(Section({ id: 'pane-group-details' }).visible);
+  },
+
+  deleteGroupViaActions: () => {
+    cy.do([
+      Button('Actions').click(),
+      Button('Delete').click(),
+      Button('Delete', { id:'clickable-group-remove-confirmation-confirm' }).click()
+    ]);
+  },
+
+  tryToCreateGroupWithoutMandatoryFields(groupName) {
+    cy.do([
+      newButton.click(),
+      nameField.fillIn(groupName),
+      Button('Save & Close').click(),
+    ]);
+    cy.expect(codeField.has({ error: 'Required!' }));
+    cy.do([
+      // try to navigate without saving
+      Button('Agreements').click(),
+      Button('Keep editing').click,
+      Button('Cancel').click(),
+      Button('Close without saving').click()
+    ]);
+  },
+
+  checkCreatedGroup: (defaultGroup) => {
+    cy.expect(Accordion({ id: 'information' }).find(KeyValue({ value: defaultGroup.name })).exists());
   },
 
   deleteGroupViaApi: (groupId) => cy.okapiRequest({
