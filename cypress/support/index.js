ac58f0b737d3821c7c8a909603692c9324649207
IhorBohdan <ihor_bohdan@epam.com>
2022-09-26 16:01:39 +0300

    FAT-1315: C347878 Add multiple funds to a group with plugin

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -17,7 +17,7 @@ import 'cypress-file-upload';
 import './data-import';
 import './commands';
 
-setInteractorTimeout(100_000);
+setInteractorTimeout(35_000);
 
 require('cypress-xpath');
 require('cypress-grep')();
