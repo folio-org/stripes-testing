a5016e4c72ece22c2bca0a8da8522148e1aa8540
Maksim Terekhov* <maksim_terekhov@epam.com>
2022-10-04 18:47:20 +0300

    test migration

---
+++ cypress/e2e/data-import/mrc-import-file-for-create-items.cy.js
@@ -0,0 +1,120 @@
+import FieldMappingProfiles from '../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
+import NewActionProfile from '../../support/fragments/data_import/action_profiles/newActionProfile';
+import NewMappingProfile from '../../support/fragments/data_import/mapping_profiles/newMappingProfile';
+import ActionProfiles from '../../support/fragments/data_import/action_profiles/actionProfiles';
+import NewJobProfile from '../../support/fragments/data_import/job_profiles/newJobProfile';
+import getRandomPostfix from '../../support/utils/stringTools';
+import DataImport from '../../support/fragments/data_import/dataImport';
+import Logs from '../../support/fragments/data_import/logs/logs';
+import JobProfiles from '../../support/fragments/data_import/job_profiles/jobProfiles';
+import TestTypes from '../../support/dictionary/testTypes';
+import SettingsMenu from '../../support/fragments/settingsMenu';
+import FileDetails from '../../support/fragments/data_import/logs/fileDetails';
+import TopMenu from '../../support/fragments/topMenu';
+import permissions from '../../support/dictionary/permissions';
+import Users from '../../support/fragments/users/users';
+import DevTeams from '../../support/dictionary/devTeams';
+
+describe('ui-data-import: MARC file import with creating of the new instance, holding and item', () => {
+  let user = {};
+
+  // unique file name to upload
+  const fileName = `C343334autotestFile.${getRandomPostfix()}.mrc`;
+
+  // unique profile names
+  const jobProfileName = `autotestJobProf${getRandomPostfix()}`;
+  const actionProfileNameForInstance = `autotestActionInstance${getRandomPostfix()}`;
+  const actionProfileNameForHoldings = `autotestActionHoldings${getRandomPostfix()}`;
+  const actionProfileNameForItem = `autotestActionItem${getRandomPostfix()}`;
+  const mappingProfileNameForInstance = `autotestMappingInstance${getRandomPostfix()}`;
+  const mappingProfileNameForHoldings = `autotestMappingHoldings${getRandomPostfix()}`;
+  const mappingProfileNameForItem = `autotestMappingItem${getRandomPostfix()}`;
+
+  before(() => {
+    cy.createTempUser([
+      permissions.dataImportUploadAll.gui,
+      permissions.moduleDataImportEnabled.gui,
+      permissions.settingsDataImportEnabled.gui,
+      permissions.uiInventoryViewInstances.gui,
+    ])
+      .then(userProperties => {
+        user = userProperties;
+        cy.login(userProperties.username, userProperties.password);
+      });
+    DataImport.checkUploadState();
+  });
+
+  after(() => {
+    DataImport.checkUploadState();
+    Users.deleteViaApi(user.userId);
+  });
+
+  // removed tag smoke because test runs infinite
+  it('C343334 MARC file import with creating a new mapping profiles, action profiles and job profile (folijet)', { tags: [DevTeams.folijet] }, () => {
+    const collectionOfProfiles = [
+      {
+        mappingProfile: { typeValue: NewMappingProfile.folioRecordTypeValue.instance,
+          name: mappingProfileNameForInstance,
+          permanentLocation: '"Annex (KU/CC/DI/A)"' },
+        actionProfile: { typeValue: NewActionProfile.folioRecordTypeValue.instance,
+          name: actionProfileNameForInstance }
+      },
+      {
+        mappingProfile: { typeValue: NewMappingProfile.folioRecordTypeValue.holdings,
+          name: mappingProfileNameForHoldings },
+        actionProfile: { typeValue: NewActionProfile.folioRecordTypeValue.holdings,
+          name: actionProfileNameForHoldings }
+      },
+      {
+        mappingProfile: { typeValue: NewMappingProfile.folioRecordTypeValue.item,
+          name: mappingProfileNameForItem },
+        actionProfile: { typeValue: NewActionProfile.folioRecordTypeValue.item,
+          name: actionProfileNameForItem }
+      }
+    ];
+
+    const specialJobProfile = { ...NewJobProfile.defaultJobProfile,
+      profileName: jobProfileName,
+      acceptedType: NewJobProfile.acceptedDataType.marc };
+
+    collectionOfProfiles.forEach(profile => {
+      cy.visit(SettingsMenu.mappingProfilePath);
+      FieldMappingProfiles.createMappingProfile(profile.mappingProfile);
+      FieldMappingProfiles.checkMappingProfilePresented(profile.mappingProfile.name);
+      cy.visit(SettingsMenu.actionProfilePath);
+      ActionProfiles.createActionProfile(profile.actionProfile, profile.mappingProfile.name);
+      ActionProfiles.checkActionProfilePresented(profile.actionProfile.name);
+    });
+
+    cy.visit(SettingsMenu.jobProfilePath);
+    JobProfiles.createJobProfile(specialJobProfile);
+    collectionOfProfiles.forEach(profile => {
+      NewJobProfile.linkActionProfile(profile.actionProfile);
+    });
+    NewJobProfile.saveAndClose();
+    JobProfiles.checkJobProfilePresented(specialJobProfile.profileName);
+
+    cy.visit(TopMenu.dataImportPath);
+    DataImport.uploadFile('oneMarcBib.mrc', fileName);
+    JobProfiles.searchJobProfileForImport(specialJobProfile.profileName);
+    JobProfiles.runImportFile(fileName);
+    Logs.checkStatusOfJobProfile();
+    Logs.checkImportFile(specialJobProfile.profileName);
+    Logs.openFileDetails(fileName);
+
+    [FileDetails.columnName.srsMarc,
+      FileDetails.columnName.instance,
+      FileDetails.columnName.holdings,
+      FileDetails.columnName.item].forEach(columnName => {
+      FileDetails.checkStatusInColumn(FileDetails.status.created, columnName);
+    });
+    FileDetails.checkItemsQuantityInSummaryTable(0, '1');
+
+    // delete generated profiles
+    JobProfiles.deleteJobProfile(specialJobProfile.profileName);
+    collectionOfProfiles.forEach(profile => {
+      ActionProfiles.deleteActionProfile(profile.actionProfile.name);
+      FieldMappingProfiles.deleteFieldMappingProfile(profile.mappingProfile.name);
+    });
+  });
+});
