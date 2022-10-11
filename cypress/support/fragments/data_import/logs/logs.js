0f33656d7caf6532acb71e5dae686122e66a5847
Oll Isanbaev <oll_isanbaev@epam.com>
2022-10-11 12:53:22 +0500

    add test C360521 and methods for it

--- cypress/support/fragments/data_import/logs/logs.js
+++ cypress/support/fragments/data_import/logs/logs.js
@@ -5,7 +5,8 @@ import {
   Selection,
   SelectionList,
   Link,
-  MultiColumnList
+  MultiColumnList,
+  HTML,
 } from '../../../../../interactors';
 
 const anyProfileAccordion = Accordion({ id: 'profileIdAny' });
@@ -50,4 +51,24 @@ export default {
   },
 
   quantityRecordsInInvoice,
+
+  goToTitleLink: (title) => {
+    // When you click on a link, it opens in a new tab. Because of this, a direct transition to the link is carried out.
+    cy.do(
+      MultiColumnList({ id: 'search-results-list' })
+        .find(Link(title)).perform(element => {
+          cy.visit(element.href);
+        })
+    );
+  },
+
+  checkAuthorityLogJSON: () => {
+    cy.do(Button('Authority').click());
+    cy.expect([
+      HTML('"sourceFileId":').exists(),
+      HTML('"af045f2f-e851-4613-984c-4bc13430454a"').exists(),
+      HTML('"naturalId":').exists(),
+      HTML('"n2015002050"').exists(),
+    ]);
+  },
 };
