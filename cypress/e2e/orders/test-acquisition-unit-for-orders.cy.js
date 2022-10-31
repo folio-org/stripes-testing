6f1c4604669935f39ba8b282f3df0720e0b7bef5
IhorBohdan <ihor_bohdan@epam.com>
2022-10-31 20:38:56 +0200

    FAT-1206:C163929 Test acquisition unit restrictions for Order records

--- cypress/e2e/orders/test-acquisition-unit-for-orders.cy.js
+++ cypress/e2e/orders/test-acquisition-unit-for-orders.cy.js
@@ -1,7 +1,6 @@
 import permissions from '../../support/dictionary/permissions';
 import testType from '../../support/dictionary/testTypes';
 import devTeams from '../../support/dictionary/devTeams';
-import FiscalYears from '../../support/fragments/finance/fiscalYears/fiscalYears';
 import TopMenu from '../../support/fragments/topMenu';
 import Users from '../../support/fragments/users/users';
 import FinanceHelp from '../../support/fragments/finance/financeHelper';
