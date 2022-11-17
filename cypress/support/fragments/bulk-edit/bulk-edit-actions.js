357bb1f4db1b52cd2fb0c851bf044f12f6e0e781
Maxim Terekhov <maksim_terekhov@epam.com>
2022-11-17 12:54:54 +0300

    FAT-3363 C359210 Verify the in-app bulk edit permanent loan type (#668)
    * test added
    
    * linter fixes
    
    * review fixes

--- cypress/support/fragments/bulk-edit/bulk-edit-actions.js
+++ cypress/support/fragments/bulk-edit/bulk-edit-actions.js
@@ -4,6 +4,10 @@ import {Modal, SelectionOption, Button, DropdownMenu, Checkbox, MultiColumnListH
 
 const actionsBtn = Button('Actions');
 const dropdownMenu = DropdownMenu();
+const cancelBtn = Button({ id: 'clickable-cancel' });
+const createBtn = Button({ id: 'clickable-create-widget' });
+const plusBtn = Button({ icon: 'plus-sign' });
+const deleteBtn = Button({ icon: 'trash' });
 // interactor doesn't allow to pick second the same select
 function getLocationSelect() {
   return cy.get('select').eq(2);
@@ -23,6 +27,10 @@ function getPatronBlockSelect() {
 
 function getPatronGroupTypeSelect() {
   return cy.get('select').eq(3);
+}
+
+function getPermanentLoanSelect() {
+  return cy.get('select').eq(1);
 }
 
 
@@ -81,6 +89,14 @@ export default {
   fillPatronGroup(group = 'staff (Staff Member)') {
     getPatronBlockSelect().select('Patron group');
     getPatronGroupTypeSelect().select(group);
+  },
+
+  fillLoanType(type = 'Selected') {
+    getPermanentLoanSelect().select('Permanent loan type');
+    cy.do([
+      Button({ id: 'loanType' }).click(),
+      SelectionOption(including(type)).click(),
+    ]);
   },
 
   verifyNoMatchingOptionsForLocationFilter() {
@@ -161,6 +177,46 @@ export default {
       dropdownMenu.find(Checkbox({ name: 'username', checked: true, disabled: isDisabled })).exists(),
       dropdownMenu.find(Checkbox({ name: 'email', checked: false, disabled: isDisabled })).exists(),
       dropdownMenu.find(Checkbox({ name: 'expirationDate', checked: false, disabled: isDisabled })).exists(),
+    ]);
+  },
+
+  verifyItemActionDropdownItems(isDisabled = false) {
+    cy.expect([
+      dropdownMenu.find(Checkbox({ name: 'barcode', checked: true, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'status', checked: true, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'effectiveLocation', checked: true, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'callNumber', checked: true, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'hrid', checked: true, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'materialType', checked: true, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'permanentLoanType', checked: true, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'temporaryLoanType', checked: true, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'id', checked: false, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'formerIds', checked: false, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'accessionNumber', checked: false, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'permanentLocation', checked: false, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'temporaryLocation', checked: false, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'copyNumber', checked: false, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'enumeration', checked: false, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'chronology', checked: false, disabled: isDisabled })).exists(),
+      dropdownMenu.find(Checkbox({ name: 'volume', checked: false, disabled: isDisabled })).exists(),
+    ]);
+  },
+
+  verifyModifyLandingPageBeforeModifying() {
+    cy.expect([
+      cancelBtn.has({ disabled: false }),
+      createBtn.has({ disabled: true }),
+      plusBtn.has({ disabled: false }),
+      deleteBtn.has({ disabled: true }),
+    ]);
+  },
+
+  verifyModifyLandingPageAfterModifying() {
+    cy.expect([
+      cancelBtn.has({ disabled: false }),
+      createBtn.has({ disabled: false }),
+      plusBtn.has({ disabled: false }),
+      deleteBtn.has({ disabled: true }),
     ]);
   },
 
