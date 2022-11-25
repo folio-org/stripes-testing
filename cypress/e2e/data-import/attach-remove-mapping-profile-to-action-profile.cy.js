06e171613acd35b1751c2bf582d27220a5223d58
Tetiana_Paranich <tetiana_paranich@epam.com>
2022-11-25 13:23:52 +0200

    added last check

--- cypress/e2e/data-import/attach-remove-mapping-profile-to-action-profile.cy.js
+++ cypress/e2e/data-import/attach-remove-mapping-profile-to-action-profile.cy.js
@@ -6,11 +6,15 @@ import FieldMappingProfiles from '../../support/fragments/data_import/mapping_pr
 import NewFieldMappingProfile from '../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
 import ActionProfiles from '../../support/fragments/data_import/action_profiles/actionProfiles';
 import NewActionProfile from '../../support/fragments/data_import/action_profiles/newActionProfile';
+import ActionProfileView from '../../support/fragments/data_import/action_profiles/actionProfileView';
+import FieldMappingProfileView from '../../support/fragments/data_import/mapping_profiles/fieldMappingProfileView';
+import ActionProfileEdit from '../../support/fragments/data_import/action_profiles/actionProfileEdit';
+import ConfirmRemoval from '../../support/fragments/data_import/action_profiles/modals/confirmRemoval';
 
-describe('ui-data-import: MARC file import with matching for 999 ff field', () => {
+describe('ui-data-import: Attach/Remove a field mapping profile to an action profile', () => {
   // unique profile names
   const mappingProfileName = `C11115 autotest mapping profile ${Helper.getRandomBarcode()}`;
-  const actionProfileName = `C11115 autotest ${Helper.getRandomBarcode()}`;
+  const actionProfileName = `C11115 autotest action profile ${Helper.getRandomBarcode()}`;
 
   const mappingProfile = {
     name: mappingProfileName,
@@ -38,6 +42,20 @@ describe('ui-data-import: MARC file import with matching for 999 ff field', () =
     ActionProfiles.createActionProfile(actionProfile, mappingProfile.name);
     ActionProfiles.checkActionProfilePresented(actionProfile.name);
 
-    ActionProfiles.selectActionProfile(actionProfile.name);
+    ActionProfileView.verifyLinkedFieldMappingProfile(mappingProfile.name);
+    ActionProfileView.openFieldMappingProfileView();
+    FieldMappingProfileView.verifyLinkedActionProfile(actionProfile.name);
+    FieldMappingProfileView.openAssociatedActionProfile();
+    ActionProfiles.verifyActionProfileOpened();
+
+    ActionProfileView.edit();
+    ActionProfileEdit.unlinkFieldMappingProfile();
+    ConfirmRemoval.canselRemoveFieldMappingProfile();
+    ActionProfileEdit.fieldMappingProfilePresented(mappingProfile.name);
+    ActionProfileEdit.unlinkFieldMappingProfile();
+    ConfirmRemoval.confirmRemovefieldMappingProfile();
+    ActionProfileEdit.fieldMappingProfileAbsent();
+    ActionProfileEdit.save();
+    ActionProfileView.verifyLinkedFieldMappingProfileAbsent(mappingProfile.name);
   });
 });
