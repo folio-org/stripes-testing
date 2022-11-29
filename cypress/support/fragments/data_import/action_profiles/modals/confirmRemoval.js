26886a0403809f8467a4221152d0df8e8db06c5e
Tetiana_Paranich <tetiana_paranich@epam.com>
2022-11-29 22:25:33 +0200

    fixed C11115

--- cypress/support/fragments/data_import/action_profiles/modals/confirmRemoval.js
+++ cypress/support/fragments/data_import/action_profiles/modals/confirmRemoval.js
@@ -2,9 +2,10 @@ import { Button, Modal } from '../../../../../../interactors';
 
 const confirmButton = Button('Confirm');
 const confirmEditModal = Modal({ id:'confirm-edit-action-profile-modal' });
+const canselButton = Button('Cancel');
 
 export default {
-  confirmRemovefieldMappingProfile:() => cy.do(confirmEditModal.find(confirmButton).click()),
-  canselRemoveFieldMappingProfile:() => cy.do(confirmEditModal.find(confirmButton).click())
+  canselRemoveFieldMappingProfile:() => cy.do(confirmEditModal.find(canselButton).click()),
+  confirmRemovefieldMappingProfile:() => cy.do(confirmEditModal.find(confirmButton).click())
 };
 
