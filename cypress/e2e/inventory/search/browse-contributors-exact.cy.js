a91435fb4a51aeea1b07d91595bae7467d1288ec
Oll Isanbaev <oll_isanbaev@epam.com>
2022-11-29 17:39:33 +0500

    delete obsolete comments

--- cypress/e2e/inventory/search/browse-contributors-exact.cy.js
+++ cypress/e2e/inventory/search/browse-contributors-exact.cy.js
@@ -50,7 +50,6 @@ describe('ui-inventory: search', () => {
     });
   });
 
-  // https://issues.folio.org/browse/UIIN-2199
   it('C353639 Browse contributors with exact match query (spitfire)', { tags: [testType.smoke, devTeams.spitfire] }, () => {
     InventorySearchAndFilter.verifyKeywordsAsDefault();
     BrowseContributors.checkBrowseOptions();
