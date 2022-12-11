a55b719013b6f04e9c2c96eeb43f3ed3486b02b6
IhorBohdan <ihor_bohdan@epam.com>
2022-12-11 13:12:16 +0200

    Fat-2659:C350602 Select Acquisition Method in Order Template

---
+++ cypress/support/fragments/settings/orders/acquisitionMethods.js
@@ -0,0 +1,21 @@
+import uuid from 'uuid';
+import getRandomPostfix from '../../../utils/stringTools';
+
+export default {
+    defaultAcquisitionMethod: {
+        id: uuid(),
+        value: `AU_name_${getRandomPostfix()}`,
+      },
+    createNewAcquisitionMethodViaAPI: (acquisitionMethod) => cy.okapiRequest({
+        method: 'POST',
+        path: 'orders/acquisition-methods',
+        body: acquisitionMethod,
+        isDefaultSearchParamsRequired: false,
+      }).then(response => response.body),
+  
+    deleteAcquisitionMethodViaAPI: (id) => cy.okapiRequest({
+        method: 'DELETE',
+        path: `orders/acquisition-methods/${id}`,
+        isDefaultSearchParamsRequired: false,
+      }),
+  };
---
+++ cypress/support/fragments/settings/orders/orderTemplates.js
@@ -0,0 +1,41 @@
+import { Button, Checkbox, Modal, NavListItem, Pane, PaneContent, PaneHeader, SearchField, SelectionOption, TextField } from '../../../../../interactors';
+import SearchHelper from '../../finance/financeHelper';
+import getRandomPostfix from '../../../utils/stringTools';
+
+export default {
+    defaultOrdertemplate: {
+        name: `OT_name_${getRandomPostfix()}`,
+      },
+
+    waitLoading: () => {
+        cy.expect(Pane({ id: 'order-settings-order-templates-list' }).exists());
+      },
+    
+    newTemplate: () => {
+    cy.do(Button('New').click());
+    },
+    
+    fillTemplateInformationWithAcquisitionMethod: (templateName,organizationName,acquisitionMethod) => {
+        cy.do([
+            TextField({ name: 'templateName'}).fillIn(templateName),
+            Button('PO information').click(),
+            Button({ id: 'vendor-plugin' }).click(),
+            Modal('Select Organization').find(SearchField({ id: 'input-record-search' })).fillIn(organizationName),
+            Button('Search').click(),
+        ]);
+        SearchHelper.selectFromResultsList();
+        cy.do([
+            Button({ id: 'accordion-toggle-button-lineDetails' }).click(),
+            Button({ id: 'acquisition-method' }).click(),
+            SelectionOption(acquisitionMethod).click(),
+        ]);
+    },
+
+    saveTemplate : () => {
+        cy.do(Button({ id: 'save-order-template-button' }).click());
+    },
+
+    checkTemplateCreated: (templateName) => {
+        cy.expect(PaneContent({ id: 'order-settings-order-templates-list-content'}).find(NavListItem(templateName)).exists());
+    }
+};
