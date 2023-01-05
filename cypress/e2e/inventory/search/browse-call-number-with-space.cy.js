e4651647ebf400e2e9ff7181e517218d2c4fd490
Sherzod Kenjaev <kenjaevsherzod@gmail.com>
2023-01-05 20:39:35 +0500

    fixes in tests

--- cypress/e2e/inventory/search/browse-call-number-with-space.cy.js
+++ cypress/e2e/inventory/search/browse-call-number-with-space.cy.js
@@ -1,24 +1,28 @@
-import getRandomPostfix from "../../../support/utils/stringTools";
-import DevTeams from "../../../support/dictionary/devTeams";
-import TestTypes from "../../../support/dictionary/testTypes";
-import InventoryActions from "../../../support/fragments/inventory/inventoryActions";
-import InventoryInstances from "../../../support/fragments/inventory/inventoryInstances";
-import InventorySearchAndFilter from "../../../support/fragments/inventory/inventorySearchAndFilter";
-import BrowseCallNumber from "../../../support/fragments/inventory/search/browseCallNumber";
-import TopMenu from "../../../support/fragments/topMenu";
-import BrowseContributors from "../../../support/fragments/inventory/search/browseContributors";
+import getRandomPostfix from '../../../support/utils/stringTools';
+import DevTeams from '../../../support/dictionary/devTeams';
+import TestTypes from '../../../support/dictionary/testTypes';
+import InventoryActions from '../../../support/fragments/inventory/inventoryActions';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import BrowseCallNumber from '../../../support/fragments/inventory/search/browseCallNumber';
+import TopMenu from '../../../support/fragments/topMenu';
+import BrowseContributors from '../../../support/fragments/inventory/search/browseContributors';
+import Users from '../../../support/fragments/users/users';
+import permissions from '../../../support/dictionary/permissions';
 
-describe("ui-inventory: search", () => {
+describe('ui-inventory: search', () => {
   const item = {
     instanceName: `instanceForRecord_${getRandomPostfix()}`,
     itemBarcode: getRandomPostfix(),
     publisher: null,
-    holdingCallNumber: "1",
-    itemCallNumber: "RR 718",
+    holdingCallNumber: '1',
+    itemCallNumber: 'RR 718',
   };
+
   const testData = {
     exactSearch: item.itemCallNumber,
-    nonExactSearch: item.itemCallNumber.replace(/ /g, ""),
+    itemWithoutSpace: 'RR718',
+    itemWithLowerCaseR: 'Rr 718'
   };
 
   const search = (query) => {
@@ -29,45 +33,35 @@ describe("ui-inventory: search", () => {
     InventoryActions.actionsIsAbsent();
     InventorySearchAndFilter.showsOnlyEffectiveLocation();
     InventorySearchAndFilter.browseSubjectsSearch(query);
-    BrowseCallNumber.checkExactSearchResult(
-      testData.exactSearch,
-      item.instanceName
-    );
+    BrowseCallNumber.checkExactSearchResult(testData.exactSearch, item.instanceName);
   };
 
-  beforeEach("Creating user and instance with item with call number", () => {
+  beforeEach('Creating user and instance with item with call number', () => {
     cy.getAdminToken().then(() => {
-      InventoryInstances.createInstanceViaApi(
-        item.instanceName,
-        item.itemBarcode,
-        item.publisher,
-        item.holdingCallNumber,
-        item.itemCallNumber
-      );
-    });
-
-    cy.loginAsAdmin({
-      path: TopMenu.inventoryPath,
-      waiter: InventorySearchAndFilter.waitLoading,
+      InventoryInstances.createInstanceViaApi(item.instanceName, item.itemBarcode, item.publisher, item.holdingCallNumber, item.itemCallNumber);
+      cy.createTempUser([
+        permissions.uiCallNumberBrowse.gui,
+        permissions.inventoryAll.gui
+      ]).then(userProperties => {
+        testData.user = userProperties;
+        cy.login(userProperties.username, userProperties.password, { path: TopMenu.inventoryPath, waiter: InventorySearchAndFilter.waitLoading });
+      });
     });
   });
 
   afterEach("Deleting user and instance", () => {
-    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(
-      item.itemBarcode
-    );
+    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.itemBarcode);
+    Users.deleteViaApi(testData.user.userId);
   });
 
-  it(
-    'C358140 Verify that browsing for "call number" with "space" value will get the correct result (spitfire)',
-    { tags: [DevTeams.spitfire, TestTypes.smoke] },
-    () => {
+  it('C358140 Verify that browsing for "call number" with "space" value will get the correct result (spitfire)', { tags: [DevTeams.spitfire, TestTypes.smoke] }, () => {
       search(testData.exactSearch);
       BrowseCallNumber.checkExactSearchResult(testData.exactSearch);
       BrowseContributors.resetAllInSearchPane();
-      search(testData.nonExactSearch);
+      search(testData.itemWithoutSpace);
       BrowseCallNumber.checkExactSearchResult(testData.exactSearch);
-      // Add the test. There are no several additional checks that are present in steps.
-    }
-  );
+      BrowseContributors.resetAllInSearchPane();
+      search(testData.itemWithLowerCaseR);
+      BrowseCallNumber.checkExactSearchResult(testData.exactSearch);
+  });
 });
--- cypress/e2e/inventory/search/browse-contributors-exact.cy.js
+++ cypress/e2e/inventory/search/browse-contributors-exact.cy.js
@@ -1,13 +1,13 @@
-import testType from "../../../support/dictionary/testTypes";
-import Permissions from "../../../support/dictionary/permissions";
-import InventoryInstance from "../../../support/fragments/inventory/inventoryInstance";
-import InventorySearchAndFilter from "../../../support/fragments/inventory/inventorySearchAndFilter";
-import BrowseContributors from "../../../support/fragments/inventory/search/browseContributors";
-import TopMenu from "../../../support/fragments/topMenu";
-import Users from "../../../support/fragments/users/users";
-import devTeams from "../../../support/dictionary/devTeams";
+import testType from '../../../support/dictionary/testTypes';
+import permissions from '../../../support/dictionary/permissions';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import BrowseContributors from '../../../support/fragments/inventory/search/browseContributors';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import devTeams from '../../../support/dictionary/devTeams';
 
-describe("ui-inventory: search", () => {
+describe('ui-inventory: search', () => {
   const testData = {};
   const instanceA = BrowseContributors.defaultInstanceAWithContributor;
   const instanceZ = BrowseContributors.defaultInstanceZWithContributor;
@@ -21,43 +21,36 @@ describe("ui-inventory: search", () => {
     });
 
     BrowseContributors.getContributorNameTypes().then((res) => {
-      instanceA.contributors[0].contributorNameTypeId =
-        res.body.contributorNameTypes[0].id;
-      instanceA.contributors[0].contributorNameType =
-        res.body.contributorNameTypes[0].name;
-      instanceA.contributors[0].contributorTypeText =
-        res.body.contributorNameTypes[0].name;
-      instanceZ.contributors[0].contributorNameTypeId =
-        res.body.contributorNameTypes[0].id;
-      instanceZ.contributors[0].contributorNameType =
-        res.body.contributorNameTypes[0].name;
-      instanceZ.contributors[0].contributorTypeText =
-        res.body.contributorNameTypes[0].name;
+      instanceA.contributors[0].contributorNameTypeId = res.body.contributorNameTypes[0].id;
+      instanceA.contributors[0].contributorNameType = res.body.contributorNameTypes[0].name;
+      instanceA.contributors[0].contributorTypeText = res.body.contributorNameTypes[0].name;
+      instanceZ.contributors[0].contributorNameTypeId = res.body.contributorNameTypes[0].id;
+      instanceZ.contributors[0].contributorNameType = res.body.contributorNameTypes[0].name;
+      instanceZ.contributors[0].contributorTypeText = res.body.contributorNameTypes[0].name;
     });
 
     BrowseContributors.createInstanceWithContributorViaApi(instanceA);
     BrowseContributors.createInstanceWithContributorViaApi(instanceZ);
 
-    cy.getInstanceById(instanceA.id).then((res) => {
-      testData.instanceAProps = res;
-    });
-    cy.getInstanceById(instanceZ.id).then((res) => {
-      testData.instanceZProps = res;
-    });
+    cy.getInstanceById(instanceA.id)
+      .then((res) => {
+        testData.instanceAProps = res;
+      });
+    cy.getInstanceById(instanceZ.id)
+      .then((res) => {
+        testData.instanceZProps = res;
+      });
 
-    cy.createTempUser([Permissions.uiInventoryViewInstances.gui]).then(
-      (resUserProperties) => {
-        testData.user = resUserProperties;
-        cy.login(resUserProperties.username, resUserProperties.password);
-        cy.visit(TopMenu.inventoryPath);
-      }
-    );
+    cy.createTempUser([
+      permissions.uiInventoryViewInstances.gui,
+    ]).then((resUserProperties) => {
+      testData.user = resUserProperties;
+      cy.login(resUserProperties.username, resUserProperties.password);
+      cy.visit(TopMenu.inventoryPath);
+    });
   });
 
-  it(
-    "C353639 Browse contributors with exact match query (spitfire)",
-    { tags: [testType.smoke, devTeams.spitfire] },
-    () => {
+  it("C353639 Browse contributors with exact match query (spitfire)", { tags: [testType.smoke, devTeams.spitfire] }, () => {
       BrowseContributors.clickBrowseBtn();
       InventorySearchAndFilter.verifyKeywordsAsDefault();
       BrowseContributors.checkBrowseOptions();
@@ -65,16 +58,12 @@ describe("ui-inventory: search", () => {
       BrowseContributors.checkSearch();
       BrowseContributors.browse(instanceA.contributors[0].name);
       BrowseContributors.checkSearchResultsTable();
-      BrowseContributors.checkExactSearchResult(
-        instanceA.contributors[0],
-        instanceZ.contributors[0]
-      );
+      BrowseContributors.checkExactSearchResult(instanceA.contributors[0], instanceZ.contributors[0]);
       BrowseContributors.openInstance(instanceA.contributors[0]);
       BrowseContributors.checkInstance(instanceA);
-    }
-  );
+  });
 
-  afterEach("Deleting user", () => {
+  afterEach('Deleting user', () => {
     Users.deleteViaApi(testData.user.userId);
     InventoryInstance.deleteInstanceViaApi(instanceA.id);
     InventoryInstance.deleteInstanceViaApi(instanceZ.id);
