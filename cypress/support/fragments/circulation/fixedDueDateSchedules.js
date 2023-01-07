9da9d5f651bf3f39635ededba9ec846f12341a94
Jasurbek Erkinov <re.nine.jas@gmail.com>
2023-01-07 23:46:09 +0500

    fixed bugs

--- cypress/support/fragments/circulation/fixedDueDateSchedules.js
+++ cypress/support/fragments/circulation/fixedDueDateSchedules.js
@@ -6,6 +6,7 @@ import {
   NavListItem,
   TextArea,
   TextField,
+  Pane,
 } from '../../../../interactors';
 
 const createFormat = 'YYYY/MM/DD';
@@ -13,17 +14,17 @@ const detailsFormat = 'M/D/YYYY';
 
 export default {
   fillScheduleInfo(data) {
+    cy.expect(Pane(`Edit: ${data.name}`).exists());
     cy.do([
       TextField({ id: 'input_schedule_name' }).fillIn(data.name),
       TextArea({ name: 'description' }).fillIn(data.description),
     ]);
     data.schedules.forEach((schedule, index) => {
+      cy.wait(5000);
       cy.do([
         TextField({ name: `schedules[${index}].from` }).fillIn(moment(schedule.from).format(createFormat)),
         TextField({ name: `schedules[${index}].to` }).fillIn(moment(schedule.to).format(createFormat)),
         TextField({ name: `schedules[${index}].due` }).fillIn(moment(schedule.due).format(createFormat)),
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(2000),
       ]);
     });
   },
@@ -37,7 +38,8 @@ export default {
   editSchedule(name, newScheduleData) {
     cy.do([
       NavListItem(name).click(),
-      Button({ id: 'clickable-edit-item' }).click(),
+      Button('Actions').click(),
+      Button('Edit').click(),
     ]);
     this.fillScheduleInfo({ name, ...newScheduleData });
     cy.do(Button({ id: 'clickable-save-fixedDueDateSchedule' }).click());
