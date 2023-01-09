90dabd9f88c5ad484c32d3b1a2dd1621dca9ed24
Faruq Yusuff <faruqyusuff437@gmail.com>
2023-01-09 12:17:02 -0600

    FAT 4081 -> Implement e-2-e automation of test case C360954: Sort headings on "All calendars" tab

---
+++ cypress/e2e/calendar/sort-headings-all-calendars.cy.js
@@ -0,0 +1,178 @@
+import {
+  MultiColumnListHeader, Pane, Link
+} from '../../../interactors';
+import calendarFixtures from '../../fixtures/calendar_e2e_fixtures';
+import CalendarSortTableTools from '../../support/utils/uiCalendar_SortTableTools';
+import { assertCalendarsIsProperlySorted } from '../../support/fragments/calendar/sort-table';
+
+const testServicePoint = calendarFixtures.servicePoint;
+const testCalendar = calendarFixtures.calendar;
+
+const addSort = CalendarSortTableTools.sortableMultiColumnList.addSort;
+const sortCalendars = CalendarSortTableTools.sortAllCalendars.sortCalendars;
+
+
+
+describe('Sort headings on "All calendars" tab', () => {
+  let testCalendarResponse;
+  before(() => {
+    // login as admin so necessary state can be created
+    cy.loginAsAdmin();
+
+    // get admin token to use in okapiRequest to retrieve service points
+    cy.getAdminToken();
+
+    // reset db state
+    cy.deleteServicePoint(testServicePoint.id, false);
+
+    // create test service point
+    cy.createServicePoint(testServicePoint, (response) => {
+      testCalendar.assignments = [response.body.id];
+
+      cy.createCalendar(testCalendar, (calResponse) => {
+        testCalendarResponse = calResponse.body;
+      });
+      cy.openCalendarSettings();
+    });
+  });
+
+  after(() => {
+    cy.deleteServicePoint(testServicePoint.id, true);
+    cy.deleteCalendar(testCalendarResponse.id);
+  });
+
+
+  it('sorts headings on "All calendars" tab', () => {
+    const calendars = [];
+    const servicePoints = [];
+    cy.do([
+      Pane('Calendar').find(Link('All calendars')).click(),
+    ]);
+
+    // intercept API request
+    cy.intercept(Cypress.env('OKAPI_HOST') + '/calendar/calendars?limit=2147483647', (req) => {
+      if (req.method === 'GET') {
+        req.continue((res) => {
+          expect(res.statusCode).equals(200);
+          calendars.push(...res.body.calendars);
+        });
+      }
+    }).as('getCalendars');
+
+    cy.wait('@getCalendars').then(() => {
+      cy.okapiRequest({
+        path: 'service-points',
+        searchParams: {
+          'cql.allRecords': '1',
+          'limit': '2147483647'
+        },
+        body: null,
+        isDefaultSearchParamsRequired: false
+      }).then(res => {
+        servicePoints.push(...res.body.servicepoints);
+
+        // replace assignments' IDs with the service point names
+        calendars.forEach((calendar) => {
+          const servicePointNames = [];
+          if (calendar.assignments.length > 0) {
+            calendar.assignments.forEach(assignment => {
+              const [servicePoint] = servicePoints.filter((sp) => sp.id === assignment);
+              servicePointNames.push(servicePoint.name);
+            });
+            calendar.assignments = servicePointNames;
+          }
+        });
+
+
+        // currentRows is the list of rows resulting from the sorting of baseRows using the primary and secondary sort
+        let currentRows;
+
+        // 'sorts' contains the primary and secondary sort
+        // The SortableMultiColumnList component uses a primary sort and an optional secondary sort to sort its data
+        // see 'addSort' for how this sorts array is updated
+        let sorts = [];
+
+        // sort in ascending order by calendar name
+        cy.do(
+          MultiColumnListHeader('Calendar name').click(),
+        );
+
+        sorts = addSort(sorts, 'calendarName');
+        currentRows = sortCalendars(calendars, sorts);
+        assertCalendarsIsProperlySorted(currentRows, 'calendarName');
+
+        // sort in descending order by calendar name
+        cy.do(
+          MultiColumnListHeader('Calendar name').click(),
+        );
+
+        sorts = addSort(sorts, 'calendarName');
+        currentRows = sortCalendars(calendars, sorts);
+        assertCalendarsIsProperlySorted(currentRows, 'calendarName');
+
+
+
+
+        // sort in ascending order by start date
+        cy.do(
+          MultiColumnListHeader('Start date').click(),
+        );
+
+        sorts = addSort(sorts, 'startDate');
+        currentRows = sortCalendars(calendars, sorts);
+        assertCalendarsIsProperlySorted(currentRows, 'startDate');
+
+        // sort in descending order by start date
+        cy.do(
+          MultiColumnListHeader('Start date').click(),
+        );
+
+        sorts = addSort(sorts, 'startDate');
+        currentRows = sortCalendars(calendars, sorts);
+        assertCalendarsIsProperlySorted(currentRows, 'startDate');
+
+
+
+
+        // sort in ascending order by end date
+        cy.do(
+          MultiColumnListHeader('End date').click(),
+        );
+
+        sorts = addSort(sorts, 'endDate');
+        currentRows = sortCalendars(calendars, sorts);
+        assertCalendarsIsProperlySorted(currentRows, 'endDate');
+
+        // sort in descending order by end date
+        cy.do(
+          MultiColumnListHeader('End date').click(),
+        );
+
+        sorts = addSort(sorts, 'endDate');
+        currentRows = sortCalendars(calendars, sorts);
+        assertCalendarsIsProperlySorted(currentRows, 'endDate');
+
+
+
+
+        // sort in ascending order by assignments
+        cy.do(
+          MultiColumnListHeader('Assignments').click(),
+        );
+
+        sorts = addSort(sorts, 'assignments');
+        currentRows = sortCalendars(calendars, sorts);
+        assertCalendarsIsProperlySorted(currentRows, 'assignments');
+
+        // sort in descending order by assignments
+        cy.do(
+          MultiColumnListHeader('Assignments').click(),
+        );
+
+        sorts = addSort(sorts, 'assignments');
+        currentRows = sortCalendars(calendars, sorts);
+        assertCalendarsIsProperlySorted(currentRows, 'assignments');
+      });
+    });
+  });
+});
