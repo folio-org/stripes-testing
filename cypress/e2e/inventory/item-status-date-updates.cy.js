a1f6ccff27790085be36737bfd8820f5ec15b229
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-01-10 13:09:41 +0200

    deleted unused import

--- cypress/e2e/inventory/item-status-date-updates.cy.js
+++ cypress/e2e/inventory/item-status-date-updates.cy.js
@@ -8,7 +8,6 @@ import {
   LOST_ITEM_FEES_POLICY_NAMES,
   LOAN_POLICY_NAMES,
 } from '../../support/constants';
-import TestTypes from '../../support/dictionary/testTypes';
 import Orders from '../../support/fragments/orders/orders';
 import NewOrder from '../../support/fragments/orders/newOrder';
 import TopMenu from '../../support/fragments/topMenu';
@@ -35,7 +34,6 @@ import BasicOrderLine from '../../support/fragments/orders/basicOrderLine';
 import NewLocations from '../../support/fragments/settings/tenant/locations/newLocation';
 import Users from '../../support/fragments/users/users';
 import CheckOutActions from '../../support/fragments/check-out-actions/check-out-actions';
-import DevTeams from '../../support/dictionary/devTeams';
 import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
 import DateTools from '../../support/utils/dateTools';
 
