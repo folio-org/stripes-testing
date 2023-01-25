ad6540a3fc52214572eb988a768040a02de1251b
Diana Holik <Diana_Holik@epam.com>
2023-01-26 01:16:51 +0200

    waiters are removed

--- cypress/support/fragments/users/userDefaultObjects/loanDetails.js
+++ cypress/support/fragments/users/userDefaultObjects/loanDetails.js
@@ -73,7 +73,7 @@ export default {
   },
   checkLostDate(date) {
     this.checkDateValid(date);
- //   this.checkKeyValue('Lost', DateTools.getFormattedDate({ date }));
+    this.checkKeyValue('Lost', DateTools.getFormattedDate({ date }));
   },
   checkRenewalCount() {
     this.checkKeyValue('Renewal count', '1');
