267c3b5988e624c17347f249988a880186d40f7a
IhorBohdan <ihor_bohdan@epam.com>
2023-01-25 19:09:22 +0200

    Nolana fix

--- cypress/support/fragments/invoices/invoices.js
+++ cypress/support/fragments/invoices/invoices.js
@@ -49,7 +49,7 @@ export default {
     ]);
     this.selectVendorOnUi(invoice.vendorName);
     cy.do([
-      Selection('Accounting code*').open(),
+      Selection('Accounting code').open(),
       SelectionList().select(`Default (${invoice.accountingCode})`),
       Selection('Batch group*').open(),
       SelectionList().select(invoice.batchGroup),
@@ -60,6 +60,30 @@ export default {
     cy.do(saveAndClose.click());
     InteractorsTools.checkCalloutMessage(invoiceStates.invoiceCreatedMessage);
   },
+
+  createInvoiceWithoutVendorAdress(invoice) {
+    cy.do(actionsButton.click());
+    cy.expect(buttonNew.exists());
+    cy.do([
+      buttonNew.click(),
+      Selection('Status*').open(),
+      SelectionList().select(invoice.status),
+      TextField('Invoice date*').fillIn(invoice.invoiceDate),
+      TextField('Vendor invoice number*').fillIn(invoice.invoiceNumber),
+    ]);
+    this.selectVendorOnUi(invoice.vendorName);
+    cy.do([
+      Selection('Accounting code').open(),
+      SelectionList().select(`Default (${invoice.accountingCode})`),
+      Selection('Batch group*').open(),
+      SelectionList().select(invoice.batchGroup),
+      Select({ id: 'invoice-payment-method' }).choose('Cash'),
+      Checkbox('Export to accounting').click()
+    ]);
+    cy.do(saveAndClose.click());
+    InteractorsTools.checkCalloutMessage(invoiceStates.invoiceCreatedMessage);
+  },
+
   createSpecialInvoice(invoice, vendorPrimaryAddress) {
     cy.do(actionsButton.click());
     cy.expect(buttonNew.exists());
@@ -299,5 +323,9 @@ export default {
       .set('Product ID', orderLine.details.productIds[0].productId)
       .set('Product ID ISBN', orderLine.details.productIds[0].productId);
     return searchParamsMap;
-  }
+  },
+
+  waitLoading : () => {
+    cy.expect(Pane({ id: 'invoice-results-pane' }).exists());
+  },
 };
--- cypress/support/fragments/invoices/vendorAddress.js
+++ cypress/support/fragments/invoices/vendorAddress.js
@@ -9,5 +9,17 @@ export default {
     isPrimary: true,
     categories: [],
     language: ''
+  },
+
+  specialVendorAddress : {
+    addressLine1: 'FolioAdress1',
+    addressLine2: 'FoolioAdress2',
+    city: 'New-York',
+    stateRegion: 'New-York State',
+    zipCode: '1111',
+    country: 'USA',
+    isPrimary: true,
+    categories: [],
+    language: 'eng'
   }
 };
