1dcfb911dbb806d0e8f8b7124482e1256bd24035
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-01-27 00:19:05 +0300

    added confirm for deleting

--- cypress/e2e/data-import/log-details/delete-logs-from-view-all.cy.js
+++ cypress/e2e/data-import/log-details/delete-logs-from-view-all.cy.js
@@ -1,17 +1,11 @@
 import permissions from '../../../support/dictionary/permissions';
 import DevTeams from '../../../support/dictionary/devTeams';
 import TestTypes from '../../../support/dictionary/testTypes';
-
 import DataImport from '../../../support/fragments/data_import/dataImport';
 import TopMenu from '../../../support/fragments/topMenu';
 import LogsViewAll from '../../../support/fragments/data_import/logs/logsViewAll';
-import DateTools from '../../../support/utils/dateTools';
 import DeleteDataImportLogsModal from '../../../support/fragments/data_import/logs/deleteDataImportLogsModal';
 import Users from '../../../support/fragments/users/users';
-import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
-
-import Logs from '../../../support/fragments/data_import/logs/logs';
-
 
 let user;
 const maxLogsQuantityOnPage = 100;
@@ -30,17 +24,25 @@ describe('ui-data-import: delete logs from "View all" page', () => {
       });
   });
 
-  // after(() => {
-  //   Users.deleteViaApi(user.userId);
-  // });
-
-  it('C367923 A user can delete logs from the Import app "View all" page (folijet)', { tags: [TestTypes.criticalPath, DevTeams.folijet] }, () => {
-    LogsViewAll.openViewAll();
-    LogsViewAll.viewAllIsOpened();
-    LogsViewAll.selectAllLogs();
-    LogsViewAll.checkIsLogsSelected(maxLogsQuantityOnPage);
-    LogsViewAll.deleteLog();
-    DeleteDataImportLogsModal.cancelDelete(maxLogsQuantityOnPage);
-
+  after(() => {
+    Users.deleteViaApi(user.userId);
   });
+
+  it('C367923 A user can delete logs from the Import app "View all" page (folijet)',
+    { tags: [TestTypes.criticalPath, DevTeams.folijet] }, () => {
+      LogsViewAll.openViewAll();
+      LogsViewAll.viewAllIsOpened();
+      LogsViewAll.selectAllLogs();
+      LogsViewAll.checkIsLogsSelected(maxLogsQuantityOnPage);
+      LogsViewAll.deleteLog();
+      DeleteDataImportLogsModal.cancelDelete(maxLogsQuantityOnPage);
+      LogsViewAll.checkmarkAllLogsIsRemoved();
+
+      LogsViewAll.selectAllLogs();
+      LogsViewAll.checkIsLogsSelected(maxLogsQuantityOnPage);
+      LogsViewAll.deleteLog();
+      DeleteDataImportLogsModal.confirmDelete(maxLogsQuantityOnPage);
+      LogsViewAll.verifyMessageOfDeteted(maxLogsQuantityOnPage);
+      LogsViewAll.modalIsAbsent();
+    });
 });
