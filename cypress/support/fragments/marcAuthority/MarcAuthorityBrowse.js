8702e5e7b063970d2d865278f941abf540cab6dd
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2023-02-01 17:12:16 +0500

    add test C353997

--- cypress/support/fragments/marcAuthority/MarcAuthorityBrowse.js
+++ cypress/support/fragments/marcAuthority/MarcAuthorityBrowse.js
@@ -1,4 +1,4 @@
-import { Accordion, Button, MultiColumnListHeader, SearchField, Section, HTML, including, MultiColumnListRow, MultiColumnListCell } from '../../../../interactors';
+import { Accordion, Button, MultiColumnListHeader, SearchField, Select, Section, HTML, including, MultiColumnListRow, MultiColumnListCell } from '../../../../interactors';
 
 
 const searchOptions = { selectBrowseOption: { option: 'Select a browse option' },
@@ -16,6 +16,7 @@ const presentedColumns = ['Authorized/Reference', 'Heading/Reference', 'Type of 
 const rootPaneAuthoritiesFilters = Section({ id: 'pane-authorities-filters' });
 const defaultMainFilterValue = { htmlValue:'', visibleValue: searchOptions.selectBrowseOption };
 const searchButton = Button({ id: 'submit-authorities-search' });
+const enabledSearchButton = Button({ id: 'submit-authorities-search', disabled: false });
 const searchInput = SearchField({ id:'textarea-authorities-search' });
 const mainFilter = SearchField({ id:'textarea-authorities-search-qindex' });
 // TODO: initially first line has data-row-index = 52. Currently it's 0, clarify the reason in case if start index will changed once again
@@ -26,10 +27,12 @@ export default {
   waitEmptyTable: () => {
     cy.expect(rootSection.find(HTML(including('Choose a filter or enter a search query to show results'))).exists());
   },
+
   waitLoading:() => {
     cy.expect(rootSection.find(HTML(including('Loading...'))).absent());
     cy.expect(rootSection.find(MultiColumnListRow({ indexRow: getFirstLineIndexRow(0) })).exists());
   },
+
   checkPresentedColumns:() => presentedColumns.forEach(columnName => cy.expect(rootSection.find(MultiColumnListHeader(columnName)).exists())),
   // TODO: add checing of ""Type of heading" accordion button."
   checkFiltersInitialState:() => {
@@ -38,24 +41,47 @@ export default {
     cy.expect(rootPaneAuthoritiesFilters.find(Button({ id:'clickable-reset-all' })).has({ disabled:true }));
     cy.expect(rootPaneAuthoritiesFilters.find(Accordion('References')).exists());
   },
+
   searchBy:(searchOption, value) => {
     cy.do(searchInput.fillIn(value));
-    // interactor doesn't work properly with this selector
-    cy.get('#textarea-authorities-search-qindex')
-    .find('option')
-    .contains(searchOption)
-    .then($option => {
-        cy.get('#textarea-authorities-search-qindex').select($option.text());
+    cy.expect(searchInput.has({ value: value }));
+    cy.do(Select('Search field index').choose(searchOption));
+    cy.get('#textarea-authorities-search-qindex').then((elem) => {
+      expect(elem.text()).to.include(searchOption);
     });
+    cy.expect(enabledSearchButton.exists());
     cy.do(searchButton.click());
   },
+
+  searchByChangingParameter(searchOption, value) {
+    cy.expect(searchInput.has({ value: value }));
+    cy.do(Select('Search field index').choose(searchOption));
+    cy.get('#textarea-authorities-search-qindex').then((elem) => {
+      expect(elem.text()).to.include(searchOption);
+    });
+    cy.expect(enabledSearchButton.exists());
+    cy.do(searchButton.click());
+  },
+
+  searchByChangingValue(searchOption, value) {
+    cy.get('#textarea-authorities-search-qindex').then((elem) => {
+      expect(elem.text()).to.include(searchOption);
+    });
+    cy.do(searchInput.fillIn(value));
+    cy.expect(searchInput.has({ value: value }));
+    cy.expect(enabledSearchButton.exists());
+    cy.do(searchButton.click());
+  },
+
   checkSearchOptions:() => {
     // TODO: issue with openning of select by interactors and cypress. Try to find working option
     cy.get('select>option').should('have.text', Object.values(searchOptions).map(searchOption => searchOption.option).join(''));
   },
+
   checkSelectedSearchOption:(searchOption) => {
     cy.expect(mainFilter.has({ selectedFilter: searchOption.value }));
   },
+
   getNotExistingHeadingReferenceValue:(requestedHeadingReference) => `${requestedHeadingReference}\xa0would be here`,
   checkHeadingReferenceInRow(rowNumber, headingReferenceValue, isRef) {
     const specialCell = rootSection.find(MultiColumnListRow({ rowIndexInParent: `row-${rowNumber}` }))
@@ -67,6 +93,26 @@ export default {
       cy.expect(specialCell.find(Button()).absent());
     }
   },
+
+  checkResultWithNoValue(value) {
+    cy.expect(MultiColumnListCell(including(`${value} would be here`)).exists());
+  },
+
+  checkResultWithValueA(valueA, auth, valueAuth, ref, valueRef) {
+    cy.expect(MultiColumnListCell({row: 0, content: `${valueA} would be here`}).exists());
+    cy.expect(MultiColumnListCell({row: 1, content: auth}).exists());
+    cy.expect(MultiColumnListCell({row: 1, content: valueAuth}).exists());
+    cy.expect(MultiColumnListCell({row: 2, content: ref}).exists());
+    cy.expect(MultiColumnListCell({row: 2, content: valueRef}).exists());
+  },
+
+  checkResultWithValueB(auth, valueAuth, ref, valueRef) {
+    cy.expect(MultiColumnListCell({row: 0, content: auth}).exists());
+    cy.expect(MultiColumnListCell({row: 0, content: valueAuth}).exists());
+    cy.expect(MultiColumnListCell({row: 1, content: ref}).exists());
+    cy.expect(MultiColumnListCell({row: 1, content: valueRef}).exists());
+  },
+  
   checkHeadingReference: (headingReference) => {
     cy.expect([
       rootSection.find(MultiColumnListRow({ rowIndexInParent: `row-0` })).find(MultiColumnListCell({ content: `${headingReference}\xa0would be here` })),
