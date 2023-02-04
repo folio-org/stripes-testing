e9805b635a65ac7e6433d5e5e28d152f70f22695
Jasurbek Erkinov <99860864+re-jas@users.noreply.github.com>
2023-02-04 21:44:21 +0500

    fixed C9191 (#836)

--- cypress/e2e/users/loans/declare-lost.cy.js
+++ cypress/e2e/users/loans/declare-lost.cy.js
@@ -13,6 +13,10 @@ import permissions from '../../../support/dictionary/permissions';
 import InventoryHoldings from '../../../support/fragments/inventory/holdings/inventoryHoldings';
 import UserEdit from '../../../support/fragments/users/userEdit';
 import ServicePoints from '../../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import LostItemFeePolicy from '../../../support/fragments/circulation/lost-item-fee-policy';
+import CirculationRules from '../../../support/fragments/circulation/circulation-rules';
+import Users from '../../../support/fragments/users/users';
+import CheckInActions from '../../../support/fragments/check-in-actions/checkInActions';
 
 describe('ui-users-loans: Loans', () => {
   const newOwnerData = UsersOwners.getDefaultNewOwner(uuid());
@@ -22,98 +26,139 @@ describe('ui-users-loans: Loans', () => {
   const SECOND_LOAN_ROW_INDEX = 1;
   const FIRST_ACTION_ROW_INDEX = 0;
   const SECOND_ACTION_ROW_INDEX = 1;
-  let testUserId;
+  const testData = {};
   let servicePointId;
   let servicePoints = [];
+  let originalCirculationRules;
+  const lostItemFeePolicyBody = {
+    name: getTestEntityValue('lost'),
+    chargeAmountItem: {
+      chargeType: 'actualCost',
+      amount: 0.0,
+    },
+    lostItemProcessingFee: 0.0,
+    chargeAmountItemPatron: true,
+    chargeAmountItemSystem: true,
+    lostItemChargeFeeFine: {
+      duration: 2,
+      intervalId: 'Days',
+    },
+    returnedLostItemProcessingFee: true,
+    replacedLostItemProcessingFee: true,
+    replacementProcessingFee: 0.0,
+    replacementAllowed: true,
+    lostItemReturned: 'Charge',
+    id: uuid(),
+  };
 
   beforeEach(() => {
     let source;
-
     cy.getAdminToken();
-
     cy.then(() => {
-      ServicePoints.getViaApi()
-        .then((res) => {
-          servicePointId = res[0].id;
-          servicePoints = res;
-        });
-      cy.getLoanTypes({ limit: 1 });
+      ServicePoints.getViaApi().then((res) => {
+        servicePointId = res[0].id;
+        servicePoints = res;
+      });
+      cy.createLoanType({
+        name: getTestEntityValue('type'),
+      }).then((loanType) => {
+        testData.loanTypeId = loanType.id;
+      });
       cy.getMaterialTypes({ limit: 1 });
       cy.getInstanceTypes({ limit: 1 });
       cy.getLocations({ limit: 1 });
       cy.getHoldingTypes({ limit: 1 });
       source = InventoryHoldings.getHoldingSources({ limit: 1 });
     }).then(() => {
-      cy.createTempUser([
-        permissions.uiUsersViewLoans.gui,
-        permissions.uiUsersDeclareItemLost.gui,
-      ]).then(({
-        username,
-        password,
-        userId,
-        barcode: userBarcode,
-      }) => {
-        testUserId = userId;
-
-        UserEdit.addServicePointViaApi(servicePointId, userId).then(() => {
-          const servicePointOwner = servicePoints.map(({
-            id,
-            name,
-          }) => ({
-            value: id,
-            label: name,
-          }));
-
-          UsersOwners.createViaApi({
-            ...newOwnerData,
-            servicePointOwner,
-          });
-
-          cy.createInstance({
-            instance: {
-              instanceTypeId: Cypress.env('instanceTypes')[0].id,
-              title: getTestEntityValue(),
-            },
-            holdings: [{
-              holdingsTypeId: Cypress.env('holdingsTypes')[0].id,
-              permanentLocationId: Cypress.env('locations')[0].id,
-              sourceId: source.id,
-            }],
-            items: [
-              [{
-                ...newFirstItemData,
-                permanentLoanType: { id: Cypress.env('loanTypes')[0].id },
-                materialType: { id: Cypress.env('materialTypes')[0].id },
-              }, {
-                ...newSecondItemData,
-                permanentLoanType: { id: Cypress.env('loanTypes')[0].id },
-                materialType: { id: Cypress.env('materialTypes')[0].id },
-              }],
-            ],
-          }).then(() => {
-            [
-              newFirstItemData.barcode,
-              newSecondItemData.barcode,
-            ].forEach((itemBarcode) => {
-              Checkout.checkoutItemViaApi({
-                itemBarcode,
-                userBarcode,
-                servicePointId,
-              });
+      cy.createTempUser([permissions.uiUsersViewLoans.gui, permissions.uiUsersDeclareItemLost.gui]).then(
+        ({ username, password, userId, barcode: userBarcode }) => {
+          testData.userId = userId;
+          UserEdit.addServicePointViaApi(servicePointId, userId).then(() => {
+            const servicePointOwner = servicePoints.map(({ id, name }) => ({
+              value: id,
+              label: name,
+            }));
+            UsersOwners.createViaApi({
+              ...newOwnerData,
+              servicePointOwner,
+            }).then((ownerResponse) => {
+              testData.ownerId = ownerResponse.id;
             });
-
-            cy.login(username, password);
-
-            cy.visit(AppPaths.getOpenLoansPath(userId));
+            LostItemFeePolicy.createViaApi(lostItemFeePolicyBody);
+            CirculationRules.getViaApi().then((circulationRule) => {
+              originalCirculationRules = circulationRule.rulesAsText;
+              const ruleProps = CirculationRules.getRuleProps(circulationRule.rulesAsText);
+              ruleProps.i = lostItemFeePolicyBody.id;
+              CirculationRules.addRuleViaApi(originalCirculationRules, ruleProps, 't ', testData.loanTypeId);
+            });
+            cy.createInstance({
+              instance: {
+                instanceTypeId: Cypress.env('instanceTypes')[0].id,
+                title: getTestEntityValue(),
+              },
+              holdings: [
+                {
+                  holdingsTypeId: Cypress.env('holdingsTypes')[0].id,
+                  permanentLocationId: Cypress.env('locations')[0].id,
+                  sourceId: source.id,
+                },
+              ],
+              items: [
+                [
+                  {
+                    ...newFirstItemData,
+                    permanentLoanType: { id: testData.loanTypeId },
+                    materialType: { id: Cypress.env('materialTypes')[0].id },
+                  },
+                  {
+                    ...newSecondItemData,
+                    permanentLoanType: { id: testData.loanTypeId },
+                    materialType: { id: Cypress.env('materialTypes')[0].id },
+                  },
+                ],
+              ],
+            }).then(() => {
+              const date = new Date();
+              // We need this wait because "testLoanDetails" has time-based checks
+              // so "сheck out" and "declare lost" are expected to be done in the same minutes.
+              cy.wait((60 - date.getSeconds()) * 1000);
+              [newFirstItemData.barcode, newSecondItemData.barcode].forEach((itemBarcode) => {
+                Checkout.checkoutItemViaApi({
+                  itemBarcode,
+                  userBarcode,
+                  servicePointId,
+                });
+              });
+              cy.login(username, password);
+              cy.visit(AppPaths.getOpenLoansPath(userId));
+            });
           });
-        });
-      });
+        }
+      );
     });
   });
 
-  it('C9191 Declare lost (prokopovych)', { tags: [TestType.smoke, TestType.broken, DevTeams.prokopovych] }, () => {
-    UsersCard.getApi(testUserId).then((user) => {
-      Loans.getApi(testUserId).then(([firstLoan, secondLoan]) => {
+  after('Deleting created entities', () => {
+    [newFirstItemData, newSecondItemData].forEach((item) => {
+      CheckInActions.checkinItemViaApi({
+        itemBarcode: item.barcode,
+        servicePointId,
+        checkInDate: new Date().toISOString(),
+      });
+      cy.deleteItemViaApi(item.id);
+    });
+    LostItemFeePolicy.deleteViaApi(lostItemFeePolicyBody.id);
+    CirculationRules.deleteRuleViaApi(originalCirculationRules);
+    cy.deleteLoanType(testData.loanTypeId);
+    Users.deleteViaApi(testData.userId);
+    UsersOwners.deleteViaApi(testData.ownerId);
+  });
+
+  it(
+    'C9191 Loans: Declare lost (prokopovych)',
+    { tags: [TestType.smoke, TestType.broken, DevTeams.prokopovych] },
+    () => {
+      UsersCard.getApi(testData.userId).then((user) => {
         Loans.checkStatusCheckedOut(SECOND_LOAN_ROW_INDEX);
         Loans.startDeclareLost(SECOND_LOAN_ROW_INDEX);
         Loans.cancelDeclareLost();
@@ -124,7 +169,7 @@ describe('ui-users-loans: Loans', () => {
         Loans.checkStatusDeclaredLost(SECOND_LOAN_ROW_INDEX);
 
         const testLoanDetails = (shouldDeclareLost, loanId, loanHistoryFirstAction) => {
-          cy.visit(AppPaths.getLoanDetailsPath(testUserId, loanId));
+          cy.visit(AppPaths.getLoanDetailsPath(testData.userId, loanId));
 
           if (shouldDeclareLost) {
             LoanDetails.checkDeclareLostButtonActive();
@@ -151,14 +196,16 @@ describe('ui-users-loans: Loans', () => {
           LoanDetails.checkComments(FIRST_ACTION_ROW_INDEX, DECLARE_LOST_ADDITIONAL_INFORMATION);
         };
 
-        cy.getLoanHistory(secondLoan.id).then(([loanHistoryFirstAction]) => {
-          testLoanDetails(false, secondLoan.id, loanHistoryFirstAction);
-        });
+        Loans.getApi(testData.userId).then(([firstLoan, secondLoan]) => {
+          cy.getLoanHistory(secondLoan.id).then(([loanHistoryFirstAction]) => {
+            testLoanDetails(false, secondLoan.id, loanHistoryFirstAction);
+          });
 
-        cy.getLoanHistory(firstLoan.id).then(([loanHistoryFirstAction]) => {
-          testLoanDetails(true, firstLoan.id, loanHistoryFirstAction);
+          cy.getLoanHistory(firstLoan.id).then(([loanHistoryFirstAction]) => {
+            testLoanDetails(true, firstLoan.id, loanHistoryFirstAction);
+          });
         });
       });
-    });
-  });
+    }
+  );
 });
