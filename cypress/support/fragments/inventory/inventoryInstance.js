6f6830c6505159e7b6d6a5bc642fe5d4199f62f1
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-02-05 23:58:39 +0300

    renamed function

--- cypress/support/fragments/inventory/inventoryInstance.js
+++ cypress/support/fragments/inventory/inventoryInstance.js
@@ -336,7 +336,7 @@ export default {
     cy.do(Link(including(itemBarcode)).click());
     ItemRecordView.waitLoading();
   },
-  openEditItemPage() {
+  edit() {
     cy.do([
       Button('Actions').click(),
       Button('Edit').click(),
