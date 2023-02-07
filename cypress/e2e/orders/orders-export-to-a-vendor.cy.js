04f8cfb356411b9e6c5c5f9a07aecbd31d924f10
IhorBohdan <ihor_bohdan@epam.com>
2023-02-07 22:30:19 +0200

    C350396: Verify that Order is not exported to a definite Vendor if Acquisition method selected in the Order line DOES NOT match Organization Integration configs

--- cypress/e2e/orders/orders-export-to-a-vendor.cy.js
+++ cypress/e2e/orders/orders-export-to-a-vendor.cy.js
@@ -4,28 +4,18 @@ import TopMenu from '../../support/fragments/topMenu';
 import Orders from '../../support/fragments/orders/orders';
 import TestTypes from '../../support/dictionary/testTypes';
 import Users from '../../support/fragments/users/users';
-import FileManager from '../../support/utils/fileManager';
 import NewOrder from '../../support/fragments/orders/newOrder';
-import BasicOrderLine from '../../support/fragments/orders/basicOrderLine';
-import OrdersHelper from '../../support/fragments/orders/ordersHelper';
-import NewInvoice from '../../support/fragments/invoices/newInvoice';
 import Organizations from '../../support/fragments/organizations/organizations';
 import NewOrganization from '../../support/fragments/organizations/newOrganization';
-import generateItemBarcode from '../../support/utils/generateItemBarcode';
+import getRandomPostfix from '../../support/utils/stringTools';
+import InteractorsTools from '../../support/utils/interactorsTools';
+import OrderLines from '../../support/fragments/orders/orderLines';
 
 describe('orders: export', () => {
   let user;
-  const order = {
-    ...NewOrder.defaultOneTimeOrder,
-    poNumberPrefix: 'pref',
-    poNumberSuffix: 'suf',
-    poNumber: `pref${generateItemBarcode()}suf`,
-    reEncumber: true,
-    manualPo: true,
-    approved: true,
-  };
+  const order = { ...NewOrder.defaultOneTimeOrder };
   const organization = {
-    ...newOrganization.defaultUiOrganizations,
+    ...NewOrganization.defaultUiOrganizations,
     accounts: [
       {
         accountNo: getRandomPostfix(),
@@ -45,24 +35,31 @@ describe('orders: export', () => {
   const integartionDescription = 'Test Integation descripton1';
   const vendorEDICodeFor1Integration = getRandomPostfix();
   const libraryEDICodeFor1Integration = getRandomPostfix();
-  let orderNumber;
+
   before(() => {
     cy.getAdminToken();
     Organizations.createOrganizationViaApi(organization)
       .then(response => {
         organization.id = response;
-        order.vendor = response;
+        order.vendor = organization.name;
+        order.orderType = 'One-time';
       });
     cy.loginAsAdmin({ path:TopMenu.organizationsPath, waiter: Organizations.waitLoading });
     Organizations.searchByParameters('Name', organization.name);
     Organizations.checkSearchResults(organization);
     Organizations.selectOrganization(organization.name);
-
     Organizations.addIntegration();
     Organizations.fillIntegrationInformation(integrationName, integartionDescription, vendorEDICodeFor1Integration, libraryEDICodeFor1Integration, organization.accounts[0].accountNo, 'Purchase');
     InteractorsTools.checkCalloutMessage('Integration was saved');
+    
     cy.createTempUser([
-      permissions.uiOrdersView.gui, permissions.uiExportOrders.gui
+      permissions.uiOrdersView.gui,
+      permissions.uiOrdersCreate.gui, 
+      permissions.uiOrdersEdit.gui,
+      permissions.uiOrdersApprovePurchaseOrders.gui,
+      permissions.viewEditCreateOrganization.gui, 
+      permissions.viewOrganization.gui,
+      permissions.uiExportOrders.gui
     ])
       .then(userProperties => {
         user = userProperties;
@@ -74,11 +71,14 @@ describe('orders: export', () => {
     Orders.deleteOrderApi(order.id);
     Organizations.deleteOrganizationViaApi(organization.id);
     Users.deleteViaApi(user.userId);
-    FileManager.deleteFolder(Cypress.config('downloadsFolder'));
   });
 
   it('C350396: Verify that Order is not exported to a definite Vendor if Acquisition method selected in the Order line DOES NOT match Organization Integration configs', { tags: [TestTypes.smoke, devTeams.thunderjet] }, () => {
-    Orders.selectOpenStatusFilter();
-    Orders.exportResoultsCSV();
+    Orders.createOrder(order,true).then(orderId => {
+      order.id = orderId;
+      Orders.createPOLineViaActions();
+      OrderLines.fillInPOLineInfoForExport(`${organization.accounts[0].name} (${organization.accounts[0].accountNo})`);
+      OrderLines.backToEditingOrder();
+    });
   });
 }); 
