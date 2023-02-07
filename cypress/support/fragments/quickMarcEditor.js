997b2090bbb785a32a7b27292fec2fd92e6a938e
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2023-02-07 18:16:03 +0500

    some fixes

--- cypress/support/fragments/quickMarcEditor.js
+++ cypress/support/fragments/quickMarcEditor.js
@@ -277,7 +277,7 @@ export default {
     ]);
   },
 
-  closeWithoutSaving() {
+  closeWithoutSavingAfterChange() {
     cy.do(cancelButton.click());
     cy.expect(closeWithoutSavingBtn.exists());
     cy.do(closeWithoutSavingBtn.click());
