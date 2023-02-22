7dc8a1bb7dffe49d4a4d96dca7ffc5c4f538d143
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2023-02-22 19:22:53 +0500

    some updates in C359015

--- cypress/e2e/marc-authority/plug-in-marc-authority/plug-in-marc-authority-search.cy.js
+++ cypress/e2e/marc-authority/plug-in-marc-authority/plug-in-marc-authority-search.cy.js
@@ -5,9 +5,11 @@ import TopMenu from '../../../support/fragments/topMenu';
 import Users from '../../../support/fragments/users/users';
 import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
 import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import DataImport from '../../../support/fragments/data_import/dataImport';
 
 describe('plug-in MARC authority | Search', () => {
     const testData = {};
+    let instanceID;
 
   before('Creating user', () => {
     cy.createTempUser([
@@ -18,6 +20,9 @@ describe('plug-in MARC authority | Search', () => {
       Permissions.uiQuickMarcQuickMarcAuthorityLinkUnlink.gui,
     ]).then(createdUserProperties => {
       testData.userProperties = createdUserProperties;
+
+      cy.loginAsAdmin();
+      DataImport.uploadMarcBib();
     });
   });
 
@@ -27,11 +32,18 @@ describe('plug-in MARC authority | Search', () => {
 
   after('Deleting created user', () => {
     Users.deleteViaApi(testData.userProperties.userId);
+    InventoryInstance.deleteInstanceViaApi(instanceID);
+
+    cy.loginAsAdmin({ path: TopMenu.dataImportPath, waiter: DataImport.waitLoading });
+    DataImport.selectLog();
+    DataImport.openDeleteImportLogsModal();
+    DataImport.confirmDeleteImportLogs();
   });
 
   it('C359015 MARC Authority plug-in | Search for MARC authority records when the user clicks on the "Link" icon (spitfire)', { tags: [TestTypes.smoke, DevTeams.spitfire] }, () => {
-    InventoryInstances.searchBySource('MARC');
+    InventoryInstance.searchByTitle('Anglo-Saxon manuscripts');
     InventoryInstances.selectInstance();
+    InventoryInstance.getId().then(id => { instanceID = id; });
     InventoryInstance.editMarcBibliographicRecord();
     InventoryInstance.verifyAndClickLinkIcon();
     InventoryInstance.verifySelectMarcAuthorityModal();
