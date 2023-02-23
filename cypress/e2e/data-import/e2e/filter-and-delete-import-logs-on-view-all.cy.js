4f76cb363cfaad8623873ec6e52c4b4436f0ebdf
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-02-23 10:38:49 +0100

    fixed

--- cypress/e2e/data-import/e2e/filter-and-delete-import-logs-on-view-all.cy.js
+++ cypress/e2e/data-import/e2e/filter-and-delete-import-logs-on-view-all.cy.js
@@ -79,7 +79,7 @@ describe('ui-data-import: A user can filter and delete import logs from the "Vie
 
   it('C358136 A user can filter and delete import logs from the "View all" page (folijet)',
     { tags: [TestTypes.smoke, DevTeams.folijet] }, () => {
-      LogsViewAll.openViewAll();
+      Logs.openViewAllLogs();
       LogsViewAll.viewAllIsOpened();
       LogsViewAll.filterJobsByJobProfile('Default - Create SRS MARC Authority');
       LogsViewAll.filterJobsByDate({ from: formattedStart, end: formattedStart });
