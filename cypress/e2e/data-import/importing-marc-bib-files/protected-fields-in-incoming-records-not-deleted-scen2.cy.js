d63aa4f8cfcac695d3b1eadf63d5afe20999ffec
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-02-24 23:23:40 +0100

    changed functions

--- cypress/e2e/data-import/importing-marc-bib-files/protected-fields-in-incoming-records-not-deleted-scen2.cy.js
+++ cypress/e2e/data-import/importing-marc-bib-files/protected-fields-in-incoming-records-not-deleted-scen2.cy.js
@@ -16,17 +16,34 @@ describe('ui-data-import: Check that protected fields in incoming records are no
   let user = null;
   const authentication = '100473910/PAOLF';
   const oclcForImport = '19257462';
-  const fields = {
-    field580: '‡a Test ‡5 NcD',
-    field5801: '‡a Merged with: Journal of the Chemical Society. Perkin transactions I; Journal of the Chemical Society. Perkin transactions II; and Journal of the Chemical Society. Dalton transactions, to form: Perkin 1; Perkin 2; and Dalton (Cambridge, England).',
-    field780: '‡t Acta chemica Scandinavica. Series A, Physical and inorganic chemistry ‡x 0302-4377 ‡w (DLC)sn 79006037 ‡w (OCoLC)981847',
-    field7801: '‡t Acta chemica Scandinavica. Series B, Organic chemistry and biochemistry ‡x 0302-4369 ‡w (DLC)sn 78006299 ‡w (OCoLC)981837',
-    field7851: '‡t Journal of the Chemical Society. Perkin transactions I ‡x 0300-922X ‡w (DLC)  2006219014 ‡w (OCoLC)1033975',
-    field7852: '‡t Journal of the Chemical Society. Perkin transactions II ‡x 0300-9580 ‡w (DLC)   72623335 ‡w (OCoLC)1064266',
-    field7853: '‡t Journal of the Chemical Society. Dalton transactions ‡x 0300-9246 ‡w (DLC)   72624566 ‡w (OCoLC)1034240',
-    field7854: '‡t Perkin 1 ‡x 1470-4358 ‡w (DLC)   00252538 ‡w (OCoLC)44000773',
-    field7855: '‡t Perkin 2 ‡x 1470-1820 ‡w (DLC)   00214936 ‡w (OCoLC)44000837',
-    field7856: '‡t Dalton (Cambridge, England) ‡x 1470-479X ‡w (DLC)   00252543 ‡w (OCoLC)44000666'
+
+  const initialFields = {
+    first580field: '‡a Merged with: Journal of the Chemical Society. Perkin transactions I; Journal of the Chemical Society. Perkin transactions II; and Journal of the Chemical Society. Dalton transactions, to form: Perkin 1; Perkin 2; and Dalton (Cambridge, England).',
+    first780field: '‡t Acta chemica Scandinavica. Series A, Physical and inorganic chemistry ‡x 0302-4377 ‡w (DLC)sn 79006037 ‡w (OCoLC)981847',
+    second780field: '‡t Acta chemica Scandinavica. Series B, Organic chemistry and biochemistry ‡x 0302-4369 ‡w (DLC)sn 78006299 ‡w (OCoLC)981837',
+    first785field: '‡t Journal of the Chemical Society. Perkin transactions I ‡x 0300-922X ‡w (DLC)  2006219014 ‡w (OCoLC)1033975',
+    second785field: '‡t Journal of the Chemical Society. Perkin transactions II ‡x 0300-9580 ‡w (DLC)   72623335 ‡w (OCoLC)1064266',
+    third785field: '‡t Journal of the Chemical Society. Dalton transactions ‡x 0300-9246 ‡w (DLC)   72624566 ‡w (OCoLC)1034240',
+    fourth785field: '‡t Perkin 1 ‡x 1470-4358 ‡w (DLC)   00252538 ‡w (OCoLC)44000773',
+    fifth785field: '‡t Perkin 2 ‡x 1470-1820 ‡w (DLC)   00214936 ‡w (OCoLC)44000837',
+    sixth785field: '‡t Dalton (Cambridge, England) ‡x 1470-479X ‡w (DLC)   00252543 ‡w (OCoLC)44000666'
+  };
+  const fieldsForChanging = {
+    first780Field: '$t Acta chemica Scandinavica. Series B, Organic chemistry and biochemistry $x 0302-4369 $w (DLC)sn 78006299 $w (OCoLC)981837',
+    first785Field: '$t Journal of the Chemical Society. Perkin transactions II $x 0300-9580 $w (DLC)   72623335 $w (OCoLC)1064266',
+    second785Field: '$t Journal of the Chemical Society. Dalton transactions $x 0300-9246 $w (DLC)   72624566 $w (OCoLC)1034240',
+    third785Field: '$t Perkin 1 $x 1470-4358 $w (DLC)   00252538 $w (OCoLC)44000773',
+    fourth785Field: '$t Perkin 2 $x 1470-1820 $w (DLC)   00214936 $w (OCoLC)44000837',
+    fifth785Field: '$t Dalton (Cambridge, England) $x 1470-479X $w (DLC)   00252543 $w (OCoLC)44000666'
+  };
+  const changedFields = {
+    first580field: 'Test ‡5 NcD',
+    first780field: '‡t Acta chemica Scandinavica. Series B, Organic chemistry and biochemistry ‡x 0302-4369 ‡w (DLC)sn 78006299 ‡w (OCoLC)981837 ‡5 NcD',
+    first785field: '‡t Journal of the Chemical Society. Perkin transactions II ‡x 0300-9580 ‡w (DLC)   72623335 ‡w (OCoLC)1064266 ‡5 NcD',
+    third785field: '‡t Journal of the Chemical Society. Dalton transactions ‡x 0300-9246 ‡w (DLC)   72624566 ‡w (OCoLC)1034240 ‡5 NcD',
+    fourth785field: '‡t Perkin 1 ‡x 1470-4358 ‡w (DLC)   00252538 ‡w (OCoLC)44000773 ‡5 NcD',
+    fifth785field: '‡t Perkin 2 ‡x 1470-1820 ‡w (DLC)   00214936 ‡w (OCoLC)44000837 ‡5 NcD',
+    sixth785field: '‡t Dalton (Cambridge, England) ‡x 1470-479X ‡w (DLC)   00252543 ‡w (OCoLC)44000666 ‡5 NcD'
   };
 
   before(() => {
@@ -77,47 +94,14 @@ describe('ui-data-import: Check that protected fields in incoming records are no
       InventoryInstances.importWithOclc(oclcForImport);
       // check fields is presented in .mrc file
       InventoryInstance.viewSource();
-      InventoryViewSource.contains(fields.field5801);
-      InventoryViewSource.contains(fields.field780);
-      InventoryViewSource.contains(fields.field7801);
-      InventoryViewSource.contains(fields.field7851);
-      InventoryViewSource.contains(fields.field7852);
-      InventoryViewSource.contains(fields.field7853);
-      InventoryViewSource.contains(fields.field7854);
-      InventoryViewSource.contains(fields.field7855);
-      InventoryViewSource.contains(fields.field7856);
+      Object.values(initialFields).forEach((field) => InventoryViewSource.contains(field));
       cy.intercept('GET', '/orders/titles?*').as('getOrdersTitles');
       InventoryViewSource.close();
       cy.wait('@getOrdersTitles');
 
       // edit the MARC bibliographic record
       InventoryInstance.editMarcBibliographicRecord();
-      InventoryEditMarcRecord.editField(
-        '$t Acta chemica Scandinavica. Series B, Organic chemistry and biochemistry $x 0302-4369 $w (DLC)sn 78006299 $w (OCoLC)981837',
-        '$t Acta chemica Scandinavica. Series B, Organic chemistry and biochemistry $x 0302-4369 $w (DLC)sn 78006299 $w (OCoLC)981837 $5 NcD'
-      );
-      InventoryEditMarcRecord.editField(
-        '$t Journal of the Chemical Society. Perkin transactions II $x 0300-9580 $w (DLC)   72623335 $w (OCoLC)1064266',
-        '$t Journal of the Chemical Society. Perkin transactions II $x 0300-9580 $w (DLC)   72623335 $w (OCoLC)1064266 $5 NcD'
-      );
-      InventoryEditMarcRecord.editField(
-        '$t Journal of the Chemical Society. Dalton transactions $x 0300-9246 $w (DLC)   72624566 $w (OCoLC)1034240',
-        '$t Journal of the Chemical Society. Dalton transactions $x 0300-9246 $w (DLC)   72624566 $w (OCoLC)1034240 $5 NcD'
-      );
-      InventoryEditMarcRecord.editField(
-        '$t Perkin 1 $x 1470-4358 $w (DLC)   00252538 $w (OCoLC)44000773',
-        '$t Perkin 1 $x 1470-4358 $w (DLC)   00252538 $w (OCoLC)44000773 $5 NcD'
-      );
-      InventoryEditMarcRecord.editField(
-        '$t Perkin 2 $x 1470-1820 $w (DLC)   00214936 $w (OCoLC)44000837',
-        '$t Perkin 2 $x 1470-1820 $w (DLC)   00214936 $w (OCoLC)44000837 $5 NcD'
-      );
-      InventoryEditMarcRecord.editField(
-        '$t Dalton (Cambridge, England) $x 1470-479X $w (DLC)   00252543 $w (OCoLC)44000666',
-        '$t Dalton (Cambridge, England) $x 1470-479X $w (DLC)   00252543 $w (OCoLC)44000666 $5 NcD'
-      );
-      InventoryEditMarcRecord.saveAndClose();
-      InventoryInstance.editMarcBibliographicRecord();
+      Object.values(fieldsForChanging).forEach((field) => InventoryEditMarcRecord.editField(field, `${field} $5 NcD`));
       InventoryEditMarcRecord.addField('580', 'Test $5 NcD');
       InventoryEditMarcRecord.saveAndClose();
 
@@ -126,27 +110,15 @@ describe('ui-data-import: Check that protected fields in incoming records are no
       InventoryInstance.singleRecordImportModalIsPresented();
       InventoryInstance.importWithOclc(oclcForImport);
       InventoryInstance.checkCalloutMessage(`Updated record ${oclcForImport}`);
+
       // need to wait because after the import the data in the instance is displayed for a long time
       // https://issues.folio.org/browse/MODCPCT-73
       cy.wait(10000);
       InventoryInstance.viewSource();
       // check fields without NcD is presented in .mrc file
-      InventoryViewSource.contains(fields.field5801);
-      InventoryViewSource.contains(fields.field780);
-      InventoryViewSource.contains(fields.field7801);
-      InventoryViewSource.contains(fields.field7851);
-      InventoryViewSource.contains(fields.field7852);
-      InventoryViewSource.contains(fields.field7853);
-      InventoryViewSource.contains(fields.field7854);
-      InventoryViewSource.contains(fields.field7855);
-      InventoryViewSource.contains(fields.field7856);
+      Object.values(initialFields).forEach((field) => InventoryViewSource.contains(field));
+
       // check fields with NcD is presented in .mrc file
-      InventoryViewSource.contains(fields.field580);
-      InventoryViewSource.contains('‡t Acta chemica Scandinavica. Series B, Organic chemistry and biochemistry ‡x 0302-4369 ‡w (DLC)sn 78006299 ‡w (OCoLC)981837 ‡5 NcD');
-      InventoryViewSource.contains('‡t Journal of the Chemical Society. Perkin transactions II ‡x 0300-9580 ‡w (DLC)   72623335 ‡w (OCoLC)1064266 ‡5 NcD');
-      InventoryViewSource.contains('‡t Journal of the Chemical Society. Dalton transactions ‡x 0300-9246 ‡w (DLC)   72624566 ‡w (OCoLC)1034240 ‡5 NcD');
-      InventoryViewSource.contains('‡t Perkin 1 ‡x 1470-4358 ‡w (DLC)   00252538 ‡w (OCoLC)44000773 ‡5 NcD');
-      InventoryViewSource.contains('‡t Perkin 2 ‡x 1470-1820 ‡w (DLC)   00214936 ‡w (OCoLC)44000837 ‡5 NcD');
-      InventoryViewSource.contains('‡t Dalton (Cambridge, England) ‡x 1470-479X ‡w (DLC)   00252543 ‡w (OCoLC)44000666 ‡5 NcD');
+      Object.values(changedFields).forEach((field) => InventoryViewSource.contains(field));
     });
 });
