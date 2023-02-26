0c86e53cee5bfc34979785484e6f46a824edef82
nayimovag <Guliyashnar_Nayimova@epam.com>
2023-02-27 01:43:13 +0500

    FAT-2562

--- cypress/support/fragments/data-export/exportMappingProfile/exportFieldMappingProfiles.js
+++ cypress/support/fragments/data-export/exportMappingProfile/exportFieldMappingProfiles.js
@@ -1,11 +1,14 @@
-import { Button, Pane, NavListItem } from '../../../../../interactors';
+import { Button, Pane, NavListItem, TextField, MultiColumnListHeader, MultiColumnListRow } from '../../../../../interactors';
+import { including } from 'bigtest';
 import exportNewFieldMappingProfile from './exportNewFieldMappingProfile';
 
 const saveAndCloseButton = Button('Save & close');
 const fieldMappingProfilesPane = Pane('Field mapping profiles');
+const newButton = Button('New');
+const searchButton = Button('Search');
 
 const openNewMappingProfileForm = () => {
-  cy.do(Button('New').click());
+  cy.do(newButton.click());
 };
 
 const saveMappingProfile = () => {
@@ -35,14 +38,52 @@ export default {
   },
 
   getFieldMappingProfile: (searchParams) => {
-		return cy
-			.okapiRequest({
-				path: 'data-export/mapping-profiles',
-				searchParams,
-				isDefaultSearchParamsRequired: false
-			})
-			.then((response) => {
-				return response.body.mappingProfiles[0];
-			});
-	},
+    return cy
+      .okapiRequest({
+        path: 'data-export/mapping-profiles',
+        searchParams,
+        isDefaultSearchParamsRequired: false
+      })
+      .then((response) => {
+        return response.body.mappingProfiles[0];
+      });
+  },
+
+  verifyFieldMappingProfilesPane() {
+    cy.expect([
+      fieldMappingProfilesPane.exists(),
+      newButton.has({ disabled: false }),
+      TextField('Search Field mapping profiles').exists(),
+      searchButton.has({ disabled: true }),
+    ])
+    cy.do(cy.get('[class^=mclEndOfListContainer--]').should('have.text', 'End of list'))
+    this.verifyFieldMappingProfilesCount();
+    this.verifyColumnTitles();
+  },
+
+  verifyFieldMappingProfilesCount() {
+    cy.do(cy.get('#search-results-list').then(elem => {
+      cy.expect(cy.get('#paneHeaderpane-results-subtitle').should('have.text', `${elem.attr('data-total-count')} field mapping profiles`));
+    }))
+  },
+
+  verifyColumnTitles() {
+    [
+      'Name',
+      'FOLIO record type',
+      'Format',
+      'Updated',
+      'Updated by'
+    ].forEach(title => {
+      cy.expect(fieldMappingProfilesPane.find(MultiColumnListHeader(title)).exists());
+    })
+  },
+
+  verifyDefaultProfiles() {
+    cy.expect([
+      MultiColumnListRow(including('Default authority mapping profileAuthority')).exists(),
+      MultiColumnListRow(including('Default holdings mapping profileHoldings')).exists(),
+      MultiColumnListRow(including('Default instance mapping profileInstance')).exists(),
+    ])
+  }
 };
