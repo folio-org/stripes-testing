8c5074ec0a1ae11f71ec79eea5a4dffb670f6956
Jasurbek Erkinov <re.nine.jas@gmail.com>
2023-03-09 23:53:53 +0500

    fixed circrule delete

--- cypress/e2e/title-level-request/user-can-create-page-Item-level-request.cy.js
+++ cypress/e2e/title-level-request/user-can-create-page-Item-level-request.cy.js
@@ -19,6 +19,7 @@ import TitleLevelRequests from '../../support/fragments/settings/circulation/tit
 import Requests from '../../support/fragments/requests/requests';
 
 describe('Create Item or Title level request', () => {
+  let addedCirculationRule;
   let originalCirculationRules;
   let userData = {};
   const patronGroup = {
@@ -91,6 +92,7 @@ describe('Create Item or Title level request', () => {
       originalCirculationRules = circulationRule.rulesAsText;
       const ruleProps = CirculationRules.getRuleProps(circulationRule.rulesAsText);
       ruleProps.r = requestPolicyBody.id;
+      addedCirculationRule = 't ' + testData.loanTypeId + ': i ' + ruleProps.i + ' l ' + ruleProps.l + ' r ' + ruleProps.r + ' o ' + ruleProps.o + ' n ' + ruleProps.n;
       CirculationRules.addRuleViaApi(originalCirculationRules, ruleProps, 't ', testData.loanTypeId);
     });
 
@@ -127,7 +129,7 @@ describe('Create Item or Title level request', () => {
     });
     InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(testData.itemBarcode);
     RequestPolicy.deleteViaApi(requestPolicyBody.id);
-    CirculationRules.deleteRuleViaApi(originalCirculationRules);
+    CirculationRules.deleteRuleViaApi(addedCirculationRule);
     cy.deleteLoanType(testData.loanTypeId);
     UserEdit.changeServicePointPreferenceViaApi(userData.userId, [testData.userServicePoint.id]);
     ServicePoints.deleteViaApi(testData.userServicePoint.id);
