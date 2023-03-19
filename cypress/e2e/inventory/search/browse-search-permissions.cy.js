109512d3a839ec43e86cab48cd7c93bf34032f82
IhorBohdan <ihor_bohdan@epam.com>
2023-03-19 23:14:31 +0200

    Implementing of FAT-4984

--- cypress/e2e/inventory/search/browse-search-permissions.cy.js
+++ cypress/e2e/inventory/search/browse-search-permissions.cy.js
@@ -1,29 +1,34 @@
 import TopMenu from '../../../support/fragments/topMenu';
 import TestTypes from '../../../support/dictionary/testTypes';
 import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
-import InventoryActions from '../../../support/fragments/inventory/inventoryActions';
 import devTeams from '../../../support/dictionary/devTeams';
-import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
 import Users from '../../../support/fragments/users/users';
 import permissions from '../../../support/dictionary/permissions';
 
 describe('permissions: inventory', () => {
+    let user;
+
   beforeEach(() => {
     cy.createTempUser([
-        permissions.inventoryAll.gui,
-        permissions.uiCallNumberBrowse.gui
+        permissions.uiInventoryViewInstances.gui,
       ]).then(userProperties => {
-        testData.user = userProperties;
-        cy.login(userProperties.username, userProperties.password, { path:TopMenu.inventoryPath, waiter: InventoryInstances.waitLoading });
+        user = userProperties;
+        cy.login(userProperties.username, userProperties.password);
       });
   });
 
+  afterEach('Deleting data', () => {
+    Users.deleteViaApi(user.userId);
+  });
+
   it('C375072 User with "Inventory: View instances, holdings, and items" permission can see browse call numbers and subjects without assigning specific browse permissions (Orchid+) (thunderjet)', { tags: [TestTypes.smoke, devTeams.thunderjet] }, () => {
+    cy.visit(TopMenu.inventoryPath);
     InventorySearchAndFilter.switchToBrowseTab();
-    InventorySearchAndFilter.verifyKeywordsAsDefault();
     InventorySearchAndFilter.selectBrowseCallNumbers();
-    InventorySearchAndFilter.verifyCallNumberBrowseEmptyPane();
-    InventoryActions.actionsIsAbsent();
-    InventorySearchAndFilter.showsOnlyEffectiveLocation();
+    InventorySearchAndFilter.browseSearch('K1');
+    InventorySearchAndFilter.verifyCallNumberBrowsePane();
+    InventorySearchAndFilter.selectBrowseSubjects();
+    InventorySearchAndFilter.browseSearch('art');
+    InventorySearchAndFilter.verifyCallNumberBrowsePane();
   });
 });
