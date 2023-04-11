cef7a8dd53092b7f720a21290829d688a5acd28e
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-04-11 11:08:12 +0300

    added test

---
+++ cypress/e2e/data-import/log-details/import-summury-table-for-updating-actions.cy.js
@@ -0,0 +1,27 @@
+import TestTypes from '../../../support/dictionary/testTypes';
+import DevTeams from '../../../support/dictionary/devTeams';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
+import Helper from '../../../support/fragments/finance/financeHelper';
+import NewFieldMappingProfile from '../../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
+import NewActionProfile from '../../../support/fragments/data_import/action_profiles/newActionProfile';
+import ActionProfiles from '../../../support/fragments/data_import/action_profiles/actionProfiles';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import NewJobProfile from '../../../support/fragments/data_import/job_profiles/newJobProfile';
+import TopMenu from '../../../support/fragments/topMenu';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import Logs from '../../../support/fragments/data_import/logs/logs';
+import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import Users from '../../../support/fragments/users/users';
+
+describe('ui-data-import', () => {
+  before('login', () => {
+
+  });
+
+  it('C356802 Check import summary table with "Updated" actions for instance, holding and item (folijet)',
+    { tags: [TestTypes.criticalPath, DevTeams.folijet] }, () => {
+
+    });
+});
