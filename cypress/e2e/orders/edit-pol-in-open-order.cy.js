7512f1e1f639af0049e933cbbb805ef63a9f1ad2
IhorBohdan <ihor_bohdan@epam.com>
2023-04-24 14:27:10 +0300

    Implementing of FAT-1363

--- cypress/e2e/orders/edit-pol-in-open-order.cy.js
+++ cypress/e2e/orders/edit-pol-in-open-order.cy.js
@@ -1,64 +1,111 @@
 import permissions from '../../support/dictionary/permissions';
 import testType from '../../support/dictionary/testTypes';
 import devTeams from '../../support/dictionary/devTeams';
+import FiscalYears from '../../support/fragments/finance/fiscalYears/fiscalYears';
 import TopMenu from '../../support/fragments/topMenu';
+import Ledgers from '../../support/fragments/finance/ledgers/ledgers';
 import Users from '../../support/fragments/users/users';
-import SettingsMenu from '../../support/fragments/settingsMenu';
-import SettingsOrders from '../../support/fragments/settings/orders/settingsOrders';
+import Funds from '../../support/fragments/finance/funds/funds';
+import FinanceHelp from '../../support/fragments/finance/financeHelper';
 import Orders from '../../support/fragments/orders/orders';
-import NewOrder from '../../support/fragments/orders/newOrder';
+import OrderLines from '../../support/fragments/orders/orderLines';
 import Organizations from '../../support/fragments/organizations/organizations';
 import NewOrganization from '../../support/fragments/organizations/newOrganization';
-import InteractorsTools from '../../support/utils/interactorsTools';
+import NewInvoice from '../../support/fragments/invoices/newInvoice';
+import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import NewLocation from '../../support/fragments/settings/tenant/locations/newLocation';
+import NewOrder from '../../support/fragments/orders/newOrder';
 
-describe('ui-orders: Order Lines', () => {
-  const order = { ...NewOrder.defaultOneTimeOrder };
+describe('ui-orders: Orders', () => {
+  const defaultFiscalYear = { ...FiscalYears.defaultUiFiscalYear };
+  const defaultLedger = { ...Ledgers.defaultUiLedger };
+  const defaultFund = { ...Funds.defaultUiFund };
+  const defaultOrder = { ...NewOrder.defaultOneTimeOrder ,
+    approved: true,
+    reEncumber: true,
+    orderType: 'One-time',
+  };
   const organization = { ...NewOrganization.defaultUiOrganizations };
-  let orderNumber;
+  const invoice = { ...NewInvoice.defaultUiInvoice };
+  const allocatedQuantity = '1000';
   let user;
-  let orderId;
+  let orderNumber;
+  let servicePointId;
+  let location;
 
   before(() => {
     cy.getAdminToken();
+    
+    FiscalYears.createViaApi(defaultFiscalYear)
+      .then(response => {
+        defaultFiscalYear.id = response.id;
+        defaultLedger.fiscalYearOneId = defaultFiscalYear.id;
+
+        Ledgers.createViaApi(defaultLedger)
+          .then(ledgerResponse => {
+            defaultLedger.id = ledgerResponse.id;
+            defaultFund.ledgerId = defaultLedger.id;
+            secondFund.ledgerId = defaultLedger.id;
+
+            Funds.createViaApi(defaultFund)
+              .then(fundResponse => {
+                defaultFund.id = fundResponse.fund.id;
+
+                cy.loginAsAdmin({ path:TopMenu.fundPath, waiter: Funds.waitLoading });
+                FinanceHelp.searchByName(defaultFund.name);
+                Funds.selectFund(defaultFund.name);
+                Funds.addBudget(allocatedQuantity);
+              });
+          });
+      });
+
+    ServicePoints.getViaApi()
+      .then((servicePoint) => {
+        servicePointId = servicePoint[0].id;
+        NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId))
+          .then(res => {
+            location = res;
+          });
+      });
 
     Organizations.createOrganizationViaApi(organization)
-      .then(response => {
-        organization.id = response;
-        order.vendor = response;
+      .then(responseOrganizations => {
+        organization.id = responseOrganizations;
+        invoice.accountingCode = organization.erpCode;
+        defaultOrder.vendor = organization.name;
       });
-
-    cy.createOrderApi(order)
-      .then((response) => {
-        orderNumber = response.body.poNumber;
-        orderId = response.body.id;
-      });
-
+    defaultOrder.vendor = organization.name;
+    cy.visit(TopMenu.ordersPath);
+    Orders.createOrderForRollover(defaultOrder, true, false).then(orderResponse => {
+      defaultOrder.id = orderResponse.id;
+      orderNumber = orderResponse.poNumber;
+      Orders.checkCreatedOrder(defaultOrder);
+      OrderLines.addPOLine();
+      OrderLines.selectRandomInstanceInTitleLookUP('*', 1);
+      OrderLines.rolloverPOLineInfoforPhysicalMaterialWithFund(defaultFund, '100', '1', '100', location.institutionId);
+      OrderLines.backToEditingOrder();
+      Orders.openOrder();
+    });
     cy.createTempUser([
       permissions.uiOrdersEdit.gui,
+      permissions.uiOrdersView.gui,
     ])
       .then(userProperties => {
         user = userProperties;
+        cy.login(userProperties.username, userProperties.password, { path:TopMenu.ordersPath, waiter: Orders.waitLoading });
       });
-
-    cy.loginAsAdmin({ path:SettingsMenu.ordersOpeningPurchaseOrdersPath, waiter: SettingsOrders.waitLoadingOpeningPurchaseOrders });
-    SettingsOrders.expectDisabledCheckboxIsOpenOrderEnabled();
   });
 
   after(() => {
-    Orders.deleteOrderViaApi(orderId);
-    Organizations.deleteOrganizationViaApi(organization.id);
     Users.deleteViaApi(user.userId);
   });
 
   it('C16984 Edit cost/fund distribution of POL on an "Open" order (thunderjet)', { tags: [testType.criticalPath, devTeams.thunderjet] }, () => {
-    cy.login(user.username, user.password, { path:TopMenu.ordersPath, waiter: Orders.waitLoading });
     Orders.searchByParameter('PO number', orderNumber);
-    Orders.selectPendingStatusFilter();
     Orders.selectFromResultsList(orderNumber);
-    Orders.editOrder();
-    Orders.selectOngoingOrderTypeInPOForm();
-    Orders.saveEditingOrder();
-    InteractorsTools.checkCalloutMessage(`The Purchase order - ${orderNumber} has been successfully saved`);
-    Orders.checkEditedOngoingOrder(orderNumber, organization.name);
+    OrderLines.selectPOLInOrder(0);
+    OrderLines.editPOLInOrder();
+    OrderLines.editFundInPOL(defaultFund, '200', '200');
+    OrderLines.checkCurrencyInPOL('200.00');
   });
 });
