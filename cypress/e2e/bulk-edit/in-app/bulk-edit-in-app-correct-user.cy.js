a1ea9ac67ab07ec7770f47d76d9fcaa159e83581
nayimovag <Guliyashnar_Nayimova@epam.com>
2023-04-27 18:18:25 +0500

    FAT-5604

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-correct-user.cy.js
@@ -0,0 +1,122 @@
+import devTeams from "../../../support/dictionary/devTeams";
+import permissions from "../../../support/dictionary/permissions"
+import testTypes from "../../../support/dictionary/testTypes";
+import BulkEditSearchPane from "../../../support/fragments/bulk-edit/bulk-edit-search-pane";
+import TopMenu from "../../../support/fragments/topMenu";
+import Users from "../../../support/fragments/users/users";
+import getRandomPostfix from "../../../support/utils/stringTools";
+import InventoryInstances from "../../../support/fragments/inventory/inventoryInstances";
+import FileManager from "../../../support/utils/fileManager";
+import BulkEditActions from "../../../support/fragments/bulk-edit/bulk-edit-actions";
+import { getLongDelay } from "../../../support/utils/cypressTools";
+
+let user1, user2;
+const validItemUUIDsFileName = `validItemUUIDs_${getRandomPostfix()}.csv`;
+const userBarcodesFileName = `userBarcodes_${getRandomPostfix()}.csv`;
+const item = {
+  instanceName: `testBulkEdit_${getRandomPostfix()}`,
+  itemBarcode: getRandomPostfix(),
+  instanceId: '',
+};
+
+describe('Bulk Edit - Items', () => {
+  before('create test data', () => {
+    cy.createTempUser([
+      permissions.bulkEditView.gui,
+      permissions.bulkEditEdit.gui,
+      permissions.inventoryAll.gui,
+    ], 'faculty')
+      .then(userProperties => {
+        user1 = userProperties;
+        cy.login(user1.username, user1.password, {
+          path: TopMenu.bulkEditPath,
+          waiter: BulkEditSearchPane.waitLoading
+        });
+      })
+    cy.createTempUser([
+      permissions.bulkEditUpdateRecords.gui,
+      permissions.uiUsersView.gui,
+    ], 'faculty')
+      .then(userProperties => {
+        user2 = userProperties;
+      });
+
+    item.instanceId = InventoryInstances.createInstanceViaApi(item.instanceName, item.itemBarcode);
+    cy.getInstance({ limit: 1, expandAll: true, query: `"items.barcode"=="${item.itemBarcode}"` })
+      .then((instance) => {
+        item.itemId = instance.items[0].id;
+        FileManager.createFile(`cypress/fixtures/${validItemUUIDsFileName}`, item.itemId);
+        FileManager.createFile(`cypress/fixtures/${userBarcodesFileName}`, `${user1.barcode}\n${user2.barcode}`);
+      });
+  });
+
+  after('delete test data', () => {
+    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.itemBarcode);
+    Users.deleteViaApi(user1.userId);
+    Users.deleteViaApi(user2.userId);
+    FileManager.deleteFile(`cypress/fixtures/${validItemUUIDsFileName}`);
+    FileManager.deleteFile(`cypress/fixtures/${userBarcodesFileName}`);
+  });
+
+  it('C380393 Verify that bulk edit jobs run by correct user (firebird)', { tags: [testTypes.criticalPath, devTeams.firebird] }, () => {
+    BulkEditSearchPane.checkItemsRadio();
+    BulkEditSearchPane.selectRecordIdentifier('Item UUIDs');
+
+    BulkEditSearchPane.uploadFile(validItemUUIDsFileName);
+    BulkEditSearchPane.waitFileUploading();
+    cy.intercept('/bulk-operations/*').as('getData0');
+    cy.wait('@getData0', getLongDelay())
+      .then((res) => {
+        expect(res.response.body.userId).to.eq(user1.userId);
+      });
+    const newLocation = 'Online';
+
+    BulkEditActions.openActions();
+    BulkEditActions.openInAppStartBulkEditFrom();
+    BulkEditActions.replaceTemporaryLocation(newLocation, 'item', 0);
+    cy.intercept('/bulk-operations/*').as('getData1');
+    BulkEditActions.confirmChanges();
+    cy.wait('@getData1', getLongDelay())
+      .then((res) => {
+        expect(res.response.body.userId).to.eq(user1.userId);
+      });
+    cy.intercept('/bulk-operations/*').as('getData2');
+    BulkEditActions.commitChanges();
+    BulkEditSearchPane.waitFileUploading();
+    cy.wait('@getData2', getLongDelay())
+      .then((res) => {
+        expect(res.response.body.userId).to.eq(user1.userId);
+      });
+
+    cy.login(user2.username, user2.password, {
+      path: TopMenu.bulkEditPath,
+      waiter: BulkEditSearchPane.waitLoading
+    });
+    BulkEditSearchPane.selectRecordIdentifier('User Barcodes');
+
+    BulkEditSearchPane.uploadFile(userBarcodesFileName);
+    BulkEditSearchPane.waitFileUploading();
+    cy.intercept('/bulk-operations/*').as('getData3');
+    cy.wait('@getData3', getLongDelay())
+      .then((res) => {
+        expect(res.response.body.userId).to.eq(user2.userId);
+      });
+
+    BulkEditActions.openActions();
+    BulkEditActions.openInAppStartBulkEditFrom();
+    BulkEditActions.fillPatronGroup('staff (Staff Member)');
+    cy.intercept('/bulk-operations/*').as('getData4');
+    BulkEditActions.confirmChanges();
+    cy.wait('@getData4', getLongDelay())
+      .then((res) => {
+        expect(res.response.body.userId).to.eq(user2.userId);
+      });
+    BulkEditActions.commitChanges();
+    cy.intercept('/bulk-operations/*').as('getData5');
+    BulkEditSearchPane.waitFileUploading();
+    cy.wait('@getData5', getLongDelay())
+      .then((res) => {
+        expect(res.response.body.userId).to.eq(user2.userId);
+      });
+  });
+});
