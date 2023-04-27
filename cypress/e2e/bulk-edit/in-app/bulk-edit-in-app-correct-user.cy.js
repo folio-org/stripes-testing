93008bb31de0b59b84b84de93705ba0336ca3b7d
nayimovag <Guliyashnar_Nayimova@epam.com>
2023-04-27 18:24:18 +0500

    linter

--- cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-correct-user.cy.js
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-correct-user.cy.js
@@ -1,14 +1,14 @@
-import devTeams from "../../../support/dictionary/devTeams";
-import permissions from "../../../support/dictionary/permissions"
-import testTypes from "../../../support/dictionary/testTypes";
-import BulkEditSearchPane from "../../../support/fragments/bulk-edit/bulk-edit-search-pane";
-import TopMenu from "../../../support/fragments/topMenu";
-import Users from "../../../support/fragments/users/users";
-import getRandomPostfix from "../../../support/utils/stringTools";
-import InventoryInstances from "../../../support/fragments/inventory/inventoryInstances";
-import FileManager from "../../../support/utils/fileManager";
-import BulkEditActions from "../../../support/fragments/bulk-edit/bulk-edit-actions";
-import { getLongDelay } from "../../../support/utils/cypressTools";
+import devTeams from './../../support/dictionary/devTeams';
+import permissions from '../../../support/dictionary/permissions;';
+import testTypes from '../../../support/dictionary/testTypes';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import FileManager from '../../../support/utils/fileManager';
+import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';
+import { getLongDelay } from '../../../support/utils/cypressTools';
 
 let user1, user2;
 const validItemUUIDsFileName = `validItemUUIDs_${getRandomPostfix()}.csv`;
