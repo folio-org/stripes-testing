1443ba0a42e2c3b3104158f82280f2baedeb674d
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2023-05-01 10:33:41 +0500

    add test C350607

--- cypress/e2e/marc-authority/marc-authority-advanced-search.cy.js
+++ cypress/e2e/marc-authority/marc-authority-advanced-search.cy.js
@@ -96,4 +96,41 @@ describe('MARC Authority - Advanced Search', () => {
     MarcAuthorities.checkSearchInput(`personalNameTitle==${testData.titles.titlePersonalName} or uniformTitle==${testData.titles.titleUniform}`);
     MarcAuthorities.checkRowsContent([testData.titles.titlePersonalName, testData.titles.titleUniform]);
   });
+
+  it('C350607 Advanced search of MARC authority records (spitfire)', { tags: [TestTypes.criticalPath, DevTeams.spitfire] }, () => {
+    MarcAuthorities.clickAdvancedSearchButton();
+    MarcAuthorities.checkAdvancedSearchModalFields(0, testData.titles.titleDefault, testData.searchOptions.keyword);
+    MarcAuthorities.fillAdvancedSearchField(0, testData.titles.titlePersonalName, testData.searchOptions.personalName);
+    MarcAuthorities.checkAdvancedSearchOption(0);
+    MarcAuthorities.fillAdvancedSearchField(1, testData.titles.titleCorporateName, testData.searchOptions.corporateName, testData.booleanOptions.or);
+    MarcAuthorities.checkAdvancedSearchOption(1);
+    MarcAuthorities.fillAdvancedSearchField(2, testData.titles.titleUniform, testData.searchOptions.uniformTitle, testData.booleanOptions.or);
+    MarcAuthorities.checkAdvancedSearchOption(2);
+    MarcAuthorities.clickSearchButton();
+    MarcAuthorities.checkResultList([testData.titles.titleCorporateName, testData.titles.titlePersonalName, testData.titles.titleUniform]);
+    MarcAuthorities.checkSearchInput(`personalNameTitle==${testData.titles.titlePersonalName} or corporateNameTitle==${testData.titles.titleCorporateName} or uniformTitle==${testData.titles.titleUniform}`);
+    
+    MarcAuthorities.clickAdvancedSearchButton();
+    MarcAuthorities.checkAdvancedSearchModalFields(0, testData.titles.titlePersonalName, testData.searchOptions.personalName);
+    MarcAuthorities.checkAdvancedSearchModalFields(1, testData.titles.titleCorporateName, testData.searchOptions.corporateName, testData.booleanOptions.or);
+    MarcAuthorities.checkAdvancedSearchModalFields(2, testData.titles.titleUniform, testData.searchOptions.uniformTitle, testData.booleanOptions.or);
+    MarcAuthorities.clickCancelButton();
+    MarcAuthorities.checkAdvancedSearchModalAbsence();
+    MarcAuthorities.checkResultList([testData.titles.titleCorporateName, testData.titles.titlePersonalName, testData.titles.titleUniform]);
+    MarcAuthorities.checkSearchInput(`personalNameTitle==${testData.titles.titlePersonalName} or corporateNameTitle==${testData.titles.titleCorporateName} or uniformTitle==${testData.titles.titleUniform}`);
+    
+    MarcAuthorities.searchBy('Advanced search', `personalNameTitle==${testData.titles.titlePersonalName} or corporateNameTitle==${testData.titles.titleCorporateName}`);
+    MarcAuthorities.clickAdvancedSearchButton();
+    MarcAuthorities.checkAdvancedSearchModalFields(0, testData.titles.titlePersonalName, testData.searchOptions.personalName);
+    MarcAuthorities.checkAdvancedSearchModalFields(1, testData.titles.titleCorporateName, testData.searchOptions.corporateName, testData.booleanOptions.or);
+    MarcAuthorities.checkAdvancedSearchModalFields(2, testData.titles.titleDefault, testData.searchOptions.keyword, testData.booleanOptions.and);
+    MarcAuthorities.fillAdvancedSearchField(2, testData.titles.titleUniform, testData.searchOptions.uniformTitle, testData.booleanOptions.or);
+    MarcAuthorities.checkAdvancedSearchOption(2);
+    MarcAuthorities.clickSearchButton();
+    MarcAuthorities.checkSearchInput(`personalNameTitle==${testData.titles.titlePersonalName} or corporateNameTitle==${testData.titles.titleCorporateName} or uniformTitle==${testData.titles.titleUniform}`);
+    
+    MarcAuthorities.clickResetAndCheck();
+    MarcAuthorities.clickAdvancedSearchButton();
+    MarcAuthorities.checkAdvancedSearchModalFields(0, testData.titles.titleDefault, testData.searchOptions.keyword);
+  });
 });
