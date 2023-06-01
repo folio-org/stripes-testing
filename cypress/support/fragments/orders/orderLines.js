d834254a1ab0262570945b8d270b6a954bce13f4
IhorBohdan <ihor_bohdan@epam.com>
2023-06-01 09:48:34 +0300

    Implementing of FAT-5081

--- cypress/support/fragments/orders/orderLines.js
+++ cypress/support/fragments/orders/orderLines.js
@@ -739,12 +739,9 @@ export default {
   },
 
   editPOLInOrder: () => {
-    cy.do([
-      orderLineDetailsPane
-        .find(paneHeaderOrderLinesDetailes
-          .find(actionsButton)).click(),
-      Button('Edit').click(),
-    ]);
+    cy.do(orderLineDetailsPane.find(paneHeaderOrderLinesDetailes.find(actionsButton)).click());
+    cy.wait(4000);
+    cy.do(Button('Edit').click());
   },
 
   deleteFundInPOL() {
