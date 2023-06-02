af2caa0ca70c7714eacb268d8c792d6b16dec1de
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2023-06-02 23:03:20 +0300

    Fat 4779 c375126 (#1229)
    * added test
    
    * added checks

--- cypress/e2e/inventory/single-record-import/modal-for-isri-in-inventory-instance-details-menu-for-multiple-target-profiles.cy.js
+++ cypress/e2e/inventory/single-record-import/modal-for-isri-in-inventory-instance-details-menu-for-multiple-target-profiles.cy.js
@@ -4,26 +4,28 @@ import DevTeams from '../../../support/dictionary/devTeams';
 import permissions from '../../../support/dictionary/permissions';
 import Z3950TargetProfiles from '../../../support/fragments/settings/inventory/integrations/z39.50TargetProfiles';
 import TopMenu from '../../../support/fragments/topMenu';
-import DataImport from '../../../support/fragments/data_import/dataImport';
 import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
 import SettingsMenu from '../../../support/fragments/settingsMenu';
+import ViewTargetProfile from '../../../support/fragments/settings/inventory/integrations/viewTargetProfile';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
 import Logs from '../../../support/fragments/data_import/logs/logs';
 import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
-import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
-import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
-import ViewTargetProfile from '../../../support/fragments/settings/inventory/integrations/viewTargetProfile';
 import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import ReImportModal from '../../../support/fragments/inventory/reImportModal';
 import InteractorsTools from '../../../support/utils/interactorsTools';
 import InstanceRecordView from '../../../support/fragments/inventory/instanceRecordView';
 import Users from '../../../support/fragments/users/users';
-import ReImportModal from '../../../support/fragments/inventory/reImportModal';
 
 describe('ui-inventory', () => {
   let user;
   let instanceHRID;
-  const profileForImport = 'Inventory Single Record - Default Update Instance (Default)';
-  const fileName = `C375146autotestFile.${getRandomPostfix()}.mrc`;
-  const targetIdentifier = '1234567';
+  let profileId;
+  const fileName = `C375126 autotestFile.${getRandomPostfix()}.mrc`;
+  const targetProfileName = `C375126 autotest profile${getRandomPostfix()}`;
+  const defaultTargetProfileName = 'OCLC WorldCat';
+  const profileForOverlay = 'Inventory Single Record - Default Update Instance (Default)';
   const targetProfile = {
     name: 'OCLC WorldCat',
     url: 'zcat.oclc.org/OLUCWorldCat',
@@ -31,31 +33,42 @@ describe('ui-inventory', () => {
     externalId: '@attr 1=1211 $identifier',
     internalId: '999ff$i'
   };
+  const testIdentifier = '1234567';
   const successCalloutMessage = 'Record 1234567 updated. Results may take a few moments to become visible in Inventory';
   const instanceTitle = 'The Gospel according to Saint Mark : Evangelistib Markusib aglangit.';
 
-  before('login', () => {
+  before('create test data', () => {
     cy.loginAsAdmin({ path: TopMenu.dataImportPath, waiter: DataImport.waitLoading });
-    cy.getAdminToken()
-      .then(() => {
-        DataImport.uploadFileViaApi('oneMarcBib.mrc', fileName);
-        JobProfiles.waitFileIsImported(fileName);
-        Logs.openFileDetails(fileName);
-        FileDetails.openInstanceInInventory('Created');
-        InventoryInstance.getAssignedHRID().then(initialInstanceHrId => {
-          instanceHRID = initialInstanceHrId;
-        });
-        Z3950TargetProfiles.changeOclcWorldCatValueViaApi('100473910/PAOLF');
-        cy.visit(SettingsMenu.targetProfilesPath);
-        Z3950TargetProfiles.openTargetProfile();
-        ViewTargetProfile.verifyTargetProfileForm(
-          targetProfile.name,
-          targetProfile.url,
-          targetProfile.authentification,
-          targetProfile.externalId,
-          targetProfile.internalId
-        );
+    cy.getAdminToken().then(() => {
+      DataImport.uploadFileViaApi('oneMarcBib.mrc', fileName);
+      JobProfiles.waitFileIsImported(fileName);
+      Logs.openFileDetails(fileName);
+      FileDetails.openInstanceInInventory('Created');
+      InventoryInstance.getAssignedHRID().then(initialInstanceHrId => {
+        instanceHRID = initialInstanceHrId;
       });
+      Z3950TargetProfiles.changeOclcWorldCatValueViaApi('100473910/PAOLF');
+      Z3950TargetProfiles.createNewZ3950TargetProfileViaApi(targetProfileName)
+        .then(initialId => { profileId = initialId; });
+      cy.visit(SettingsMenu.targetProfilesPath);
+      Z3950TargetProfiles.openTargetProfile();
+      ViewTargetProfile.verifyTargetProfileForm(
+        targetProfile.name,
+        targetProfile.url,
+        targetProfile.authentification,
+        targetProfile.externalId,
+        targetProfile.internalId
+      );
+      Z3950TargetProfiles.openTargetProfile(profileId);
+      ViewTargetProfile.verifyTargetProfileForm(
+        targetProfile.name,
+        targetProfile.url,
+        targetProfile.authentification,
+        targetProfile.externalId,
+        targetProfile.internalId
+      );
+    });
+    cy.logout();
 
     cy.createTempUser([
       permissions.inventoryAll.gui,
@@ -78,14 +91,15 @@ describe('ui-inventory', () => {
       });
   });
 
-  it('C375146 Verify the modal window for ISRI In inventory instance details menu for single target profile (update) (folijet)',
+  it('C375126 Verify the modal window for ISRI In inventory instance details menu for multiple target profiles (folijet)',
     { tags: [TestTypes.criticalPath, DevTeams.folijet] }, () => {
       InventorySearchAndFilter.searchInstanceByHRID(instanceHRID);
       InventoryInstance.startOverlaySourceBibRecord();
-      ReImportModal.verifyModalWithOneTargetProfile();
-      ReImportModal.verifySelectTheProfileToBeUsedToOverlayTheCurrentDataField(profileForImport);
-      ReImportModal.selectTheProfileToBeUsedToOverlayTheCurrentData(profileForImport);
-      ReImportModal.fillEnterTheTargetIdentifier(targetIdentifier);
+      ReImportModal.verifyModalWithSeveralTargetProfiles();
+      ReImportModal.verifyExternalTargetField(targetProfileName);
+      ReImportModal.selectExternalTarget(defaultTargetProfileName);
+      ReImportModal.selectTheProfileToBeUsedToOverlayTheCurrentData(profileForOverlay);
+      ReImportModal.fillEnterTheTargetIdentifier(testIdentifier);
       ReImportModal.import();
       // need to wait because after the import the data in the instance is displayed for a long time
       // https://issues.folio.org/browse/MODCPCT-73
