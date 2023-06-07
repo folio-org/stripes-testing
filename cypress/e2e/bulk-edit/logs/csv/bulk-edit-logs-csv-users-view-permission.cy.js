63efe4a925c43efd1bc9ca93e079384e3b9e7fb8
nayimovag <Guliyashnar_Nayimova@epam.com>
2023-06-07 16:34:00 +0500

    fixed comments

--- cypress/e2e/bulk-edit/logs/csv/bulk-edit-logs-csv-users-view-permission.cy.js
+++ cypress/e2e/bulk-edit/logs/csv/bulk-edit-logs-csv-users-view-permission.cy.js
@@ -39,7 +39,7 @@ describe('Bulk Edit - Logs', () => {
     FileManager.deleteFileFromDownloadsByMask(`*${matchedRecordsFileName}`, changedRecordsFileName);
   });
 
-  it('C380562 Verify generated Logs files for Users CSV are hidden without "Users: Can view user profile" permission (firebird)', { tags: [testTypes.smoke, devTeams.firebird] }, () => {
+  it('C380562 Verify generated Logs files for Users CSV are hidden without "Users: Can view user profile" permission (firebird)', { tags: [testTypes.criticalPath, devTeams.firebird] }, () => {
     BulkEditSearchPane.verifyDragNDropUsersUIIDsArea();
     BulkEditSearchPane.uploadFile(userUUIDsFileName);
     BulkEditSearchPane.waitFileUploading();
