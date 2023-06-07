203321dd886a979938c68a3fc892665b290bcec9
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-06-07 17:03:15 +0300

    fixed linter issue

--- cypress/support/fragments/data_import/match_profiles/newMatchProfile.js
+++ cypress/support/fragments/data_import/match_profiles/newMatchProfile.js
@@ -6,10 +6,7 @@ import {
   SelectionList,
   Accordion,
   SelectionOption,
-  Dropdown,
-  Checkbox,
-  Section,
-  including
+  Dropdown
 } from '../../../../../interactors';
 import { EXISTING_RECORDS_NAMES } from '../../../constants';
 
