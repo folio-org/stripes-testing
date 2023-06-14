f013fca0e95cad6d793c99000f51694bd2804459
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2023-06-14 16:33:44 +0300

    Fat 1299 c347831 (#1248)
    * added test
    
    * added after block
    
    * added deleting instance
    
    * fixed opening instance
    
    * uncommented after block
    
    * added waiter

--- cypress/e2e/data-import/importing-marc-bib-files/match-on-instance-identifier-scen4.cy.js
+++ cypress/e2e/data-import/importing-marc-bib-files/match-on-instance-identifier-scen4.cy.js
@@ -1,36 +1,41 @@
+import getRandomPostfix from '../../../support/utils/stringTools';
+import permissions from '../../../support/dictionary/permissions';
 import TestTypes from '../../../support/dictionary/testTypes';
 import DevTeams from '../../../support/dictionary/devTeams';
 import TopMenu from '../../../support/fragments/topMenu';
-import { FOLIO_RECORD_TYPE,
-  INSTANCE_STATUS_TERM_NAMES,
-  ACCEPTED_DATA_TYPE_NAMES,
-  EXISTING_RECORDS_NAMES,
-  JOB_STATUS_NAMES } from '../../../support/constants';
-import getRandomPostfix from '../../../support/utils/stringTools';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
 import DataImport from '../../../support/fragments/data_import/dataImport';
-import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import {
+  JOB_STATUS_NAMES,
+  EXISTING_RECORDS_NAMES,
+  FOLIO_RECORD_TYPE,
+  INSTANCE_STATUS_TERM_NAMES,
+  ACCEPTED_DATA_TYPE_NAMES
+} from '../../../support/constants';
 import Logs from '../../../support/fragments/data_import/logs/logs';
 import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
-import SettingsMenu from '../../../support/fragments/settingsMenu';
-import MatchProfiles from '../../../support/fragments/data_import/match_profiles/matchProfiles';
+import NewMatchProfile from '../../../support/fragments/data_import/match_profiles/newMatchProfile';
 import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
 import NewFieldMappingProfile from '../../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
 import ActionProfiles from '../../../support/fragments/data_import/action_profiles/actionProfiles';
 import NewJobProfile from '../../../support/fragments/data_import/job_profiles/newJobProfile';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import MatchProfiles from '../../../support/fragments/data_import/match_profiles/matchProfiles';
+import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
 import InstanceRecordView from '../../../support/fragments/inventory/instanceRecordView';
 import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
-import permissions from '../../../support/dictionary/permissions';
-import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
 import Users from '../../../support/fragments/users/users';
 
 describe('ui-data-import', () => {
-  let userId;
+  let user;
   const filePathForCreateInstance = 'marcFileForMatchOnIdentifierForCreate.mrc';
-  const filePathForUpdateInstance = 'marcFileForMatchOnIdentifierForUpdate_2.mrc';
-  const fileNameForCreateInstance = `C347829autotestFile.${getRandomPostfix()}.mrc`;
-  const fileNameForUpdateInstance = `C347829autotestFile.${getRandomPostfix()}.mrc`;
+  const filePathForUpdateInstance = 'marcFileForMatchOnIdentifierForUpdate_4.mrc';
+  const fileNameForCreateInstance = `C347831autotestFile.${getRandomPostfix()}.mrc`;
+  const fileNameForUpdateInstance = `C347831autotestFile.${getRandomPostfix()}.mrc`;
   const jobProfileToRun = 'Default - Create instance and SRS MARC Bib';
-  const instanceGeneralNote = 'IDENTIFIER UPDATE 2';
+  const firstInstanceTitle = 'Competing with idiots : Herman and Joe Mankiewicz, a dual portrait / Nick Davis.';
+  const secondInstaneTitle = 'Letters from a Stoic : The Ancient Classic  / Seneca, with a introduction of Donald Robertson.';
+  const instanceGeneralNote = 'IDENTIFIER UPDATE 4';
   const resourceIdentifiers = [
     { type: 'UPC', value: 'ORD32671387-4' },
     { type: 'OCLC', value: '(OCoLC)84714376518561876438' },
@@ -38,74 +43,81 @@ describe('ui-data-import', () => {
     { type: 'System control number', value: '(AMB)84714376518561876438' },
   ];
   const matchProfile = {
-    profileName: `C347829autotestMatchProf${getRandomPostfix()}`,
+    profileName: `C347831 ID Match Test - Update4 (System control number).${getRandomPostfix()}`,
     incomingRecordFields: {
-      field: '024',
-      in1: '1',
-      subfield: 'z'
+      field: '035',
+      in1: '*',
+      in2: '*',
+      subfield: 'a'
     },
     matchCriterion: 'Exactly matches',
+    qualifierType: 'Begins with',
+    qualifierValue: '(AMB)',
     existingRecordType: EXISTING_RECORDS_NAMES.INSTANCE,
-    instanceOption: 'Identifier: Invalid UPC',
+    existingRecordOption: NewMatchProfile.optionsList.systemControlNumber
   };
   const mappingProfile = {
-    name: `C347829autotestMappingProf${getRandomPostfix()}`,
+    name: `C347831 ID Match Test - Update4 (System control number).${getRandomPostfix()}`,
     typeValue: FOLIO_RECORD_TYPE.INSTANCE,
-    staffSuppress: 'Mark for all affected records',
-    catalogedDate: '"2021-12-02"',
-    catalogedDateUI: '2021-12-02',
-    instanceStatus: INSTANCE_STATUS_TERM_NAMES.CATALOGED
+    staffSuppress: 'Unmark for all affected records',
+    catalogedDate: '"2021-12-04"',
+    catalogedDateUI: '2021-12-04',
+    instanceStatus: INSTANCE_STATUS_TERM_NAMES.OTHER
   };
   const actionProfile = {
     typeValue: FOLIO_RECORD_TYPE.INSTANCE,
-    name: `C347829autotestActionProf${getRandomPostfix()}`,
+    name: `C347831 ID Match Test - Update4 (System control number).${getRandomPostfix()}`,
     action: 'Update (all record types except Orders, Invoices, or MARC Holdings)'
   };
   const jobProfile = {
     ...NewJobProfile.defaultJobProfile,
-    profileName: `C347829autotestJobProf${getRandomPostfix()}`,
+    profileName: `C347831 ID Match Test - Update4 (System control number).${getRandomPostfix()}`,
     acceptedType: ACCEPTED_DATA_TYPE_NAMES.MARC
   };
 
-  beforeEach('create test data', () => {
+  before('create test data', () => {
+    cy.getAdminToken()
+    .then(() => {
+      InventorySearchAndFilter.getInstancesByIdentifierViaApi(resourceIdentifiers[0].value)
+          .then(instances => {
+            if (instances) {
+              instances.forEach(({ id }) => {
+                InventoryInstance.deleteInstanceViaApi(id);
+              });
+            }
+          });
+    });
+
     cy.createTempUser([
       permissions.moduleDataImportEnabled.gui,
       permissions.dataImportDeleteLogs.gui,
       permissions.inventoryAll.gui,
-      permissions.settingsDataImportEnabled.gui,
-      permissions.viewEditDeleteInvoiceInvoiceLine.gui,
-      permissions.viewEditCreateInvoiceInvoiceLine.gui,
-      permissions.assignAcqUnitsToNewInvoice.gui,
-      permissions.invoiceSettingsAll.gui,
-      permissions.remoteStorageView.gui
+      permissions.settingsDataImportEnabled.gui
     ])
       .then(userProperties => {
-        userId = userProperties.userId;
-        cy.login(userProperties.username, userProperties.password, {
-          path: TopMenu.dataImportPath,
-          waiter: DataImport.waitLoading
-        });
-      });
-
-    InventorySearchAndFilter.getInstancesByIdentifierViaApi(resourceIdentifiers[0].value)
-      .then(instances => {
-        instances.forEach(({ id }) => {
-          InventoryInstance.deleteInstanceViaApi(id);
-        });
+        user = userProperties;
+        cy.login(user.username, user.password,
+          { path: TopMenu.dataImportPath, waiter: DataImport.waitLoading });
       });
   });
 
   after('delete test data', () => {
-    // delete profiles
     JobProfiles.deleteJobProfile(jobProfile.profileName);
     MatchProfiles.deleteMatchProfile(matchProfile.profileName);
     ActionProfiles.deleteActionProfile(actionProfile.name);
     FieldMappingProfiles.deleteFieldMappingProfile(mappingProfile.name);
-    Users.deleteViaApi(userId);
+    Users.deleteViaApi(user.userId);
+    InventorySearchAndFilter.getInstancesByIdentifierViaApi(resourceIdentifiers[0].value)
+    .then(instances => {
+      instances.forEach(({ id }) => {
+        InventoryInstance.deleteInstanceViaApi(id);
+      });
+    });
   });
 
-  it('C347829 Match on Instance identifier match meets both the Identifier type and Data requirements (folijet)', { tags: [TestTypes.criticalPath, DevTeams.folijet] }, () => {
-    // TODO delete function after fix https://issues.folio.org/browse/MODDATAIMP-691
+  it('C347831 MODDICORE-231 "Match on Instance identifier match meets both the Identifier type and Data requirements" Scenario 4 (folijet)',
+    { tags: [TestTypes.criticalPath, DevTeams.folijet] }, () => {
+      // TODO delete function after fix https://issues.folio.org/browse/MODDATAIMP-691
     DataImport.verifyUploadState();
     DataImport.uploadFile(filePathForCreateInstance, fileNameForCreateInstance);
     JobProfiles.searchJobProfileForImport(jobProfileToRun);
@@ -114,15 +126,18 @@ describe('ui-data-import', () => {
     Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
     Logs.openFileDetails(fileNameForCreateInstance);
     Logs.clickOnHotLink(0, 3, 'Created');
+    InventoryInstance.verifyInstanceTitle(firstInstanceTitle);
     InventoryInstance.verifyResourceIdentifier(resourceIdentifiers[0].type, resourceIdentifiers[0].value, 6);
     InventoryInstance.verifyResourceIdentifier(resourceIdentifiers[1].type, resourceIdentifiers[1].value, 4);
     cy.go('back');
     Logs.clickOnHotLink(1, 3, 'Created');
+    InventoryInstance.verifyInstanceTitle(secondInstaneTitle);
     InventoryInstance.verifyResourceIdentifier(resourceIdentifiers[2].type, resourceIdentifiers[2].value, 0);
     InventoryInstance.verifyResourceIdentifier(resourceIdentifiers[3].type, resourceIdentifiers[3].value, 3);
-
+    
     cy.visit(SettingsMenu.matchProfilePath);
-    MatchProfiles.createMatchProfile(matchProfile);
+    MatchProfiles.createMatchProfileWithQualifierAndExistingRecordField(matchProfile);
+    MatchProfiles.checkMatchProfilePresented(matchProfile.profileName);
 
     cy.visit(SettingsMenu.mappingProfilePath);
     FieldMappingProfiles.openNewMappingProfileForm();
@@ -151,12 +166,15 @@ describe('ui-data-import', () => {
     JobProfiles.waitFileIsImported(fileNameForUpdateInstance);
     Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
     Logs.openFileDetails(fileNameForUpdateInstance);
-    Logs.verifyInstanceStatus(0, 3, FileDetails.status.noAction);
-    Logs.verifyInstanceStatus(1, 3, 'Updated');
-    Logs.clickOnHotLink(1, 3, 'Updated');
+    FileDetails.checkStatusInColumn(FileDetails.status.dash, FileDetails.columnNameInResultList.srsMarc);
+    FileDetails.checkStatusInColumn(FileDetails.status.noAction, FileDetails.columnNameInResultList.instance);
+    FileDetails.checkStatusInColumn(FileDetails.status.created, FileDetails.columnNameInResultList.srsMarc, 1);
+    FileDetails.checkStatusInColumn(FileDetails.status.updated, FileDetails.columnNameInResultList.instance, 1);
+    
+    // check updated instance in Inventory
+    FileDetails.openInstanceInInventory('Updated', 1);
     InstanceRecordView.verifyInstanceStatusTerm(mappingProfile.instanceStatus);
-    InstanceRecordView.verifyMarkAsSuppressed();
     InstanceRecordView.verifyCatalogedDate(mappingProfile.catalogedDateUI);
     InstanceRecordView.verifyGeneralNoteContent(instanceGeneralNote);
-  });
+    });
 });
