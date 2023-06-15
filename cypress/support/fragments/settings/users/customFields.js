a88f13d427d420ec5bf73540a504682e2564b508
nayimovag <Guliyashnar_Nayimova@epam.com>
2023-06-15 11:13:08 +0500

    Revert "FAT-6340"

--- cypress/support/fragments/settings/users/customFields.js
+++ cypress/support/fragments/settings/users/customFields.js
@@ -1,5 +1,4 @@
-import { including } from 'bigtest';
-import { Pane, Button, Dropdown, TextField, MultiColumnListRow, Accordion } from '../../../../../interactors';
+import { Pane, Button, Dropdown, TextField, MultiColumnListRow } from '../../../../../interactors';
 
 const customFieldsPane = Pane('Custom fields');
 const editNewButton = Button({ href: '/settings/users/custom-fields/edit' });
@@ -18,17 +17,6 @@ export default {
       TextField('Field label*').fillIn(data.fieldLabel),
       MultiColumnListRow({ indexRow: 'row-0' }).find(TextField()).fillIn(data.label1),
       MultiColumnListRow({ indexRow: 'row-1' }).find(TextField()).fillIn(data.label2),
-      saveAndCloseButton.click(),
-    ]);
-  },
-
-  editMultiSelectCustomField(oldData, newData) {
-    cy.do([
-      editNewButton.click(),
-      Accordion(including(oldData.fieldLabel)).clickHeader(),
-      TextField('Field label*').fillIn(newData.fieldLabel),
-      MultiColumnListRow({ indexRow: 'row-0' }).find(TextField()).fillIn(newData.label1),
-      MultiColumnListRow({ indexRow: 'row-1' }).find(TextField()).fillIn(newData.label2),
       saveAndCloseButton.click(),
     ]);
   },
