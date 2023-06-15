e4262a776a2f31da27105d0d7d519959c8870bb5
nayimovag <Guliyashnar_Nayimova@epam.com>
2023-06-15 12:28:34 +0500

    FAT-6341

--- cypress/support/fragments/settings/users/customFields.js
+++ cypress/support/fragments/settings/users/customFields.js
@@ -1,15 +1,14 @@
-import { Pane, Button, Dropdown, TextField, MultiColumnListRow } from '../../../../../interactors';
+import { including } from 'bigtest';
+import { Pane, Button, Dropdown, TextField, MultiColumnListRow, Accordion } from '../../../../../interactors';
 
 const customFieldsPane = Pane('Custom fields');
 const editNewButton = Button({ href: '/settings/users/custom-fields/edit' });
 const addCustomFieldDropdown = Dropdown('Add custom field');
 const saveAndCloseButton = Button('Save & close');
-
 export default {
   waitLoading() {
     cy.expect(customFieldsPane.exists());
   },
-
   addMultiSelectCustomField(data) {
     cy.do([
       editNewButton.click(),
@@ -17,6 +16,17 @@ export default {
       TextField('Field label*').fillIn(data.fieldLabel),
       MultiColumnListRow({ indexRow: 'row-0' }).find(TextField()).fillIn(data.label1),
       MultiColumnListRow({ indexRow: 'row-1' }).find(TextField()).fillIn(data.label2),
+      saveAndCloseButton.click(),
+    ]);
+  },
+
+  editMultiSelectCustomField(oldData, newData) {
+    cy.do([
+      editNewButton.click(),
+      Accordion(including(oldData.fieldLabel)).clickHeader(),
+      TextField('Field label*').fillIn(newData.fieldLabel),
+      MultiColumnListRow({ indexRow: 'row-0' }).find(TextField()).fillIn(newData.label1),
+      MultiColumnListRow({ indexRow: 'row-1' }).find(TextField()).fillIn(newData.label2),
       saveAndCloseButton.click(),
     ]);
   },
