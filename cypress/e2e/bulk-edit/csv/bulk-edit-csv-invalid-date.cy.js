1238b2eb4da16a83364cc2096509e123225e56f2
nayimovag <Guliyashnar_Nayimova@epam.com>
2023-06-20 12:47:02 +0500

    FAT-6224

---
+++ cypress/e2e/bulk-edit/csv/bulk-edit-csv-invalid-date.cy.js
@@ -0,0 +1,67 @@
+import permissions from '../../../support/dictionary/permissions';
+import TopMenu from '../../../support/fragments/topMenu';
+import FileManager from '../../../support/utils/fileManager';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import testTypes from '../../../support/dictionary/testTypes';
+import devTeams from '../../../support/dictionary/devTeams';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import Users from '../../../support/fragments/users/users';
+import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';
+import DateTools from '../../../support/utils/dateTools';
+import BulkEditFiles from '../../../support/fragments/bulk-edit/bulk-edit-files';
+
+let user;
+const userBarcodesFileName = `userBarcodes_${getRandomPostfix()}.csv`;
+const matchedRecordsFileName = `*Matched-Records-${userBarcodesFileName}`;
+const editedFileName = `edited-records-${getRandomPostfix()}.csv`;
+const errorsInChangedRecordsFileName = `*-Errors-${editedFileName}`;
+
+describe('bulk-edit', () => {
+  describe('csv approach', () => {
+    before('create test data', () => {
+      cy.createTempUser([
+        permissions.bulkEditCsvView.gui,
+        permissions.bulkEditCsvEdit.gui,
+      ])
+        .then(userProperties => {
+          user = userProperties;
+          cy.login(user.username, user.password, {
+            path: TopMenu.bulkEditPath,
+            waiter: BulkEditSearchPane.waitLoading
+          });
+          FileManager.createFile(`cypress/fixtures/${userBarcodesFileName}`, user.barcode);
+        });
+    });
+
+    after('delete test data', () => {
+      Users.deleteViaApi(user.userId);
+      FileManager.deleteFile(`cypress/fixtures/${userBarcodesFileName}`);
+      FileManager.deleteFile(`cypress/fixtures/${editedFileName}`);
+      FileManager.deleteFileFromDownloadsByMask(matchedRecordsFileName, errorsInChangedRecordsFileName);
+    });
+
+    it('C388498 Negative: Verify CSV updating records with invalid date (firebird)', { tags: [testTypes.criticalPath, devTeams.firebird] }, () => {
+      BulkEditSearchPane.selectRecordIdentifier('User Barcodes');
+
+      BulkEditSearchPane.uploadFile(userBarcodesFileName);
+      BulkEditSearchPane.waitFileUploading();
+      BulkEditActions.downloadMatchedResults();
+
+      const date = DateTools.getFormattedDate({ date: new Date() });
+      const invalidDate = 'June 19';
+      BulkEditActions.prepareValidBulkEditFile(matchedRecordsFileName, editedFileName, date, invalidDate);
+
+      BulkEditActions.openStartBulkEditForm();
+      BulkEditSearchPane.uploadFile(editedFileName);
+      BulkEditSearchPane.waitFileUploading();
+      BulkEditActions.clickNext();
+      BulkEditActions.commitChanges();
+
+      BulkEditSearchPane.verifyErrorLabelAfterChanges(editedFileName, 0, 1);
+      BulkEditSearchPane.verifyReasonForError('Field "createdDate"');
+      BulkEditActions.openActions();
+      BulkEditActions.downloadErrors();
+      BulkEditFiles.verifyMatchedResultFileContent(errorsInChangedRecordsFileName, [user.barcode], 'firstElement', false);
+    });
+  });
+});
