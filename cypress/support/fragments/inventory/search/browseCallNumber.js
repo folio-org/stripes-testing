77cd4deece8cd2af85ef26c2cdee14c29fa55ddf
Sherzod-Kenjaev <114989418+Sherzod-Kenjaev@users.noreply.github.com>
2023-06-22 16:58:46 +0500

    added test C347909 (#1296)

--- cypress/support/fragments/inventory/search/browseCallNumber.js
+++ cypress/support/fragments/inventory/search/browseCallNumber.js
@@ -1,6 +1,7 @@
-import {Button, including, MultiColumnListCell} from '../../../../../interactors';
+import {Button, including, MultiColumnListCell, MultiColumnListHeader, Section} from '../../../../../interactors';
 
 const browseButton = Button({ id: 'mode-navigation-browse' });
+const instanceDetailsPane = Section({ id: 'pane-instancedetails' });
 
 export default {
   clickOnResult(searchQuery) {
@@ -24,6 +25,19 @@ export default {
       MultiColumnListCell(`${callNumber}would be here`).has({ content: including(`${callNumber}would be here`) }),
       MultiColumnListCell(`${callNumber} ${suffix}`).has({ content: including(`${callNumber} ${suffix}`) }),
     ]);
+  },
+
+  checkSearchResultsTable() {
+    cy.expect([
+      MultiColumnListHeader({ id: 'list-column-callnumber' }).has({ content: 'Call number' }),
+      MultiColumnListHeader({ id: 'list-column-title' }).has({ content: 'Title' }),
+      MultiColumnListHeader({ id: 'list-column-numberoftitles' }).has({ content: 'Number of titles' }),
+    ]);
+  },
+
+  selectFoundCallNumber(callNumber) {
+    cy.do(Button(including(callNumber)).click());
+    cy.expect(instanceDetailsPane.exists());
   },
   
   clickBrowseBtn() {
