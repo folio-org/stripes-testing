b11ea81f3f5269b407dc09608f6be685020cf0fc
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2023-06-27 14:15:01 +0500

    added test C350387

--- cypress/support/fragments/inventory/search/browseSubjects.js
+++ cypress/support/fragments/inventory/search/browseSubjects.js
@@ -1,10 +1,13 @@
-import { Button, MultiColumnListCell, Pane, TextField, Link } from '../../../../../interactors';
+import { Button, MultiColumnListCell, Pane, TextField, Link, MultiColumnListHeader, MultiColumnListRow, Section } from '../../../../../interactors';
 import { including } from 'bigtest';
 import InventorySearchAndFilter from '../inventorySearchAndFilter';
 
 const searchButton = Button('Search');
 const browseInventoryPane = Pane('Browse inventory');
 const searchFilterPane = Pane('Search & filter');
+const inventorySearchResultsPane = Section({ id: 'browse-inventory-results-pane' });
+const nextButton = Button({ id: 'browse-results-list-browseSubjects-next-paging-button' });
+const previousButton = Button({ id: 'browse-results-list-browseSubjects-prev-paging-button' });
 
 export default {
   verifySearchButtonDisabled() {
@@ -33,6 +36,36 @@ export default {
     cy.do(MultiColumnListCell({ content: including(text) }).find(Link()).click());
     InventorySearchAndFilter.waitLoading();
     InventorySearchAndFilter.verifySearchResult(text);
+  },
+
+  checkSearchResultsTable() {
+    cy.expect([
+      MultiColumnListHeader({ id: 'list-column-subject' }).has({ content: 'Subject' }),
+      MultiColumnListHeader({ id: 'list-column-numberoftitles' }).has({ content: 'Number of titles' }),
+    ]);
+  },
+
+  checkResultAndItsRow(rowIndex, value) {
+    cy.expect(MultiColumnListRow({ indexRow: `row-${rowIndex}` }).has({ content: value }));
+  },
+
+  checkAbsenceResultAndItsRow(rowIndex, value) {
+    cy.expect(MultiColumnListRow({ indexRow: `row-${rowIndex}`, content: value }).absent());
+  },
+
+  checkPaginationButtons() {
+    cy.expect([
+      Button('Previous').is({ visible: true, disabled: false }),
+      Button('Next').is({ visible: true, disabled: false }),
+    ]);
+  },
+
+  clickNextPaginationButton() {
+    cy.do(inventorySearchResultsPane.find(nextButton).click());
+  },
+  
+  clickPreviousPaginationButton() {
+    cy.do(inventorySearchResultsPane.find(previousButton).click());
   },
 
   clearSearchTextfield() {
