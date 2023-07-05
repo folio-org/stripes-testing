3b8be7846ad0906a9b7a5149f8319c8298f54d13
IhorBohdan <ihor_bohdan@epam.com>
2023-07-05 16:28:42 +0300

    Fix Jenkins issues Orchid

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,58 +10,5 @@ const path = require('path');
 
 module.exports = async (on, config) => {
   cypressGrep(config);
-
-  on('task', {
-    async findFiles(mask) {
-      if (!mask) {
-        throw new Error('Missing a file mask to search');
-      }
-
-      const list = await globby(mask);
-
-      if (!list.length) {
-        return null;
-      }
-
-      return list;
-    },
-    downloadFile,
-
-    deleteFolder(folderName) {
-      return new Promise((resolve, reject) => {
-        // eslint-disable-next-line consistent-return
-        rmdir(folderName, { maxRetries: 10, recursive: true }, (err) => {
-          if (err && err.code !== 'ENOENT') {
-            return reject(err);
-          }
-
-          resolve(null);
-        });
-      });
-    },
-
-    deleteFile(pathToFile) {
-      return new Promise((resolve, reject) => {
-        // eslint-disable-next-line consistent-return
-        unlink(pathToFile, (err) => {
-          if (err && err.code !== 'ENOENT') {
-            return reject(err);
-          }
-
-          resolve(null);
-        });
-      });
-    },
-
-    readFileFromDownloads(filename) {
-      const downloadsFolder = config.downloadsFolder || path.join(__dirname, '..', '..', 'Downloads');
-      const filePath = path.join(downloadsFolder, filename);
-      return fs.readFileSync(filePath, 'utf-8');
-    },
-  });
-
-  allureWriter(on, config);
-  await require('cypress-testrail-simple/src/plugin')(on, config);
-
   return config;
 };
