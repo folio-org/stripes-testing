b66e81ed99e1e26bf8cb479c5d454febb6ede7a7
IhorBohdan <ihor_bohdan@epam.com>
2023-07-05 16:37:37 +0300

    Fix Jenkins issues Orchid

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,12 +1,6 @@
 /// <reference types="@shelex/cypress-allure-plugin" />
 
-const globby = require('globby');
-const { rmdir, unlink } = require('fs');
 const cypressGrep = require('cypress-grep/src/plugin');
-const { downloadFile } = require('cypress-downloadfile/lib/addPlugin');
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-const fs = require('fs');
-const path = require('path');
 
 module.exports = async (on, config) => {
   cypressGrep(config);
