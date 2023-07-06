9ea333e4ebb3422993dbcb8b4b65fc644ac3e6e0
IhorBohdan <99126301+IhorBohdan@users.noreply.github.com>
2023-07-06 09:42:47 +0300

    Fat 6859 nolana (#1332)
    * Fix Jenkins issues Nolana
    
    * Fix Jenkins issues Nolana

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('cypress');
+const path = require('path');
+const globby = require('globby');
+const { rmdir, unlink } = require('fs');
+const { downloadFile } = require('cypress-downloadfile/lib/addPlugin');
+const fs = require('fs');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   viewportWidth: 1920,
@@ -18,10 +24,62 @@ module.exports = defineConfig({
     grepOmitFiltered: true,
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+    async setupNodeEvents(on, config) {
+      allureWriter(on, config);
+
+      on('task', {
+        async findFiles(mask) {
+          if (!mask) {
+            throw new Error('Missing a file mask to search');
+          }
+
+          const list = await globby(mask);
+
+          if (!list.length) {
+            return null;
+          }
+
+          return list;
+        },
+        downloadFile,
+
+        deleteFolder(folderName) {
+          return new Promise((resolve, reject) => {
+            // eslint-disable-next-line consistent-return
+            rmdir(folderName, { maxRetries: 10, recursive: true }, (err) => {
+              if (err && err.code !== 'ENOENT') {
+                return reject(err);
+              }
+
+              resolve(null);
+            });
+          });
+        },
+
+        deleteFile(pathToFile) {
+          return new Promise((resolve, reject) => {
+            // eslint-disable-next-line consistent-return
+            unlink(pathToFile, (err) => {
+              if (err && err.code !== 'ENOENT') {
+                return reject(err);
+              }
+
+              resolve(null);
+            });
+          });
+        },
+
+        readFileFromDownloads(filename) {
+          const downloadsFolder = config.downloadsFolder || path.join(__dirname, '..', '..', 'Downloads');
+          const filePath = path.join(downloadsFolder, filename);
+          return fs.readFileSync(filePath, 'utf-8');
+        },
+      });
+
+      // eslint-disable-next-line global-require
+      await require('cypress-testrail-simple/src/plugin')(on, config);
+
+      return config;
     },
     baseUrl: 'https://bugfest-nolana-aqa.int.aws.folio.org',
   },
