05fe669988c2636d2259d5eabb81c73edead781a
MaddiSwathiReddy <swathim@ideyalabs.com>
2023-07-21 19:59:19 +0530

    remove cy.wait()

--- cypress/e2e/ideyalabs/D_order.cy.js
+++ cypress/e2e/ideyalabs/D_order.cy.js
@@ -1,43 +1,62 @@
+import orderLines from "../../support/fragments/orders/orderLines";
+import ordersa from "../../support/a_ideyalabs/orders";
+import receiving from "../../support/fragments/receiving/receiving";
+import topMenu from "../../support/fragments/topMenu";
+import SettingsMenu from "../../support/fragments/settingsMenu";
+import titleLevelRequests from "../../support/fragments/settings/circulation/titleLevelRequests";
 
-import orderLines from '../../support/fragments/orders/orderLines';
-import ordersa from '../../support/a_ideyalabs/orders';
-import receiving from '../../support/fragments/receiving/receiving';
-import topMenu from '../../support/fragments/topMenu';
-import SettingsMenu from '../../support/fragments/settingsMenu';
-import titleLevelRequests from '../../support/fragments/settings/circulation/titleLevelRequests';
 
-describe('Orders app ', () => {
-  it('C378899-Encumbrance releases when receive piece for order with payment status ""Payment Not Required""', () => {
-    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+const OrderDetails={
+  searchByParameter:'PO line number',
+  EnterPoLineNumber:'20692-1',
+  checkOrderlineSearchResults:'20692-1',
+  TittleName:'Approve rolled',
+  Pieces1:'xys',
+  Pieces2:'Electronic',
+  PoLineNumber:'20692-1',
+  SelectFund:'Fund HBZ(HBZ)'
+}
+const PatronData={
+  notice1:'Test TLR',
+  notice2:'Request expired',
+  notice3:'Test TLR'
+}
+
+
+describe("Orders app ", () => {
+  xit('C378899-Encumbrance releases when receive piece for order with payment status ""Payment Not Required""', () => {
+    cy.login(Cypress.env("diku_login"), Cypress.env("diku_password"));
     cy.visit(topMenu.ordersPath);
     orderLines.clickOnOrderLines();
-    orderLines.searchByParameter('PO line number', '20692-1');
-    orderLines.checkOrderlineSearchResults('20692-1');
-    orderLines.selectOrderline('20692-1');
+    orderLines.searchByParameter(OrderDetails.searchByParameter, OrderDetails.EnterPoLineNumber);
+    orderLines.checkOrderlineSearchResults(OrderDetails.checkOrderlineSearchResults);
+    orderLines.selectOrderline(OrderDetails.checkOrderlineSearchResults);
     orderLines.receiveOrderLineViaActions();
-    orderLines.selectreceivedTitleName('Approve rolled');
-    receiving.addPieceProcess('xys', 'Electronic');
+    orderLines.selectreceivedTitleName(OrderDetails.TittleName);
+    receiving.addPieceProcess(OrderDetails.Pieces1, OrderDetails.Pieces2);
     receiving.quickReceivePiece();
+     receiving.clickOnPOLnumber(OrderDetails.PoLineNumber)
+     orderLines.selectFund(OrderDetails.SelectFund)
   });
-  it('C350428 Patron notice', () => {
+  it("C350428 Patron notice", () => {
     cy.visit(SettingsMenu.circulationTitleLevelRequestsPath);
 
-    titleLevelRequests.SelectConfirmationNoticeDropdown({
-      notice1: 'Test TLR',
-    }); // need to change the data "Test TLR"/Request expired
+    titleLevelRequests.SelectConfirmationNoticeDropdown({notice1:
+     PatronData.notice1}
+); //need to change the data "Test TLR"/Request expired
 
-    titleLevelRequests.SelectCancelleationNoticeDropdown({
-      notice2: 'Test TLR',
-    }); // need to change the data
+    titleLevelRequests.SelectCancelleationNoticeDropdown({notice2:
+      PatronData.notice2}
+    ); //need to change the data
 
-    titleLevelRequests.SelectExpirationNoticeDropdown({
-      notice3: 'Test TLR',
-    }); // need to change the data
+    titleLevelRequests.SelectExpirationNoticeDropdown({notice3:
+      PatronData.notice3}
+    ); //need to change the data
 
     titleLevelRequests.clickOnSaveButton();
   });
 
-  it('C365619 Re-exported Order contains more than two PO lines is successfully exported in the next scheduled run', () => {
+  it("C365619 Re-exported Order contains more than two PO lines is successfully exported in the next scheduled run", () => {
     cy.visit(topMenu.ordersPath);
     ordersa.switchToOrders();
     ordersa.status();
--- cypress/support/a_ideyalabs/orders.js
+++ cypress/support/a_ideyalabs/orders.js
@@ -1,36 +1,37 @@
-import { Button, Checkbox, Modal, Link, PaneContent, Section } from '../../../interactors';
+import { Button, Checkbox, Link, Modal, PaneContent, PaneHeader, Section } from "../../../interactors";
 
 
-const ordersTab = Button('Orders');
-const ordersPane = PaneContent({ id:'orders-filters-pane-content' });
-const statusDropDown = Button('Status');
-const openCheckBox = Checkbox('Open');
-const pONumber = Link('21211');
+const ordersTab =  Button('Orders');
+const ordersPane = PaneContent({id:"orders-filters-pane-content"});
+const statusDropDown = Button('Status')
+const openCheckBox = Checkbox('Open')
+const pONumber = Link("21211");
 const actionsBtn = Button('Actions');
-const ordersSection = Section({ id: 'order-details' });
-const reExportBtn = Button('Re-export');
-const reExportModal = Modal({ id:'reexport-order-confirm-modal' });
+const ordersSection = Section({id: "order-details"});
+const paneHeaderorderDetails = PaneHeader({id:"paneHeaderorder-details"})
+const orderDetailsActions = Section({id: "order-details-actions"})
+const reExportBtn = Button("Re-export");
+const reExportModal = Modal({id:"reexport-order-confirm-modal"})
 const confirmButton = Button('Confirm');
 
-export default {
-  switchToOrders:() => {
-    cy.do(ordersPane.find(ordersTab).click());
-  },
-  status:() => {
-    cy.do([ordersPane.find(statusDropDown).click(),
-      openCheckBox.click()]);
-  },
-  poNumberRecord:() => {
-    cy.xpath(pONumber).click();
-  },
+export default{
+    switchToOrders:()=>{
+        cy.do(ordersPane.find(ordersTab).click())
+    },
+    status:()=>{
+        cy.do([ordersPane.find(statusDropDown).click(),
+        openCheckBox.click()]); 
+    },
+    poNumberRecord:()=>{
+        cy.do(pONumber.click());
+    },
 
-  reExportActions() {
-    cy.do(ordersSection.find(actionsBtn).click());
-    cy.xpath(reExportBtn).click();
-  },
+    reExportActions() {
+        cy.do([ordersSection.find(paneHeaderorderDetails).find(actionsBtn).click(),
+        orderDetailsActions.find(reExportBtn).click()]);
+    },
 
-  reExportOrderModal: () => {
-    cy.do(reExportModal.find(confirmButton).click());
-  }
-
-};
+    reExportOrderModal: ()=>{
+        cy.do(reExportModal.find(confirmButton).click())
+    }
+}
