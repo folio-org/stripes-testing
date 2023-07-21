cee11159456873042c0f20f889eba4d9c52d296c
MaddiSwathiReddy <swathim@ideyalabs.com>
2023-07-21 18:45:16 +0530

    modified code

--- cypress/e2e/ideyalabs/users.cy.js
+++ cypress/e2e/ideyalabs/users.cy.js
@@ -1,55 +1,84 @@
-
-import topMenu from '../../support/fragments/topMenu';
-import users from '../../support/fragments/users/users';
+import topMenu from "../../support/fragments/topMenu";
+import users from "../../support/fragments/users/users";
+import ledgers from "../../support/fragments/finance/ledgers/ledgers";
+const RandomNumber = Math.floor(Math.random(9000) * 1000) + 1000;
 
 const userOne = {
-  patronGroup:'A1A1',
-  barcode:'237452322',
-  username:'shdusdh330',
-  personal:{
-    lastName:'somai',
-    firstName:'king1',
-    email:'dan@gmail.com'
-  }
+  patronGroup: "A1A1",
+  barcode: `237${RandomNumber}`,
+  username: `dan${RandomNumber}`,
+  personal: {
+    lastName: "somai",
+    firstName: "king1",
+    email: "dan@gmail.com",
+  },
 };
 const userTwo = {
-  patronGroup:'A1A1',
-  barcode:'23745',
-  username:'shdusdhA330',
-  personal:{
-    lastName:'somaid',
-    firstName:'king3',
-    email:'dan123@gmail.com'
-  }
+  patronGroup: "A1A1",
+  barcode: `237${RandomNumber}`,
+  username: `Son${RandomNumber}`,
+  personal: {
+    lastName: "somaid",
+    firstName: "king3",
+    email: "dan123@gmail.com",
+  },
+};
+const UserThree = {
+  patronGroup: "A1A1",
+  barcode: `2988${RandomNumber}`,
+  username: `Mann${RandomNumber}`,
+  personal: {
+    lastName: "Array",
+    firstName: "king1",
+    email: "dan@gmail.com",
+    middleName: "chary",
+    Preferredfirstname: "Dan basco1",
+  },
+};
+const VerifyData = {
+  verifyLastNameOnUserDetailsPane: "Array",
+  verifyPreferredfirstnameOnUserDetailsPane: "Dan basco1",
+  verifyFirstNameOnUserDetailsPane: "king1",
+  verifyMiddleNameOnUserDetailsPane: "chary",
 };
 
-describe('create a users', () => {
-  it('C421-Create: new user; required: contact info, email, phone, external system ID, address', () => {
-    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+describe("creat a users", () => {
+  it("C421-Create: new user; required: contact info, email, phone, external system ID, address", () => {
+    cy.login(Cypress.env("diku_login"), Cypress.env("diku_password"));
     cy.visit(topMenu.usersPath);
     users.createViaUi(userOne);
     cy.visit(topMenu.usersPath);
-    users.createViaUi(userTwo);// barcode
+    users.createViaUi(userTwo); //barcode
+    users.Assertion();
+    ledgers.closeOpenedPage();
+    users.Closewithoutsavingbutton();
   });
-  it('C11096-Add Preferred first name and confirm its display in the User record View and Edit screens', () => {
+  it("C11096-Add Preferred first name and confirm its display in the User record View and Edit screens", () => {
     cy.visit(topMenu.usersPath);
-    users.createData({ patronGroup: 'A1A1', barcode: '253123712819', username: 'aysgt26', personal: { lastName: 'Array', email: 'dan@gmail.com', middleName:'chary', firstName: 'king1', Preferredfirstname:'Dan basco1' } });
-    users.verifyPreferredfirstnameOnUserDetailsPane('Dan basco1');
-    users.verifyLastNameOnUserDetailsPane('Array');
-    users.verifyMiddleNameOnUserDetailsPane('chary');
+    users.createData(UserThree);
+    users.verifyPreferredfirstnameOnUserDetailsPane(
+      VerifyData.verifyPreferredfirstnameOnUserDetailsPane
+    );
+    users.verifyLastNameOnUserDetailsPane(
+      VerifyData.verifyLastNameOnUserDetailsPane
+    );
+    users.verifyMiddleNameOnUserDetailsPane(
+      VerifyData.verifyMiddleNameOnUserDetailsPane
+    );
     users.editButton();
     users.clearTextfield();
     users.SaveBtn();
-    users.verifyLastNameOnUserDetailsPane('Array');
-    users.verifyMiddleNameOnUserDetailsPane('chary');
-    users.verifyFirstNameOnUserDetailsPane('king1');
+    users.verifyLastNameOnUserDetailsPane(
+      VerifyData.verifyLastNameOnUserDetailsPane
+    );
+    users.verifyMiddleNameOnUserDetailsPane(
+      VerifyData.verifyMiddleNameOnUserDetailsPane
+    );
+    users.verifyFirstNameOnUserDetailsPane(
+      VerifyData.verifyFirstNameOnUserDetailsPane
+    );
     users.editButton();
     users.clearTextfieldfirstName();
     users.SaveBtn();
   });
 });
-
-
-
-
-
--- cypress/support/fragments/users/users.js
+++ cypress/support/fragments/users/users.js
@@ -1,72 +1,129 @@
-import { including } from '@interactors/html';
-import getRandomPostfix from '../../utils/stringTools';
+import { including } from "@interactors/html";
 import {
-  Button,
-  Select,
-  TextField,
-  Pane,
-  Dropdown,
-  MultiColumnListCell,
-  KeyValue,
   Accordion,
-} from '../../../../interactors';
-
-const userDetailsPane = Pane({ id: 'pane-userdetails' });
-const contactInformationAccordion = Accordion('Contact information');
+  Button,
+  Dropdown,
+  KeyValue,
+  Link,
+  MultiColumnListCell,
+  Pane,
+  PaneHeader,
+  Section,
+  Select,
+  TextField
+} from "../../../../interactors";
+import getRandomPostfix from "../../utils/stringTools";
+const errorMsg = TextField({ error: "This barcode has already been taken" });
+const closeButton = Button("Close");
+const cancelButton = Button({ id: "clickable-cancel" });
+const userDetailsPane = Pane({ id: "pane-userdetails" });
+const ClosedLoancheckboxclick = "//input[@name='Closed loan']";
+const contactInformationAccordion = Accordion("Contact information");
 const defaultUserName = `AutotestUser${getRandomPostfix()}`;
+const ActionButton = "//span[text()='Actions']";
+const newButton = Button("New");
+const editButton = Button("Edit");
+const checkbox = "//input[@name='check-all']";
+const BarCode = TextField("Barcode");
+const Closewithoutsavingbutton = Button({
+  id: "clickable-cancel-editing-confirmation-cancel",
+});
+
 const defaultUser = {
   username: defaultUserName,
   active: true,
   // should be defined
   barcode: undefined,
   personal: {
-    preferredContactTypeId: '002',
-    firstName: 'testPermFirst',
-    middleName: 'testMiddleName',
+    preferredContactTypeId: "002",
+    firstName: "testPermFirst",
+    middleName: "testMiddleName",
     lastName: defaultUserName,
-    email: 'test@folio.org',
+    email: "test@folio.org",
   },
   // should be defined
   patronGroup: undefined,
-  departments: []
+  departments: [],
 };
 
 export default {
   defaultUser,
 
-  createViaApi: (user) => cy.okapiRequest({
-    method: 'POST',
-    path: 'users',
-    body: user,
-    isDefaultSearchParamsRequired: false
-  }).then(response => ({ id: response.body.id,
-    username: response.body.username,
-    barcode:  response.body.barcode,
-    lastName: response.body.personal.lastName,
-    firstName : response.body.personal.firstName,
-    middleName : response.body.personal.middleName })),
-
-  deleteViaApi:(userId) => cy.okapiRequest({
-    method: 'DELETE',
-    path: `bl-users/by-id/${userId}`,
-    isDefaultSearchParamsRequired : false
-  }),
-
-  createViaUi: (userData) => {
-    return cy.do([
-      Dropdown('Actions').find(Button()).click(),
-      Button({ id: 'clickable-newuser' }).click(),
-      TextField({ id: 'adduser_lastname' }).fillIn(userData.personal.lastName),
-      Select({ id: 'adduser_group' }).choose(userData.patronGroup),
-      TextField({ name: 'barcode' }).fillIn(userData.barcode),
-      TextField({ name: 'username' }).fillIn(userData.username),
-      TextField({ id: 'adduser_email' }).fillIn(userData.personal.email),
-      Button({ id: 'clickable-save' }).click()]).then(() => {
-      cy.intercept('/users').as('user');
-      return cy.wait('@user', { timeout: 80000 }).then((xhr) => xhr.response.body.id);
-    });
+  createViaApi: (user) =>
+    cy
+      .okapiRequest({
+        method: "POST",
+        path: "users",
+        body: user,
+        isDefaultSearchParamsRequired: false,
+      })
+      .then((response) => ({
+        id: response.body.id,
+        username: response.body.username,
+        barcode: response.body.barcode,
+        lastName: response.body.personal.lastName,
+        firstName: response.body.personal.firstName,
+        middleName: response.body.personal.middleName,
+      })),
+  Assertion: () => {
+    cy.do(BarCode.has({ error: "This barcode has already been taken" }));
+  },
+  Closewithoutsavingbutton: () => {
+    cy.do([Closewithoutsavingbutton.click()]);
   },
 
+  deleteViaApi: (userId) =>
+    cy.okapiRequest({
+      method: "DELETE",
+      path: `bl-users/by-id/${userId}`,
+      isDefaultSearchParamsRequired: false,
+    }),
+
+  createViaUi: (userData) => {
+    return cy
+      .do([
+        Dropdown("Actions").find(Button()).click(),
+        Button({ id: "clickable-newuser" }).click(),
+        TextField({ id: "adduser_lastname" }).fillIn(
+          userData.personal.lastName
+        ),
+        TextField({ id: "adduser_firstname" }).fillIn(
+          userData.personal.firstName
+        ),
+        Select({ id: "adduser_group" }).choose(userData.patronGroup),
+        TextField({ name: "barcode" }).fillIn(userData.barcode),
+        TextField({ name: "username" }).fillIn(userData.username),
+        TextField({ id: "adduser_email" }).fillIn(userData.personal.email),
+        Button({ id: "clickable-save" }).click(),
+      ])
+      .then(() => {
+        cy.intercept("/users").as("user");
+        return cy
+          .wait("@user", { timeout: 800000 })
+          .then((xhr) => xhr.response.body.id);
+      });
+  },
+  createData: (userData) => {
+    return cy.do([
+      Dropdown("Actions").find(Button()).click(),
+      Button({ id: "clickable-newuser" }).click(),
+      TextField({ id: "adduser_lastname" }).fillIn(userData.personal.lastName),
+      TextField({ id: "adduser_middlename" }).fillIn(
+        userData.personal.middleName
+      ),
+      TextField({ id: "adduser_firstname" }).fillIn(
+        userData.personal.firstName
+      ),
+      Select({ id: "adduser_group" }).choose(userData.patronGroup),
+      TextField({ name: "barcode" }).fillIn(userData.barcode),
+      TextField({ name: "username" }).fillIn(userData.username),
+      TextField({ id: "adduser_email" }).fillIn(userData.personal.email),
+      TextField({ id: "adduser_preferredname" }).fillIn(
+        userData.personal.Preferredfirstname
+      ),
+      Button({ id: "clickable-save" }).click(),
+    ]);
+  },
   checkIsUserCreated: (userData) => {
     cy.expect(Pane(userData.personal.lastName).exists());
   },
@@ -79,35 +136,131 @@ export default {
   },
 
   checkPatronIsNotBlocked: (userId) => {
-    cy.intercept(`/automated-patron-blocks/${userId}`).as('patronBlockStatus');
-    cy.wait('@patronBlockStatus', { timeout: 10000 }).then(xhr => {
-      cy.wrap(xhr.response.body.automatedPatronBlocks.length).should('eq', 0);
-      cy.expect(TextField({ value:'Patron has block(s) in place' }).absent());
+    cy.intercept(`/automated-patron-blocks/${userId}`).as("patronBlockStatus");
+    cy.wait("@patronBlockStatus", { timeout: 10000 }).then((xhr) => {
+      cy.wrap(xhr.response.body.automatedPatronBlocks.length).should("eq", 0);
+      cy.expect(TextField({ value: "Patron has block(s) in place" }).absent());
     });
   },
 
   verifyFirstNameOnUserDetailsPane(firstName) {
-    cy.expect(userDetailsPane.find(KeyValue('First name')).has({ value: `${firstName}` }));
+    cy.expect(
+      userDetailsPane
+        .find(KeyValue("First name"))
+        .has({ value: `${firstName}` })
+    );
   },
-
-  verifyMiddleNameOnUserDetailsPane(firstName) {
-    cy.expect(userDetailsPane.find(KeyValue('Middle name')).has({ value: `${firstName}` }));
+  verifyLastNameOnUserDetailsPane(lastName) {
+    cy.expect(
+      userDetailsPane.find(KeyValue("Last name")).has({ value: `${lastName}` })
+    );
+  },
+  verifyMiddleNameOnUserDetailsPane(middleName) {
+    cy.expect(
+      userDetailsPane
+        .find(KeyValue("Middle name"))
+        .has({ value: `${middleName}` })
+    );
+  },
+  verifyPreferredfirstnameOnUserDetailsPane(Preferredfirstname) {
+    cy.expect(
+      userDetailsPane
+        .find(KeyValue("Preferred first name"))
+        .has({ value: `${Preferredfirstname}` })
+    );
   },
 
   verifyPatronGroupOnUserDetailsPane(patronGroup) {
-    cy.expect(userDetailsPane.find(KeyValue('Patron group')).has({ value: `${patronGroup}` }));
+    cy.expect(
+      userDetailsPane
+        .find(KeyValue("Patron group"))
+        .has({ value: `${patronGroup}` })
+    );
   },
 
   verifyEmailDomainOnUserDetailsPane(emailDomain) {
     cy.do(contactInformationAccordion.clickHeader());
-    cy.expect(userDetailsPane.find(KeyValue('Email')).has({ value: including(`@${emailDomain}`) }));
+    cy.expect(
+      userDetailsPane
+        .find(KeyValue("Email"))
+        .has({ value: including(`@${emailDomain}`) })
+    );
   },
 
   verifyExpirationDateOnUserDetailsPane(expirationDate) {
-    cy.expect(userDetailsPane.find(KeyValue('Expiration date')).has({ value: `${expirationDate}` }));
+    cy.expect(
+      userDetailsPane
+        .find(KeyValue("Expiration date"))
+        .has({ value: `${expirationDate}` })
+    );
+  },
+  closeDetailsPane: () => {
+    cy.do([cy.xpath(closeButton).click()]);
+  },
+  CancelButton: () => {
+    cy.do([
+      cancelButton.click(),
+      //Closewithoutsavingbutton.click({ multiple: true })
+    ]);
+  },
+  selectFirstUser: (userName) => {
+    cy.do(Pane({ id: "list-users" }).find(Link(userName)).click());
+  },
+  selectUserByName: (Barcode) => {
+    cy.do([MultiColumnListCell(Barcode).click({ row: 0, columnIndex: 2 })]);
+  },
+  checkErrorMessage: () => {
+    cy.expect(errorMsg.has({ error: "This barcode has already been taken" }));
+  },
+  clearTextfield() {
+    cy.do(TextField({ id: "adduser_preferredname" }).clear());
+  },
+  clearTextfieldfirstName() {
+    cy.do(TextField({ id: "adduser_firstname" }).clear());
+  },
+  clickOnLoans: () => {
+    cy.do([Button({ id: "accordion-toggle-button-loansSection" }).click()]);
+  },
+  EntertheitemBarcodeSearchFiled(Barcode) {
+    cy.do([TextField({ id: "text-input-9" }).fillIn(Barcode)]);
+  },
+  clickOnOpenLoans: () => {
+    cy.do([Button({ id: "clickable-viewcurrentloans" }).click()]);
+  },
+  clickOnCirculationLoanFilterDropDown: () => {
+    cy.do([Button({ id: "accordion-toggle-button-loan" }).click()]);
+  },
+  clickOncheckbox: () => {
+    cy.do([cy.xpath(checkbox).click()]);
+  },
+  clickOnClosedLoanCheckBox: () => {
+    cy.do([cy.xpath(ClosedLoancheckboxclick).click()]);
   },
 
-  verifyCustomFieldOnUserDetailsPane(name, text) {
-    cy.expect(userDetailsPane.find(KeyValue(name)).has({ value: text }));
+  clickOnXButton() {
+    cy.do([Button({ id: "clickable-closenewuserdialog" }).click()]);
+  },
+  clickOnResetFilterText() {
+    cy.do([Button({ id: "reset-receiving-filters" }).click()]);
+  },
+  SaveBtn() {
+    cy.do([Button({ id: "clickable-save" }).click()]);
+  },
+
+  ActionsViaNew: () => {
+    cy.do([
+      cy.xpath(ActionButton).click({ multiple: true }),
+      newButton.click({ multiple: true }),
+    ]);
+  },
+
+  editButton: () => {
+    cy.do([
+      Section({ id: "pane-userdetails" })
+        .find(PaneHeader({ id: "paneHeaderpane-userdetails" }))
+        .find(Button("Actions"))
+        .click(),
+      editButton.click(),
+    ]);
   },
 };
