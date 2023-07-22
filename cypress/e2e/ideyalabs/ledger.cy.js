39eba8f91ef83ff1c6441e5025865fb9ea2451f9
MaddiSwathiReddy <swathim@ideyalabs.com>
2023-07-22 15:34:03 +0530

    code refactor

--- cypress/e2e/ideyalabs/ledger.cy.js
+++ cypress/e2e/ideyalabs/ledger.cy.js
@@ -7,41 +7,70 @@ import funds from '../../support/fragments/finance/funds/funds';
 import groups from '../../support/fragments/finance/groups/groups';
 
 const testData = {
-  Fiscalname : 'Fiscal Year 2024',
-  Selectname:'Fiscal Year 2024',
-  Ledgername:'Future',
-  SelectLedgerName:'Future',
-  GroupsName:'Test N1',
-  SelectGroupName:'Test N1',
-  FundName:'AA1',
-  SelectFundName:'AA1',
-  SearchByInvoiceName:'12344',
-  SelectSearchByInvoiceNameRecord:'12344',
-  SelectInvoiceLineNumber:'12320886456-1',
-  searchByFinanceName:'Ledger NIX 1',
-  SelectsearchByFinanceNameRecord:'Ledger NIX 1',
-  FillRolloverFiscalYearINFo:'NIX2024',
-  Ledgername2:'Ledger NIX 1',
-  SelectLedgerName2:'Ledger NIX 1',
-  SearchByParameterFiscalYear:'Name',
-  SearchByNameFiscalYesar:'AA2023',
-  selectFirstFiscalRecord:'AA2023',
-  SearchByParameterFiscalYear2:'Name',
-  SearchByNameFiscalYear2:'AA2024',
-  SelectSecondFiscalRecord:'AA2024',
-  SearchByInvoiceName2:'12344',
-  SelectSearchByInvoiceNameRecord2:'12344',
+  fiscalName : 'Fiscal Year 2024',
+  selectName:'Fiscal Year 2024',
+  ledgerName:'Future',
+  selectLedgerName:'Future',
+  groupsName:'Test N1',
+  selectGroupName:'Test N1',
+  fundName:'AA1',
+  selectFundName:'AA1',
+  searchByInvoiceName:'12344',
+  selectSearchByInvoiceNameRecord:'12344',
   selectInvoiceLineNumber:'12320886456-1',
-  SelectFundIDFromthelist:'Fund A(A)',
-  SelectCurrentBudgerFromthelist:'A-FYA2023',
-  SelectTransactionListDetailsResultsFromCurrentBudget:'3/6/2023, 7:48 AM',
-  SelectviewDetailsPreviousBudgets:'BFYRO-FYRO2021',
-  SearchFundName:'AF2',
-  SelectFundNameRecordList:'AF2',
-  SelectCurrentBudgerFromthelistFunds:'AF2-AF2024',
-  SelectTransactionListDetailsResultsFromEmbaranceDate:'6/20/2023, 5:39 AM',
-  ViewDetailsPreviousBudgetsRecord:'AF2-AF2021'
+  searchByFinanceName:'Ledger NIX 1',
+  selectsearchByFinanceNameRecord:'Ledger NIX 1',
+  fillRolloverFiscalYearINFo:'NIX2024',
+  ledgername2:'Ledger NIX 1',
+  selectLedgerName2:'Ledger NIX 1',
+  searchByParameterFiscalYear:'Name',
+  searchByNameFiscalYesar:'AA2023',
+  selectFirstFiscalRecord:'AA2023',
+  searchByParameterFiscalYear2:'Name',
+  searchByNameFiscalYear2:'AA2024',
+  selectSecondFiscalRecord:'AA2024',
+  searchByInvoiceName2:'12344',
+  selectSearchByInvoiceNameRecord2:'12344',
+  selectFundIDFromthelist:'Fund A(A)',
+  selectCurrentBudgerFromthelist:'A-FYA2023',
+  selectTransactionListDetailsResultsFromCurrentBudget:'3/6/2023, 7:48 AM',
+  selectviewDetailsPreviousBudgets:'BFYRO-FYRO2021',
+  searchFundName:'AF2',
+  selectFundNameRecordList:'AF2',
+  selectCurrentBudgerFromthelistFunds:'AF2-AF2024',
+  selectTransactionListDetailsResultsFromEmbaranceDate:'6/20/2023, 5:39 AM',
+  viewDetailsPreviousBudgetsRecord:'AF2-AF2021'
 };
+const rollOverData={
+  ledgerName:'AE2',
+  selectLedger:'AE2',
+  fiscalYearDate:'AE2025',
+  rollOverDate:"7/21/2023",
+  searchByParameter:"Name",
+  searchByName:'AE2',
+  selectFundRecord:'AE2',
+  searchledger:'Test N8',
+  selectLedgerName:'Test N8',
+  fillInRolloverInfo:'FYG2222',
+  currentBudeget:'Test N11-FYG2024',
+  plannedBudget:'Test N11-FYG1111',
+  selectEmbranceResult:'6/20/2023, 4:41 AM'
+}
+
+const encumbranceData= {
+  searchByName:'autotest_ledger_275.5001376680388208',
+  selectFirstLedger:'autotest_ledger_275.5001376680388208' ,
+  selectFirstCheckBox:'FY2024',
+  rollOverDate:"7/21/2023",
+  fillInRolloverInfo:'FY2029',
+  ledgerName:'AJ',
+  selectLedgerName:'AJ',
+  fundName:'AJ2',
+  selectFund:'AJ2',
+  selectCurrentBudgerFromthelist:'AJ2-AJ2024',
+  transactionListDetailsResultsFromEmbarance:'7/3/2023, 8:12 AM'
+}
+
 describe('Users-loans App', () => {
   it('login to Folio', () => {
     cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
@@ -50,127 +79,126 @@ describe('Users-loans App', () => {
   it('C377030-Available balance"" is displayed as a negative number when running a deficit', () => {
     cy.visit(topMenu.financePath);
     fiscalYears.clickOnFiscialYear();
-    financeHelper.searchByName(testData.Fiscalname);
-    financeHelper.selectFirstFinace(testData.Selectname);
+    financeHelper.searchByName(testData.fiscalName);
+    financeHelper.selectFirstFinace(testData.selectName);
     fiscalYears.clickOnLedgerTab();
-    financeHelper.searchByName(testData.Ledgername);
-    ledgers.selectLedger(testData.SelectLedgerName);
+    financeHelper.searchByName(testData.ledgerName);
+    ledgers.selectLedger(testData.selectLedgerName);
     groups.clickOnGroupTab();
-    groups.searchByName(testData.GroupsName);
-    groups.selectGroup(testData.SelectGroupName);
+    groups.searchByName(testData.groupsName);
+    groups.selectGroup(testData.selectGroupName);
     funds.clickOnFundsTab();
-    funds.searchByName(testData.FundName);
-    funds.selectFund(testData.SelectFundName);
+    funds.searchByName(testData.fundName);
+    funds.selectFund(testData.selectFundName);
     funds.selectBudgetDetails();
   });
   it('C396360-Save invoice fiscal year after fund distribution change to fund using different ledger if FY was undefined', () => {
     cy.visit(topMenu.invoicesPath);
-    invoices.searchByNumber(testData.SearchByInvoiceName);
-    invoices.clickOnFirstInvoicesResultList(testData.SelectSearchByInvoiceNameRecord);
-    invoices.selectInvoiceLine(testData.SelectInvoiceLineNumber);
+    invoices.searchByNumber(testData.searchByInvoiceName);
+    invoices.clickOnFirstInvoicesResultList(testData.selectSearchByInvoiceNameRecord);
+    invoices.selectInvoiceLine(testData.selectInvoiceLineNumber);
     invoices.editInvoiceLine();
-    invoices.SelectFundID(); // need to modified FundID
+    invoices.SelectFundID();
     invoices.editVendorInvoiceNumber();
     cy.visit(topMenu.financePath);
     financeHelper.searchByName(testData.searchByFinanceName);
-    financeHelper.selectFirstLedger(testData.SelectsearchByFinanceNameRecord);
+    financeHelper.selectFirstLedger(testData.selectsearchByFinanceNameRecord);
     ledgers.clickonViewledgerDetails();
-    // ledgers.rollover()
-    // ledgers.fillInRolloverInfo(testData.FillRolloverFiscalYearINFo);
     ledgers.resetAll();
-    financeHelper.searchByName(testData.Ledgername2);
-    financeHelper.selectFirstLedger(testData.SelectLedgerName2);
+    financeHelper.searchByName(testData.ledgername2);
+    financeHelper.selectFirstLedger(testData.selectLedgerName2);
     ledgers.clickOnFiscalyearTab();
-    invoices.searchByParameter(testData.SearchByParameterFiscalYear, testData.SearchByNameFiscalYesar);
+    invoices.searchByParameter(testData.searchByParameterFiscalYear, testData.searchByNameFiscalYesar);
     financeHelper.selectFirstFiscalRecord(testData.selectFirstFiscalRecord);
     fiscalYears.EditFiscalYEarDetails();
-    fiscalYears.FilltheStartAndEndDateoncalenderstartDateField1(); // need to change the startdate and end date
+    fiscalYears.FilltheStartAndEndDateoncalenderstartDateField1();
     ledgers.resetAll();
-    invoices.searchByParameter(testData.SearchByParameterFiscalYear2, testData.SearchByNameFiscalYear2);
-    financeHelper.selectFirstFiscalRecord(testData.SelectSecondFiscalRecord);
+    invoices.searchByParameter(testData.searchByParameterFiscalYear2, testData.searchByNameFiscalYear2);
+    financeHelper.selectFirstFiscalRecord(testData.selectSecondFiscalRecord);
     fiscalYears.EditFiscalYEarDetails();
-    fiscalYears.FilltheStartAndEndDateoncalenderstartDateField2(); // need to change the startdate and end date
+    fiscalYears.FilltheStartAndEndDateoncalenderstartDateField2();
     cy.visit(topMenu.invoicesPath);
-    invoices.searchByNumber(testData.SearchByInvoiceName2);
-    invoices.clickOnFirstInvoicesResultList(testData.SelectSearchByInvoiceNameRecord2);
-    invoices.approveInvoice();// it is not getting approved so here script will be failed.
+    invoices.searchByNumber(testData.searchByInvoiceName2);
+    invoices.clickOnFirstInvoicesResultList(testData.selectSearchByInvoiceNameRecord2);
+    invoices.approveInvoice();// API getting failed
     invoices.selectInvoiceLine(testData.selectInvoiceLineNumber);
-    invoices.SelectFundIDFromthelist(testData.SelectFundIDFromthelist);
-    invoices.SelectCurrentBudgerFromthelist(testData.SelectCurrentBudgerFromthelist);
+    invoices.SelectFundIDFromthelist(testData.selectFundIDFromthelist);
+    invoices.SelectCurrentBudgerFromthelist(testData.selectCurrentBudgerFromthelist);
     invoices.ClickOnViewTransactionsHyperText();
-    invoices.TransactionListDetailsResultsFromCurrentBudget(testData.SelectTransactionListDetailsResultsFromCurrentBudget);
+    invoices.TransactionListDetailsResultsFromCurrentBudget(testData.selectTransactionListDetailsResultsFromCurrentBudget);
     invoices.CloseTwoXmarkInOneScreen();
     ledgers.closeOpenedPage();
     ledgers.closeOpenedPage();
-    invoices.viewDetailsPreviousBudgets(testData.SelectviewDetailsPreviousBudgets);
+    invoices.viewDetailsPreviousBudgets(testData.selectviewDetailsPreviousBudgets);
     invoices.ClickOnViewTransactionsHyperText();
     invoices.TransactionListDetailsResultsFromPreviousBudget();
     cy.visit(topMenu.financePath);
     ledgers.clickOnFundTab();
-    invoices.searchByParameter('Name', testData.SearchFundName);
-    financeHelper.selectFirstFundRecord(testData.SelectFundNameRecordList);
-    invoices.SelectCurrentBudgerFromthelist(testData.SelectTransactionListDetailsResultsFromEmbaranceDate);
+    invoices.searchByParameter('Name', testData.searchFundName);
+    financeHelper.selectFirstFundRecord(testData.selectFundNameRecordList);
+    invoices.SelectCurrentBudgerFromthelist(testData.selectTransactionListDetailsResultsFromEmbaranceDate);
     invoices.ClickOnViewTransactionsHyperText();
-    invoices.TransactionListDetailsResultsFromEmbarance(testData.SelectTransactionListDetailsResultsFromEmbaranceDate);
+    invoices.TransactionListDetailsResultsFromEmbarance(testData.selectTransactionListDetailsResultsFromEmbaranceDate);
     ledgers.closeOpenedPage();
     ledgers.closeOpenedPage();
-    invoices.viewDetailsPreviousBudgets(testData.ViewDetailsPreviousBudgetsRecord);
+    invoices.viewDetailsPreviousBudgets(testData.viewDetailsPreviousBudgetsRecord);
     invoices.ClickOnViewTransactionsHyperText();
     invoices.TransactionListDetailsResultsFromPreviousBudgetEmbrance();
   });
-
   it('C359604-Make more than one preview for one ledger and same fiscal year with ""Test rollover"", check test rollover results', () => {
-    // cy.login(Cypress.env("diku_login"), Cypress.env("diku_password"));
     cy.visit(topMenu.financePath);
-    financeHelper.searchByName('AE2');
-    financeHelper.selectFirstLedger('AE2');
+    financeHelper.searchByName(rollOverData.ledgerName);
+    financeHelper.selectFirstLedger(rollOverData.selectLedger);
     ledgers.clickonViewledgerDetails();
     ledgers.rollover();
-    ledgers.selectFirstCheckBox('AE2025');
+    ledgers.selectFirstCheckBox(rollOverData.FiscalYearDate);
     ledgers.clickonViewledgerDetails();
     ledgers.rolloverLogs();
-    // invoices.TransactionListDetailsResultsFromRolledLogs();
+    ledgers.exportRollover(rollOverData.rollOverDate);
     ledgers.closeOpenedPage();
     ledgers.clickOnFundTab();
-    invoices.searchByParameter('Name', 'AE2');
-    financeHelper.selectFirstFundRecord('AE2');
+    invoices.searchByParameter(rollOverData.searchByParameter, rollOverData.searchByName);
+    financeHelper.selectFirstFundRecord(rollOverData.selectFundRecord);
     ledgers.clickOnFiscalyearTab();
     ledgers.clickOnLedgerTab();
-    financeHelper.searchByName('Test N8');
-    financeHelper.selectFirstLedger('Test N8');
+    financeHelper.searchByName(rollOverData.searchledger);
+    financeHelper.selectFirstLedger(rollOverData.selectLedgerName);
     ledgers.clickonViewledgerDetails();
     ledgers.rollover();
-    ledgers.fillInRolloverInfo('FYG1111');
+    ledgers.fillInRolloverInfo(rollOverData.fillInRolloverInfo);
     ledgers.clickonViewledgerDetails();
     ledgers.selectFund();
-    invoices.SelectCurrentBudgerFromthelist('Test N11-FYG2024');
+    invoices.SelectCurrentBudgerFromthelist(rollOverData.currentBudeget);
     ledgers.closeOpenedPage();
-    invoices.viewDetailsPlannedBudgets('Test N11-FYG1111');
+    invoices.viewDetailsPlannedBudgets(rollOverData.plannedBudget);
     invoices.ClickOnViewTransactionsHyperText();
-    invoices.TransactionListDetailsResultsFromEmbarance('6/20/2023, 4:41 AM');
+    invoices.TransactionListDetailsResultsFromEmbarance(rollOverData.selectEmbranceResult);
   });
-  it('C375267-Encumbrances are rolled over correctly when order fund distribution was changed and related paid invoice exists (based on Remaining)', () => {
-    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+  it("C375267-Encumbrances are rolled over correctly when order fund distribution was changed and related paid invoice exists (based on Remaining)", () => {
     cy.visit(topMenu.financePath);
-    financeHelper.searchByName('autotest_ledger_275.5001376680388208');
-    financeHelper.selectFirstLedger('autotest_ledger_275.5001376680388208');
+    financeHelper.searchByName(encumbranceData.searchByName);
+    financeHelper.selectFirstLedger(encumbranceData.selectFirstLedger);
     ledgers.clickonViewledgerDetails();
     ledgers.rollover();
-    ledgers.selectFirstCheckBox('FY2024');
+    ledgers.selectFirstCheckBox(encumbranceData.selectFirstCheckBox);
     ledgers.clickonViewledgerDetails();
     ledgers.rolloverLogs();
+    ledgers.exportRollover(encumbranceData.rollOverDate);
     ledgers.closeOpenedPage();
     ledgers.clickonViewledgerDetails();
-    ledgers.clickonRollerlogResult('07/11/2023-result');
     ledgers.rollover();
-    ledgers.fillInRolloverInfo('FY2022');
+    ledgers.fillInRolloverInfo(encumbranceData.fillInRolloverInfo);
     ledgers.clickonViewledgerDetails();
     ledgers.resetAll();
-    financeHelper.searchByName('AJ');
-    financeHelper.selectFirstLedger('AJ');
+    financeHelper.searchByName(encumbranceData.ledgerName);
+    financeHelper.selectFirstLedger(encumbranceData.selectLedgerName);
     ledgers.selectFund();
     ledgers.closeOpenedPage();
-    financeHelper.searchByName('AJ2');
-    financeHelper.selectFirstFundRecord('AJ2');
+    financeHelper.searchByName(encumbranceData.fundName);
+    financeHelper.selectFirstFundRecord(encumbranceData.selectFund);
+    invoices.SelectCurrentBudgerFromthelist(encumbranceData.selectCurrentBudgerFromthelist);
+    invoices.ClickOnViewTransactionsHyperText();
+    invoices.TransactionListDetailsResultsFromEmbarance(encumbranceData.transactionListDetailsResultsFromEmbarance);
   });
 });
+
