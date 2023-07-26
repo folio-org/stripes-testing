98f273cc33e31edd492af37de99b2a122b505582
santosh <santoshp@ideyalabs.com>
2023-07-26 19:57:15 +0530

    modified files

--- cypress/e2e/ideyalabs/order.cy.js
+++ cypress/e2e/ideyalabs/order.cy.js
@@ -1,29 +1,29 @@
-import order from "../../support/a_ideyalabs/orders"
-import orderFragment from "../../support/fragments/orders/orders";
-import orderLines from "../../support/fragments/orders/orderLines";
-import receiving from "../../support/fragments/receiving/receiving";
-import titleLevelRequests from "../../support/fragments/settings/circulation/titleLevelRequests";
-import SettingsMenu from "../../support/fragments/settingsMenu";
-import topMenu from "../../support/fragments/topMenu";
+import order from '../../support/a_ideyalabs/orders';
+import orderFragment from '../../support/fragments/orders/orders';
+import orderLines from '../../support/fragments/orders/orderLines';
+import receiving from '../../support/fragments/receiving/receiving';
+import titleLevelRequests from '../../support/fragments/settings/circulation/titleLevelRequests';
+import SettingsMenu from '../../support/fragments/settingsMenu';
+import topMenu from '../../support/fragments/topMenu';
 
 const orderDetails = {
-  searchByParameter: "PO line number",
-  enterPoLineNumber: "20692-1",
-  checkOrderLineSearchResults: "20692-1",
-  titleName: "Approve rolled",
-  caption: "Done",
-  enumeration: "Electronic",
-  poLineNumber: "20692-1",
+  searchByParameter: 'PO line number',
+  enterPoLineNumber: '20692-1',
+  checkOrderLineSearchResults: '20692-1',
+  titleName: 'Approve rolled',
+  caption: 'Done',
+  enumeration: 'Electronic',
+  poLineNumber: '20692-1',
 };
 const patronData = {
-  notice1: "Request expired",
-  notice2: "Test TLR",
-  notice3: "Requested item - available",
+  notice1: 'Request expired',
+  notice2: 'Test TLR',
+  notice3: 'Requested item - available',
 };
 
-describe("Orders app ", () => {
+describe('Orders app ', () => {
   it('C378899-Encumbrance releases when receive piece for order with payment status ""Payment Not Required""', () => {
-    cy.login(Cypress.env("diku_login"), Cypress.env("diku_password"));
+    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
     cy.visit(topMenu.ordersPath);
     orderLines.clickOnOrderLines();
     orderLines.searchByParameter(
@@ -39,10 +39,10 @@ describe("Orders app ", () => {
     receiving.addPieceProcess(orderDetails.caption, orderDetails.enumeration);
     receiving.quickReceivePiece();
     receiving.clickOnPOLnumber(orderDetails.poLineNumber);
-    orderFragment.selectFundIDFromthelist()
+    orderFragment.selectFundIDFromthelist();
   });
 
-  it("C350428 Patron notice", () => {
+  it('C350428 Patron notice', () => {
     cy.visit(SettingsMenu.circulationTitleLevelRequestsPath);
     titleLevelRequests.SelectConfirmationNoticeDropdown({
       notice1: patronData.notice1,
@@ -55,8 +55,8 @@ describe("Orders app ", () => {
     });
     titleLevelRequests.clickOnSaveButton();
   });
-  
-  it("C365619 Re-exported Order contains more than two PO lines is successfully exported in the next scheduled run", () => {
+
+  it('C365619 Re-exported Order contains more than two PO lines is successfully exported in the next scheduled run', () => {
     cy.visit(topMenu.ordersPath);
     order.switchToOrders();
     order.status();
--- cypress/support/a_ideyalabs/orders.js
+++ cypress/support/a_ideyalabs/orders.js
@@ -1,25 +1,25 @@
 import {
-    Button,
-    Checkbox,
-    Link,
-    Modal,
-    PaneContent,
-    PaneHeader,
-    Section,
-} from "../../../interactors";
+  Button,
+  Checkbox,
+  Link,
+  Modal,
+  PaneContent,
+  PaneHeader,
+  Section,
+} from '../../../interactors';
 
-const ordersTab = Button("Orders");
-const ordersPane = PaneContent({ id: "orders-filters-pane-content" });
-const statusDropDown = Button("Status");
-const openCheckBox = Checkbox("Open");
-const poNumber = Link("21211");
-const actionsButton = Button("Actions");
-const ordersSection = Section({ id: "order-details" });
-const paneHeaderOrderDetails = PaneHeader({ id: "paneHeaderorder-details" });
-const orderDetailsActions = Section({ id: "order-details-actions" });
-const reExportButton = Button("Re-export");
-const reExportModal = Modal({ id: "reexport-order-confirm-modal" });
-const confirmButton = Button("Confirm");
+const ordersTab = Button('Orders');
+const ordersPane = PaneContent({ id: 'orders-filters-pane-content' });
+const statusDropDown = Button('Status');
+const openCheckBox = Checkbox('Open');
+const poNumber = Link('21211');
+const actionsButton = Button('Actions');
+const ordersSection = Section({ id: 'order-details' });
+const paneHeaderOrderDetails = PaneHeader({ id: 'paneHeaderorder-details' });
+const orderDetailsActions = Section({ id: 'order-details-actions' });
+const reExportButton = Button('Re-export');
+const reExportModal = Modal({ id: 'reexport-order-confirm-modal' });
+const confirmButton = Button('Confirm');
 
 export default {
   switchToOrders: () => {
