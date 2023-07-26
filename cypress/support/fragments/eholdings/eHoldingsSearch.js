786960c39ad067f9d22a4250753de5275323725e
pavithradasari <pavithrad@ideyalabs.com>
2023-07-27 02:38:21 +0530

    eHolding Search changes

--- cypress/support/fragments/eholdings/eHoldingsSearch.js
+++ cypress/support/fragments/eholdings/eHoldingsSearch.js
@@ -3,9 +3,7 @@ import { Button } from '../../../../interactors';
 
 export default {
   switchToTitles: () => {
-    cy.intercept('/tags?**').as('getTags');
     cy.do(Button({ id: 'titles-tab' }).click());
-    cy.wait('@getTags');
   },
   switchToPackages: () => {
     cy.do(Button({ id: 'packages-tab' }).click());
