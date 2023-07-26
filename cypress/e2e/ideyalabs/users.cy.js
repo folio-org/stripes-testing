bb192d76c02ae0eaf4b55149164ce1bb2befb3a8
santosh <santoshp@ideyalabs.com>
2023-07-26 19:12:34 +0530

    Modified Files

--- cypress/e2e/ideyalabs/users.cy.js
+++ cypress/e2e/ideyalabs/users.cy.js
@@ -1,43 +1,43 @@
-import ledgers from "../../support/fragments/finance/ledgers/ledgers";
-import topMenu from "../../support/fragments/topMenu";
-import users from "../../support/fragments/users/users";
-import usersSearchPane from "../../support/fragments/users/usersSearchPane";
-import getRandomPostfix, { getFourDigitRandomNumber } from "../../support/utils/stringTools";
+import ledgers from '../../support/fragments/finance/ledgers/ledgers';
+import topMenu from '../../support/fragments/topMenu';
+import users from '../../support/fragments/users/users';
+import usersSearchPane from '../../support/fragments/users/usersSearchPane';
+import getRandomPostfix, { getFourDigitRandomNumber } from '../../support/utils/stringTools';
 
 const barcodeNumber = getRandomPostfix();
 
 const userOne = {
-  patronGroup: "A1A1",
+  patronGroup: 'A1A1',
   barcode: barcodeNumber,
   userName: `AutotestUser_${getRandomPostfix()}`,
   personal: {
-    lastName: "User",
-    firstName: "Delete",
+    lastName: 'User',
+    firstName: 'Delete',
     email: `dan${getFourDigitRandomNumber}@gmail.com`,
   },
 };
 
 const userTwo = {
-  patronGroup: "A1A1",
+  patronGroup: 'A1A1',
   barcode: barcodeNumber,
   userName: `AutotestUser_${getRandomPostfix()}`,
   personal: {
-    lastName: "User",
-    firstName: "Delete",
+    lastName: 'User',
+    firstName: 'Delete',
     email: `dan${getFourDigitRandomNumber}@gmail.com`,
   },
 };
 
 const userThree = {
-  patronGroup: "A1A1",
+  patronGroup: 'A1A1',
   barcode: `678${getFourDigitRandomNumber}`,
   userName: `Mann${getFourDigitRandomNumber}`,
   personal: {
-    lastName: "Script",
-    firstName: "Auto",
-    email: "dan@gmail.com",
-    middleName: "Test",
-    preferredFirstName: "Dan",
+    lastName: 'Script',
+    firstName: 'Auto',
+    email: 'dan@gmail.com',
+    middleName: 'Test',
+    preferredFirstName: 'Dan',
   },
 };
 
@@ -46,41 +46,41 @@ const deleteData = {
   selectName: 'User, Delete',
   lastName: 'Script',
   selectLastName: 'Script Test'
-}
+};
 
 const verifyData = {
-  verifyLastNameOnUserDetailsPane: "Script",
-  verifyPreferredfirstnameOnUserDetailsPane: "Dan",
-  verifyFirstNameOnUserDetailsPane: "Auto",
-  verifyMiddleNameOnUserDetailsPane: "Test",
+  verifyLastNameOnUserDetailsPane: 'Script',
+  verifyPreferredfirstnameOnUserDetailsPane: 'Dan',
+  verifyFirstNameOnUserDetailsPane: 'Auto',
+  verifyMiddleNameOnUserDetailsPane: 'Test',
   verifyClearFirstNameDetailsPane: 'No value set-'
 };
 
-describe("create a users", () => {
+describe('create a users', () => {
   before(() => {
-    cy.login(Cypress.env("diku_login"), Cypress.env("diku_password"));
+    cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
     cy.visit(topMenu.usersPath);
   });
 
   after(() => {
-    usersSearchPane.searchByLastname(deleteData.lastName)
-    usersSearchPane.selectUsersFromList(deleteData.selectLastName)
-    users.deleteUser()
+    usersSearchPane.searchByLastname(deleteData.lastName);
+    usersSearchPane.selectUsersFromList(deleteData.selectLastName);
+    users.deleteUser();
   });
 
-  it("C421-Create: new user; required: contact info, email, phone, external system ID, address", () => {
+  it('C421-Create: new user; required: contact info, email, phone, external system ID, address', () => {
     users.createViaUi(userOne);
     cy.visit(topMenu.usersPath);
     users.createViaUi(userTwo);
     users.assertion();
     ledgers.closeOpenedPage();
     users.closeWithoutSavingButton();
-    usersSearchPane.searchByKeywords(deleteData.name)
-    usersSearchPane.selectUsersFromList(deleteData.selectName)
-    users.deleteUser()
+    usersSearchPane.searchByKeywords(deleteData.name);
+    usersSearchPane.selectUsersFromList(deleteData.selectName);
+    users.deleteUser();
   });
 
-  it("C11096-Add Preferred first name and confirm its display in the User record View and Edit screens", () => {
+  it('C11096-Add Preferred first name and confirm its display in the User record View and Edit screens', () => {
     users.createData(userThree);
     users.verifyPreferredfirstnameOnUserDetailsPane(
       verifyData.verifyPreferredfirstnameOnUserDetailsPane
@@ -108,5 +108,4 @@ describe("create a users", () => {
     users.saveButton();
     users.verifyFirstNameOnUserDetailsPane(verifyData.verifyClearFirstNameDetailsPane);
   });
-
 });
