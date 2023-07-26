8d3e0eb7b9a79175d28b7674606f3a2842a325e5
pavithradasari <pavithrad@ideyalabs.com>
2023-07-26 21:27:46 +0530

    Modified Checkout File

--- cypress/e2e/ideyalabs/C_checkout.cy.js
+++ cypress/e2e/ideyalabs/C_checkout.cy.js
@@ -12,29 +12,38 @@ const testData = {
   name: 'A2L 3, Holly',
   parameter: 'Keyword (name, email, identifier)',
   note: {
-    title: `Test Title ${getRandomPostfix()}`,
     details: `Test details ${getRandomPostfix()}`,
     editTitle: `Test Title ${getRandomPostfix()}`,
   },
+  barcode: '555555',
+  itemBarcode: '108204829'
 };
-describe('checkout item', () => {
-  before('login', () => {
+
+describe('Checkout item', () => {
+  before('Login', () => {
     cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
+  });
+
+  after('Delete test data', () => {
+    cy.visit(topMenu.usersPath);
+    usersSearchPane.searchByStatus(testData.status);
+    usersSearchPane.searchByUsername(testData.name);
+    users.openNote();
+    users.deleteNote();
   });
 
   it('C343243 Check out app: Display a pop-up note and Close note', () => {
     cy.visit(topMenu.usersPath);
     usersSearchPane.searchByStatus(testData.status);
-    usersSearchPane.searchByKeywords(testData.parameter, testData.value);
     usersSearchPane.searchByUsername(testData.name);
-    users.createNote(testData.note.title, testData.note.details);
+    users.createNote(testData.note.details);
     users.getUserBarcode();
     users.editNote(testData.note.editTitle, testData.note.details);
   });
 
   it('C642 Cannot find the patron and item that meet circulation rule criteria', () => {
     cy.visit(topMenu.checkOutPath);
-    users.enterBardcodeCheckout(testData.patronBarcodeOne);
+    users.enterPatronBarcodeCheckOut(testData.patronBarcodeOne);
     users.clickOpenLoansCount();
     userLoans.checkOffLoanByBarcode(testData.loanBarcode);
     users.renewButton();
@@ -42,14 +51,27 @@ describe('checkout item', () => {
 
   it('C646 Cannot find the patron and item that meet circulation rule criteria', () => {
     cy.visit(topMenu.checkOutPath);
-    users.enterBardcodeCheckout(testData.patronBarcodeTwo);
+    users.enterPatronBarcodeCheckOut(testData.patronBarcodeTwo);
     users.clickOpenLoansCount();
     users.dueDate();
   });
 
   it('C777 Check out: override non-circulating items', () => {
     cy.visit(topMenu.checkOutPath);
-    users.enterBardcodeCheckout(testData.patronBarcodeTwo);
-    users.enterBardcodeCheckout(testData.patronBarcodeTwo);
+    users.enterPatronBarcodeCheckOut(testData.barcode);
+    users.enterItemBarcodeCheckOut(testData.itemBarcode);
+    users.closeButton();
+    users.enterPatronBarcodeCheckOut(testData.barcode);
+    users.enterItemBarcodeCheckOut(testData.itemBarcode);
+    users.patronOverride();
+    users.cancelButton();
+    users.enterPatronBarcodeCheckOut(testData.barcode);
+    users.enterItemBarcodeCheckOut(testData.itemBarcode);
+    users.closeButton();
+    users.enterPatronBarcodeCheckOut(testData.barcode);
+    users.enterItemBarcodeCheckOut(testData.itemBarcode);
+    users.patronOverride();
+    users.saveAndCloseButton();
+    users.clickOpenLoansCount();
   });
 });
--- cypress/support/a_ideyalabs/users.js
+++ cypress/support/a_ideyalabs/users.js
@@ -11,25 +11,33 @@ import {
   RichEditor,
   Section,
   Select,
+  TextArea,
   TextField,
 } from '../../../interactors';
 import topMenu from '../fragments/topMenu';
+import getRandomPostfix from '../utils/stringTools';
 
 const titleField = TextField({ name: 'title' });
 const detailsField = RichEditor('Details');
 const submitButton = Button({ type: 'submit' });
 const newButton = Button('New');
 const checkOutApp = Checkbox({ name: 'popUpOnCheckOut' });
-const checkOut = TextField({ id: 'input-patron-identifier' });
-const patronButton = Button({ id: 'clickable-find-patron' });
 const userNoteModal = Modal('User note');
 const searchField = TextField({ id: 'input-user-search' });
 const searchButton = Button('Search');
 const usersPane = PaneContent({ id: 'users-search-results-pane-content' });
 const renewButton = Button('Renew');
+const patronBarcodeTextField = TextField({ id: 'input-patron-identifier' });
+const itemBarcodeTextField = TextField({ id: 'input-item-barcode' });
+const patronButton = Button({ id: 'clickable-find-patron' });
+const itemButton = Button({ id: 'clickable-add-item' });
+const actionsButton = Button('Actions');
+const deleteButton = Button('Delete');
+const deleteButtonInConfirmation = Button({ id: 'clickable-confirm-delete-note-confirm' });
+const title = `Test Title ${getRandomPostfix()}`;
 
 export default {
-  createNote: (title, details) => {
+  createNote: (details) => {
     cy.expect(Accordion({ id: 'notesAccordion' }).exists());
     cy.do([
       Accordion({ id: 'notesAccordion' }).clickHeader(),
@@ -41,7 +49,7 @@ export default {
     ]);
   },
 
-  editNote: (title, details) => {
+  editNote: (details) => {
     cy.expect(Accordion({ id: 'notesAccordion' }).exists());
     cy.do([
       Accordion({ id: 'notesAccordion' }).clickHeader(),
@@ -49,18 +57,38 @@ export default {
         .find(MultiColumnListRow({ index: 0 }))
         .find(Button('Edit'))
         .click(),
-      titleField.clear(),
-      titleField.fillIn(title),
       detailsField.fillIn(details),
       submitButton.click(),
     ]);
   },
 
+  deleteNote: () => {
+    cy.do([
+      actionsButton.click(),
+      deleteButton.click(),
+      deleteButtonInConfirmation.click(),
+      Accordion({ id: 'notesAccordion' }).clickHeader(),
+    ]);
+    cy.expect(Section({ id: 'notesAccordion' }).find(MultiColumnListRow({ index: 0, content: title })).absent());
+  },
+
+  openNote: () => {
+    cy.expect(Accordion({ id: 'notesAccordion' }).exists());
+    cy.do([
+      Accordion({ id: 'notesAccordion' }).clickHeader(),
+      Section({ id: 'notesAccordion' })
+        .find(MultiColumnListRow({ index: 0 }))
+        .click(),
+    ]);
+  },
+
   selectRecord: (userName) => {
+    cy.expect(usersPane.find(Select(userName)).exists());
     cy.do(usersPane.find(Select(userName)).click());
   },
 
   selectUserRecord: (content) => {
+    cy.expect(usersPane.find(Link(content)).exists());
     cy.do(usersPane.find(Link(content)).click());
   },
 
@@ -69,12 +97,13 @@ export default {
   getUserBarcode() {
     this.getBarcode().then((barcode) => {
       cy.visit(topMenu.checkOutPath);
+      cy.expect(patronBarcodeTextField.exists());
       cy.do([
-        checkOut.fillIn(barcode),
+        patronBarcodeTextField.fillIn(barcode),
         patronButton.click(),
         userNoteModal.find(Button('Close')).click(),
-      ]),
-      cy.visit(topMenu.usersPath),
+      ]);
+      cy.visit(topMenu.usersPath)
       cy.do([
         Select({ id: 'input-user-search-qindex' }).choose('Barcode'),
         searchField.fillIn(barcode),
@@ -83,8 +112,42 @@ export default {
     });
   },
 
-  enterBardcodeCheckout: (barcode) => {
-    cy.do([checkOut.fillIn(barcode), patronButton.click()]);
+  enterPatronBarcodeCheckOut: (patronBarcode) => {
+    cy.expect(patronBarcodeTextField.exists());
+    cy.do(patronBarcodeTextField.fillIn(patronBarcode));
+    cy.expect(patronButton.exists());
+    cy.do(patronButton.click());
+  },
+
+  enterItemBarcodeCheckOut: (searchItemBarcode) => {
+    cy.expect(itemBarcodeTextField.exists());
+    cy.do(itemBarcodeTextField.fillIn(searchItemBarcode));
+    cy.expect(itemButton.exists());
+    cy.do(itemButton.click());
+  },
+
+  closeButton: () => {
+    cy.expect(Modal('Patron blocked from borrowing').find(Button('Close')).exists());
+    cy.do(Modal('Patron blocked from borrowing').find(Button('Close')).click());
+  },
+
+  cancelButton:() => {
+    cy.expect(Modal('Override patron block').find(Button('Cancel')).exists());
+    cy.do(Modal('Override patron block').find(Button('Cancel')).click());
+  },
+
+  saveAndCloseButton:() => {
+    cy.expect(TextArea({ type:'text' }).exists());
+    cy.do([
+      TextArea({ type:'text' }).fillIn('test'),
+      Button('Save & close').click()]);
+  },
+
+  patronOverride: () => {
+    cy.expect(Modal('Patron blocked from borrowing').find(Button('Override')).exists());
+    cy.do([
+      Modal('Patron blocked from borrowing').find(Button('Override')).click(),
+    ]);
   },
 
   dueDate: (rowIndex = 0) => {
@@ -101,6 +164,7 @@ export default {
 
   clickOpenLoansCount() {
     this.getOpenLoans().then((val) => {
+      cy.expect(Section({ id: 'patron-details' }).find(KeyValue('Open loans')).exists());
       cy.do(
         Section({ id: 'patron-details' })
           .find(KeyValue('Open loans'))
