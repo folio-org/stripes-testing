010dc0706ee2037858372dbaa268b9fb2e206fc4
Santosh <santoshp@ideyalabs.com>
2023-07-28 12:38:34 +0530

    Added Tags

--- cypress/e2e/ideyalabs/I_agreements.cy.js
+++ cypress/e2e/ideyalabs/I_agreements.cy.js
@@ -1,28 +1,30 @@
-import eHoldings from '../../support/a_ideyalabs/eHolding';
+import devTeams from '../../support/dictionary/devTeams';
+import testTypes from '../../support/dictionary/testTypes';
 import agreementsDetails from '../../support/fragments/agreements/agreementsDetails';
 import newAgreement from '../../support/fragments/agreements/newAgreement';
 import eHoldingsPackages from '../../support/fragments/eholdings/eHoldingsPackages';
 import topMenu from '../../support/fragments/topMenu';
 import dateTools from '../../support/utils/dateTools';
 import getRandomPostfix from '../../support/utils/stringTools';
+import eHoldings from '../../support/a_ideyalabs/eHolding';
 
-describe('Creating New Agreement', () => {
+describe('Agreement', () => {
   const defaultAgreement = {
     name: `autotest_agreement_${getRandomPostfix()}`,
     status: 'Active',
-    startDate: dateTools.getCurrentDate(),
+    startDate: dateTools.getCurrentDate()
   };
   before('Login to Folio', () => {
     cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
   });
-  it('C757__Create an Agreement', () => {
+  it('C757 Create an Agreement (ERM)', { tags: [testTypes.smoke, devTeams.erm] }, () => {
     cy.visit(topMenu.agreementsPath);
     newAgreement.newButtonClick();
     newAgreement.fill();
     newAgreement.save();
     newAgreement.validateDateAndTime();
   });
-  it('C1295__Create a new Agreement and attach a package', () => {
+  it('C1295 Create a new Agreement and attach a package (spitfire)', { tags: [testTypes.extendedPath, devTeams.spitfire] }, () => {
     cy.visit(topMenu.eholdingsPath);
     eHoldings.packageSearch();
     eHoldingsPackages.openPackage();
@@ -31,7 +33,7 @@ describe('Creating New Agreement', () => {
     newAgreement.save();
     newAgreement.agreementLine();
   });
-  after('remove the test data', () => {
+  after('delete test data', () => {
     newAgreement.findAgreement(defaultAgreement);
     newAgreement.deleteAgreement();
     newAgreement.searchAgreement();
