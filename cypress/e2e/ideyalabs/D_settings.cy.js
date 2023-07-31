264103c98f86032961872bb304a1339aa40d4005
Santosh <santoshp@ideyalabs.com>
2023-07-31 11:53:10 +0530

    Modified Files

--- cypress/e2e/ideyalabs/D_settings.cy.js
+++ cypress/e2e/ideyalabs/D_settings.cy.js
@@ -18,18 +18,35 @@ const checkboxData = { fieldLabel: `CheckBox${getFourDigitRandomNumber()}`, help
 const radioButtonData = { data: { fieldLabel: `RadioButton${getFourDigitRandomNumber()}`, helpText: `testData${getFourDigitRandomNumber()}`, label1: `Radio1${getFourDigitRandomNumber()}`, label2: `Radio2${getFourDigitRandomNumber()}` } };
 const label2 = `select${getFourDigitRandomNumber()}`;
 const singleSelectData = { data: { fieldLabel: `Single Select Dropdown${getFourDigitRandomNumber()}`, helpText: 'select One Data', label1: `Select${getFourDigitRandomNumber()}`, label2 } };
+const noteData = `New Note${getFourDigitRandomNumber()}`;
 const testData = {
   name: `Administrator${getFourDigitRandomNumber()}`,
   description: `Hello${getFourDigitRandomNumber()}`,
 };
-const noteData = `New Note${getFourDigitRandomNumber()}`;
-const categoryName = `Test${getFourDigitRandomNumber()}`;
 const noteType = `Item${getFourDigitRandomNumber()}`;
+const categoryName = `Test${getFourDigitRandomNumber()}`;
 
 describe('Settings', () => {
-  before('login to Folio', () => {
+  before('Login to Folio', () => {
     cy.login(Cypress.env('diku_login'), Cypress.env('diku_password'));
   });
+
+  after('Delete test data', () => {
+    cy.visit(topMenu.customFieldsPath);
+    customFields.editButton();
+    customFields.deleteCustomField(`${textFieldData.fieldLabel} · Text field`);
+    customFields.deleteCustomField(`${testAreaData.fieldLabel} · Text area`);
+    customFields.deleteCustomField(`${checkboxData.fieldLabel} · Checkbox`);
+    customFields.deleteCustomField(`${radioButtonData.data.fieldLabel} · Radio button set`);
+    customFields.deleteCustomField(`${singleSelectData.data.fieldLabel} · Single select`);
+    customFields.confirmDeletion();
+    customFields.verifyDeletedCustomFields(`${textFieldData.fieldLabel} · Text field`);
+    customFields.verifyDeletedCustomFields(`${testAreaData.fieldLabel} · Text area`);
+    customFields.verifyDeletedCustomFields(`${checkboxData.fieldLabel} · Checkbox`);
+    customFields.verifyDeletedCustomFields(`${radioButtonData.data.fieldLabel} · Radio button set`);
+    customFields.verifyDeletedCustomFields(`${singleSelectData.data.fieldLabel} · Single select`);
+  });
+
   it('C15693 Create a text field custom field (volaris)', { tags: [testTypes.extendedPath, devTeams.volaris] }, () => {
     cy.visit(topMenu.customFieldsPath);
     customFields.addCustomTextField(textFieldData);
@@ -38,6 +55,7 @@ describe('Settings', () => {
     usersSearchPane.selectFirstUser('Excel, Testing');
     usersSearchPane.verifyTextField(textFieldData.fieldLabel);
   });
+
   it('C15694 Create a text area custom field and add help text (volaris)', { tags: [testTypes.extendedPath, devTeams.volaris] }, () => {
     cy.visit(topMenu.customFieldsPath);
     customFields.addCustomTextArea(testAreaData);
@@ -46,6 +64,7 @@ describe('Settings', () => {
     usersSearchPane.selectFirstUser('Excel, Testing');
     usersSearchPane.verifyTextArea(testAreaData.fieldLabel);
   });
+
   it('C15695 Create a checkbox custom field (volaris)', { tags: [testTypes.extendedPath, devTeams.volaris] }, () => {
     cy.visit(topMenu.customFieldsPath);
     customFields.addCustomCheckBox(checkboxData);
@@ -54,6 +73,7 @@ describe('Settings', () => {
     usersSearchPane.selectFirstUser('Excel, Testing');
     usersSearchPane.verifyCheckBox(checkboxData.fieldLabel);
   });
+
   it('C15696 Create a radio button custom field (volaris)', { tags: [testTypes.extendedPath, devTeams.volaris] }, () => {
     cy.visit(topMenu.customFieldsPath);
     customFields.addCustomRadioButton(radioButtonData);
@@ -62,6 +82,7 @@ describe('Settings', () => {
     usersSearchPane.selectFirstUser('Excel, Testing');
     usersSearchPane.verifyRadioButton(radioButtonData.data.fieldLabel);
   });
+
   it('C15697 Create a single select custom field (volaris)', { tags: [testTypes.extendedPath, devTeams.volaris] }, () => {
     cy.visit(topMenu.customFieldsPath);
     customFields.addCustomSingleSelect(singleSelectData);
@@ -70,6 +91,7 @@ describe('Settings', () => {
     usersSearchPane.selectFirstUser('Excel, Testing');
     usersSearchPane.verifySingleSelect(singleSelectData.data.fieldLabel, label2);
   });
+
   it('C15701 Change custom fields order (volaris)', { tags: [testTypes.extendedPath, devTeams.volaris] }, () => {
     cy.visit(topMenu.customFieldsPath);
     customFields.editButton();
@@ -79,12 +101,14 @@ describe('Settings', () => {
     usersSearchPane.selectFirstUser('Excel, Testing');
     usersSearchPane.verifyDragItem();
   });
+
   it('C2234 Create new permission set (volaris)', { tags: [testTypes.criticalPath, devTeams.volaris] }, () => {
     cy.visit(topMenu.permissionSetPath);
     customFields.clickOnNewButton();
     customFields.createPermission(testData);
     customFields.deletePermission(testData);
   });
+
   it('C16985 Settings | Set up a note type (spitfire)', { tags: [testTypes.extendedPath, devTeams.spitfire] }, () => {
     cy.visit(topMenu.notesPath);
     newNote.fillNote(noteData);
@@ -95,6 +119,7 @@ describe('Settings', () => {
     newNote.clickOnNoteType(noteData);
     newNote.deleteNote(noteData);
   });
+
   it('C1304 Settings | Edit a note type (spitfire)', { tags: [testTypes.extendedPath, devTeams.spitfire] }, () => {
     cy.visit(topMenu.notesPath);
     existingNoteEdit.clickEditButton(noteType);
@@ -110,6 +135,7 @@ describe('Settings', () => {
     existingNoteView.gotoEdit();
     newNote.clickOnNoteType(noteType);
   });
+
   it('C731 Create new categories (thunderjet)', { tags: [testTypes.extendedPath, devTeams.thunderjet] }, () => {
     cy.visit(settingsMenu.organizationsPath);
     organizations.addNewCategory(categoryName);
@@ -121,23 +147,9 @@ describe('Settings', () => {
     cy.visit(settingsMenu.organizationCategoryPath);
     organizations.deleteCreatedCategory(categoryName);
   });
+
   it('C367970 Check that User can save changes while edit ""Fixed due date schedules"" without changing ""Fixed due date schedules name"" field (vega)', { tags: [testTypes.criticalPath, devTeams.vega] }, () => {
     cy.visit(settingsMenu.circulationFixedDueDateSchedulesPath);
     eHoldingsProviders.editSchedule({ data: { name: 'Magnus test', description: `Test${getFourDigitRandomNumber()}` } });
-  });
-  after('delete test data', () => {
-    cy.visit(topMenu.customFieldsPath);
-    customFields.editButton();
-    customFields.deleteCustomField(`${textFieldData.fieldLabel} · Text field`);
-    customFields.deleteCustomField(`${testAreaData.fieldLabel} · Text area`);
-    customFields.deleteCustomField(`${checkboxData.fieldLabel} · Checkbox`);
-    customFields.deleteCustomField(`${radioButtonData.data.fieldLabel} · Radio button set`);
-    customFields.deleteCustomField(`${singleSelectData.data.fieldLabel} · Single select`);
-    customFields.confirmDeletion();
-    customFields.verifyDeletedCustomFields(`${textFieldData.fieldLabel} · Text field`);
-    customFields.verifyDeletedCustomFields(`${testAreaData.fieldLabel} · Text area`);
-    customFields.verifyDeletedCustomFields(`${checkboxData.fieldLabel} · Checkbox`);
-    customFields.verifyDeletedCustomFields(`${radioButtonData.data.fieldLabel} · Radio button set`);
-    customFields.verifyDeletedCustomFields(`${singleSelectData.data.fieldLabel} · Single select`);
   });
 });
--- cypress/support/fragments/settings/users/customFields.js
+++ cypress/support/fragments/settings/users/customFields.js
@@ -1,6 +1,15 @@
 import { including } from 'bigtest';
-import { Accordion, Button, Callout, Dropdown, Modal, MultiColumnListRow, Pane, Spinner, TextArea, TextField } from '../../../../../interactors';
-
+import {
+  Accordion,
+  Button,
+  Callout,
+  Dropdown,
+  Modal,
+  MultiColumnListRow,
+  Pane,
+  TextArea,
+  TextField
+} from '../../../../../interactors';
 
 const customFieldsPane = Pane('Custom fields');
 const editNewButton = Button({ href: '/settings/users/custom-fields/edit' });
@@ -14,9 +23,12 @@ const fieldLabel = TextField('Field label*');
 const helpText = TextField('Help text');
 const editButton = Button('Edit');
 const deleteButton = Button('Delete');
-const deleteConfirmationModal = Modal({ id: 'deletepermissionset-confirmation' });
-const deleteButtonInConfirmation = Button('Delete', { id: 'clickable-deletepermissionset-confirmation-confirm' });
-
+const deleteConfirmationModal = Modal({
+  id: 'deletepermissionset-confirmation',
+});
+const deleteButtonInConfirmation = Button('Delete', {
+  id: 'clickable-deletepermissionset-confirmation-confirm',
+});
 
 export default {
   waitLoading() {
@@ -27,8 +39,12 @@ export default {
       editNewButton.click(),
       addCustomFieldDropdown.choose('Multi-select'),
       TextField('Field label*').fillIn(data.fieldLabel),
-      MultiColumnListRow({ indexRow: 'row-0' }).find(TextField()).fillIn(data.label1),
-      MultiColumnListRow({ indexRow: 'row-1' }).find(TextField()).fillIn(data.label2),
+      MultiColumnListRow({ indexRow: 'row-0' })
+        .find(TextField())
+        .fillIn(data.label1),
+      MultiColumnListRow({ indexRow: 'row-1' })
+        .find(TextField())
+        .fillIn(data.label2),
       saveAndCloseButton.click(),
     ]);
   },
@@ -38,8 +54,12 @@ export default {
       editNewButton.click(),
       Accordion(including(oldData.fieldLabel)).clickHeader(),
       TextField('Field label*').fillIn(newData.fieldLabel),
-      MultiColumnListRow({ indexRow: 'row-0' }).find(TextField()).fillIn(newData.label1),
-      MultiColumnListRow({ indexRow: 'row-1' }).find(TextField()).fillIn(newData.label2),
+      MultiColumnListRow({ indexRow: 'row-0' })
+        .find(TextField())
+        .fillIn(newData.label1),
+      MultiColumnListRow({ indexRow: 'row-1' })
+        .find(TextField())
+        .fillIn(newData.label2),
       saveAndCloseButton.click(),
     ]);
   },
@@ -50,42 +70,70 @@ export default {
   },
 
   deletePermission(data) {
-    cy.do([editButton.click(),
+    cy.do([
+      editButton.click(),
       deleteButton.click(),
-      deleteConfirmationModal.find(deleteButtonInConfirmation).click()]);
-    cy.expect(Callout({ type: 'success' }).has({ text: including(`The permission set ${data.name} was successfully deleted.`) }));
+      deleteConfirmationModal.find(deleteButtonInConfirmation).click(),
+    ]);
+    cy.expect(
+      Callout({ type: 'success' }).has({
+        text: including(
+          `The permission set ${data.name} was successfully deleted.`
+        ),
+      })
+    );
   },
 
   createPermission(data) {
-    cy.do([permissionTitle.fillIn(data.name), permissionDescription.fillIn(data.description), addPermissionButton.click(), saveAndCloseButton.click()]);
-    cy.expect(Callout({ type: 'success' }).has({ text: including(`The permission set ${data.name} was successfully created`) }));
+    cy.do([
+      permissionTitle.fillIn(data.name),
+      permissionDescription.fillIn(data.description),
+      addPermissionButton.click(),
+      saveAndCloseButton.click(),
+    ]);
+    cy.expect(
+      Callout({ type: 'success' }).has({
+        text: including(
+          `The permission set ${data.name} was successfully created`
+        ),
+      })
+    );
   },
 
   addCustomTextField(data) {
-    cy.do([editNewButton.click(),
+    cy.do([
+      editNewButton.click(),
       addCustomFieldDropdown.choose('Text field'),
       fieldLabel.fillIn(data.fieldLabel),
       helpText.fillIn(data.helpText),
-      saveAndCloseButton.click()]);
-    cy.expect(Spinner().absent());
+      saveAndCloseButton.click(),
+    ]);
+    cy.expect(saveAndCloseButton.absent());
+    cy.expect(Accordion(`${data.fieldLabel} · Text field`).exists());
   },
 
   addCustomTextArea(data) {
-    cy.do([editNewButton.click(),
+    cy.do([
+      editNewButton.click(),
       addCustomFieldDropdown.choose('Text area'),
       fieldLabel.fillIn(data.fieldLabel),
       helpText.fillIn(data.helpText),
-      saveAndCloseButton.click()]);
-    cy.expect(Spinner().absent());
+      saveAndCloseButton.click(),
+    ]);
+    cy.expect(saveAndCloseButton.absent());
+    cy.expect(Accordion(`${data.fieldLabel} · Text area`).exists());
   },
 
   addCustomCheckBox(data) {
-    cy.do([editNewButton.click(),
+    cy.do([
+      editNewButton.click(),
       addCustomFieldDropdown.choose('Checkbox'),
       fieldLabel.fillIn(data.fieldLabel),
       helpText.fillIn(data.helpText),
-      saveAndCloseButton.click()]);
-    cy.expect(Spinner().absent());
+      saveAndCloseButton.click(),
+    ]);
+    cy.expect(saveAndCloseButton.absent());
+    cy.expect(Accordion(`${data.fieldLabel} · Checkbox`).exists());
   },
 
   addCustomRadioButton({ data }) {
@@ -94,10 +142,16 @@ export default {
       addCustomFieldDropdown.choose('Radio button set'),
       fieldLabel.fillIn(data.fieldLabel),
       helpText.fillIn(data.helpText),
-      MultiColumnListRow({ indexRow: 'row-1' }).find(TextField()).fillIn(data.label1),
-      MultiColumnListRow({ indexRow: 'row-2' }).find(TextField()).fillIn(data.label2),
+      MultiColumnListRow({ indexRow: 'row-1' })
+        .find(TextField())
+        .fillIn(data.label1),
+      MultiColumnListRow({ indexRow: 'row-2' })
+        .find(TextField())
+        .fillIn(data.label2),
       saveAndCloseButton.click(),
     ]);
+    cy.expect(saveAndCloseButton.absent());
+    cy.expect(Accordion(`${data.fieldLabel} · Radio button set`).exists());
   },
 
   addCustomSingleSelect({ data }) {
@@ -106,20 +160,29 @@ export default {
       addCustomFieldDropdown.choose('Single select'),
       fieldLabel.fillIn(data.fieldLabel),
       helpText.fillIn(data.helpText),
-      MultiColumnListRow({ indexRow: 'row-1' }).find(TextField()).fillIn(data.label1),
-      MultiColumnListRow({ indexRow: 'row-2' }).find(TextField()).fillIn(data.label2),
+      MultiColumnListRow({ indexRow: 'row-1' })
+        .find(TextField())
+        .fillIn(data.label1),
+      MultiColumnListRow({ indexRow: 'row-2' })
+        .find(TextField())
+        .fillIn(data.label2),
       saveAndCloseButton.click(),
     ]);
+    cy.expect(saveAndCloseButton.absent());
+    cy.expect(Accordion(`${data.fieldLabel} · Single select`).exists());
   },
 
   editButton() {
     cy.expect(editNewButton.exists());
-    cy.do([
-      editNewButton.click()]);
+    cy.do([editNewButton.click()]);
   },
 
   deleteCustomField(field) {
-    cy.do(Accordion(field).find(Button({ icon: 'trash' })).click());
+    cy.do(
+      Accordion(field)
+        .find(Button({ icon: 'trash' }))
+        .click()
+    );
   },
 
   confirmDeletion() {
@@ -129,6 +192,5 @@ export default {
 
   verifyDeletedCustomFields(field) {
     cy.expect(Accordion(field).absent());
-  }
+  },
 };
-
