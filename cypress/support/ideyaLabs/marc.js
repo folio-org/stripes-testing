5c198bd2ef4c56ec2e9fa9c0e64f9bc06683d57a
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-08-03 18:02:29 +0300

    deleted unused import

--- cypress/support/ideyaLabs/marc.js
+++ cypress/support/ideyaLabs/marc.js
@@ -8,28 +8,27 @@ import {
   including,
   Checkbox,
   KeyValue,
-  Modal,
   Select,
   SearchField,
   QuickMarcEditorRow,
   Callout
-} from "../../../interactors";
-import holdingsRecordView from "../fragments/inventory/holdingsRecordView";
+} from '../../../interactors';
+import holdingsRecordView from '../fragments/inventory/holdingsRecordView';
 
-const rootSection = Section({ id: "marc-view-pane" });
-const filterSection = Section({ id: "pane-filter" });
-const saveAndCloseBtn = Button("Save & close");
+const rootSection = Section({ id: 'marc-view-pane' });
+const filterSection = Section({ id: 'pane-filter' });
+const saveAndCloseBtn = Button('Save & close');
 const closeButton = Button({
-  ariaLabel: "Close The !!!Kung of Nyae Nyae / Lorna Marshall.",
+  ariaLabel: 'Close The !!!Kung of Nyae Nyae / Lorna Marshall.',
 });
 const buttonLink = Button({ icon: 'unlink' });
 const editorSection = Section({ id: 'quick-marc-editor-pane' });
-const linkHeadingsButton = Button("Link headings");
-const searchButton = Button({ type: "submit" });
-const actionsButton = Button("Actions");
-const deleteButton = Button("Delete");
+const linkHeadingsButton = Button('Link headings');
+const searchButton = Button({ type: 'submit' });
+const actionsButton = Button('Actions');
+const deleteButton = Button('Delete');
 const deleteButtonInConfirmation = Button({
-  id: "clickable-delete-confirmation-modal-confirm",
+  id: 'clickable-delete-confirmation-modal-confirm',
 });
 const availableProxies = ['Inherited - None', 'FOLIO-Bugfest', 'EZProxy'];
 const proxySelect = Select({ id: 'eholdings-proxy-id' });
@@ -65,14 +64,14 @@ export default {
   },
 
   printButton: () => {
-    cy.do(rootSection.find(Button("Actions")).click());
-    cy.do(Button("Print").click());
+    cy.do(rootSection.find(Button('Actions')).click());
+    cy.do(Button('Print').click());
   },
 
   searchByValue: (value) => {
     cy.do(
       filterSection
-        .find(TextField({ id: "input-inventory-search" }))
+        .find(TextField({ id: 'input-inventory-search' }))
         .fillIn(value)
     );
     searchButton.click();
@@ -87,63 +86,63 @@ export default {
   },
 
   popupUnlinkButton: () => {
-    cy.do(Button("Unlink").click());
+    cy.do(Button('Unlink').click());
   },
 
   keepLinkingButton: () => {
-    cy.do(Button("Keep linking").click());
+    cy.do(Button('Keep linking').click());
   },
 
   closeEditMarc: () => {
-    cy.do(Button({ icon: "times" }).click());
+    cy.do(Button({ icon: 'times' }).click());
   },
 
   crossIcon: () => {
-    cy.do(Button({ icon: "times" }).click());
+    cy.do(Button({ icon: 'times' }).click());
   },
 
   create006Tag: () => {
-    cy.get(".quickMarcEditorAddField:last").click();
+    cy.get('.quickMarcEditorAddField:last').click();
     cy.get('[class*="quickMarcEditorRow--"]:last-child')
-      .find("input")
+      .find('input')
       .then(([tag]) => {
-        cy.wrap(tag).type("006");
+        cy.wrap(tag).type('006');
       });
-    cy.get('[class*="quickMarcEditorRow--"]:last-child').contains("Type");
-    cy.get('[class*="quickMarcEditorRow--"]:last-child select').select("a");
+    cy.get('[class*="quickMarcEditorRow--"]:last-child').contains('Type');
+    cy.get('[class*="quickMarcEditorRow--"]:last-child select').select('a');
   },
 
   create007Tag: () => {
-    cy.get(".quickMarcEditorAddField:last").click();
+    cy.get('.quickMarcEditorAddField:last').click();
     cy.get('[class*="quickMarcEditorRow--"]:last-child')
-      .find("input")
+      .find('input')
       .then(([tag]) => {
-        cy.wrap(tag).type("007");
+        cy.wrap(tag).type('007');
       });
-    cy.get('[class*="quickMarcEditorRow--"]:last-child').contains("Type");
-    cy.get('[class*="quickMarcEditorRow--"]:last-child select').select("a");
+    cy.get('[class*="quickMarcEditorRow--"]:last-child').contains('Type');
+    cy.get('[class*="quickMarcEditorRow--"]:last-child select').select('a');
   },
 
   recordLastUpdated: () => {
     cy.expect(Spinner().absent());
     cy.do(
-      Accordion("Administrative data")
-        .find(Button(including("Record last updated")))
+      Accordion('Administrative data')
+        .find(Button(including('Record last updated')))
         .click()
     );
   },
 
   checkFieldContentMatch() {
-    cy.wrap(Accordion({ headline: "Update information" }).text()).as("message");
-    cy.get("@message").then((val) => {
+    cy.wrap(Accordion({ headline: 'Update information' }).text()).as('message');
+    cy.get('@message').then((val) => {
       const sourceRegex = /Source: [^\n]*/;
       const sourceLineMatch = val.match(sourceRegex);
-      const sourceText = sourceLineMatch ? sourceLineMatch[0].slice(8) : "";
-      const words = sourceText.split(", ");
-      const swappedString = words.join(", ");
+      const sourceText = sourceLineMatch ? sourceLineMatch[0].slice(8) : '';
+      const words = sourceText.split(', ');
+      const swappedString = words.join(', ');
       holdingsRecordView.editInQuickMarc();
       cy.expect(
-        PaneHeader({ id: "paneHeaderquick-marc-editor-pane" }).has({
+        PaneHeader({ id: 'paneHeaderquick-marc-editor-pane' }).has({
           text: including(`Source: ${swappedString}`),
         })
       );
