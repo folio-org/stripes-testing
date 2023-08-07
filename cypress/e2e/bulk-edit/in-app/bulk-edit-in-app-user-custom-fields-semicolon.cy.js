d8618747a21b5377d063cb9fd8db76ef395eefcd
nayimovag <guliyashnar_nayimova@epam.com>
2023-08-07 12:50:53 +0500

    FAT-1232-1240 (#1448)

--- cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-user-custom-fields-semicolon.cy.js
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-user-custom-fields-semicolon.cy.js
@@ -25,60 +25,62 @@ const customFieldData = {
 const userBarcodesFileName = `userBarcodes_${getRandomPostfix()}.csv`;
 const previewOfProposedChangesFileName = `*-Updates-Preview-${userBarcodesFileName}`;
 
-describe('Bulk Edit-- Users - in app approach', () => {
-  before('create test data', () => {
-    cy.createTempUser([
-      permissions.bulkEditUpdateRecords.gui,
-      permissions.bulkEditLogsView.gui,
-      permissions.uiUsersPermissionsView.gui,
-      permissions.uiUsersCustomField.gui,
-      permissions.uiUserEdit.gui,
-    ], 'faculty')
-      .then(userProperties => {
-        user = userProperties;
-        cy.login(user.username, user.password, {
-          path: SettingsMenu.customFieldsPath,
-          waiter: CustomFields.waitLoading
+describe('bulk-edit', () => {
+  describe('in-app approach', { retries: 1 }, () => {
+    before('create test data', () => {
+      cy.createTempUser([
+        permissions.bulkEditUpdateRecords.gui,
+        permissions.bulkEditLogsView.gui,
+        permissions.uiUsersPermissionsView.gui,
+        permissions.uiUsersCustomField.gui,
+        permissions.uiUserEdit.gui,
+      ], 'faculty')
+        .then(userProperties => {
+          user = userProperties;
+          cy.login(user.username, user.password, {
+            path: SettingsMenu.customFieldsPath,
+            waiter: CustomFields.waitLoading
+          });
+          FileManager.createFile(`cypress/fixtures/${userBarcodesFileName}`, user.barcode);
+          CustomFields.addMultiSelectCustomField(customFieldData);
+          cy.visit(TopMenu.usersPath);
+          UsersSearchPane.searchByUsername(user.username);
+          UserEdit.addMultiSelectCustomField(customFieldData);
         });
-        FileManager.createFile(`cypress/fixtures/${userBarcodesFileName}`, user.barcode);
-      });
-  });
+    });
 
-  after('delete test data', () => {
-    FileManager.deleteFile(`cypress/fixtures/${userBarcodesFileName}`);
-    FileManager.deleteFileFromDownloadsByMask(previewOfProposedChangesFileName);
-    Users.deleteViaApi(user.userId);
-  });
+    after('delete test data', () => {
+      FileManager.deleteFile(`cypress/fixtures/${userBarcodesFileName}`);
+      FileManager.deleteFileFromDownloadsByMask(previewOfProposedChangesFileName);
+      Users.deleteViaApi(user.userId);
+    });
 
-  it('C389568 In app | Verify that User\'s Custom fields with semicolons are updated correctly (firebird)', { tags: [testTypes.criticalPath, devTeams.firebird] }, () => {
-    CustomFields.addMultiSelectCustomField(customFieldData);
-    cy.visit(TopMenu.usersPath);
-    UsersSearchPane.searchByUsername(user.username);
-    UserEdit.addMultiSelectCustomField(customFieldData);
-    cy.visit(TopMenu.bulkEditPath);
+    it('C389568 In app | Verify that User\'s Custom fields with semicolons are updated correctly (firebird)', { tags: [testTypes.criticalPath, devTeams.firebird] }, () => {
+      cy.visit(TopMenu.bulkEditPath);
 
-    BulkEditSearchPane.checkUsersRadio();
-    BulkEditSearchPane.selectRecordIdentifier('User Barcodes');
+      BulkEditSearchPane.checkUsersRadio();
+      BulkEditSearchPane.selectRecordIdentifier('User Barcodes');
 
-    BulkEditSearchPane.uploadFile(userBarcodesFileName);
-    BulkEditSearchPane.waitFileUploading();
+      BulkEditSearchPane.uploadFile(userBarcodesFileName);
+      BulkEditSearchPane.waitFileUploading();
 
-    BulkEditActions.openActions();
-    BulkEditSearchPane.changeShowColumnCheckbox('Custom fields');
+      BulkEditActions.openActions();
+      BulkEditSearchPane.changeShowColumnCheckbox('Custom fields');
 
-    BulkEditActions.openInAppStartBulkEditFrom();
-    BulkEditActions.fillPatronGroup('staff (Staff Member)');
-    BulkEditActions.confirmChanges();
-    BulkEditActions.downloadPreview();
-    BulkEditFiles.verifyMatchedResultFileContent(previewOfProposedChangesFileName, ['staff'], 'patronGroup', true);
-    BulkEditActions.commitChanges();
-    BulkEditSearchPane.verifyChangesUnderColumns('Custom fields', `${customFieldData.fieldLabel}:${customFieldData.label1};${customFieldData.label2}`);
+      BulkEditActions.openInAppStartBulkEditFrom();
+      BulkEditActions.fillPatronGroup('staff (Staff Member)');
+      BulkEditActions.confirmChanges();
+      BulkEditActions.downloadPreview();
+      BulkEditFiles.verifyMatchedResultFileContent(previewOfProposedChangesFileName, ['staff'], 'patronGroup', true);
+      BulkEditActions.commitChanges();
+      BulkEditSearchPane.verifyChangesUnderColumns('Custom fields', `${customFieldData.fieldLabel}:${customFieldData.label1};${customFieldData.label2}`);
 
-    cy.visit(TopMenu.usersPath);
-    UsersSearchPane.searchByUsername(user.username);
-    Users.verifyPatronGroupOnUserDetailsPane('staff');
+      cy.visit(TopMenu.usersPath);
+      UsersSearchPane.searchByUsername(user.username);
+      Users.verifyPatronGroupOnUserDetailsPane('staff');
 
-    cy.visit(SettingsMenu.customFieldsPath);
-    CustomFields.deleteCustomField(customFieldData.fieldLabel);
+      cy.visit(SettingsMenu.customFieldsPath);
+      CustomFields.deleteCustomField(customFieldData.fieldLabel);
+    });
   });
 });
