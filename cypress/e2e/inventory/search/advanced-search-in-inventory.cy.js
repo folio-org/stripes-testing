c0f95c12eb3048ec44fdaa9cb21193f2060db2d2
Yauhen Viazau <Yauhen_Viazau@epam.com>
2023-08-10 22:40:18 +0500

    FAT-7486: precondition, methods added

--- cypress/e2e/inventory/search/advanced-search-in-inventory.cy.js
+++ cypress/e2e/inventory/search/advanced-search-in-inventory.cy.js
@@ -11,11 +11,16 @@ import InventoryInstance from '../../../support/fragments/inventory/inventoryIns
 import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
 import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
 import { JOB_STATUS_NAMES } from '../../../support/constants';
+import HoldingsRecordView from '../../../support/fragments/inventory/holdingsRecordView';
+import HoldingsRecordEdit from '../../../support/fragments/inventory/holdingsRecordEdit';
+import InventoryNewHoldings from '../../../support/fragments/inventory/inventoryNewHoldings';
 
 describe('Inventory -> Advanced search', () => {
   const testData = {
     advSearchOption: 'Advanced search',
-    expectedSearchResult: 'The Beatles in mono. Adv search 001'
+    expectedSearchResult: 'The Beatles in mono. Adv search 001',
+    callNumberValue: 'YCN002003400616',
+    itemBarcode: 'ITBRCC400616'
   };
   const createdRecordIDs = [];
 
@@ -25,6 +30,12 @@ describe('Inventory -> Advanced search', () => {
       fileName: `testMarcFile.${getRandomPostfix()}.mrc`,
       jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
       numberOfRecords: 2
+    },
+    {
+      marc: 'marcBibFileC400616.mrc',
+      fileName: `testMarcFile.${getRandomPostfix()}.mrc`,
+      jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
+      numberOfRecords: 4
     }
   ];
 
@@ -49,33 +60,69 @@ describe('Inventory -> Advanced search', () => {
           }
         });
       });
-      cy.login(testData.userProperties.username, testData.userProperties.password, { path: TopMenu.inventoryPath, waiter: InventoryInstances.waitContentLoading });
+      cy.visit(TopMenu.inventoryPath).then(() => {
+        InventoryInstance.searchByTitle(createdRecordIDs[3]);
+        InventoryInstances.selectInstance();
+        InventoryInstance.pressAddHoldingsButton();
+        InventoryNewHoldings.fillRequiredFields();
+        HoldingsRecordEdit.fillCallNumber(testData.callNumberValue);
+        InventoryNewHoldings.saveAndClose();
+        InventoryInstance.waitLoading();
+        InventoryInstance.addItem();
+        InventoryInstance.fillItemRequiredFields();
+        InventoryInstance.fillItemBarcode(testData.itemBarcode);
+        InventoryInstance.saveItemDataAndVerifyExistence('-');
+      });
     });
+  });
+
+  beforeEach('Logging in', () => {
+    cy.login(testData.userProperties.username, testData.userProperties.password, { path: TopMenu.inventoryPath, waiter: InventoryInstances.waitContentLoading });
   });
 
   after('Deleting data', () => {
     Users.deleteViaApi(testData.userProperties.userId);
-    createdRecordIDs.forEach(id => {
-      InventoryInstance.deleteInstanceViaApi(id);
+    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(testData.itemBarcode);
+    createdRecordIDs.forEach((id, index) => {
+      if (index !== 3) InventoryInstance.deleteInstanceViaApi(id);
     });
   });
 
-  it('C400610 Search Instances using advanced search with "AND" operator (spitfire)', { tags: [TestTypes.criticalPath, DevTeams.spitfire] }, () => {
-    InventoryInstances.clickAdvSearchButton();
-    InventoryInstances.checkAdvSearchInstancesModalFields(0);
-    InventoryInstances.checkAdvSearchInstancesModalFields(1);
-    InventoryInstances.checkAdvSearchInstancesModalFields(2);
-    InventoryInstances.checkAdvSearchInstancesModalFields(3);
-    InventoryInstances.checkAdvSearchInstancesModalFields(4);
-    InventoryInstances.checkAdvSearchInstancesModalFields(5);
-    InventoryInstances.fillAdvSearchRow(0, 'The Beatles Adv search keyword', 'Starts with', 'Keyword (title, contributor, identifier, HRID, UUID)');
-    InventoryInstances.checkAdvSearchModalValues(0, 'The Beatles Adv search keyword', 'Starts with', 'Keyword (title, contributor, identifier, HRID, UUID)');
-    InventoryInstances.fillAdvSearchRow(1, 'Rock music Adv search subj 001', 'Exact phrase', 'Subject', 'AND');
-    InventoryInstances.checkAdvSearchModalValues(1, 'Rock music Adv search subj 001', 'Exact phrase', 'Subject', 'AND');
-    InventoryInstances.clickSearchBtnInAdvSearchModal();
-    InventoryInstances.checkAdvSearchModalAbsence();
-    InventoryInstances.verifySelectedSearchOption(testData.advSearchOption);
-    InventorySearchAndFilter.verifySearchResult(testData.expectedSearchResult);
-    InventorySearchAndFilter.checkRowsCount(1);
+  // it('C400610 Search Instances using advanced search with "AND" operator (spitfire)', { tags: [TestTypes.criticalPath, DevTeams.spitfire] }, () => {
+  //   InventoryInstances.clickAdvSearchButton();
+  //   InventoryInstances.checkAdvSearchInstancesModalFields(0);
+  //   InventoryInstances.checkAdvSearchInstancesModalFields(1);
+  //   InventoryInstances.checkAdvSearchInstancesModalFields(2);
+  //   InventoryInstances.checkAdvSearchInstancesModalFields(3);
+  //   InventoryInstances.checkAdvSearchInstancesModalFields(4);
+  //   InventoryInstances.checkAdvSearchInstancesModalFields(5);
+  //   InventoryInstances.fillAdvSearchRow(0, 'The Beatles Adv search keyword', 'Starts with', 'Keyword (title, contributor, identifier, HRID, UUID)');
+  //   InventoryInstances.checkAdvSearchModalValues(0, 'The Beatles Adv search keyword', 'Starts with', 'Keyword (title, contributor, identifier, HRID, UUID)');
+  //   InventoryInstances.fillAdvSearchRow(1, 'Rock music Adv search subj 001', 'Exact phrase', 'Subject', 'AND');
+  //   InventoryInstances.checkAdvSearchModalValues(1, 'Rock music Adv search subj 001', 'Exact phrase', 'Subject', 'AND');
+  //   InventoryInstances.clickSearchBtnInAdvSearchModal();
+  //   InventoryInstances.checkAdvSearchModalAbsence();
+  //   InventoryInstances.verifySelectedSearchOption(testData.advSearchOption);
+  //   InventorySearchAndFilter.verifySearchResult(testData.expectedSearchResult);
+  //   InventorySearchAndFilter.checkRowsCount(1);
+  // });
+
+  it('C400616 Search Instances using advanced search with a combination of operators (spitfire)', { tags: [TestTypes.criticalPath, DevTeams.spitfire] }, () => {
+    // InventoryInstances.clickAdvSearchButton();
+    // InventoryInstances.checkAdvSearchInstancesModalFields(0);
+    // InventoryInstances.checkAdvSearchInstancesModalFields(1);
+    // InventoryInstances.checkAdvSearchInstancesModalFields(2);
+    // InventoryInstances.checkAdvSearchInstancesModalFields(3);
+    // InventoryInstances.checkAdvSearchInstancesModalFields(4);
+    // InventoryInstances.checkAdvSearchInstancesModalFields(5);
+    // InventoryInstances.fillAdvSearchRow(0, 'The Beatles Adv search keyword', 'Starts with', 'Keyword (title, contributor, identifier, HRID, UUID)');
+    // InventoryInstances.checkAdvSearchModalValues(0, 'The Beatles Adv search keyword', 'Starts with', 'Keyword (title, contributor, identifier, HRID, UUID)');
+    // InventoryInstances.fillAdvSearchRow(1, 'Rock music Adv search subj 001', 'Exact phrase', 'Subject', 'AND');
+    // InventoryInstances.checkAdvSearchModalValues(1, 'Rock music Adv search subj 001', 'Exact phrase', 'Subject', 'AND');
+    // InventoryInstances.clickSearchBtnInAdvSearchModal();
+    // InventoryInstances.checkAdvSearchModalAbsence();
+    // InventoryInstances.verifySelectedSearchOption(testData.advSearchOption);
+    // InventorySearchAndFilter.verifySearchResult(testData.expectedSearchResult);
+    // InventorySearchAndFilter.checkRowsCount(1);
   });
 });
