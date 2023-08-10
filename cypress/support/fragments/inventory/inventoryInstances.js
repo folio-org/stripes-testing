eb11d99579177128bec5cca39bf69500a5957968
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-08-10 19:36:51 +0300

    deleted waiter

--- cypress/support/fragments/inventory/inventoryInstances.js
+++ cypress/support/fragments/inventory/inventoryInstances.js
@@ -51,8 +51,6 @@ export default {
     cy.expect(rootSection.find(HTML(including('Loading…'))).absent());
     cy.expect(or(inventoriesList.exists()),
       rootSection.find(HTML(including('No results found'))).exists());
-    // need to wait until list to be formed
-    cy.wait(2500);
   },
 
   selectInstance: (rowNumber = 0) => {
