3873f13245885bff0cd49e99f7e410d158fafb0b
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2023-08-11 15:56:59 +0500

    small fix in holding records actions

--- cypress/e2e/inventory/holdings/holdings-records-Instancesource-marc.cy.js
+++ cypress/e2e/inventory/holdings/holdings-records-Instancesource-marc.cy.js
@@ -30,11 +30,11 @@ describe('Manage holding records with MARC source', { retries: 2 }, () => {
     cy.wait(10000);
     HoldingsRecordView.getId().then(initialHoldindsRecordId => {
       HoldingsRecordView.checkSource('MARC');
-      HoldingsRecordView.checkActionsMenuOptionsInMarcSource();
-      HoldingsRecordView.tryToDelete();
       //TODO: Delete below two lines of code after Actions -> View source of Holding's view works as expected.
       HoldingsRecordView.close();
       InventoryInstance.openHoldingView();
+      HoldingsRecordView.checkActionsMenuOptionsInMarcSource();
+      HoldingsRecordView.tryToDelete();
       HoldingsRecordView.viewSource();
       InventoryViewSource.close();
       HoldingsRecordView.editInQuickMarc();
