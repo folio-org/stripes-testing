123377f408365f455f413f467f6e71fc8b0f6c95
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-08-25 17:07:03 +0300

    added test

--- cypress/support/fragments/data_import/job_profiles/jobProfileView.js
+++ cypress/support/fragments/data_import/job_profiles/jobProfileView.js
@@ -1,5 +1,12 @@
 import { HTML, including } from '@interactors/html';
-import { Button, Pane } from '../../../../../interactors';
+import { Accordion,
+  Button,
+  Pane,
+  MultiSelect,
+  ValueChipRoot,
+  MultiColumnList,
+  MultiColumnListCell,
+  MultiSelectOption } from '../../../../../interactors';
 
 const viewPane = Pane({ id:'view-job-profile-pane' });
 const resultsPane = Pane({ id:'pane-results' });
@@ -11,9 +18,48 @@ export default {
     cy.do(Button('Edit').click());
   },
 
+  addExistingTag:(tag) => {
+    cy.intercept({
+      method: 'GET',
+      url: '/tags?limit=10000',
+    }).as('getTags');
+    cy.do(Accordion({ id:'tag-accordion' }).clickHeader());
+    cy.wait('@getTags');
+    cy.expect(MultiSelect({ id:'input-tag' }).exists());
+    cy.do(MultiSelect({ id:'input-tag' }).choose(tag));
+  },
+
+  addNewTag:(newTag) => {
+    cy.expect(MultiSelect({ id:'input-tag' }).exists());
+    cy.get('#input-tag-input').type(newTag);
+    cy.expect(MultiSelectOption(including('Add tag for:')).exists());
+    cy.do(MultiSelectOption(including('Add tag for:')).click());
+  },
+
+  removeTag:(tag) => {
+    cy.do(ValueChipRoot(tag).find(Button({ icon: 'times' })).click());
+    cy.expect(ValueChipRoot(tag).absent());
+  },
+
   verifyJobProfileOpened:() => {
     cy.expect(resultsPane.exists());
     cy.expect(viewPane.exists());
+  },
+  verifyAssignedTags:(tag, quantityOfTags = 1) => {
+    cy.expect(MultiSelect({ selectedCount: quantityOfTags }).exists());
+    cy.expect(ValueChipRoot(tag).exists());
+    cy.expect(Pane({ id:'pane-results' })
+      .find(MultiColumnList({ id: 'job-profiles-list' }))
+      .find(MultiColumnListCell({ row: 0, columnIndex: 2, content: including(tag) }))
+      .exists());
+  },
+  verifyAssignedTagsIsAbsent:(tag, quantityOfTags = 1) => {
+    cy.expect(MultiSelect({ selectedCount: quantityOfTags }).exists());
+    cy.expect(ValueChipRoot(tag).absent());
+    cy.expect(Pane({ id:'pane-results' })
+      .find(MultiColumnList({ id: 'job-profiles-list' }))
+      .find(MultiColumnListCell({ row: 0, columnIndex: 2, content: including(tag) }))
+      .absent());
   },
 
   verifyJobProfileName:(profileName) => cy.expect(viewPane.find(HTML(including(profileName))).exists()),
