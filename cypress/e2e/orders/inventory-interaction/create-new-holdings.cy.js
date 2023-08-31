54cc87287809763b44e3768aa8a519480d02a63a
IhorBohdan <ihor_bohdan@epam.com>
2023-08-31 10:26:17 +0300

    Implementing of FAT-5118

--- cypress/e2e/orders/inventory-interaction/create-new-holdings.cy.js
+++ cypress/e2e/orders/inventory-interaction/create-new-holdings.cy.js
@@ -4,7 +4,6 @@ import testType from '../../../support/dictionary/testTypes';
 import getRandomPostfix from '../../../support/utils/stringTools';
 import NewOrder from '../../../support/fragments/orders/newOrder';
 import Orders from '../../../support/fragments/orders/orders';
-import Receiving from '../../../support/fragments/receiving/receiving';
 import TopMenu from '../../../support/fragments/topMenu';
 import Helper from '../../../support/fragments/finance/financeHelper';
 import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
@@ -18,6 +17,9 @@ import InventoryInstance from '../../../support/fragments/inventory/inventoryIns
 import ItemActions from '../../../support/fragments/inventory/inventoryItem/itemActions';
 import { ITEM_STATUS_NAMES } from '../../../support/constants';
 import Users from '../../../support/fragments/users/users';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import InventoryNewHoldings from '../../../support/fragments/inventory/inventoryNewHoldings';
+import ItemRecordEdit from '../../../support/fragments/inventory/item/itemRecordEdit';
 
 describe('Orders: Inventory interaction', () => {
   const order = {
@@ -43,47 +45,60 @@ describe('Orders: Inventory interaction', () => {
   };
   const barcodeForFirstItem = Helper.getRandomBarcode();
   const barcodeForSecondItem = Helper.getRandomBarcode();
-  const changedBarcode = Helper.getRandomBarcode();
 
   let orderNumber;
   let user;
   let effectiveLocationServicePoint;
-  let location;
+  let firstLocation;
+  let instanceRecord = null;
+  let secondLocation;
 
   before(() => {
     cy.getAdminToken();
-
-    ServicePoints.getViaApi({ limit: 1, query: 'name=="Circ Desk 2"' })
-      .then((servicePoints) => {
-        effectiveLocationServicePoint = servicePoints[0];
-        NewLocation.createViaApi(NewLocation.getDefaultLocation(effectiveLocationServicePoint.id))
-          .then((locationResponse) => {
-            location = locationResponse;
-            Organizations.createOrganizationViaApi(organization)
-              .then(organizationsResponse => {
-                organization.id = organizationsResponse;
-                order.vendor = organizationsResponse;
-              });
-
-            cy.loginAsAdmin({ path:TopMenu.ordersPath, waiter: Orders.waitLoading });
-            cy.createOrderApi(order)
-              .then((response) => {
-                orderNumber = response.body.poNumber;
-                Orders.searchByParameter('PO number', orderNumber);
-                Orders.selectFromResultsList(orderNumber);
-                Orders.createPOLineViaActions();
-                OrderLines.selectRandomInstanceInTitleLookUP('*', 10);
-                OrderLines.fillInPOLineInfoForExportWithLocationForPhysicalResource(`${organization.accounts[0].name} (${organization.accounts[0].accountNo})`, 'Purchase', locationResponse.institutionId, '2');
-                OrderLines.backToEditingOrder();
-              });
-          });
-      });
-
+    InventorySearchAndFilter.createInstanceViaApi().then(({ instanceData }) => {
+      instanceRecord = instanceData;
+      ServicePoints.getViaApi({ limit: 1, query: 'name=="Circ Desk 2"' })
+        .then((servicePoints) => {
+          effectiveLocationServicePoint = servicePoints[0];
+          NewLocation.createViaApi(NewLocation.getDefaultLocation(effectiveLocationServicePoint.id))
+            .then((firstLocationResponse) => {
+              firstLocation = firstLocationResponse;
+              Organizations.createOrganizationViaApi(organization)
+                .then(organizationsResponse => {
+                  organization.id = organizationsResponse;
+                  order.vendor = organizationsResponse;
+                });
+              NewLocation.createViaApi(NewLocation.getDefaultLocation(effectiveLocationServicePoint.id))
+                .then((secondLocationResponse) => {
+                  secondLocation = secondLocationResponse;
+                  cy.loginAsAdmin(); cy.visit(TopMenu.inventoryPath).then(() => {
+                    InventoryInstance.searchByTitle(`${instanceRecord.instanceTitle}`);
+                    InventoryInstances.selectInstance();
+                    InventoryInstance.pressAddHoldingsButton();
+                    InventoryNewHoldings.fillRequiredFields(`${firstLocation.name} (${firstLocation.code}) `);
+                    InventoryNewHoldings.saveAndClose();
+                    InventoryInstance.waitLoading();
+                  });
+                  cy.loginAsAdmin({ path:TopMenu.ordersPath, waiter: Orders.waitLoading });
+                  cy.createOrderApi(order)
+                    .then((response) => {
+                      orderNumber = response.body.poNumber;
+                      Orders.searchByParameter('PO number', orderNumber);
+                      Orders.selectFromResultsList(orderNumber);
+                      Orders.createPOLineViaActions();
+                      OrderLines.selectRandomInstanceInTitleLookUP(`${instanceRecord.instanceTitle}`, 0);
+                      OrderLines.fillInPOLineInfoForExportWithLocationForPhysicalResource(`${organization.accounts[0].name} (${organization.accounts[0].accountNo})`, 'Purchase', firstLocation.institutionId, '2');
+                      OrderLines.backToEditingOrder();
+                    });
+                });
+            });
+        });
+    });
     cy.createTempUser([
       permissions.uiInventoryViewInstances.gui,
       permissions.uiOrdersEdit.gui,
-      permissions.uiOrdersView.gui,
-      permissions.uiReceivingViewEditCreate.gui,
+      permissions.uiOrdersApprovePurchaseOrders.gui,
+      permissions.uiInventoryViewCreateEditInstances.gui,
       permissions.uiInventoryViewCreateEditItems.gui,
     ])
       .then(userProperties => {
@@ -93,43 +108,45 @@ describe('Orders: Inventory interaction', () => {
   });
 
   after(() => {
-    cy.loginAsAdmin({ path:TopMenu.receivingPath, waiter: Receiving.waitLoading });
-    Orders.searchByParameter('PO number', orderNumber);
-    Receiving.selectLinkFromResultsList();
-    Receiving.unreceiveFromReceivedSection();
-    cy.visit(TopMenu.ordersPath);
-    Orders.searchByParameter('PO number', orderNumber);
-    Orders.selectFromResultsList();
-    Orders.unOpenOrder(orderNumber);
-    OrderLines.selectPOLInOrder(0);
-    OrderLines.deleteOrderLine();
-    // Need to wait until the order is opened before deleting it
-    cy.wait(2000);
-    Orders.deleteOrderViaApi(order.id);
     Users.deleteViaApi(user.userId);
   });
 
   it('C375238 Create new holdings for already existing location when editing an order line (thunderjet)', { tags: [testType.criticalPath, devTeams.thunderjet] }, () => {
     Orders.searchByParameter('PO number', orderNumber);
     Orders.selectFromResultsList(orderNumber);
+    OrderLines.selectPOLInOrder(0);
+    OrderLines.editPOLInOrder();
+    OrderLines.selectRandomInstanceInTitleLookUP(`${instanceRecord.instanceTitle}`, 0);
+    OrderLines.edeiPOLineInfoAndChangeLocation(`${organization.accounts[0].name} (${organization.accounts[0].accountNo})`, 'Purchase', secondLocation.institutionId, '2');
+    OrderLines.backToEditingOrder();
     Orders.openOrder();
-    Orders.receiveOrderViaActions();
-    Receiving.selectLinkFromResultsList();
-    Receiving.receiveFromExpectedSection();
-    Receiving.receiveAllPhysicalItemsWithBarcodes(barcodeForFirstItem, barcodeForSecondItem);
-    Receiving.clickOnInstance();
-    InventoryInstance.openHoldingsAccordion(location.name);
-    InventorySearchAndFilter.switchToItem();
-    InventorySearchAndFilter.searchByParameter('Barcode', barcodeForFirstItem);
-    ItemRecordView.checkItemDetails(location.name, barcodeForFirstItem, ITEM_STATUS_NAMES.IN_PROCESS);
+    OrderLines.selectPOLInOrder(0);
+    OrderLines.openLinkedInstance();
+    InventoryInstance.checkIsHoldingsCreated([`${firstLocation.name} >`]);
+    InventoryInstance.checkIsHoldingsCreated([`${secondLocation.name} >`]);
+    InventoryInstance.openHoldingsAccordion(secondLocation.name);
+    InventoryInstance.openItemByBarcodeAndIndex('No barcode');
     ItemActions.edit();
-    ItemRecordView.changeItemBarcode(changedBarcode);
-    ItemRecordView.checkItemDetails(location.name, changedBarcode, ITEM_STATUS_NAMES.IN_PROCESS);
+    ItemRecordEdit.addBarcode(barcodeForFirstItem);
+    ItemRecordEdit.save();
+    // Need to wait,while instance will be saved
+    cy.wait(5000);
     ItemActions.closeItem();
-    InventorySearchAndFilter.switchToItem();
-    InventorySearchAndFilter.searchByParameter('Barcode', barcodeForSecondItem);
-    ItemRecordView.checkItemDetails(location.name, barcodeForSecondItem, ITEM_STATUS_NAMES.IN_PROCESS);
+    InventoryInstance.openHoldingsAccordion(secondLocation.name);
+    InventoryInstance.openItemByBarcodeAndIndex('No barcode');
+    ItemActions.edit();
+    ItemRecordEdit.addBarcode(barcodeForSecondItem);
+    ItemRecordEdit.save();
+    // Need to wait,while instance will be saved
+    cy.wait(5000);
     ItemActions.closeItem();
-    InventorySearchAndFilter.switchToItem();
+    InventoryInstance.openHoldingsAccordion(secondLocation.name);
+    InventoryInstance.openItemByBarcodeAndIndex(barcodeForFirstItem);
+    ItemRecordView.checkItemDetails(secondLocation.name, barcodeForFirstItem, ITEM_STATUS_NAMES.ON_ORDER);
+    ItemActions.closeItem();
+    InventoryInstance.openHoldingsAccordion(secondLocation.name);
+    InventoryInstance.openItemByBarcodeAndIndex(barcodeForSecondItem);
+    ItemRecordView.checkItemDetails(secondLocation.name, barcodeForSecondItem, ITEM_STATUS_NAMES.ON_ORDER);
+    ItemActions.closeItem();
   });
 });
