d53493b42d17ad8614a69692b1588c7fa1caaff6
Sherzod-Kenjaev <114989418+Sherzod-Kenjaev@users.noreply.github.com>
2023-09-07 16:12:42 +0500

    FAT-7730/C380755 (#1551)
    * added test C380755
    
    * deleted idealabs version of the test

--- cypress/e2e/marc-authority/manual-linking/link-empty-marc-bib-with-marc-auth-record.cy.js
+++ cypress/e2e/marc-authority/manual-linking/link-empty-marc-bib-with-marc-auth-record.cy.js
@@ -13,47 +13,37 @@ import MarcAuthority from '../../../support/fragments/marcAuthority/marcAuthorit
 import MarcAuthorities from '../../../support/fragments/marcAuthority/marcAuthorities';
 import QuickMarcEditor from '../../../support/fragments/quickMarcEditor';
 
-describe('Manual Unlinking Bib field from Authority 1XX', () => {
-    const testData = {};
+describe('Manual Linking Empty Bib field to Authority 1XX', () => {
+    const testData = {
+      searchOption: 'Personal name',
+      marcValue: 'C380755 Lee, Stan, 1922-2018',
+      markedValue: 'C380755 Lee, Stan,',
+    };
     
     const marcFiles = [
       {
-        marc: 'marcBibFileForC365602.mrc', 
+        marc: 'marcBibFileForC380755.mrc', 
         fileName: `testMarcFile.${getRandomPostfix()}.mrc`, 
         jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
         numOfRecords: 1,
       },
       {
-        marc: 'marcAuthFileForC365602.mrc',
+        marc: 'marcAuthFileForC380755.mrc',
         fileName: `testMarcFile.${getRandomPostfix()}.mrc`,
         jobProfileToRun: 'Default - Create SRS MARC Authority',
-        numOfRecords: 2,
+        numOfRecords: 1,
       },
     ]
-
-    const linkingTagAndValues = [
-      {
-        rowIndex: 76,
-        value: 'Sprouse, Chris',
-        tag: 700
-      },
-      {
-        rowIndex: 77,
-        value: 'Martin, Laura',
-        tag: 700
-      },
-    ];
 
     let createdAuthorityIDs = [];
 
   before('Creating user and records', () => {
     cy.createTempUser([
       Permissions.inventoryAll.gui,
+      Permissions.moduleDataImportEnabled.gui,
       Permissions.uiMarcAuthoritiesAuthorityRecordView.gui,
-      Permissions.uiQuickMarcQuickMarcAuthoritiesEditorAll.gui,
       Permissions.uiQuickMarcQuickMarcBibliographicEditorAll.gui,
       Permissions.uiQuickMarcQuickMarcAuthorityLinkUnlink.gui,
-      Permissions.uiQuickMarcQuickMarcEditorDuplicate.gui,
     ]).then(createdUserProperties => {
       testData.userProperties = createdUserProperties;
 
@@ -73,53 +63,39 @@ describe('Manual Unlinking Bib field from Authority 1XX', () => {
           }
         });
       });
-
-      cy.visit(TopMenu.inventoryPath).then(() => {
-        InventoryInstance.searchByTitle(createdAuthorityIDs[0]);
-        InventoryInstances.selectInstance();
-        InventoryInstance.editMarcBibliographicRecord();
-        linkingTagAndValues.forEach(linking => {
-          QuickMarcEditor.clickLinkIconInTagField(linking.rowIndex);
-          MarcAuthorities.switchToSearch();
-          InventoryInstance.verifySelectMarcAuthorityModal();
-          InventoryInstance.verifySearchOptions();
-          InventoryInstance.searchResults(linking.value);
-          InventoryInstance.clickLinkButton();
-          QuickMarcEditor.verifyAfterLinkingUsingRowIndex(linking.tag, linking.rowIndex);
-        });
-        QuickMarcEditor.pressSaveAndClose();
-        QuickMarcEditor.checkAfterSaveAndClose();
-      });
+      
+      cy.login(testData.userProperties.username, testData.userProperties.password, { path: TopMenu.inventoryPath, waiter: InventoryInstances.waitContentLoading });
     });
   });
 
   after('Deleting created user and records', () => {
     Users.deleteViaApi(testData.userProperties.userId);
     InventoryInstance.deleteInstanceViaApi(createdAuthorityIDs[0]);
-    createdAuthorityIDs.forEach((id, index) => {
-      if (index) MarcAuthority.deleteViaAPI(id);
-    });
+    MarcAuthority.deleteViaAPI(createdAuthorityIDs[1]);
   });
 
-  it('C365602 Derive | Unlink "MARC Bibliographic" field from "MARC Authority" record and use the "Save & close" button in deriving window. (spitfire)', { tags: [TestTypes.extendedPath, DevTeams.spitfire] }, () => {
-    cy.login(testData.userProperties.username, testData.userProperties.password, { path: TopMenu.inventoryPath, waiter: InventoryInstances.waitContentLoading });
+  it('C380755 Link of empty MARC Bib field with "MARC Authority" record (spitfire)', { tags: [TestTypes.extendedPath, DevTeams.spitfire] }, () => {
     InventoryInstance.searchByTitle(createdAuthorityIDs[0]);
     InventoryInstances.selectInstance();
-    InventoryInstance.deriveNewMarcBibRecord();
-    QuickMarcEditor.verifyRemoveLinkingModal('Do you want to remove authority linking for this new bibliographic record?');
-    
-    QuickMarcEditor.clickKeepLinkingButton();
-    QuickMarcEditor.verifyUnlinkAndViewAuthorityButtons(76);
-    QuickMarcEditor.verifyUnlinkAndViewAuthorityButtons(77);
-    QuickMarcEditor.checkButtonSaveAndCloseEnable();
-    QuickMarcEditor.verifyTagFieldAfterLinking(76, '700', '1', '\\', '$a Sprouse, Chris', '$e artist.', '$0 1357871', '');
+    InventoryInstance.editMarcBibliographicRecord();
 
-    QuickMarcEditor.clickUnlinkIconInTagField(76);
-    QuickMarcEditor.verifyTagFieldAfterUnlinking(76, '700', '1', '\\', '$a Sprouse, Chris $e artist. $0 1357871');
-    
+    QuickMarcEditor.checkLinkButtonExistByRowIndex(79);
+    QuickMarcEditor.updateExistingFieldContent(79, '');
+    QuickMarcEditor.clickLinkIconInTagField(79);
+
+    MarcAuthorities.switchToBrowse();
+    MarcAuthorities.checkDefaultBrowseOptions();
+    MarcAuthorities.searchByParameter(testData.searchOption, testData.marcValue);
+    MarcAuthorities.selectTitle(testData.marcValue);
+    MarcAuthorities.checkRecordDetailPageMarkedValue(testData.markedValue);
+    InventoryInstance.clickLinkButton();
+
+    QuickMarcEditor.verifyAfterLinkingUsingRowIndex('700', 79);
+    QuickMarcEditor.verifyUnlinkAndViewAuthorityButtons(79);
+    QuickMarcEditor.verifyTagFieldAfterLinking(79, '700', '1', '\\', '$a C380755 Lee, Stan, $d 1922-2018', '', '$0 id.loc.gov/authorities/names/n83169267', '');
+
     QuickMarcEditor.pressSaveAndClose();
-    QuickMarcEditor.checkCallout('Creating record may take several seconds.');
-    QuickMarcEditor.checkCallout('Record created.');
-    InventoryInstance.checkExistanceOfAuthorityIconInInstanceDetailPane('Contributor');
+    QuickMarcEditor.checkAfterSaveAndClose();
+    InventoryInstance.checkExistanceOfAuthorityIconInInstanceDetailPane('Contributor');    
   });
 });
