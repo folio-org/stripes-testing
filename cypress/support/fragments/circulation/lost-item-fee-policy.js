fd965ac764974632575bdaf2d183af74dd827ce0
ernestyedigaryan1 <ernest_yedigaryan@epam.com>
2023-09-22 17:12:21 +0400

    C10949: Close declared lost loan (vega) (#1631)
    * C10949: Close declared lost loan (vega)
    
    * New implementation
    
    ---------
    
    Co-authored-by: Daniil Novikau <novikaud@gmail.com>

--- cypress/support/fragments/circulation/lost-item-fee-policy.js
+++ cypress/support/fragments/circulation/lost-item-fee-policy.js
@@ -94,7 +94,6 @@ export default {
         isDefaultSearchParamsRequired: false,
       })
       .then(({ body }) => {
-        Cypress.env('lostItemFeesPolicy', body);
         return body;
       });
   },
