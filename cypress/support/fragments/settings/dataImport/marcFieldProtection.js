1d531da5990c3af1c0587dbc052799dd7f054604
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-09-27 13:17:54 +0300

    added test

--- cypress/support/fragments/settings/dataImport/marcFieldProtection.js
+++ cypress/support/fragments/settings/dataImport/marcFieldProtection.js
@@ -4,14 +4,38 @@ import {
   Pane,
   MultiColumnList,
   MultiColumnListCell,
+  MultiColumnListRow,
 } from '../../../../../interactors';
 
 const editList = MultiColumnList({ id: 'editList-marc-field-protection' });
 const newButton = Button('+ New');
 const saveButton = Button('Save');
 
+const clickNewButton = () => cy.do(Pane({ id: 'controlled-vocab-pane' }).find(newButton).click());
+const save = () => {
+  cy.do(saveButton.click());
+};
+const fillMarcFieldProtection = (fieldNumber, subfield = '*', data = '*') => {
+  cy.do(TextField({ name: 'items[0].field' }).fillIn(fieldNumber));
+  if (subfield) {
+    // TODO: redesign to interactors
+    cy.get('input[name="items[0].subfield"]').clear().type(subfield);
+  }
+  if (data) {
+    // TODO: redesign to interactors
+    cy.get('input[name="items[0].data"]').clear().type(data);
+  }
+};
+
 export default {
-  createMarcFieldProtectionViaApi: (fieldBody) => {
+  // actions
+  save,
+  create: (fieldNumber, subfield = '*', data = '*') => {
+    clickNewButton();
+    fillMarcFieldProtection(fieldNumber, subfield, data);
+    save();
+  },
+  createViaApi: (fieldBody) => {
     return cy
       .okapiRequest({
         method: 'POST',
@@ -23,16 +47,14 @@ export default {
         return body;
       });
   },
-
-  deleteMarcFieldProtectionViaApi: (id) => {
+  deleteViaApi: (id) => {
     cy.okapiRequest({
       method: 'DELETE',
       path: `field-protection-settings/marc/${id}`,
       isDefaultSearchParamsRequired: false,
     });
   },
-
-  getListOfMarcFieldProtectionViaApi: (searchParams) => {
+  getListViaApi: (searchParams) => {
     return cy
       .okapiRequest({
         path: 'field-protection-settings/marc',
@@ -44,21 +66,30 @@ export default {
       });
   },
 
-  createNewMarcFieldProtection: () => cy.do(Pane({ id: 'controlled-vocab-pane' }).find(newButton).click()),
-  checkListOfExistingProfilesIsDisplayed: () => cy.expect(editList.exists()),
-  checkFieldProtectionIsCreated: (data) => cy.expect(editList.find(MultiColumnListCell({ content: data })).exists()),
+  // checks
+  verifyListOfExistingProfilesIsDisplayed: () => cy.expect(editList.exists()),
+  verifyFieldProtectionIsCreated: (data) => cy.expect(editList.find(MultiColumnListCell({ content: data })).exists()),
   verifyNewButtonAbsent: () => cy.expect(Pane({ id: 'controlled-vocab-pane' }).find(newButton).absent()),
-
-  fillMarcFieldProtection: (fieldNumber, subfield = '*', data = '*') => {
-    cy.do(TextField({ name: 'items[0].field' }).fillIn(fieldNumber));
-    if (subfield) {
-      // TODO: redesign to interactors
-      cy.get('input[name="items[0].subfield"]').clear().type(subfield);
-    }
-    if (data) {
-      // TODO: redesign to interactors
-      cy.get('input[name="items[0].data"]').clear().type(data);
-    }
-    cy.do(saveButton.click());
+  verifyNewRow: () => {
+    cy.expect([
+      MultiColumnListRow({ rowIndexInParent: 'row-0' })
+        .find(TextField({ name: 'items[0].field' }))
+        .has({ value: '' }),
+      MultiColumnListRow({ rowIndexInParent: 'row-0' })
+        .find(TextField({ name: 'items[0].indicator1' }))
+        .has({ value: '*' }),
+      MultiColumnListRow({ rowIndexInParent: 'row-0' })
+        .find(TextField({ name: 'items[0].indicator2' }))
+        .has({ value: '*' }),
+      MultiColumnListRow({ rowIndexInParent: 'row-0' })
+        .find(TextField({ name: 'items[0].subfield' }))
+        .has({ value: '*' }),
+      MultiColumnListRow({ rowIndexInParent: 'row-0' })
+        .find(TextField({ name: 'items[0].data' }))
+        .has({ value: '*' }),
+      MultiColumnListRow({ rowIndexInParent: 'row-0' })
+        .find(MultiColumnListCell({ content: 'User' }))
+        .exists(),
+    ]);
   },
 };
