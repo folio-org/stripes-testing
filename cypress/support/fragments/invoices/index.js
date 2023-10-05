47a4d7ba141aa6491465a86be6697011ff376b3c
Daniil Novikau <novikaud@gmail.com>
2023-10-05 11:18:44 +0300

    FAT-389582

--- cypress/support/fragments/invoices/index.js
+++ cypress/support/fragments/invoices/index.js
@@ -1,3 +1,4 @@
-export { default as InvoicesLines } from './invoiceLines';
+export { default as InvoiceLines } from './invoiceLines';
 export { default as Invoices } from './invoices';
+export { default as InvoiceView } from './invoiceView';
 export { default as Vouchers } from './vouchers';
