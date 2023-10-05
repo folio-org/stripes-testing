eb9d71a7d6db9fc783d51e147e087ba829179f0f
Ilya_Lyskavets <Ilya_Lyskavets@epam.com>
2023-10-05 19:08:30 +0300

    C350712: Syntax for impots and permissions fixes

--- cypress/e2e/circulation-log/same-date-and-time-format-as-fee-fine-details-page.cy.js
+++ cypress/e2e/circulation-log/same-date-and-time-format-as-fee-fine-details-page.cy.js
@@ -1,77 +1,65 @@
 import uuid from 'uuid';
 import moment from 'moment';
-import { TestTypes } from '../../support/dictionary';
-import devTeams from '../../support/dictionary/devTeams';
-import permissions from '../../support/dictionary/permissions';
-import servicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
-import manualCharges from '../../support/fragments/settings/users/manualCharges';
-import transferAccounts from '../../support/fragments/settings/users/transferAccounts';
-import usersOwners from '../../support/fragments/settings/users/usersOwners';
-import topMenu from '../../support/fragments/topMenu';
-import newFeeFine from '../../support/fragments/users/newFeeFine';
-import users from '../../support/fragments/users/users';
-import userEdit from '../../support/fragments/users/userEdit';
-import searchPane from '../../support/fragments/circulation-log/searchPane';
-import searchResults from '../../support/fragments/circulation-log/searchResults';
-import feeFineDetails from '../../support/fragments/users/feeFineDetails';
-import paymentMethods from '../../support/fragments/settings/users/paymentMethods';
+import { TestTypes, DevTeams, Permissions } from '../../support/dictionary';
+import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import ManualCharges from '../../support/fragments/settings/users/manualCharges';
+import TransferAccounts from '../../support/fragments/settings/users/transferAccounts';
+import UsersOwners from '../../support/fragments/settings/users/usersOwners';
+import TopMenu from '../../support/fragments/topMenu';
+import NewFeeFine from '../../support/fragments/users/newFeeFine';
+import Users from '../../support/fragments/users/users';
+import UserEdit from '../../support/fragments/users/userEdit';
+import SearchPane from '../../support/fragments/circulation-log/searchPane';
+import SearchResults from '../../support/fragments/circulation-log/searchResults';
+import FeeFineDetails from '../../support/fragments/users/feeFineDetails';
+import PaymentMethods from '../../support/fragments/settings/users/paymentMethods';
 
 describe('circulation-log', () => {
   const userData = {};
   const ownerData = {};
   const feeFineType = {};
-  const transferAccount = transferAccounts.getDefaultNewTransferAccount(uuid());
+  const transferAccount = TransferAccounts.getDefaultNewTransferAccount(uuid());
   let feeFineAccount;
   let servicePointId;
 
   before('Create test data', () => {
     cy.getAdminToken();
-    servicePoints.getViaApi({ limit: 1 }).then((servicePoint) => {
+    ServicePoints.getViaApi({ limit: 1 }).then((servicePoint) => {
       servicePointId = servicePoint[0].id;
     });
 
-    usersOwners
-      .createViaApi(usersOwners.getDefaultNewOwner())
+    UsersOwners.createViaApi(UsersOwners.getDefaultNewOwner())
       .then(({ id, owner }) => {
         ownerData.name = owner;
         ownerData.id = id;
       })
       .then(() => {
-        transferAccounts.createViaApi({ ...transferAccount, ownerId: ownerData.id });
-        manualCharges
-          .createViaApi({
-            ...manualCharges.defaultFeeFineType,
-            ownerId: ownerData.id,
-          })
-          .then((manualCharge) => {
-            feeFineType.id = manualCharge.id;
-            feeFineType.name = manualCharge.feeFineType;
-            feeFineType.amount = manualCharge.amount;
-          });
-        paymentMethods.createViaApi(ownerData.id).then(({ name, id }) => {
-          paymentMethods.name = name;
-          paymentMethods.id = id;
+        TransferAccounts.createViaApi({ ...transferAccount, ownerId: ownerData.id });
+        ManualCharges.createViaApi({
+          ...ManualCharges.defaultFeeFineType,
+          ownerId: ownerData.id,
+        }).then((manualCharge) => {
+          feeFineType.id = manualCharge.id;
+          feeFineType.name = manualCharge.feeFineType;
+          feeFineType.amount = manualCharge.amount;
+        });
+        PaymentMethods.createViaApi(ownerData.id).then(({ name, id }) => {
+          PaymentMethods.name = name;
+          PaymentMethods.id = id;
         });
 
         cy.createTempUser([
-          permissions.uiUsersView.gui,
-          permissions.uiUsersfeefinesCRUD.gui,
-          permissions.uiUsersViewServicePoints.gui,
-          permissions.uiUsersfeefinesView.gui,
-          permissions.uiUsersManualCharge.gui,
-          permissions.uiUsersManualPay.gui,
-          permissions.uiUserAccounts.gui,
-          permissions.circulationLogAll.gui,
+          Permissions.circulationLogAll.gui,
+          Permissions.uiUsersfeefinesView.gui,
+          Permissions.uiUsersfeefinesCRUD.gui,
         ])
           .then((userProperties) => {
             userData.username = userProperties.username;
             userData.password = userProperties.password;
             userData.userId = userProperties.userId;
-            userData.barcode = userProperties.barcode;
-            userData.firstName = userProperties.firstName;
           })
           .then(() => {
-            userEdit.addServicePointViaApi(servicePointId, userData.userId);
+            UserEdit.addServicePointViaApi(servicePointId, userData.userId);
             feeFineAccount = {
               id: uuid(),
               ownerId: ownerData.id,
@@ -84,7 +72,7 @@ describe('circulation-log', () => {
               dateAction: moment.utc().format(),
               source: 'ADMINISTRATOR, DIKU',
             };
-            newFeeFine.createViaApi(feeFineAccount).then((feeFineAccountId) => {
+            NewFeeFine.createViaApi(feeFineAccount).then((feeFineAccountId) => {
               feeFineAccount.id = feeFineAccountId;
               cy.login(userData.username, userData.password);
             });
@@ -93,24 +81,24 @@ describe('circulation-log', () => {
   });
 
   after('Delete owner, transfer account, feeFineType, paymentMethod, user', () => {
-    transferAccounts.deleteViaApi(transferAccount.id);
-    manualCharges.deleteViaApi(feeFineType.id);
-    paymentMethods.deleteViaApi(paymentMethods.id);
-    newFeeFine.deleteFeeFineAccountViaApi(feeFineAccount.id);
-    usersOwners.deleteViaApi(ownerData.id);
-    users.deleteViaApi(userData.userId);
+    TransferAccounts.deleteViaApi(transferAccount.id);
+    ManualCharges.deleteViaApi(feeFineType.id);
+    PaymentMethods.deleteViaApi(PaymentMethods.id);
+    NewFeeFine.deleteFeeFineAccountViaApi(feeFineAccount.id);
+    UsersOwners.deleteViaApi(ownerData.id);
+    Users.deleteViaApi(userData.userId);
   });
 
   it(
     'C350712 Check date and time --fee/fines (Firebird)',
-    { tags: [TestTypes.extendedPath, devTeams.firebird] },
+    { tags: [TestTypes.extendedPath, DevTeams.firebird] },
     () => {
-      cy.visit(topMenu.circulationLogPath);
-      searchPane.searchByBilled();
+      cy.visit(TopMenu.circulationLogPath);
+      SearchPane.searchByBilled();
       // Get Billed Date value for the first row
-      searchResults.getBilledDate(0).then((expectedBilledDate) => {
-        searchResults.chooseActionByRow(0, 'Fee/fine details');
-        feeFineDetails.verifyBilledDateValue(expectedBilledDate);
+      SearchResults.getBilledDate(0).then((expectedBilledDate) => {
+        SearchResults.chooseActionByRow(0, 'Fee/fine details');
+        FeeFineDetails.verifyBilledDateValue(expectedBilledDate);
       });
     },
   );
