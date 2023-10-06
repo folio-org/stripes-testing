299a1eada4c82ce68de5689e8f214c01be0d7592
artsiom.bandarenka <artsiom_bandarenka@epam.com>
2023-10-06 20:09:55 +0400

    add C2353

---
+++ cypress/e2e/data-import/settings/delite-mapping-profile.cy.js
@@ -0,0 +1,82 @@
+import getRandomPostfix from '../../../support/utils/stringTools';
+import { DevTeams, Permissions, TestTypes } from '../../../support/dictionary';
+import NewFieldMappingProfile from '../../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
+import NewActionProfile from '../../../support/fragments/data_import/action_profiles/newActionProfile';
+import Users from '../../../support/fragments/users/users';
+import ActionProfiles from '../../../support/fragments/data_import/action_profiles/actionProfiles';
+import FieldMappingProfileView from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfileView';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
+
+describe('data-import', () => {
+  describe('Settings', () => {
+    let user;
+    const notLinkedMappingProfile = `Autotest_mappingProfile${getRandomPostfix()}`;
+    const linkedMappingProfile = `Autotest_mappingProfile${getRandomPostfix()}`;
+    const actionProfile = `Autotest_actionProfile${getRandomPostfix()}`;
+
+    before('login', () => {
+      cy.getAdminToken().then(() => {
+        // create mapping profile profile linked to action profile
+        NewFieldMappingProfile.createMappingProfileViaApi(linkedMappingProfile).then(
+          (mappingProfileResponse) => {
+            NewActionProfile.createActionProfileViaApi(
+              actionProfile,
+              mappingProfileResponse.body.id,
+            );
+          },
+        );
+        // createt not linked mapping profile
+        NewFieldMappingProfile.createMappingProfileViaApi(notLinkedMappingProfile);
+      });
+      cy.createTempUser([
+        Permissions.settingsDataImportEnabled.gui,
+        Permissions.uiInventorySettingsConfigureSingleRecordImport.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+
+        cy.login(user.username, user.password);
+      });
+    });
+
+    after('delete test data', () => {
+      ActionProfiles.deleteActionProfile(actionProfile);
+      FieldMappingProfileView.deleteViaApi(linkedMappingProfile);
+      Users.deleteViaApi(user.userId);
+    });
+
+    it(
+      'C2353 Delete an existing field mapping profile (folijet) (TaaS)',
+      { tags: [TestTypes.extendedPath, DevTeams.folijet] },
+      () => {
+        // #1 Go to "Settings" application-> "Data import" section-> "Field mapping profiles" section
+        cy.visit(SettingsMenu.mappingProfilePath);
+
+        // #2 Click field mapping profiles from the list until you find one with an associated job profile
+        FieldMappingProfiles.search(linkedMappingProfile);
+        FieldMappingProfiles.selectMappingProfileFromList(linkedMappingProfile);
+
+        // #3 Click the Action menu at the top right to display possible actions
+        // #4 Click "Delete" option and confirm by clicking "Delete" again
+        FieldMappingProfileView.delete(linkedMappingProfile);
+        // // You will be presented with a "Cannot delete field mapping profile" prompt and your only option will be to click "Close" and return to the Field mapping profile view
+        FieldMappingProfileView.verifyCannotDeleteModalOpened();
+        FieldMappingProfileView.closeCannotDeleteModal();
+        FieldMappingProfileView.closeViewMode(linkedMappingProfile);
+
+        // #5 Click the field mapping profiles that were created in the precondition
+        FieldMappingProfiles.search(notLinkedMappingProfile);
+        FieldMappingProfiles.selectMappingProfileFromList(notLinkedMappingProfile);
+
+        // #6 Click the Action menu at the top right to display possible actions
+        // #7 Click the "Delete" button
+        // #8 Click  the "Delete" button in the confirmation modal window
+        FieldMappingProfileView.delete(notLinkedMappingProfile);
+        // You are returned to the Field mapping profile list, you are shown a green popup notification of the deletion.
+        FieldMappingProfiles.checkSuccessDelitionCallout(notLinkedMappingProfile);
+        FieldMappingProfiles.search(notLinkedMappingProfile);
+        FieldMappingProfiles.verifyMappingProfileAbsent();
+      },
+    );
+  });
+});
--- cypress/support/fragments/data_import/mapping_profiles/fieldMappingProfiles.js
+++ cypress/support/fragments/data_import/mapping_profiles/fieldMappingProfiles.js
@@ -13,6 +13,7 @@ import { getLongDelay } from '../../../utils/cypressTools';
 import FieldMappingProfileEdit from './fieldMappingProfileEdit';
 import NewFieldMappingProfile from './newFieldMappingProfile';
 import FieldMappingProfileView from './fieldMappingProfileView';
+import Callout from '../../../../../interactors/callout';
 
 const actionsButton = Button('Actions');
 const searchButton = Button('Search');
@@ -114,5 +115,14 @@ export default {
   verifySearchFieldIsEmpty: () => cy.expect(searchField.has({ value: '' })),
   verifySearchResult: (profileName) => {
     cy.expect(resultsPane.find(MultiColumnListCell({ row: 0, content: profileName })).exists());
+  },
+  checkSuccessDelitionCallout: (profileName) => {
+    cy.expect(
+      Callout({
+        textContent: including(
+          `The field mapping profile "${profileName}" was successfully deleted`,
+        ),
+      }).exists(),
+    );
   },
 };
