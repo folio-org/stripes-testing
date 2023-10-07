23013682d0605ab66a35f845997459574fc23c65
TatianaOne <tapusja2014@gmail.com>
2023-10-07 21:38:09 +0300

    added checks

--- cypress/e2e/data-import/settings/error-notification-after-creating-file-extension-with-already-existing-name.cy.js
+++ cypress/e2e/data-import/settings/error-notification-after-creating-file-extension-with-already-existing-name.cy.js
@@ -1,51 +1,42 @@
-import getRandomPostfix from '../../../support/utils/stringTools';
 import { DevTeams, TestTypes, Permissions } from '../../../support/dictionary';
-import { FOLIO_RECORD_TYPE } from '../../../support/constants';
-import NewFieldMappingProfile from '../../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
-import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
-import FieldMappingProfileView from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfileView';
-import ActionProfiles from '../../../support/fragments/data_import/action_profiles/actionProfiles';
 import SettingsMenu from '../../../support/fragments/settingsMenu';
 import Users from '../../../support/fragments/users/users';
-import ActionProfileView from '../../../support/fragments/data_import/action_profiles/actionProfileView';
-import NewActionProfile from '../../../support/fragments/data_import/action_profiles/newActionProfile';
+import FileExtensions from '../../../support/fragments/settings/dataImport/fileExtentions/fileExtensions';
+import NewFileExtention from '../../../support/fragments/settings/dataImport/fileExtentions/newFileExtention';
 
 describe('data-import', () => {
   describe('Settings', () => {
     let user;
-    const mappingProfile = {
-      name: `C404371 autotest mapping profile ${getRandomPostfix()}`,
-      typeValue: FOLIO_RECORD_TYPE.INSTANCE,
+    const testData = {
+      fileExtension: '.dat',
+      dataType: 'MARC',
     };
-    const actionProfile = {
-      typeValue: FOLIO_RECORD_TYPE.HOLDINGS,
-      name: `C404371 autotest action profile ${getRandomPostfix()}`,
-      action: 'Create (all record types except MARC Authority or MARC Holdings)',
-    };
-    const calloutMessage = `Mapping profile '${mappingProfile.name}' can not be linked to this Action profile. ExistingRecordType and FolioRecord types are different`;
+    const calloutMessage = `File extension ${testData.fileExtension} already exists`;
 
     before('create user', () => {
-      cy.createTempUser([Permissions.inventoryAll.gui]).then((userProperties) => {
+      cy.createTempUser([Permissions.settingsDataImportEnabled.gui]).then((userProperties) => {
         user = userProperties;
         cy.login(user.username, user.password);
-
-        // create field mapping profile
-        FieldMappingProfiles.openNewMappingProfileForm();
-        NewFieldMappingProfile.fillSummaryInMappingProfile(mappingProfile);
-        NewFieldMappingProfile.save();
-        FieldMappingProfileView.closeViewMode(mappingProfile.name);
       });
     });
 
     after('delete test data', () => {
       Users.deleteViaApi(user.userId);
-      FieldMappingProfileView.deleteViaApi(mappingProfile.name);
     });
 
     it(
       'C410707 Verify error notification after creating file extension with already existing name (folijet) (TaaS)',
       { tags: [TestTypes.extendedPath, DevTeams.folijet] },
-      () => {},
+      () => {
+        cy.visit(SettingsMenu.fileExtensionsPath);
+        FileExtensions.openNewFileExtentionForm();
+        NewFileExtention.verifyNewFileExtentionFormIsOpened();
+        NewFileExtention.fill(testData);
+        NewFileExtention.save();
+        NewFileExtention.verifyCalloutMessage(calloutMessage);
+        NewFileExtention.verifyPreviouslyPopulatedDataIsNotDisplayed();
+        NewFileExtention.verifyNewFileExtentionFormIsOpened();
+      },
     );
   });
 });
