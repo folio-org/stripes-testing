751e3f4412a84380febc7edc8080fa4787bdb9a4
nayimovag <guliyashnar_nayimova@epam.com>
2023-10-09 17:52:49 +0500

    linter

--- cypress/support/fragments/users/addNewStaffInfo.js
+++ cypress/support/fragments/users/addNewStaffInfo.js
@@ -1,7 +1,7 @@
 import uuid from 'uuid';
 import moment from 'moment';
 import { Button, Modal, TextArea } from '../../../../interactors';
-import FeeFines from './feeFines'
+import FeeFines from './feeFines';
 
 const rootModal = Modal('Staff information');
 const commentTextArea = rootModal.find(TextArea({ name: 'comment' }));
@@ -13,7 +13,7 @@ export default {
   fillIn: (info) => cy.do(commentTextArea.fillIn(info)),
   submit: () => cy.do(rootModal.find(Button('Save')).click()),
   checkStaffInfoModalClosed: () => cy.expect(rootModal.absent()),
-  
+
   addNewStaffInfoViaApi: (userId, staffText) => {
     FeeFines.getUserFeesFines(userId).then((res) => {
       cy.okapiRequest({
@@ -26,7 +26,7 @@ export default {
           id: uuid(),
           source: 'ADMINISTRATOR, Diku_admin',
           transactionInformation: '',
-          typeAction: "Staff info only",
+          typeAction: 'Staff info only',
         }
       });
     });
