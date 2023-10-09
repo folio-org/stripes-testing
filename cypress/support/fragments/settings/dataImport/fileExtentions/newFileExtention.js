7f9199f83bd47258f17e92372962b84e584ebaba
TatianaOne <55943982+TatianaOne@users.noreply.github.com>
2023-10-09 12:12:01 +0300

    C410707 Verify error notification after creating file extension with already existing name (Folijet) (TaaS) (#1778)
    * added test
    
    * added checks

---
+++ cypress/support/fragments/settings/dataImport/fileExtentions/newFileExtention.js
@@ -0,0 +1,33 @@
+import { including } from '@interactors/html';
+import { Pane, TextField, MultiSelect, Button, Callout } from '../../../../../../interactors';
+
+const verifyNewFileExtentionFormIsOpened = () => cy.expect(Pane('New file extension mapping').exists());
+
+export default {
+  verifyNewFileExtentionFormIsOpened,
+  fill: (data) => {
+    // TODO need to wait until page will be uploaded
+    cy.wait(2000);
+    cy.do([
+      TextField({ name: 'extension' }).fillIn(data.fileExtension),
+      MultiSelect({ id: 'multiselect-3' }).select(data.dataType),
+    ]);
+  },
+  save: () => {
+    cy.do(Button('Save as file extension & Close').click());
+  },
+
+  verifyCalloutMessage: (message) => {
+    cy.expect(
+      Callout({
+        textContent: including(message),
+      }).exists(),
+    );
+  },
+  verifyPreviouslyPopulatedDataIsNotDisplayed: () => {
+    cy.expect([
+      TextField({ name: 'extension' }).has({ value: '' }),
+      MultiSelect({ id: 'multiselect-3' }).has({ filterValue: '' }),
+    ]);
+  },
+};
