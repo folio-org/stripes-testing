7f13acb930567523df2ef2b8097dee84da31bcc0
Alexandr Ivashnev <alexandr_ivashnev@epam.com>
2023-10-11 12:05:03 +0300

    c365598 fix depricated method

--- cypress/e2e/marc-authority/marc-bibliographic-unlink-fields.cy.js
+++ cypress/e2e/marc-authority/marc-bibliographic-unlink-fields.cy.js
@@ -84,7 +84,7 @@ describe('MARC -> MARC Bibliographic', () => {
 
           DataImport.uploadFile(marcFile.marc, marcFile.fileName);
           JobProfiles.waitLoadingList();
-          JobProfiles.searchJobProfileForImport(marcFile.jobProfileToRun);
+          JobProfiles.search(marcFile.jobProfileToRun);
           JobProfiles.runImportFile();
           JobProfiles.waitFileIsImported(marcFile.fileName);
           Logs.checkStatusOfJobProfile('Completed');
