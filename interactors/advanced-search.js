059286f0b0172c0248668fa5104ae0bf392ed5d5
Denys Bohdan <denys_bohdan@epam.com>
2023-10-11 15:33:00 +0200

    UITEST-111 Updated `<AdvancedSearch>` interactor for search match support. Updated e2e tests.
    (cherry picked from commit d77670153715715d0269a5cfa7d2788d4cc57a9f)

--- interactors/advanced-search.js
+++ interactors/advanced-search.js
@@ -7,13 +7,15 @@ export const AdvancedSearchRow = HTML.extend('advanced search row')
   .selector('[class*=AdvancedSearchRow-]')
   .filters({
     index: (el) => parseInt(el.getAttribute('data-row-index'), 10),
-    query: (el) => el.querySelector('textarea').getAttribute('value'),
+    query: (el) => el.querySelector('input').getAttribute('value'),
     bool: (el) => el.querySelectorAll('select')[0].value,
-    option: (el) => el.querySelectorAll('select')[1].value,
+    matchOption: (el) => el.querySelectorAll('select')[1].value,
+    option: (el) => el.querySelectorAll('select')[2].value,
   })
   .actions({
-    fillQuery: ({ find }, value) => find(TextArea({ ariaLabel: 'Search for' })).fillIn(value),
+    fillQuery: ({ find }, value) => find(TextArea({ label: 'Search for' })).fillIn(value),
     selectBoolean: ({ find }, rowIndex, value) => find(Select({ id: `advanced-search-bool-${rowIndex}` })).choose(value),
+    selectMatchOption: ({ find }, rowIndex, value) => find(Select({ id: `advanced-match-${rowIndex}` })).choose(value),
     selectSearchOption: ({ find }, rowIndex, value) => find(Select({ id: `advanced-search-option-${rowIndex}` })).choose(value),
   });
 
@@ -25,6 +27,7 @@ export const AdvancedSearch = HTML.extend('advanced search')
     rows,
     id: (el) => el.getAttribute('id'),
     rowCount: (el) => rows(el).length,
+    hasMatchSelect: (el) => [...el.querySelectorAll('[data-test-advanced-match]')].length > 0,
   })
   .actions({
     cancel: ({ find }) => find(Button('Cancel')).click(),
