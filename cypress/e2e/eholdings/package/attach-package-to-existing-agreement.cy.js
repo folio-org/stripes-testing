558f5fdb4bad8cf76123f96e3a5f21b32543fed9
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2023-10-16 20:44:37 +0500

    small change

--- cypress/e2e/eholdings/package/attach-package-to-existing-agreement.cy.js
+++ cypress/e2e/eholdings/package/attach-package-to-existing-agreement.cy.js
@@ -9,67 +9,71 @@ import Users from '../../../support/fragments/users/users';
 import AgreementViewDetails from '../../../support/fragments/agreements/agreementViewDetails';
 import Agreements from '../../../support/fragments/agreements/agreements';
 
-describe('eHoldings -> Package', () => {
-  const testData = {
-    defaultPackage: { ...EHoldingsPackages.getdefaultPackage() },
-    defaultAgreementForAPI: { ...Agreements.defaultAgreement },
-  };
+describe('eHoldings', () => {
+  describe('Package', () => {
+    const testData = {
+      defaultPackage: { ...EHoldingsPackages.getdefaultPackage() },
+      defaultAgreementForAPI: { ...Agreements.defaultAgreement },
+    };
 
-  before('Creating usera and creating data', () => {
-    cy.createTempUser([
-      Permissions.uiAgreementsAgreementsEdit.gui,
-      Permissions.uiAgreementsSearchAndView,
-      Permissions.uieHoldingsRecordsEdit.gui,
-      Permissions.uiAgreementsSearch,
-    ])
-      .then((createdUserProperties) => {
-        testData.userProperties = createdUserProperties;
-        cy.login(testData.userProperties.username, testData.userProperties.password, {
-          path: TopMenu.eholdingsPath,
-          waiter: EHoldingsTitlesSearch.waitLoading,
+    before('Creating usera and creating data', () => {
+      cy.createTempUser([
+        Permissions.uiAgreementsAgreementsEdit.gui,
+        Permissions.uiAgreementsSearchAndView,
+        Permissions.uieHoldingsRecordsEdit.gui,
+        Permissions.uiAgreementsSearch,
+      ])
+        .then((createdUserProperties) => {
+          testData.userProperties = createdUserProperties;
+          cy.login(testData.userProperties.username, testData.userProperties.password, {
+            path: TopMenu.eholdingsPath,
+            waiter: EHoldingsTitlesSearch.waitLoading,
+          });
+        })
+        .then(() => {
+          EHoldingsPackages.createPackageViaAPI().then((response) => {
+            testData.packegeId = response.id;
+          });
+        })
+        .then(() => {
+          Agreements.createViaApi().then((response) => {
+            testData.agreementId = response.id;
+          });
         });
-      })
-      .then(() => {
-        EHoldingsPackages.createPackageViaAPI().then((response) => {
-          testData.packegeId = response.id;
-        });
-      })
-      .then(() => {
-        Agreements.createViaApi().then((response) => {
-          testData.agreementId = response.id;
-        });
-      });
+    });
+
+    after('Deleting user and data', () => {
+      Users.deleteViaApi(testData.userProperties.userId);
+      EHoldingsPackages.deletePackageViaAPI(testData.defaultPackage.data.attributes.name);
+      Agreements.deleteViaApi(testData.agreementId);
+    });
+
+    it(
+      'C751 Attach a package to an existing Agreement (spitfire)',
+      { tags: [TestTypes.criticalPath, DevTeams.spitfire] },
+      () => {
+        EHoldingSearch.switchToPackages();
+        // wait until package is created via API
+        cy.wait(8000);
+        EHoldingsPackagesSearch.byName(testData.defaultPackage.data.attributes.name);
+        EHoldingsPackages.verifyPackageInResults(testData.defaultPackage.data.attributes.name);
+        EHoldingsPackages.openPackage();
+        EHoldingsPackageView.addExistingAgreement();
+        EHoldingsPackageView.searchForExistingAgreement(testData.defaultAgreementForAPI.name);
+        EHoldingsPackageView.clickOnFoundAgreementInModal(testData.defaultAgreementForAPI.name);
+        EHoldingsPackageView.verifyLinkedAgreement(testData.defaultAgreementForAPI.name);
+        EHoldingsPackageView.clickOnAgreementInAgreementSection(
+          testData.defaultAgreementForAPI.name,
+        );
+        AgreementViewDetails.verifyAgreementDetailsIsDisplayedByTitle(
+          testData.defaultAgreementForAPI.name,
+        );
+        AgreementViewDetails.openAgreementLinesSection();
+        AgreementViewDetails.verifyAgreementLinePresented(
+          testData.defaultPackage.data.attributes.name,
+        );
+        AgreementViewDetails.deletionOfAgreementLine();
+      },
+    );
   });
-
-  after('Deleting user and data', () => {
-    Users.deleteViaApi(testData.userProperties.userId);
-    EHoldingsPackages.deletePackageViaAPI(testData.defaultPackage.data.attributes.name);
-    Agreements.deleteViaApi(testData.agreementId);
-  });
-
-  it(
-    'C751 Attach a package to an existing Agreement (spitfire)',
-    { tags: [TestTypes.criticalPath, DevTeams.spitfire] },
-    () => {
-      EHoldingSearch.switchToPackages();
-      // wait until package is created via API
-      cy.wait(8000);
-      EHoldingsPackagesSearch.byName(testData.defaultPackage.data.attributes.name);
-      EHoldingsPackages.verifyPackageInResults(testData.defaultPackage.data.attributes.name);
-      EHoldingsPackages.openPackage();
-      EHoldingsPackageView.addExistingAgreement();
-      EHoldingsPackageView.searchForExistingAgreement(testData.defaultAgreementForAPI.name);
-      EHoldingsPackageView.clickOnFoundAgreementInModal(testData.defaultAgreementForAPI.name);
-      EHoldingsPackageView.verifyLinkedAgreement(testData.defaultAgreementForAPI.name);
-      EHoldingsPackageView.clickOnAgreementInAgreementSection(testData.defaultAgreementForAPI.name);
-      AgreementViewDetails.verifyAgreementDetailsIsDisplayedByTitle(
-        testData.defaultAgreementForAPI.name,
-      );
-      AgreementViewDetails.openAgreementLinesSection();
-      AgreementViewDetails.verifyAgreementLinePresented(
-        testData.defaultPackage.data.attributes.name,
-      );
-      AgreementViewDetails.deletionOfAgreementLine();
-    },
-  );
 });
