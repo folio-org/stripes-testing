20621f6b0766e7c0f1ae094e143ca6f8d61c1d43
blue-raisin <bunicorn486@gmail.com>
2023-10-16 11:19:35 +0500

    C350907 Verify Bulk Edit form -- In app approach (firebird)

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-items-form.cy.js
@@ -0,0 +1,70 @@
+import TopMenu from '../../../support/fragments/topMenu';
+import testTypes from '../../../support/dictionary/testTypes';
+import permissions from '../../../support/dictionary/permissions';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import devTeams from '../../../support/dictionary/devTeams';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import FileManager from '../../../support/utils/fileManager';
+import Users from '../../../support/fragments/users/users';
+import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';
+
+let user;
+const item = {
+  barcode: getRandomPostfix(),
+  instanceName: `instance-${getRandomPostfix()}`,
+};
+const itemBarcodesFileName = `itemBarcodes_${getRandomPostfix()}.csv`;
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+      cy.createTempUser([
+        permissions.bulkEditView.gui,
+        permissions.bulkEditEdit.gui,
+        permissions.uiInventoryViewCreateEditDeleteItems.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password, {
+          path: TopMenu.bulkEditPath,
+          waiter: BulkEditSearchPane.waitLoading,
+        });
+        InventoryInstances.createInstanceViaApi(item.instanceName, item.barcode);
+        FileManager.createFile(`cypress/fixtures/${itemBarcodesFileName}`, item.barcode);
+      });
+    });
+
+    after('delete test data', () => {
+      InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.barcode);
+      Users.deleteViaApi(user.userId);
+      FileManager.deleteFile(`cypress/fixtures/${itemBarcodesFileName}`);
+    });
+
+    it(
+      'C350907 Verify Bulk Edit form -- In app approach (firebird) (TaaS)',
+      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      () => {
+        BulkEditSearchPane.checkItemsRadio();
+        BulkEditSearchPane.selectRecordIdentifier('Item barcode');
+
+        BulkEditSearchPane.uploadFile(itemBarcodesFileName);
+        BulkEditSearchPane.waitFileUploading();
+        BulkEditSearchPane.verifyMatchedResults(item.barcode);
+
+        BulkEditActions.openActions();
+        BulkEditActions.openInAppStartBulkEditFrom();
+
+        const newLocation = 'Online';
+        BulkEditActions.verifyModifyLandingPageBeforeModifying();
+        BulkEditActions.verifyItemOptions();
+        BulkEditActions.replacePermanentLocation(newLocation);
+        BulkEditActions.clickLocationLookup();
+        BulkEditActions.verifyLocationLookupModal();
+        BulkEditActions.replaceTemporaryLocation(newLocation);
+        BulkEditActions.addNewBulkEditFilterString();
+        BulkEditActions.clearTemporaryLocation();
+        BulkEditActions.verifyModifyLandingPageAfterModifying();
+      },
+    );
+  });
+});
