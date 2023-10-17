f291b3a6454a1035d2081e86ed6fd9f943cffadc
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2023-10-17 17:05:33 +0300

    added tests for FAT-1425

---
+++ cypress/e2e/requests/patron-comments-request-csv.cy.js
@@ -0,0 +1,152 @@
+import uuid from 'uuid';
+import TestTypes from '../../support/dictionary/testTypes';
+import EditRequest from '../../support/fragments/requests/edit-request';
+import TopMenu from '../../support/fragments/topMenu';
+import Requests from '../../support/fragments/requests/requests';
+import Users from '../../support/fragments/users/users';
+import DevTeams from '../../support/dictionary/devTeams';
+import Permissions from '../../support/dictionary/permissions';
+import Parallelization from '../../support/dictionary/parallelization';
+import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import UserEdit from '../../support/fragments/users/userEdit';
+import {
+  ITEM_STATUS_NAMES,
+  REQUEST_TYPES,
+  REQUEST_LEVELS,
+  FULFILMENT_PREFERENCES,
+} from '../../support/constants';
+import generateItemBarcode from '../../support/utils/generateItemBarcode';
+import getRandomPostfix from '../../support/utils/stringTools';
+import Location from '../../support/fragments/settings/tenant/locations/newLocation';
+import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
+
+describe('Requests Export CSV File', () => {
+  const patronComment = 'patron test comment';
+  const fileName = 'export.csv';
+
+  const userData = {};
+  let cancellationReason;
+  const servicePoint = ServicePoints.getDefaultServicePointWithPickUpLocation();
+
+  const itemData = {
+    barcode: generateItemBarcode(),
+    instanceTitle: `Instance ${getRandomPostfix()}`,
+  };
+  let defaultLocation;
+
+  const requestData = {
+    id: uuid(),
+    requestType: REQUEST_TYPES.PAGE,
+    requesterId: null,
+    requestLevel: REQUEST_LEVELS.ITEM,
+    requestDate: new Date().toISOString(),
+    fulfillmentPreference: FULFILMENT_PREFERENCES.HOLD_SHELF,
+  };
+
+  before('Create New Item and New User', () => {
+    cy.getAdminToken()
+      .then(() => {
+        ServicePoints.createViaApi(servicePoint);
+        defaultLocation = Location.getDefaultLocation(servicePoint.id);
+        Location.createViaApi(defaultLocation);
+        cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
+          itemData.instanceTypeId = instanceTypes[0].id;
+        });
+        cy.getHoldingTypes({ limit: 1 }).then((res) => {
+          itemData.holdingTypeId = res[0].id;
+        });
+        cy.getLoanTypes({ limit: 1 }).then((res) => {
+          itemData.loanTypeId = res[0].id;
+        });
+        cy.getMaterialTypes({ limit: 1 }).then((res) => {
+          itemData.materialTypeId = res.id;
+          itemData.materialTypeName = res.name;
+        });
+      })
+      .then(() => {
+        InventoryInstances.createFolioInstanceViaApi({
+          instance: {
+            instanceTypeId: itemData.instanceTypeId,
+            title: itemData.instanceTitle,
+          },
+          holdings: [
+            {
+              holdingsTypeId: itemData.holdingTypeId,
+              permanentLocationId: defaultLocation.id,
+            },
+          ],
+          items: [
+            {
+              barcode: itemData.barcode,
+              status: { name: ITEM_STATUS_NAMES.AVAILABLE },
+              permanentLoanType: { id: itemData.loanTypeId },
+              materialType: { id: itemData.materialTypeId },
+            },
+          ],
+        });
+      })
+      .then((specialInstanceIds) => {
+        itemData.testInstanceIds = specialInstanceIds;
+      });
+
+    cy.createTempUser([Permissions.requestsAll.gui, Permissions.checkinAll.gui], userData.group)
+      .then((userProperties) => {
+        userData.username = userProperties.username;
+        userData.password = userProperties.password;
+        userData.userId = userProperties.userId;
+        userData.barcode = userProperties.barcode;
+        userData.firstName = userProperties.firstName;
+      })
+      .then(() => {
+        cy.wrap(true)
+          .then(() => {
+            requestData.instanceId = itemData.testInstanceIds.instanceId;
+            requestData.holdingsRecordId = itemData.testInstanceIds.holdingIds[0].id;
+            requestData.itemId = itemData.testInstanceIds.holdingIds[0].itemIds[0];
+            requestData.requesterId = userData.userId;
+            requestData.pickupServicePointId = servicePoint.id;
+            requestData.patronComments = patronComment;
+          })
+          .then(() => {
+            cy.createItemRequestApi(requestData);
+          });
+
+        UserEdit.addServicePointViaApi(servicePoint.id, userData.userId, servicePoint.id);
+
+        cy.login(userData.username, userData.password);
+        cy.visit(TopMenu.requestsPath);
+      });
+  });
+
+  after('Delete New Service point, Item and User', () => {
+    EditRequest.updateRequestApi({
+      ...requestData,
+      status: 'Closed - Cancelled',
+      cancelledByUserId: requestData.requesterId,
+      cancellationReasonId: cancellationReason,
+      cancelledDate: new Date().toISOString(),
+    });
+
+    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(itemData.barcode);
+    UserEdit.changeServicePointPreferenceViaApi(userData.userId, [servicePoint.id]);
+    Users.deleteViaApi(userData.userId);
+    Location.deleteViaApiIncludingInstitutionCampusLibrary(
+      defaultLocation.institutionId,
+      defaultLocation.campusId,
+      defaultLocation.libraryId,
+      defaultLocation.id,
+    );
+    ServicePoints.deleteViaApi(servicePoint.id);
+  });
+
+  it(
+    'C199705 Patron Comments are Displayed in Requests Export CSV File (vega)',
+    { tags: [TestTypes.criticalPath, DevTeams.vega, Parallelization.nonParallel] },
+    () => {
+      Requests.selectNotYetFilledRequest();
+      Requests.findCreatedRequest(itemData.barcode);
+      Requests.exportRequestToCsv();
+      Requests.checkCellInCsvFileContainsValue(fileName, 1, 30, patronComment);
+    },
+  );
+});
