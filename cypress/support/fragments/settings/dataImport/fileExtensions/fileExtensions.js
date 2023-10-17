4dedcf414f3734da51c95dbe765b95f0f111f656
TatianaOne <tapusja2014@gmail.com>
2023-10-17 12:06:48 +0300

    added checks

--- cypress/support/fragments/settings/dataImport/fileExtensions/fileExtensions.js
+++ cypress/support/fragments/settings/dataImport/fileExtensions/fileExtensions.js
@@ -6,6 +6,7 @@ import {
   MultiColumnList,
   MultiColumnListCell,
   Callout,
+  MultiColumnListRow,
 } from '../../../../../../interactors';
 import { REQUEST_METHOD } from '../../../../constants';
 
@@ -20,7 +21,27 @@ const defaultFileExtension = {
   dataTypes: ['MARC'],
 };
 
+function getFileExtensionNames() {
+  const cells = [];
+
+  cy.wait(2000);
+  return cy
+    .get('div[class^="mclRowContainer--"]')
+    .find('[data-row-index]')
+    .each(($row) => {
+      // from each row, choose specific cell
+      cy.get('[class*="mclCell-"]:nth-child(1)', { withinSubject: $row })
+        // extract its text content
+        .invoke('text')
+        .then((cellValue) => {
+          cells.push(cellValue);
+        });
+    })
+    .then(() => cells);
+}
+
 export default {
+  getFileExtensionNames,
   createViaApi: () => {
     return cy
       .okapiRequest({
@@ -36,29 +57,37 @@ export default {
       extensionsList.find(MultiColumnListCell({ column: 'Extension', content: extension })).click(),
     );
   },
+  openNewFileExtensionForm: () => {
+    cy.do([resultsPane.find(actionsButton).click(), Button('New file extension').click()]);
+  },
+  verifyActionMenuOnViewPaneAbsent: () => cy.expect(Pane({ id: 'view-file-extension-pane' }).find(actionsButton).absent()),
   verifyListOfExistingFileExtensionsIsDisplayed: () => cy.expect(resultsPane.exists()),
   verifyActionMenuAbsent: () => cy.expect(resultsPane.find(actionsButton).absent()),
   verifyCalloutMessage: (message) => {
     cy.expect(Callout({ textContent: including(message) }).exists());
   },
-  verifyDeletedFileExtensionAbsent: (extention) => {
-    const cells = [];
 
-    cy.wait(2000);
-    return cy
-      .get('div[class^="mclRowContainer--"]')
-      .find('[data-row-index]')
-      .each(($row) => {
-        // from each row, choose specific cell
-        cy.get('[class*="mclCell-"]:nth-child(1)', { withinSubject: $row })
-          // extract its text content
-          .invoke('text')
-          .then((cellValue) => {
-            cells.push(cellValue);
-          });
-      })
-      .then(() => {
-        cy.expect(cells).to.not.deep.equal(extention);
-      });
+  verifyDeletedFileExtensionAbsent: (extention) => {
+    getFileExtensionNames().then((cells) => {
+      cy.expect(cells).to.not.deep.equal(extention);
+    });
+  },
+  verifyCreateFileExtensionPresented: (extention) => {
+    getFileExtensionNames().then((cells) => {
+      cy.expect(cells).to.include(extention);
+    });
+  },
+  verifyCreatedFileExtension: (extension, importStatus) => {
+    cy.do(
+      MultiColumnListCell({ content: extension }).perform((element) => {
+        const rowNumber = element.closest('[data-row-index').getAttribute('data-row-index');
+
+        cy.expect(
+          MultiColumnListRow({ rowIndexInParent: rowNumber })
+            .find(MultiColumnListCell({ columnIndex: 1 }))
+            .has({ content: importStatus }),
+        );
+      }),
+    );
   },
 };
