12d7cf34c51a78e9b3b942bb13b634ab50f93b73
TatianaOne <tapusja2014@gmail.com>
2023-10-18 22:14:19 +0300

    added test

---
+++ cypress/e2e/data-import/importing-marc-bib-files/import-marc-bib-file-using-default-mapping-profile.cy.js
@@ -0,0 +1,54 @@
+import getRandomPostfix from '../../../support/utils/stringTools';
+import { DevTeams, TestTypes, Permissions } from '../../../support/dictionary';
+import { JOB_STATUS_NAMES } from '../../../support/constants';
+import TopMenu from '../../../support/fragments/topMenu';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import Logs from '../../../support/fragments/data_import/logs/logs';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import Users from '../../../support/fragments/users/users';
+
+describe('data-import', () => {
+  describe('Importing MARC Bib files', () => {
+    let user;
+    const filePath = 'oneMarcBib.mrc';
+    const marcFileName = `C2325 autotestFileName ${getRandomPostfix()}`;
+    const jobProfileToRun = 'Default - Create instance and SRS MARC Bib';
+
+    before('login', () => {
+      cy.createTempUser([
+        Permissions.moduleDataImportEnabled.gui,
+        Permissions.settingsDataImportEnabled.gui,
+        Permissions.inventoryAll.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+
+        cy.login(userProperties.username, userProperties.password, {
+          path: TopMenu.dataImportPath,
+          waiter: DataImport.waitLoading,
+        });
+      });
+    });
+
+    after('delete test data', () => {
+      Users.deleteViaApi(user.userId);
+    });
+
+    it(
+      'C2325 Import a MARC Bib file using the default mapping profile (folijet)',
+      { tags: [TestTypes.criticalPath, DevTeams.folijet] },
+      () => {
+        // upload a marc file
+        cy.visit(TopMenu.dataImportPath);
+        // TODO delete function after fix https://issues.folio.org/browse/MODDATAIMP-691
+        DataImport.verifyUploadState();
+        DataImport.uploadFile(filePath, marcFileName);
+        JobProfiles.search(jobProfileToRun);
+        JobProfiles.runImportFile();
+        DataImport.checkIsLandingPageOpened();
+        Logs.checkFileIsRunning(marcFileName);
+        JobProfiles.waitFileIsImported(marcFileName);
+        Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
+      },
+    );
+  });
+});
