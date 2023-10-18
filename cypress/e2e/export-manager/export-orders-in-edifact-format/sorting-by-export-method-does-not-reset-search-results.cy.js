401c850993f3c431d16091e60c4163999586a819
kirstaVova <grouplbua@gmail.com>
2023-10-18 22:13:30 +0300

    C377045: Sorting by export method does not reset search results (thunderjet) (TaaS)

---
+++ cypress/e2e/export-manager/export-orders-in-edifact-format/sorting-by-export-method-does-not-reset-search-results.cy.js
@@ -0,0 +1,124 @@
+import permissions from '../../../support/dictionary/permissions';
+import devTeams from '../../../support/dictionary/devTeams';
+import TopMenu from '../../../support/fragments/topMenu';
+import Orders from '../../../support/fragments/orders/orders';
+import TestTypes from '../../../support/dictionary/testTypes';
+import Users from '../../../support/fragments/users/users';
+import NewOrder from '../../../support/fragments/orders/newOrder';
+import Organizations from '../../../support/fragments/organizations/organizations';
+import NewOrganization from '../../../support/fragments/organizations/newOrganization';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import OrderLines from '../../../support/fragments/orders/orderLines';
+import ServicePoints from '../../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import NewLocation from '../../../support/fragments/settings/tenant/locations/newLocation';
+import DateTools from '../../../support/utils/dateTools';
+
+describe('orders: export', () => {
+  const order = { ...NewOrder.defaultOneTimeOrder };
+  const organization = {
+    ...NewOrganization.defaultUiOrganizations,
+    accounts: [
+      {
+        accountNo: getRandomPostfix(),
+        accountStatus: 'Active',
+        acqUnitIds: [],
+        appSystemNo: '',
+        description: 'Main library account',
+        libraryCode: 'COB',
+        libraryEdiCode: getRandomPostfix(),
+        name: 'TestAccout1',
+        notes: '',
+        paymentMethod: 'Cash',
+      },
+    ],
+  };
+  const integrationName1 = `FirstIntegrationName${getRandomPostfix()}`;
+  const integartionDescription1 = 'Test Integation descripton1';
+  const vendorEDICodeFor1Integration = getRandomPostfix();
+  const libraryEDICodeFor1Integration = getRandomPostfix();
+
+  let user;
+  let location;
+  let servicePointId;
+  const UTCTime = DateTools.getUTCDateForScheduling();
+
+  before(() => {
+    cy.getAdminToken();
+
+    ServicePoints.getViaApi().then((servicePoint) => {
+      servicePointId = servicePoint[0].id;
+      NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then((res) => {
+        location = res;
+      });
+    });
+
+    Organizations.createOrganizationViaApi(organization).then((organizationsResponse) => {
+      organization.id = organizationsResponse;
+      order.vendor = organization.name;
+      order.orderType = 'One-time';
+    });
+    cy.loginAsAdmin({ path: TopMenu.organizationsPath, waiter: Organizations.waitLoading });
+    Organizations.searchByParameters('Name', organization.name);
+    Organizations.checkSearchResults(organization);
+    Organizations.selectOrganization(organization.name);
+    Organizations.addIntegration();
+    Organizations.fillIntegrationInformation(
+      integrationName1,
+      integartionDescription1,
+      vendorEDICodeFor1Integration,
+      libraryEDICodeFor1Integration,
+      organization.accounts[0].accountNo,
+      'Purchase',
+      UTCTime,
+    );
+
+    cy.createTempUser([
+      permissions.uiOrdersView.gui,
+      permissions.uiOrdersCreate.gui,
+      permissions.uiOrdersEdit.gui,
+      permissions.uiOrdersApprovePurchaseOrders.gui,
+      permissions.uiOrganizationsViewEditCreate.gui,
+      permissions.uiOrganizationsView.gui,
+      permissions.uiExportOrders.gui,
+      permissions.exportManagerAll.gui,
+      permissions.exportManagerDownloadAndResendFiles.gui,
+    ]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password, {
+        path: TopMenu.ordersPath,
+        waiter: Orders.waitLoading,
+      });
+    });
+  });
+
+  after(() => {
+    Orders.deleteOrderViaApi(order.id);
+    Organizations.deleteOrganizationViaApi(organization.id);
+    NewLocation.deleteViaApiIncludingInstitutionCampusLibrary(
+      location.institutionId,
+      location.campusId,
+      location.libraryId,
+      location.id,
+    );
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C377045: Sorting by export method does not reset search results (thunderjet) (TaaS)',
+    { tags: [TestTypes.smoke, devTeams.thunderjet] },
+    () => {
+      cy.visit(TopMenu.ordersPath);
+      Orders.createOrder(order, true, false).then((orderId) => {
+        order.id = orderId;
+        Orders.createPOLineViaActions();
+        OrderLines.selectRandomInstanceInTitleLookUP('*', 3);
+        OrderLines.fillInPOLineInfoForExportWithLocationForPhysicalResource(
+          'Purchase',
+          location.institutionId,
+          '3',
+        );
+        OrderLines.backToEditingOrder();
+      });
+    },
+  );
+});
