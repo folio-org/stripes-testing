c82275ab272f57dc73ee86c9dad3b1eed5269b6c
ernestyedigaryan1 <ernest_yedigaryan@epam.com>
2023-10-18 20:26:15 +0400

    C380512: Verify that all notes assigned to user pop up when user scan patron card (“Close” option) (Spitfire) (TaaS) (#1734)
    * C380512: Verify that all notes assigned to user pop up when user scan patron card (“Close” option) (Spitfire) (TaaS)
    
    * made changes after code review

--- cypress/e2e/checkout/checkout.notes.cy.js
+++ cypress/e2e/checkout/checkout.notes.cy.js
@@ -1,21 +1,26 @@
 import { HTML, including } from '@interactors/html';
 
-import Permissions from '../../support/dictionary/permissions';
 import TopMenu from '../../support/fragments/topMenu';
 import UsersSearchPane from '../../support/fragments/users/usersSearchPane';
 import AgreementsDetails from '../../support/fragments/agreements/agreementViewDetails';
 import usersCard from '../../support/fragments/users/usersCard';
 import CheckOutActions from '../../support/fragments/check-out-actions/check-out-actions';
-import NewServicePoint from '../../support/fragments/settings/tenant/servicePoints/newServicePoint';
 import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
 import UserEdit from '../../support/fragments/users/userEdit';
 import Users from '../../support/fragments/users/users';
-import TestTypes from '../../support/dictionary/testTypes';
-import DevTeams from '../../support/dictionary/devTeams';
+import { TestTypes, DevTeams, Permissions } from '../../support/dictionary';
+import Location from '../../support/fragments/settings/tenant/locations/newLocation';
+import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
 
 describe('Check out - Notes', () => {
   let testData;
-  let servicePoint;
+  const instanceData = {
+    folioInstances: InventoryInstances.generateFolioInstances(),
+    servicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
+    requestsId: '',
+  };
+  const note1 = { title: 'Note 1', details: 'This is Note 1' };
+  const note2 = { title: 'Note 2', details: 'This is Note 2' };
 
   before('Creating data', () => {
     cy.createTempUser([
@@ -27,50 +32,106 @@ describe('Check out - Notes', () => {
       Permissions.uiUsersView.gui,
     ]).then((createdUserProperties) => {
       testData = createdUserProperties;
-      servicePoint = NewServicePoint.getDefaultServicePoint();
-      ServicePoints.createViaApi(servicePoint);
+      ServicePoints.createViaApi(instanceData.servicePoint);
+      instanceData.defaultLocation = Location.getDefaultLocation(instanceData.servicePoint.id);
+      Location.createViaApi(instanceData.defaultLocation).then((location) => {
+        InventoryInstances.createFolioInstancesViaApi({
+          folioInstances: instanceData.folioInstances,
+          location,
+        });
+      });
       UserEdit.addServicePointViaApi(
-        servicePoint.id,
+        instanceData.servicePoint.id,
         createdUserProperties.userId,
-        servicePoint.id,
+        instanceData.servicePoint.id,
       );
       cy.login(testData.username, testData.password);
-      cy.visit(TopMenu.usersPath);
-      UsersSearchPane.waitLoading();
     });
   });
 
+  beforeEach('Create notes', () => {
+    cy.visit(TopMenu.usersPath);
+    UsersSearchPane.waitLoading();
+    UsersSearchPane.searchByUsername(testData.username);
+    UsersSearchPane.waitLoading();
+    // Scroll down to "Notes" accordion button and click on it.
+    usersCard.openNotesSection();
+    // Create Note 1
+    AgreementsDetails.createNote({ ...note1, checkoutApp: true });
+    // Scroll down to "Notes" accordion button and click on it.
+    usersCard.openNotesSection();
+    // Create Note 2
+    AgreementsDetails.createNote({ ...note2, checkoutApp: true });
+    // Navigate to "Check out" app
+    cy.visit(TopMenu.checkOutPath);
+  });
+
   after('Deleting created entities', () => {
-    UserEdit.changeServicePointPreferenceViaApi(testData.userId, [servicePoint.id]);
-    ServicePoints.deleteViaApi(servicePoint.id);
+    UserEdit.changeServicePointPreferenceViaApi(testData.userId, [instanceData.servicePoint.id]);
+    ServicePoints.deleteViaApi(instanceData.servicePoint.id);
+    InventoryInstances.deleteInstanceViaApi({
+      instance: instanceData.folioInstances[0],
+      servicePoint: instanceData.servicePoint,
+      shouldCheckIn: true,
+    });
     Users.deleteViaApi(testData.userId);
   });
 
+  // May be failing because of this bug (https://issues.folio.org/browse/STSMACOM-783)
   it(
     'C356781: Verify that all notes assigned to user pop up when user scan patron card (“Delete” option) (Spitfire) (TaaS)',
     { tags: [TestTypes.criticalPath, DevTeams.spitfire] },
     () => {
-      const note1 = { title: 'Note 1', details: 'This is Note 1' };
-      const note2 = { title: 'Note 2', details: 'This is Note 2' };
-
-      UsersSearchPane.searchByUsername(testData.username);
-      UsersSearchPane.waitLoading();
-      usersCard.openNotesSection();
-      AgreementsDetails.createNote({ ...note1, checkoutApp: true });
-      usersCard.openNotesSection();
-      AgreementsDetails.createNote({ ...note2, checkoutApp: true });
-      cy.visit(TopMenu.checkOutPath);
+      // Fill in user barcode number in the input field at "Scan patron card" pane → Click "Enter" button.
       CheckOutActions.checkOutUser(testData.barcode);
+      // Modal window "Note for patron" with the Note 1 is displayed.
       CheckOutActions.checkUserNote(note1);
+      // Click on the "Delete note" button.
       CheckOutActions.deleteNote();
+      // Modal window "Note for patron" with the Note 2 is displayed
       CheckOutActions.checkUserNote(note2);
+      // Click on the "Delete note" button.
       CheckOutActions.deleteNote();
+      // Open user Details
       cy.visit(TopMenu.usersPath);
       UsersSearchPane.waitLoading();
       UsersSearchPane.searchByUsername(testData.username);
       UsersSearchPane.waitLoading();
+      // Scroll down to "Notes" accordion button and click on it.
       usersCard.openNotesSection();
+      // "Notes" accordion button expanded and the message "No notes found" displayed.
       cy.expect(HTML(including('No notes found')).exists());
+    },
+  );
+
+  // May be failing because of this bug (https://issues.folio.org/browse/STSMACOM-783)
+  it(
+    'C380512: Verify that all notes assigned to user pop up when user scan patron card (“Close” option) (Spitfire) (TaaS)',
+    { tags: [TestTypes.criticalPath, DevTeams.spitfire] },
+    () => {
+      const itemBarcode = instanceData.folioInstances[0].barcodes[0];
+      // Fill in user barcode number in the input field at "Scan patron card" pane → Click "Enter" button.
+      CheckOutActions.checkOutUser(testData.barcode);
+      // Modal window "Note for patron" with the Note 1 is displayed.
+      CheckOutActions.checkUserNote(note1);
+      // Click on the "Close" button.
+      CheckOutActions.closeNote();
+      // Modal window "Note for patron" with the Note 2 is displayed.
+      CheckOutActions.checkUserNote(note2);
+      // Click on the "Close" button.
+      CheckOutActions.closeNote();
+      // Input any valid item barcode in input field at "Scan items" pane → Click "Enter" button
+      CheckOutActions.checkOutItem(itemBarcode);
+      // Modals with user notes do NOT appear
+      CheckOutActions.checkNoteModalNotDisplayed();
+      // Repeat step "Checkout Item"
+      CheckOutActions.checkOutItem(itemBarcode);
+      // Modals with user notes do NOT appear
+      CheckOutActions.checkNoteModalNotDisplayed();
+      // Repeat step "Checkout Item" one more time
+      CheckOutActions.checkOutItem(itemBarcode);
+      // Modals with user notes do NOT appear
+      CheckOutActions.checkNoteModalNotDisplayed();
     },
   );
 });
