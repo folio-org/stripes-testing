f178ba4350b75b8ee44df7de103ba4ff265bb86c
Arsen_Hovhannisyan <arsen_hovhannisyan@epam.com>
2023-10-20 14:08:57 +0400

    C345342 Hide placeholder attribute once user populates the first row (firebird)

---
+++ cypress/e2e/settings/data-export/mapping-profiles-transformations-placeholder-attribute.cy.js
@@ -0,0 +1,93 @@
+import devTeams from '../../../support/dictionary/devTeams';
+import Users from '../../../support/fragments/users/users';
+import permissions from '../../../support/dictionary/permissions';
+import TopMenu from '../../../support/fragments/topMenu';
+import SettingsPane from '../../../support/fragments/settings/settingsPane';
+import ExportFieldMappingProfiles from '../../../support/fragments/data-export/exportMappingProfile/exportFieldMappingProfiles';
+import ExportNewFieldMappingProfile from '../../../support/fragments/data-export/exportMappingProfile/exportNewFieldMappingProfile';
+import ModalSelectTransformations from '../../../support/fragments/data-export/exportMappingProfile/modalSelectTransformations';
+import InteractorsTools from '../../../support/utils/interactorsTools';
+import { TestTypes } from '../../../support/dictionary';
+
+let user;
+const newTransformationCalloutMessage = '1 transformation has been successfully added';
+
+describe('settings: data-export: Field mapping profiles', () => {
+  before('create test data', () => {
+    cy.createTempUser([
+      permissions.dataExportEnableSettings.gui,
+      permissions.dataExportEnableApp.gui,
+    ]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password, {
+        path: TopMenu.settingsPath,
+        waiter: SettingsPane.waitLoading,
+      });
+    });
+  });
+
+  after('delete test data', () => {
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C345342 Hide placeholder attribute once user populates the first row (firebird)',
+    { tags: [TestTypes.extendedPath, devTeams.firebird] },
+    () => {
+      ExportFieldMappingProfiles.goToFieldMappingProfilesTab();
+      ExportFieldMappingProfiles.verifyFieldMappingProfilesPane();
+      ExportNewFieldMappingProfile.createNewFieldMappingProfile('test.1', []);
+
+      ModalSelectTransformations.verifyTransformationsFirstRowTextFieldsPlaceholders(
+        '900',
+        '0',
+        '0',
+        '$a',
+      );
+      ModalSelectTransformations.fillInTransformationsFirstRowMarcTextField('901');
+      ModalSelectTransformations.verifyTransformationsFirstRowTextFieldsPlaceholders(
+        '',
+        '',
+        '',
+        '',
+      );
+      ModalSelectTransformations.fillInTransformationsFirstRowMarcTextField('');
+      ModalSelectTransformations.verifyTransformationsFirstRowTextFieldsPlaceholders(
+        '900',
+        '0',
+        '0',
+        '$a',
+      );
+      ModalSelectTransformations.fillInTransformationsFirstRowMarcTextField('123');
+      ModalSelectTransformations.verifyTransformationsFirstRowTextFieldsPlaceholders(
+        '',
+        '',
+        '',
+        '',
+      );
+      ModalSelectTransformations.fillInTransformationsFirstRowMarcTextField('');
+      ModalSelectTransformations.clickTransformationsCancelButton();
+
+      ExportNewFieldMappingProfile.clickAddTransformationsButton();
+      ModalSelectTransformations.verifyTransformationsFirstRowTextFieldsPlaceholders(
+        '900',
+        '0',
+        '0',
+        '$a',
+      );
+      ModalSelectTransformations.clickNthCheckbox();
+      ModalSelectTransformations.fillInTransformationsTextfields('902', '1', '2', '$b');
+      ModalSelectTransformations.clickTransformationsSaveAndCloseButton();
+
+      InteractorsTools.checkCalloutMessage(newTransformationCalloutMessage);
+      ExportNewFieldMappingProfile.clickAddTransformationsButton();
+      ModalSelectTransformations.verifyTransformationsFirstRowTextFieldsValues(
+        '902',
+        '1',
+        '2',
+        '$b',
+      );
+      ModalSelectTransformations.clickTransformationsCancelButton();
+    },
+  );
+});
