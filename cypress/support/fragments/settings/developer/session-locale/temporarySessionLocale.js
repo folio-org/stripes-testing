e51119c7200c5722e40a4a166338ae15e5248a42
Ilya_Lyskavets <Ilya_Lyskavets@epam.com>
2023-10-20 16:13:47 +0300

    C402348 add test

--- cypress/support/fragments/settings/developer/session-locale/temporarySessionLocale.js
+++ cypress/support/fragments/settings/developer/session-locale/temporarySessionLocale.js
@@ -1,7 +1,18 @@
-import { Button } from '../../../../../../interactors';
+import { Button, Pane, PaneHeader } from '../../../../../../interactors';
+
+const chooseLanguagePane = Pane({ index: 2 });
 
 export default {
+  waitLoading() {
+    cy.expect(chooseLanguagePane.exists());
+  },
+
   selectCountry(countryName) {
     cy.do(Button(countryName).click());
+  },
+
+  verifyTitleOfPaneHeader(titleValue) {
+    cy.expect(chooseLanguagePane.find(PaneHeader()).has({ title: titleValue }));
+    cy.wait(1000);
   },
 };
