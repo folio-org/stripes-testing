294cc74dfbfd968af7a22f46280b187aa403e6a3
Daniil Novikau <novikaud@gmail.com>
2023-10-20 15:54:02 +0300

    [397985] Organization is not specified in voucher export details

---
+++ cypress/support/fragments/settings/invoices/batchGroups.js
@@ -0,0 +1,38 @@
+import uuid from 'uuid';
+import getRandomPostfix from '../../../utils/stringTools';
+
+export default {
+  getDefaultBatchGroup({ name, description } = {}) {
+    return {
+      id: uuid(),
+      name: name || `autotest_bg_name_${getRandomPostfix()}`,
+      description: description || `autotest_bg_description_${getRandomPostfix()}`,
+    };
+  },
+  getBatchGroupsViaApi(searchParams) {
+    return cy
+      .okapiRequest({
+        path: 'batch-groups',
+        searchParams,
+        isDefaultSearchParamsRequired: false,
+      })
+      .then(({ body }) => {
+        return body.batchGroups;
+      });
+  },
+  createBatchGroupViaApi(batchGroup) {
+    return cy
+      .okapiRequest({
+        method: 'POST',
+        path: 'batch-groups',
+        body: batchGroup,
+      })
+      .then(({ body }) => body);
+  },
+  deleteBatchGroupViaApi(batchGroupId) {
+    return cy.okapiRequest({
+      method: 'DELETE',
+      path: `batch-groups/${batchGroupId}`,
+    });
+  },
+};
