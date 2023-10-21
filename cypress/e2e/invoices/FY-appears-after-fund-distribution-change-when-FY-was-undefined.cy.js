031bc92b70611108d4072da27bed341ebcc7dbb7
kirstaVova <grouplbua@gmail.com>
2023-10-21 12:09:22 +0300

    C396390 Fiscal year appears after fund distribution change when FY was undefined (for previous FY) (thunderjet) (TaaS)

--- cypress/e2e/invoices/FY-appears-after-fund-distribution-change-when-FY-was-undefined.cy.js
+++ cypress/e2e/invoices/FY-appears-after-fund-distribution-change-when-FY-was-undefined.cy.js
@@ -29,20 +29,27 @@ describe('Invoices', () => {
     description: `This is fiscal year created by E2E test automation script_${getRandomPostfix()}`,
     series: 'FY',
   };
-  const thirdFiscalYear = {
-    name: `autotest_3_year_${getRandomPostfix()}`,
-    code: DateTools.getRandomFiscalYearCode(2000, 9999),
-    periodStart: `${DateTools.getDayAfterTomorrowDateForFiscalYear()}T00:00:00.000+00:00`,
-    periodEnd: `${DateTools.get2DaysAfterTomorrowDateForFiscalYear()}T00:00:00.000+00:00`,
-    description: `This is fiscal year created by E2E test automation script_${getRandomPostfix()}`,
-    series: 'FY',
-  };
-  const defaultLedger = {
+  const firstLedger = {
     ...Ledgers.defaultUiLedger,
-    restrictEncumbrance: false,
-    restrictExpenditures: false,
+    restrictEncumbrance: true,
+    restrictExpenditures: true,
   };
-  const defaultFund = { ...Funds.defaultUiFund };
+  const secondLedger = {
+    name: `autotest_2_ledger_${getRandomPostfix()}`,
+    ledgerStatus: 'Active',
+    code: `test_automation_code_${getRandomPostfix()}`,
+    description: 'This is ledger created by E2E test automation script',
+    restrictEncumbrance: true,
+    restrictExpenditures: true,
+  };
+  const firstFund = { ...Funds.defaultUiFund };
+  const secondFund = {
+    name: `autotest_2_fund_${getRandomPostfix()}`,
+    code: getRandomPostfix(),
+    externalAccountNo: getRandomPostfix(),
+    fundStatus: 'Active',
+    description: `This is fund created by E2E test automation script_${getRandomPostfix()}`,
+  };
   const defaultOrder = {
     ...NewOrder.defaultOneTimeOrder,
     orderType: 'One-time',
@@ -52,14 +59,6 @@ describe('Invoices', () => {
   const organization = { ...NewOrganization.defaultUiOrganizations };
   const invoice = { ...NewInvoice.defaultUiInvoice };
   const allocatedQuantity = '100';
-  const periodStartForFirstFY = DateTools.getThreePreviousDaysDateForFiscalYearOnUIEdit();
-  const periodEndForFirstFY = DateTools.getPreviousDayDateForFiscalYearOnUIEdit();
-  const periodEndForEarlierFirstFY = DateTools.getTwoPreviousDaysDateForFiscalYearOnUIEdit();
-  const periodStartForSecondFY = DateTools.getCurrentDateForFiscalYearOnUIEdit();
-  const periodEndForSecondFY = DateTools.getDayTomorrowDateForFiscalYearOnUIEdit();
-  const periodStartForThirdFY = DateTools.getPreviousDayDateForFiscalYearOnUIEdit();
-  const periodEndForThirdFY = DateTools.getDayTomorrowDateForFiscalYearOnUIEdit();
-
   firstFiscalYear.code = firstFiscalYear.code.slice(0, -1) + '1';
   let user;
   let orderNumber;
@@ -68,107 +67,92 @@ describe('Invoices', () => {
 
   before(() => {
     cy.getAdminToken();
+    cy.loginAsAdmin();
 
-    FiscalYears.createViaApi(firstFiscalYear).then((firstFiscalYearResponse) => {
-      firstFiscalYear.id = firstFiscalYearResponse.id;
-      defaultLedger.fiscalYearOneId = firstFiscalYear.id;
-      Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
-        defaultLedger.id = ledgerResponse.id;
-        defaultFund.ledgerId = defaultLedger.id;
-
-        Funds.createViaApi(defaultFund).then((fundResponse) => {
-          defaultFund.id = fundResponse.fund.id;
-
-          cy.loginAsAdmin({ path: TopMenu.fundPath, waiter: Funds.waitLoading });
-          FinanceHelp.searchByName(defaultFund.name);
-          Funds.selectFund(defaultFund.name);
-          Funds.addBudget(allocatedQuantity);
-        });
-
-        ServicePoints.getViaApi().then((servicePoint) => {
-          servicePointId = servicePoint[0].id;
-          NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then((res) => {
-            location = res;
-          });
-        });
-
-        Organizations.createOrganizationViaApi(organization).then((responseOrganizations) => {
-          organization.id = responseOrganizations;
-          invoice.accountingCode = organization.erpCode;
-          cy.getBatchGroups().then((batchGroup) => {
-            invoice.batchGroup = batchGroup.name;
-          });
-        });
-        defaultOrder.vendor = organization.name;
-        cy.visit(TopMenu.ordersPath);
-        Orders.createApprovedOrderForRollover(defaultOrder, true, false).then(
-          (firstOrderResponse) => {
-            defaultOrder.id = firstOrderResponse.id;
-            orderNumber = firstOrderResponse.poNumber;
-            Orders.checkCreatedOrder(defaultOrder);
-            OrderLines.addPOLine();
-            OrderLines.selectRandomInstanceInTitleLookUP('*', 15);
-            OrderLines.rolloverPOLineInfoforPhysicalMaterialWithFund(
-              defaultFund,
-              '10',
-              '1',
-              '10',
-              location.institutionId,
-            );
-            OrderLines.backToEditingOrder();
-            Orders.openOrder();
-          },
-        );
-
-        secondFiscalYear.code = firstFiscalYear.code.slice(0, -1) + '2';
-        thirdFiscalYear.code = firstFiscalYear.code.slice(0, -1) + '3';
-        FiscalYears.createViaApi(secondFiscalYear).then((secondFiscalYearResponse) => {
-          secondFiscalYear.id = secondFiscalYearResponse.id;
-        });
-
-        FiscalYears.createViaApi(thirdFiscalYear).then((thirdFiscalYearResponse) => {
-          thirdFiscalYear.id = thirdFiscalYearResponse.id;
-        });
-
-        cy.visit(TopMenu.ledgerPath);
-        FinanceHelp.searchByName(defaultLedger.name);
-        Ledgers.selectLedger(defaultLedger.name);
-        Ledgers.rollover();
-        Ledgers.fillInCommonRolloverInfo(secondFiscalYear.code, 'None', 'Allocation');
+    Organizations.createOrganizationViaApi(organization).then((responseOrganizations) => {
+      organization.id = responseOrganizations;
+      invoice.accountingCode = organization.erpCode;
+      cy.getBatchGroups().then((batchGroup) => {
+        invoice.batchGroup = batchGroup.name;
       });
-    });
+      FiscalYears.createViaApi(firstFiscalYear).then((firstFiscalYearResponse) => {
+        firstFiscalYear.id = firstFiscalYearResponse.id;
+        firstLedger.fiscalYearOneId = firstFiscalYear.id;
+        secondLedger.fiscalYearOneId = firstFiscalYear.id;
+        Ledgers.createViaApi(firstLedger).then((ledgerResponse) => {
+          firstLedger.id = ledgerResponse.id;
+          firstFund.ledgerId = firstLedger.id;
 
-    cy.visit(TopMenu.fiscalYearPath);
-    FinanceHelp.searchByName(firstFiscalYear.name);
-    FiscalYears.selectFY(firstFiscalYear.name);
-    FiscalYears.editFiscalYearDetails();
-    FiscalYears.filltheStartAndEndDateonCalenderstartDateField(
-      periodStartForFirstFY,
-      periodEndForFirstFY,
-    );
-    FinanceHelp.searchByName(secondFiscalYear.name);
-    FiscalYears.selectFY(secondFiscalYear.name);
-    FiscalYears.editFiscalYearDetails();
-    FiscalYears.filltheStartAndEndDateonCalenderstartDateField(
-      periodStartForSecondFY,
-      periodEndForSecondFY,
-    );
+          Funds.createViaApi(firstFund).then((firstFundResponse) => {
+            firstFund.id = firstFundResponse.fund.id;
 
-    cy.createTempUser([
-      permissions.uiFinanceExecuteFiscalYearRollover.gui,
-      permissions.uiFinanceViewFundAndBudget.gui,
-      permissions.uiFinanceViewLedger.gui,
-      permissions.uiFinanceViewEditFiscalYear.gui,
-      permissions.uiInvoicesApproveInvoices.gui,
-      permissions.viewEditCreateInvoiceInvoiceLine.gui,
-      permissions.uiOrdersView.gui,
-      permissions.uiInvoicesPayInvoices.gui,
-      permissions.uiInvoicesPayInvoicesInDifferentFiscalYear.gui,
-    ]).then((userProperties) => {
-      user = userProperties;
-      cy.login(userProperties.username, userProperties.password, {
-        path: TopMenu.ordersPath,
-        waiter: Orders.waitLoading,
+            cy.loginAsAdmin({ path: TopMenu.fundPath, waiter: Funds.waitLoading });
+            FinanceHelp.searchByName(firstFund.name);
+            Funds.selectFund(firstFund.name);
+            Funds.addBudget(allocatedQuantity);
+            Funds.closeBudgetDetails();
+          });
+
+          Ledgers.createViaApi(secondLedger).then((secondLedgerResponse) => {
+            secondLedger.id = secondLedgerResponse.id;
+            secondFund.ledgerId = secondLedger.id;
+
+            Funds.createViaApi(secondFund).then((secondFundResponse) => {
+              secondFund.id = secondFundResponse.fund.id;
+              FinanceHelp.searchByName(secondFund.name);
+              Funds.selectFund(secondFund.name);
+              Funds.addBudget(allocatedQuantity);
+            });
+
+            ServicePoints.getViaApi().then((servicePoint) => {
+              servicePointId = servicePoint[0].id;
+              NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then(
+                (res) => {
+                  location = res;
+                },
+              );
+            });
+
+            defaultOrder.vendor = organization.name;
+            cy.visit(TopMenu.ordersPath);
+            Orders.createApprovedOrderForRollover(defaultOrder, true, false).then(
+              (firstOrderResponse) => {
+                defaultOrder.id = firstOrderResponse.id;
+                orderNumber = firstOrderResponse.poNumber;
+                OrderLines.addPOLine();
+                OrderLines.selectRandomInstanceInTitleLookUP('*', 1);
+                OrderLines.rolloverPOLineInfoforPhysicalMaterialWithFund(
+                  firstFund,
+                  '10',
+                  '1',
+                  '10',
+                  location.institutionId,
+                );
+                OrderLines.backToEditingOrder();
+                Orders.openOrder();
+
+                secondFiscalYear.code = firstFiscalYear.code.slice(0, -1) + '2';
+                FiscalYears.createViaApi(secondFiscalYear).then((secondFiscalYearResponse) => {
+                  secondFiscalYear.id = secondFiscalYearResponse.id;
+                });
+              },
+            );
+          });
+        });
+      });
+
+      cy.createTempUser([
+        permissions.uiFinanceViewFundAndBudget.gui,
+        permissions.uiInvoicesApproveInvoices.gui,
+        permissions.viewEditCreateInvoiceInvoiceLine.gui,
+        permissions.uiInvoicesPayInvoices.gui,
+        permissions.uiInvoicesPayInvoicesInDifferentFiscalYear.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+        cy.login(userProperties.username, userProperties.password, {
+          path: TopMenu.invoicesPath,
+          waiter: Invoices.waitLoading,
+        });
       });
     });
   });
@@ -181,114 +165,14 @@ describe('Invoices', () => {
     'C396390 Fiscal year appears after fund distribution change when FY was undefined (for previous FY) (thunderjet) (TaaS)',
     { tags: [testType.criticalPath, devTeams.thunderjet] },
     () => {
-      Orders.searchByParameter('PO number', orderNumber);
-      Orders.selectFromResultsList();
-      Orders.newInvoiceFromOrder();
-      Invoices.createInvoiceFromOrder(invoice, firstFiscalYear.code);
-      Invoices.approveInvoice();
+      Invoices.createRolloverInvoice(invoice, organization.name);
+      Invoices.createInvoiceLineFromPol(orderNumber);
+      Invoices.editInvoice();
+      Invoices.checkAbsentFYOptionInInvoice(secondFiscalYear.code);
+      Invoices.cancelEditInvoice();
       Invoices.selectInvoiceLine();
-      Invoices.openPageCurrentEncumbrance('$0.00');
-      Funds.selectTransactionInList('Encumbrance');
-      Funds.varifyDetailsInTransaction(
-        firstFiscalYear.code,
-        '$0.00',
-        `${orderNumber}-1`,
-        'Encumbrance',
-        `${defaultFund.name} (${defaultFund.code})`,
-      );
-      Funds.checkStatusInTransactionDetails('Released');
-      Funds.selectTransactionInList('Pending payment');
-      Funds.varifyDetailsInTransaction(
-        firstFiscalYear.code,
-        '($10.00)',
-        invoice.invoiceNumber,
-        'Pending payment',
-        `${defaultFund.name} (${defaultFund.code})`,
-      );
-      Funds.closeTransactionApp(defaultFund, firstFiscalYear);
-      Funds.closeBudgetDetails();
-      Funds.selectBudgetDetails();
-      Funds.viewTransactions();
-      Funds.selectTransactionInList('Encumbrance');
-      Funds.varifyDetailsInTransaction(
-        secondFiscalYear.code,
-        '($10.00)',
-        `${orderNumber}-1`,
-        'Encumbrance',
-        `${defaultFund.name} (${defaultFund.code})`,
-      );
-      Funds.checkStatusInTransactionDetails('Unreleased');
-      cy.visit(TopMenu.ledgerPath);
-      FinanceHelp.searchByName(defaultLedger.name);
-      Ledgers.selectLedger(defaultLedger.name);
-      Ledgers.rollover();
-      Ledgers.fillInCommonRolloverInfo(thirdFiscalYear.code, 'None', 'Allocation');
-
-      cy.visit(TopMenu.fiscalYearPath);
-      FinanceHelp.searchByName(secondFiscalYear.name);
-      FiscalYears.selectFY(secondFiscalYear.name);
-      FiscalYears.editFiscalYearDetails();
-      FiscalYears.filltheStartAndEndDateonCalenderstartDateField(
-        periodStartForFirstFY,
-        periodEndForEarlierFirstFY,
-      );
-      FinanceHelp.searchByName(thirdFiscalYear.name);
-      FiscalYears.selectFY(thirdFiscalYear.name);
-      FiscalYears.editFiscalYearDetails();
-      FiscalYears.filltheStartAndEndDateonCalenderstartDateField(
-        periodStartForThirdFY,
-        periodEndForThirdFY,
-      );
-
-      cy.visit(TopMenu.invoicesPath);
-      Invoices.searchByNumber(invoice.invoiceNumber);
-      Invoices.selectInvoice(invoice.invoiceNumber);
-      Invoices.payInvoice();
-      Invoices.selectInvoiceLine();
-      Invoices.openPageCurrentEncumbrance('$0.00');
-      Funds.selectTransactionInList('Encumbrance');
-      Funds.varifyDetailsInTransaction(
-        firstFiscalYear.code,
-        '$0.00',
-        `${orderNumber}-1`,
-        'Encumbrance',
-        `${defaultFund.name} (${defaultFund.code})`,
-      );
-      Funds.checkStatusInTransactionDetails('Released');
-      Funds.selectTransactionInList('Payment');
-      Funds.varifyDetailsInTransaction(
-        firstFiscalYear.code,
-        '($10.00)',
-        invoice.invoiceNumber,
-        'Payment',
-        `${defaultFund.name} (${defaultFund.code})`,
-      );
-      Funds.closeTransactionApp(defaultFund, firstFiscalYear);
-      Funds.closeBudgetDetails();
-      Funds.selectPreviousBudgetDetailsByFY(defaultFund, secondFiscalYear);
-      Funds.viewTransactions();
-      Funds.selectTransactionInList('Encumbrance');
-      Funds.varifyDetailsInTransaction(
-        secondFiscalYear.code,
-        '($10.00)',
-        `${orderNumber}-1`,
-        'Encumbrance',
-        `${defaultFund.name} (${defaultFund.code})`,
-      );
-      Funds.checkStatusInTransactionDetails('Unreleased');
-      Funds.closeTransactionApp(defaultFund, secondFiscalYear);
-      Funds.closeBudgetDetails();
-      Funds.selectBudgetDetails();
-      Funds.viewTransactions();
-      Funds.selectTransactionInList('Encumbrance');
-      Funds.varifyDetailsInTransaction(
-        thirdFiscalYear.code,
-        '($10.00)',
-        `${orderNumber}-1`,
-        'Encumbrance',
-        `${defaultFund.name} (${defaultFund.code})`,
-      );
-      Funds.checkStatusInTransactionDetails('Unreleased');
+      Invoices.editInvoiceLine();
+      Invoices.changeFundInLine(secondFund);
     },
   );
 });
