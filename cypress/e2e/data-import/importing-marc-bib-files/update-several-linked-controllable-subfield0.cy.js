9230157750d87a3f3c3ae6a75f6e280941e13681
ali-soft-engineer <147161616+ali-soft-engineer@users.noreply.github.com>
2023-10-21 13:23:04 +0500

    test C385667 (#1963)

---
+++ cypress/e2e/data-import/importing-marc-bib-files/update-several-linked-controllable-subfield0.cy.js
@@ -0,0 +1,275 @@
+import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
+import ActionProfiles from '../../../support/fragments/data_import/action_profiles/actionProfiles';
+import NewJobProfile from '../../../support/fragments/data_import/job_profiles/newJobProfile';
+import MatchProfiles from '../../../support/fragments/data_import/match_profiles/matchProfiles';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import Logs from '../../../support/fragments/data_import/logs/logs';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import { DevTeams, TestTypes, Permissions } from '../../../support/dictionary';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import ExportFile from '../../../support/fragments/data-export/exportFile';
+import FileManager from '../../../support/utils/fileManager';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import MarcAuthority from '../../../support/fragments/marcAuthority/marcAuthority';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import MarcAuthorities from '../../../support/fragments/marcAuthority/marcAuthorities';
+import QuickMarcEditor from '../../../support/fragments/quickMarcEditor';
+import Parallelization from '../../../support/dictionary/parallelization';
+import {
+  LOCATION_NAMES,
+  FOLIO_RECORD_TYPE,
+  ACCEPTED_DATA_TYPE_NAMES,
+  EXISTING_RECORDS_NAMES,
+} from '../../../support/constants';
+import FieldMappingProfileView from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfileView';
+
+describe('data-import', () => {
+  describe('Importing MARC Bib files', () => {
+    const testData = {};
+    // unique file name to upload
+    const nameForUpdatedMarcFile = `C385667autotestFile${getRandomPostfix()}.mrc`;
+    const nameForExportedMarcFile = `C385667autotestFile${getRandomPostfix()}.mrc`;
+    const nameForCSVFile = `C385667autotestFile${getRandomPostfix()}.csv`;
+    const mappingProfile = {
+      name: 'Update MARC Bib records by matching 999 ff $s subfield value',
+      typeValue: FOLIO_RECORD_TYPE.MARCBIBLIOGRAPHIC,
+      update: true,
+      permanentLocation: `"${LOCATION_NAMES.ANNEX}"`,
+    };
+    const actionProfile = {
+      typeValue: FOLIO_RECORD_TYPE.MARCBIBLIOGRAPHIC,
+      name: 'Update MARC Bib records by matching 999 ff $s subfield value',
+      action: 'Update (all record types except Orders, Invoices, or MARC Holdings)',
+    };
+    const matchProfile = {
+      profileName: 'Update MARC Bib records by matching 999 ff $s subfield value',
+      incomingRecordFields: {
+        field: '999',
+        in1: 'f',
+        in2: 'f',
+        subfield: 's',
+      },
+      existingRecordFields: {
+        field: '999',
+        in1: 'f',
+        in2: 'f',
+        subfield: 's',
+      },
+      matchCriterion: 'Exactly matches',
+      existingRecordType: EXISTING_RECORDS_NAMES.MARC_BIBLIOGRAPHIC,
+    };
+    const jobProfile = {
+      ...NewJobProfile.defaultJobProfile,
+      profileName: 'Update MARC Bib records by matching 999 ff $s subfield value',
+      acceptedType: ACCEPTED_DATA_TYPE_NAMES.MARC,
+    };
+    const marcFiles = [
+      {
+        marc: 'marcBibFileForC385667.mrc',
+        fileName: `testMarcFile.${getRandomPostfix()}.mrc`,
+        jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
+        numOfRecords: 1,
+      },
+      {
+        marc: 'marcAuthFileForC385667.mrc',
+        fileName: `testMarcFile.${getRandomPostfix()}.mrc`,
+        jobProfileToRun: 'Default - Create SRS MARC Authority',
+        numOfRecords: 3,
+      },
+    ];
+    const linkingTagAndValues = [
+      {
+        rowIndex: 75,
+        value: 'C385667 Chin, Staceyann, 1972-',
+        tag: '700',
+      },
+      {
+        rowIndex: 76,
+        value: 'C385667 Lee, Stan, 1922-2018',
+        tag: '700',
+      },
+      {
+        rowIndex: 77,
+        value: 'C385667 Kirby, Jack',
+        tag: '700',
+      },
+    ];
+    const createdAuthorityIDs = [];
+
+    before('Creating user and test data', () => {
+      cy.createTempUser([
+        Permissions.moduleDataImportEnabled.gui,
+        Permissions.inventoryAll.gui,
+        Permissions.uiQuickMarcQuickMarcBibliographicEditorAll.gui,
+        Permissions.dataExportEnableApp.gui,
+      ]).then((createdUserProperties) => {
+        testData.userProperties = createdUserProperties;
+        cy.loginAsAdmin()
+          .then(() => {
+            marcFiles.forEach((marcFile) => {
+              cy.visit(TopMenu.dataImportPath);
+              DataImport.verifyUploadState();
+              DataImport.uploadFileAndRetry(marcFile.marc, marcFile.fileName);
+              JobProfiles.waitLoadingList();
+              JobProfiles.search(marcFile.jobProfileToRun);
+              JobProfiles.runImportFile();
+              JobProfiles.waitFileIsImported(marcFile.fileName);
+              Logs.checkStatusOfJobProfile('Completed');
+              Logs.openFileDetails(marcFile.fileName);
+              for (let i = 0; i < marcFile.numOfRecords; i++) {
+                Logs.getCreatedItemsID(i).then((link) => {
+                  createdAuthorityIDs.push(link.split('/')[5]);
+                });
+              }
+            });
+          })
+          .then(() => {
+            cy.visit(TopMenu.inventoryPath);
+            InventoryInstance.searchByTitle(createdAuthorityIDs[0]);
+            InventoryInstances.selectInstance();
+            InventoryInstance.editMarcBibliographicRecord();
+            linkingTagAndValues.forEach((linking) => {
+              QuickMarcEditor.clickLinkIconInTagField(linking.rowIndex);
+              MarcAuthorities.switchToSearch();
+              InventoryInstance.verifySelectMarcAuthorityModal();
+              InventoryInstance.verifySearchOptions();
+              InventoryInstance.searchResults(linking.value);
+              InventoryInstance.clickLinkButton();
+              QuickMarcEditor.verifyAfterLinkingUsingRowIndex(linking.tag, linking.rowIndex);
+            });
+            QuickMarcEditor.clickArrowDownButton(76);
+            QuickMarcEditor.pressSaveAndClose();
+            QuickMarcEditor.checkAfterSaveAndClose();
+          })
+          .then(() => {
+            // create Match profile
+            cy.visit(SettingsMenu.matchProfilePath);
+            MatchProfiles.createMatchProfile(matchProfile);
+            // create Field mapping profile
+            cy.visit(SettingsMenu.mappingProfilePath);
+            FieldMappingProfiles.createMappingProfileForUpdatesMarc(mappingProfile);
+            FieldMappingProfileView.closeViewMode(mappingProfile.name);
+            FieldMappingProfiles.checkMappingProfilePresented(mappingProfile.name);
+            // create Action profile and link it to Field mapping profile
+            cy.visit(SettingsMenu.actionProfilePath);
+            ActionProfiles.create(actionProfile, mappingProfile.name);
+            ActionProfiles.checkActionProfilePresented(actionProfile.name);
+            // create Job profile
+            cy.visit(SettingsMenu.jobProfilePath);
+            JobProfiles.openNewJobProfileForm();
+            NewJobProfile.fillJobProfile(jobProfile);
+            NewJobProfile.linkMatchProfile(matchProfile.profileName);
+            NewJobProfile.linkActionProfileByName(actionProfile.name);
+            // wait for the action profile to be linked
+            cy.wait(1000);
+            NewJobProfile.saveAndClose();
+            JobProfiles.waitLoadingList();
+            JobProfiles.checkJobProfilePresented(jobProfile.profileName);
+          });
+
+        cy.login(testData.userProperties.username, testData.userProperties.password, {
+          path: TopMenu.inventoryPath,
+          waiter: InventoryInstances.waitContentLoading,
+        });
+      });
+    });
+
+    after('Delete user and test data', () => {
+      Users.deleteViaApi(testData.userProperties.userId);
+      if (createdAuthorityIDs[0]) InventoryInstance.deleteInstanceViaApi(createdAuthorityIDs[0]);
+      createdAuthorityIDs.forEach((id, index) => {
+        if (index) MarcAuthority.deleteViaAPI(id);
+      });
+      // clean up generated profiles
+      JobProfiles.deleteJobProfile(jobProfile.profileName);
+      MatchProfiles.deleteMatchProfile(matchProfile.profileName);
+      ActionProfiles.deleteActionProfile(actionProfile.name);
+      FieldMappingProfileView.deleteViaApi(mappingProfile.name);
+      // delete created files in fixtures
+      FileManager.deleteFile(`cypress/fixtures/${nameForExportedMarcFile}`);
+      FileManager.deleteFile(`cypress/fixtures/${nameForCSVFile}`);
+      FileManager.deleteFile(`cypress/fixtures/${nameForUpdatedMarcFile}`);
+    });
+
+    it(
+      'C385667 Update controllable subfield, "$0" in several linked repeatable fields and change field order (multiple repeatable fields with same indicators) (spitfire) (TaaS)',
+      { tags: [TestTypes.criticalPath, DevTeams.spitfire, Parallelization.nonParallel] },
+      () => {
+        InventoryInstance.searchByTitle(createdAuthorityIDs[0]);
+        InventoryInstances.selectInstance();
+        // download .csv file
+        InventorySearchAndFilter.saveUUIDs();
+        ExportFile.downloadCSVFile(nameForCSVFile, 'SearchInstanceUUIDs*');
+        FileManager.deleteFolder(Cypress.config('downloadsFolder'));
+        cy.visit(TopMenu.dataExportPath);
+        // download exported marc file
+        ExportFile.uploadFile(nameForCSVFile);
+        ExportFile.exportWithDefaultJobProfile(nameForCSVFile);
+        ExportFile.downloadExportedMarcFile(nameForExportedMarcFile);
+        FileManager.deleteFolder(Cypress.config('downloadsFolder'));
+        cy.log('#####End Of Export#####');
+
+        DataImport.editMarcFile(
+          nameForExportedMarcFile,
+          nameForUpdatedMarcFile,
+          [
+            'n2008052404',
+            'n83169267',
+            'n77020008',
+            'Chin, Staceyann,',
+            'Lee, Stan,',
+            'Kirby, Jack',
+          ],
+          [
+            'n2008052404123',
+            'n83169267123',
+            'n77020008123',
+            'Chin, S-nn,',
+            'Lee, Stanley,',
+            'Kirby, Steve,',
+          ],
+        );
+
+        // upload the exported marc file with 999.f.f.s fields
+        cy.visit(TopMenu.dataImportPath);
+        DataImport.uploadFile(nameForUpdatedMarcFile, nameForUpdatedMarcFile);
+        JobProfiles.waitLoadingList();
+        JobProfiles.search(jobProfile.profileName);
+        JobProfiles.runImportFile();
+        JobProfiles.waitFileIsImported(nameForUpdatedMarcFile);
+        Logs.checkStatusOfJobProfile('Completed');
+        Logs.openFileDetails(nameForUpdatedMarcFile);
+
+        cy.visit(TopMenu.inventoryPath);
+        InventoryInstance.searchByTitle(createdAuthorityIDs[0]);
+        InventoryInstances.selectInstance();
+        InventoryInstance.editMarcBibliographicRecord();
+        QuickMarcEditor.verifyTagFieldAfterUnlinking(
+          75,
+          '700',
+          '1',
+          '\\',
+          '$a C385667 Chin, S-nn, $d 1972- $e letterer. $0 id.loc.gov/authorities/names/n2008052404123',
+        );
+        QuickMarcEditor.verifyTagFieldAfterUnlinking(
+          76,
+          '700',
+          '1',
+          '\\',
+          '$a C385667 Kirby, Steve, $e creator. $0 id.loc.gov/authorities/names/n77020008123',
+        );
+        QuickMarcEditor.verifyTagFieldAfterUnlinking(
+          77,
+          '700',
+          '1',
+          '\\',
+          '$a C385667 Lee, Stanley, $d 1922-2018 $e creator $0 id.loc.gov/authorities/names/n83169267123',
+        );
+      },
+    );
+  });
+});
