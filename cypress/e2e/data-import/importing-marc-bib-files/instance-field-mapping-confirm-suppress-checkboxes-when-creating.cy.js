18a9b37efb1471480f473539d516c3bbdd81cad0
TatianaOne <tapusja2014@gmail.com>
2023-10-22 22:54:50 +0300

    added test

---
+++ cypress/e2e/data-import/importing-marc-bib-files/instance-field-mapping-confirm-suppress-checkboxes-when-creating.cy.js
@@ -0,0 +1,116 @@
+import getRandomPostfix from '../../../support/utils/stringTools';
+import { DevTeams, TestTypes } from '../../../support/dictionary';
+import {
+  FOLIO_RECORD_TYPE,
+  ACCEPTED_DATA_TYPE_NAMES,
+  JOB_STATUS_NAMES,
+} from '../../../support/constants';
+import TopMenu from '../../../support/fragments/topMenu';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import Logs from '../../../support/fragments/data_import/logs/logs';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
+import NewFieldMappingProfile from '../../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
+import ActionProfiles from '../../../support/fragments/data_import/action_profiles/actionProfiles';
+import InstanceRecordView from '../../../support/fragments/inventory/instanceRecordView';
+import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
+import FieldMappingProfileView from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfileView';
+import NewJobProfile from '../../../support/fragments/data_import/job_profiles/newJobProfile';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import JsonScreenView from '../../../support/fragments/data_import/logs/jsonScreenView';
+import InstanceRecordEdit from '../../../support/fragments/inventory/instanceRecordEdit';
+
+describe('data-import', () => {
+  describe('Importing MARC Bib files', () => {
+    let instanceHrid;
+    const instanceTitle =
+      "101 things I wish I'd known when I started using hypnosis / Dabney Ewin.";
+    const marcFileName = `C11087 autotestFile_${getRandomPostfix()}.mrc`;
+    const filePathToUpload = 'marcBibFileForC11087.mrc';
+    const mappingProfile = {
+      name: `C11087 autotestMappingProfile_${getRandomPostfix()}`,
+      typeValue: FOLIO_RECORD_TYPE.INSTANCE,
+      suppressFromDiscavery: 'Mark for all affected records',
+      staffSuppress: 'Unmark for all affected records',
+      previouslyHeld: 'Keep the existing value for all affected records',
+    };
+    const actionProfile = {
+      name: `C11087 autotestActionProfile_${getRandomPostfix()}`,
+      typeValue: FOLIO_RECORD_TYPE.INSTANCE,
+    };
+    const jobProfile = {
+      profileName: `C11087 autotestJobProfile_${getRandomPostfix()}`,
+      acceptedType: ACCEPTED_DATA_TYPE_NAMES.MARC,
+    };
+
+    before('login', () => {
+      cy.getAdminToken();
+      cy.loginAsAdmin({
+        path: SettingsMenu.mappingProfilePath,
+        waiter: FieldMappingProfiles.waitLoading,
+      });
+    });
+
+    // after('delete test data', () => {
+    //   JobProfiles.deleteJobProfile(jobProfile.profileName);
+    //   ActionProfiles.deleteActionProfile(actionProfile.name);
+    //   FieldMappingProfileView.deleteViaApi(mappingProfile.name);
+    //     cy.getInstance({ limit: 1, expandAll: true, query: `"hrid"=="${hrid}"` }).then(
+    //       (instance) => {
+    //         InventoryInstance.deleteInstanceViaApi(instance.id);
+    //       },
+    //     );
+    // });
+
+    it(
+      'C11087 Instance field mapping: Confirm the "suppress" checkboxes when creating (folijet) (TaaS)',
+      { tags: [TestTypes.extendedPath, DevTeams.folijet] },
+      () => {
+        // create mapping profiles
+        FieldMappingProfiles.openNewMappingProfileForm();
+        NewFieldMappingProfile.fillSummaryInMappingProfile(mappingProfile);
+        NewFieldMappingProfile.addStaffSuppress(mappingProfile.staffSuppress);
+        NewFieldMappingProfile.addSuppressFromDiscovery(mappingProfile.suppressFromDiscavery);
+        NewFieldMappingProfile.addPreviouslyHeld(mappingProfile.previouslyHeld);
+        NewFieldMappingProfile.save();
+        FieldMappingProfileView.closeViewMode(mappingProfile.name);
+        FieldMappingProfiles.checkMappingProfilePresented(mappingProfile.name);
+
+        cy.visit(SettingsMenu.actionProfilePath);
+        ActionProfiles.create(actionProfile, mappingProfile.name);
+        ActionProfiles.checkActionProfilePresented(actionProfile.name);
+
+        cy.visit(SettingsMenu.jobProfilePath);
+        JobProfiles.createJobProfile(jobProfile);
+        NewJobProfile.linkActionProfile(actionProfile);
+        NewJobProfile.saveAndClose();
+        JobProfiles.checkJobProfilePresented(jobProfile.profileName);
+
+        // upload a marc file
+        cy.visit(TopMenu.dataImportPath);
+        // TODO delete function after fix https://issues.folio.org/browse/MODDATAIMP-691
+        DataImport.verifyUploadState();
+        DataImport.uploadFile(filePathToUpload, marcFileName);
+        JobProfiles.search(jobProfile.profileName);
+        JobProfiles.runImportFile();
+        JobProfiles.waitFileIsImported(marcFileName);
+        Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
+        Logs.openFileDetails(marcFileName);
+        FileDetails.verifyLogDetailsPageIsOpened();
+        FileDetails.openJsonScreen(instanceTitle);
+        JsonScreenView.verifyJsonScreenIsOpened();
+        JsonScreenView.getInstanceHrid().then((hrid) => {
+          instanceHrid = hrid;
+
+          cy.visit(TopMenu.inventoryPath);
+          InventorySearchAndFilter.searchInstanceByHRID(instanceHrid);
+          InstanceRecordView.verifyInstancePaneExists();
+          //
+          InstanceRecordView.edit();
+          InstanceRecordEdit.verify();
+        });
+      },
+    );
+  });
+});
