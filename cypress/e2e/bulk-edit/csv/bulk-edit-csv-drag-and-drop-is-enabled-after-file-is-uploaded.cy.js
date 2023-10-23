ebbffc5be29bd814fdc2839acac5eab9edf4577f
Ilya_Lyskavets <Ilya_Lyskavets@epam.com>
2023-10-23 22:12:11 +0300

    C353540 test for drag and drop is created

---
+++ cypress/e2e/bulk-edit/csv/bulk-edit-csv-drag-and-drop-is-enabled-after-file-is-uploaded.cy.js
@@ -0,0 +1,63 @@
+import getRandomPostfix, { randomFourDigitNumber } from '../../../support/utils/stringTools';
+import { DevTeams, Permissions, TestTypes } from '../../../support/dictionary';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import FileManager from '../../../support/utils/fileManager';
+
+describe('Bulk Edit -- Users- Local approach', () => {
+  const testData = {};
+  const userUUIDsFileName = `userUUIDs_${getRandomPostfix()}.csv`;
+  const invalidUserUUID = getRandomPostfix();
+
+  before('Create test data', () => {
+    cy.getAdminToken().then(() => {
+      // create all test objects
+    });
+
+    cy.createTempUser([
+      Permissions.bulkEditCsvView.gui,
+      Permissions.bulkEditCsvEdit.gui,
+      Permissions.uiUsersView.gui,
+    ]).then((userProperties) => {
+      testData.user = userProperties;
+
+      cy.login(testData.user.username, testData.user.password, {
+        path: TopMenu.bulkEditPath,
+        waiter: BulkEditSearchPane.waitLoading,
+      });
+      FileManager.createFile(
+        `cypress/fixtures/${userUUIDsFileName}`,
+        `${testData.user.userId}\r\n${invalidUserUUID}`,
+      );
+    });
+  });
+
+  after('Delete test data', () => {
+    Users.deleteViaApi(testData.user.userId);
+    FileManager.deleteFile(`cypress/fixtures/${userUUIDsFileName}`);
+  });
+
+  it(
+    'C353540 Verify that the "Drag and Drop" is enabled after file is uploaded (firebird) (TaaS)',
+    { tags: [TestTypes.extendedPath, DevTeams.firebird] },
+    () => {
+      BulkEditSearchPane.verifyDefaultFilterState();
+      BulkEditSearchPane.checkUsersRadio();
+      BulkEditSearchPane.isDragAndDropAreaDisabled(true);
+
+      BulkEditSearchPane.selectRecordIdentifier('User UUIDs');
+      BulkEditSearchPane.isDragAndDropAreaDisabled(false);
+      BulkEditSearchPane.uploadFile(userUUIDsFileName);
+      BulkEditSearchPane.waitFileUploading();
+
+      BulkEditSearchPane.verifyMatchedResults(testData.user.username);
+      BulkEditSearchPane.verifyPaneRecordsCount(1);
+      BulkEditSearchPane.isDragAndDropAreaDisabled(true);
+
+      BulkEditSearchPane.selectRecordIdentifier('User Barcodes');
+      BulkEditSearchPane.matchedAccordionIsAbsent();
+      BulkEditSearchPane.isDragAndDropAreaDisabled(false);
+    },
+  );
+});
