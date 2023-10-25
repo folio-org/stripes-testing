d6816e150126d884c5020afb823cf16280a1e417
arsenhovh <arsen_hovhannisyan@epam.com>
2023-10-25 14:24:38 +0400

    added new permission , and removed one of describe blocks

--- cypress/e2e/data-import/settings/file-extensions-are-alphabetical-order.cy.js
+++ cypress/e2e/data-import/settings/file-extensions-are-alphabetical-order.cy.js
@@ -4,33 +4,34 @@ import FileExtensions from '../../../support/fragments/settings/dataImport/fileE
 import Users from '../../../support/fragments/users/users';
 import FileExtensionView from '../../../support/fragments/settings/dataImport/fileExtensions/fileExtensionView';
 
-describe('data-import', () => {
-  describe('Settings', () => {
-    let user;
-    const fileExtensionName = '.foc';
+describe('data-import: Settings', () => {
+  let user;
+  const fileExtensionName = '.foc';
 
-    before('login and create test data', () => {
-      cy.createTempUser([Permissions.settingsDataImportEnabled.gui]).then((userProperties) => {
-        user = userProperties;
-        cy.login(user.username, user.password);
-      });
+  before('login and create test data', () => {
+    cy.createTempUser([
+      Permissions.settingsDataImportEnabled.gui,
+      Permissions.settingsDataImportView.gui,
+    ]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password);
     });
-
-    after('delete user', () => {
-      FileExtensionView.delete(fileExtensionName);
-      Users.deleteViaApi(user.userId);
-    });
-
-    it(
-      'C15851 Make sure the file extension settings are in alphabetical order when a new one is added (folijet) (TaaS)',
-      { tags: [TestTypes.extendedPath, DevTeams.folijet] },
-      () => {
-        cy.visit(SettingsMenu.fileExtensionsPath);
-        FileExtensions.verifyListOfExistingFileExtensionsIsDisplayed();
-        FileExtensions.verifyListIsSortedInAlphabeticalOrder();
-        FileExtensions.creatNewFileExtension(fileExtensionName);
-        FileExtensions.verifyListIsSortedInAlphabeticalOrder();
-      },
-    );
   });
+
+  after('delete user', () => {
+    FileExtensionView.delete(fileExtensionName);
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C15851 Make sure the file extension settings are in alphabetical order when a new one is added (folijet) (TaaS)',
+    { tags: [TestTypes.extendedPath, DevTeams.folijet] },
+    () => {
+      cy.visit(SettingsMenu.fileExtensionsPath);
+      FileExtensions.verifyListOfExistingFileExtensionsIsDisplayed();
+      FileExtensions.verifyListIsSortedInAlphabeticalOrder();
+      FileExtensions.creatNewFileExtension(fileExtensionName);
+      FileExtensions.verifyListIsSortedInAlphabeticalOrder();
+    },
+  );
 });
