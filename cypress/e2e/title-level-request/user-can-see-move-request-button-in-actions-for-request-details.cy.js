67ce7c8db7ad5aaf18973a9847a0a6f4a50a4382
Ilya_Lyskavets <Ilya_Lyskavets@epam.com>
2023-10-27 01:10:01 +0300

    fix test to use createFolioInstanceViaApi

--- cypress/e2e/title-level-request/user-can-see-move-request-button-in-actions-for-request-details.cy.js
+++ cypress/e2e/title-level-request/user-can-see-move-request-button-in-actions-for-request-details.cy.js
@@ -7,38 +7,90 @@ import ServicePoints from '../../support/fragments/settings/tenant/servicePoints
 import TitleLevelRequests from '../../support/fragments/settings/circulation/titleLevelRequests';
 import SettingsMenu from '../../support/fragments/settingsMenu';
 import UserEdit from '../../support/fragments/users/userEdit';
-import { FULFILMENT_PREFERENCES, REQUEST_LEVELS, REQUEST_TYPES } from '../../support/constants';
+import {
+  FULFILMENT_PREFERENCES,
+  ITEM_STATUS_NAMES,
+  REQUEST_LEVELS,
+  REQUEST_TYPES,
+} from '../../support/constants';
 import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
 import RequestDetail from '../../support/fragments/requests/requestDetail';
+import getRandomPostfix from '../../support/utils/stringTools';
+import generateItemBarcode from '../../support/utils/generateItemBarcode';
+import PatronGroups from '../../support/fragments/settings/users/patronGroups';
 
 describe('Title Level Request', () => {
-  const testData = {
-    servicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
-    folioInstances: InventoryInstances.generateFolioInstances(),
-  };
   let requestId;
+  const instanceData = {
+    title: `Instance ${getRandomPostfix()}`,
+  };
+  const testData = {
+    userServicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
+    itemBarcode: generateItemBarcode(),
+  };
+  const patronGroup = {
+    name: 'groupTLR' + getRandomPostfix(),
+  };
 
   before('Create test data', () => {
-    cy.getAdminToken();
-    ServicePoints.createViaApi(testData.servicePoint);
-    testData.defaultLocation = Location.getDefaultLocation(testData.servicePoint.id);
-    Location.createViaApi(testData.defaultLocation).then((location) => {
-      InventoryInstances.createFolioInstancesViaApi({
-        folioInstances: testData.folioInstances,
-        location,
+    cy.getAdminToken()
+      .then(() => {
+        cy.loginAsAdmin({
+          path: SettingsMenu.circulationTitleLevelRequestsPath,
+          waiter: TitleLevelRequests.waitLoading,
+        });
+        TitleLevelRequests.changeTitleLevelRequestsStatus('allow');
+        ServicePoints.createViaApi(testData.userServicePoint);
+        testData.defaultLocation = Location.getDefaultLocation(testData.userServicePoint.id);
+        Location.createViaApi(testData.defaultLocation);
+        cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
+          testData.instanceTypeId = instanceTypes[0].id;
+        });
+        cy.getHoldingTypes({ limit: 1 }).then((holdingTypes) => {
+          testData.holdingTypeId = holdingTypes[0].id;
+        });
+        cy.createLoanType({
+          name: `type_${getRandomPostfix()}`,
+        }).then((loanType) => {
+          testData.loanTypeId = loanType.id;
+        });
+        cy.getMaterialTypes({ limit: 1 }).then((materialTypes) => {
+          testData.materialTypeId = materialTypes.id;
+        });
+      })
+      .then(() => {
+        InventoryInstances.createFolioInstanceViaApi({
+          instance: {
+            instanceTypeId: testData.instanceTypeId,
+            title: instanceData.title,
+          },
+          holdings: [
+            {
+              holdingsTypeId: testData.holdingTypeId,
+              permanentLocationId: testData.defaultLocation.id,
+            },
+          ],
+          items: [
+            {
+              barcode: testData.itemBarcode,
+              status: { name: ITEM_STATUS_NAMES.AVAILABLE },
+              permanentLoanType: { id: testData.loanTypeId },
+              materialType: { id: testData.materialTypeId },
+            },
+          ],
+        }).then((instance) => {
+          testData.instanceId = instance.instanceId;
+        });
       });
+    PatronGroups.createViaApi(patronGroup.name).then((patronGroupResponse) => {
+      patronGroup.id = patronGroupResponse;
     });
-    cy.loginAsAdmin({
-      path: SettingsMenu.circulationTitleLevelRequestsPath,
-      waiter: TitleLevelRequests.waitLoading,
-    });
-    TitleLevelRequests.changeTitleLevelRequestsStatus('allow');
-    cy.createTempUser([Permissions.requestsAll.gui]).then((userProperties) => {
+    cy.createTempUser([Permissions.requestsAll.gui], patronGroup.name).then((userProperties) => {
       testData.user = userProperties;
       UserEdit.addServicePointsViaApi(
-        [testData.servicePoint.id],
+        [testData.userServicePoint.id],
         testData.user.userId,
-        testData.servicePoint.id,
+        testData.userServicePoint.id,
       );
       cy.login(testData.user.username, testData.user.password, {
         path: TopMenu.requestsPath,
@@ -46,8 +98,8 @@ describe('Title Level Request', () => {
       });
       Requests.createNewRequestViaApi({
         fulfillmentPreference: FULFILMENT_PREFERENCES.HOLD_SHELF,
-        instanceId: testData.folioInstances[0].instanceId,
-        pickupServicePointId: testData.servicePoint.id,
+        instanceId: testData.instanceId,
+        pickupServicePointId: testData.userServicePoint.id,
         requestDate: new Date(),
         requestLevel: REQUEST_LEVELS.TITLE,
         requestType: REQUEST_TYPES.PAGE,
@@ -60,12 +112,13 @@ describe('Title Level Request', () => {
 
   after('Delete test data', () => {
     Requests.deleteRequestViaApi(requestId);
-    UserEdit.changeServicePointPreferenceViaApi(testData.user.userId, [testData.servicePoint.id]);
-    ServicePoints.deleteViaApi(testData.servicePoint.id);
+    UserEdit.changeServicePointPreferenceViaApi(testData.user.userId, [
+      testData.userServicePoint.id,
+    ]);
+    ServicePoints.deleteViaApi(testData.userServicePoint.id);
     Users.deleteViaApi(testData.user.userId);
-    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(
-      testData.folioInstances[0].barcodes[0],
-    );
+    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(testData.itemBarcode);
+    PatronGroups.deleteViaApi(patronGroup.id);
     Location.deleteViaApiIncludingInstitutionCampusLibrary(
       testData.defaultLocation.institutionId,
       testData.defaultLocation.campusId,
@@ -79,8 +132,8 @@ describe('Title Level Request', () => {
     { tags: [TestTypes.criticalPath, DevTeams.vega] },
     () => {
       Requests.waitContentLoading();
-      Requests.findCreatedRequest(testData.folioInstances[0].barcodes[0]);
-      Requests.selectFirstRequest(testData.folioInstances[0].instanceTitle);
+      Requests.findCreatedRequest(testData.itemBarcode);
+      Requests.selectFirstRequest(instanceData.title);
       RequestDetail.openActions();
       RequestDetail.verifyMoveRequestButtonExists();
     },
