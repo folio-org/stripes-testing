02f34191cda891b065a6e327824abc974ed0d691
Aliaksandr Shuleika <Aliaksandr_Shuleika@epam.com>
2023-10-26 10:02:43 +0300

    added collection of action profiles

--- cypress/e2e/data-import/settings/unlink-from-job-profile.cy.js
+++ cypress/e2e/data-import/settings/unlink-from-job-profile.cy.js
@@ -17,21 +17,23 @@ import ActionProfileView from '../../../support/fragments/data_import/action_pro
 describe('data-import', () => {
   describe('Settings', () => {
     let user;
-    const actionProfile1 = {
-      typeValue: FOLIO_RECORD_TYPE.INSTANCE,
-      name: `C11116 autotest action profile1 ${getRandomPostfix()}`,
-      action: 'Create (all record types except MARC Authority or MARC Holdings)',
-    };
-    const actionProfile2 = {
-      typeValue: FOLIO_RECORD_TYPE.INSTANCE,
-      name: `C11116 autotest action profile2 ${getRandomPostfix()}`,
-      action: 'Create (all record types except MARC Authority or MARC Holdings)',
-    };
-    const actionProfile3 = {
-      typeValue: FOLIO_RECORD_TYPE.INSTANCE,
-      name: `C11116 autotest action profile3 ${getRandomPostfix()}`,
-      action: 'Create (all record types except MARC Authority or MARC Holdings)',
-    };
+    const collectionOfActionProfiles = [
+      {
+        typeValue: FOLIO_RECORD_TYPE.INSTANCE,
+        name: `C11116 autotest action profile1 ${getRandomPostfix()}`,
+        action: 'Create (all record types except MARC Authority or MARC Holdings)',
+      },
+      {
+        typeValue: FOLIO_RECORD_TYPE.INSTANCE,
+        name: `C11116 autotest action profile2 ${getRandomPostfix()}`,
+        action: 'Create (all record types except MARC Authority or MARC Holdings)',
+      },
+      {
+        typeValue: FOLIO_RECORD_TYPE.INSTANCE,
+        name: `C11116 autotest action profile3 ${getRandomPostfix()}`,
+        action: 'Create (all record types except MARC Authority or MARC Holdings)',
+      },
+    ];
     const matchProfile = {
       profileName: `C11116 autotest match profile ${getRandomPostfix()}`,
     };
@@ -58,11 +60,9 @@ describe('data-import', () => {
 
     after('delete test data', () => {
       JobProfiles.deleteJobProfile(jobProfile.profileName);
-      ActionProfiles.deleteActionProfile(actionProfile1.name);
-      ActionProfiles.deleteActionProfile(actionProfile2.name);
-      ActionProfiles.deleteActionProfile(actionProfile3.name);
-      FieldMappingProfileView.deleteViaApi(mappingProfile.name);
       MatchProfiles.deleteMatchProfile(matchProfile.profileName);
+      collectionOfActionProfiles.forEach((profile) => ActionProfiles.deleteActionProfile(profile.name));
+      FieldMappingProfileView.deleteViaApi(mappingProfile.name);
       Users.deleteViaApi(user.userId);
     });
 
@@ -72,28 +72,28 @@ describe('data-import', () => {
       () => {
         // create 3 action profiles
         cy.visit(SettingsMenu.actionProfilePath);
-        ActionProfiles.create(actionProfile1, mappingProfile.name);
-        ActionProfileView.close();
-        ActionProfiles.waitLoading();
-        ActionProfiles.create(actionProfile2, mappingProfile.name);
-        ActionProfileView.close();
-        ActionProfiles.waitLoading();
-        ActionProfiles.create(actionProfile3, mappingProfile.name);
-        ActionProfileView.close();
-        ActionProfiles.waitLoading();
+        collectionOfActionProfiles.forEach((profile) => {
+          ActionProfiles.create(profile, mappingProfile.name);
+          ActionProfileView.close();
+          ActionProfiles.waitLoading();
+        });
         // create Job profile with linked match and action profiles
         cy.visit(SettingsMenu.jobProfilePath);
         JobProfiles.createJobProfile(jobProfile);
         NewJobProfile.linkMatchProfile(matchProfile.profileName);
-        NewJobProfile.linkActionProfileForMatches(actionProfile1.name);
-        NewJobProfile.linkActionProfileForMatches(actionProfile2.name);
-        NewJobProfile.linkActionProfileForNonMatches(actionProfile3.name);
+        NewJobProfile.linkActionProfileForMatches(collectionOfActionProfiles[0].name);
+        NewJobProfile.linkActionProfileForMatches(collectionOfActionProfiles[1].name);
+        NewJobProfile.linkActionProfileForNonMatches(collectionOfActionProfiles[2].name);
         NewJobProfile.saveAndClose();
         JobProfileView.edit();
         JobProfileEdit.unlinkMatchProfile(0);
         JobProfileEdit.saveAndClose();
         JobProfileView.verifyLinkedProfiles(
-          [matchProfile.profileName, actionProfile2.name, actionProfile3.name],
+          [
+            matchProfile.profileName,
+            collectionOfActionProfiles[1].name,
+            collectionOfActionProfiles[2].name,
+          ],
           3,
         );
       },
