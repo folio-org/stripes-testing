2d7ec5ea8a5015f030974a64b972d2f87f1a14bc
kirstaVova <grouplbua@gmail.com>
2023-10-29 17:58:00 +0200

    C357551: Electronic format specific fields are cleared when changing order format from P/E Mix to Other (thunderjet) (TaaS)

---
+++ cypress/e2e/orders/changing-order-format-from-pe-mix-to-other.cy.js
@@ -0,0 +1,132 @@
+import uuid from 'uuid';
+import permissions from '../../support/dictionary/permissions';
+import testType from '../../support/dictionary/testTypes';
+import devTeams from '../../support/dictionary/devTeams';
+import FiscalYears from '../../support/fragments/finance/fiscalYears/fiscalYears';
+import TopMenu from '../../support/fragments/topMenu';
+import Ledgers from '../../support/fragments/finance/ledgers/ledgers';
+import Users from '../../support/fragments/users/users';
+import Funds from '../../support/fragments/finance/funds/funds';
+import FinanceHelp from '../../support/fragments/finance/financeHelper';
+import NewOrder from '../../support/fragments/orders/newOrder';
+import Orders from '../../support/fragments/orders/orders';
+import OrderLines from '../../support/fragments/orders/orderLines';
+import Organizations from '../../support/fragments/organizations/organizations';
+import NewOrganization from '../../support/fragments/organizations/newOrganization';
+import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import NewLocation from '../../support/fragments/settings/tenant/locations/newLocation';
+import NewInvoice from '../../support/fragments/invoices/newInvoice';
+import DateTools from '../../support/utils/dateTools';
+
+describe('Orders', () => {
+  const defaultFiscalYear = { ...FiscalYears.defaultUiFiscalYear };
+  const defaultLedger = { ...Ledgers.defaultUiLedger };
+  const defaultFund = { ...Funds.defaultUiFund };
+  const defaultOrder = {
+    ...NewOrder.defaultOneTimeOrder,
+    orderType: 'Ongoing',
+    ongoing: { isSubscription: false, manualRenewal: false },
+    approved: true,
+    reEncumber: true,
+  };
+  const secondOrder = {
+    ...NewOrder.defaultOneTimeOrder,
+    approved: true,
+    orderType: 'One-time',
+    reEncumber: true,
+  };
+  const organization = { ...NewOrganization.defaultUiOrganizations };
+  const allocatedQuantity = '100';
+  let user;
+  let orderNumber;
+  let servicePointId;
+  let location;
+
+  before(() => {
+    cy.getAdminToken();
+
+    FiscalYears.createViaApi(defaultFiscalYear).then((firstFiscalYearResponse) => {
+      defaultFiscalYear.id = firstFiscalYearResponse.id;
+      defaultLedger.fiscalYearOneId = defaultFiscalYear.id;
+      Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
+        defaultLedger.id = ledgerResponse.id;
+        defaultFund.ledgerId = defaultLedger.id;
+
+        Funds.createViaApi(defaultFund).then((fundResponse) => {
+          defaultFund.id = fundResponse.fund.id;
+
+          cy.loginAsAdmin({ path: TopMenu.fundPath, waiter: Funds.waitLoading });
+          FinanceHelp.searchByName(defaultFund.name);
+          Funds.selectFund(defaultFund.name);
+          Funds.addBudget(allocatedQuantity);
+        });
+      });
+    });
+    ServicePoints.getViaApi().then((servicePoint) => {
+      servicePointId = servicePoint[0].id;
+      NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then((res) => {
+        location = res;
+      });
+    });
+
+    Organizations.createOrganizationViaApi(organization).then((responseOrganizations) => {
+      organization.id = responseOrganizations;
+    });
+    defaultOrder.vendor = organization.name;
+
+    cy.visit(TopMenu.ordersPath);
+    Orders.createOrderForRollover(defaultOrder).then((orderResponse) => {
+      secondOrder.id = orderResponse.id;
+      orderNumber = orderResponse.poNumber;
+      OrderLines.addPOLine();
+      OrderLines.selectRandomInstanceInTitleLookUP('*', 15);
+      OrderLines.fillInPOLineInfoforPEMIXWithFund(
+        defaultFund,
+        '10',
+        '1',
+        '20',
+        location.institutionId,
+      );
+      OrderLines.backToEditingOrder();
+    });
+
+    cy.createTempUser([permissions.uiOrdersEdit.gui, permissions.uiOrdersView.gui]).then(
+      (userProperties) => {
+        user = userProperties;
+        cy.login(userProperties.username, userProperties.password, {
+          path: TopMenu.ordersPath,
+          waiter: Orders.waitLoading,
+        });
+      },
+    );
+  });
+
+  after(() => {
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C357551: “Electronic” format specific fields are cleared when changing order format from "P/E Mix" to "Other" (thunderjet) (TaaS)',
+    { tags: [testType.extendedPath, devTeams.thunderjet] },
+    () => {
+      Orders.searchByParameter('PO number', orderNumber);
+      Orders.selectFromResultsList(orderNumber);
+      OrderLines.selectPOLInOrder(0);
+      OrderLines.editPOLInOrder();
+      OrderLines.deleteFundsInPOL();
+      OrderLines.deleteLocationsInPOL();
+      OrderLines.fillInPOLineInfoforOtherWithFund(
+        defaultFund,
+        '10',
+        '1',
+        '10',
+        location.institutionId,
+      );
+      OrderLines.backToEditingOrder();
+      Orders.openOrder();
+      OrderLines.selectPOLInOrder(0);
+      OrderLines.editPOLInOrder();
+      OrderLines.addReveivingNoteToItemDetailsAndSave(orderNumber);
+    },
+  );
+});
