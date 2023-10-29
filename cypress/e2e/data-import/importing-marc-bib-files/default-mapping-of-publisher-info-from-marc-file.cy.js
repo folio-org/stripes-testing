8bd86a743a1921550d2f54c9142c8d1cb0a58e03
TatianaOne <tapusja2014@gmail.com>
2023-10-30 01:04:24 +0200

    added test

---
+++ cypress/e2e/data-import/importing-marc-bib-files/default-mapping-of-publisher-info-from-marc-file.cy.js
@@ -0,0 +1,66 @@
+import getRandomPostfix from '../../../support/utils/stringTools';
+import { DevTeams, TestTypes, Permissions } from '../../../support/dictionary';
+import { JOB_STATUS_NAMES } from '../../../support/constants';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import Logs from '../../../support/fragments/data_import/logs/logs';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
+import InstanceRecordView from '../../../support/fragments/inventory/instanceRecordView';
+
+describe('data-import', () => {
+  describe('Importing MARC Bib files', () => {
+    let user;
+    let instanceHrid;
+    const jobProfileToRun = 'Default - Create instance and SRS MARC Bib';
+    const filePathToUpload = 'marcBibFileForC6691.mrc';
+    const fileName = `C6691 autotestFile.${getRandomPostfix()}.mrc`;
+
+    before('create user and login', () => {
+      cy.createTempUser([
+        Permissions.moduleDataImportEnabled.gui,
+        Permissions.inventoryAll.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+
+        cy.login(user.username, user.password, {
+          path: TopMenu.dataImportPath,
+          waiter: DataImport.waitLoading,
+        });
+      });
+    });
+
+    // after('delete test data', () => {
+    //   Users.deleteViaApi(user.userId);
+    //   cy.getInstance({ limit: 1, expandAll: true, query: `"hrid"=="${instanceHrid}"` }).then(
+    //     (instance) => {
+    //       InventoryInstance.deleteInstanceViaApi(instance.id);
+    //     },
+    //   );
+    // });
+
+    it(
+      'C6691 Check the default mapping of publisher info (260/264) from the MARC record to the Inventory Instance Publication fields (folijet) (TaaS)',
+      { tags: [TestTypes.extendedPath, DevTeams.folijet] },
+      () => {
+        // TODO delete function after fix https://issues.folio.org/browse/MODDATAIMP-691
+        DataImport.verifyUploadState();
+        DataImport.uploadFile(filePathToUpload, fileName);
+        JobProfiles.search(jobProfileToRun);
+        JobProfiles.runImportFile();
+        JobProfiles.waitFileIsImported(fileName);
+        Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
+        Logs.openFileDetails(fileName);
+        FileDetails.openInstanceInInventory('Created');
+        InventoryInstance.getAssignedHRID().then((initialInstanceHrId) => {
+          instanceHrid = initialInstanceHrId;
+
+          InstanceRecordView.waitLoading();
+          InstanceRecordView.verifyInstanceRecordViewOpened();
+        });
+      },
+    );
+  });
+});
