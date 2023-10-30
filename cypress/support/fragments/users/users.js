15fad6ae1f93b009528c929a854e7e809badca41
tomlee <tom_li@epam.com>
2023-10-30 04:23:18 -0500

    C410760 - "User type" field is displayed on create user page (Poppy +) (Thunderjet) (TaaS) (#1929)
    * add new scripts for C410760
    
    * add new file for C410760
    
    * add new permission for C410760
    
    * change the test path and dev team for C410760
    
    * fix issue with review suggestion:
    remove log
    change permission - uiUserCreateResetPassword
    move cancel button to const
    change describe
    
    * change code --
    use 1 line import for DevTeams, Permissions, TestTypes
    remove select because not be used
    
    * combine 'before' into one

--- cypress/support/fragments/users/users.js
+++ cypress/support/fragments/users/users.js
@@ -162,6 +162,12 @@ export default {
     );
   },
 
+  verifyUserTypeOnUserDetailsPane(userType) {
+    cy.expect(
+      userDetailsPane.find(KeyValue('User type')).has({ value: `${userType}` }),
+    );
+  },
+
   verifyCustomFieldOnUserDetailsPane(name, text) {
     cy.expect(userDetailsPane.find(KeyValue(name)).has({ value: text }));
   },
