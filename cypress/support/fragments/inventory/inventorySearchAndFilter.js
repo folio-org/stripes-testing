a560f266610965b5556082437419fbc787ccd9f8
Victor Bystrov <bystrov1988@gmail.com>
2023-11-02 15:57:57 +0300

    C353938

--- cypress/support/fragments/inventory/inventorySearchAndFilter.js
+++ cypress/support/fragments/inventory/inventorySearchAndFilter.js
@@ -494,6 +494,10 @@ export default {
     cy.do([inventorySearchAndFilterInput.choose(searchOption), keywordInput.fillIn(text)]);
   },
 
+  verifySelectedSearchOption(option) {
+    cy.expect(inventorySearchAndFilterInput.has({ value: option }));
+  },
+
   clickSearch() {
     cy.do(searchButton.click());
   },
