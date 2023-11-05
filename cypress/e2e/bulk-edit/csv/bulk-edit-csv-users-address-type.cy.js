5e1edbac8e2067ce5408c5ffc00a4666903eaa23
blue-raisin <bunicorn486@gmail.com>
2023-11-05 20:23:43 +0500

    C408810

---
+++ cypress/e2e/bulk-edit/csv/bulk-edit-csv-users-address-type.cy.js
@@ -0,0 +1,131 @@
+import TopMenu from '../../../support/fragments/topMenu';
+import testTypes from '../../../support/dictionary/testTypes';
+import permissions from '../../../support/dictionary/permissions';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import FileManager from '../../../support/utils/fileManager';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import devTeams from '../../../support/dictionary/devTeams';
+import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';
+import Users from '../../../support/fragments/users/users';
+import UsersSearchPane from '../../../support/fragments/users/usersSearchPane';
+import UserEdit from '../../../support/fragments/users/userEdit';
+import ExportFile from '../../../support/fragments/data-export/exportFile';
+
+let user;
+let addressTypeId;
+let addressType;
+const userBarcodesFileName = `userBarcodes_${getRandomPostfix()}.csv`;
+const matchedRecordsFileName = `*Matched-Records-${userBarcodesFileName}`;
+const previewOfProposedChangesFileName = `*-Updates-Preview-${userBarcodesFileName}`;
+const updatedRecordsFileName = `*-Changed-Records*-${userBarcodesFileName}`;
+const editedFileName = `edited-records-${getRandomPostfix()}.csv`;
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+      cy.createTempUser(
+        [
+          permissions.bulkEditLogsView.gui,
+          permissions.bulkEditCsvView.gui,
+          permissions.bulkEditCsvEdit.gui,
+          permissions.uiUsersViewAllSettings.gui,
+          permissions.uiUserEdit.gui,
+        ],
+        'faculty',
+      ).then((userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password, {
+          path: TopMenu.usersPath,
+          waiter: UsersSearchPane.waitLoading,
+        });
+        FileManager.createFile(`cypress/fixtures/${userBarcodesFileName}`, user.barcode);
+      });
+    });
+
+    after('delete test data', () => {
+      FileManager.deleteFile(`cypress/fixtures/${userBarcodesFileName}`);
+      FileManager.deleteFile(`cypress/fixtures/${editedFileName}`);
+      FileManager.deleteFileFromDownloadsByMask(
+        userBarcodesFileName,
+        matchedRecordsFileName,
+        previewOfProposedChangesFileName,
+        updatedRecordsFileName,
+      );
+      cy.getAdminToken(() => {
+        Users.deleteViaApi(user.userId);
+      });
+    });
+
+    it(
+      'C408810 Verify that "addressType" is shown in the Previews _ Locals (firebird) (TaaS)',
+      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      () => {
+        UsersSearchPane.searchByUsername(user.username);
+        UserEdit.openEdit();
+        UserEdit.addAddress();
+        UserEdit.saveAndClose();
+        cy.wait(2000);
+
+        cy.getUsers({ limit: 1, query: `"username"="${user.username}"` })
+          .then((users) => {
+            addressTypeId = users[0].personal.addresses[0].addressTypeId;
+          })
+          .then(() => {
+            Users.getUserAddressTypesApi(addressTypeId)
+              .then((body) => {
+                addressType = body.addressType;
+              })
+              .then(() => {
+                cy.visit(TopMenu.bulkEditPath);
+
+                BulkEditSearchPane.checkUsersRadio();
+                BulkEditSearchPane.selectRecordIdentifier('User Barcodes');
+                BulkEditSearchPane.uploadFile(userBarcodesFileName);
+                BulkEditSearchPane.waitFileUploading();
+                BulkEditActions.downloadMatchedResults();
+                BulkEditSearchPane.changeShowColumnCheckboxIfNotYet('Addresses');
+                BulkEditSearchPane.verifyChangesUnderColumns('Addresses', addressType);
+
+                const newFirstName = `testNewFirstName_${getRandomPostfix()}`;
+                BulkEditActions.prepareValidBulkEditFile(
+                  matchedRecordsFileName,
+                  editedFileName,
+                  user.firstName,
+                  newFirstName,
+                );
+                BulkEditActions.openStartBulkEditForm();
+                BulkEditSearchPane.uploadFile(editedFileName);
+                BulkEditSearchPane.waitFileUploading();
+                BulkEditActions.clickNext();
+                BulkEditActions.commitChanges();
+                BulkEditSearchPane.waitFileUploading();
+                BulkEditActions.openActions();
+                BulkEditActions.downloadMatchedRecordsAbsent();
+
+                BulkEditSearchPane.openLogsSearch();
+                BulkEditSearchPane.verifyLogsPane();
+                BulkEditSearchPane.checkUsersCheckbox();
+                BulkEditSearchPane.clickActionsRunBy(user.username);
+                BulkEditSearchPane.verifyLogsRowActionWhenCompleted();
+
+                BulkEditSearchPane.downloadFileUsedToTrigger();
+                ExportFile.verifyFileIncludes(userBarcodesFileName, [user.barcode]);
+
+                BulkEditSearchPane.downloadFileWithMatchingRecords();
+                ExportFile.verifyFileIncludes(matchedRecordsFileName, [user.barcode]);
+
+                BulkEditSearchPane.downloadFileWithProposedChanges();
+                ExportFile.verifyFileIncludes(previewOfProposedChangesFileName, [newFirstName]);
+
+                BulkEditSearchPane.downloadFileWithUpdatedRecords();
+                ExportFile.verifyFileIncludes(updatedRecordsFileName, [newFirstName]);
+
+                cy.visit(TopMenu.usersPath);
+                UsersSearchPane.searchByUsername(user.username);
+                Users.verifyFirstNameOnUserDetailsPane(newFirstName);
+              });
+          });
+      },
+    );
+  });
+});
