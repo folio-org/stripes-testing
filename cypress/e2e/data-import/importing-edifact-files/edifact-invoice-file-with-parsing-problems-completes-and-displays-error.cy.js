6569d03522704d15a73f1546ec0076a6ff183cce
TatianaOne <tapusja2014@gmail.com>
2023-11-06 16:41:59 +0200

    added checks

--- cypress/e2e/data-import/importing-edifact-files/edifact-invoice-file-with-parsing-problems-completes-and-displays-error.cy.js
+++ cypress/e2e/data-import/importing-edifact-files/edifact-invoice-file-with-parsing-problems-completes-and-displays-error.cy.js
@@ -11,6 +11,7 @@ import {
   BATCH_GROUP,
   ACCEPTED_DATA_TYPE_NAMES,
   VENDOR_NAMES,
+  JOB_STATUS_NAMES,
 } from '../../../support/constants';
 import TopMenu from '../../../support/fragments/topMenu';
 import DataImport from '../../../support/fragments/data_import/dataImport';
@@ -18,12 +19,37 @@ import Logs from '../../../support/fragments/data_import/logs/logs';
 import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
 import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
 import Users from '../../../support/fragments/users/users';
-import InvoiceView from '../../../support/fragments/invoices/invoiceView';
 import FieldMappingProfileView from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfileView';
+import JsonScreenView from '../../../support/fragments/data_import/logs/jsonScreenView';
 
 describe('data-import', () => {
   describe('Importing EDIFACT files', () => {
     let user;
+    const profileForDuplicate = FieldMappingProfiles.mappingProfileForDuplicate.harrassowitz;
+    const filePathToUpload = 'ediFileForC377019.edi';
+    const fileName = `C377019 autotestFile.${getRandomPostfix()}.edi`;
+    const title = 'ERROR_INVOICE';
+    const errorMessage =
+      '{"errors":[{"name":"io.xlate.edi.internal.stream.tokenization.EDIException","message":"EDIE003 - Invalid processing state; INVALID (previous: TAG_1); input: \' \' in segment IMD at position 44"}]}';
+    const mappingProfile = {
+      name: `C377019 Harrassowitz error invoice ${getRandomPostfix()}`,
+      incomingRecordType: NewFieldMappingProfile.incomingRecordType.edifact,
+      typeValue: FOLIO_RECORD_TYPE.INVOICE,
+      description: '',
+      batchGroup: BATCH_GROUP.FOLIO,
+      organizationName: VENDOR_NAMES.HARRASSOWITZ,
+      paymentMethod: PAYMENT_METHOD.CASH,
+      currency: 'USD',
+    };
+    const actionProfile = {
+      name: `C377019 Harrassowitz error invoice ${getRandomPostfix()}`,
+      typeValue: FOLIO_RECORD_TYPE.INVOICE,
+    };
+    const jobProfile = {
+      ...NewJobProfile.defaultJobProfile,
+      profileName: `C377019 Harrassowitz error invoice ${getRandomPostfix()}`,
+      acceptedType: ACCEPTED_DATA_TYPE_NAMES.EDIFACT,
+    };
 
     before('login', () => {
       cy.createTempUser([
@@ -39,10 +65,52 @@ describe('data-import', () => {
       });
     });
 
+    after('delete test data', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(user.userId);
+      JobProfiles.deleteJobProfile(jobProfile.profileName);
+      ActionProfiles.deleteActionProfile(actionProfile.name);
+      FieldMappingProfileView.deleteViaApi(mappingProfile.name);
+    });
+
     it(
       'C377019 Confirm an EDIFACT invoice file with parsing problems completes and displays an error (folijet) (TaaS)',
       { tags: [TestTypes.extendedPath, DevTeams.folijet] },
-      () => {},
+      () => {
+        // create Field mapping profile
+        cy.visit(SettingsMenu.mappingProfilePath);
+        FieldMappingProfiles.createInvoiceMappingProfile(mappingProfile, profileForDuplicate);
+        FieldMappingProfiles.checkMappingProfilePresented(mappingProfile.name);
+
+        // create Action profile and link it to Field mapping profile
+        cy.visit(SettingsMenu.actionProfilePath);
+        ActionProfiles.create(actionProfile, mappingProfile.name);
+        ActionProfiles.checkActionProfilePresented(actionProfile.name);
+
+        // create Job profile
+        cy.visit(SettingsMenu.jobProfilePath);
+        JobProfiles.createJobProfile(jobProfile);
+        NewJobProfile.linkActionProfile(actionProfile);
+        NewJobProfile.saveAndClose();
+        JobProfiles.checkJobProfilePresented(jobProfile.profileName);
+
+        // upload a marc file for creating of the new instance, holding and item
+        cy.visit(TopMenu.dataImportPath);
+        // TODO delete function after fix https://issues.folio.org/browse/MODDATAIMP-691
+        DataImport.verifyUploadState();
+        DataImport.uploadFile(filePathToUpload, fileName);
+        DataImport.waitFileIsUploaded();
+        JobProfiles.search(jobProfile.profileName);
+        JobProfiles.selectJobProfile();
+        JobProfiles.runImportFile();
+        JobProfiles.waitFileIsImported(fileName);
+        Logs.checkImportFile(jobProfile.profileName);
+        Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED_WITH_ERRORS);
+        Logs.openFileDetails(fileName);
+        FileDetails.openJsonScreen(title);
+        JsonScreenView.verifyJsonScreenIsOpened();
+        JsonScreenView.verifyContentInTab(errorMessage);
+      },
     );
   });
 });
