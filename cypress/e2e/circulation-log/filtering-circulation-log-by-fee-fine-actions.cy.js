9bd796a5f65dcee92084b9a36b475bf9cb439221
Jasurbek Erkinov <re.nine.jas@gmail.com>
2023-11-06 20:22:09 +0500

    added C17065

--- cypress/e2e/circulation-log/filtering-circulation-log-by-fee-fine-actions.cy.js
+++ cypress/e2e/circulation-log/filtering-circulation-log-by-fee-fine-actions.cy.js
@@ -442,4 +442,12 @@ describe('Circulation log', () => {
       });
     },
   );
+
+  it(
+    'C17065 Filter circulation log by cancelled as errorror (volaris)',
+    { tags: [TestTypes.criticalPath, devTeams.volaris] },
+    () => {
+      filterByAction('Cancelled as error');
+    },
+  );
 });
