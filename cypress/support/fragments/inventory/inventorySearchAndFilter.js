db82da2530eb60a81993d51af058abff7700ba35
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-11-07 14:28:30 +0200

    changed search button

--- cypress/support/fragments/inventory/inventorySearchAndFilter.js
+++ cypress/support/fragments/inventory/inventorySearchAndFilter.js
@@ -44,7 +44,6 @@ const instanceStatusAccordion = Accordion({ id: 'instanceStatus' });
 const tagsAccordion = Accordion({ id: 'instancesTags' });
 const keywordInput = TextArea({ id: 'input-inventory-search' });
 const searchButton = Button({ type: 'submit' });
-const searchTextField = TextField('Search ');
 const inventorySearchAndFilter = TextInput({ id: 'input-inventory-search' });
 const inventorySearchAndFilterInput = Select({
   id: 'input-inventory-search-qindex',
@@ -429,7 +428,7 @@ export default {
 
   searchByParameter: (parameter, value) => {
     cy.do(SearchField({ id: 'input-inventory-search' }).selectIndex(parameter));
-    cy.do(searchTextField.fillIn(value));
+    cy.do(keywordInput.fillIn(value));
     cy.do(searchButton.focus());
     cy.do(searchButton.click());
   },
