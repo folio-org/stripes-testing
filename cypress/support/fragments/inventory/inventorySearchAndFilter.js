5ca2995e1c74ebdc565072f483d28dfa85f59056
arsenhovh <arsen_hovhannisyan@epam.com>
2023-11-07 12:44:58 +0400

    fix linter

--- cypress/support/fragments/inventory/inventorySearchAndFilter.js
+++ cypress/support/fragments/inventory/inventorySearchAndFilter.js
@@ -44,7 +44,6 @@ const instanceStatusAccordion = Accordion({ id: 'instanceStatus' });
 const tagsAccordion = Accordion({ id: 'instancesTags' });
 const keywordInput = TextArea({ id: 'input-inventory-search' });
 const searchButton = Button({ type: 'submit' });
-const searchTextField = TextField('Search ');
 const inventorySearchAndFilter = TextInput({ id: 'input-inventory-search' });
 const inventorySearchAndFilterInput = Select({
   id: 'input-inventory-search-qindex',
