91e23fce94bfcb6d120fda62f03ddf2915dc5f4a
kirstaVova <grouplbua@gmail.com>
2023-11-08 18:32:58 +0200

    C350976: Ledger export settings: last year Fund with budget, Economic (Active) Class, Export settings; Expense classes- None (thunderjet) (TaaS)

---
+++ cypress/e2e/finance/ledgers/ledger-export-settings-pairwise-variants/Ledger export settings.cy.js
@@ -0,0 +1,93 @@
+import permissions from '../../../../support/dictionary/permissions';
+import testType from '../../../../support/dictionary/testTypes';
+import devTeams from '../../../../support/dictionary/devTeams';
+import getRandomPostfix from '../../../../support/utils/stringTools';
+import FiscalYears from '../../../../support/fragments/finance/fiscalYears/fiscalYears';
+import TopMenu from '../../../../support/fragments/topMenu';
+import Ledgers from '../../../../support/fragments/finance/ledgers/ledgers';
+import Users from '../../../../support/fragments/users/users';
+import Funds from '../../../../support/fragments/finance/funds/funds';
+import FinanceHelp from '../../../../support/fragments/finance/financeHelper';
+
+describe('Finance: Ledgers', () => {
+  const defaultFiscalYear = { ...FiscalYears.defaultRolloverFiscalYear };
+  const defaultLedger = { ...Ledgers.defaultUiLedger };
+  const firstFund = { ...Funds.defaultUiFund };
+  const secondFund = {
+    name: `autotest_fund2_${getRandomPostfix()}`,
+    code: getRandomPostfix(),
+    externalAccountNo: getRandomPostfix(),
+    fundStatus: 'Active',
+    description: `This is fund created by E2E test automation script_${getRandomPostfix()}`,
+  };
+  const allocatedQuantityForFirstFund = '100';
+  const allocatedQuantityForSecondFund = '0';
+  let user;
+
+  before(() => {
+    cy.getAdminToken();
+    // create first Fiscal Year and prepere 2 Funds for Rollover
+    FiscalYears.createViaApi(defaultFiscalYear).then((firstFiscalYearResponse) => {
+      defaultFiscalYear.id = firstFiscalYearResponse.id;
+      defaultLedger.fiscalYearOneId = defaultFiscalYear.id;
+      Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
+        defaultLedger.id = ledgerResponse.id;
+        firstFund.ledgerId = defaultLedger.id;
+        secondFund.ledgerId = defaultLedger.id;
+
+        Funds.createViaApi(firstFund).then((fundResponse) => {
+          firstFund.id = fundResponse.fund.id;
+
+          cy.loginAsAdmin({ path: TopMenu.fundPath, waiter: Funds.waitLoading });
+          FinanceHelp.searchByName(firstFund.name);
+          Funds.selectFund(firstFund.name);
+          Funds.addBudget(allocatedQuantityForFirstFund);
+        });
+
+        Funds.createViaApi(secondFund).then((secondFundResponse) => {
+          secondFund.id = secondFundResponse.fund.id;
+
+          cy.visit(TopMenu.fundPath);
+          FinanceHelp.searchByName(secondFund.name);
+          Funds.selectFund(secondFund.name);
+          Funds.addBudget(allocatedQuantityForSecondFund);
+        });
+      });
+    });
+
+    cy.createTempUser([
+      permissions.uiFinanceCreateAllocations.gui,
+      permissions.uiFinanceViewFundAndBudget.gui,
+      permissions.uiFinanceViewLedger.gui,
+    ]).then((userProperties) => {
+      user = userProperties;
+      cy.login(userProperties.username, userProperties.password, {
+        path: TopMenu.ledgerPath,
+        waiter: Ledgers.waitForLedgerDetailsLoading,
+      });
+    });
+  });
+
+  after(() => {
+    cy.getAdminToken();
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C350976: Ledger export settings: last year Fund with budget, Economic (Active) Class, Export settings; Expense classes- None (thunderjet) (TaaS)',
+    { tags: [testType.criticalPath, devTeams.thunderjet] },
+    () => {
+      FinanceHelp.searchByName(defaultLedger.name);
+      Ledgers.selectLedger(defaultLedger.name);
+      Ledgers.selectFundInLedger(secondFund.name);
+      Funds.selectBudgetDetails();
+      Funds.moveSimpleAllocation(firstFund, secondFund, '20');
+      Funds.closeBudgetDetails();
+      cy.visit(TopMenu.fundPath);
+      FinanceHelp.searchByName(firstFund.name);
+      Funds.selectFund(firstFund.name);
+      Funds.selectBudgetDetails();
+      Funds.checkFundingInformation('$100.00', '$0.00', '$20.00', '$80.00', '$0.00', '$80.00');
+    },
+  );
+});
