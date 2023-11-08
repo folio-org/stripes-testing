28af32346c0fe12cb2b1653515cc285fa71636cc
kirstaVova <grouplbua@gmail.com>
2023-11-08 18:04:48 +0200

    C350937: Update accordion labels and logic on Invoice (thunderjet) (TaaS)

---
+++ cypress/e2e/invoices/update-accordion-labels-and-logic-on-invoice.cy.js
@@ -0,0 +1,145 @@
+import permissions from '../../support/dictionary/permissions';
+import testType from '../../support/dictionary/testTypes';
+import devTeams from '../../support/dictionary/devTeams';
+import FiscalYears from '../../support/fragments/finance/fiscalYears/fiscalYears';
+import TopMenu from '../../support/fragments/topMenu';
+import Ledgers from '../../support/fragments/finance/ledgers/ledgers';
+import Users from '../../support/fragments/users/users';
+import Funds from '../../support/fragments/finance/funds/funds';
+import FinanceHelp from '../../support/fragments/finance/financeHelper';
+import NewOrder from '../../support/fragments/orders/newOrder';
+import Orders from '../../support/fragments/orders/orders';
+import OrderLines from '../../support/fragments/orders/orderLines';
+import Organizations from '../../support/fragments/organizations/organizations';
+import NewOrganization from '../../support/fragments/organizations/newOrganization';
+import NewInvoice from '../../support/fragments/invoices/newInvoice';
+import Invoices from '../../support/fragments/invoices/invoices';
+import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import NewLocation from '../../support/fragments/settings/tenant/locations/newLocation';
+import NewExpenceClass from '../../support/fragments/settings/finance/newExpenseClass';
+import SettingsFinance from '../../support/fragments/settings/finance/settingsFinance';
+import SettingsMenu from '../../support/fragments/settingsMenu';
+import { InvoiceView } from '../../support/fragments/invoices';
+
+describe('Invoices', () => {
+  const defaultFiscalYear = { ...FiscalYears.defaultUiFiscalYear };
+  const defaultLedger = {
+    ...Ledgers.defaultUiLedger,
+    restrictEncumbrance: false,
+    restrictExpenditures: true,
+  };
+  const defaultFund = { ...Funds.defaultUiFund };
+  const defaultOrder = {
+    ...NewOrder.defaultOneTimeOrder,
+    orderType: 'One-time',
+  };
+  const organization = { ...NewOrganization.defaultUiOrganizations };
+  const invoice = { ...NewInvoice.defaultUiInvoice };
+  const firstExpenseClass = { ...NewExpenceClass.defaultUiBatchGroup };
+  const allocatedQuantity = '100';
+  defaultFiscalYear.code = defaultFiscalYear.code.slice(0, -1) + '1';
+  let user;
+  let orderNumber;
+  let servicePointId;
+  let location;
+
+  before(() => {
+    cy.getAdminToken();
+    cy.loginAsAdmin();
+    cy.visit(SettingsMenu.expenseClassesPath);
+    SettingsFinance.createNewExpenseClass(firstExpenseClass);
+    FiscalYears.createViaApi(defaultFiscalYear).then((firstFiscalYearResponse) => {
+      defaultFiscalYear.id = firstFiscalYearResponse.id;
+      defaultLedger.fiscalYearOneId = defaultFiscalYear.id;
+      Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
+        defaultLedger.id = ledgerResponse.id;
+        defaultFund.ledgerId = defaultLedger.id;
+
+        Funds.createViaApi(defaultFund).then((fundResponse) => {
+          defaultFund.id = fundResponse.fund.id;
+
+          cy.visit(TopMenu.fundPath);
+          FinanceHelp.searchByName(defaultFund.name);
+          Funds.selectFund(defaultFund.name);
+          Funds.addBudget(allocatedQuantity);
+        });
+
+        ServicePoints.getViaApi().then((servicePoint) => {
+          servicePointId = servicePoint[0].id;
+          NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then((res) => {
+            location = res;
+          });
+        });
+
+        Organizations.createOrganizationViaApi(organization).then((responseOrganizations) => {
+          organization.id = responseOrganizations;
+          invoice.accountingCode = organization.erpCode;
+          cy.getBatchGroups().then((batchGroup) => {
+            invoice.batchGroup = batchGroup.name;
+          });
+        });
+        defaultOrder.vendor = organization.name;
+        cy.visit(TopMenu.ordersPath);
+        Orders.createApprovedOrderForRollover(defaultOrder, true, true).then(
+          (firstOrderResponse) => {
+            defaultOrder.id = firstOrderResponse.id;
+            orderNumber = firstOrderResponse.poNumber;
+            Orders.checkCreatedOrder(defaultOrder);
+            OrderLines.addPOLine();
+            OrderLines.selectRandomInstanceInTitleLookUP('*', 15);
+            OrderLines.rolloverPOLineInfoforPhysicalMaterialWithFund(
+              defaultFund,
+              '10',
+              '1',
+              '10',
+              location.institutionId,
+            );
+            OrderLines.backToEditingOrder();
+            Orders.openOrder();
+          },
+        );
+        cy.visit(TopMenu.invoicesPath);
+        Invoices.createRolloverInvoiceWithFY(invoice, organization.name, defaultFiscalYear);
+      });
+    });
+
+    cy.createTempUser([
+      permissions.uiInvoicesApproveInvoices.gui,
+      permissions.viewEditCreateInvoiceInvoiceLine.gui,
+      permissions.viewEditDeleteInvoiceInvoiceLine.gui,
+      permissions.uiInvoicesPayInvoices.gui,
+      permissions.uiOrdersApprovePurchaseOrders.gui,
+      permissions.uiOrdersEdit.gui,
+      permissions.uiOrdersUnopenpurchaseorders.gui,
+    ]).then((userProperties) => {
+      user = userProperties;
+      cy.login(userProperties.username, userProperties.password, {
+        path: TopMenu.invoicesPath,
+        waiter: Invoices.waitLoading,
+      });
+    });
+  });
+
+  after(() => {
+    cy.getAdminToken();
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C350937: Update accordion labels and logic on Invoice (thunderjet) (TaaS)',
+    { tags: [testType.criticalPath, devTeams.thunderjet] },
+    () => {
+      Invoices.searchByNumber(invoice.invoiceNumber);
+      Invoices.selectInvoice(invoice.invoiceNumber);
+      Invoices.createInvoiceLineFromPol(orderNumber);
+      Invoices.approveInvoice();
+      Invoices.selectInvoiceLine();
+      Invoices.openPOLFromInvoiceLineInCurrentPage(`${orderNumber}-1`);
+      OrderLines.viewPO();
+      Orders.unOpenOrderAndDeleteItems();
+      Orders.selectInvoiceInRelatedInvoicesList(invoice.invoiceNumber);
+      InvoiceView.checkInvoiceCanNotBeApprovedWarning();
+      Invoices.checkPayButtonIsDissabled();
+    },
+  );
+});
