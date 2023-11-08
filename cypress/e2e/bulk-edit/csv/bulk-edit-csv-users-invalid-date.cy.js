6e967a0ff8a20ea1958ded277efd6852f1f7eb1b
KarapetyanSofia <sofia_karapetyan@epam.com>
2023-11-08 14:44:48 +0400

    C353943 Negative: Verify Local updating records with invalid data (firebird) (TaaS)

---
+++ cypress/e2e/bulk-edit/csv/bulk-edit-csv-users-invalid-date.cy.js
@@ -0,0 +1,76 @@
+import { DevTeams, Permissions, TestTypes } from '../../../support/dictionary';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import FileManager from '../../../support/utils/fileManager';
+import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';
+import getRandomPostfix, { getTestEntityValue } from '../../../support/utils/stringTools';
+import ExportFile from '../../../support/fragments/data-export/exportFile';
+
+let user;
+const userUUIDsFileName = `userUUIDs_${getRandomPostfix()}.csv`;
+const matchedRecordsFile = `Matched-Records-${userUUIDsFileName}`;
+const editedFileName = `Committing-changes-Errors-${getRandomPostfix()}.csv`;
+const patronGroup = {
+  name: getTestEntityValue('staff'),
+};
+const invalidPatronGroup = 'invalidPatronGroup';
+
+describe('bulk-edit', () => {
+  describe('csv approach', () => {
+    before('create test data', () => {
+      cy.createTempUser([
+        Permissions.bulkEditCsvView.gui,
+        Permissions.bulkEditCsvEdit.gui,
+        Permissions.uiUserEdit.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password, {
+          path: TopMenu.bulkEditPath,
+          waiter: BulkEditSearchPane.waitLoading,
+        });
+
+        FileManager.createFile(`cypress/fixtures/${userUUIDsFileName}`, user.userId);
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(user.userId);
+        FileManager.deleteFile(`cypress/fixtures/${userUUIDsFileName}`);
+        FileManager.deleteFileFromDownloadsByMask(editedFileName);
+      });
+    });
+
+    it(
+      'C353943 Negative: Verify Local updating records with invalid data (firebird) (TaaS)',
+      { tags: [TestTypes.extendedPath, DevTeams.firebird] },
+      () => {
+        BulkEditSearchPane.checkUsersRadio();
+        BulkEditSearchPane.selectRecordIdentifier('User UUIDs');
+        BulkEditSearchPane.isDragAndDropAreaDisabled(false);
+        BulkEditSearchPane.uploadFile(userUUIDsFileName);
+        BulkEditSearchPane.waitFileUploading();
+        BulkEditActions.downloadMatchedResults();
+
+        BulkEditActions.prepareValidBulkEditFile(
+          matchedRecordsFile,
+          editedFileName,
+          patronGroup,
+          invalidPatronGroup,
+        );
+        BulkEditActions.openActions();
+        BulkEditActions.openStartBulkEditForm();
+        BulkEditSearchPane.uploadFile(editedFileName);
+        BulkEditSearchPane.waitFileUploading();
+        BulkEditActions.clickNext();
+        BulkEditActions.commitChanges();
+
+        BulkEditSearchPane.verifyErrorLabelAfterChanges(editedFileName, 0, 1);
+        BulkEditActions.openActions();
+        BulkEditActions.downloadErrors();
+        ExportFile.verifyFileIncludes(editedFileName, ['No change in value required']);
+      },
+    );
+  });
+});
