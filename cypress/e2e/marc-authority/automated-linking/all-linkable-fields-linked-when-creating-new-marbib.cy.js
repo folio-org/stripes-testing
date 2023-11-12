5d59e3099564cf9a58c6af3f02f69f0cde231653
ali-soft-engineer <alisherken.1966.1969@gmail.com>
2023-11-13 02:11:23 +0500

    small fix

--- cypress/e2e/marc-authority/automated-linking/all-linkable-fields-linked-when-creating-new-marbib.cy.js
+++ cypress/e2e/marc-authority/automated-linking/all-linkable-fields-linked-when-creating-new-marbib.cy.js
@@ -31,7 +31,7 @@ describe('MARC -> MARC Bibliographic -> Edit MARC bib -> Automated linking', () 
       rowIndex: 4,
       tag: '100',
       content: '$a C389483 Coates, Ta-Nehisi,$eauthor. $0 n2008001084C389483',
-      value: 'C387538 Coates, Ta-Nehisi',
+      value: 'C389483 Coates, Ta-Nehisi',
       type: 'Contributor',
     },
     {
@@ -45,70 +45,70 @@ describe('MARC -> MARC Bibliographic -> Edit MARC bib -> Automated linking', () 
       rowIndex: 6,
       tag: '111',
       content: '$a C389483 Pimedate Ööde Filmifestival $0 no2009176429C389483',
-      value: 'C387538 Pimedate Ööde Filmifestival',
+      value: 'C389483 Pimedate Ööde Filmifestival',
       type: 'Contributor',
     },
     {
       rowIndex: 7,
       tag: '130',
       content: '$a C389483 Marvel comics $0 n80026980C389483',
-      value: 'C387538 Marvel comics',
+      value: 'C389483 Marvel comics',
       type: 'Title data',
     },
     {
       rowIndex: 8,
       tag: '240',
       content: '$a C389483 Black Panther $0 no2020024230C389483',
-      value: 'C387538 Black Panther',
+      value: 'C389483 Black Panther',
       type: 'Title data',
     },
     {
       rowIndex: 9,
       tag: '600',
       content: '$a C389483 Black Panther $c (Fictitious character) $2 fast $0 n2016004081C389483',
-      value: 'C387538 Black Panther (Fictitious character)',
+      value: 'C389483 Black Panther (Fictitious character)',
       type: 'Subject',
     },
     {
       rowIndex: 10,
       tag: '610',
       content: '$a C389483 Black Panther $0 nb2009024488C389483',
-      value: 'C387538 Black Panther Movement',
+      value: 'C389483 Black Panther Movement',
       type: 'Subject',
     },
     {
       rowIndex: 11,
       tag: '611',
       content: '$a C389483 Panther Photographic $0 n82216757C389483',
-      value: 'C387538 Panther Photographic International',
+      value: 'C389483 Panther Photographic International',
       type: 'Subject',
     },
     {
       rowIndex: 12,
       tag: '630',
       content: '$a C389483 Black Panther $0 no2023006889C389483',
-      value: 'C387538 Black Panther, Wakanda forever (Motion picture)',
+      value: 'C389483 Black Panther, Wakanda forever (Motion picture)',
       type: 'Subject',
     },
     {
       rowIndex: 13,
       tag: '650',
       content: '$a C389483 Good and evil. $2 fast $0 sh2009125989C389483',
-      value: 'C387538 Good and evil',
+      value: 'C389483 Good and evil',
       type: 'Subject',
     },
     {
       rowIndex: 14,
       tag: '651',
       content: '$a C389483 Africa. $2 fast $0 sh85001531C389483',
-      value: 'C387538 Africa',
+      value: 'C389483 Africa',
       type: 'Subject',
     },
     {
       rowIndex: 15,
       tag: '655',
       content: '$a C389483 Comics (Graphic works) $2 fast $0 gf2014026266C389483',
-      value: 'C387538 Comics (Graphic works)',
+      value: 'C389483 Comics (Graphic works)',
       type: 'Subject',
     },
     {
@@ -116,14 +116,14 @@ describe('MARC -> MARC Bibliographic -> Edit MARC bib -> Automated linking', () 
       tag: '700',
       content:
         '$a C389483 Lee, Stan, $d 1922-2018, $e creator.$0 http://id.loc.gov/authorities/names/n83169267C389483',
-      value: 'C387538 Lee, Stan, 1922-2018',
+      value: 'C389483 Lee, Stan, 1922-2018',
       type: 'Contributor',
     },
     {
       rowIndex: 17,
       tag: '710',
       content: '$a C389483 Robinson $0 no2008081921C389483',
-      value: 'C387538 Robinson & Associates, Inc',
+      value: 'C389483 Robinson & Associates, Inc',
       type: 'Contributor',
     },
     {
@@ -131,52 +131,47 @@ describe('MARC -> MARC Bibliographic -> Edit MARC bib -> Automated linking', () 
       tag: '711',
       content: '$a C389483 Delaware $0 n84745425C389483',
       value:
-        'C387538 Delaware Symposium on Language Studies. Delaware symposia on language studies 1985',
+        'C389483 Delaware Symposium on Language Studies. 1985 Delaware symposia on language studies',
       type: 'Contributor',
     },
     {
       rowIndex: 19,
       tag: '730',
       content: '$a C389483 Gone T $0 n79066095C389483',
-      value: 'C387538 Neilson, Donald, 1936-2011',
+      value: 'C389483 Neilson, Donald, 1936-2011',
       type: 'Title data',
     },
     {
       rowIndex: 20,
       tag: '800',
       content: '$a C389483 Neilson, Donald $0 n79023811C389483',
-      value: 'C387538 Neilson, Donald, 1936-2011',
+      value: 'C389483 Neilson, Donald, 1936-2011',
       type: 'Title data',
     },
     {
       rowIndex: 21,
       tag: '810',
       content: '$a C389483 Black Panther Party $0 n80095585C389483',
-      value: 'C387538 Black Panther Party',
+      value: 'C389483 Black Panther Party',
       type: 'Title data',
     },
     {
       rowIndex: 22,
       tag: '811',
       content: '$a C389483 Stockholm International Film Festival $0 no2018125587C389483',
-      value: 'C387538 Stockholm International Film Festival',
+      value: 'C389483 Stockholm International Film Festival',
       type: 'Title data',
     },
     {
       rowIndex: 23,
       tag: '830',
       content: '$a C389483 Black Panther $0 no2018018754C389483',
-      value: 'C387538 Black Panther (Motion picture : 2018)',
+      value: 'C389483 Black Panther (Motion picture : 2018)',
       type: 'Title data',
     },
   ];
 
   let userData = {};
-
-  const linkableFields = [
-    100, 110, 111, 130, 240, 600, 610, 611, 630, 650, 651, 655, 700, 710, 711, 730, 800, 810, 811,
-    830,
-  ];
 
   const marcFiles = [
     {
@@ -249,10 +244,8 @@ describe('MARC -> MARC Bibliographic -> Edit MARC bib -> Automated linking', () 
       newFields.forEach((newField) => {
         MarcAuthority.addNewField(newField.rowIndex, newField.tag, newField.content);
       });
-
-      linkableFields.forEach((tag) => {
-        QuickMarcEditor.setRulesForField(tag, true);
-      });
+      // wait for fields to be filled in
+      cy.wait(2000);
       QuickMarcEditor.clickLinkHeadingsButton();
       QuickMarcEditor.checkCallout(
         'Field 100, 110, 111, 130, 240, 600, 610, 611, 630, 650, 651, 655, 700, 710, 711, 730, 800, 810, 811, and 830 has been linked to MARC authority record(s).',
