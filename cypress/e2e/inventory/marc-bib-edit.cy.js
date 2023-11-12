8aaef523945c8124bf3649dc7818dfda33b42c02
TatianaOne <tapusja2014@gmail.com>
2023-11-12 21:02:28 +0200

    added file

--- cypress/e2e/inventory/marc-bib-edit.cy.js
+++ cypress/e2e/inventory/marc-bib-edit.cy.js
@@ -30,7 +30,7 @@ describe('MARC -> MARC Bibliographic -> Edit MARC bib', () => {
     jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
   };
   const marcFileC359239 = {
-    marc: 'marcBibFileC360098.mrc',
+    marc: 'marcBibFileC359239.mrc',
     fileName: `testMarcFileC359239.${getRandomPostfix()}.mrc`,
     jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
   };
@@ -65,18 +65,18 @@ describe('MARC -> MARC Bibliographic -> Edit MARC bib', () => {
         //   Logs.getCreatedItemsID().then((link) => {
         //     createdInstanceIDs.push(link.split('/')[5]);
         //   });
-        cy.visit(TopMenu.dataImportPath);
-        DataImport.waitLoading();
-        DataImport.verifyUploadState();
-        DataImport.uploadFileAndRetry(marcFileC359239.marc, marcFileC359239.fileName);
-        JobProfiles.search(marcFileC359239.jobProfileToRun);
-        JobProfiles.runImportFile();
-        JobProfiles.waitFileIsImported(marcFileC359239.fileName);
-        Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
-        Logs.openFileDetails(marcFile.fileName);
-        Logs.getCreatedItemsID().then((link) => {
-          createdInstanceIDs.push(link.split('/')[5]);
-        });
+        //   cy.visit(TopMenu.dataImportPath);
+        //   DataImport.waitLoading();
+        //   DataImport.verifyUploadState();
+        //   DataImport.uploadFileAndRetry(marcFileC359239.marc, marcFileC359239.fileName);
+        //   JobProfiles.search(marcFileC359239.jobProfileToRun);
+        //   JobProfiles.runImportFile();
+        //   JobProfiles.waitFileIsImported(marcFileC359239.fileName);
+        //   Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
+        //   Logs.openFileDetails(marcFileC359239.fileName);
+        //   Logs.getCreatedItemsID().then((link) => {
+        //     createdInstanceIDs.push(link.split('/')[5]);
+        //   });
       });
     });
   });
@@ -199,14 +199,126 @@ describe('MARC -> MARC Bibliographic -> Edit MARC bib', () => {
     'C359239 Edit MARC Bib | Displaying of placeholder message when user deletes a row (spitfire) (TaaS)',
     { tags: [TestTypes.criticalPath, DevTeams.spitfire] },
     () => {
+      const fieldData = {
+        firstFieldForAdding: { tag: '', content: '' },
+        secondFieldForAdding: { tag: '151', content: '' },
+        thirdFieldForAdding: { tag: '', content: 'Filled' },
+        forthFieldForAdding: { tag: '400', content: 'value' },
+        fieldForDeleting: { tag: '222', content: '$a The Journal of ecclesiastical history' },
+
+        field902: { tag: '903', content: '$a pfnd $b Lintz', ind1: '\\', ind2: '\\' },
+        field905: { tag: '905', content: '$a 19890510120000.0 test', ind1: '\\', ind2: '\\' },
+        field948: {
+          tag: '948',
+          content: '$a 20141106 $b m $d batch $e lts $x addfast test1',
+          ind1: '2',
+          ind2: '\\',
+        },
+        emptyField: {
+          tag: '',
+          content: '$l OLIN $a BR140 $b .J86 $h 01/01/01 N',
+          ind1: '\\',
+          ind2: '\\',
+        },
+      };
+
       cy.login(testData.userProperties.username, testData.userProperties.password, {
         path: TopMenu.inventoryPath,
         waiter: InventoryInstances.waitContentLoading,
       });
-      InventoryInstances.waitContentLoading();
-      InventoryInstance.searchByTitle(createdInstanceIDs[2]);
+
+      // InventoryInstance.searchByTitle(createdInstanceIDs[2]);
+      InventoryInstance.searchByTitle('e2fb3f2d-300f-45bc-9eea-1790b3798deb');
       InventoryInstances.selectInstance();
       InventoryInstance.editMarcBibliographicRecord();
+      QuickMarcEditor.addNewField(
+        fieldData.firstFieldForAdding.tag,
+        fieldData.firstFieldForAdding.content,
+        13,
+      );
+      QuickMarcEditor.checkContent(fieldData.firstFieldForAdding.content, 14);
+      QuickMarcEditor.addNewField(
+        fieldData.secondFieldForAdding.tag,
+        fieldData.secondFieldForAdding.content,
+        14,
+      );
+      QuickMarcEditor.checkContent(fieldData.secondFieldForAdding.content, 15);
+      QuickMarcEditor.addNewField(
+        fieldData.thirdFieldForAdding.tag,
+        fieldData.thirdFieldForAdding.content,
+        15,
+      );
+      QuickMarcEditor.checkContent(fieldData.thirdFieldForAdding.content, 16);
+      QuickMarcEditor.addNewField(
+        fieldData.forthFieldForAdding.tag,
+        fieldData.forthFieldForAdding.content,
+        16,
+      );
+      QuickMarcEditor.checkContent(fieldData.forthFieldForAdding.content, 17);
+      QuickMarcEditor.deleteField(14);
+      cy.wait(1000);
+      QuickMarcEditor.deleteField(14);
+      cy.wait(1000);
+      QuickMarcEditor.deleteField(14);
+      cy.wait(1000);
+      QuickMarcEditor.deleteField(14);
+      cy.wait(1000);
+      QuickMarcEditor.checkButtonsDisabled();
+      QuickMarcEditor.deleteField(13);
+      QuickMarcEditor.afterDeleteNotification(fieldData.fieldForDeleting.tag);
+      QuickMarcEditor.undoDelete();
+      QuickMarcEditor.verifyTagValue(13, fieldData.fieldForDeleting.tag);
+
+      QuickMarcEditor.addValuesToExistingField(
+        27,
+        fieldData.field902.tag,
+        fieldData.field902.content,
+        fieldData.field902.ind1,
+        fieldData.field902.ind2,
+      );
+      QuickMarcEditor.addValuesToExistingField(
+        28,
+        fieldData.field905.tag,
+        fieldData.field905.content,
+        fieldData.field905.ind1,
+        fieldData.field905.ind2,
+      );
+      QuickMarcEditor.addValuesToExistingField(
+        29,
+        fieldData.field948.tag,
+        fieldData.field948.content,
+        fieldData.field948.ind1,
+        fieldData.field948.ind2,
+      );
+      QuickMarcEditor.addValuesToExistingField(
+        30,
+        fieldData.emptyField.tag,
+        fieldData.emptyField.content,
+        fieldData.emptyField.ind1,
+        fieldData.emptyField.ind2,
+      );
+
+      QuickMarcEditor.deleteField(28);
+      QuickMarcEditor.afterDeleteNotification(fieldData.field902.tag);
+      QuickMarcEditor.undoDelete();
+      QuickMarcEditor.verifyTagValue(28, fieldData.field902.tag);
+      QuickMarcEditor.deleteField(29);
+      QuickMarcEditor.afterDeleteNotification(fieldData.field905.tag);
+      QuickMarcEditor.undoDelete();
+      QuickMarcEditor.verifyTagValue(29, fieldData.field905.tag);
+      QuickMarcEditor.deleteField(30);
+      QuickMarcEditor.afterDeleteNotification(fieldData.field948.tag);
+      QuickMarcEditor.undoDelete();
+      QuickMarcEditor.verifyTagValue(30, fieldData.field948.tag);
+      QuickMarcEditor.deleteField(31);
+      QuickMarcEditor.afterDeleteNotification(fieldData.emptyField.tag);
+      QuickMarcEditor.undoDelete();
+      QuickMarcEditor.verifyTagValue(31, fieldData.emptyField.tag);
+      QuickMarcEditor.deleteField(4);
+      cy.wait(1000);
+      QuickMarcEditor.deleteField(31);
+      cy.wait(1000);
+      QuickMarcEditor.pressSaveAndClose();
     },
   );
 });
