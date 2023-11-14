3c1b14d65bb59fb6fd94798e6b3b56b8ffd83533
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2023-11-14 19:26:45 +0200

    FAT-9265-C358133 (#2290)
    * added test
    
    * added checks
    
    * added after block
    
    * added values to checks

--- cypress/e2e/data-import/log-details/import-summary-table-with-create-update-actions.cy.js
+++ cypress/e2e/data-import/log-details/import-summary-table-with-create-update-actions.cy.js
@@ -342,6 +342,7 @@ describe.skip('data-import', () => {
         // upload the marc file for creating
         cy.visit(TopMenu.dataImportPath);
         // TODO delete function after fix https://issues.folio.org/browse/MODDATAIMP-691
+        DataImport.verifyUploadState();
         DataImport.uploadFile(filePathForCreateInstance, fileNameForCreateInstance);
         JobProfiles.waitFileIsUploaded();
         JobProfiles.search(jobProfileForCreate.profileName);
