ffe15c741ebe2b850053425928844dc39e355760
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2023-11-14 11:29:35 +0200

    fixed merge conflicts

--- cypress/support/api/auth.js
+++ cypress/support/api/auth.js
@@ -1,15 +1,20 @@
 Cypress.Commands.add('getToken', (username, password) => {
+  let pathToSet = 'bl-users/login-with-expiry';
+  if (!Cypress.env('rtrAuth')) {
+    pathToSet = 'bl-users/login';
+  }
   cy.okapiRequest({
     method: 'POST',
-    path: 'bl-users/login',
+    path: pathToSet,
     body: { username, password },
     isDefaultSearchParamsRequired: false,
   }).then(({ body, headers }) => {
     const defaultServicePoint = body.servicePointsUser.servicePoints.find(
       ({ id }) => id === body.servicePointsUser.defaultServicePointId,
     );
-
-    Cypress.env('token', headers['x-okapi-token']);
+    if (!Cypress.env('rtrAuth')) {
+      Cypress.env('token', headers['x-okapi-token']);
+    }
     Cypress.env('defaultServicePoint', defaultServicePoint);
   });
 });
