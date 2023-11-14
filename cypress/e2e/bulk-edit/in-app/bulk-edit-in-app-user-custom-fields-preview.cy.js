335377df1393763478cacb44a499b6bf46f3fcd0
blue-raisin <bunicorn486@gmail.com>
2023-11-15 01:41:09 +0500

    C380731

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-user-custom-fields-preview.cy.js
@@ -0,0 +1,22 @@
+import testTypes from '../../../support/dictionary/testTypes';
+import devTeams from '../../../support/dictionary/devTeams';
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+
+    });
+
+    after('delete test data', () => {
+
+    });
+
+    it(
+      'C380731 Verify that User\'s Custom fields with special characters are displayed consistently through "Previews" and downloaded Bulk edit files (firebird) (TaaS)',
+      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      () => {
+
+      },
+    );
+  });
+});
