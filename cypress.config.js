64077c1fb0777595163a4c1b1da67904205d5199
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2023-11-17 12:51:00 +0200

    reset configs

--- cypress.config.js
+++ cypress.config.js
@@ -20,9 +20,9 @@ module.exports = defineConfig({
   defaultCommandTimeout: 101000,
   pageLoadTimeout: 120000,
   env: {
-    OKAPI_HOST: 'https://okapi-bugfest-poppy-consortium.int.aws.folio.org',
-    OKAPI_TENANT: 'cs00000int',
-    diku_login: 'ecs_admin',
+    OKAPI_HOST: 'https://folio-testing-cypress-okapi.ci.folio.org',
+    OKAPI_TENANT: 'diku',
+    diku_login: 'diku_admin',
     diku_password: 'admin',
     is_kiwi_release: false,
     downloadTimeout: 1000,
@@ -102,7 +102,7 @@ module.exports = defineConfig({
 
       return result;
     },
-    baseUrl: 'https://bugfest-poppy-consortium.int.aws.folio.org',
+    baseUrl: 'https://folio-testing-cypress-diku.ci.folio.org',
     testIsolation: false,
   },
 });
