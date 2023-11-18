5d0189bf633454daf92087fbdf4da8d3b83ee399
blue-raisin <bunicorn486@gmail.com>
2023-11-18 05:19:22 +0500

    C396401

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-icon.cy.js
@@ -0,0 +1,20 @@
+import devTeams from '../../../support/dictionary/devTeams';
+import testTypes from '../../../support/dictionary/testTypes';
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C396401 Verify reset state after clicking "Bulk edit" icon from the bulk edit In app form (firebird) (TaaS)',
+      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      () => {
+
+      }
+    );
+  });
+});
