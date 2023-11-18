070b14717f72e7a475d9f788e4f2aa5c4201ceb7
blue-raisin <bunicorn486@gmail.com>
2023-11-18 05:27:15 +0500

    C365590

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-email-partial.cy.js
@@ -0,0 +1,20 @@
+import devTeams from '../../../support/dictionary/devTeams';
+import testTypes from '../../../support/dictionary/testTypes';
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C365590 Verify that User\'s Email can be edited partially (firebird) (TaaS)',
+      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      () => {
+
+      }
+    );
+  });
+});
