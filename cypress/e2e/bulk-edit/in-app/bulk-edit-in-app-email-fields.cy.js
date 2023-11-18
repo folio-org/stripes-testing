52165cf0036c346baf853c212a09465d5777b3e6
blue-raisin <bunicorn486@gmail.com>
2023-11-18 19:01:48 +0500

    C360537

--- cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-email-fields.cy.js
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-email-fields.cy.js
@@ -1,19 +1,56 @@
-import devTeams from '../../../support/dictionary/devTeams';
+import TopMenu from '../../../support/fragments/topMenu';
 import testTypes from '../../../support/dictionary/testTypes';
+import permissions from '../../../support/dictionary/permissions';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import FileManager from '../../../support/utils/fileManager';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import devTeams from '../../../support/dictionary/devTeams';
+import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';
+import Users from '../../../support/fragments/users/users';
+
+let user;
+const userUUIDsFileName = `userUUIDs_${getRandomPostfix()}.csv`;
 
 describe('bulk-edit', () => {
   describe('in-app approach', () => {
     before('create test data', () => {
+      cy.createTempUser([permissions.bulkEditUpdateRecords.gui, permissions.uiUserEdit.gui], 'staff').then(
+        (userProperties) => {
+          user = userProperties;
+          cy.login(user.username, user.password, {
+            path: TopMenu.bulkEditPath,
+            waiter: BulkEditSearchPane.waitLoading,
+          });
+          FileManager.createFile(`cypress/fixtures/${userUUIDsFileName}`, user.userId);
+        },
+      );
     });
 
     after('delete test data', () => {
+      cy.getAdminToken();
+      FileManager.deleteFile(`cypress/fixtures/${userUUIDsFileName}`);
+      Users.deleteViaApi(user.userId);
     });
 
     it(
       'C360537 Verify that the "Confirm changes" button is disabled until all fields are filled in Email update (firebird) (TaaS)',
       { tags: [testTypes.extendedPath, devTeams.firebird] },
       () => {
+        BulkEditSearchPane.verifyDragNDropUsersUUIDsArea();
+        BulkEditSearchPane.uploadFile(userUUIDsFileName);
+        BulkEditSearchPane.waitFileUploading();
 
+        BulkEditActions.openActions();
+        BulkEditSearchPane.changeShowColumnCheckboxIfNotYet('Email');
+        BulkEditActions.openInAppStartBulkEditFrom();
+        const oldEmailDomain = 'folio.org';
+        const newEmailDomain = 'google.com';
+        BulkEditActions.replaceEmail(oldEmailDomain, newEmailDomain);
+        BulkEditActions.addNewBulkEditFilterString();
+        BulkEditActions.fillPatronGroup('faculty (Faculty Member)', 1);
+        BulkEditActions.confirmChanges();
+        BulkEditActions.commitChanges();
+        BulkEditSearchPane.verifyChangedResults(`test@${newEmailDomain}`);
       }
     );
   });
