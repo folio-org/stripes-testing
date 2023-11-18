707610ea10000fac3654412b519702ea8c5604cd
blue-raisin <bunicorn486@gmail.com>
2023-11-18 20:11:23 +0500

    C380588

--- cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-holdings-with-null.cy.js
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-holdings-with-null.cy.js
@@ -1,19 +1,88 @@
 import devTeams from '../../../support/dictionary/devTeams';
+import TopMenu from '../../../support/fragments/topMenu';
 import testTypes from '../../../support/dictionary/testTypes';
+import permissions from '../../../support/dictionary/permissions';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import FileManager from '../../../support/utils/fileManager';
+import Users from '../../../support/fragments/users/users';
+import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';;
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import HoldingsRecordView from '../../../support/fragments/inventory/holdingsRecordView';
+
+let user;
+const item = {
+  instanceName: `testBulkEdit_${getRandomPostfix()}`,
+  itemBarcode: getRandomPostfix(),
+};
+const holdingUUIDsFileName = `holdingUUIDs_${getRandomPostfix()}.csv`;
 
 describe('bulk-edit', () => {
   describe('in-app approach', () => {
     before('create test data', () => {
+      cy.createTempUser([
+        permissions.bulkEditView.gui,
+        permissions.bulkEditEdit.gui,
+        permissions.inventoryAll.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+
+        item.instanceId = InventoryInstances.createInstanceViaApi(item.instanceName, item.itemBarcode);
+        cy.getHoldings({
+          limit: 1,
+          query: `"instanceId"="${item.instanceId}"`,
+        }).then((holdings) => {
+          item.holdingUUID = holdings[0].id;
+          item.holdingHRID = holdings[0].hrid;
+          cy.updateHoldingRecord(holdings[0].id, {
+            ...holdings[0],
+            // Popular Reading Collection
+            formerIds: null,
+          });
+          FileManager.createFile(`cypress/fixtures/${holdingUUIDsFileName}`, item.holdingUUID);
+        });
+        cy.login(user.username, user.password, {
+          path: TopMenu.bulkEditPath,
+          waiter: BulkEditSearchPane.waitLoading,
+        });
+      });
     });
 
     after('delete test data', () => {
+      cy.getAdminToken();
+      InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.itemBarcode);
+      Users.deleteViaApi(user.userId);
+      FileManager.deleteFile(`cypress/fixtures/${holdingUUIDsFileName}`);
     });
 
     it(
       'C380588 Verify bulk edit of Holdings record that contains NULL values in reference data (firebird) (TaaS)',
       { tags: [testTypes.extendedPath, devTeams.firebird] },
       () => {
+        BulkEditSearchPane.checkHoldingsRadio();
+        BulkEditSearchPane.selectRecordIdentifier('Holdings UUIDs');
+        BulkEditSearchPane.uploadFile(holdingUUIDsFileName);
+        BulkEditSearchPane.waitFileUploading();
+        BulkEditSearchPane.verifyMatchedResults(item.holdingHRID);
 
+        const newLocation = 'Online';
+        BulkEditActions.openActions();
+        BulkEditActions.openInAppStartBulkEditFrom();
+        BulkEditActions.replacePermanentLocation(newLocation, 'holdings');
+        BulkEditActions.confirmChanges();
+        BulkEditActions.commitChanges();
+        BulkEditSearchPane.waitFileUploading();
+        BulkEditActions.verifySuccessBanner(1);
+        BulkEditSearchPane.verifyChangedResults(newLocation);
+        
+        cy.visit(TopMenu.inventoryPath);
+        InventorySearchAndFilter.switchToHoldings();
+        InventorySearchAndFilter.searchByParameter('Holdings HRID', item.holdingHRID);
+        InventorySearchAndFilter.selectSearchResultItem();
+        InventorySearchAndFilter.selectViewHoldings();
+        InventoryInstance.verifyHoldingsPermanentLocation(newLocation);
       }
     );
   });
