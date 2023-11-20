decbd6e0c18d95fd269cd737ac7c00b3ca4c99b5
artsiom.bandarenka <artsiom_bandarenka@epam.com>
2023-11-20 10:43:07 +0400

    refactor import

--- cypress/e2e/marc-authority/create-marc-bib-with-multiple-010-fields.cy.js
+++ cypress/e2e/marc-authority/create-marc-bib-with-multiple-010-fields.cy.js
@@ -1,6 +1,4 @@
-import TestTypes from '../../support/dictionary/testTypes';
-import DevTeams from '../../support/dictionary/devTeams';
-import Permissions from '../../support/dictionary/permissions';
+import { DevTeams, TestTypes, Permissions } from '../../support/dictionary';
 import TopMenu from '../../support/fragments/topMenu';
 import Users from '../../support/fragments/users/users';
 import InventoryInstance from '../../support/fragments/inventory/inventoryInstance';
