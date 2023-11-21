80b4f0910e485c9890d133f8d4da9d103d12e6bf
Mikita Zhuleha <mikita_zhuleha@epam.com>
2023-11-21 11:21:42 +0300

    C375939 performed

---
+++ cypress/e2e/title-level-request/recall-request-for-item-with-status-in-process.cy.js
@@ -0,0 +1,93 @@
+import TopMenu from '../../support/fragments/topMenu';
+import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
+import Users from '../../support/fragments/users/users';
+import { REQUEST_TYPES } from '../../support/constants';
+import Location from '../../support/fragments/settings/tenant/locations/newLocation';
+import TitleLevelRequests from '../../support/fragments/settings/circulation/titleLevelRequests';
+import InventorySearchAndFilter from '../../support/fragments/inventory/inventorySearchAndFilter';
+import NewRequest from '../../support/fragments/requests/newRequest';
+import Permissions from '../../support/dictionary/permissions';
+import SettingsMenu from '../../support/fragments/settingsMenu';
+import InventoryInstance from '../../support/fragments/inventory/inventoryInstance';
+import UserEdit from '../../support/fragments/users/userEdit';
+import InventoryItems from '../../support/fragments/inventory/item/inventoryItems';
+import RequestDetail from '../../support/fragments/requests/requestDetail';
+
+describe('Title level request for Item with status In process', () => {
+  const testData = {
+    folioInstances: InventoryInstances.generateFolioInstances(),
+    servicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
+  };
+  let userData;
+  let defaultLocation;
+
+  before('Create test data', () => {
+    cy.getAdminToken();
+    ServicePoints.createViaApi(testData.servicePoint);
+    defaultLocation = Location.getDefaultLocation(testData.servicePoint.id);
+    Location.createViaApi(defaultLocation).then((location) => {
+      InventoryInstances.createFolioInstancesViaApi({
+        folioInstances: testData.folioInstances,
+        location,
+      });
+    });
+    cy.createTempUser([
+      Permissions.requestsAll.gui,
+      Permissions.uiRequestsEdit.gui,
+      Permissions.uiRequestsCreate.gui,
+      Permissions.inventoryAll.gui,
+      Permissions.checkinAll.gui,
+    ]).then((userProperties) => {
+      userData = userProperties;
+      UserEdit.addServicePointViaApi(
+        testData.servicePoint.id,
+        userData.userId,
+        testData.servicePoint.id,
+      );
+      cy.loginAsAdmin({
+        path: SettingsMenu.circulationTitleLevelRequestsPath,
+        waiter: TitleLevelRequests.waitLoading,
+      });
+      TitleLevelRequests.changeTitleLevelRequestsStatus('allow');
+      InventoryItems.updateItemStatusViaAPI(
+        testData.folioInstances[0].itemIds[0],
+        'mark-in-process',
+      );
+      cy.login(userData.username, userData.password, {
+        path: TopMenu.inventoryPath,
+        waiter: InventorySearchAndFilter.waitLoading,
+      });
+    });
+  });
+
+  after('Delete test data', () => {
+    UserEdit.changeServicePointPreferenceViaApi(userData.userId, [testData.servicePoint.id]);
+    ServicePoints.deleteViaApi(testData.servicePoint.id);
+    InventoryInstances.deleteInstanceViaApi({
+      instance: testData.folioInstances[0],
+      servicePoint: testData.servicePoint,
+      shouldCheckIn: true,
+    });
+    Location.deleteViaApiIncludingInstitutionCampusLibrary(
+      defaultLocation.institutionId,
+      defaultLocation.campusId,
+      defaultLocation.libraryId,
+      defaultLocation.id,
+    );
+    Users.deleteViaApi(userData.userId);
+  });
+
+  it('Check that user can create a TLR Recall for Item with status In process', () => {
+    InventorySearchAndFilter.searchInstanceByTitle(testData.folioInstances[0].instanceTitle);
+    InventoryInstance.checkNewRequestAtNewPane();
+    NewRequest.verifyTitleLevelRequestsCheckbox('checked');
+    NewRequest.enterRequesterBarcode(userData.barcode);
+    NewRequest.chooseRequestType(REQUEST_TYPES.RECALL);
+    NewRequest.choosepickupServicePoint(testData.servicePoint.name);
+    NewRequest.saveRequestAndClose();
+    NewRequest.verifyRequestSuccessfullyCreated(userData.username);
+    RequestDetail.checkItemStatus('Recall');
+    RequestDetail.checkRequestsOnItem('1');
+  });
+});
