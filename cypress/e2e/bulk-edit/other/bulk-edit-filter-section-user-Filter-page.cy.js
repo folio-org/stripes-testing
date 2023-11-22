0cae6cf32798a1cdb1cc129d4d13290e7ff0115d
Aman Juneja <aman_juneja1@epam.com>
2023-11-22 15:25:15 +0530

    Update bulk-edit-filter-section-user-Filter-page.cy.js

--- cypress/e2e/bulk-edit/other/bulk-edit-filter-section-user-Filter-page.cy.js
+++ cypress/e2e/bulk-edit/other/bulk-edit-filter-section-user-Filter-page.cy.js
@@ -2,6 +2,7 @@ import permissions from '../../../support/dictionary/permissions';
 import TopMenu from '../../../support/fragments/topMenu';
 import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
 import users from '../../../support/fragments/users/users';
+import { Permissions } from '../../../support/dictionary';
 
 let user;
 describe('bulk-edit', () => {
@@ -23,11 +24,13 @@ describe('bulk-edit', () => {
     cy.getAdminToken();
     users.deleteViaApi(user.userId);
   });
+
   it('Filters section: Users filter', () => {
     BulkEditSearchPane.openLogsSearch();
     BulkEditSearchPane.verifyLogsPane();
     BulkEditSearchPane.checkHoldingsCheckbox();
     BulkEditSearchPane.checkItemsCheckbox();
     BulkEditSearchPane.checkUsersCheckbox();
+    BulkEditSearchPane.verifyLogsTableHeaders();
   });
 });
