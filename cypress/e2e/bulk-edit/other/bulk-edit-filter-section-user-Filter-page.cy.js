55c0e43726b678d24e381d4322c043dc1c797d0c
Aman Juneja <aman_juneja1@epam.com>
2023-11-22 13:50:50 +0530

    Create bulk-edit-filter-section-user-Filter-page.cy.js

---
+++ cypress/e2e/bulk-edit/other/bulk-edit-filter-section-user-Filter-page.cy.js
@@ -0,0 +1,33 @@
+import permissions from '../../../support/dictionary/permissions';
+import TopMenu from '../../../support/fragments/topMenu';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import users from '../../../support/fragments/users/users';
+
+let user;
+describe('bulk-edit', () => {
+  before('create test users', () => {
+    cy.createTempUser([
+      permissions.bulkEditLogsView.gui,
+      permissions.bulkEditCsvView.gui,
+      permissions.bulkEditView.gui,
+    ]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password, {
+        path: TopMenu.bulkEditPath,
+        waiter: BulkEditSearchPane.waitLoading,
+      });
+    });
+  });
+
+  after('delete test data', () => {
+    cy.getAdminToken();
+    users.deleteViaApi(user.userId);
+  });
+  it('Filters section: Users filter', () => {
+    BulkEditSearchPane.openLogsSearch();
+    BulkEditSearchPane.verifyLogsPane();
+    BulkEditSearchPane.checkHoldingsCheckbox();
+    BulkEditSearchPane.checkItemsCheckbox();
+    BulkEditSearchPane.checkUsersCheckbox();
+  });
+});
