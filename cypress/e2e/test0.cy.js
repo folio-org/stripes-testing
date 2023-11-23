9a240c696e6e439a420749c947fe9e8b851def0f
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2023-11-23 10:19:00 +0200

    fixed grep plugin

--- cypress/e2e/test0.cy.js
+++
@@ -1,6 +0,0 @@
-describe('describe0', () => {
-  it('C360534', { tags: ['testtg', 'parallel', 'nonParallel'] }, () => {
-    cy.task('log', '\t' + new Date().toLocaleTimeString());
-    expect(true).to.equal(true);
-  });
-});
