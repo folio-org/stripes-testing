aad223868cca99990d386e696b22faef559d9e4f
blue-raisin <bunicorn486@gmail.com>
2023-11-24 14:34:17 +0500

    C389569

---
+++ cypress/e2e/bulk-edit/csv/bulk-edit-csv-user-custom-fields-recent.cy.js
@@ -0,0 +1,21 @@
+import testTypes from '../../../support/dictionary/testTypes';
+import devTeams from '../../../support/dictionary/devTeams';
+
+describe('bulk-edit', () => {
+  describe('csv approach', () => {
+    before('create test data', () => {
+
+    });
+
+    after('delete test data', () => {
+      
+    });
+
+    it(
+      'C389569 Local | Verify bulk edit Users records with recently updated Custom fields (firebird) (TaaS)',
+      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      () => {
+      }
+    );
+  });
+});
