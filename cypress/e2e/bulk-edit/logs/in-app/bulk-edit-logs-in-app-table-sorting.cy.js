bf179707517599a8eb31e87e5573da96aa120f9c
blue-raisin <bunicorn486@gmail.com>
2023-11-24 14:26:40 +0500

    C380770

---
+++ cypress/e2e/bulk-edit/logs/in-app/bulk-edit-logs-in-app-table-sorting.cy.js
@@ -0,0 +1,19 @@
+import testTypes from '../../../../support/dictionary/testTypes';
+import devTeams from '../../../../support/dictionary/devTeams';
+
+describe('Bulk Edit - Logs', () => {
+  before('create test data', () => {
+
+  });
+
+  after('delete test data', () => {
+  });
+
+  it(
+    'C380770 Verify sorting in "Bulk edit logs" table (firebird) (TaaS)',
+    { tags: [testTypes.extendedPath, devTeams.firebird] },
+    () => {
+      
+    },
+  );
+});
