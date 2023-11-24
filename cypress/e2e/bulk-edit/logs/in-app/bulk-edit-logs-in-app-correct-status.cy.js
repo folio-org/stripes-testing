0c6d2b2eb1a54da58fe2fb43e4d76f24384f57ce
blue-raisin <bunicorn486@gmail.com>
2023-11-24 14:57:24 +0500

    C380443

---
+++ cypress/e2e/bulk-edit/logs/in-app/bulk-edit-logs-in-app-correct-status.cy.js
@@ -0,0 +1,19 @@
+import testTypes from '../../../../support/dictionary/testTypes';
+import devTeams from '../../../../support/dictionary/devTeams';
+
+describe('Bulk Edit - Logs', () => {
+  before('create test data', () => {
+
+  });
+
+  after('delete test data', () => {
+  });
+
+  it(
+    'C380443 Verify the correctness of the Bulk Edit job status in Logs tab (firebird) (TaaS)',
+    { tags: [testTypes.extendedPath, devTeams.firebird] },
+    () => {
+      
+    },
+  );
+});
