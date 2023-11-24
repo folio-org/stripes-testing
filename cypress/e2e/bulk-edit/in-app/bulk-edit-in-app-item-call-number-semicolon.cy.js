63c6b1147e15ed1265962435d01af795e75774ba
blue-raisin <bunicorn486@gmail.com>
2023-11-24 15:19:20 +0500

    C365124

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-item-call-number-semicolon.cy.js
@@ -0,0 +1,21 @@
+import testTypes from '../../../support/dictionary/testTypes';
+import devTeams from '../../../support/dictionary/devTeams';
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+
+    });
+
+    after('delete test data', () => {
+
+    });
+
+    it(
+      'C365124 Verify that preview of records matched is populated if Items call number contains semicolon (firebird) (TaaS)',
+      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      () => {
+      }
+    );
+  });
+});
