31489d736f4ba371d576defaf828df0749c525e9
blue-raisin <bunicorn486@gmail.com>
2023-11-25 20:30:02 +0500

    C365124

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-item-select-option.cy.js
@@ -0,0 +1,21 @@
+import testTypes from '../../../support/dictionary/testTypes';
+import devTeams from '../../../support/dictionary/devTeams';
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+
+    });
+
+    after('delete test data', () => {
+
+    });
+
+    it(
+      'C365616 Verify that In app bulk edit form for items records contains "Select option" placeholder for Options dropdown (firebird) (TaaS)',
+      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      () => {
+      }
+    );
+  });
+});
