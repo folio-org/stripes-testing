bbd5be5113b7528d18bb63de75096853c83b8361
IhorBohdan <Ihor_Bohdan@epam.com>
2023-11-28 10:34:31 +0200

    C353213: Ledger export settings: last year Fund with NO budget, NO Classes, Export settings-Inactive status (thunderjet) (TaaS)

--- cypress/e2e/finance/ledgers/ledger-export-settings-pairwise-variants/export-settings-inactive-status.cy.js
+++ cypress/e2e/finance/ledgers/ledger-export-settings-pairwise-variants/export-settings-inactive-status.cy.js
@@ -1,6 +1,4 @@
 import permissions from '../../../../support/dictionary/permissions';
-import testType from '../../../../support/dictionary/testTypes';
-import devTeams from '../../../../support/dictionary/devTeams';
 import getRandomPostfix from '../../../../support/utils/stringTools';
 import FiscalYears from '../../../../support/fragments/finance/fiscalYears/fiscalYears';
 import TopMenu from '../../../../support/fragments/topMenu';
