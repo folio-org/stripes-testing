54af3119996d87bcf9a21be8fbf9eb309f4aac07
Mikita Zhuleha <mikita_zhuleha@epam.com>
2023-11-28 11:08:21 +0300

    C353992 implemented

--- cypress/e2e/inventory/instance/cancelling-creation-order-from-instance-record.cy.js
+++ cypress/e2e/inventory/instance/cancelling-creation-order-from-instance-record.cy.js
@@ -6,57 +6,56 @@ import InventorySearchAndFilter from '../../../support/fragments/inventory/inven
 import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
 import getRandomPostfix from '../../../support/utils/stringTools';
 
-describe('Orders', () => {
-  describe('Inventory interactions', () => {
-    const instance = {
-      instanceName: `C353992_${getRandomPostfix()}`,
-      itemBarcode: getRandomPostfix(),
-    };
-    let userData;
+describe('Inventory instance', () => {
+  const instance = {
+    instanceName: `C353992_${getRandomPostfix()}`,
+    itemBarcode: getRandomPostfix(),
+  };
+  let userData;
 
-    before('Create test data', () => {
-      cy.createTempUser([
-        Permissions.uiInventoryCreateOrderFromInstance.gui,
-        Permissions.uiInventoryViewCreateEditHoldings.gui,
-        Permissions.uiInventoryViewCreateEditInstances.gui,
-        Permissions.uiInventoryViewCreateEditItems.gui,
-        Permissions.uiOrdersView.gui,
-      ]).then((userProperties) => {
-        userData = userProperties;
-        InventoryInstances.createInstanceViaApi(instance.instanceName, instance.itemBarcode);
-        cy.login(userData.username, userData.password, {
-          path: TopMenu.inventoryPath,
-          waiter: InventorySearchAndFilter.waitLoading,
-        });
+  before('Create test data', () => {
+    cy.getAdminToken();
+    cy.createTempUser([
+      Permissions.uiInventoryCreateOrderFromInstance.gui,
+      Permissions.uiInventoryViewCreateEditHoldings.gui,
+      Permissions.uiInventoryViewCreateEditInstances.gui,
+      Permissions.uiInventoryViewCreateEditItems.gui,
+      Permissions.uiOrdersView.gui,
+    ]).then((userProperties) => {
+      userData = userProperties;
+      InventoryInstances.createInstanceViaApi(instance.instanceName, instance.itemBarcode);
+      cy.login(userData.username, userData.password, {
+        path: TopMenu.inventoryPath,
+        waiter: InventorySearchAndFilter.waitLoading,
       });
     });
-
-    after('Delete test data', () => {
-      cy.getAdminToken();
-      Users.deleteViaApi(userData.userId);
-      InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(instance.itemBarcode);
-    });
-
-    it(
-      'C353992 Cancelling creation an order from instance record (thunderjet) (TaaS)',
-      { tags: ['extendedPath', 'thunderjet'] },
-      () => {
-        // Navigate to the instance from preconditions
-        InventorySearchAndFilter.searchInstanceByTitle(instance.instanceName);
-        InventoryInstances.selectInstance();
-
-        // Click Actions->New order
-        const NewOrderModal = InventoryInstance.newOrder();
-        NewOrderModal.waitLoading();
-
-        // Click "Cancel" button
-        NewOrderModal.clickCancel();
-
-        // Check that the Create order modal is closed and the Instance pane is displayed
-        NewOrderModal.isNotDisplayed();
-        InventoryInstance.waitLoading();
-        InventoryInstance.verifyOrdersCount(0);
-      },
-    );
   });
+
+  after('Delete test data', () => {
+    cy.getAdminToken();
+    Users.deleteViaApi(userData.userId);
+    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(instance.itemBarcode);
+  });
+
+  it(
+    'C353992 Cancelling creation an order from instance record (thunderjet) (TaaS)',
+    { tags: ['extendedPath', 'thunderjet'] },
+    () => {
+      // Navigate to the instance from preconditions
+      InventorySearchAndFilter.searchInstanceByTitle(instance.instanceName);
+      InventoryInstances.selectInstance();
+
+      // Click Actions->New order
+      const NewOrderModal = InventoryInstance.newOrder();
+      NewOrderModal.waitLoading();
+
+      // Click "Cancel" button
+      NewOrderModal.clickCancel();
+
+      // Check that the Create order modal is closed and the Instance pane is displayed
+      NewOrderModal.isNotDisplayed();
+      InventoryInstance.waitLoading();
+      InventoryInstance.verifyOrdersCount(0);
+    },
+  );
 });
