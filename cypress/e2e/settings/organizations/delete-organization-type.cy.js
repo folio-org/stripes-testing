fb7de4a7e2463ab3a1d89885b442c0b31bc414c9
IhorBohdan <Ihor_Bohdan@epam.com>
2023-11-29 10:16:44 +0200

    Implementing of FAT-9306

---
+++ cypress/e2e/settings/organizations/delete-organization-type.cy.js
@@ -0,0 +1,35 @@
+import permissions from '../../../support/dictionary/permissions';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import Users from '../../../support/fragments/users/users';
+import SettingsOrganizations from '../../../support/fragments/settings/organizations/settingsOrganizations';
+
+describe('Organizations: Settings (Organizations)', () => {
+  let user;
+  before(() => {
+    cy.createTempUser([permissions.uiSettingsOrganizationsCanViewOnlySettings.gui]).then(
+      (userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password, {
+          path: SettingsMenu.organizationsPath,
+          waiter: SettingsOrganizations.waitLoadingOrganizationSettings,
+        });
+      },
+    );
+  });
+
+  after(() => {
+    cy.getAdminToken();
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C367990: Delete organization type (thunderjet)',
+    { tags: ['extendedPath', 'thunderjet'] },
+    () => {
+      SettingsOrganizations.selectCategories();
+      SettingsOrganizations.checkButtonNewInCategoriesIsDisabled();
+      SettingsOrganizations.selectTypes();
+      SettingsOrganizations.checkButtonNewInTypesIsDisabled();
+    },
+  );
+});
