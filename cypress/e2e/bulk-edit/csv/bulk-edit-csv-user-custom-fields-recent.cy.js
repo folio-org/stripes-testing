b925a174bd23dd6a7a4dc108d0361c03efffc100
blue-raisin <bunicorn486@gmail.com>
2023-11-30 12:21:58 +0500

    C389569

--- cypress/e2e/bulk-edit/csv/bulk-edit-csv-user-custom-fields-recent.cy.js
+++ cypress/e2e/bulk-edit/csv/bulk-edit-csv-user-custom-fields-recent.cy.js
@@ -1,10 +1,8 @@
 import TopMenu from '../../../support/fragments/topMenu';
-import testTypes from '../../../support/dictionary/testTypes';
 import permissions from '../../../support/dictionary/permissions';
 import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
 import FileManager from '../../../support/utils/fileManager';
 import getRandomPostfix from '../../../support/utils/stringTools';
-import devTeams from '../../../support/dictionary/devTeams';
 import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';
 import Users from '../../../support/fragments/users/users';
 import UsersSearchPane from '../../../support/fragments/users/usersSearchPane';
@@ -61,7 +59,7 @@ describe('bulk-edit', () => {
 
     it(
       'C389569 Local | Verify bulk edit Users records with recently updated Custom fields (firebird) (TaaS)',
-      { tags: [testTypes.extendedPath, devTeams.firebird] },
+      { tags: ['extendedPath', 'firebird'] },
       () => {
         cy.visit(TopMenu.bulkEditPath);
         BulkEditSearchPane.checkUsersRadio();
