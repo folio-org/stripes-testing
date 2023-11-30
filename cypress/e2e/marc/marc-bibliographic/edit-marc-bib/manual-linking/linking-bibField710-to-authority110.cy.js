2fc1dc15d47f129dec905dbfdac5a68a0a33c0f0
Mikhail Subach <mikhail_subach@epam.com>
2023-11-30 13:29:44 +0300

    tags updated to text

--- cypress/e2e/marc/marc-bibliographic/edit-marc-bib/manual-linking/linking-bibField710-to-authority110.cy.js
+++ cypress/e2e/marc/marc-bibliographic/edit-marc-bib/manual-linking/linking-bibField710-to-authority110.cy.js
@@ -106,7 +106,7 @@ describe('Manual Linking Bib field to Authority 1XX', () => {
 
   it(
     'C375081 Link the "710" of "MARC Bib" field with "110" field of "MARC Authority" record. (spitfire) (TaaS)',
-    { tags: [TestTypes.extendedPath, DevTeams.spitfire] },
+    { tags: ['extendedPath', 'spitfire'] },
     () => {
       InventoryInstance.searchByTitle(createdRecordIDs[0]);
       InventoryInstances.selectInstance();
