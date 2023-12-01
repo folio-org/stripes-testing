f52339f46a50982e85dac487032613334f6d04b6
blue-raisin <bunicorn486@gmail.com>
2023-12-01 18:12:17 +0500

    C357068

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-item-status-negative.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C357068 Negative: Verify Items status update (firebird) (TaaS)',
+      { tags: ['extendedPath', 'firebird'] },
+      () => {
+      },
+    );
+  });
+});
