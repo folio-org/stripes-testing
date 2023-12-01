e29b7fb235d3a7d6ba49f51b0983bc70c0f1af57
blue-raisin <bunicorn486@gmail.com>
2023-12-01 18:02:49 +0500

    C365615

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-user-options-placeholder.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('bulk-edit', () => {
+  describe('in app approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C365615 Verify that Options dropdown contains placeholder--Users in app (firebird) (TaaS)',
+      { tags: ['extendedPath', 'firebird'] },
+      () => {
+      },
+    );
+  });
+});
