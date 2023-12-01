fd217069fcd9d6c81dd5a58090d3104d70f0bb65
blue-raisin <bunicorn486@gmail.com>
2023-12-01 17:42:09 +0500

    C380585

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-user-birth-date.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('bulk-edit', () => {
+  describe('in app approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C380585 Verify that "Birth date" field is formatted to show date only(firebird) (TaaS)',
+      { tags: ['extendedPath', 'firebird'] },
+      () => {
+      },
+    );
+  });
+});
