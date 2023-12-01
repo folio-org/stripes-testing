b2f95e96e02b5c110f38022ecd301a24970785c4
blue-raisin <bunicorn486@gmail.com>
2023-12-01 17:53:32 +0500

    C357066

---
+++ cypress/e2e/bulk-edit/csv/bulk-edit-csv-users-preview.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('bulk-edit', () => {
+  describe('csv approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C357066 Verify populating preview records changed (Local approach) (firebird) (TaaS)',
+      { tags: ['extendedPath', 'firebird'] },
+      () => {
+      },
+    );
+  });
+});
