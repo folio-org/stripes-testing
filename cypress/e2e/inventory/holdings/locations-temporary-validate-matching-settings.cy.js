ba5a57c42b98a8d7942de5a47caf8817f0d9168c
KazachenkaTatsiana <139146179+KazachenkaTatsiana@users.noreply.github.com>
2023-12-05 11:43:23 +0300

    Update locations-temporary-validate-matching-settings.cy.js
    fix

--- cypress/e2e/inventory/holdings/locations-temporary-validate-matching-settings.cy.js
+++ cypress/e2e/inventory/holdings/locations-temporary-validate-matching-settings.cy.js
@@ -12,128 +12,128 @@ import { Locations } from '../../../support/fragments/settings/tenant';
 import settingsMenu from '../../../support/fragments/settingsMenu';
 
 describe('inventory', () => {
-describe('item', () => {
-  const itemData = {
-    barcode: generateItemBarcode(),
-    instanceTitle: `Instance ${getRandomPostfix()}`,
-    instances: [
-      {
-        title: `instance ${getRandomPostfix()}`,
-      },
-      {
-        title: `instance ${getRandomPostfix()}`,
-      },
-      {
-        title: `instance ${getRandomPostfix()}`,
-      },
-    ],
-  };
-  let location;
-  const service = ServicePoints.getDefaultServicePointWithPickUpLocation();
-  const checkInResultsData = [ITEM_STATUS_NAMES.AVAILABLE, itemData.barcode];
+  describe('item', () => {
+    const itemData = {
+      barcode: generateItemBarcode(),
+      instanceTitle: `Instance ${getRandomPostfix()}`,
+      instances: [
+        {
+          title: `instance ${getRandomPostfix()}`,
+        },
+        {
+          title: `instance ${getRandomPostfix()}`,
+        },
+        {
+          title: `instance ${getRandomPostfix()}`,
+        },
+      ],
+    };
+    let location;
+    const service = ServicePoints.getDefaultServicePointWithPickUpLocation();
+    const checkInResultsData = [ITEM_STATUS_NAMES.AVAILABLE, itemData.barcode];
 
-  before('Create test data', () => {
-    cy.getAdminToken()
-      .then(() => {
-        cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
-          itemData.instanceTypeId = instanceTypes[0].id;
-        });
-        cy.getHoldingTypes({ limit: 1 }).then((res) => {
-          itemData.holdingTypeId = res[0].id;
-        });
-        ServicePoints.createViaApi(service);
-        location = Location.getDefaultLocation(service.id);
-        Location.createViaApi(location);
-        cy.getLoanTypes({ limit: 1 }).then((res) => {
-          itemData.loanTypeId = res[0].id;
-        });
-        cy.getMaterialTypes({ limit: 1 }).then((res) => {
-          itemData.materialTypeId = res.id;
-          itemData.materialTypeName = res.name;
-          checkInResultsData.push(`${itemData.instanceTitle} (${itemData.materialTypeName})`);
-        });
-        [...Array(3)].forEach((_, index) => {
-          const servicePoint = ServicePoints.getDefaultServicePointWithPickUpLocation();
-          const defaultLocation = Location.getDefaultLocation(servicePoint.id);
-          Location.createViaApi(defaultLocation);
-          itemData.instances[index].defaultLocation = defaultLocation;
-        });
-      })
-      .then(() => {
-        InventoryInstances.createFolioInstanceViaApi({
-          instance: {
-            instanceTypeId: itemData.instanceTypeId,
-            title: itemData.instanceTitle,
-          },
-          holdings: [
-            {
-              holdingsTypeId: itemData.holdingTypeId,
-              permanentLocationId: location.id,
+    before('Create test data', () => {
+      cy.getAdminToken()
+        .then(() => {
+          cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
+            itemData.instanceTypeId = instanceTypes[0].id;
+          });
+          cy.getHoldingTypes({ limit: 1 }).then((res) => {
+            itemData.holdingTypeId = res[0].id;
+          });
+          ServicePoints.createViaApi(service);
+          location = Location.getDefaultLocation(service.id);
+          Location.createViaApi(location);
+          cy.getLoanTypes({ limit: 1 }).then((res) => {
+            itemData.loanTypeId = res[0].id;
+          });
+          cy.getMaterialTypes({ limit: 1 }).then((res) => {
+            itemData.materialTypeId = res.id;
+            itemData.materialTypeName = res.name;
+            checkInResultsData.push(`${itemData.instanceTitle} (${itemData.materialTypeName})`);
+          });
+          [...Array(3)].forEach((_, index) => {
+            const servicePoint = ServicePoints.getDefaultServicePointWithPickUpLocation();
+            const defaultLocation = Location.getDefaultLocation(servicePoint.id);
+            Location.createViaApi(defaultLocation);
+            itemData.instances[index].defaultLocation = defaultLocation;
+          });
+        })
+        .then(() => {
+          InventoryInstances.createFolioInstanceViaApi({
+            instance: {
+              instanceTypeId: itemData.instanceTypeId,
+              title: itemData.instanceTitle,
             },
-          ],
-          items: [
-            {
-              barcode: itemData.barcode,
-              status: { name: ITEM_STATUS_NAMES.AVAILABLE },
-              permanentLoanType: { id: itemData.loanTypeId },
-              materialType: { id: itemData.materialTypeId },
-            },
-          ],
+            holdings: [
+              {
+                holdingsTypeId: itemData.holdingTypeId,
+                permanentLocationId: location.id,
+              },
+            ],
+            items: [
+              {
+                barcode: itemData.barcode,
+                status: { name: ITEM_STATUS_NAMES.AVAILABLE },
+                permanentLoanType: { id: itemData.loanTypeId },
+                materialType: { id: itemData.materialTypeId },
+              },
+            ],
+          });
+        })
+        .then((specialInstanceIds) => {
+          itemData.testInstanceIds = specialInstanceIds;
         });
-      })
-      .then((specialInstanceIds) => {
-        itemData.testInstanceIds = specialInstanceIds;
+
+      cy.loginAsAdmin({
+        path: TopMenu.inventoryPath,
+        waiter: InventoryInstances.waitContentLoading,
       });
-
-    cy.loginAsAdmin({
-      path: TopMenu.inventoryPath,
-      waiter: InventoryInstances.waitContentLoading,
     });
-  });
 
-  after('Delete test data', () => {
-    cy.getAdminToken();
-    ServicePoints.deleteViaApi(service.id);
-    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(itemData.barcode);
-    Location.deleteViaApi(location.id);
-    [...Array(3)].forEach((_, index) => {
-      Location.deleteViaApiIncludingInstitutionCampusLibrary(
-        itemData.instances[index].defaultLocation.institutionId,
-        itemData.instances[index].defaultLocation.campusId,
-        itemData.instances[index].defaultLocation.libraryId,
-        itemData.instances[index].defaultLocation.id,
-      );
-    });
-  });
-
-  it(
-    'C622 - Locations --> Temporary Location --> (Validate matching settings) (Folijet)(TaaS)',
-    {
-      tags: ['extendedPath', 'folijet'],
-    },
-    () => {
-      InventorySearchAndFilter.searchInstanceByTitle(itemData.instanceTitle);
-      InventorySearchAndFilter.selectViewHoldings();
-      HoldingsRecordView.waitLoading();
-
-      HoldingsRecordView.edit();
-      HoldingsRecordEdit.waitLoading();
-      HoldingsRecordEdit.openTemporaryLocation();
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      ServicePoints.deleteViaApi(service.id);
+      InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(itemData.barcode);
+      Location.deleteViaApi(location.id);
       [...Array(3)].forEach((_, index) => {
-        HoldingsRecordEdit.verifyTemporaryLocationItemExists(
-          itemData.instances[index].defaultLocation.name,
+        Location.deleteViaApiIncludingInstitutionCampusLibrary(
+          itemData.instances[index].defaultLocation.institutionId,
+          itemData.instances[index].defaultLocation.campusId,
+          itemData.instances[index].defaultLocation.libraryId,
+          itemData.instances[index].defaultLocation.id,
         );
       });
+    });
 
-      cy.visit(settingsMenu.tenantLocationsPath);
-      Locations.waitLoading();
-      [...Array(3)].forEach((_, index) => {
-        Locations.selectInstitution(itemData.instances[index].defaultLocation.institutionName);
-        Locations.selectCampus(itemData.instances[index].defaultLocation.campusName);
-        Locations.selectLibrary(itemData.instances[index].defaultLocation.libraryName);
-        Locations.checkResultsTableContent([itemData.instances[index].defaultLocation]);
-      });
-    },
-  );
-});
+    it(
+      'C622 - Locations --> Temporary Location --> (Validate matching settings) (Folijet)(TaaS)',
+      {
+        tags: ['extendedPath', 'folijet'],
+      },
+      () => {
+        InventorySearchAndFilter.searchInstanceByTitle(itemData.instanceTitle);
+        InventorySearchAndFilter.selectViewHoldings();
+        HoldingsRecordView.waitLoading();
+
+        HoldingsRecordView.edit();
+        HoldingsRecordEdit.waitLoading();
+        HoldingsRecordEdit.openTemporaryLocation();
+        [...Array(3)].forEach((_, index) => {
+          HoldingsRecordEdit.verifyTemporaryLocationItemExists(
+            itemData.instances[index].defaultLocation.name,
+          );
+        });
+
+        cy.visit(settingsMenu.tenantLocationsPath);
+        Locations.waitLoading();
+        [...Array(3)].forEach((_, index) => {
+          Locations.selectInstitution(itemData.instances[index].defaultLocation.institutionName);
+          Locations.selectCampus(itemData.instances[index].defaultLocation.campusName);
+          Locations.selectLibrary(itemData.instances[index].defaultLocation.libraryName);
+          Locations.checkResultsTableContent([itemData.instances[index].defaultLocation]);
+        });
+      },
+    );
+  });
 });
