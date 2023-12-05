cee0eeed7605533b6d65f6c64b6ef67764ca04f5
arsenhovh <arsen_hovhannisyan@epam.com>
2023-12-05 13:29:35 +0400

    delete after create

--- cypress/e2e/marc/marc-bibliographic/create-new-marc-bib/search-created-marc-bib-record-by-title-OCLC-number.cy.js
+++ cypress/e2e/marc/marc-bibliographic/create-new-marc-bib/search-created-marc-bib-record-by-title-OCLC-number.cy.js
@@ -4,10 +4,21 @@ import InventoryInstances from '../../../../support/fragments/inventory/inventor
 import QuickMarcEditor from '../../../../support/fragments/quickMarcEditor';
 import TopMenu from '../../../../support/fragments/topMenu';
 import Users from '../../../../support/fragments/users/users';
+import InventorySearchAndFilter from '../../../../support/fragments/inventory/inventorySearchAndFilter';
+
+let userId;
+const testData = {
+  firstSearchOption: 'Keyword (title, contributor, identifier, HRID, UUID)',
+  secondSearchOption: 'OCLC number, normalized',
+  textFor245Filed: '$a Sophisticated title #1',
+  textFor035Filed: '$a (OCoLC)ocn607TST001',
+  searchText1: '(OCoLC)ocn607TST001',
+  searchText2: 'Sophisticated title #1',
+  instanceTitle: 'Sophisticated title #1',
+};
+const createdInstanceIDs = [];
 
 describe('MARC -> MARC Bibliographic -> Create new MARC bib', () => {
-  let userId;
-
   before(() => {
     cy.createTempUser([
       Permissions.inventoryAll.gui,
@@ -21,7 +32,9 @@ describe('MARC -> MARC Bibliographic -> Create new MARC bib', () => {
     });
   });
   after('Delete test data', () => {
+    cy.getAdminToken();
     Users.deleteViaApi(userId);
+    InventoryInstance.deleteInstanceViaApi(createdInstanceIDs[0]);
   });
 
   it(
@@ -30,18 +43,35 @@ describe('MARC -> MARC Bibliographic -> Create new MARC bib', () => {
     () => {
       InventoryInstance.newMarcBibRecord();
       QuickMarcEditor.waitLoading();
+      QuickMarcEditor.checkSubfieldsAbsenceInTag008();
       QuickMarcEditor.updateExistingField('LDR', '00000naa\\a2200000uu\\4500');
+      QuickMarcEditor.check008FieldContent();
       QuickMarcEditor.updateExistingField('245', 'Sophisticated title #1');
       QuickMarcEditor.addNewField('035', '$a (OCoLC)ocn607TST001', 4);
       QuickMarcEditor.pressSaveAndClose();
-      InventoryInstance.searchResultsWithOption(
-        'Keyword (title, contributor, identifier, HRID, UUID)',
-        '$a (OCoLC)ocn607TST001',
+      QuickMarcEditor.checkAfterSaveAndClose();
+      InventoryInstance.getId().then((id) => {
+        createdInstanceIDs.push(id);
+      });
+      InventorySearchAndFilter.selectSearchOptions(
+        testData.firstSearchOption,
+        testData.searchText1,
       );
-      InventoryInstance.searchResultsWithOption(
-        'OCLC number, normalized',
-        'Sophisticated title #1',
+      InventorySearchAndFilter.clickSearch();
+      InventorySearchAndFilter.verifyInstanceDisplayed(testData.instanceTitle, true);
+      InventorySearchAndFilter.selectSearchOptions(
+        testData.secondSearchOption,
+        testData.searchText1,
       );
+      InventorySearchAndFilter.clickSearch();
+      InventorySearchAndFilter.verifyInstanceDisplayed(testData.instanceTitle, true);
+      InventorySearchAndFilter.selectSearchOptions(
+        testData.firstSearchOption,
+        testData.searchText2,
+      );
+      InventorySearchAndFilter.clickSearch();
+      InventorySearchAndFilter.verifyInstanceDisplayed(testData.instanceTitle, true);
+      InventoryInstance.waitInstanceRecordViewOpened();
     },
   );
 });
