660647cd7ddce736f9fb76084a4fd48e2c259f55
Daniil Novikau <novikaud@gmail.com>
2023-12-06 13:52:36 +0300

    [353987] Create and open new order and PO line from instance record

--- cypress/support/fragments/orders/index.js
+++ cypress/support/fragments/orders/index.js
@@ -1,7 +1,9 @@
 export { default as BasicOrderLine } from './basicOrderLine';
 export { default as NewOrder } from './newOrder';
 export { default as OrderDetails } from './orderDetails';
+export { default as OrderEditForm } from './orderEditForm';
 export { default as OrderLineDetails } from './orderLineDetails';
+export { default as OrderLineEditForm } from './orderLineEditForm';
 export { default as OrderLines } from './orderLines';
 export { default as Orders } from './orders';
 export { default as OrderHerlper } from './ordersHelper';
