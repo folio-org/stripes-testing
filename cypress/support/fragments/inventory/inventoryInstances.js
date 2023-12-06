b82f2966b20940ad146a7335cd2107e4c5b37191
Yauhen Viazau <Yauhen_Viazau@epam.com>
2023-12-06 20:29:39 +0500

    FAT-9727: steps added

--- cypress/support/fragments/inventory/inventoryInstances.js
+++ cypress/support/fragments/inventory/inventoryInstances.js
@@ -1070,4 +1070,12 @@ export default {
   verifyActionsSortedBy(value) {
     cy.expect(actionsSortSelect.has({ checkedOptionText: value }));
   },
+
+  clickColumnHeader(headerName) {
+    cy.do([
+      inventoriesList.find(MultiColumnListHeader(headerName)).click(),
+      // wait for sort to apply
+      cy.wait(2000),
+    ]);
+  },
 };
