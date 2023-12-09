63d1e922a377669c47643c4a3ad1b8b6c52a37b1
miso-soup-lover <misosouplover2023@gmail.com>
2023-12-09 15:34:14 +0500

    C365102

---
+++ cypress/e2e/export-manager/bulk-edit-csv-users-hyperlinks.cy.js
@@ -0,0 +1,15 @@
+
+describe('export-manager', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C365102 Verify hyperlink on the "JobID" column -- Local approach (firebird)',
+      { tags: ['criticalPath', 'firebird'] },
+      () => {
+      },
+    );
+});
