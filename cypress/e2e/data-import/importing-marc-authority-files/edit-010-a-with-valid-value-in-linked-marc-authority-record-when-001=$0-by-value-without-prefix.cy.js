9af58aa3363644b1bf3a6a950655a33bc93d06e4
Tetiana_Paranich <tetiana_paranich@epam.com>
2023-12-09 19:12:50 +0200

    added test

---
+++ cypress/e2e/data-import/importing-marc-authority-files/edit-010-a-with-valid-value-in-linked-marc-authority-record-when-001=$0-by-value-without-prefix.cy.js
@@ -0,0 +1,257 @@
+import Permissions from '../../../support/dictionary/permissions';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import Logs from '../../../support/fragments/data_import/logs/logs';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import MarcAuthority from '../../../support/fragments/marcAuthority/marcAuthority';
+import MarcAuthorities from '../../../support/fragments/marcAuthority/marcAuthorities';
+import QuickMarcEditor from '../../../support/fragments/quickMarcEditor';
+import NewJobProfile from '../../../support/fragments/data_import/job_profiles/newJobProfile';
+import ActionProfiles from '../../../support/fragments/data_import/action_profiles/actionProfiles';
+import MatchProfiles from '../../../support/fragments/data_import/match_profiles/matchProfiles';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import ExportFile from '../../../support/fragments/data-export/exportFile';
+import FileManager from '../../../support/utils/fileManager';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import {
+  JOB_STATUS_NAMES,
+  FOLIO_RECORD_TYPE,
+  ACCEPTED_DATA_TYPE_NAMES,
+  RECORD_STATUSES,
+} from '../../../support/constants';
+import FieldMappingProfileView from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfileView';
+import MarcAuthoritiesSearch from '../../../support/fragments/marcAuthority/marcAuthoritiesSearch';
+import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
+import NewMatchProfile from '../../../support/fragments/data_import/match_profiles/newMatchProfile';
+import NewFieldMappingProfile from '../../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
+
+describe('data-import', () => {
+  describe('Importing MARC Authority files', () => {
+    const testData = {
+      tag630: '630',
+      tag130: '130',
+      tag130content: '',
+      createdRecordIDs: [],
+      marcValue: 'C374184 Marvel comics',
+      searchOption: 'Keyword',
+      csvFile: `C374184 exportedCSVFile${getRandomPostfix()}.csv`,
+      exportedMarcFile: `C374184 exportedMarcFile${getRandomPostfix()}.mrc`,
+      marcFileForModify: 'marcBibFileForC374184_1.mrc',
+      modifiedMarcFile: `C374184 editedMarcFile${getRandomPostfix()}.mrc`,
+      uploadModifiedMarcFile: `C374184 testMarcFile${getRandomPostfix()}.mrc`,
+      updated700Field: [
+        23,
+        '630',
+        '0',
+        '0',
+        '$a C374184 Marvel comics',
+        '$v Catalogs.',
+        '$0 id.loc.gov/authorities/names/n80026980',
+        '',
+      ],
+      instanceTitle: 'Marvel comics direct distributors meeting / Marvel Comics Group.',
+      calloutMessage:
+        "is complete. The .csv downloaded contains selected records' UIIDs. To retrieve the .mrc file, please go to the Data export app.",
+    };
+    const mappingProfile = {
+      name: `C374184 Update MARC authority records by matching 999 ff $s subfield value ${getRandomPostfix()}`,
+    };
+    const actionProfile = {
+      typeValue: FOLIO_RECORD_TYPE.MARCAUTHORITY,
+      name: `C374184 Update MARC authority records by matching 999 ff $s subfield value ${getRandomPostfix()}`,
+      action: 'Update (all record types except Orders, Invoices, or MARC Holdings)',
+    };
+    const matchProfile = {
+      profileName: `C374184 Update MARC authority records by matching 999 ff $s subfield value ${getRandomPostfix()}`,
+      incomingRecordFields: {
+        field: '999',
+        in1: 'f',
+        in2: 'f',
+        subfield: 's',
+      },
+      existingRecordFields: {
+        field: '999',
+        in1: 'f',
+        in2: 'f',
+        subfield: 's',
+      },
+    };
+    const jobProfile = {
+      ...NewJobProfile.defaultJobProfile,
+      profileName: `C374184 Update MARC authority records by matching 999 ff $s subfield value ${getRandomPostfix()}`,
+      acceptedType: ACCEPTED_DATA_TYPE_NAMES.MARC,
+    };
+    const marcFiles = [
+      {
+        marc: 'marcBibFileForC374184.mrc',
+        fileName: `C374184 testMarcFile${getRandomPostfix()}.mrc`,
+        jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
+        numOfRecords: 1,
+      },
+      {
+        marc: 'marcAuthFileForC374184.mrc',
+        fileName: `C374184 testMarcFile.${getRandomPostfix()}.mrc`,
+        jobProfileToRun: 'Default - Create SRS MARC Authority',
+        numOfRecords: 1,
+        authorityHeading: 'C374184 Marvel comics',
+      },
+    ];
+    const linkingTagAndValue = {
+      rowIndex: 23,
+      value: 'C374184 Marvel comics',
+      tag: '630',
+    };
+
+    before('Create test data and login', () => {
+      cy.getAdminToken();
+      cy.loginAsAdmin();
+      // create Match profile
+      NewMatchProfile.createMatchProfileViaApiMarc(matchProfile);
+
+      // create Field mapping profile
+      NewFieldMappingProfile.createMappingProfileViaApiMarc(mappingProfile);
+
+      // create Action profile and link it to Field mapping profile
+      cy.visit(SettingsMenu.actionProfilePath);
+      ActionProfiles.create(actionProfile, mappingProfile.name);
+
+      // create Job profile
+      cy.visit(SettingsMenu.jobProfilePath);
+      JobProfiles.openNewJobProfileForm();
+      NewJobProfile.fillJobProfile(jobProfile);
+      NewJobProfile.linkMatchProfile(matchProfile.profileName);
+      NewJobProfile.linkActionProfileForMatches(actionProfile.name);
+      // wait for the action profile to be linked
+      cy.wait(1000);
+      NewJobProfile.saveAndClose();
+
+      marcFiles.forEach((marcFile) => {
+        cy.visit(TopMenu.dataImportPath);
+        DataImport.verifyUploadState();
+        DataImport.uploadFileAndRetry(marcFile.marc, marcFile.fileName);
+        JobProfiles.search(marcFile.jobProfileToRun);
+        JobProfiles.runImportFile();
+        JobProfiles.waitFileIsImported(marcFile.fileName);
+        Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
+        Logs.openFileDetails(marcFile.fileName);
+        Logs.getCreatedItemsID().then((link) => {
+          testData.createdRecordIDs.push(link.split('/')[5]);
+        });
+      });
+
+      cy.createTempUser([
+        Permissions.moduleDataImportEnabled.gui,
+        Permissions.inventoryAll.gui,
+        Permissions.uiMarcAuthoritiesAuthorityRecordView.gui,
+        Permissions.uiQuickMarcQuickMarcAuthorityLinkUnlink.gui,
+        Permissions.uiQuickMarcQuickMarcBibliographicEditorView.gui,
+        Permissions.uiQuickMarcQuickMarcBibliographicEditorAll.gui,
+        Permissions.dataExportEnableApp.gui,
+      ]).then((userProperties) => {
+        testData.user = userProperties;
+
+        cy.loginAsAdmin();
+        cy.visit(TopMenu.inventoryPath);
+        InventoryInstances.searchByTitle(testData.createdRecordIDs[0]);
+        InventoryInstances.selectInstance();
+        InventoryInstance.editMarcBibliographicRecord();
+        InventoryInstance.verifyAndClickLinkIconByIndex(23);
+        MarcAuthorities.switchToSearch();
+        InventoryInstance.verifySelectMarcAuthorityModal();
+        InventoryInstance.searchResults(linkingTagAndValue.value);
+        InventoryInstance.clickLinkButton();
+        QuickMarcEditor.verifyAfterLinkingUsingRowIndex(
+          linkingTagAndValue.tag,
+          linkingTagAndValue.rowIndex,
+        );
+        QuickMarcEditor.pressSaveAndClose();
+        QuickMarcEditor.checkAfterSaveAndClose();
+
+        cy.login(testData.user.username, testData.user.password, {
+          path: TopMenu.marcAuthorities,
+          waiter: MarcAuthorities.waitLoading,
+        });
+        MarcAuthoritiesSearch.searchBy(testData.searchOption, testData.marcValue);
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      JobProfiles.deleteJobProfile(jobProfile.profileName);
+      MatchProfiles.deleteMatchProfile(matchProfile.profileName);
+      ActionProfiles.deleteActionProfile(actionProfile.name);
+      FieldMappingProfileView.deleteViaApi(mappingProfile.name);
+      Users.deleteViaApi(testData.user.userId);
+      InventoryInstance.deleteInstanceViaApi(testData.createdRecordIDs[0]);
+      testData.createdRecordIDs.forEach((id, index) => {
+        if (index) MarcAuthority.deleteViaAPI(id);
+      });
+      FileManager.deleteFolder(Cypress.config('downloadsFolder'));
+      FileManager.deleteFile(`cypress/fixtures/${testData.modifiedMarcFile}`);
+      FileManager.deleteFile(`cypress/fixtures/${testData.exportedMarcFile}`);
+      FileManager.deleteFile(`cypress/fixtures/${testData.csvFile}`);
+    });
+
+    it(
+      'C374184 Edit "010 $a" with valid value in linked "MARC Authority" record when "001" = "$0" by value without prefix (spitfire) (TaaS)',
+      { tags: ['extendedPath', 'spitfire'] },
+      () => {
+        cy.wait(1000);
+        MarcAuthorities.selectAllRecords();
+        MarcAuthorities.verifyTextOfPaneHeaderMarcAuthority('1 record selected');
+        MarcAuthorities.exportSelected();
+        cy.wait(1000);
+        MarcAuthorities.checkCallout(testData.calloutMessage);
+        ExportFile.downloadCSVFile(testData.csvFile, 'QuickAuthorityExport*');
+        MarcAuthorities.verifyAllCheckboxesAreUnchecked();
+        MarcAuthorities.verifyTextOfPaneHeaderMarcAuthority('1 record found');
+
+        cy.visit(TopMenu.dataExportPath);
+        ExportFile.uploadFile(testData.csvFile);
+        ExportFile.exportWithDefaultJobProfile(testData.csvFile, 'authority', 'Authorities');
+        ExportFile.downloadExportedMarcFile(testData.exportedMarcFile);
+
+        // change exported file
+        DataImport.replace999SubfieldsInPreupdatedFile(
+          testData.exportedMarcFile,
+          testData.marcFileForModify,
+          testData.modifiedMarcFile,
+        );
+
+        cy.visit(TopMenu.dataImportPath);
+        DataImport.uploadFile(testData.modifiedMarcFile, testData.uploadModifiedMarcFile);
+        JobProfiles.waitFileIsUploaded();
+        JobProfiles.search(jobProfile.profileName);
+        JobProfiles.runImportFile();
+        Logs.waitFileIsImported(testData.uploadModifiedMarcFile);
+        Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
+        Logs.openFileDetails(testData.uploadModifiedMarcFile);
+        [
+          FileDetails.columnNameInResultList.srsMarc,
+          FileDetails.columnNameInResultList.authority,
+        ].forEach((columnName) => {
+          FileDetails.checkStatusInColumn(RECORD_STATUSES.UPDATED, columnName);
+        });
+
+        cy.visit(TopMenu.marcAuthorities);
+        MarcAuthoritiesSearch.searchBy(testData.searchOption, testData.marcValue);
+        MarcAuthorities.verifyNumberOfTitles(5, '1');
+        MarcAuthorities.selectTitle(testData.marcValue);
+        MarcAuthorities.checkRecordDetailPageMarkedValue(testData.marcValue);
+        MarcAuthority.contains(testData.tag130);
+        MarcAuthority.contains(testData.tag130content);
+        MarcAuthorities.closeMarcViewPane();
+        MarcAuthorities.verifyNumberOfTitles(5, '1');
+        MarcAuthorities.clickOnNumberOfTitlesLink(5, '1');
+        InventoryInstance.verifyInstanceTitle(testData.instanceTitle);
+        InventoryInstance.editMarcBibliographicRecord();
+        QuickMarcEditor.checkEditableQuickMarcFormIsOpened();
+        QuickMarcEditor.verifyUnlinkAndViewAuthorityButtons(23);
+        QuickMarcEditor.verifyTagFieldAfterLinking(...testData.updated700Field);
+      },
+    );
+  });
+});
