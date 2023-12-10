4f911b14db4b785d7501a49f400efbc4f0a76a8d
kirstaVova <grouplbua@gmail.com>
2023-12-10 13:42:56 +0200

    C375260: Editing fund distribution from Fund A to Fund B and back to Fund A when related Paid invoices exists (thunderjet) (TaaS)

--- cypress/e2e/orders/editing-fund-distribution-from-funds-and-back.cy.js
+++ cypress/e2e/orders/editing-fund-distribution-from-funds-and-back.cy.js
@@ -1,38 +1,88 @@
 import permissions from '../../support/dictionary/permissions';
-import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
+import FinanceHelp from '../../support/fragments/finance/financeHelper';
+import FiscalYears from '../../support/fragments/finance/fiscalYears/fiscalYears';
+import Funds from '../../support/fragments/finance/funds/funds';
+import Ledgers from '../../support/fragments/finance/ledgers/ledgers';
+import Invoices from '../../support/fragments/invoices/invoices';
+import NewInvoice from '../../support/fragments/invoices/newInvoice';
 import NewOrder from '../../support/fragments/orders/newOrder';
 import OrderLines from '../../support/fragments/orders/orderLines';
 import Orders from '../../support/fragments/orders/orders';
 import NewOrganization from '../../support/fragments/organizations/newOrganization';
 import Organizations from '../../support/fragments/organizations/organizations';
-import Receiving from '../../support/fragments/receiving/receiving';
 import NewLocation from '../../support/fragments/settings/tenant/locations/newLocation';
 import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
 import TopMenu from '../../support/fragments/topMenu';
 import Users from '../../support/fragments/users/users';
+import DateTools from '../../support/utils/dateTools';
 import getRandomPostfix from '../../support/utils/stringTools';
+import InvoiceView from '../../support/fragments/invoices/invoiceView';
 
-describe('orders: Receive piece from Order', () => {
-  const order = { ...NewOrder.defaultOneTimeOrder, approved: true };
-  const organization = { ...NewOrganization.defaultUiOrganizations };
-  const item = {
-    instanceName: `testBulkEdit_${getRandomPostfix()}`,
-    itemBarcode: getRandomPostfix(),
+describe('Orders', () => {
+  const defaultFiscalYear = { ...FiscalYears.defaultUiFiscalYear };
+  const defaultLedger = { ...Ledgers.defaultUiLedger };
+  const firstFund = { ...Funds.defaultUiFund };
+  const secondFund = {
+    name: `autotest_fund2_${getRandomPostfix()}`,
+    code: getRandomPostfix(),
+    externalAccountNo: getRandomPostfix(),
+    fundStatus: 'Active',
+    description: `This is fund created by E2E test automation script_${getRandomPostfix()}`,
   };
+  const firstOrder = {
+    ...NewOrder.defaultOneTimeOrder,
+    orderType: 'Ongoing',
+    ongoing: { isSubscription: false, manualRenewal: false },
+    approved: true,
+    reEncumber: true,
+  };
+  const organization = { ...NewOrganization.defaultUiOrganizations };
+  const firstInvoice = { ...NewInvoice.defaultUiInvoice };
+  const secondInvoice = {
+    status: 'Open',
+    invoiceDate: DateTools.getCurrentDate(),
+    vendorName: 'Amazon.com',
+    accountingCode: '',
+    batchGroup: '',
+    invoiceNumber: FinanceHelp.getRandomInvoiceNumber(),
+  };
+  const allocatedQuantity = '100';
   let user;
   let orderNumber;
-  let orderID;
-  let location;
   let servicePointId;
+  let location;
 
   before(() => {
     cy.getAdminToken();
+    // create first Fiscal Year and prepere 2 Funds for Rollover
+    FiscalYears.createViaApi(defaultFiscalYear).then((firstFiscalYearResponse) => {
+      defaultFiscalYear.id = firstFiscalYearResponse.id;
+      defaultLedger.fiscalYearOneId = defaultFiscalYear.id;
+      Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
+        defaultLedger.id = ledgerResponse.id;
+        firstFund.ledgerId = defaultLedger.id;
+        secondFund.ledgerId = defaultLedger.id;
 
-    Organizations.createOrganizationViaApi(organization).then((response) => {
-      organization.id = response;
-      order.vendor = response;
+        Funds.createViaApi(firstFund).then((fundResponse) => {
+          firstFund.id = fundResponse.fund.id;
+
+          cy.loginAsAdmin({ path: TopMenu.fundPath, waiter: Funds.waitLoading });
+          FinanceHelp.searchByName(firstFund.name);
+          Funds.selectFund(firstFund.name);
+          Funds.addBudget(allocatedQuantity);
+        });
+
+        Funds.createViaApi(secondFund).then((secondFundResponse) => {
+          secondFund.id = secondFundResponse.fund.id;
+
+          cy.visit(TopMenu.fundPath);
+          FinanceHelp.searchByName(secondFund.name);
+          Funds.selectFund(secondFund.name);
+          Funds.addBudget(allocatedQuantity);
+        });
+      });
     });
-    InventoryInstances.createInstanceViaApi(item.instanceName, item.itemBarcode);
+
     ServicePoints.getViaApi().then((servicePoint) => {
       servicePointId = servicePoint[0].id;
       NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then((res) => {
@@ -40,29 +90,48 @@ describe('orders: Receive piece from Order', () => {
       });
     });
 
-    cy.loginAsAdmin({ path: TopMenu.ordersPath, waiter: Orders.waitLoading });
-
-    cy.createOrderApi(order).then((response) => {
-      orderNumber = response.body.poNumber;
-      orderID = response.body.id;
-      Orders.searchByParameter('PO number', orderNumber);
-      Orders.selectFromResultsList(orderNumber);
+    Organizations.createOrganizationViaApi(organization).then((responseOrganizations) => {
+      organization.id = responseOrganizations;
+      firstInvoice.accountingCode = organization.erpCode;
+      secondInvoice.accountingCode = organization.erpCode;
+      cy.getBatchGroups().then((batchGroup) => {
+        secondInvoice.batchGroup = batchGroup.name;
+      });
+    });
+    firstOrder.vendor = organization.name;
+    cy.visit(TopMenu.ordersPath);
+    Orders.createApprovedOrderForRollover(firstOrder, true).then((firstOrderResponse) => {
+      firstOrder.id = firstOrderResponse.id;
+      orderNumber = firstOrderResponse.poNumber;
+      Orders.checkCreatedOrder(firstOrder);
       OrderLines.addPOLine();
-      OrderLines.selectRandomInstanceInTitleLookUP(item.instanceName);
-      OrderLines.fillPOLWithTitleLookUp();
+      OrderLines.selectRandomInstanceInTitleLookUP('*', 15);
+      OrderLines.rolloverPOLineInfoforPhysicalMaterialWithFund(
+        firstFund,
+        '100',
+        '1',
+        '100',
+        location.institutionId,
+      );
       OrderLines.backToEditingOrder();
       Orders.openOrder();
+      cy.visit(TopMenu.invoicesPath);
+      Invoices.createRolloverInvoice(firstInvoice, organization.name);
+      Invoices.createInvoiceLineFromPol(orderNumber);
+      // Need to wait, while data will be loaded
+      cy.wait(4000);
+      Invoices.approveInvoice();
+      Invoices.payInvoice();
     });
-
     cy.createTempUser([
-      permissions.uiOrdersView.gui,
+      permissions.uiInvoicesApproveInvoices.gui,
+      permissions.viewEditCreateInvoiceInvoiceLine.gui,
+      permissions.uiFinanceViewFundAndBudget.gui,
+      permissions.uiInvoicesPayInvoices.gui,
       permissions.uiOrdersEdit.gui,
-      permissions.uiInventoryViewInstances.gui,
-      permissions.uiReceivingViewEditCreate.gui,
     ]).then((userProperties) => {
       user = userProperties;
-
-      cy.login(user.username, user.password, {
+      cy.login(userProperties.username, userProperties.password, {
         path: TopMenu.ordersPath,
         waiter: Orders.waitLoading,
       });
@@ -71,15 +140,6 @@ describe('orders: Receive piece from Order', () => {
 
   after(() => {
     cy.getAdminToken();
-    Orders.deleteOrderViaApi(orderID);
-    Organizations.deleteOrganizationViaApi(organization.id);
-    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.itemBarcode);
-    NewLocation.deleteViaApiIncludingInstitutionCampusLibrary(
-      location.institutionId,
-      location.campusId,
-      location.libraryId,
-      location.id,
-    );
     Users.deleteViaApi(user.userId);
   });
 
@@ -87,16 +147,88 @@ describe('orders: Receive piece from Order', () => {
     'C375260: Editing fund distribution from Fund A to Fund B and back to Fund A when related Paid invoices exists (thunderjet) (TaaS)',
     { tags: ['extended', 'thunderjet'] },
     () => {
-      const caption = 'autotestCaption';
       Orders.searchByParameter('PO number', orderNumber);
       Orders.selectFromResultsList(orderNumber);
-      // Receiving part
-      Orders.receiveOrderViaActions();
-      Receiving.selectFromResultsList(item.instanceName);
-      Receiving.receiveAndChangeLocation(0, caption, location.institutionId);
+      OrderLines.selectPOLInOrder();
+      OrderLines.editPOLInOrder();
+      OrderLines.editFundInPOL(secondFund, '100', '100');
+      OrderLines.backToEditingOrder();
+      Orders.newInvoiceFromOrder();
+      Invoices.createInvoiceFromOrderWithoutFY(secondInvoice);
+      Invoices.approveInvoice();
+      Invoices.payInvoice();
 
-      Receiving.checkReceived(0, caption);
-      Receiving.selectInstanceInReceive(item.instanceName);
+      cy.visit(TopMenu.ordersPath);
+      Orders.searchByParameter('PO number', orderNumber);
+      Orders.selectFromResultsList(orderNumber);
+      OrderLines.selectPOLInOrder();
+      OrderLines.openPageCurrentEncumbrance('$0.00');
+      Funds.varifyDetailsInTransaction(
+        defaultFiscalYear.code,
+        '$0.00',
+        `${orderNumber}-1`,
+        'Encumbrance',
+        `${secondFund.name} (${secondFund.code})`,
+      );
+      Funds.checkStatusInTransactionDetails('Released');
+      Funds.checkExpendedInTransactionDetails('$200.00');
+      Funds.selectTransactionInList('Payment');
+      Funds.varifyDetailsInTransaction(
+        defaultFiscalYear.code,
+        '($100.00)',
+        secondInvoice.invoiceNumber,
+        'Payment',
+        `${secondFund.name} (${secondFund.code})`,
+      );
+
+      cy.visit(TopMenu.invoicesPath);
+      Invoices.searchByNumber(firstInvoice.invoiceNumber);
+      Invoices.selectInvoice(firstInvoice.invoiceNumber);
+      InvoiceView.selectInvoiceLine();
+      Invoices.openPageFundInInvoiceLine(`${firstFund.name}(${firstFund.code})`);
+      Funds.selectBudgetDetails();
+      Funds.viewTransactions();
+      Funds.selectTransactionInList('Payment');
+      Funds.varifyDetailsInTransaction(
+        defaultFiscalYear.code,
+        '($100.00)',
+        firstInvoice.invoiceNumber,
+        'Payment',
+        `${firstFund.name} (${firstFund.code})`,
+      );
+
+      cy.visit(TopMenu.ordersPath);
+      Orders.searchByParameter('PO number', orderNumber);
+      Orders.selectFromResultsList(orderNumber);
+      OrderLines.selectPOLInOrder();
+      OrderLines.editPOLInOrder();
+      OrderLines.editFundInPOL(secondFund, '100', '100');
+      OrderLines.openPageCurrentEncumbrance('$0.00');
+      Funds.varifyDetailsInTransaction(
+        defaultFiscalYear.code,
+        '$0.00',
+        `${orderNumber}-1`,
+        'Encumbrance',
+        `${firstFund.name} (${firstFund.code})`,
+      );
+      Funds.checkStatusInTransactionDetails('Released');
+      Funds.checkExpendedInTransactionDetails('$200.00');
+
+      cy.visit(TopMenu.invoicesPath);
+      Invoices.searchByNumber(secondInvoice.invoiceNumber);
+      Invoices.selectInvoice(secondInvoice.invoiceNumber);
+      InvoiceView.selectInvoiceLine();
+      Invoices.openPageFundInInvoiceLine(`${secondFund.name}(${secondFund.code})`);
+      Funds.selectBudgetDetails();
+      Funds.viewTransactions();
+      Funds.selectTransactionInList('Payment');
+      Funds.varifyDetailsInTransaction(
+        defaultFiscalYear.code,
+        '($100.00)',
+        secondInvoice.invoiceNumber,
+        'Payment',
+        `${secondFund.name} (${secondFund.code})`,
+      );
     },
   );
 });
