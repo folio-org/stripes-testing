1602533a4b77c87e39fe2cb36982d5d55a7ef7ee
Yuri_Karpov <Yuri_Karpov@epam.com>
2023-12-12 14:54:17 +0400

    Removed checkout and checking, added inner describe block

--- cypress/e2e/bulk-edit/logs/in-app/verify-that-actions-menu-is-hidden-on-the-logs-tab-with-in-app-permissions.cy.js
+++ cypress/e2e/bulk-edit/logs/in-app/verify-that-actions-menu-is-hidden-on-the-logs-tab-with-in-app-permissions.cy.js
@@ -8,8 +8,6 @@ import FileManager from '../../../../support/utils/fileManager';
 import ServicePoints from '../../../../support/fragments/settings/tenant/servicePoints/servicePoints';
 import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
 import UserEdit from '../../../../support/fragments/users/userEdit';
-import Checkout from '../../../../support/fragments/checkout/checkout';
-import CheckInActions from '../../../../support/fragments/check-in-actions/checkInActions';
 
 let user;
 let servicePointId;
@@ -20,81 +18,73 @@ const item = {
 const itemBarcodesFileName = `itemBarcodes_${getRandomPostfix()}.csv`;
 
 describe('bulk-edit', () => {
-  before('create user', () => {
-    cy.createTempUser([
-      permissions.bulkEditView.gui,
-      permissions.bulkEditEdit.gui,
-      permissions.bulkEditLogsView.gui,
-      permissions.inventoryAll.gui,
-      permissions.uiUserEdit.gui,
-      permissions.uiUsersPermissions.gui,
-    ]).then((userProperties) => {
-      user = userProperties;
-      cy.login(user.username, user.password, {
-        path: TopMenu.bulkEditPath,
-        waiter: BulkEditSearchPane.waitLoading,
-      });
-      InventoryInstances.createInstanceViaApi(item.instanceName, item.barcode);
-      ServicePoints.getViaApi({ limit: 1, query: 'name=="Circ Desk 1"' })
-        .then((servicePoints) => {
-          servicePointId = servicePoints[0].id;
-        })
-        .then(() => {
-          UserEdit.addServicePointViaApi(servicePointId, user.userId, servicePointId);
-          Checkout.checkoutItemViaApi({
-            itemBarcode: item.barcode,
-            servicePointId,
-            userBarcode: user.barcode,
-          });
-          FileManager.createFile(`cypress/fixtures/${itemBarcodesFileName}`, item.barcode);
+  describe('in-app approach', () => {
+    before('create user', () => {
+      cy.createTempUser([
+        permissions.bulkEditView.gui,
+        permissions.bulkEditEdit.gui,
+        permissions.bulkEditLogsView.gui,
+        permissions.inventoryAll.gui,
+        permissions.uiUserEdit.gui,
+        permissions.uiUsersPermissions.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password, {
+          path: TopMenu.bulkEditPath,
+          waiter: BulkEditSearchPane.waitLoading,
         });
+        InventoryInstances.createInstanceViaApi(item.instanceName, item.barcode);
+        ServicePoints.getViaApi({ limit: 1, query: 'name=="Circ Desk 1"' })
+          .then((servicePoints) => {
+            servicePointId = servicePoints[0].id;
+          })
+          .then(() => {
+            UserEdit.addServicePointViaApi(servicePointId, user.userId, servicePointId);
+            FileManager.createFile(`cypress/fixtures/${itemBarcodesFileName}`, item.barcode);
+          });
+      });
     });
-  });
 
-  after('delete test data', () => {
-    FileManager.deleteFile(`cypress/fixtures/${itemBarcodesFileName}`);
+    after('delete test data', () => {
+      FileManager.deleteFile(`cypress/fixtures/${itemBarcodesFileName}`);
 
-    cy.getAdminToken();
-    CheckInActions.checkinItemViaApi({
-      itemBarcode: item.barcode,
-      servicePointId,
-      checkInDate: new Date().toISOString(),
+      cy.getAdminToken();
+      InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.barcode);
+      Users.deleteViaApi(user.userId);
     });
-    InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.barcode);
-    Users.deleteViaApi(user.userId);
+
+    it(
+      'C367996 Verify that "Actions" menu is hidden on the "Logs" tab with In-app permissions (firebird) (TaaS)',
+      { tags: ['extendedPath', 'firebird'] },
+      () => {
+        BulkEditSearchPane.verifySetCriteriaPaneSpecificTabs('Identifier', 'Logs');
+        BulkEditSearchPane.verifySpecificTabHighlighted('Identifier');
+        BulkEditSearchPane.isHoldingsRadioChecked(false);
+        BulkEditSearchPane.isItemsRadioChecked(false);
+        BulkEditSearchPane.actionsIsAbsent();
+
+        BulkEditSearchPane.checkItemsRadio();
+        BulkEditSearchPane.selectRecordIdentifier('Item barcode');
+        BulkEditSearchPane.uploadFile(itemBarcodesFileName);
+        BulkEditSearchPane.waitFileUploading();
+
+        BulkEditSearchPane.verifyActionsAfterConductedInAppUploading(false);
+
+        BulkEditActions.openInAppStartBulkEditFrom();
+        BulkEditActions.addItemNote('Action note', 'Test note');
+        BulkEditActions.confirmChanges();
+        BulkEditActions.commitChanges();
+
+        BulkEditSearchPane.openLogsSearch();
+        BulkEditSearchPane.verifyLogsPane();
+        BulkEditSearchPane.actionsIsAbsent();
+
+        cy.reload();
+        BulkEditSearchPane.checkItemsCheckbox();
+        BulkEditSearchPane.verifyActionsRunBy(
+          `${user.username}, ${user.firstName} ${Users.defaultUser.personal.middleName}`,
+        );
+      },
+    );
   });
-
-  it(
-    'C367996 Verify that "Actions" menu is hidden on the "Logs" tab with In-app permissions (firebird) (TaaS)',
-    { tags: ['extendedPath', 'firebird'] },
-    () => {
-      BulkEditSearchPane.verifySetCriteriaPaneSpecificTabs('Identifier', 'Logs');
-      BulkEditSearchPane.verifySpecificTabHighlighted('Identifier');
-      BulkEditSearchPane.isHoldingsRadioChecked(false);
-      BulkEditSearchPane.isItemsRadioChecked(false);
-      BulkEditSearchPane.actionsIsAbsent();
-
-      BulkEditSearchPane.checkItemsRadio();
-      BulkEditSearchPane.selectRecordIdentifier('Item barcode');
-      BulkEditSearchPane.uploadFile(itemBarcodesFileName);
-      BulkEditSearchPane.waitFileUploading();
-
-      BulkEditSearchPane.verifyActionsAfterConductedInAppUploading(false);
-
-      BulkEditActions.openInAppStartBulkEditFrom();
-      BulkEditActions.addItemNote('Action note', 'Test note');
-      BulkEditActions.confirmChanges();
-      BulkEditActions.commitChanges();
-
-      BulkEditSearchPane.openLogsSearch();
-      BulkEditSearchPane.verifyLogsPane();
-      BulkEditSearchPane.actionsIsAbsent();
-
-      cy.reload();
-      BulkEditSearchPane.checkItemsCheckbox();
-      BulkEditSearchPane.verifyActionsRunBy(
-        `${user.username}, ${user.firstName} ${Users.defaultUser.personal.middleName}`,
-      );
-    },
-  );
 });
