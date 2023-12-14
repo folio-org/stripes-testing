2b6e658ca173b894251f8e53aa306060f6bd4e9c
IhorBohdan <99126301+IhorBohdan@users.noreply.github.com>
2023-12-14 19:02:12 +0200

    Implementing of FAT-9485 (#2719)
    * Implementing of FAT-9485
    
    * Implementing of FAT-9485
    
    * Implementing of FAT-9485
    
    * add methods
    
    * Implementing of FAT-9485
    
    * Implementing of FAT-9485
    
    ---------
    
    Co-authored-by: kirstaVova <grouplbua@gmail.com>

--- cypress/support/dictionary/affiliations.js
+++ cypress/support/dictionary/affiliations.js
@@ -8,4 +8,21 @@ export const tenantNames = {
   central: 'Central Office',
   college: 'College',
   university: 'University',
+  professional: 'Professional',
+  school: 'School',
+  special: 'Special,',
+};
+
+export const tenantCodes = {
+  central: 'CEN',
+  college: 'COL',
+  university: 'UNI',
+  professional: 'PROF',
+  school: 'SCHO',
+  special: 'SPE,',
+};
+
+export const tenantErrors = {
+  code: 'The code must not be longer than 5 characters alphanumeric.',
+  name: 'Tenant name must not be longer than 150 characters.',
 };
--- cypress/support/fragments/settings/consortium-manager/consortium-manager.js
+++ cypress/support/fragments/settings/consortium-manager/consortium-manager.js
@@ -1,6 +1,61 @@
-import { Button, Modal, SelectionOption, including } from '../../../../../interactors';
+import {
+  Button,
+  Modal,
+  NavListItem,
+  Section,
+  SelectionOption,
+  TextField,
+  including,
+} from '../../../../../interactors';
 
 export default {
+  waitLoading() {
+    cy.expect(Section({ id: 'app-settings-nav-pane' }).exists());
+  },
+  waitLoadingForAddresses() {
+    cy.expect(Section({ id: 'app-settings-nav-pane' }).exists());
+  },
+
+  selectMembership() {
+    cy.do(NavListItem('Membership').click());
+  },
+
+  editTenant(name) {
+    cy.contains('div[class*="mclCell-"]', name)
+      .parent('div[class*="mclRow-"]')
+      .find('button[icon="edit"]')
+      .click();
+  },
+
+  editTenantInformation(tenantIndex, codeText, nameText) {
+    cy.do([
+      TextField({ name: `items[${tenantIndex}].code` }).fillIn(codeText),
+      TextField({ name: `items[${tenantIndex}].name` }).fillIn(nameText),
+    ]);
+  },
+
+  saveEditingTenantInformation(tenantIndex) {
+    cy.do(Button({ id: `clickable-save-consortia-membership-${tenantIndex}` }).click());
+  },
+
+  cancelEditingTenantInformation(tenantIndex) {
+    cy.do(Button({ id: `clickable-cancel-consortia-membership-${tenantIndex}` }).click());
+  },
+
+  checkEditedTenantInformation(tenantIndex, codeText, nameText) {
+    cy.expect([
+      TextField({ name: `items[${tenantIndex}].code` }).has({ value: codeText }),
+      TextField({ name: `items[${tenantIndex}].name` }).has({ value: nameText }),
+    ]);
+  },
+
+  checkErrorsInEditedTenantInformation(tenantIndex, codeText, nameText) {
+    cy.expect([
+      TextField({ name: `items[${tenantIndex}].code` }).has({ error: codeText }),
+      TextField({ name: `items[${tenantIndex}].name` }).has({ error: nameText }),
+    ]);
+  },
+
   switchActiveAffiliation(tenantName) {
     cy.wait(8000);
     cy.do([
