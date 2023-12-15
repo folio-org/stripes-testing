6e800911f5f0b037f257620706cf1156a1528f53
Yauhen Viazau <Yauhen_Viazau@epam.com>
2023-12-15 11:56:48 +0500

    tenant name updated

--- cypress/e2e/consortia/create-shared-marc-bib-in-central.cy.js
+++ cypress/e2e/consortia/create-shared-marc-bib-in-central.cy.js
@@ -1,5 +1,5 @@
 import Permissions from '../../support/dictionary/permissions';
-import Affiliations, { TENANT_NAMES } from '../../support/dictionary/affiliations';
+import Affiliations, { tenantNames } from '../../support/dictionary/affiliations';
 import Users from '../../support/fragments/users/users';
 import TopMenu from '../../support/fragments/topMenu';
 import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
@@ -105,7 +105,7 @@ describe('MARC -> MARC Bibliographic -> Create new MARC bib -> Consortia', () =>
         path: TopMenu.inventoryPath,
         waiter: InventoryInstances.waitContentLoading,
       });
-      ConsortiumManager.switchActiveAffiliation(TENANT_NAMES.COLLEGE);
+      ConsortiumManager.switchActiveAffiliation(tenantNames.college);
       InventoryInstance.searchByTitle(testData.fieldContents.tag245Content);
       InventoryInstances.selectInstance();
       InventoryInstance.verifySharedIcon();
