832a5908910c7c366624a9cfccf19d0d22cf592c
Daniil Novikau <novikaud@gmail.com>
2023-12-19 15:05:13 +0300

    [422162] Add donor information to a Fund

--- cypress/e2e/finance/funds/add-donor-information-to-fund.cy.js
+++ cypress/e2e/finance/funds/add-donor-information-to-fund.cy.js
@@ -5,47 +5,55 @@ import TopMenu from '../../../support/fragments/topMenu';
 import Users from '../../../support/fragments/users/users';
 
 describe('Finance', () => {
+  const fiscalYear = FiscalYears.getDefaultFiscalYear();
+  const ledger = { ...Ledgers.getDefaultLedger(), fiscalYearOneId: fiscalYear.id };
+  const testData = {
+    organization: NewOrganization.getDefaultOrganization({
+      isDonor: true,
+      isVendor: false,
+    }),
+    fiscalYear,
+    ledger,
+    user: {},
+  };
+
+  before('Create test data', () => {
+    cy.getAdminToken().then(() => {
+      Organizations.createOrganizationViaApi(testData.organization);
+      FiscalYears.createViaApi(testData.fiscalYear);
+      Ledgers.createViaApi(testData.ledger);
+    });
+
+    cy.createTempUser([
+      Permissions.uiFinanceViewEditCreateFundAndBudget.gui,
+      Permissions.uiOrganizationsView.gui,
+    ]).then((userProperties) => {
+      testData.user = userProperties;
+    });
+  });
+
+  after('Delete test data', () => {
+    cy.getAdminToken().then(() => {
+      Ledgers.deleteledgerViaApi(testData.ledger.id);
+      FiscalYears.deleteFiscalYearViaApi(testData.fiscalYear.id);
+      Organizations.deleteOrganizationViaApi(testData.organization.id);
+      Users.deleteViaApi(testData.user.userId);
+    });
+  });
+
   describe('Funds', () => {
-    const fiscalYear = FiscalYears.getDefaultFiscalYear();
-    const ledger = { ...Ledgers.getDefaultLedger(), fiscalYearOneId: fiscalYear.id };
-    const testData = {
-      organization: NewOrganization.getDefaultOrganization({
-        isDonor: true,
-        isVendor: false,
-      }),
-      fiscalYear,
-      ledger,
-      fund: { ...Funds.getDefaultFund(), ledgerId: ledger.id },
-      user: {},
-    };
+    testData.fund = { ...Funds.getDefaultFund(), ledgerId: ledger.id };
 
-    before('Create test data', () => {
-      cy.getAdminToken().then(() => {
-        Organizations.createOrganizationViaApi(testData.organization);
-        FiscalYears.createViaApi(testData.fiscalYear);
-        Ledgers.createViaApi(testData.ledger);
-      });
-
-      cy.createTempUser([
-        Permissions.uiFinanceViewEditCreateFundAndBudget.gui,
-        Permissions.uiOrganizationsView.gui,
-      ]).then((userProperties) => {
-        testData.user = userProperties;
-
-        cy.login(testData.user.username, testData.user.password, {
-          path: TopMenu.fundPath,
-          waiter: Funds.waitLoading,
-        });
+    before('Login', () => {
+      cy.login(testData.user.username, testData.user.password, {
+        path: TopMenu.fundPath,
+        waiter: Funds.waitLoading,
       });
     });
 
-    after('Delete test data', () => {
+    after('Delete test Fund', () => {
       cy.getAdminToken().then(() => {
         Funds.deleteFundsByLedgerIdViaApi(testData.ledger.id);
-        Ledgers.deleteledgerViaApi(testData.ledger.id);
-        FiscalYears.deleteFiscalYearViaApi(testData.fiscalYear.id);
-        Organizations.deleteOrganizationViaApi(testData.organization.id);
-        Users.deleteViaApi(testData.user.userId);
       });
     });
 
@@ -89,6 +97,49 @@ describe('Finance', () => {
           donorName: testData.organization.name,
           shouldExpand: false,
         });
+        FundEditForm.checkDonorInformationSectionContent({
+          donors: [{ name: testData.organization.name, code: testData.organization.code }],
+        });
+
+        // Click on "Save & close" button on "Create fund" page
+        FundEditForm.clickSaveAndCloseButton();
+      },
+    );
+  });
+
+  describe('Funds', () => {
+    testData.fund = { ...Funds.getDefaultFund(), ledgerId: ledger.id };
+
+    before('Create test Fund', () => {
+      cy.getAdminToken().then(() => {
+        Funds.createViaApi(testData.fund);
+      });
+
+      cy.login(testData.user.username, testData.user.password, {
+        path: TopMenu.fundPath,
+        waiter: Funds.waitLoading,
+      });
+    });
+
+    after('Delete test Fund', () => {
+      cy.getAdminToken().then(() => {
+        Funds.deleteFundViaApi(testData.fund.id);
+      });
+    });
+
+    it(
+      'C422162 Add donor information to an existing fund (thunderjet) (TaaS)',
+      { tags: ['criticalPath', 'thunderjet'] },
+      () => {
+        // Open Fund from Preconditions
+        Funds.searchByName(testData.fund.name);
+        const FundDetails = Funds.selectFund(testData.fund.name);
+
+        // Click on "Actions" button, Select "Edit" option
+        const FundEditForm = FundDetails.openFundEditForm();
+
+        // Click on "Add donor" button, Check the checkbox next to a donor organization, Click "Save" button
+        FundEditForm.fillDonorInfoSectionFields({ donorName: testData.organization.name });
         FundEditForm.checkDonorInformationSectionContent({
           donors: [{ name: testData.organization.name, code: testData.organization.code }],
         });
