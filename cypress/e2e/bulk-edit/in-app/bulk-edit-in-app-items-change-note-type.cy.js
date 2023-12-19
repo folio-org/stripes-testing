3b7c4ec01380aaeb70e85c9351288b3a5cf8c1d8
miso-soup-lover <misosouplover2023@gmail.com>
2023-12-19 17:56:06 +0500

    C402336

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-items-change-note-type.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C402336 Verify Bulk Edit actions for Items notes - change note type (firebird) (TaaS)',
+      { tags: ['criticalPath', 'firebird'] },
+      () => {
+      },
+    );
+  });
+});
