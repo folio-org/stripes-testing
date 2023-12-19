a0853158fb72907443d16c32e45c4ebc7626a749
miso-soup-lover <misosouplover2023@gmail.com>
2023-12-19 18:00:11 +0500

    C397354

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-items-cancel.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C397354 Verify CANCEL during Bulk edit Items In app (firebird) (TaaS)',
+      { tags: ['criticalPath', 'firebird'] },
+      () => {
+      },
+    );
+  });
+});
