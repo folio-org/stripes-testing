d748fc529696a8c8d7c375e341361421506e0f50
miso-soup-lover <misosouplover2023@gmail.com>
2023-12-19 18:11:24 +0500

    C422070

---
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-holdings-notes-find-remove-add-same-type.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('bulk-edit', () => {
+  describe('in-app approach', () => {
+    before('create test data', () => {
+    });
+
+    after('delete test data', () => {
+    });
+
+    it(
+      'C422070  Verify Bulk Edit actions for Holdings notes - Find-Remove and Add the same type (firebird) (TaaS)',
+      { tags: ['criticalPath', 'firebird'] },
+      () => {
+      },
+    );
+  });
+});
