fbaf777188be91b0dda3327c050548073536d389
Ilya_Lyskavets <Ilya_Lyskavets@epam.com>
2023-12-19 12:54:55 +0300

    add describe block

--- cypress/e2e/inventory/settings/verifyWidgetOnHRIDSettingsPage.cy.js
+++ cypress/e2e/inventory/settings/verifyWidgetOnHRIDSettingsPage.cy.js
@@ -4,44 +4,46 @@ import SettingsMenu from '../../../support/fragments/settingsMenu';
 import Users from '../../../support/fragments/users/users';
 import DateTools from '../../../support/utils/dateTools';
 
-describe('Settings', () => {
-  const testData = {};
+describe('inventory', () => {
+  describe('Settings', () => {
+    const testData = {};
 
-  before('Create test data', () => {
-    cy.createTempUser([
-      Permissions.uiUsersView.gui,
-      Permissions.uiSettingsHRIDHandlingCreateEditDelete.gui,
-    ]).then((userProperties) => {
-      testData.user = userProperties;
-      cy.login(testData.user.username, testData.user.password);
+    before('Create test data', () => {
+      cy.createTempUser([
+        Permissions.uiUsersView.gui,
+        Permissions.uiSettingsHRIDHandlingCreateEditDelete.gui,
+      ]).then((userProperties) => {
+        testData.user = userProperties;
+        cy.login(testData.user.username, testData.user.password);
+      });
     });
-  });
 
-  after('Delete test data', () => {
-    Users.deleteViaApi(testData.user.userId);
-  });
+    after('Delete test data', () => {
+      Users.deleteViaApi(testData.user.userId);
+    });
 
-  it(
-    'C369055 Verify created/updated by widget on HRID Settings page (folijet) (TaaS)',
-    { tags: [TestTypes.extendedPath, DevTeams.folijet] },
-    () => {
-      cy.visit(SettingsMenu.hridHandlingPath);
-      HridHandling.waitloading();
-      HridHandling.recordLastUpdatedAccourdionLabelValueCorrect();
-      HridHandling.clickRecordLastUpdatedAccordion();
-      HridHandling.verifyValueInRecordDetailsSection('Unknown user');
-      // wait, because next steps can be failed without it
-      cy.wait(2000);
-      // uncheck first, since this checkbox can be checked by default
-      HridHandling.uncheckRemoveLeadingZeroesIfCheckedAndSave();
-      HridHandling.checkRemoveLeadingZeroesAndSave();
-      let date = DateTools.getFormattedDateWithTime().replace(',', '');
-      HridHandling.verifyValueInRecordDetailsSection(testData.user.username);
-      HridHandling.verifyValueInRecordDetailsSection(date);
-      HridHandling.uncheckRemoveLeadingZeroesAndSave();
-      date = DateTools.getFormattedDateWithTime().replace(',', '');
-      HridHandling.verifyValueInRecordDetailsSection(testData.user.username);
-      HridHandling.verifyValueInRecordDetailsSection(date);
-    },
-  );
+    it(
+      'C369055 Verify created/updated by widget on HRID Settings page (folijet) (TaaS)',
+      { tags: [TestTypes.extendedPath, DevTeams.folijet] },
+      () => {
+        cy.visit(SettingsMenu.hridHandlingPath);
+        HridHandling.waitloading();
+        HridHandling.recordLastUpdatedAccourdionLabelValueCorrect();
+        HridHandling.clickRecordLastUpdatedAccordion();
+        HridHandling.verifyValueInRecordDetailsSection('Unknown user');
+        // wait, because next steps can be failed without it
+        cy.wait(2000);
+        // uncheck first, since this checkbox can be checked by default
+        HridHandling.uncheckRemoveLeadingZeroesIfCheckedAndSave();
+        HridHandling.checkRemoveLeadingZeroesAndSave();
+        let date = DateTools.getFormattedDateWithTime().replace(',', '');
+        HridHandling.verifyValueInRecordDetailsSection(testData.user.username);
+        HridHandling.verifyValueInRecordDetailsSection(date);
+        HridHandling.uncheckRemoveLeadingZeroesAndSave();
+        date = DateTools.getFormattedDateWithTime().replace(',', '');
+        HridHandling.verifyValueInRecordDetailsSection(testData.user.username);
+        HridHandling.verifyValueInRecordDetailsSection(date);
+      },
+    );
+  });
 });
