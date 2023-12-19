8547e4b8ca1f1816ec26fdd430646f3503ef4a44
Daniil Novikau <novikaud@gmail.com>
2023-12-19 11:08:55 +0300

    [422161] Add donor information to a Fund

---
+++ cypress/e2e/finance/funds/add-donor-information-to-fund.cy.js
@@ -0,0 +1,101 @@
+import { Permissions } from '../../../support/dictionary';
+import { FiscalYears, Funds, Ledgers } from '../../../support/fragments/finance';
+import { NewOrganization, Organizations } from '../../../support/fragments/organizations';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+
+describe('Finance', () => {
+  describe('Funds', () => {
+    const fiscalYear = FiscalYears.getDefaultFiscalYear();
+    const ledger = { ...Ledgers.getDefaultLedger(), fiscalYearOneId: fiscalYear.id };
+    const testData = {
+      organization: NewOrganization.getDefaultOrganization({
+        isDonor: true,
+        isVendor: false,
+      }),
+      fiscalYear,
+      ledger,
+      fund: { ...Funds.getDefaultFund(), ledgerId: ledger.id },
+      user: {},
+    };
+
+    before('Create test data', () => {
+      cy.getAdminToken().then(() => {
+        Organizations.createOrganizationViaApi(testData.organization);
+        FiscalYears.createViaApi(testData.fiscalYear);
+        Ledgers.createViaApi(testData.ledger);
+      });
+
+      cy.createTempUser([
+        Permissions.uiFinanceViewEditCreateFundAndBudget.gui,
+        Permissions.uiOrganizationsView.gui,
+      ]).then((userProperties) => {
+        testData.user = userProperties;
+
+        cy.login(testData.user.username, testData.user.password, {
+          path: TopMenu.fundPath,
+          waiter: Funds.waitLoading,
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Funds.deleteFundsByLedgerIdViaApi(testData.ledger.id);
+        Ledgers.deleteledgerViaApi(testData.ledger.id);
+        FiscalYears.deleteFiscalYearViaApi(testData.fiscalYear.id);
+        Organizations.deleteOrganizationViaApi(testData.organization.id);
+        Users.deleteViaApi(testData.user.userId);
+      });
+    });
+
+    it(
+      'C422161 Add donor information to a new Fund (thunderjet) (TaaS)',
+      { tags: ['criticalPath', 'thunderjet'] },
+      () => {
+        // On "Fund" page of "Finance" app click on "New" button
+        const FundEditForm = Funds.clickCreateNewFundButton();
+
+        // Fill all mandatory fields in "Fund information" accordion
+        FundEditForm.fillFundFields({
+          fundInfo: {
+            ...testData.fund,
+            ledger: testData.ledger.name,
+          },
+        });
+        FundEditForm.checkButtonsConditions([
+          { label: 'Save & close', conditions: { disabled: false } },
+        ]);
+
+        // Expand "Donor information" accordion by clicking on it
+        FundEditForm.expandDonorInformationSection();
+        FundEditForm.checkButtonsConditions([
+          { label: 'Add donor', conditions: { disabled: false } },
+        ]);
+
+        // Click on "Add donor" button
+        const AddDonorsModal = FundEditForm.clickAddDonnorsButton();
+
+        // Check the checkbox next to a donor organization
+        AddDonorsModal.searchByName(testData.organization.name);
+        AddDonorsModal.selectCheckboxFromResultsList([testData.organization.name]);
+
+        // Click "Close" button on "Add donors" modal
+        AddDonorsModal.clickCloseButton();
+        FundEditForm.checkDonorInformationSectionContent();
+
+        // Click on "Add donor" button, Check the checkbox next to a donor organization, Click "Save" button
+        FundEditForm.fillDonorInfoSectionFields({
+          donorName: testData.organization.name,
+          shouldExpand: false,
+        });
+        FundEditForm.checkDonorInformationSectionContent({
+          donors: [{ name: testData.organization.name, code: testData.organization.code }],
+        });
+
+        // Click on "Save & close" button on "Create fund" page
+        FundEditForm.clickSaveAndCloseButton();
+      },
+    );
+  });
+});
