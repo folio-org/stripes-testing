78cef84c69b6818b495bc2358d15e0a397560d8a
Kiryl Shupenich <Kiryl_Shupenich@epam.com>
2023-12-20 12:19:54 +0100

    Add test case 380722

---
+++ cypress/e2e/data-import/settings/review-info-icon-for-acquisitions-units-field-in-field-mapping-profile.cy.js
@@ -0,0 +1,38 @@
+import { Permissions } from '../../../support/dictionary';
+import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
+import NewFieldMappingProfile from '../../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import Users from '../../../support/fragments/users/users';
+
+describe('data-import', () => {
+  describe('Settings', () => {
+    let user;
+
+    before('create user', () => {
+      cy.createTempUser([Permissions.settingsDataImportEnabled.gui]).then((userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password);
+      });
+    });
+
+    after('delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(user.userId);
+      });
+    });
+
+    it(
+      'C380722 Invoice field mapping: review adjusted info icon to the "Acquisitions units" field in the Create page (folijet) (TaaS)',
+      { tags: ['extendedPath', 'folijet'] },
+      () => {
+        const message =
+          'Invoice creation will error unless the importing user is a member of the specified acquisitions unit';
+
+        cy.visit(SettingsMenu.mappingProfilePath);
+        FieldMappingProfiles.openNewMappingProfileForm();
+        NewFieldMappingProfile.addFolioRecordType('Invoice');
+        NewFieldMappingProfile.verifyAcquisitionsUnitsInfoMessage(message);
+      },
+    );
+  });
+});
