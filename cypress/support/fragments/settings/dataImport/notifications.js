66e634d5101589329193f0a3a36c95f2400bd4f2
Daniil Novikau <novikaud@gmail.com>
2023-12-26 13:14:33 +0300

    [376006] Order field mapping profile: Check Contributor type mappings

--- cypress/support/fragments/settings/dataImport/notifications.js
+++ cypress/support/fragments/settings/dataImport/notifications.js
@@ -1,4 +1,6 @@
 export default {
+  actionProfileCreatedSuccessfully: 'The action profile (?:\\S+) was successfully created',
+  jobProfileCreatedSuccessfully: 'The job profile (?:\\S+) was successfully created',
   fieldMappingProfileCreatedSuccessfully:
     'The field mapping profile "(?:\\S+)" was successfully created',
 };
