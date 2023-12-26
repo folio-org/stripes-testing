f2d5e82d00332781f416f485e774789f3241d039
Ilya_Lyskavets <Ilya_Lyskavets@epam.com>
2023-12-26 15:41:26 +0300

    fixes

--- cypress/e2e/settings/tenant/verify-item-status-language-when-locale-is-german.cy.js
+++ cypress/e2e/settings/tenant/verify-item-status-language-when-locale-is-german.cy.js
@@ -1,10 +1,10 @@
 import Helper from '../../../support/fragments/finance/financeHelper';
-import { DevTeams, Permissions, TestTypes } from '../../../support/dictionary';
+import { Permissions } from '../../../support/dictionary';
 import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
 import TenantPane, { TENANTS } from '../../../support/fragments/settings/tenant/tenantPane';
 import Users from '../../../support/fragments/users/users';
 import { randomFourDigitNumber } from '../../../support/utils/stringTools';
-import settingsMenu from '../../../support/fragments/settingsMenu';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
 import { Localization } from '../../../support/fragments/settings/tenant/general';
 import TemporarySessionLocale from '../../../support/fragments/settings/developer/session-locale/temporarySessionLocale';
 import TopMenu from '../../../support/fragments/topMenu';
@@ -14,7 +14,9 @@ import InventoryInstance from '../../../support/fragments/inventory/inventoryIns
 describe('Settings: Tenant', () => {
   const testData = {
     buttonLanguage: 'German (Germany) / Deutsch (Deutschland)',
+    defaultButtonLanguage: 'American English / American English',
     paneHeaderTitle: 'TEMPORÄRES Session-Gebietsschema',
+    defaultPaneHeaderTitle: 'TEMPORARY Session locale',
   };
   const instanceTitle = `C407670 autotestInstance ${randomFourDigitNumber()}`;
   const itemBarcode = Helper.getRandomBarcode();
@@ -37,13 +39,19 @@ describe('Settings: Tenant', () => {
       testData.user = userProperties;
 
       cy.login(testData.user.username, testData.user.password, {
-        path: settingsMenu.tenantPath,
+        path: SettingsMenu.tenantPath,
         waiter: TenantPane.waitLoading,
       });
     });
   });
 
   after('Delete test data', () => {
+    cy.visit(SettingsMenu.tenantPath);
+    TenantPane.selectTenant(TENANTS.LANGUAGE_AND_LOCALIZATION);
+    Localization.clickChangeSessionLocalLanguage();
+    TemporarySessionLocale.waitLoading();
+    TemporarySessionLocale.selectCountry(testData.defaultButtonLanguage);
+    TemporarySessionLocale.verifyTitleOfPaneHeader(testData.defaultPaneHeaderTitle);
     cy.getAdminToken().then(() => {
       Users.deleteViaApi(testData.user.userId);
       InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(itemBarcode);
@@ -52,7 +60,7 @@ describe('Settings: Tenant', () => {
 
   it(
     'C407670 Verify the item status language in the instances item list when session locale is "German" (folijet) (TaaS)',
-    { tags: [TestTypes.extendedPath, DevTeams.folijet] },
+    { tags: ['extendedPath', 'folijet', 'nonParallel'] },
     () => {
       TenantPane.selectTenant(TENANTS.LANGUAGE_AND_LOCALIZATION);
       Localization.clickChangeSessionLocalLanguage();
