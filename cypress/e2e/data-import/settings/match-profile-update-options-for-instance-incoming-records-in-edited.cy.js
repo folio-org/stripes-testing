bd47a6c44fdb4f23e69014483f8c7ec9591f1be5
Yuri_Karpov <Yuri_Karpov@epam.com>
2023-12-27 11:21:36 +0400

    Moved login to before step

--- cypress/e2e/data-import/settings/match-profile-update-options-for-instance-incoming-records-in-edited.cy.js
+++ cypress/e2e/data-import/settings/match-profile-update-options-for-instance-incoming-records-in-edited.cy.js
@@ -37,6 +37,10 @@ describe('data-import', () => {
 
       cy.createTempUser([Permissions.settingsDataImportEnabled.gui]).then((userProperties) => {
         user = userProperties;
+        cy.login(user.username, user.password, {
+          path: TopMenu.settingsPath,
+          waiter: SettingsPane.waitLoading,
+        });
       });
     });
 
@@ -52,10 +56,6 @@ describe('data-import', () => {
       { tags: ['extendedPath', 'folijet'] },
       () => {
         // Go to "Settings" application-> "Data import" section-> "Match profiles" section
-        cy.login(user.username, user.password, {
-          path: TopMenu.settingsPath,
-          waiter: SettingsPane.waitLoading,
-        });
         SettingsDataImport.goToSettingsDataImport();
         DataImport.selectDataImportProfile('Match profiles');
 
