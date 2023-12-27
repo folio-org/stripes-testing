e9631d4d2f027d06816c128cd2b843875fbff4d5
Ilya_Lyskavets <Ilya_Lyskavets@epam.com>
2023-12-27 11:27:21 +0300

    test created

--- cypress/support/fragments/settings/tenant/general/addresses.js
+++ cypress/support/fragments/settings/tenant/general/addresses.js
@@ -2,6 +2,8 @@ import { HTML, including, Link } from '@interactors/html';
 
 import {
   Button,
+  Callout,
+  Modal,
   MultiColumnListCell,
   MultiColumnListRow,
   Pane,
@@ -11,6 +13,11 @@ import LocationDetails from '../locations/locationDetails';
 import { randomFourDigitNumber } from '../../../../utils/stringTools';
 
 const addButton = Button('New');
+const deleteAddressButton = Button({ id: including('clickable-delete-addresses') });
+const deleteAddressModal = Modal('Delete address');
+const cancelButtonInDeleteAdressModal = deleteAddressModal.find(Button('Cancel'));
+const deleteButtonInDeleteAdressModal = deleteAddressModal.find(Button('Delete'));
+const deleteCalloutMessage = ['The address', 'was successfully', 'deleted'];
 
 export default {
   ...SettingsPane,
@@ -54,5 +61,27 @@ export default {
       path: `configurations/entries/${addressId}`,
       method: 'DELETE',
     });
+  },
+  clickDeleteButtonForAddressValue(addressValue) {
+    cy.do(MultiColumnListRow(including(addressValue)).find(deleteAddressButton).click());
+  },
+  verifyDeleteModalDisplayed() {
+    cy.expect(deleteAddressModal.exists());
+    cy.expect(cancelButtonInDeleteAdressModal.has({ disabled: false }));
+    cy.expect(deleteButtonInDeleteAdressModal.has({ disabled: false }));
+  },
+  verifyDeleteModalIsNotDisplayed() {
+    cy.expect(deleteAddressModal.absent());
+  },
+  clickDeleteButtonInDeleteModal() {
+    cy.do(deleteButtonInDeleteAdressModal.click());
+  },
+  verifyCalloutForAddressDeletionAppears() {
+    cy.expect(Callout({ textContent: including(deleteCalloutMessage[0]) }).exists());
+    cy.expect(Callout({ textContent: including(deleteCalloutMessage[1]) }).exists());
+    cy.expect(Callout({ textContent: including(deleteCalloutMessage[2]) }).exists());
+  },
+  addressRowWithValueIsAbsent(addressValue) {
+    cy.expect(MultiColumnListRow(including(addressValue)).absent());
   },
 };
