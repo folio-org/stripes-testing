bee332c82769c5c12f4451d56ab1c08119758a76
Kiryl Shupenich <Kiryl_Shupenich@epam.com>
2023-12-29 12:05:54 +0100

    Case '369052' moved in file with simikar tests. Separate file with test is deleted.

--- cypress/e2e/data-import/settings/review-info-icons-on-create-page-in-field-mapping-profile.cy.js
+++ cypress/e2e/data-import/settings/review-info-icons-on-create-page-in-field-mapping-profile.cy.js
@@ -1,10 +1,10 @@
-import { FOLIO_RECORD_TYPE } from '../../../support/constants';
 import { Permissions } from '../../../support/dictionary';
 import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
 import NewFieldMappingProfile from '../../../support/fragments/data_import/mapping_profiles/newFieldMappingProfile';
 import SettingsMenu from '../../../support/fragments/settingsMenu';
 import Users from '../../../support/fragments/users/users';
 import getRandomStringCode from '../../../support/utils/genereteTextCode';
+import { FOLIO_RECORD_TYPE } from '../../../support/constants';
 
 describe('data-import', () => {
   describe('Settings', () => {
@@ -27,6 +27,34 @@ describe('data-import', () => {
         Users.deleteViaApi(user.userId);
       });
     });
+
+    it(
+      'C380720 Order field mapping: review adjusted info icon to the "Acquisitions units" field in the Create page (folijet) (TaaS)',
+      { tags: ['extendedPath', 'folijet'] },
+      () => {
+        const message =
+          'Order creation will error unless the importing user is a member of the specified acquisitions unit';
+
+        cy.visit(SettingsMenu.mappingProfilePath);
+        FieldMappingProfiles.openNewMappingProfileForm();
+        NewFieldMappingProfile.addFolioRecordType('Order');
+        NewFieldMappingProfile.verifyAcquisitionsUnitsInfoMessage(message);
+      },
+    );
+
+    it(
+      'C380722 Invoice field mapping: review adjusted info icon to the "Acquisitions units" field in the Create page (folijet) (TaaS)',
+      { tags: ['extendedPath', 'folijet'] },
+      () => {
+        const message =
+          'Invoice creation will error unless the importing user is a member of the specified acquisitions unit';
+
+        cy.visit(SettingsMenu.mappingProfilePath);
+        FieldMappingProfiles.openNewMappingProfileForm();
+        NewFieldMappingProfile.addFolioRecordType('Invoice');
+        NewFieldMappingProfile.verifyAcquisitionsUnitsInfoMessage(message);
+      },
+    );
 
     it(
       'C369052 Field mapping profile: Check info icons when creating field mapping profile for holdings (folijet) (TaaS)',
