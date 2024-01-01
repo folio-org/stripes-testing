70bee4e219bc8227072f8f2ee58261f0e62edc4b
TatianaOne <tapusja2014@gmail.com>
2024-01-01 14:25:10 +0200

    added test

---
+++ cypress/e2e/data-import/log-details/log-details-for-no-action-order-records.cy.js
@@ -0,0 +1,166 @@
+import {
+  ACQUISITION_METHOD_NAMES,
+  FOLIO_RECORD_TYPE,
+  JOB_STATUS_NAMES,
+  MATERIAL_TYPE_NAMES,
+  ORDER_FORMAT_NAMES_IN_PROFILE,
+  ORDER_STATUSES,
+  VENDOR_NAMES,
+} from '../../../support/constants';
+import { Permissions } from '../../../support/dictionary';
+import ActionProfiles from '../../../support/fragments/data_import/action_profiles/actionProfiles';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+import NewJobProfile from '../../../support/fragments/data_import/job_profiles/newJobProfile';
+import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
+import Logs from '../../../support/fragments/data_import/logs/logs';
+import FieldMappingProfileView from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfileView';
+import FieldMappingProfiles from '../../../support/fragments/data_import/mapping_profiles/fieldMappingProfiles';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import JsonScreenView from '../../../support/fragments/data_import/logs/jsonScreenView';
+
+describe('data-import', () => {
+  describe('Log details', () => {
+    let user;
+    const ordersData = [
+      { title: 'CULTURAL HISTORY OF IDEAS', rowNumber: 0 },
+      { title: 'BOAT PEOPLE; TRANS. BY VANESSA PERE-ROSARIO.', rowNumber: 1 },
+      {
+        title:
+          "GREAT BOOK OF KING ARTHUR AND HIS KNIGHTS OF THE ROUND TABLE : A NEW MORTE D'ARTHUR.",
+        rowNumber: 2,
+      },
+      { title: 'JAN MORRIS : LIFE FROM BOTH SIDES : A BIOGRAPHY.', rowNumber: 3 },
+      { title: 'CHRISTINA FERNANDEZ : MULTIPLE EXPOSURES', rowNumber: 4 },
+      { title: 'HOBBIT : AN ILLUSTRATED EDITION OF THE FANTASY CLASSIC.', rowNumber: 5 },
+    ];
+    const filePathForCreateOrder = 'marcBibFileForC375179.mrc';
+    const marcFileName = `C375179 autotestFileName ${getRandomPostfix()}`;
+    const mappingProfile = {
+      name: `C375179 Test Order ${getRandomPostfix()}`,
+      typeValue: FOLIO_RECORD_TYPE.ORDER,
+      orderStatus: ORDER_STATUSES.PENDING,
+      approved: true,
+      vendor: VENDOR_NAMES.GOBI,
+      reEncumber: 'false',
+      title: '245$a',
+      mustAcknowledgeReceivingNote: 'false',
+      publicationDate: '264$c; else 260$c',
+      publisher: '264$b; else 260$b',
+      edition: '250$a',
+      internalNote: '981$d',
+      acquisitionMethod: ACQUISITION_METHOD_NAMES.PURCHASE_AT_VENDOR_SYSTEM,
+      orderFormat: ORDER_FORMAT_NAMES_IN_PROFILE.PHYSICAL_RESOURCE,
+      receiptStatus: 'Pending',
+      paymentStatus: 'Pending',
+      selector: '981$e',
+      cancellationRestriction: 'false',
+      rush: '981$h',
+      receivingWorkflow: 'Synchronized',
+      accountNumber: '981$g',
+      physicalUnitPrice: '980$b',
+      quantityPhysical: '980$g',
+      currency: 'USD',
+      materialSupplier: VENDOR_NAMES.GOBI,
+      createInventory: 'Instance, Holding, Item',
+      materialType: MATERIAL_TYPE_NAMES.BOOK,
+      contributor: '100$a',
+      contributorType: 'Personal name',
+      productId: '020$a',
+      qualifier: '020$q',
+      productIDType: 'ISBN',
+      vendorReferenceNumber: '980$f',
+      vendorReferenceType: 'Vendor order reference number',
+      fundId: '049$a',
+      expenseClass: '981$c',
+      value: '100',
+      type: '%',
+      locationName: '049$a',
+      locationQuantityPhysical: '980$g',
+      volume: '993$a',
+    };
+    const actionProfile = {
+      typeValue: FOLIO_RECORD_TYPE.ORDER,
+      name: `C375179 Test Order ${getRandomPostfix()}`,
+    };
+    const jobProfile = {
+      ...NewJobProfile.defaultJobProfile,
+      profileName: `C375179 Test Order ${getRandomPostfix()}`,
+    };
+
+    before('create test data and login', () => {
+      cy.loginAsAdmin({
+        path: SettingsMenu.mappingProfilePath,
+        waiter: FieldMappingProfiles.waitLoading,
+      });
+      // create mapping profile
+      FieldMappingProfiles.createOrderMappingProfile(mappingProfile);
+      FieldMappingProfiles.checkMappingProfilePresented(mappingProfile.name);
+
+      // create action profile
+      cy.visit(SettingsMenu.actionProfilePath);
+      ActionProfiles.create(actionProfile, mappingProfile.name);
+      ActionProfiles.checkActionProfilePresented(actionProfile.name);
+
+      // create job profile
+      cy.visit(SettingsMenu.jobProfilePath);
+      JobProfiles.createJobProfile(jobProfile);
+      NewJobProfile.linkActionProfile(actionProfile);
+      NewJobProfile.saveAndClose();
+      JobProfiles.checkJobProfilePresented(jobProfile.profileName);
+
+      cy.createTempUser([
+        Permissions.settingsDataImportEnabled.gui,
+        Permissions.moduleDataImportEnabled.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+
+        cy.login(userProperties.username, userProperties.password, {
+          path: TopMenu.dataImportPath,
+          waiter: DataImport.waitLoading,
+        });
+      });
+    });
+
+    after('delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(user.userId);
+        JobProfiles.deleteJobProfile(jobProfile.profileName);
+        ActionProfiles.deleteActionProfile(actionProfile.name);
+        FieldMappingProfileView.deleteViaApi(mappingProfile.name);
+      });
+    });
+
+    it(
+      'C375179 Verify the log details for no action order records (folijet) (TaaS)',
+      { tags: ['extendedPath', 'folijet'] },
+      () => {
+        // TODO delete function after fix https://issues.folio.org/browse/MODDATAIMP-691
+        DataImport.verifyUploadState();
+        DataImport.uploadFile(filePathForCreateOrder, marcFileName);
+        JobProfiles.waitFileIsUploaded();
+        JobProfiles.search(jobProfile.profileName);
+        JobProfiles.runImportFile();
+        JobProfiles.waitFileIsImported(marcFileName);
+        Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED_WITH_ERRORS);
+        Logs.openFileDetails(marcFileName);
+        FileDetails.checkOrderQuantityInSummaryTable('6', 2);
+        FileDetails.verifyRecordColumnHasStandardSequentialNumberingForRecords();
+        cy.wrap(ordersData).each((order) => {
+          FileDetails.verifyTitle(
+            order.title,
+            FileDetails.columnNameInResultList.title,
+            order.rowNumber,
+          );
+        });
+        FileDetails.openJsonScreen(ordersData[0].title);
+        JsonScreenView.verifyJsonScreenIsOpened();
+        JsonScreenView.openOrderTab();
+        // JsonScreenView.verifyContentInTab('No action');
+      },
+    );
+  });
+});
