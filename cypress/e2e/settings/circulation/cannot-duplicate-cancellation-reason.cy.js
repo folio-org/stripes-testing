40a03cfcd3a5232060193279d650e77db4129238
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2024-01-04 10:25:15 +0200

    FAT-9619 FAT-9620

---
+++ cypress/e2e/settings/circulation/cannot-duplicate-cancellation-reason.cy.js
@@ -0,0 +1,56 @@
+import uuid from 'uuid';
+import Permissions from '../../../support/dictionary/permissions';
+import CancellationReason from '../../../support/fragments/settings/circulation/cancellationReason';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import Users from '../../../support/fragments/users/users';
+
+describe('Cancel item level request', () => {
+  const userData = {};
+  const cancellationReason = {
+    id: uuid(),
+    name: uuid(),
+    description: 'description',
+    publicDescription: 'publicDescription',
+  };
+
+  before('Prepare test data', () => {
+    cy.getAdminToken()
+      .then(() => {
+        cy.addCancellationReasonApi(cancellationReason);
+      })
+      .then(() => {
+        cy.createTempUser([Permissions.settingsCircView.gui])
+          .then((userProperties) => {
+            userData.username = userProperties.username;
+            userData.password = userProperties.password;
+            userData.userId = userProperties.userId;
+            userData.barcode = userProperties.barcode;
+            userData.firstName = userProperties.firstName;
+            userData.patronGroup = userProperties.patronGroup;
+          })
+          .then(() => {
+            cy.login(userData.username, userData.password, {
+              path: SettingsMenu.circulationRequestCancellationReasonsPath,
+              waiter: CancellationReason.waitLoading,
+            });
+          });
+      });
+  });
+
+  after('Delete test data', () => {
+    cy.getAdminToken();
+    cy.deleteCancellationReasonApi(cancellationReason.id);
+    Users.deleteViaApi(userData.userId);
+  });
+
+  it(
+    'C359160 Check that user can not create duplicate "Cancellation reasons" (vega)',
+    { tags: ['extendedPath', 'vega'] },
+    () => {
+      CancellationReason.clickNewButton();
+      CancellationReason.setCancellationReasonName(cancellationReason.name);
+      CancellationReason.saveCancellationReason();
+      CancellationReason.verifyErrorMessage('Error on saving data');
+    },
+  );
+});
