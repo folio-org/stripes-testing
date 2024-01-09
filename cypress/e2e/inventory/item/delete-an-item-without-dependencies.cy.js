92648ad21b9d89c2885a31100bb6d51b74e800ba
arsenhovh <arsen_hovhannisyan@epam.com>
2024-01-09 16:54:04 +0400

    minor fix

--- cypress/e2e/inventory/item/delete-an-item-without-dependencies.cy.js
+++ cypress/e2e/inventory/item/delete-an-item-without-dependencies.cy.js
@@ -10,85 +10,87 @@ import ItemRecordView from '../../../support/fragments/inventory/item/itemRecord
 import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
 import ConfirmDeleteItemModal from '../../../support/fragments/inventory/modals/confirmDeleteItemModal';
 
-describe('Find requester via user barcode', () => {
-  const servicePoint = ServicePoints.getDefaultServicePointWithPickUpLocation();
-  const itemData = {
-    barcode: generateItemBarcode(),
-    instanceTitle: `Instance ${getRandomPostfix()}`,
-  };
-  let defaultLocation;
+describe('Inventory', () => {
+  describe('Item', () => {
+    const servicePoint = ServicePoints.getDefaultServicePointWithPickUpLocation();
+    const itemData = {
+      barcode: generateItemBarcode(),
+      instanceTitle: `Instance ${getRandomPostfix()}`,
+    };
+    let defaultLocation;
 
-  before('Create test data', () => {
-    cy.getAdminToken()
-      .then(() => {
-        ServicePoints.createViaApi(servicePoint);
-        defaultLocation = Location.getDefaultLocation(servicePoint.id);
-        Location.createViaApi(defaultLocation);
-        cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
-          itemData.instanceTypeId = instanceTypes[0].id;
-        });
-        cy.getHoldingTypes({ limit: 1 }).then((res) => {
-          itemData.holdingTypeId = res[0].id;
-        });
-        cy.getLoanTypes({ limit: 1 }).then((res) => {
-          itemData.loanTypeId = res[0].id;
-        });
-        cy.getMaterialTypes({ limit: 1 }).then((res) => {
-          itemData.materialTypeId = res.id;
-          itemData.materialTypeName = res.name;
-        });
-      })
-      .then(() => {
-        InventoryInstances.createFolioInstanceViaApi({
-          instance: {
-            instanceTypeId: itemData.instanceTypeId,
-            title: itemData.instanceTitle,
-          },
-          holdings: [
-            {
-              holdingsTypeId: itemData.holdingTypeId,
-              permanentLocationId: defaultLocation.id,
+    before('Create test data', () => {
+      cy.getAdminToken()
+        .then(() => {
+          ServicePoints.createViaApi(servicePoint);
+          defaultLocation = Location.getDefaultLocation(servicePoint.id);
+          Location.createViaApi(defaultLocation);
+          cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
+            itemData.instanceTypeId = instanceTypes[0].id;
+          });
+          cy.getHoldingTypes({ limit: 1 }).then((res) => {
+            itemData.holdingTypeId = res[0].id;
+          });
+          cy.getLoanTypes({ limit: 1 }).then((res) => {
+            itemData.loanTypeId = res[0].id;
+          });
+          cy.getMaterialTypes({ limit: 1 }).then((res) => {
+            itemData.materialTypeId = res.id;
+            itemData.materialTypeName = res.name;
+          });
+        })
+        .then(() => {
+          InventoryInstances.createFolioInstanceViaApi({
+            instance: {
+              instanceTypeId: itemData.instanceTypeId,
+              title: itemData.instanceTitle,
             },
-          ],
-          items: [
-            {
-              barcode: itemData.barcode,
-              status: { name: ITEM_STATUS_NAMES.AVAILABLE },
-              permanentLoanType: { id: itemData.loanTypeId },
-              materialType: { id: itemData.materialTypeId },
-            },
-          ],
+            holdings: [
+              {
+                holdingsTypeId: itemData.holdingTypeId,
+                permanentLocationId: defaultLocation.id,
+              },
+            ],
+            items: [
+              {
+                barcode: itemData.barcode,
+                status: { name: ITEM_STATUS_NAMES.AVAILABLE },
+                permanentLoanType: { id: itemData.loanTypeId },
+                materialType: { id: itemData.materialTypeId },
+              },
+            ],
+          });
+        })
+        .then((specialInstanceIds) => {
+          itemData.testInstanceIds = specialInstanceIds;
+        })
+        .then(() => {
+          cy.loginAsAdmin({
+            path: TopMenu.inventoryPath,
+            waiter: InventoryInstances.waitContentLoading,
+          });
         });
-      })
-      .then((specialInstanceIds) => {
-        itemData.testInstanceIds = specialInstanceIds;
-      })
-      .then(() => {
-        cy.loginAsAdmin({
-          path: TopMenu.inventoryPath,
-          waiter: InventoryInstances.waitContentLoading,
-        });
-      });
-  });
-
-  after('Delete test data', () => {
-    cy.getAdminToken().then(() => {
-      InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(
-        itemData.testInstanceIds.instanceId,
-      );
     });
-  });
 
-  it(
-    'C715 Delete an item without dependencies (folijet) (TaaS)',
-    { tags: ['extendedPath', 'folijet'] },
-    () => {
-      InventorySearchAndFilter.switchToItem();
-      InventorySearchAndFilter.searchByParameter('Barcode', itemData.barcode);
-      ItemRecordView.waitLoading();
-      ItemRecordView.clickDeleteButton();
-      ConfirmDeleteItemModal.clickDeleteButton();
-      InventoryInstance.verifyItemBarcode(itemData.barcode, false);
-    },
-  );
+    after('Delete test data', () => {
+      cy.getAdminToken().then(() => {
+        InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(
+          itemData.testInstanceIds.instanceId,
+        );
+      });
+    });
+
+    it(
+      'C715 Delete an item without dependencies (folijet) (TaaS)',
+      { tags: ['extendedPath', 'folijet'] },
+      () => {
+        InventorySearchAndFilter.switchToItem();
+        InventorySearchAndFilter.searchByParameter('Barcode', itemData.barcode);
+        ItemRecordView.waitLoading();
+        ItemRecordView.clickDeleteButton();
+        ConfirmDeleteItemModal.clickDeleteButton();
+        InventoryInstance.verifyItemBarcode(itemData.barcode, false);
+      },
+    );
+  });
 });
