08b0c0f520fe0b4960c0718d7fcddb11e605629a
IhorBohdan <Ihor_Bohdan@epam.com>
2024-01-11 00:57:06 +0200

    Implementation of FAT-10196

--- cypress/e2e/consortia/consortium-manager/consortium-manager-option-is-displayed-in-settings-only-for-central-tenant.cy.js
+++ cypress/e2e/consortia/consortium-manager/consortium-manager-option-is-displayed-in-settings-only-for-central-tenant.cy.js
@@ -1,40 +1,16 @@
 import Permissions from '../../../support/dictionary/permissions';
 import Affiliations, { tenantNames } from '../../../support/dictionary/affiliations';
 import Users from '../../../support/fragments/users/users';
-import TopMenu from '../../../support/fragments/topMenu';
 import ConsortiumManager from '../../../support/fragments/settings/consortium-manager/consortium-manager';
-import getRandomPostfix, { getTestEntityValue } from '../../../support/utils/stringTools';
-import UsersSearchPane from '../../../support/fragments/users/usersSearchPane';
-import UsersCard from '../../../support/fragments/users/usersCard';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
 
 describe('Consortia -> Consortium manager', () => {
-  const createUserData = () => ({
-    username: getTestEntityValue('username'),
-    barcode: getRandomPostfix(),
-    personal: {
-      firstName: getTestEntityValue('firstname'),
-      preferredFirstName: getTestEntityValue('prefname'),
-      middleName: getTestEntityValue('midname'),
-      lastName: getTestEntityValue('lastname'),
-      email: 'test@folio.org',
-    },
-    patronGroup: 'undergrad (Undergraduate Student)',
-    userType: 'Staff',
-  });
-  const testUser = createUserData();
-
   let user;
 
   before('Create users, data', () => {
     cy.getAdminToken();
 
-    cy.createTempUser([
-      Permissions.consortiaSettingsConsortiaAffiliationsEdit.gui,
-      Permissions.uiUsersPermissions.gui,
-      Permissions.uiUsersCreate.gui,
-      Permissions.uiUsersPermissionsView.gui,
-      Permissions.uiUsersView.gui,
-    ])
+    cy.createTempUser([Permissions.consortiaSettingsSettingsMembershipEdit.gui])
       .then((userProperties) => {
         user = userProperties;
       })
@@ -42,17 +18,13 @@ describe('Consortia -> Consortium manager', () => {
         cy.assignAffiliationToUser(Affiliations.College, user.userId);
         cy.setTenant(Affiliations.College);
         cy.assignPermissionsToExistingUser(user.userId, [
-          Permissions.consortiaSettingsConsortiaAffiliationsEdit.gui,
-          Permissions.uiUsersPermissions.gui,
-          Permissions.uiUsersCreate.gui,
-          Permissions.uiUsersPermissionsView.gui,
-          Permissions.uiUsersView.gui,
+          Permissions.consortiaSettingsSettingsMembershipEdit.gui,
+          Permissions.consortiaSettingsSettingsMembershipView.gui,
         ]);
         cy.login(user.username, user.password, {
-          path: TopMenu.usersPath,
-          waiter: Users.waitLoading,
+          path: SettingsMenu.consortiumManagerPath,
+          waiter: ConsortiumManager.waitLoading,
         });
-        ConsortiumManager.switchActiveAffiliation(tenantNames.college);
       });
   });
 
@@ -60,22 +32,17 @@ describe('Consortia -> Consortium manager', () => {
     cy.resetTenant();
     cy.getAdminToken();
     Users.deleteViaApi(user.userId);
-    Users.deleteViaApi(testUser.id);
   });
 
   it(
     'C386869: "Consortium manager" option is displayed in "Settings" only for Central Tenant (consortia)(thunderjet)',
     { tags: ['criticalPath', 'thunderjet'] },
     () => {
-      Users.createViaUi(testUser).then((id) => {
-        testUser.id = id;
-      });
-      ConsortiumManager.switchActiveAffiliation(tenantNames.central);
-      UsersSearchPane.searchByUsername(testUser.username);
-      Users.verifyUserDetailsPane();
-      UsersCard.verifyAffiliationsQuantity('2');
-      UsersCard.expandAffiliationsAccordion();
-      UsersCard.verifyAffiliationsDetails('College', 2, 'Central Office');
+      ConsortiumManager.varifyConsortiumManagerOnPage();
+      ConsortiumManager.switchActiveAffiliationExists();
+      ConsortiumManager.switchActiveAffiliation(tenantNames.college);
+      ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.college);
+      ConsortiumManager.varifyConsortiumManagerIsAbsent();
     },
   );
 });
