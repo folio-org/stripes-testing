c8e881e0a9c3d0e73ea267dd8954100be8af9fe6
Tetiana_Paranich <tetiana_paranich@epam.com>
2024-01-17 17:23:02 +0200

    moved folders

--- cypress/e2e/consortia/inventory/instance/holding-actions-menu-on-central-tenant-for-member-librarys-holdings-record.cy.js
+++ cypress/e2e/consortia/inventory/instance/holding-actions-menu-on-central-tenant-for-member-librarys-holdings-record.cy.js
@@ -11,75 +11,77 @@ import TopMenu from '../../../../support/fragments/topMenu';
 import Users from '../../../../support/fragments/users/users';
 import getRandomPostfix from '../../../../support/utils/stringTools';
 
-describe('Inventory -> Instance -> Consortia', () => {
-  let user;
-  const testData = {
-    newInstanceTitle: `C411384 instanceTitle${getRandomPostfix()}`,
-    servicePoint: ServicePoints.defaultServicePoint,
-  };
+describe('Inventory', () => {
+  describe('Instance', () => {
+    let user;
+    const testData = {
+      newInstanceTitle: `C411384 instanceTitle${getRandomPostfix()}`,
+      servicePoint: ServicePoints.defaultServicePoint,
+    };
 
-  before('Create test data', () => {
-    cy.getAdminToken();
-    InventoryInstance.createInstanceViaApi()
-      .then(({ instanceData }) => {
-        testData.instance = instanceData;
-      })
-      .then(() => {
-        cy.setTenant(Affiliations.College);
-        ServicePoints.createViaApi(testData.servicePoint).then(() => {
-          testData.location = Locations.getDefaultLocation({
-            servicePointId: testData.servicePoint.id,
-          }).location;
+    before('Create test data', () => {
+      cy.getAdminToken();
+      InventoryInstance.createInstanceViaApi()
+        .then(({ instanceData }) => {
+          testData.instance = instanceData;
+        })
+        .then(() => {
+          cy.setTenant(Affiliations.College);
+          ServicePoints.createViaApi(testData.servicePoint).then(() => {
+            testData.location = Locations.getDefaultLocation({
+              servicePointId: testData.servicePoint.id,
+            }).location;
 
-          Locations.createViaApi(testData.location).then(() => {
-            InventoryHoldings.getHoldingsFolioSource().then((folioSource) => {
-              InventoryHoldings.createHoldingRecordViaApi({
-                instanceId: testData.instance.instanceId,
-                permanentLocationId: testData.location.id,
-                sourceId: folioSource.id,
-              }).then(({ id: holdingId }) => {
-                testData.instance.holdingId = holdingId;
+            Locations.createViaApi(testData.location).then(() => {
+              InventoryHoldings.getHoldingsFolioSource().then((folioSource) => {
+                InventoryHoldings.createHoldingRecordViaApi({
+                  instanceId: testData.instance.instanceId,
+                  permanentLocationId: testData.location.id,
+                  sourceId: folioSource.id,
+                }).then(({ id: holdingId }) => {
+                  testData.instance.holdingId = holdingId;
+                });
               });
             });
           });
         });
-      });
-    cy.resetTenant();
-    cy.createTempUser([Permissions.inventoryAll.gui]).then((userProperties) => {
-      user = userProperties;
+      cy.resetTenant();
+      cy.createTempUser([Permissions.inventoryAll.gui]).then((userProperties) => {
+        user = userProperties;
 
-      cy.assignAffiliationToUser(Affiliations.College, user.userId);
-      cy.setTenant(Affiliations.College);
-      cy.assignPermissionsToExistingUser(user.userId, [Permissions.inventoryAll.gui]).then(() => {
-        cy.login(user.username, user.password, {
-          path: TopMenu.inventoryPath,
-          waiter: InventoryInstances.waitContentLoading,
+        cy.assignAffiliationToUser(Affiliations.College, user.userId);
+        cy.setTenant(Affiliations.College);
+        cy.assignPermissionsToExistingUser(user.userId, [Permissions.inventoryAll.gui]).then(() => {
+          cy.login(user.username, user.password, {
+            path: TopMenu.inventoryPath,
+            waiter: InventoryInstances.waitContentLoading,
+          });
         });
       });
     });
-  });
 
-  after('Delete test data', () => {
-    cy.resetTenant();
-    cy.getAdminToken();
-    InventoryHoldings.deleteHoldingRecordViaApi(testData.instance.holdingId);
-    InventoryInstance.deleteInstanceViaApi(testData.instance.instanceId);
-    Locations.deleteViaApi(testData.location);
-    ServicePoints.deleteViaApi(testData.servicePoint.id);
-    Users.deleteViaApi(user.userId);
-  });
+    after('Delete test data', () => {
+      cy.resetTenant();
+      cy.getAdminToken();
+      InventoryHoldings.deleteHoldingRecordViaApi(testData.instance.holdingId);
+      InventoryInstance.deleteInstanceViaApi(testData.instance.instanceId);
+      Locations.deleteViaApi(testData.location);
+      ServicePoints.deleteViaApi(testData.servicePoint.id);
+      Users.deleteViaApi(user.userId);
+    });
 
-  it(
-    'C411384 (CONSORTIA) Check Holdings "Actions" menu on Central tenant for a member librarys holdings record',
-    { tags: ['criticalPathECS', 'folijet'] },
-    () => {
-      InventoryInstance.searchByTitle(testData.instance.instanceTitle);
-      InventoryInstances.selectInstance();
-      InventoryInstance.expandConsortiaHoldings();
-      InventoryInstance.expandMemberHoldings(tenantNames.college);
-      InventoryInstance.openHoldingView();
-      ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.college);
-      HoldingsRecordView.checkActionsMenuOptions();
-    },
-  );
+    it(
+      'C411384 (CONSORTIA) Check Holdings "Actions" menu on Central tenant for a member librarys holdings record',
+      { tags: ['criticalPathECS', 'folijet'] },
+      () => {
+        InventoryInstance.searchByTitle(testData.instance.instanceTitle);
+        InventoryInstances.selectInstance();
+        InventoryInstance.expandConsortiaHoldings();
+        InventoryInstance.expandMemberHoldings(tenantNames.college);
+        InventoryInstance.openHoldingView();
+        ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.college);
+        HoldingsRecordView.checkActionsMenuOptions();
+      },
+    );
+  });
 });
--- cypress/e2e/consortia/inventory/instance/permission-for-editing-shared-instance-on-member-tenant.cy.js
+++ cypress/e2e/consortia/inventory/instance/permission-for-editing-shared-instance-on-member-tenant.cy.js
@@ -11,59 +11,61 @@ import InteractorsTools from '../../../../support/utils/interactorsTools';
 import Affiliations, { tenantNames } from '../../../../support/dictionary/affiliations';
 import ConsortiumManager from '../../../../support/fragments/settings/consortium-manager/consortium-manager';
 
-describe('Inventory -> Instance -> Consortia', () => {
-  let user;
-  const testData = {
-    newInstanceTitle: `C407746 instanceTitle${getRandomPostfix()}`,
-  };
+describe('Inventory', () => {
+  describe('Instance', () => {
+    let user;
+    const testData = {
+      newInstanceTitle: `C407746 instanceTitle${getRandomPostfix()}`,
+    };
 
-  before('Create test data', () => {
-    cy.getAdminToken();
-    InventoryInstance.createInstanceViaApi().then(({ instanceData }) => {
-      testData.instance = instanceData;
+    before('Create test data', () => {
+      cy.getAdminToken();
+      InventoryInstance.createInstanceViaApi().then(({ instanceData }) => {
+        testData.instance = instanceData;
+      });
+
+      cy.createTempUser([Permissions.uiInventoryViewCreateEditInstances.gui]).then(
+        (userProperties) => {
+          user = userProperties;
+
+          cy.assignAffiliationToUser(Affiliations.College, user.userId);
+          cy.setTenant(Affiliations.College);
+          cy.assignPermissionsToExistingUser(user.userId, [
+            Permissions.uiInventoryViewCreateEditInstances.gui,
+          ]);
+
+          cy.login(user.username, user.password, {
+            path: TopMenu.inventoryPath,
+            waiter: InventoryInstances.waitContentLoading,
+          });
+          ConsortiumManager.switchActiveAffiliation(tenantNames.college);
+        },
+      );
     });
 
-    cy.createTempUser([Permissions.uiInventoryViewCreateEditInstances.gui]).then(
-      (userProperties) => {
-        user = userProperties;
+    after('Delete test data', () => {
+      cy.resetTenant();
+      cy.getAdminToken();
+      InventoryInstance.deleteInstanceViaApi(testData.instance.instanceId);
+      Users.deleteViaApi(user.userId);
+    });
 
-        cy.assignAffiliationToUser(Affiliations.College, user.userId);
-        cy.setTenant(Affiliations.College);
-        cy.assignPermissionsToExistingUser(user.userId, [
-          Permissions.uiInventoryViewCreateEditInstances.gui,
-        ]);
-
-        cy.login(user.username, user.password, {
-          path: TopMenu.inventoryPath,
-          waiter: InventoryInstances.waitContentLoading,
-        });
-        ConsortiumManager.switchActiveAffiliation(tenantNames.college);
+    it(
+      'C407751 (CONSORTIA) Verify the permission for editing shared instance on Member tenant (folijet)',
+      { tags: ['smokeECS', 'folijet'] },
+      () => {
+        InventorySearchAndFilter.searchInstanceByTitle(testData.instance.instanceTitle);
+        InstanceRecordView.verifyInstanceRecordViewOpened();
+        InstanceRecordView.edit();
+        InstanceRecordEdit.waitLoading();
+        InstanceRecordEdit.fillResourceTitle(testData.newInstanceTitle);
+        InstanceRecordEdit.saveAndClose();
+        InteractorsTools.checkCalloutMessage(
+          'This shared instance has been saved centrally, and updates to associated member library records are in process. Changes in this copy of the instance may not appear immediately.',
+        );
+        InstanceRecordView.verifyInstanceRecordViewOpened();
+        InstanceRecordView.verifyResourceTitle(testData.newInstanceTitle);
       },
     );
   });
-
-  after('Delete test data', () => {
-    cy.resetTenant();
-    cy.getAdminToken();
-    InventoryInstance.deleteInstanceViaApi(testData.instance.instanceId);
-    Users.deleteViaApi(user.userId);
-  });
-
-  it(
-    'C407751 (CONSORTIA) Verify the permission for editing shared instance on Member tenant (folijet)',
-    { tags: ['smokeECS', 'folijet'] },
-    () => {
-      InventorySearchAndFilter.searchInstanceByTitle(testData.instance.instanceTitle);
-      InstanceRecordView.verifyInstanceRecordViewOpened();
-      InstanceRecordView.edit();
-      InstanceRecordEdit.waitLoading();
-      InstanceRecordEdit.fillResourceTitle(testData.newInstanceTitle);
-      InstanceRecordEdit.saveAndClose();
-      InteractorsTools.checkCalloutMessage(
-        'This shared instance has been saved centrally, and updates to associated member library records are in process. Changes in this copy of the instance may not appear immediately.',
-      );
-      InstanceRecordView.verifyInstanceRecordViewOpened();
-      InstanceRecordView.verifyResourceTitle(testData.newInstanceTitle);
-    },
-  );
 });
