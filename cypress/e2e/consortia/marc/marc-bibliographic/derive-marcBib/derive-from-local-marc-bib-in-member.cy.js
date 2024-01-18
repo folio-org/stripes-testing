b8bab9b8b2b762a32ef97d285aeea8cbc24d4f21
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-01-18 20:48:08 +0500

    FAT-10434: test added

---
+++ cypress/e2e/consortia/marc/marc-bibliographic/derive-marcBib/derive-from-local-marc-bib-in-member.cy.js
@@ -0,0 +1,165 @@
+import Permissions from '../../../../../support/dictionary/permissions';
+import Affiliations, { tenantNames } from '../../../../../support/dictionary/affiliations';
+import Users from '../../../../../support/fragments/users/users';
+import TopMenu from '../../../../../support/fragments/topMenu';
+import InventoryInstances from '../../../../../support/fragments/inventory/inventoryInstances';
+import InventoryInstance from '../../../../../support/fragments/inventory/inventoryInstance';
+import InventoryViewSource from '../../../../../support/fragments/inventory/inventoryViewSource';
+import QuickMarcEditor from '../../../../../support/fragments/quickMarcEditor';
+import ConsortiumManager from '../../../../../support/fragments/settings/consortium-manager/consortium-manager';
+import DataImport from '../../../../../support/fragments/data_import/dataImport';
+import { JOB_STATUS_NAMES } from '../../../../../support/constants';
+import JobProfiles from '../../../../../support/fragments/data_import/job_profiles/jobProfiles';
+import Logs from '../../../../../support/fragments/data_import/logs/logs';
+import getRandomPostfix from '../../../../../support/utils/stringTools';
+import InventorySearchAndFilter from '../../../../../support/fragments/inventory/inventorySearchAndFilter';
+
+describe('MARC', () => {
+  describe('MARC Bibliographic', () => {
+    describe('Derive MARC bib', () => {
+      const testData = {
+        tag245: '245',
+        tag245DerivedContent:
+          '$a C402770 The other side of paradise (derived record): $b a memoir / $c Staceyann Chin.',
+        tag245EditedContent:
+          '$a C402770 The other side of paradise (derived and edited record): $b a memoir / $c Staceyann Chin.',
+        instanceTitle: 'C402770 The other side of paradise : a memoir / Staceyann Chin.',
+        instanceDerivedTitle:
+          'C402770 The other side of paradise (derived record): a memoir / Staceyann Chin.',
+        instanceEditedTitle:
+          'C402770 The other side of paradise (derived and edited record): a memoir / Staceyann Chin.',
+        deriveLocalPaneheaderText: 'Derive a new local MARC bib record',
+        sourceViewLocalText: 'Local MARC bibliographic record',
+      };
+
+      const marcFile = {
+        marc: 'marcBibFileC402770.mrc',
+        fileNameImported: `testMarcFileC402770.${getRandomPostfix()}.mrc`,
+        jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
+      };
+
+      const users = {};
+
+      const createdInstanceIDs = [];
+
+      before('Create user, data', () => {
+        cy.resetTenant();
+        cy.getAdminToken();
+        cy.createTempUser([
+          Permissions.uiInventoryViewInstances.gui,
+          Permissions.uiQuickMarcQuickMarcEditorDuplicate.gui,
+        ])
+          .then((userProperties) => {
+            users.userProperties = userProperties;
+          })
+          .then(() => {
+            cy.assignAffiliationToUser(Affiliations.College, users.userProperties.userId);
+            cy.assignAffiliationToUser(Affiliations.University, users.userProperties.userId);
+            cy.setTenant(Affiliations.College);
+            cy.assignPermissionsToExistingUser(users.userProperties.userId, [
+              Permissions.uiInventoryViewInstances.gui,
+              Permissions.uiQuickMarcQuickMarcBibliographicEditorAll.gui,
+              Permissions.uiQuickMarcQuickMarcEditorDuplicate.gui,
+            ]);
+          })
+          .then(() => {
+            cy.setTenant(Affiliations.University);
+            cy.assignPermissionsToExistingUser(users.userProperties.userId, [
+              Permissions.uiInventoryViewInstances.gui,
+              Permissions.uiQuickMarcQuickMarcBibliographicEditorAll.gui,
+              Permissions.uiQuickMarcQuickMarcEditorDuplicate.gui,
+            ]);
+          })
+          .then(() => {
+            cy.loginAsCollegeAdmin().then(() => {
+              cy.visit(TopMenu.dataImportPath);
+              ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.college);
+              DataImport.verifyUploadState();
+              DataImport.uploadFileAndRetry(marcFile.marc, marcFile.fileNameImported);
+              JobProfiles.waitLoadingList();
+              JobProfiles.search(marcFile.jobProfileToRun);
+              JobProfiles.runImportFile();
+              JobProfiles.waitFileIsImported(marcFile.fileNameImported);
+              Logs.checkStatusOfJobProfile(JOB_STATUS_NAMES.COMPLETED);
+              Logs.openFileDetails(marcFile.fileNameImported);
+              Logs.getCreatedItemsID().then((link) => {
+                createdInstanceIDs.push(link.split('/')[5]);
+              });
+            });
+
+            cy.login(users.userProperties.username, users.userProperties.password, {
+              path: TopMenu.inventoryPath,
+              waiter: InventoryInstances.waitContentLoading,
+            }).then(() => {
+              ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
+              ConsortiumManager.switchActiveAffiliation(tenantNames.college);
+              InventoryInstances.waitContentLoading();
+              ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.college);
+            });
+          });
+      });
+
+      after('Delete user, data', () => {
+        cy.resetTenant();
+        cy.getAdminToken();
+        Users.deleteViaApi(users.userProperties.userId);
+        cy.setTenant(Affiliations.College);
+        createdInstanceIDs.forEach((instanceID) => {
+          InventoryInstance.deleteInstanceViaApi(instanceID);
+        });
+      });
+
+      it(
+        'C402770 Derive new Local MARC bib record from Local Instance in Member tenant (consortia) (spitfire)',
+        { tags: ['criticalPath', 'spitfire'] },
+        () => {
+          cy.visit(`${TopMenu.inventoryPath}/view/${createdInstanceIDs[0]}`);
+          InventoryInstance.waitLoading();
+          InventoryInstance.checkPresentedText(testData.instanceTitle);
+          InventoryInstance.checkSharedTextInDetailView(false);
+
+          InventoryInstance.deriveNewMarcBib();
+          QuickMarcEditor.checkPaneheaderContains(testData.deriveLocalPaneheaderText);
+          QuickMarcEditor.updateExistingField(testData.tag245, testData.tag245DerivedContent);
+          QuickMarcEditor.checkContentByTag(testData.tag245, testData.tag245DerivedContent);
+          QuickMarcEditor.pressSaveAndClose();
+          QuickMarcEditor.checkAfterSaveAndCloseDerive();
+          InventoryInstance.checkSharedTextInDetailView(false);
+          InventoryInstance.checkExpectedMARCSource();
+          InventoryInstance.checkPresentedText(testData.instanceDerivedTitle);
+          InventoryInstance.getId().then((id) => {
+            createdInstanceIDs.push(id);
+
+            InventoryInstance.editMarcBibliographicRecord();
+            QuickMarcEditor.checkContentByTag(testData.tag245, testData.tag245DerivedContent);
+            QuickMarcEditor.updateExistingField(testData.tag245, testData.tag245EditedContent);
+            QuickMarcEditor.checkContentByTag(testData.tag245, testData.tag245EditedContent);
+            QuickMarcEditor.pressSaveAndClose();
+            QuickMarcEditor.checkAfterSaveAndClose();
+            InventoryInstance.checkSharedTextInDetailView(false);
+            InventoryInstance.checkExpectedMARCSource();
+            InventoryInstance.checkPresentedText(testData.instanceEditedTitle);
+
+            InventoryInstance.viewSource();
+            InventoryViewSource.contains(testData.tag245EditedContent);
+            InventoryViewSource.contains(testData.sourceViewLocalText);
+
+            ConsortiumManager.switchActiveAffiliation(tenantNames.university);
+            InventoryInstances.waitContentLoading();
+            ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.university);
+
+            InventoryInstances.searchByTitle(createdInstanceIDs[1], false);
+            InventorySearchAndFilter.verifyNoRecordsFound();
+
+            ConsortiumManager.switchActiveAffiliation(tenantNames.central);
+            InventoryInstances.waitContentLoading();
+            ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
+
+            InventoryInstances.searchByTitle(createdInstanceIDs[1], false);
+            InventorySearchAndFilter.verifyNoRecordsFound();
+          });
+        },
+      );
+    });
+  });
+});
