d8c83183b2d4e667cb28c2fc26cb54a4a4ca4016
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2024-01-19 17:47:10 +0200

    FAT-11076 (#3098)
    * fixed C378883
    
    * fixed C356824
    
    * added waiter
    
    * fixed C505
    
    * fixed tests

--- cypress/e2e/calendar/copy-calendar.cy.js
+++ cypress/e2e/calendar/copy-calendar.cy.js
@@ -10,59 +10,61 @@ import PaneActions from '../../support/fragments/calendar/pane-actions';
 
 const testCalendar = calendarFixtures.calendar;
 
-describe('Duplicate an existing calendar to make a new one', () => {
-  let testCalendarResponse;
-  const duplicateCalendarName = 'test-calendar-a8531527-aa3b-447a-8c76-88f905ade409-duplicate';
+describe('Calendar', () => {
+  describe('Calendar New', () => {
+    let testCalendarResponse;
+    const duplicateCalendarName = 'test-calendar-a8531527-aa3b-447a-8c76-88f905ade409-duplicate';
 
-  before(() => {
-    // login
-    openCalendarSettings(false);
+    before(() => {
+      // login
+      openCalendarSettings(false);
 
-    // get admin token to use in okapiRequest to retrieve service points
-    if (!Cypress.env('token')) {
-      cy.getAdminToken();
-    }
+      // get admin token to use in okapiRequest to retrieve service points
+      if (!Cypress.env('token')) {
+        cy.getAdminToken();
+      }
 
-    // create test calendar
-    createCalendar(testCalendar, (calResponse) => {
-      testCalendarResponse = calResponse.body;
+      // create test calendar
+      createCalendar(testCalendar, (calResponse) => {
+        testCalendarResponse = calResponse.body;
+      });
+
+      openCalendarSettings();
     });
 
-    openCalendarSettings();
-  });
-
-  it(
-    'C360953 Copy -> Duplicate an existing calendar to make a new one (bama)',
-    { tags: ['smoke', 'bama'] },
-    () => {
-      PaneActions.allCalendarsPane.openAllCalendarsPane();
-      PaneActions.allCalendarsPane.selectCalendar(testCalendarResponse.name);
-      PaneActions.individualCalendarPane.selectDuplicateAction({
-        calendarName: testCalendarResponse.name,
-      });
-      CreateCalendarForm.editNameAndSave({ newCalendarName: duplicateCalendarName });
-
-      // intercept http request
-      let duplicateCalendarID;
-      cy.intercept(Cypress.env('OKAPI_HOST') + '/calendar/calendars', (req) => {
-        if (req.method === 'POST') {
-          req.continue((res) => {
-            expect(res.statusCode).equals(201);
-            duplicateCalendarID = res.body.id;
-          });
-        }
-      }).as('createDuplicateCalendar');
-
-      // check that duplicate calendar exists in list of calendars
-      cy.wait('@createDuplicateCalendar').then(() => {
-        deleteCalendar(testCalendarResponse.id);
-        openCalendarSettings();
+    it(
+      'C360953 Copy -> Duplicate an existing calendar to make a new one (bama)',
+      { tags: ['smoke', 'bama'] },
+      () => {
         PaneActions.allCalendarsPane.openAllCalendarsPane();
-        PaneActions.allCalendarsPane.checkCalendarExists(duplicateCalendarName);
+        PaneActions.allCalendarsPane.selectCalendar(testCalendarResponse.name);
+        PaneActions.individualCalendarPane.selectDuplicateAction({
+          calendarName: testCalendarResponse.name,
+        });
+        CreateCalendarForm.editNameAndSave({ newCalendarName: duplicateCalendarName });
 
-        // delete duplicate calendar
-        deleteCalendar(duplicateCalendarID);
-      });
-    },
-  );
+        // intercept http request
+        let duplicateCalendarID;
+        cy.intercept(Cypress.env('OKAPI_HOST') + '/calendar/calendars', (req) => {
+          if (req.method === 'POST') {
+            req.continue((res) => {
+              expect(res.statusCode).equals(201);
+              duplicateCalendarID = res.body.id;
+            });
+          }
+        }).as('createDuplicateCalendar');
+
+        // check that duplicate calendar exists in list of calendars
+        cy.wait('@createDuplicateCalendar').then(() => {
+          deleteCalendar(testCalendarResponse.id);
+          openCalendarSettings();
+          PaneActions.allCalendarsPane.openAllCalendarsPane();
+          PaneActions.allCalendarsPane.checkCalendarExists(duplicateCalendarName);
+
+          // delete duplicate calendar
+          deleteCalendar(duplicateCalendarID);
+        });
+      },
+    );
+  });
 });
--- cypress/e2e/calendar/delete-existing-calendar.cy.js
+++ cypress/e2e/calendar/delete-existing-calendar.cy.js
@@ -11,51 +11,53 @@ import PaneActions from '../../support/fragments/calendar/pane-actions';
 const testServicePoint = calendarFixtures.servicePoint;
 const testCalendar = calendarFixtures.calendar;
 
-describe('Delete existing calendars', () => {
-  before(() => {
-    // login
-    cy.loginAsAdmin();
+describe('Calendar', () => {
+  describe('Calendar New', () => {
+    before(() => {
+      // login
+      cy.loginAsAdmin();
 
-    // get admin token to use in okapiRequest to retrieve service points
-    if (!Cypress.env('token')) {
-      cy.getAdminToken();
-    }
+      // get admin token to use in okapiRequest to retrieve service points
+      if (!Cypress.env('token')) {
+        cy.getAdminToken();
+      }
 
-    // reset db state
-    deleteServicePoint(testServicePoint.id, false);
+      // reset db state
+      deleteServicePoint(testServicePoint.id, false);
 
-    // create test service point
-    createServicePoint(testServicePoint, (response) => {
-      testCalendar.assignments = [response.body.id];
+      // create test service point
+      createServicePoint(testServicePoint, (response) => {
+        testCalendar.assignments = [response.body.id];
 
-      createCalendar(testCalendar);
-      openCalendarSettings();
+        createCalendar(testCalendar);
+        openCalendarSettings();
+      });
     });
-  });
 
-  after(() => {
-    deleteServicePoint(testServicePoint.id, true);
-  });
-
-  it('C360943 Delete -> Delete existing calendar (bama)', { tags: ['smoke', 'bama'] }, () => {
-    PaneActions.allCalendarsPane.openAllCalendarsPane();
-    PaneActions.allCalendarsPane.selectCalendar(testCalendar.name);
-    PaneActions.individualCalendarPane.selectDeleteAction({
-      calendarName: testCalendar.name,
+    after(() => {
+      deleteServicePoint(testServicePoint.id, true);
     });
-    ModalFragments.checkCalendarDeletionModalWithCancelButton();
 
-    PaneActions.individualCalendarPane.checkIndividualCalendarPaneExists(testCalendar.name);
-    PaneActions.individualCalendarPane.selectDeleteAction({
-      calendarName: testCalendar.name,
-    });
-    ModalFragments.checkCalendarDeletionModalWithDismiss();
+    it('C360943 Delete -> Delete existing calendar (bama)', { tags: ['smoke', 'bama'] }, () => {
+      PaneActions.allCalendarsPane.openAllCalendarsPane();
+      PaneActions.allCalendarsPane.selectCalendar(testCalendar.name);
+      PaneActions.individualCalendarPane.selectDeleteAction({
+        calendarName: testCalendar.name,
+      });
+      ModalFragments.checkCalendarDeletionModalWithCancelButton();
 
-    PaneActions.individualCalendarPane.selectDeleteAction({
-      calendarName: testCalendar.name,
+      PaneActions.individualCalendarPane.checkIndividualCalendarPaneExists(testCalendar.name);
+      PaneActions.individualCalendarPane.selectDeleteAction({
+        calendarName: testCalendar.name,
+      });
+      ModalFragments.checkCalendarDeletionModalWithDismiss();
+
+      PaneActions.individualCalendarPane.selectDeleteAction({
+        calendarName: testCalendar.name,
+      });
+      ModalFragments.checkCalendarDeletionModalExists();
+      ModalFragments.clickDeleteButtonOnCalendarDeletionModal();
+      PaneActions.allCalendarsPane.checkCalendarAbsent(testCalendar.name);
     });
-    ModalFragments.checkCalendarDeletionModalExists();
-    ModalFragments.clickDeleteButtonOnCalendarDeletionModal();
-    PaneActions.allCalendarsPane.checkCalendarAbsent(testCalendar.name);
   });
 });
