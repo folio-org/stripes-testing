0771bfd9382b1a34d1ffdf60b08719901b8b6511
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-01-23 14:17:10 +0500

    FAT-11465: test added

--- cypress/e2e/settings/marc-authority/create-new-authority-file.cy.js
+++ cypress/e2e/settings/marc-authority/create-new-authority-file.cy.js
@@ -1,21 +1,25 @@
 import Permissions from '../../../support/dictionary/permissions';
 import ManageAuthorityFiles from '../../../support/fragments/settings/marc-authority/manageAuthorityFiles';
-// import DataImport from '../../../support/fragments/data_import/dataImport';
-// import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
-// import Logs from '../../../support/fragments/data_import/logs/logs';
-// import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
-// import MarcAuthorities from '../../../support/fragments/marcAuthority/marcAuthorities';
-// import MarcAuthority from '../../../support/fragments/marcAuthority/marcAuthority';
 import TopMenu from '../../../support/fragments/topMenu';
-// import Users from '../../../support/fragments/users/users';
-// import getRandomPostfix from '../../../support/utils/stringTools';
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix, {
+  randomFourDigitNumber,
+  getRandomLetters,
+} from '../../../support/utils/stringTools';
+import DateTools from '../../../support/utils/dateTools';
 
 describe('MARC', () => {
   describe('MARC Authority', () => {
     describe('Settings', () => {
-      const testData = {};
+      const testData = {
+        name: `Local source ${getRandomPostfix()}`,
+        prefix: getRandomLetters(7),
+        startsWith: `1${randomFourDigitNumber()}`,
+        isActive: true,
+        date: DateTools.getFormattedDateWithSlashes({ date: new Date() }),
+      };
 
-      before(() => {
+      before('Create user, login', () => {
         cy.getAdminToken();
         cy.createTempUser([Permissions.uiSettingsManageAuthorityFiles.gui]).then(
           (userProperties) => {
@@ -29,7 +33,13 @@ describe('MARC', () => {
         );
       });
 
-      after(() => {});
+      after('Delete data, user', () => {
+        cy.getAdminToken();
+        Users.deleteViaApi(testData.user.userId);
+        cy.getAuthoritySourceFileIdViaAPI(testData.name).then((id) => {
+          cy.deleteAuthoritySourceFileViaAPI(id);
+        });
+      });
 
       it(
         'C423992 Create new "Authority file" with empty "Base URL" field at "Settings >> MARC authority >> Manage authority files" pane (spitfire)',
@@ -38,6 +48,26 @@ describe('MARC', () => {
           ManageAuthorityFiles.clickNewButton();
           ManageAuthorityFiles.verifyEditableRowAdded();
           ManageAuthorityFiles.verifyTableHeaders();
+          ManageAuthorityFiles.fillAllFields(
+            testData.name,
+            testData.prefix,
+            testData.startsWith,
+            '',
+            testData.isActive,
+          );
+          ManageAuthorityFiles.checkCancelButtonEnabled();
+          ManageAuthorityFiles.checkSaveButtonEnabled();
+          ManageAuthorityFiles.clickSaveButton();
+          ManageAuthorityFiles.checkAfterSave(testData.name);
+          ManageAuthorityFiles.checkSourceFileExists(
+            testData.name,
+            testData.prefix,
+            testData.startsWith,
+            '',
+            testData.isActive,
+            `${testData.date} by ${testData.user.lastName}, ${testData.user.firstName}`,
+            true,
+          );
         },
       );
     });
--- cypress/support/fragments/settings/marc-authority/manageAuthorityFiles.js
+++ cypress/support/fragments/settings/marc-authority/manageAuthorityFiles.js
@@ -1,23 +1,11 @@
-/* eslint-disable no-unused-vars */
 import {
-  Accordion,
   Button,
+  Callout,
   Checkbox,
-  DropdownMenu,
-  Form,
-  KeyValue,
-  MultiColumnList,
   MultiColumnListCell,
   MultiColumnListHeader,
   MultiColumnListRow,
-  MultiSelect,
-  MultiSelectOption,
   Pane,
-  PaneHeader,
-  SearchField,
-  Section,
-  Select,
-  TextArea,
   TextField,
   including,
 } from '../../../../../interactors';
@@ -29,13 +17,13 @@ const newButton = manageAuthorityFilesPane.find(
 const firstRow = manageAuthorityFilesPane.find(MultiColumnListRow({ ariaRowIndex: 2 }));
 const nameTextField = TextField({ placeholder: 'name' });
 const prefixTextField = TextField({ placeholder: 'codes' });
-const HridStartsWithTextField = TextField({ placeholder: 'startNumber' });
+const hridStartsWithTextField = TextField({ placeholder: 'startNumber' });
 const baseUrlTextField = TextField({ placeholder: 'baseUrl' });
 const activeCheckbox = Checkbox({ ariaLabel: 'Active' });
 const sourceCell = MultiColumnListCell({ columnIndex: 5 });
 const lastUpdatedCell = MultiColumnListCell({ columnIndex: 6 });
 const cancelButton = Button('Cancel');
-const savelButton = Button('Save');
+const saveButton = Button('Save');
 const tableHeaderTexts = [
   'Name*',
   'Prefix*',
@@ -45,32 +33,112 @@ const tableHeaderTexts = [
   'Source',
   'Last updated by',
 ];
+const editButton = Button({ icon: 'edit' });
+const deleteButton = Button({ icon: 'trash' });
+const successSaveCalloutText = (authorityFileName) => `The authority file ${authorityFileName} was successfully created`;
+
+const waitLoading = () => {
+  cy.expect(newButton.has({ disabled: false }));
+  cy.expect(firstRow.exists());
+  cy.wait(3000);
+};
+
+const clickNewButton = () => {
+  cy.do(newButton.click());
+};
+
+const verifyEditableRowAdded = () => {
+  cy.expect(firstRow.find(nameTextField).has({ value: '' }));
+  cy.expect(firstRow.find(prefixTextField).has({ value: '' }));
+  cy.expect(firstRow.find(hridStartsWithTextField).has({ value: '' }));
+  cy.expect(firstRow.find(baseUrlTextField).has({ value: '' }));
+  cy.expect(firstRow.find(activeCheckbox).has({ checked: false }));
+  cy.expect(firstRow.find(sourceCell).has({ content: 'Local' }));
+  cy.expect(firstRow.find(lastUpdatedCell).has({ content: '-' }));
+  cy.expect(firstRow.find(cancelButton).has({ disabled: false }));
+  cy.expect(firstRow.find(saveButton).has({ disabled: false }));
+};
+
+const verifyTableHeaders = () => {
+  tableHeaderTexts.forEach((headerText) => {
+    cy.expect(manageAuthorityFilesPane.find(MultiColumnListHeader(headerText)).exists());
+  });
+};
+
+const fillName = (name) => {
+  cy.do(firstRow.find(nameTextField).fillIn(name));
+  cy.expect(firstRow.find(nameTextField).has({ value: name }));
+};
+
+const fillPrefix = (prefix) => {
+  cy.do(firstRow.find(prefixTextField).fillIn(prefix));
+  cy.expect(firstRow.find(prefixTextField).has({ value: prefix }));
+};
+
+const fillHridStartsWith = (startsWith) => {
+  cy.do(firstRow.find(hridStartsWithTextField).fillIn(startsWith));
+  cy.expect(firstRow.find(hridStartsWithTextField).has({ value: startsWith }));
+};
+
+const fillBaseUrl = (baseUrl) => {
+  cy.do(firstRow.find(baseUrlTextField).fillIn(baseUrl));
+  cy.expect(firstRow.find(baseUrlTextField).has({ value: baseUrl }));
+};
+
+const switchActiveCheckbox = (isChecked = true) => {
+  cy.do(firstRow.find(activeCheckbox).click());
+  cy.expect(firstRow.find(activeCheckbox).has({ checked: isChecked }));
+};
+
+const checkCancelButtonEnabled = (isEnabled = true) => {
+  cy.expect(firstRow.find(cancelButton).has({ disabled: !isEnabled }));
+};
+
+const checkSaveButtonEnabled = (isEnabled = true) => {
+  cy.expect(firstRow.find(saveButton).has({ disabled: !isEnabled }));
+};
+
+const clickSaveButton = () => {
+  cy.do(firstRow.find(saveButton).click());
+};
+
+const checkAfterSave = (authorityFileName) => {
+  cy.expect(Callout(successSaveCalloutText(authorityFileName)).exists());
+};
 
 export default {
-  waitLoading: () => {
-    cy.expect(newButton.has({ disabled: false }));
-    cy.wait(1000);
+  waitLoading,
+  clickNewButton,
+  verifyEditableRowAdded,
+  verifyTableHeaders,
+  fillName,
+  fillPrefix,
+  fillHridStartsWith,
+  fillBaseUrl,
+  switchActiveCheckbox,
+  checkCancelButtonEnabled,
+  checkSaveButtonEnabled,
+  clickSaveButton,
+  checkAfterSave,
+
+  fillAllFields: (name, prefix, startsWith, baseUrl, isActive) => {
+    fillName(name);
+    fillPrefix(prefix);
+    fillHridStartsWith(startsWith);
+    fillBaseUrl(baseUrl);
+    switchActiveCheckbox(isActive);
   },
 
-  clickNewButton: () => {
-    cy.do(newButton.click());
-  },
-
-  verifyEditableRowAdded: () => {
-    cy.expect(firstRow.find(nameTextField).has({ value: '' }));
-    cy.expect(firstRow.find(prefixTextField).has({ value: '' }));
-    cy.expect(firstRow.find(HridStartsWithTextField).has({ value: '' }));
-    cy.expect(firstRow.find(baseUrlTextField).has({ value: '' }));
-    cy.expect(firstRow.find(activeCheckbox).has({ checked: false }));
-    cy.expect(firstRow.find(sourceCell).has({ content: 'Local' }));
-    cy.expect(firstRow.find(lastUpdatedCell).has({ content: '-' }));
-    cy.expect(firstRow.find(cancelButton).has({ disabled: false }));
-    cy.expect(firstRow.find(savelButton).has({ disabled: false }));
-  },
-
-  verifyTableHeaders() {
-    tableHeaderTexts.forEach((headerText) => {
-      cy.expect(manageAuthorityFilesPane.find(MultiColumnListHeader(headerText)).exists());
-    });
+  checkSourceFileExists(name, prefix, startsWith, baseUrl, isActive, lastUpdatedBy, isDeletable) {
+    const targetRow = manageAuthorityFilesPane.find(MultiColumnListRow(including(name)));
+    cy.expect([
+      targetRow.find(MultiColumnListCell(prefix)).exists(),
+      targetRow.find(MultiColumnListCell(startsWith)).exists(),
+      targetRow.find(MultiColumnListCell(baseUrl)).exists(),
+      targetRow.find(activeCheckbox).has({ checked: isActive, disabled: true }),
+      targetRow.find(MultiColumnListCell(including(lastUpdatedBy))).exists(),
+      targetRow.find(editButton).exists(),
+    ]);
+    if (isDeletable) cy.expect(targetRow.find(deleteButton).exists());
   },
 };
