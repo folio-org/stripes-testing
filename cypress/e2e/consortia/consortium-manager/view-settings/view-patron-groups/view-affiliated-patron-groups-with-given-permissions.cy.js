367645f5ce85ed2f1fcd6b9a52d78a2b5cffa417
nayimovag <guliyashnar_nayimova@epam.com>
2024-02-05 14:50:19 +0500

    C407754

--- cypress/e2e/consortia/consortium-manager/view-settings/view-patron-groups/view-affiliated-patron-groups-with-given-permissions.cy.js
+++ cypress/e2e/consortia/consortium-manager/view-settings/view-patron-groups/view-affiliated-patron-groups-with-given-permissions.cy.js
@@ -10,6 +10,7 @@ import ConsortiumManagerApp, {
 import SelectMembers from '../../../../../support/fragments/consortium-manager/modal/select-members';
 import PatronGroups from '../../../../../support/fragments/settings/users/patronGroups';
 import PatronGroupsConcortiumManager from '../../../../../support/fragments/consortium-manager/users/patronGroupsConcortiumManager';
+import ConsortiumManager from '../../../../../support/fragments/settings/consortium-manager/consortium-manager';
 
 const testData = {
   centralSharedPatronGroup: {
@@ -49,34 +50,49 @@ describe('Consortium manager', () => {
           permissions.consortiaSettingsConsortiumManagerView.gui,
           permissions.uiUsersViewPatronGroups.gui,
         ]).then((userProperties) => {
-          testData.user = userProperties;
+          // User for test C407753
+          testData.user753 = userProperties;
 
           cy.resetTenant();
-          cy.assignAffiliationToUser(Affiliations.College, testData.user.userId);
+          cy.assignAffiliationToUser(Affiliations.College, testData.user753.userId);
           cy.setTenant(Affiliations.College);
-          cy.assignPermissionsToExistingUser(testData.user.userId, [
+          cy.assignPermissionsToExistingUser(testData.user753.userId, [
             permissions.uiUsersViewPatronGroups.gui,
           ]);
-          PatronGroups.createViaApi(testData.collegeLocalPatronGroup.name).then((response) => {
-            testData.collegeLocalPatronGroup.id = response;
-          });
+          cy.createTempUser([permissions.uiUsersCreatePatronGroups.gui])
+            .then((user) => {
+              // User for test C407754
+              testData.user754 = user;
+            })
+            .then(() => {
+              PatronGroups.createViaApi(testData.collegeLocalPatronGroup.name).then((response) => {
+                testData.collegeLocalPatronGroup.id = response;
+              });
+              cy.resetTenant();
+              cy.getAdminToken();
+              cy.assignPermissionsToExistingUser(testData.user754.userId, [
+                permissions.consortiaSettingsConsortiumManagerView.gui,
+                permissions.uiUsersViewPatronGroups.gui,
+              ]);
 
-          cy.resetTenant();
-          cy.assignAffiliationToUser(Affiliations.University, testData.user.userId);
-          cy.setTenant(Affiliations.University);
-          cy.assignPermissionsToExistingUser(testData.user.userId, [
-            permissions.uiUsersCreatePatronGroups.gui,
-          ]);
-          PatronGroups.createViaApi(testData.universityLocalPatronGroup.name).then((response) => {
-            testData.universityLocalPatronGroup.id = response;
-          });
+              cy.resetTenant();
+              cy.assignAffiliationToUser(Affiliations.University, testData.user753.userId);
+              cy.assignAffiliationToUser(Affiliations.University, testData.user754.userId);
+              cy.setTenant(Affiliations.University);
+              cy.assignPermissionsToExistingUser(testData.user753.userId, [
+                permissions.uiUsersCreatePatronGroups.gui,
+              ]);
+              cy.assignPermissionsToExistingUser(testData.user754.userId, [
+                permissions.uiUsersViewPatronGroups.gui,
+              ]);
+              PatronGroups.createViaApi(testData.universityLocalPatronGroup.name).then(
+                (response) => {
+                  testData.universityLocalPatronGroup.id = response;
+                },
+              );
 
-          cy.resetTenant();
-          cy.login(testData.user.username, testData.user.password);
-          // Without waiter, permissions aren't loading
-          cy.wait(10000);
-          TopMenuNavigation.navigateToApp('Consortium manager');
-          ConsortiumManagerApp.waitLoading();
+              cy.resetTenant();
+            });
         });
       });
 
@@ -96,13 +112,19 @@ describe('Consortium manager', () => {
         cy.getAdminToken();
         PatronGroups.deleteViaApi(testData.centralLocalPatronGroup.id);
         PatronGroupsConcortiumManager.deleteViaApi(testData.centralSharedPatronGroup);
-        Users.deleteViaApi(testData.user.userId);
+        Users.deleteViaApi(testData.user753.userId);
+        Users.deleteViaApi(testData.user754.userId);
       });
 
       it(
         'C407753 User with "Consortium manager: Can view existing settings" permission is able to view the list of patron groups of affiliated tenants in "Consortium manager" app (consortia) (thunderjet)',
         { tags: ['criticalPathECS', 'thunderjet'] },
         () => {
+          cy.login(testData.user753.username, testData.user753.password);
+          // Without waiter, permissions aren't loading
+          cy.wait(10000);
+          TopMenuNavigation.navigateToApp('Consortium manager');
+          ConsortiumManagerApp.waitLoading();
           SelectMembers.selectAllMembers();
           ConsortiumManagerApp.verifyStatusOfConsortiumManager(3);
           ConsortiumManagerApp.chooseSettingsItem(settingsItems.users);
@@ -149,6 +171,66 @@ describe('Consortium manager', () => {
             tenantNames.university,
             'edit',
             'trash',
+          );
+        },
+      );
+
+      it(
+        'C407754 User with "Consortium manager: Can create, edit and remove settings" permission is able to view the list of patron groups of affiliated tenants in "Consortium manager" app (consortia) (thunderjet)',
+        { tags: ['criticalPathECS', 'thunderjet'] },
+        () => {
+          cy.login(testData.user754.username, testData.user754.password);
+          ConsortiumManager.switchActiveAffiliation(tenantNames.college, tenantNames.central);
+          // Without waiter, permissions aren't loading
+          cy.wait(10000);
+          TopMenuNavigation.navigateToApp('Consortium manager');
+          SelectMembers.selectAllMembers();
+          ConsortiumManagerApp.verifyStatusOfConsortiumManager(3);
+          ConsortiumManagerApp.clickSelectMembers();
+          SelectMembers.verifyStatusOfSelectMembersModal(3, 3, true);
+          SelectMembers.selectMembers(tenantNames.university);
+          SelectMembers.saveAndClose();
+          ConsortiumManagerApp.verifyStatusOfConsortiumManager(2);
+          ConsortiumManagerApp.chooseSettingsItem(settingsItems.users);
+          PatronGroupsConcortiumManager.choose();
+          PatronGroupsConcortiumManager.verifyGroupInTheList(
+            testData.centralSharedPatronGroup.payload.group,
+            'All',
+          );
+          PatronGroupsConcortiumManager.verifyGroupInTheList(
+            testData.centralLocalPatronGroup.name,
+            tenantNames.central,
+          );
+
+          PatronGroupsConcortiumManager.verifyGroupInTheList(
+            testData.collegeLocalPatronGroup.name,
+            tenantNames.college,
+            'edit',
+            'trash',
+          );
+          PatronGroupsConcortiumManager.verifyNoGroupInTheList(
+            testData.universityLocalPatronGroup.name,
+          );
+
+          ConsortiumManagerApp.clickSelectMembers();
+          SelectMembers.verifyStatusOfSelectMembersModal(3, 2);
+          SelectMembers.selectMembers(tenantNames.college);
+          SelectMembers.saveAndClose();
+          ConsortiumManagerApp.verifyMembersSelected(1);
+          PatronGroupsConcortiumManager.verifyGroupInTheList(
+            testData.centralSharedPatronGroup.payload.group,
+            'All',
+          );
+          PatronGroupsConcortiumManager.verifyGroupInTheList(
+            testData.centralLocalPatronGroup.name,
+            tenantNames.central,
+          );
+
+          PatronGroupsConcortiumManager.verifyNoGroupInTheList(
+            testData.collegeLocalPatronGroup.name,
+          );
+          PatronGroupsConcortiumManager.verifyNoGroupInTheList(
+            testData.universityLocalPatronGroup.name,
           );
         },
       );
