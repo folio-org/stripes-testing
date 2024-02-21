d0d676a4ab260a1ae10ab5733e1dbee080e0cee6
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2024-02-21 13:39:20 +0500

    Add final part of the test

--- cypress/e2e/consortia/marc/plug-in-marc-authority/searching-browsing-authority-in-central.cy.js
+++ cypress/e2e/consortia/marc/plug-in-marc-authority/searching-browsing-authority-in-central.cy.js
@@ -3,22 +3,22 @@ import Affiliations, { tenantNames } from '../../../../support/dictionary/affili
 import Users from '../../../../support/fragments/users/users';
 import TopMenu from '../../../../support/fragments/topMenu';
 import ConsortiumManager from '../../../../support/fragments/settings/consortium-manager/consortium-manager';
-// import { JOB_STATUS_NAMES, MARC_AUTHORITY_BROWSE_OPTIONS } from '../../../../support/constants';
+import { JOB_STATUS_NAMES } from '../../../../support/constants';
 import getRandomPostfix from '../../../../support/utils/stringTools';
-// import DataImport from '../../../../support/fragments/data_import/dataImport';
-// import JobProfiles from '../../../../support/fragments/data_import/job_profiles/jobProfiles';
-// import Logs from '../../../../support/fragments/data_import/logs/logs';
-// import MarcAuthority from '../../../../support/fragments/marcAuthority/marcAuthority';
+import DataImport from '../../../../support/fragments/data_import/dataImport';
+import JobProfiles from '../../../../support/fragments/data_import/job_profiles/jobProfiles';
+import Logs from '../../../../support/fragments/data_import/logs/logs';
+import MarcAuthority from '../../../../support/fragments/marcAuthority/marcAuthority';
 import MarcAuthorities from '../../../../support/fragments/marcAuthority/marcAuthorities';
-// import MarcAuthorityBrowse from '../../../../support/fragments/marcAuthority/MarcAuthorityBrowse';
 import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
 import InventoryInstance from '../../../../support/fragments/inventory/inventoryInstance';
-// import InventorySearchAndFilter from '../../../../support/fragments/inventory/inventorySearchAndFilter';
 
 describe('MARC', () => {
   describe('plug-in MARC authority', () => {
     const users = {};
-    // const createdRecordIDs = [];
+    const createdRecordIDs = [];
+    const searchValue = 'C404417 Auth';
+    const headLine = 'Shared MARC authority record';
     const marcFiles = [
       {
         marc: 'marcBibFileForC404417Central.mrc',
@@ -26,7 +26,6 @@ describe('MARC', () => {
         jobProfileToRun: 'Default - Create instance and SRS MARC Bib',
         title: 'C404417 Sleeping in the ground : an Inspector Banks novel / Peter Jackson.',
         tenant: tenantNames.central,
-        affiliation: Affiliations.Consortia,
         numOfRecords: 1,
       },
       {
@@ -34,7 +33,6 @@ describe('MARC', () => {
         fileName: `C404417 Central testMarcFile${getRandomPostfix()}.mrc`,
         jobProfileToRun: 'Default - Create SRS MARC Authority',
         tenant: tenantNames.central,
-        affiliation: Affiliations.Consortia,
         numOfRecords: 2,
       },
       {
@@ -42,42 +40,19 @@ describe('MARC', () => {
         fileName: `C404417 Local testMarcFile${getRandomPostfix()}.mrc`,
         jobProfileToRun: 'Default - Create SRS MARC Authority',
         tenant: tenantNames.college,
-        affiliation: Affiliations.College,
         numOfRecords: 1,
       },
     ];
-
-    const headings = [
+    const headingLocalRecord = 'C404417 Auth Member Local';
+    const headingsSharedRecords = [
       'C404417 Auth Shared 1',
       'C404417 Auth Shared 2',
       'C404417 Auth Shared 1 - 400 field',
     ];
-    // const sharedAuthorityRecord1FromCentralTenant = {
-    //   authRefType: 'Authorized',
-    //   heading: 'C404417 Auth Shared 1',
-    //   heading2: 'C404417 Auth Shared 1 - 400 field',
-    //   typeOfHeading: 'Personal Name',
-    // };
-    // const sharedAuthorityRecord2FromCentralTenant = {
-    //   authRefType: 'Authorized',
-    //   heading: 'C404417 Auth Shared 2',
-    //   typeOfHeading: 'Personal Name',
-    // };
-    // const localAuthorityRecord2FromMember1Tenant = {
-    //   authRefType: 'Authorized',
-    //   heading: 'C404449 MARC authority 2nd record, from Member 1 tenant',
-    //   typeOfHeading: 'Personal Name',
-    // };
-    // const localAuthorityRecord5FromMember1Tenant = {
-    //   authRefType: 'Authorized',
-    //   heading: 'C404449 MARC authority 5th record, from Member 1 tenant',
-    //   typeOfHeading: 'Conference Name',
-    // };
-    // const Dropdowns = {
-    //   SHARED: 'Shared',
-    //   YES: 'Yes',
-    //   NO: 'No',
-    // };
+    const accordion = {
+      name: 'References',
+      option: 'Exclude see from',
+    };
 
     before('Create users, data', () => {
       cy.getAdminToken();
@@ -90,39 +65,39 @@ describe('MARC', () => {
         .then((userProperties) => {
           users.userProperties = userProperties;
         })
-        // .then(() => {
-        //   cy.loginAsAdmin({
-        //     path: TopMenu.dataImportPath,
-        //     waiter: DataImport.waitLoading,
-        //   });
-        // })
-        // .then(() => {
-        //   marcFiles.forEach((marcFile, index) => {
-        //     cy.visit(TopMenu.dataImportPath);
-        //     if (marcFile.tenant !== tenantNames.central) {
-        //       ConsortiumManager.switchActiveAffiliation(
-        //         marcFiles[index - 1].tenant,
-        //         marcFile.tenant,
-        //       );
-        //       DataImport.waitLoading();
-        //       ConsortiumManager.checkCurrentTenantInTopMenu(marcFile.tenant);
-        //     }
+        .then(() => {
+          cy.loginAsAdmin({
+            path: TopMenu.dataImportPath,
+            waiter: DataImport.waitLoading,
+          });
+        })
+        .then(() => {
+          marcFiles.forEach((marcFile, index) => {
+            cy.visit(TopMenu.dataImportPath);
+            if (marcFile.tenant !== tenantNames.central) {
+              ConsortiumManager.switchActiveAffiliation(
+                marcFiles[index - 1].tenant,
+                marcFile.tenant,
+              );
+              DataImport.waitLoading();
+              ConsortiumManager.checkCurrentTenantInTopMenu(marcFile.tenant);
+            }
 
-        //     DataImport.verifyUploadState();
-        //     DataImport.uploadFileAndRetry(marcFile.marc, marcFile.fileName);
-        //     JobProfiles.waitLoadingList();
-        //     JobProfiles.search(marcFile.jobProfileToRun);
-        //     JobProfiles.runImportFile();
-        //     JobProfiles.waitFileIsImported(marcFile.fileName);
-        //     Logs.checkJobStatus(marcFile.fileName, JOB_STATUS_NAMES.COMPLETED);
-        //     Logs.openFileDetails(marcFile.fileName);
-        //     for (let i = 0; i < marcFile.numOfRecords; i++) {
-        //       Logs.getCreatedItemsID(i).then((link) => {
-        //         createdRecordIDs.push(link.split('/')[5]);
-        //       });
-        //     }
-        //   });
-        // })
+            DataImport.verifyUploadState();
+            DataImport.uploadFileAndRetry(marcFile.marc, marcFile.fileName);
+            JobProfiles.waitLoadingList();
+            JobProfiles.search(marcFile.jobProfileToRun);
+            JobProfiles.runImportFile();
+            JobProfiles.waitFileIsImported(marcFile.fileName);
+            Logs.checkJobStatus(marcFile.fileName, JOB_STATUS_NAMES.COMPLETED);
+            Logs.openFileDetails(marcFile.fileName);
+            for (let i = 0; i < marcFile.numOfRecords; i++) {
+              Logs.getCreatedItemsID(i).then((link) => {
+                createdRecordIDs.push(link.split('/')[5]);
+              });
+            }
+          });
+        })
         .then(() => {
           cy.resetTenant();
           cy.login(users.userProperties.username, users.userProperties.password, {
@@ -141,12 +116,12 @@ describe('MARC', () => {
       cy.resetTenant();
       cy.getAdminToken();
       Users.deleteViaApi(users.userProperties.userId);
-      //   InventoryInstance.deleteInstanceViaApi(createdRecordIDs[0]);
-      //   MarcAuthority.deleteViaAPI(createdRecordIDs[1]);
-      //   MarcAuthority.deleteViaAPI(createdRecordIDs[2]);
+      InventoryInstance.deleteInstanceViaApi(createdRecordIDs[0]);
+      MarcAuthority.deleteViaAPI(createdRecordIDs[1]);
+      MarcAuthority.deleteViaAPI(createdRecordIDs[2]);
 
-      //   cy.setTenant(Affiliations.College);
-      //   MarcAuthority.deleteViaAPI(createdRecordIDs[3]);
+      cy.setTenant(Affiliations.College);
+      MarcAuthority.deleteViaAPI(createdRecordIDs[3]);
     });
 
     it(
@@ -166,20 +141,53 @@ describe('MARC', () => {
 
         // 4 Fill search input field with query which will return all "MARC Authority" records from Preconditions
         // (e.g., "C404417 Auth" ) → Click "Search" button.
-        MarcAuthorities.searchBeats('C404417 Auth');
-        headings.forEach((heading, index) => {
+        MarcAuthorities.searchBeats(searchValue);
+        headingsSharedRecords.forEach((heading, index) => {
           MarcAuthorities.verifyResultsRowContent(heading);
           MarcAuthorities.verifySharedIcon(index);
         });
 
         // 5 Click on the "Heading/Reference" value for any record in the results list.
-        cy.log('Step 5');
         MarcAuthorities.selectRecordByIndex(0);
         MarcAuthorities.verifyViewPaneContentExists();
         MarcAuthorities.checkSharedTextInDetailView();
-        MarcAuthorities.checkHeadlineInBoldExistsInMarkViewPaneContent(
-          'Shared MARC authority record',
-        );
+        MarcAuthorities.checkHeadlineInBoldExistsInMarkViewPaneContent(headLine);
+
+        // 6 Select "Exclude see from" checkbox in "References" accordion at the first pane in modal
+        MarcAuthorities.clickAccordionByName(accordion.name);
+        MarcAuthorities.actionsSelectCheckbox(accordion.option);
+        headingsSharedRecords.slice(0, 2).forEach((heading) => {
+          MarcAuthorities.verifyResultsRowContent(heading);
+          MarcAuthorities.verifyResultRowContentSharedIcon(heading, true);
+        });
+
+        // 7 Select "Browse" toggle at the first pane in modal
+        // Fill browse input field with query which will return all "MARC Authority" records from Preconditions (e.g., "C404417 Auth" )
+        // Click "Search" button
+
+        MarcAuthorities.switchToBrowse();
+        MarcAuthorities.searchBeats(searchValue);
+        headingsSharedRecords.forEach((heading, index) => {
+          MarcAuthorities.verifyResultsRowContent(heading);
+          MarcAuthorities.verifySharedIcon(index);
+        });
+        MarcAuthorities.checkRowAbsentByContent(headingLocalRecord);
+
+        // 8 Select "Exclude see from" checkbox in "References" accordion at the first pane in modal
+        MarcAuthorities.clickAccordionByName(accordion.name);
+        MarcAuthorities.actionsSelectCheckbox(accordion.option);
+        headingsSharedRecords.slice(0, 2).forEach((heading) => {
+          MarcAuthorities.verifyResultsRowContent(heading);
+          MarcAuthorities.verifyResultRowContentSharedIcon(heading, true);
+        });
+        MarcAuthorities.checkRowAbsentByContent(headingLocalRecord);
+
+        // 9 Click on the "Heading/Reference" value for any record from Preconditions in the results list
+        // (i.e., with "Heading/Reference" starting with "C404417 Auth")
+        MarcAuthorities.selectTitle(`Shared\n${headingsSharedRecords[0]}`);
+        MarcAuthorities.verifyViewPaneContentExists();
+        MarcAuthorities.checkSharedTextInDetailView();
+        MarcAuthorities.checkHeadlineInBoldExistsInMarkViewPaneContent(headLine);
       },
     );
   });
