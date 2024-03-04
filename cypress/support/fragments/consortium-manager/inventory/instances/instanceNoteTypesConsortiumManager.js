a2fcfaae2553c52c69d2243fcb1668edef6bb2b5
Ostap Voitsekhovskyi <Ostap_Voitsekhovskyi@epam.com>
2024-03-04 15:24:25 +0200

    FAT-11067 (#3285)

--- cypress/support/fragments/consortium-manager/inventory/instances/instanceNoteTypesConsortiumManager.js
+++ cypress/support/fragments/consortium-manager/inventory/instances/instanceNoteTypesConsortiumManager.js
@@ -39,6 +39,32 @@ export default {
     });
   },
 
+  getInstanceNoteTypeByNameAndTenant(name, tenantId) {
+    return cy.getConsortiaId().then((consortiaId) => {
+      cy.getPublications([tenantId], '/instance-note-types?limit=2000&offset=0').then((publicationId) => {
+        cy.okapiRequest({
+          method: REQUEST_METHOD.GET,
+          path: `consortia/${consortiaId}/publications/${publicationId}/results`,
+        }).then(({ body }) => {
+          const instanceNoteTypes = JSON.parse(body.publicationResults.find((publication) => publication.tenantId === tenantId).response).instanceNoteTypes;
+          return instanceNoteTypes.find((instanceNoteType) => instanceNoteType.name === name);
+        });
+      });
+    });
+  },
+
+  deleteInstanceNoteTypeByNameAndTenant(name, tenantId) {
+    this.getInstanceNoteTypeByNameAndTenant(name, tenantId).then((instanceNoteType) => {
+      cy.setTenant(tenantId);
+      cy.okapiRequest({
+        method: REQUEST_METHOD.DELETE,
+        path: `instance-note-types/${instanceNoteType.id}`,
+        failOnStatusCode: false,
+      });
+      cy.resetTenant();
+    });
+  },
+
   waitLoading() {
     ConsortiaControlledVocabularyPaneset.waitLoading('Instance note types');
   },
