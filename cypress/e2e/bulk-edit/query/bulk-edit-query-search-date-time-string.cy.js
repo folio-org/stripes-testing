27c998193cd6f69f7b94a9200392bfb47262a277
Guliyashnar Nayimova <nayimovag@gmail.com>
2024-03-15 02:38:45 +0500

    C436782

---
+++ cypress/e2e/bulk-edit/query/bulk-edit-query-search-date-time-string.cy.js
@@ -0,0 +1,138 @@
+import permissions from '../../../support/dictionary/permissions';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import QueryModal, {
+  usersFieldValues,
+  dateTimeOperators,
+  stringOperators,
+} from '../../../support/fragments/bulk-edit/query-modal';
+import DateTools from '../../../support/utils/dateTools';
+
+let user;
+// const expirationUserDate = DateTools.getTomorrowDay();
+
+describe('Bulk Edit - Query', () => {
+  before('create test data', () => {
+    cy.getAdminToken();
+    cy.createTempUser([
+      permissions.bulkEditUpdateRecords.gui,
+      permissions.uiUserEdit.gui,
+      permissions.bulkEditQueryView.gui,
+    ]).then((userProperties) => {
+      user = userProperties;
+      cy.getUsers({ limit: 1, query: `username=${user.username}` }).then((users) => {
+        cy.updateUser({
+          ...users[0],
+          expirationDate: DateTools.getTomorrowDay(),
+        });
+      });
+      cy.login(user.username, user.password, {
+        path: TopMenu.bulkEditPath,
+        waiter: BulkEditSearchPane.waitLoading,
+      });
+    });
+  });
+
+  after('delete test data', () => {
+    cy.getAdminToken();
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C436782 Query builder - Search users with expiration date in specified date range and first name, last name containing specific letters ("Date-time" and "String" property types) (firebird)',
+    { tags: ['smoke', 'firebird'] },
+    () => {
+      BulkEditSearchPane.openQuerySearch();
+      BulkEditSearchPane.checkUsersRadio();
+      BulkEditSearchPane.clickBuildQueryButton();
+      QueryModal.verify();
+      QueryModal.verifyFieldsSortedAlphabetically();
+      QueryModal.selectField(usersFieldValues.expirationDate);
+      QueryModal.verifySelectedField(usersFieldValues.expirationDate);
+      QueryModal.verifyQueryAreaContent('(user_expiration_date  )');
+      QueryModal.verifyOperatorColumn();
+      QueryModal.selectOperator('>=');
+      QueryModal.verifyOperatorsList(dateTimeOperators);
+      QueryModal.verifyQueryAreaContent('(user_expiration_date >= )');
+      QueryModal.verifyValueColumn();
+      const todayDate = DateTools.getCurrentDate();
+      const todayDateWithDashes = DateTools.getCurrentDateForFiscalYear();
+      QueryModal.pickDate(todayDate);
+      QueryModal.testQueryDisabled(false);
+      QueryModal.runQueryDisabled();
+      QueryModal.addNewRow();
+      QueryModal.verifyBooleanColumn();
+      QueryModal.verifyEmptyField(1);
+      QueryModal.verifyEmptyOperator(1);
+      QueryModal.verifyEmptyValue(1);
+      QueryModal.verifyPlusAndTrashButtonsDisabled(1, false, false);
+      QueryModal.verifyPlusAndTrashButtonsDisabled(0, false, true);
+      QueryModal.verifyQueryAreaContent(
+        `(user_expiration_date >= "${todayDateWithDashes}") AND (  )`,
+      );
+      QueryModal.testQueryDisabled();
+      QueryModal.runQueryDisabled();
+      QueryModal.typeInAndSelectField(usersFieldValues.expirationDate, 1);
+      QueryModal.selectOperator('<=', 1);
+      QueryModal.verifyValueColumn();
+      const nextWeekDate = DateTools.get2DaysAfterTomorrowDateForFiscalYearOnUIEdit();
+      const nextWeekDateWithDashes = DateTools.getSomeDaysAfterTomorrowDateForFiscalYear(3);
+      QueryModal.pickDate(nextWeekDate, 1);
+      QueryModal.verifyQueryAreaContent(
+        `(user_expiration_date >= "${todayDateWithDashes}") AND (user_expiration_date <= "${nextWeekDateWithDashes}")`,
+      );
+      QueryModal.testQueryDisabled(false);
+      QueryModal.runQueryDisabled();
+      QueryModal.addNewRow(1);
+      QueryModal.verifyBooleanColumn(2);
+      QueryModal.verifyEmptyField(2);
+      QueryModal.verifyEmptyOperator(2);
+      QueryModal.verifyEmptyValue(2);
+      QueryModal.verifyPlusAndTrashButtonsDisabled(0, false, true);
+      QueryModal.testQueryDisabled();
+      QueryModal.runQueryDisabled();
+      QueryModal.selectField(usersFieldValues.lastName, 2);
+      QueryModal.verifySelectedField(usersFieldValues.lastName, 2);
+      QueryModal.verifyQueryAreaContent(
+        `(user_expiration_date >= "${todayDateWithDashes}") AND (user_expiration_date <= "${nextWeekDateWithDashes}") AND (user_last_name  )`,
+      );
+      QueryModal.testQueryDisabled();
+      QueryModal.runQueryDisabled();
+      QueryModal.selectOperator('starts with', 2);
+      QueryModal.verifyOperatorsList(stringOperators, 2);
+      QueryModal.verifyQueryAreaContent(
+        `(user_expiration_date >= "${todayDateWithDashes}") AND (user_expiration_date <= "${nextWeekDateWithDashes}") AND (user_last_name starts with )`,
+      );
+      QueryModal.fillInValueTextfield('cypressTestUser', 2);
+      QueryModal.verifyQueryAreaContent(
+        `(user_expiration_date >= "${todayDateWithDashes}") AND (user_expiration_date <= "${nextWeekDateWithDashes}") AND (user_last_name starts with "cypressTestUser")`,
+      );
+      QueryModal.testQueryDisabled(false);
+      QueryModal.runQueryDisabled();
+      QueryModal.addNewRow(2);
+      QueryModal.verifyBooleanColumn(3);
+      QueryModal.verifyEmptyField(3);
+      QueryModal.verifyEmptyOperator(3);
+      QueryModal.verifyEmptyValue(3);
+      QueryModal.verifyPlusAndTrashButtonsDisabled(0, false, true);
+      QueryModal.testQueryDisabled();
+      QueryModal.runQueryDisabled();
+      QueryModal.selectField(usersFieldValues.firstName, 3);
+      QueryModal.verifySelectedField(usersFieldValues.firstName, 3);
+      QueryModal.testQueryDisabled();
+      QueryModal.runQueryDisabled();
+      QueryModal.selectOperator('contains', 3);
+      QueryModal.fillInValueTextfield('testPermFirst', 3);
+      QueryModal.verifyQueryAreaContent(
+        `(user_expiration_date >= "${todayDateWithDashes}") AND (user_expiration_date <= "${nextWeekDateWithDashes}") AND (user_last_name starts with "cypressTestUser") AND (user_first_name contains "testPermFirst")`,
+      );
+      QueryModal.testQueryDisabled(false);
+      QueryModal.runQueryDisabled();
+      QueryModal.clickTestQuery();
+      QueryModal.verifyPreviewOfRecordsMatched();
+      QueryModal.clickRunQuery();
+      QueryModal.verifyClosed();
+    },
+  );
+});
