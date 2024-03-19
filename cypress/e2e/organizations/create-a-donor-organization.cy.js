eae09979a962f7771a2715190d632b8dd9bc5978
IhorBohdan <99126301+IhorBohdan@users.noreply.github.com>
2024-03-19 14:59:17 +0200

    Implement of FAT-12404 (#3352)
    * Implement of FAT-12404
    
    * Add some fixes
    
    ---------
    
    Co-authored-by: bohdanihor <bohdvn@gmail.com>

---
+++ cypress/e2e/organizations/create-a-donor-organization.cy.js
@@ -0,0 +1,54 @@
+import permissions from '../../support/dictionary/permissions';
+import Organizations from '../../support/fragments/organizations/organizations';
+import TopMenu from '../../support/fragments/topMenu';
+import Users from '../../support/fragments/users/users';
+import getRandomPostfix from '../../support/utils/stringTools';
+
+describe('Organizations', () => {
+  const organization = {
+    name: `1_autotest_name_${getRandomPostfix()}`,
+    status: 'Active',
+    code: `autotest_code_${getRandomPostfix()}`,
+    isVendor: true,
+    erpCode: `ERP-${getRandomPostfix()}`,
+  };
+  let user;
+
+  before(() => {
+    cy.getAdminToken();
+    cy.createTempUser([permissions.uiOrganizationsViewEditCreate.gui]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password, {
+        path: TopMenu.organizationsPath,
+        waiter: Organizations.waitLoading,
+      });
+    });
+  });
+
+  after(() => {
+    cy.getAdminToken();
+    Users.deleteViaApi(user.userId);
+    Organizations.getOrganizationViaApi({ query: `name="${organization.name}"` }).then(
+      (returnedOrganization) => {
+        Organizations.deleteOrganizationViaApi(returnedOrganization.id);
+      },
+    );
+  });
+
+  it(
+    'C421980 Create a donor Organization (thunderjet)',
+    { tags: ['criticalPath', 'thunderjet'] },
+    () => {
+      Organizations.createDonorOrganizationViaUi(organization);
+      Organizations.closeDetailsPane();
+      Organizations.selectIsDonorFilter('Yes');
+      Organizations.selectOrganization(organization.name);
+      Organizations.checkOrganizationInfo(organization);
+      Organizations.closeDetailsPane();
+      Organizations.resetFilters();
+      Organizations.selectIsDonorFilter('No');
+      Organizations.organizationIsAbsent(organization.name);
+      Organizations.resetFilters();
+    },
+  );
+});
