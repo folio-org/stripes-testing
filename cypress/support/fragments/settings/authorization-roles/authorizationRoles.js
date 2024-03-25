c14395149f0eceb1b4702d1a76b1c3af7c38d47c
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-03-25 12:48:59 +0300

    FAT-2459-C436928 (#3365)
    * FAT-12456: initial methods, steps added
    
    * FAT-12459: test updated

---
+++ cypress/support/fragments/settings/authorization-roles/authorizationRoles.js
@@ -0,0 +1,176 @@
+import {
+  Button,
+  Checkbox,
+  MultiColumnListHeader,
+  MultiColumnListRow,
+  Pane,
+  TextField,
+  including,
+  Modal,
+  TextArea,
+  Accordion,
+  MultiColumnList,
+  MultiColumnListCell,
+  HTML,
+} from '../../../../../interactors';
+
+const rolesPane = Pane('Authorization roles');
+const newButton = Button('+ New');
+const createRolePane = Pane('Create role');
+const roleNameInput = TextField('Name*');
+const roleDescriptionInput = TextArea('Description');
+const selectApplicationButton = Button({ id: 'find-application-trigger' });
+const selectApplicationModal = Modal('Select application');
+const selectAppSearchInput = selectApplicationModal.find(
+  TextField({ id: 'input-applications-search' }),
+);
+const selectAppSearchButton = selectApplicationModal.find(
+  Button({ id: 'clickable-search-applications' }),
+);
+const saveButtonInModal = selectApplicationModal.find(
+  Button({ dataTestID: 'submit-applications-modal' }),
+);
+const capabilitiesAccordion = Accordion('Capabilities');
+const capabilitySetsAccordion = Accordion('Capability sets');
+const saveButton = Button('Save and close');
+
+const capabilityTables = {
+  Data: MultiColumnList({ dataTestId: 'capabilities-data-type' }),
+  Settings: MultiColumnList({ dataTestId: 'capabilities-settings-type' }),
+  Procedural: MultiColumnList({ dataTestId: 'capabilities-procedural-type' }),
+};
+
+const getResultsListByColumn = (columnIndex) => {
+  const cells = [];
+
+  cy.wait(2000);
+  return cy
+    .get('div[data-testid^="capabilities-"] [data-row-index]')
+    .each(($row) => {
+      // from each row, choose specific cell
+      cy.get(`[class*="mclCell-"]:nth-child(${columnIndex + 1})`, { withinSubject: $row })
+        // extract its text content
+        .invoke('text')
+        .then((cellValue) => {
+          cells.push(cellValue);
+        });
+    })
+    .then(() => cells);
+};
+
+export default {
+  waitLoading: () => {
+    cy.expect(rolesPane.exists());
+  },
+
+  waitContentLoading: () => {
+    cy.expect(rolesPane.find(MultiColumnListHeader('Name')).exists());
+  },
+
+  clickNewButton: () => {
+    cy.do(newButton.click());
+    cy.expect([
+      createRolePane.exists(),
+      capabilitiesAccordion.has({ open: true }),
+      capabilitySetsAccordion.exists(),
+      saveButton.has({ disabled: true }),
+    ]);
+  },
+
+  fillRoleNameDescription: (roleName, roleDescription = '') => {
+    cy.do([roleNameInput.fillIn(roleName), roleDescriptionInput.fillIn(roleDescription)]);
+  },
+
+  clickSelectApplication: () => {
+    cy.do(selectApplicationButton.click());
+    cy.expect(selectApplicationModal.exists());
+  },
+
+  selectApplicationInModal: (appName, isSelected = true) => {
+    const targetCheckbox = selectApplicationModal
+      .find(MultiColumnListRow(including(appName), { isContainer: false }))
+      .find(Checkbox());
+    cy.do(targetCheckbox.click());
+    cy.expect(targetCheckbox.has({ checked: isSelected }));
+  },
+
+  clickSaveInModal: () => {
+    cy.do(saveButtonInModal.click());
+    cy.expect(selectApplicationModal.absent());
+  },
+
+  searchForAppInModal: (appName) => {
+    cy.do([selectAppSearchInput.fillIn(appName), selectAppSearchButton.click()]);
+  },
+
+  checkSaveButton: (enabled = true) => {
+    cy.expect(saveButton.has({ disabled: !enabled }));
+  },
+
+  clickSaveButton: () => {
+    cy.do(saveButton.click());
+  },
+
+  verifyAppNamesInCapabilityTables: (appNamesArray) => {
+    getResultsListByColumn(0).then((cellTexts) => {
+      cellTexts.forEach((cellText) => {
+        expect(cellText).to.be.oneOf([...appNamesArray]);
+      });
+    });
+  },
+
+  selectCapabilitySetCheckbox: ({ table, application, resource, action }, isSelected = true) => {
+    const targetCheckbox = capabilitySetsAccordion
+      .find(capabilityTables[table])
+      .find(MultiColumnListRow(`${application}${resource}`, { isContainer: false }))
+      .find(MultiColumnListCell({ column: action }))
+      .find(Checkbox({ isWrapper: false }));
+    cy.do(targetCheckbox.click());
+    cy.expect(targetCheckbox.has({ checked: isSelected }));
+    // wait for capabilities selection to be updated
+    cy.wait(1000);
+  },
+
+  verifyCapabilityCheckboxCheckedAndDisabled: ({ table, resource, action }) => {
+    const targetCheckbox = capabilitiesAccordion
+      .find(capabilityTables[table])
+      .find(Checkbox({ ariaLabel: `${action} ${resource}`, isWrapper: false }));
+    cy.expect(targetCheckbox.has({ checked: true, labelText: 'Read-only' }));
+  },
+
+  clickOnRoleName: (roleName) => {
+    cy.do(rolesPane.find(HTML(roleName, { className: including('root') })).click());
+    cy.expect([
+      Pane(roleName).exists(),
+      capabilitiesAccordion.has({ open: false }),
+      capabilitySetsAccordion.has({ open: false }),
+    ]);
+  },
+
+  clickOnCapabilitiesAccordion: () => {
+    cy.do(capabilitiesAccordion.clickHeader());
+    cy.expect(capabilitiesAccordion.has({ open: true }));
+  },
+
+  clickOnCapabilitySetsAccordion: () => {
+    cy.do(capabilitySetsAccordion.clickHeader());
+    cy.expect(capabilitySetsAccordion.has({ open: true }));
+  },
+
+  verifyCapabilitySetCheckboxCheckedAndDisabled: ({ table, resource, action }) => {
+    const targetCheckbox = capabilitySetsAccordion
+      .find(capabilityTables[table])
+      .find(Checkbox({ ariaLabel: `${action} ${resource}`, isWrapper: false }));
+    cy.expect(targetCheckbox.has({ checked: true, labelText: 'Read-only' }));
+  },
+
+  checkCountOfCapablities: (table, expectedCount) => {
+    cy.expect(capabilitiesAccordion.find(capabilityTables[table]).has({ rowCount: expectedCount }));
+  },
+
+  checkCountOfCapablitySets: (table, expectedCount) => {
+    cy.expect(
+      capabilitySetsAccordion.find(capabilityTables[table]).has({ rowCount: expectedCount }),
+    );
+  },
+};
