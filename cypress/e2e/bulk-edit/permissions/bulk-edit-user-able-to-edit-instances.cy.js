c86b0937b76f74c92f685308b6c4f0c05b8694c8
Guliyashnar Nayimova <nayimovag@gmail.com>
2024-03-26 16:24:40 +0500

    FAT-12492

---
+++ cypress/e2e/bulk-edit/permissions/bulk-edit-user-able-to-edit-instances.cy.js
@@ -0,0 +1,79 @@
+import permissions from '../../../support/dictionary/permissions';
+import TopMenu from '../../../support/fragments/topMenu';
+import BulkEditSearchPane from '../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import FileManager from '../../../support/utils/fileManager';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import BulkEditActions from '../../../support/fragments/bulk-edit/bulk-edit-actions';
+
+let user;
+const item = {
+  barcode: getRandomPostfix(),
+  instanceName: `instance-${getRandomPostfix()}`,
+};
+// const instanceHRIDFileName = `instanceHRID_${getRandomPostfix()}.csv`;
+const instanceUUIDsFileName = `validInstanceUUIDs_${getRandomPostfix()}.csv`;
+
+describe('bulk-edit', () => {
+  describe('permissions', () => {
+    before('create test data', () => {
+      cy.createTempUser([
+        permissions.bulkEditEdit.gui,
+        permissions.uiInventoryViewCreateEditInstances.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password, {
+          path: TopMenu.bulkEditPath,
+          waiter: BulkEditSearchPane.waitLoading,
+        });
+        item.instanceId = InventoryInstances.createInstanceViaApi(item.instanceName, item.barcode);
+        FileManager.createFile(`cypress/fixtures/${instanceUUIDsFileName}`, item.instanceId);
+      });
+    });
+
+    after('delete test data', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(user.userId);
+      InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.barcode);
+      FileManager.deleteFile(`cypress/fixtures/${instanceUUIDsFileName}`);
+    });
+
+    it(
+      'C423647 User with "Bulk Edit: In app - Edit inventory records" and "Inventory: View, create, edit instances" permissions is able to start bulk edit of Instances (firebird)',
+      { tags: ['criticalPath', 'firebird'] },
+      () => {
+        BulkEditSearchPane.verifyBulkEditImage();
+        BulkEditSearchPane.verifyPanesBeforeImport();
+        BulkEditSearchPane.verifyBulkEditPaneItems();
+        BulkEditSearchPane.verifySetCriteriaPaneSpecificTabs('Identifier');
+        BulkEditSearchPane.verifySetCriteriaPaneSpecificTabsHidden('Query', 'Logs');
+        BulkEditSearchPane.recordTypesAccordionExpanded();
+        BulkEditSearchPane.verifyRecordIdentifierDisabled();
+        BulkEditSearchPane.isDragAndDropAreaDisabled(true);
+        BulkEditSearchPane.isHoldingsRadioChecked(false);
+        BulkEditSearchPane.isInstancesRadioChecked(false);
+        BulkEditSearchPane.isItemsRadioChecked(false);
+        BulkEditSearchPane.verifyRadioHidden('Users');
+        BulkEditSearchPane.verifyFirstOptionRecordIdentifierDropdown('Select record identifier');
+        BulkEditSearchPane.verifyDefaultFilterState();
+        BulkEditSearchPane.isDragAndDropAreaDisabled(true);
+        BulkEditSearchPane.isHoldingsRadioChecked(false);
+        BulkEditSearchPane.isInstancesRadioChecked(false);
+        BulkEditSearchPane.isItemsRadioChecked(false);
+        BulkEditSearchPane.verifyRecordIdentifierDisabled(true);
+        BulkEditSearchPane.verifyFirstOptionRecordIdentifierDropdown('Select record identifier');
+        BulkEditSearchPane.isDragAndDropAreaDisabled(true);
+        BulkEditSearchPane.verifyDragNDropRecordTypeIdentifierArea('Instances', 'Instance UUIDs');
+        BulkEditSearchPane.uploadFile(instanceUUIDsFileName);
+        BulkEditSearchPane.checkForUploading(instanceUUIDsFileName);
+        BulkEditActions.openActions();
+        BulkEditSearchPane.changeShowColumnCheckboxIfNotYet('Instance UUID');
+        BulkEditSearchPane.verifyMatchedResults(item.instanceId);
+        BulkEditActions.openActions();
+        BulkEditSearchPane.verifyPaneRecordsCount(1);
+        BulkEditSearchPane.verifyActionsAfterConductedInAppUploading(false);
+      },
+    );
+  });
+});
