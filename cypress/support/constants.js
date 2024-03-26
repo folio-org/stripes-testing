809f0c0f902738a1b6fff045e57050e699064b46
Guliyashnar Nayimova <nayimovag@gmail.com>
2024-03-26 19:12:20 +0500

    FAT-11979

--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -412,3 +412,15 @@ export const ACTION_NAMES_IN_ACTION_PROFILE = {
   MODIFY: 'Modify (MARC Bibliographic record type only)',
   UPDATE: 'Update (all record types except Orders, Invoices, or MARC Holdings)',
 };
+
+export const patronGroupNames = {
+  STAFF: 'staff',
+  FACULTY: 'faculty',
+  UNDERGRAD: 'undergrad',
+};
+
+export const patronGroupUuids = {
+  STAFF: '3684a786-6671-4268-8ed0-9db82ebca60b',
+  FACULTY: '503a81cd-6c26-400f-b620-14c08943697c',
+  UNDERGRAD: 'bdc2b6d4-5ceb-4a12-ab46-249b9a68473e',
+};
