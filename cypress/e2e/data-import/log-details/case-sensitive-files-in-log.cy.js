ad269f56962966124550e971fa2ed6175e1bfc4f
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2024-03-27 11:51:51 +0200

    added test (#3388)

---
+++ cypress/e2e/data-import/log-details/case-sensitive-files-in-log.cy.js
@@ -0,0 +1,58 @@
+import Permissions from '../../../support/dictionary/permissions';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import TopMenu from '../../../support/fragments/topMenu';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import Users from '../../../support/fragments/users/users';
+import { DEFAULT_JOB_PROFILE_NAMES } from '../../../support/constants';
+import Logs from '../../../support/fragments/data_import/logs/logs';
+import LogsViewAll from '../../../support/fragments/data_import/logs/logsViewAll';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+
+describe('Data Import', () => {
+  describe('Log details', () => {
+    let user;
+    let instanceId;
+    const filePathToUpload = 'oneMarcBib.mrc';
+    const fileName = `C423386 Default_file${getRandomPostfix()}.mrc`;
+
+    before('Create user and upload file', () => {
+      cy.getAdminToken();
+      DataImport.uploadFileViaApi(
+        filePathToUpload,
+        fileName,
+        DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS,
+      ).then((response) => {
+        instanceId = response[0].instance.id;
+      });
+
+      cy.createTempUser([Permissions.moduleDataImportEnabled.gui]).then((userProperties) => {
+        user = userProperties;
+
+        cy.login(user.username, user.password, {
+          path: TopMenu.dataImportPath,
+          waiter: DataImport.waitLoading,
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(user.userId);
+      InventoryInstance.deleteInstanceViaApi(instanceId);
+    });
+
+    it(
+      'C423386 Check the case-sensitive files in log (folijet)',
+      { tags: ['criticalPath', 'folijet'] },
+      () => {
+        Logs.openViewAllLogs();
+        LogsViewAll.verifyLogsPaneIsOpened();
+        ['default_file', 'DEFAULT'].forEach((value) => {
+          LogsViewAll.searchByKeyword(value);
+          LogsViewAll.verifySearchResult(fileName);
+          LogsViewAll.resetAllFilters(false);
+        });
+      },
+    );
+  });
+});
