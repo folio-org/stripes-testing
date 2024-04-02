0c015531ccac3cc6b902b17cdac422e07ebd1e7d
Sviatlana Stsiapanava <152482600+sviatlana-stsiapanava@users.noreply.github.com>
2024-04-02 19:28:09 +0500

    FAT-12583 Fix/update test cases in bugfest (#3426)
    * Update the test case C350757
    
    * Update the locator in the method MarcAuthorityBrowse.checkSearchOptions()
    
    * Remove the duplicated test case C422117

--- cypress/e2e/marc/marc-holdings/create-new-marc-holdings.cy.js
+++ cypress/e2e/marc/marc-holdings/create-new-marc-holdings.cy.js
@@ -29,7 +29,6 @@ describe('MARC', () => {
       tag866Value: 'Test',
       headerTitle: 'Create a new MARC Holdings record',
       headerSubtitle: 'New',
-      defaultLDRmask: /00000nu.{2}\\.{7}un\\4500/,
       tagLDRValueInSourceMask: /LEADER\s\d{5}[c,d,n][u,v,x,y]\s{3}22\d{5}[1,2,3,4,5,m,u,z].\s4500/,
       tag001ValueInSourceMask: /[a-z]+\d+/,
       tag004ValueInSourceMask: /[a-z]+\d+/,
@@ -215,10 +214,7 @@ describe('MARC', () => {
         QuickMarcEditor.waitLoading();
         QuickMarcEditor.checkPaneheaderContains(testData.headerTitle);
         QuickMarcEditor.checkPaneheaderContains(testData.headerSubtitle);
-        QuickMarcEditor.checkFieldContentMatch(
-          'textarea[name="records[0].content"]',
-          testData.defaultLDRmask,
-        );
+        QuickMarcEditor.verifyInitialLDRFieldsValuesInMarcHoldingRecord();
         QuickMarcEditor.checkReadOnlyHoldingsTags();
         QuickMarcEditor.verifyHoldingsDefault008BoxesValues(testData.default008BoxesValues);
         QuickMarcEditor.verifyTagValue(5, testData.tag852);
