4390dd7da282988b2967df8a8a7d33bc46999c64
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2024-04-17 20:00:25 +0500

    Add the test case C423559

---
+++ cypress/e2e/marc/marc-authority/create-marc-authority/create-new-marcAuth-record-with-local-auth-file-with-default-prefix.cy.js
@@ -0,0 +1,127 @@
+import Permissions from '../../../../support/dictionary/permissions';
+import MarcAuthority from '../../../../support/fragments/marcAuthority/marcAuthority';
+import QuickMarcEditor from '../../../../support/fragments/quickMarcEditor';
+import TopMenu from '../../../../support/fragments/topMenu';
+import Users from '../../../../support/fragments/users/users';
+import MarcAuthorities from '../../../../support/fragments/marcAuthority/marcAuthorities';
+import getRandomPostfix, { getRandomLetters } from '../../../../support/utils/stringTools';
+
+describe('MARC', () => {
+  describe('MARC Authority', () => {
+    describe('Create MARC Authority', () => {
+      const randomPostfix = getRandomPostfix();
+      const tag001 = '001';
+      const headerText = 'Create a new MARC authority record';
+      let createdAuthorityId;
+      const newField = {
+        rowIndex: 4,
+        tag: '111',
+        content:
+          '$a C423559 Create a new MARC authority record with Local authority file which includes default prefix in it',
+      };
+      const recordTitle =
+        'C423559 Create a new MARC authority record with Local authority file which includes default prefix in it';
+      const localAuthFile = {
+        name: `C423559 auth source file active ${randomPostfix}`,
+        prefix: `na${getRandomLetters(6)}`,
+        startWithNumber: '1',
+        isActive: true,
+      };
+      const users = {};
+
+      before('Create users, data', () => {
+        cy.getAdminToken();
+        cy.createTempUser([
+          Permissions.uiMarcAuthoritiesAuthorityRecordView.gui,
+          Permissions.uiQuickMarcQuickMarcAuthorityCreate.gui,
+          Permissions.uiMarcAuthoritiesAuthorityRecordCreate.gui,
+        ])
+          .then((userProperties) => {
+            users.userProperties = userProperties;
+          })
+          .then(() => {
+            cy.createAuthoritySourceFileUsingAPI(
+              localAuthFile.prefix,
+              localAuthFile.startWithNumber,
+              localAuthFile.name,
+              localAuthFile.isActive,
+            ).then((sourceId) => {
+              localAuthFile.id = sourceId;
+            });
+          })
+          .then(() => {
+            cy.login(users.userProperties.username, users.userProperties.password, {
+              path: TopMenu.marcAuthorities,
+              waiter: MarcAuthorities.waitLoading,
+            });
+          });
+      });
+
+      after('Delete users, data', () => {
+        cy.getAdminToken();
+        Users.deleteViaApi(users.userProperties.userId);
+        MarcAuthority.deleteViaAPI(createdAuthorityId);
+        cy.deleteAuthoritySourceFileViaAPI(localAuthFile.id, true);
+      });
+
+      it(
+        'C423559 Create a new MARC authority record with "Local" authority file selected which includes default prefix in it (spitfire)',
+        { tags: ['criticalPath', 'spitfire'] },
+        () => {
+          // 1 Click on "Actions" button in second pane >> Select "+ New" option
+          MarcAuthorities.clickNewAuthorityButton();
+          QuickMarcEditor.checkPaneheaderContains(headerText);
+          QuickMarcEditor.verifyAuthorityLookUpButton();
+
+          // 2 Click on "Authority file look-up" hyperlink
+          QuickMarcEditor.clickAuthorityLookUpButton();
+
+          // 3 Click on the "Select authority file" placeholder in "Authority file name" dropdown and select the "Local" option created by user in preconditions
+          QuickMarcEditor.selectAuthorityFile(localAuthFile.name);
+          QuickMarcEditor.verifyAuthorityFileSelected(localAuthFile.name);
+
+          // 4 Click on the "Save & close" button
+          QuickMarcEditor.clickSaveAndCloseInModal();
+          QuickMarcEditor.checkFourthBoxDisabled(1);
+          QuickMarcEditor.checkContentByTag(
+            tag001,
+            `${localAuthFile.prefix}${localAuthFile.startWithNumber}`,
+          );
+
+          // 5 Add 1 new field by clicking on "+" icon and fill it as specified:
+          // 111 \\ "$a Create a new MARC authority record with Local authority file which includes default prefix in it"
+          MarcAuthority.addNewField(newField.rowIndex, newField.tag, newField.content);
+          QuickMarcEditor.checkContentByTag(newField.tag, newField.content);
+
+          // 6 Click on the "Save & close" button
+          QuickMarcEditor.pressSaveAndClose();
+          MarcAuthority.verifyAfterSaveAndClose();
+          QuickMarcEditor.verifyPaneheaderWithContentAbsent(headerText);
+          MarcAuthorities.verifyViewPaneContentExists();
+          MarcAuthority.getId().then((id) => {
+            createdAuthorityId = id;
+          });
+
+          MarcAuthority.contains(tag001);
+          MarcAuthority.contains(`${localAuthFile.prefix}${localAuthFile.startWithNumber}`);
+          MarcAuthority.contains(newField.tag);
+          MarcAuthority.contains(newField.content);
+
+          // 7 Close the detail view pane by clicking on "X" icon placed in the left upper corner of the pane
+          MarcAuthorities.closeMarcViewPane();
+          MarcAuthorities.verifyMarcViewPaneIsOpened(false);
+
+          // 8 Click on the "Authority source" multi select element in "Authority source" accordion placed on "Search & filter" pane
+          MarcAuthorities.clickMultiSelectToggleButtonInAccordion('Authority source');
+          MarcAuthorities.checkAuthoritySourceDropdownHasOption(localAuthFile.name);
+
+          // 9 Click on the "Local" authority file created at preconditions in expanded dropdonw
+          MarcAuthorities.chooseAuthoritySourceOption(localAuthFile.name);
+          MarcAuthorities.checkSelectedAuthoritySource(localAuthFile.name);
+          MarcAuthorities.checkAfterSearch('Authorized', recordTitle);
+          MarcAuthorities.checkRecordDetailPageMarkedValue(recordTitle);
+        },
+      );
+    });
+  });
+});
