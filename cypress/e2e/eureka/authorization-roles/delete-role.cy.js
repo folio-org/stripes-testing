bf8de63591331312cf946b774ab6f1d25563ca5b
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-04-26 23:09:18 +0500

    FAT-12946: test added

--- cypress/e2e/eureka/authorization-roles/delete-role.cy.js
+++ cypress/e2e/eureka/authorization-roles/delete-role.cy.js
@@ -7,8 +7,8 @@ describe('Eureka', () => {
   describe('Settings', () => {
     describe('Authorization roles', () => {
       const testData = {
-        roleName: `Auto Role C446119 ${getRandomPostfix()}`,
-        roleDescription: `Description C446119 ${getRandomPostfix()}`,
+        roleName: `Auto Role C446120 ${getRandomPostfix()}`,
+        roleDescription: `Description C446120 ${getRandomPostfix()}`,
       };
 
       before('Create role, user', () => {
@@ -17,6 +17,22 @@ describe('Eureka', () => {
           cy.createAuthorizationRoleApi(testData.roleName, testData.roleDescription).then(
             (role) => {
               testData.roleId = role.id;
+              cy.getCapabilitiesApi(10).then((capabs) => {
+                cy.getCapabilitySetsApi(3).then((capabSets) => {
+                  cy.getUserRoleIdByNameApi(testData.roleName).then((roleId) => {
+                    testData.roleId = roleId;
+                    cy.addCapabilitiesToNewRoleApi(
+                      roleId,
+                      capabs.map((capab) => capab.id),
+                    );
+                    cy.addCapabilitySetsToNewRoleApi(
+                      roleId,
+                      capabSets.map((capab) => capab.id),
+                    );
+                    cy.addRolesToNewUserApi(createdUserProperties.userId, [roleId]);
+                  });
+                });
+              });
               cy.login(testData.user.username, testData.user.password, {
                 path: TopMenu.settingsAuthorizationRoles,
                 waiter: AuthorizationRoles.waitContentLoading,
@@ -33,14 +49,14 @@ describe('Eureka', () => {
       });
 
       it(
-        'C446119 Delete a role without capabilities and users (eureka)',
+        'C446120 Delete a role with capabilities and users assigned (eureka)',
         { tags: ['smoke', 'eureka', 'eurekaPhase1'] },
         () => {
           AuthorizationRoles.searchRole(testData.roleName);
           AuthorizationRoles.clickOnRoleName(testData.roleName);
-          AuthorizationRoles.checkCapabilitiesAccordionCounter('0');
-          AuthorizationRoles.checkCapabilitySetsAccordionCounter('0');
-          AuthorizationRoles.checkUsersAccordion();
+          AuthorizationRoles.checkCapabilitiesAccordionCounter('10');
+          AuthorizationRoles.checkCapabilitySetsAccordionCounter('3');
+          AuthorizationRoles.checkUsersAccordion(1);
           AuthorizationRoles.clickDeleteRole();
           AuthorizationRoles.cancelDeleteRole(testData.roleName);
           AuthorizationRoles.clickDeleteRole();
