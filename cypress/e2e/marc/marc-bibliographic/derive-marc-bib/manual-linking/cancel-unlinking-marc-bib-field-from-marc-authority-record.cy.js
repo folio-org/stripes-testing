9decc340884d79da55be7856db7e1efe323cd090
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2024-04-29 10:03:12 +0500

    fixed Q bugfest non ECS tests

--- cypress/e2e/marc/marc-bibliographic/derive-marc-bib/manual-linking/cancel-unlinking-marc-bib-field-from-marc-authority-record.cy.js
+++ cypress/e2e/marc/marc-bibliographic/derive-marc-bib/manual-linking/cancel-unlinking-marc-bib-field-from-marc-authority-record.cy.js
@@ -139,8 +139,7 @@ describe('MARC', () => {
             InventoryKeyboardShortcuts.pressHotKey(hotKeys.close);
             QuickMarcEditor.checkEditableQuickMarcFormIsOpened();
             QuickMarcEditor.verifyUnlinkAndViewAuthorityButtons(linkingTagAndValues[0].rowIndex);
-            // TODO: check if button not enabled is bug or not
-            // QuickMarcEditor.checkButtonSaveAndCloseEnable();
+            QuickMarcEditor.checkButtonSaveAndCloseDisabled();
             QuickMarcEditor.verifyTagFieldAfterLinking(...bib700AfterLinkingToAuth100);
             QuickMarcEditor.checkUnlinkTooltipText(75, 'Unlink from MARC Authority record');
             QuickMarcEditor.clickUnlinkIconInTagField(linkingTagAndValues[0].rowIndex);
