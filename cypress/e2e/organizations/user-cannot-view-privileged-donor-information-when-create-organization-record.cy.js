c61acbd0f002b9ddc81957cc2bb99f302470f773
IhorBohdan <99126301+IhorBohdan@users.noreply.github.com>
2024-04-29 14:10:25 +0300

    Implement FAT-12541 (#3549)
    * Fix extended tests Thunderjet
    
    * Implement FAT-12541
    
    ---------

---
+++ cypress/e2e/organizations/user-cannot-view-privileged-donor-information-when-create-organization-record.cy.js
@@ -0,0 +1,51 @@
+import permissions from '../../support/dictionary/permissions';
+import Organizations from '../../support/fragments/organizations/organizations';
+import TopMenu from '../../support/fragments/topMenu';
+import Users from '../../support/fragments/users/users';
+import NewOrganization from '../../support/fragments/organizations/newOrganization';
+
+describe('Organizations', () => {
+  const organization = { ...NewOrganization.defaultUiOrganizations };
+  let user;
+
+  before(() => {
+    cy.getAdminToken();
+    Organizations.createOrganizationViaApi(organization).then((response) => {
+      organization.id = response;
+    });
+    cy.createTempUser([permissions.uiOrganizationsViewEditCreate.gui]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password, {
+        path: TopMenu.organizationsPath,
+        waiter: Organizations.waitLoading,
+      });
+    });
+  });
+
+  after(() => {
+    cy.getAdminToken();
+    Users.deleteViaApi(user.userId);
+    Organizations.getOrganizationViaApi({ query: `name="${organization.name}"` }).then(
+      (returnedOrganization) => {
+        Organizations.deleteOrganizationViaApi(returnedOrganization.id);
+      },
+    );
+  });
+
+  it(
+    'C423632 A user without  privileged donor information permission cannot view privileged donor information when create organization record (thunderjet)',
+    { tags: ['criticalPath', 'thunderjet'] },
+    () => {
+      Organizations.newOrganization();
+      Organizations.selectDonorCheckbox();
+      Organizations.varifyAbsentPrivilegedDonorInformationSection();
+      Organizations.selectDonorCheckbox();
+      Organizations.varifyAbsentPrivilegedDonorInformationSection();
+      Organizations.fillInInfoNewOrganization(organization);
+      Organizations.selectDonorCheckbox();
+      Organizations.saveOrganization();
+      Organizations.varifySaveOrganizationCalloutMessage(organization);
+      Organizations.varifyAbsentPrivilegedDonorInformationSection();
+    },
+  );
+});
