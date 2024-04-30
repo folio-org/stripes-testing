d2ed1301ec6b2c7691f0f173a8f2a9e4584b71ea
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-04-30 14:48:04 +0300

    FAT-12947-C464314 (#3550)
    * FAT-12945: test added
    
    * FAT-12945: delete method updated
    
    * FAT-12946: test added
    
    * FAT-12945: replaced user accordion method
    
    * reverted user accordion method
    
    * FAT-12945: updated methods
    
    * FAT-12945: removed unused step
    
    * updated user acc method
    
    * methods updated
    
    * timeout added
    
    * reverted all updates
    
    * FAT-12947: preconditions added
    
    * FAT-12947: test added

---
+++ cypress/e2e/eureka/users/assigned-roles-in-user-view.cy.js
@@ -0,0 +1,120 @@
+import Users from '../../../support/fragments/users/users';
+import UsersCard from '../../../support/fragments/users/usersCard';
+import UsersSearchPane from '../../../support/fragments/users/usersSearchPane';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import AuthorizationRoles from '../../../support/fragments/settings/authorization-roles/authorizationRoles';
+import TopMenu from '../../../support/fragments/topMenu';
+
+describe('Eureka', () => {
+  describe('Users', () => {
+    const testData = {
+      role0Name: `Auto Role 0 C464314 ${getRandomPostfix()}`,
+      roleAName: `Auto Role A C464314 ${getRandomPostfix()}`,
+      roleBName: `Auto Role B C464314 ${getRandomPostfix()}`,
+    };
+
+    before('Create users, roles', () => {
+      cy.getAdminToken();
+      cy.createTempUser([]).then((createdUserProperties) => {
+        testData.tempUser = createdUserProperties;
+      });
+      cy.createTempUser([]).then((createdUserAProperties) => {
+        testData.userA = createdUserAProperties;
+      });
+      cy.createTempUser([]).then((createdUserBProperties) => {
+        testData.userB = createdUserBProperties;
+      });
+      cy.createAuthorizationRoleApi(testData.role0Name).then((role0) => {
+        testData.role0Id = role0.id;
+        cy.createAuthorizationRoleApi(testData.roleAName).then((roleA) => {
+          testData.roleAId = roleA.id;
+          cy.createAuthorizationRoleApi(testData.roleBName).then((roleB) => {
+            testData.roleBId = roleB.id;
+            cy.getCapabilitiesApi(2).then((capabs) => {
+              cy.getCapabilitySetsApi(2).then((capabSets) => {
+                cy.addCapabilitiesToNewRoleApi(
+                  testData.roleAId,
+                  capabs.map((capab) => capab.id),
+                );
+                cy.addCapabilitiesToNewRoleApi(
+                  testData.roleBId,
+                  capabs.map((capab) => capab.id),
+                );
+                cy.addCapabilitySetsToNewRoleApi(
+                  testData.roleAId,
+                  capabSets.map((capab) => capab.id),
+                );
+              });
+            });
+          });
+        });
+      });
+    });
+
+    before('Assign roles, login', () => {
+      cy.getAdminToken();
+      // TO DO: rewrite when users will not have admin role assigned upon creation
+      cy.deleteRolesForUserApi(testData.userA.userId);
+      cy.updateRolesForUserApi(testData.userB.userId, [testData.role0Id]);
+      cy.login(testData.tempUser.username, testData.tempUser.password, {
+        path: TopMenu.usersPath,
+        waiter: Users.waitLoading,
+      });
+      UsersSearchPane.searchByUsername(testData.userB.username);
+    });
+
+    after('Delete roles, users', () => {
+      cy.getAdminToken();
+      cy.deleteCapabilitiesFromRoleApi(testData.roleAId);
+      cy.deleteCapabilitiesFromRoleApi(testData.roleBId);
+      cy.deleteCapabilitySetsFromRoleApi(testData.roleAId);
+      cy.deleteAuthorizationRoleApi(testData.roleAId);
+      cy.deleteAuthorizationRoleApi(testData.roleBId);
+      cy.deleteAuthorizationRoleApi(testData.role0Id);
+      Users.deleteViaApi(testData.userA.userId);
+      Users.deleteViaApi(testData.userB.userId);
+      Users.deleteViaApi(testData.tempUser.userId);
+    });
+
+    it(
+      'C464314 Assigned roles shown in user detailed view (eureka)',
+      { tags: ['criticalPath', 'eureka', 'eurekaPhase1'] },
+      () => {
+        UsersSearchPane.selectUserFromList(testData.userB.username);
+        UsersCard.verifyUserRolesCounter('1');
+        UsersCard.clickUserRolesAccordion();
+        UsersCard.verifyUserRoleNames([testData.role0Name]);
+
+        UsersSearchPane.searchByUsername(testData.userA.username);
+        UsersSearchPane.selectUserFromList(testData.userB.username);
+        UsersCard.verifyUserRolesCounter('0');
+        UsersCard.clickUserRolesAccordion();
+        UsersCard.verifyUserRolesAccordionEmpty();
+
+        cy.visit(TopMenu.settingsAuthorizationRoles);
+        AuthorizationRoles.waitContentLoading();
+        AuthorizationRoles.searchRole(testData.roleAName);
+        AuthorizationRoles.clickOnRoleName(testData.roleAName);
+        AuthorizationRoles.clickAssignUsersButton();
+        AuthorizationRoles.selectUserInModal(testData.userA.username);
+        AuthorizationRoles.clickSaveInAssignModal();
+        AuthorizationRoles.verifyAssignedUser(testData.userA.lastName, testData.userA.firstName);
+
+        AuthorizationRoles.searchRole(testData.roleBName);
+        AuthorizationRoles.clickOnRoleName(testData.roleBName);
+        AuthorizationRoles.clickAssignUsersButton();
+        AuthorizationRoles.selectUserInModal(testData.userA.username);
+        AuthorizationRoles.clickSaveInAssignModal();
+        AuthorizationRoles.verifyAssignedUser(testData.userA.lastName, testData.userA.firstName);
+
+        cy.visit(TopMenu.usersPath);
+        Users.waitLoading();
+        UsersSearchPane.searchByUsername(testData.userA.username);
+        UsersSearchPane.selectUserFromList(testData.userA.username);
+        UsersCard.verifyUserRolesCounter('2');
+        UsersCard.clickUserRolesAccordion();
+        UsersCard.verifyUserRoleNames([testData.roleAName, testData.roleBName]);
+      },
+    );
+  });
+});
