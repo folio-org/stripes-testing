9dfa750f50717295e165ef20c7f4c3b76561d554
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2024-04-30 10:28:38 +0500

    Add the test case C436839

--- cypress/e2e/settings/marc-authority/edit-prefix-field-of-local-authority-file.cy.js
+++ cypress/e2e/settings/marc-authority/edit-prefix-field-of-local-authority-file.cy.js
@@ -9,21 +9,24 @@ describe('MARC', () => {
   describe('MARC Authority', () => {
     describe('Create MARC Authority', () => {
       const randomPostfix = getRandomPostfix();
-
+      const date = DateTools.getFormattedDateWithSlashes({ date: new Date() });
       const localAuthFile = {
         name: `C436839 auth source file active ${randomPostfix}`,
         prefix: getRandomLetters(6),
+        newPrefix: `test${getRandomLetters(4)}`,
         startWithNumber: '1',
+        baseUrl: '',
+        source: 'Local',
         isActive: true,
+        createdByAdmin: `${date} by ADMINISTRATOR, Diku_admin`,
       };
-      const date = DateTools.getFormattedDateWithSlashes({ date: new Date() });
-      const user = {};
+      let user;
 
       before('Create users, data', () => {
         cy.getAdminToken();
         cy.createTempUser([Permissions.uiSettingsManageAuthorityFiles.gui])
           .then((userProperties) => {
-            user.userProperties = userProperties;
+            user = userProperties;
           })
           .then(() => {
             cy.createAuthoritySourceFileUsingAPI(
@@ -39,8 +42,8 @@ describe('MARC', () => {
 
       after('Delete users, data', () => {
         cy.getAdminToken();
-        Users.deleteViaApi(user.userProperties.userId);
-        cy.deleteAuthoritySourceFileViaAPI(localAuthFile.id, true);
+        Users.deleteViaApi(user.userId);
+        cy.deleteAuthoritySourceFileViaAPI(localAuthFile.id);
       });
 
       it(
@@ -48,7 +51,7 @@ describe('MARC', () => {
         { tags: ['criticalPath', 'spitfire'] },
         () => {
           // 1 Go to "Settings" app >> "MARC authority" >> "Manage authority files"
-          cy.login(user.userProperties.username, user.userProperties.password, {
+          cy.login(user.username, user.password, {
             path: TopMenu.settingsAuthorityFilesPath,
             waiter: ManageAuthorityFiles.waitLoading,
           });
@@ -58,11 +61,40 @@ describe('MARC', () => {
             localAuthFile.name,
             localAuthFile.prefix,
             localAuthFile.startsWith,
-            '',
+            localAuthFile.baseUrl,
             localAuthFile.isActive,
-            `${date} by ADMINISTRATOR, DIKU`,
+            localAuthFile.createdByAdmin,
             true,
           );
+
+          // 2 Click on the "Edit" (pencil) icon of "Local" authority file
+          ManageAuthorityFiles.clickEditButton(localAuthFile.name);
+          ManageAuthorityFiles.checkRowEditableAfterClickEditButton(
+            localAuthFile.name,
+            localAuthFile.prefix,
+            localAuthFile.startWithNumber,
+            localAuthFile.baseUrl,
+            localAuthFile.source,
+            localAuthFile.createdByAdmin,
+          );
+          ManageAuthorityFiles.checkNewButtonEnabled(false);
+
+          // 3 Update value in editable "Prefix" field with unique valid value, ex.: "Prefix" = "test"
+          ManageAuthorityFiles.editField(localAuthFile.name, 'Prefix', localAuthFile.newPrefix);
+
+          // 4 Click on the "Save" button
+          ManageAuthorityFiles.clickSaveButtonAfterEditingFile(localAuthFile.name);
+          ManageAuthorityFiles.checkAfterSaveEditedFile(localAuthFile.name);
+          ManageAuthorityFiles.checkSourceFileExists(
+            localAuthFile.name,
+            localAuthFile.newPrefix,
+            localAuthFile.startWithNumber,
+            localAuthFile.baseUrl,
+            localAuthFile.isActive,
+            `${date} by ${user.lastName}, ${user.firstName}`,
+            true,
+          );
+          ManageAuthorityFiles.checkEditButtonInRow(localAuthFile.name);
         },
       );
     });
