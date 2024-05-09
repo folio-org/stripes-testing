aa8f6eebfcd0c7c52a4af9b8110157770215d920
PavloPak <pak.pavlo@gmail.com>
2024-05-09 15:33:38 +0300

    lint fix

--- cypress/e2e/users/profile-picture-validations.cy.js
+++ cypress/e2e/users/profile-picture-validations.cy.js
@@ -6,56 +6,54 @@ import UsersSearchPane from '../../support/fragments/users/usersSearchPane';
 import UserEdit from '../../support/fragments/users/userEdit';
 import ProfileCard from '../../support/fragments/users/profilePicture';
 
-
 describe('profile-picture-validation', () => {
-
   let testUser = {};
-  
+
   before('Preconditions', () => {
     cy.getAdminToken();
-    cy.createTempUser(
-      [permissions.uiUserEdit.gui, permissions.uiUsersPermissionsView.gui]
-      ).then((userProperties) => {
+    cy.createTempUser([permissions.uiUserEdit.gui, permissions.uiUsersPermissionsView.gui])
+      .then((userProperties) => {
         testUser = userProperties;
-        }).then(() => {
-          cy.loginAsAdmin({
-            path: TopMenu.usersPath,
-            waiter: UsersSearchPane.waitLoading,
-          });
+      })
+      .then(() => {
+        cy.loginAsAdmin({
+          path: TopMenu.usersPath,
+          waiter: UsersSearchPane.waitLoading,
         });
-    });
-    
-    after('Deleting created entities', () => {
-      cy.getAdminToken();
-      Users.deleteViaApi(testUser.userId);
-    });
+      });
+  });
 
-    it(
-      'C446093 Update profile picture via local storage or external Url (volaris)',
-      { tags: ['smokeBroken', 'volaris'] },
-      () => {
-        UsersSearchPane.searchByUsername(testUser.username);
-        UsersCard.waitLoading();
-        UserEdit.openEdit();
-        ProfileCard.verifyProfileCardIsPresent();
-        ProfileCard.expandUpdateDropDown();
-        ProfileCard.verifyLocalUploadbuttonIsPresent();
-        ProfileCard.setPictureFromExternalUrl();
-        ProfileCard.verifyPictureIsSet();
-      },
-    );
+  after('Deleting created entities', () => {
+    cy.getAdminToken();
+    Users.deleteViaApi(testUser.userId);
+  });
 
-    it(
-      'C442795, C442796 Verify that profile picture and associated update options display appropriately with Users: Can view, edit, and delete profile pictures',
-      { tags: ['smokeBroken', 'volaris'] },
-      () => {
-        UsersSearchPane.searchByUsername(testUser.username);
-        UsersCard.waitLoading();
-        UserEdit.openEdit();
-        ProfileCard.verifyProfileCardIsPresent();
-        ProfileCard.expandUpdateDropDown();
-        ProfileCard.deleteProfilePicture();
-        ProfileCard.verifyPictureIsRemoved();
-      },
-    );
+  it(
+    'C446093 Update profile picture via local storage or external Url (volaris)',
+    { tags: ['smokeBroken', 'volaris'] },
+    () => {
+      UsersSearchPane.searchByUsername(testUser.username);
+      UsersCard.waitLoading();
+      UserEdit.openEdit();
+      ProfileCard.verifyProfileCardIsPresent();
+      ProfileCard.expandUpdateDropDown();
+      ProfileCard.verifyLocalUploadbuttonIsPresent();
+      ProfileCard.setPictureFromExternalUrl();
+      ProfileCard.verifyPictureIsSet();
+    },
+  );
+
+  it(
+    'C442795, C442796 Verify that profile picture and associated update options display appropriately with Users: Can view, edit, and delete profile pictures',
+    { tags: ['smokeBroken', 'volaris'] },
+    () => {
+      UsersSearchPane.searchByUsername(testUser.username);
+      UsersCard.waitLoading();
+      UserEdit.openEdit();
+      ProfileCard.verifyProfileCardIsPresent();
+      ProfileCard.expandUpdateDropDown();
+      ProfileCard.deleteProfilePicture();
+      ProfileCard.verifyPictureIsRemoved();
+    },
+  );
 });
