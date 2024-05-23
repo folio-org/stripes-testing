49e1e04656c0489e400cb20cd6688e24130a4dad
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2024-05-23 10:12:59 +0300

    added slow down plugin to the framework

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,7 +19,9 @@ import './users';
 import 'cypress-file-upload';
 import 'cypress-recurse/commands';
 import './commands';
+import { slowCypressDown } from 'cypress-slow-down';
 
+slowCypressDown(100);
 registerCypressGrep();
 setInteractorTimeout(50_000);
 
