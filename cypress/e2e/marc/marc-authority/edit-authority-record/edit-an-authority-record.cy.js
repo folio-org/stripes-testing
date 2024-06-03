7f4fa3f762ab2d0d7a1b47a6db1f680f33e9eee5
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2024-06-03 17:22:27 +0500

    Move the test case C350572 to the appropriate folder and add the 'shiftLeft' tag

---
+++ cypress/e2e/marc/marc-authority/edit-authority-record/edit-an-authority-record.cy.js
@@ -0,0 +1,93 @@
+import { DEFAULT_JOB_PROFILE_NAMES } from '../../../../support/constants';
+import Permissions from '../../../../support/dictionary/permissions';
+import DataImport from '../../../../support/fragments/data_import/dataImport';
+import MarcAuthorities from '../../../../support/fragments/marcAuthority/marcAuthorities';
+import MarcAuthority from '../../../../support/fragments/marcAuthority/marcAuthority';
+import QuickMarcEditor from '../../../../support/fragments/quickMarcEditor';
+import TopMenu from '../../../../support/fragments/topMenu';
+import Users from '../../../../support/fragments/users/users';
+import getRandomPostfix from '../../../../support/utils/stringTools';
+
+describe('MARC', () => {
+  describe('MARC Authority', () => {
+    describe('Edit Authority record', () => {
+      const testData = {
+        authority: {
+          title: 'C350572 Congress and foreign policy series',
+          searchOption: 'Uniform title',
+          newField: {
+            title: `Test authority ${getRandomPostfix()}`,
+            tag: '901',
+            content: 'venn',
+          },
+        },
+      };
+      const jobProfileToRun = DEFAULT_JOB_PROFILE_NAMES.CREATE_AUTHORITY;
+      const fileName = `testMarcFile.${getRandomPostfix()}.mrc`;
+      const propertyName = 'authority';
+      let createdAuthorityID;
+
+      before('Creating data', () => {
+        cy.getAdminToken();
+        cy.createTempUser([
+          Permissions.settingsDataImportEnabled.gui,
+          Permissions.moduleDataImportEnabled.gui,
+          Permissions.uiMarcAuthoritiesAuthorityRecordView.gui,
+          Permissions.uiMarcAuthoritiesAuthorityRecordEdit.gui,
+          Permissions.uiMarcAuthoritiesAuthorityRecordDelete.gui,
+          Permissions.uiQuickMarcQuickMarcAuthoritiesEditorAll.gui,
+        ]).then((createdUserProperties) => {
+          testData.userProperties = createdUserProperties;
+
+          DataImport.uploadFileViaApi('marcAuthC350572.mrc', fileName, jobProfileToRun).then(
+            (response) => {
+              response.forEach((record) => {
+                createdAuthorityID = record[propertyName].id;
+              });
+            },
+          );
+
+          cy.login(testData.userProperties.username, testData.userProperties.password, {
+            path: TopMenu.marcAuthorities,
+            waiter: MarcAuthorities.waitLoading,
+          });
+        });
+      });
+
+      after('Deleting data', () => {
+        cy.getAdminToken();
+        if (createdAuthorityID) MarcAuthority.deleteViaAPI(createdAuthorityID);
+        Users.deleteViaApi(testData.userProperties.userId);
+      });
+
+      it(
+        'C350572 Edit an Authority record (spitfire)',
+        { tags: ['smoke', 'spitfire', 'shiftLeft'] },
+        () => {
+          MarcAuthorities.searchBy(testData.authority.searchOption, testData.authority.title);
+          MarcAuthorities.selectFirst(testData.authority.title);
+          MarcAuthority.edit();
+          MarcAuthority.addNewField(
+            5,
+            testData.authority.newField.tag,
+            `$a ${testData.authority.newField.content}`,
+          );
+          cy.wait(1000);
+          MarcAuthority.changeField('130', testData.authority.newField.title);
+          cy.wait(1000);
+          MarcAuthority.clicksaveAndCloseButton();
+          QuickMarcEditor.checkAfterSaveAndCloseAuthority();
+
+          MarcAuthority.contains(testData.authority.newField.tag);
+          MarcAuthority.contains(testData.authority.newField.content);
+
+          MarcAuthorities.searchBy(
+            testData.authority.searchOption,
+            testData.authority.newField.title,
+          );
+          MarcAuthorities.checkRow(testData.authority.newField.title);
+        },
+      );
+    });
+  });
+});
