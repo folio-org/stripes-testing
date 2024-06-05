e3eaae6e9e3d468ffd0da7657250e65ab31be2b2
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2024-06-05 18:59:21 +0300

    Fat 13973 (#3707)
    * fixed function for uploading .csv file
    
    * added tag shiftLeft
    
    * renamed function
    
    * added waiters
    
    * fixed function
    
    * added waiters
    
    * fixed indexes

--- cypress/e2e/data-import/log-details/import-summury-table-for-updating-actions.cy.js
+++ cypress/e2e/data-import/log-details/import-summury-table-for-updating-actions.cy.js
@@ -357,14 +357,13 @@ describe('Data Import', () => {
           InventorySearchAndFilter.selectYesfilterStaffSuppress();
           InventorySearchAndFilter.searchInstanceByHRID(instanceHrid);
           InstanceRecordView.verifyInstancePaneExists();
-          cy.intercept('/search/instances?query=id**').as('getIds');
           InventorySearchAndFilter.saveUUIDs();
-          // need to create a new file with instance UUID because tests are run in multiple threads
+          // need to create a new file with instance UUID because tests are runing in multiple threads
+          cy.intercept('/search/instances/ids**').as('getIds');
           cy.wait('@getIds', getLongDelay()).then((req) => {
-            const expectedUUID = InventorySearchAndFilter.getUUIDFromRequest(req);
+            const expectedUUID = InventorySearchAndFilter.getUUIDsFromRequest(req);
 
-            FileManager.createFile(`cypress/fixtures/${nameForCSVFile}`, expectedUUID);
-            FileManager.deleteFileFromDownloadsByMask('*SearchInstanceUUIDs*');
+            FileManager.createFile(`cypress/fixtures/${nameForCSVFile}`, expectedUUID[0]);
           });
         });
 
