db76a4adeaaf44f4eef77985afa20ed9c970443a
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2024-06-11 15:20:51 +0500

    Update the test case C387452

--- cypress/e2e/marc/marc-bibliographic/derive-marc-bib/008-field-existence-validation-when-derive-imported-marc-bib.cy.js
+++ cypress/e2e/marc/marc-bibliographic/derive-marc-bib/008-field-existence-validation-when-derive-imported-marc-bib.cy.js
@@ -1,4 +1,12 @@
-import { DEFAULT_JOB_PROFILE_NAMES } from '../../../../support/constants';
+import {
+  DEFAULT_JOB_PROFILE_NAMES,
+  INVENTORY_008_FIELD_DTST_DROPDOWN,
+  INVENTORY_008_FIELD_DROPDOWNS_NAMES,
+  INVENTORY_008_FIELD_CONF_DROPDOWN,
+  INVENTORY_008_FIELD_FEST_DROPDOWN,
+  INVENTORY_008_FIELD_INDX_DROPDOWN,
+  INVENTORY_008_FIELD_LITF_DROPDOWN,
+} from '../../../../support/constants';
 import Permissions from '../../../../support/dictionary/permissions';
 import DataImport from '../../../../support/fragments/data_import/dataImport';
 import InventoryInstance from '../../../../support/fragments/inventory/inventoryInstance';
@@ -14,6 +22,7 @@ describe('MARC', () => {
   describe('MARC Bibliographic', () => {
     describe('Derive MARC bib', () => {
       const testData = {
+        tag245Value: `Derived_C387452_${getRandomPostfix()}`,
         createdRecordIDs: [],
         tag008: '008',
         tag008RowIndex: 3,
@@ -37,11 +46,32 @@ describe('MARC', () => {
           'MRec',
           'Srce',
         ],
-        tag008BoxValues: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
         calloutMessage: 'Creating record may take several seconds.',
         errorCalloutMessage: 'Record cannot be saved without 008 field',
         initial008EnteredValue: DateTools.getCurrentDateYYMMDD(),
       };
+      const field008DropdownValues = [
+        {
+          dropdownLabel: INVENTORY_008_FIELD_DROPDOWNS_NAMES.DTST,
+          option: INVENTORY_008_FIELD_DTST_DROPDOWN.M,
+        },
+        {
+          dropdownLabel: INVENTORY_008_FIELD_DROPDOWNS_NAMES.CONF,
+          option: INVENTORY_008_FIELD_CONF_DROPDOWN.ONE,
+        },
+        {
+          dropdownLabel: INVENTORY_008_FIELD_DROPDOWNS_NAMES.FEST,
+          option: INVENTORY_008_FIELD_FEST_DROPDOWN.ONE,
+        },
+        {
+          dropdownLabel: INVENTORY_008_FIELD_DROPDOWNS_NAMES.INDX,
+          option: INVENTORY_008_FIELD_INDX_DROPDOWN.ONE,
+        },
+        {
+          dropdownLabel: INVENTORY_008_FIELD_DROPDOWNS_NAMES.LITF,
+          option: INVENTORY_008_FIELD_LITF_DROPDOWN.I,
+        },
+      ];
       const marcFile = {
         marc: 'marcBibFileForC387452.mrc',
         fileName: `C387452 testMarcFile${getRandomPostfix()}.mrc`,
@@ -96,10 +126,9 @@ describe('MARC', () => {
           QuickMarcEditor.checkFieldAbsense(testData.tag008);
           QuickMarcEditor.pressCancel();
           InventoryInstance.waitInventoryLoading();
-
           InventoryInstance.deriveNewMarcBibRecord();
           QuickMarcEditor.waitLoading();
-          QuickMarcEditor.updateExistingField('245', `Derived_C387452_${getRandomPostfix()}`);
+          QuickMarcEditor.updateExistingField('245', testData.tag245Value);
           QuickMarcEditor.checkFieldAbsense(testData.tag008);
           QuickMarcEditor.checkButtonSaveAndCloseEnable();
           QuickMarcEditor.pressSaveAndClose();
@@ -112,9 +141,19 @@ describe('MARC', () => {
           QuickMarcEditor.checkContent('', 4);
           QuickMarcEditor.checkDeleteButtonExist(4);
           QuickMarcEditor.updateExistingTagValue(4, testData.tag008);
+          field008DropdownValues.forEach((field008DropdownValue) => {
+            QuickMarcEditor.selectFieldsDropdownOption(
+              testData.tag008,
+              field008DropdownValue.dropdownLabel,
+              field008DropdownValue.option,
+            );
+            cy.wait(500);
+          });
           QuickMarcEditor.pressSaveAndClose();
           QuickMarcEditor.checkAfterSaveAndCloseDerive();
           InventoryInstance.editMarcBibliographicRecord();
+          QuickMarcEditor.checkContentByTag('245', `$a ${testData.tag245Value}`);
+          QuickMarcEditor.check008FieldContent();
           QuickMarcEditor.checkSubfieldsPresenceInTag008();
           QuickMarcEditor.saveInstanceIdToArrayInQuickMarc(testData.createdRecordIDs).then(() => {
             cy.getAdminToken();
