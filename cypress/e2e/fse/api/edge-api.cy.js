4054a1656304f53afc94ad1b9b5176eb6e0babfb
Kyrylo Brener <kbrener@ebsco.com>
2024-06-11 12:42:12 +0300

    draft edge tests added

--- cypress/e2e/fse/api/edge-api.cy.js
+++ cypress/e2e/fse/api/edge-api.cy.js
@@ -4,4 +4,34 @@ describe('fse-edge', () => {
       cy.expect(response.status).to.eq(200);
     });
   });
+
+  it('TC - edge-ncip verification', { tags: ['fse', 'api', 'edge'] }, () => {
+    cy.postEdgeErm().then((response) => {
+      cy.expect(response.status).to.eq(200);
+    });
+  });
+
+  it('TC - edge-oai-pmh verification', { tags: ['fse', 'api', 'edge'] }, () => {
+    cy.postEdgeErm().then((response) => {
+      cy.expect(response.status).to.eq(200);
+    });
+  });
+
+  it('TC - edge-orders verification', { tags: ['fse', 'api', 'edge'] }, () => {
+    cy.postEdgeErm().then((response) => {
+      cy.expect(response.status).to.eq(200);
+    });
+  });
+
+  it('TC - edge-patron verification', { tags: ['fse', 'api', 'edge'] }, () => {
+    cy.postEdgeErm().then((response) => {
+      cy.expect(response.status).to.eq(200);
+    });
+  });
+
+  it('TC - edge-rtac verification', { tags: ['fse', 'api', 'edge'] }, () => {
+    cy.postEdgeErm().then((response) => {
+      cy.expect(response.status).to.eq(200);
+    });
+  });
 });
