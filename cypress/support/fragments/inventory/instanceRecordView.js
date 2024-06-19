51c25286d038685acb6d62e49d1091f35e6253fc
Tetiana_Paranich <tetiana_paranich@epam.com>
2024-06-19 19:38:08 +0300

    added test

--- cypress/support/fragments/inventory/instanceRecordView.js
+++ cypress/support/fragments/inventory/instanceRecordView.js
@@ -495,4 +495,9 @@ export default {
     cy.do(rootSection.find(actionsButton).click());
     cy.expect(Button({ id: 'Add MARC holdings record' }).absent());
   },
+
+  verifyViewRequestOptionAbsent() {
+    cy.do(rootSection.find(actionsButton).click());
+    cy.expect(Button({ id: 'Add MARC holdings record' }).absent());
+  },
 };
