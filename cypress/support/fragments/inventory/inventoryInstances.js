0cde907ec9f51a69f6ad6d9222ca7960cc702f9f
Sviatlana Stsiapanava <152482600+sviatlana-stsiapanava@users.noreply.github.com>
2024-06-19 17:47:06 +0500

    FAT-14127 Support automated tests in daily run  (#3787)
    * Update the test case id according to the Test rail, add search option
    
    * Fix uploadFileWithSplitFilesViaApi() method
    
    ---------
    
    Co-authored-by: Ostap Voitsekhovskyi <Ostap_Voitsekhovskyi@epam.com>

--- cypress/support/fragments/inventory/inventoryInstances.js
+++ cypress/support/fragments/inventory/inventoryInstances.js
@@ -68,6 +68,7 @@ const searchInstancesOptions = [
   'Contributor',
   'Title (all)',
   'Identifier (all)',
+  'Classification, normalized',
   'ISBN',
   'ISSN',
   'LCCN, normalized',
@@ -114,6 +115,7 @@ const searchInstancesOptionsValues = [
   'contributor',
   'title',
   'identifier',
+  'normalizedClassificationNumber',
   'isbn',
   'issn',
   'lccn',
@@ -155,12 +157,12 @@ const searchItemsOptionsValues = [
   'iid',
   'allFields',
 ];
-const advSearchInstancesOptions = searchInstancesOptions.filter((option, index) => index <= 14);
+const advSearchInstancesOptions = searchInstancesOptions.filter((option, index) => index <= 16);
 const advSearchHoldingsOptions = searchHoldingsOptions.filter((option, index) => index <= 14);
 const advSearchItemsOptions = searchItemsOptions.filter((option, index) => index <= 14);
 const advSearchInstancesOptionsValues = searchInstancesOptionsValues
   .map((option, index) => (index ? option : 'keyword'))
-  .filter((option, index) => index <= 14);
+  .filter((option, index) => index <= 16);
 const advSearchHoldingsOptionsValues = searchHoldingsOptionsValues
   .map((option, index) => (index ? option : 'keyword'))
   .filter((option, index) => index <= 14);
