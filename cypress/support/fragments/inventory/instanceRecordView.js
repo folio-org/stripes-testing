990a1939e978ac0d34a65f38dd1dfe770982b174
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2024-06-21 13:37:13 +0300

    added test (#3797)

--- cypress/support/fragments/inventory/instanceRecordView.js
+++ cypress/support/fragments/inventory/instanceRecordView.js
@@ -496,6 +496,11 @@ export default {
     cy.expect(Button({ id: 'Add MARC holdings record' }).absent());
   },
 
+  verifyNewOrderOptionAbsent() {
+    cy.do(rootSection.find(actionsButton).click());
+    cy.expect(Button({ id: 'clickable-create-order' }).absent());
+  },
+
   verifyShareLocalInstanceOptionAbsent() {
     cy.do(rootSection.find(actionsButton).click());
     cy.expect(Button({ id: 'share-local-instance' }).absent());
