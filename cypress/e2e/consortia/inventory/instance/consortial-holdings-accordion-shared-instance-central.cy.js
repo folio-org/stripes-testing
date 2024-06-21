095daad72b2d5fe929d84a7bd0fd7753e745c4f7
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2024-06-21 10:05:43 +0300

    added new tests FAT-10358_FAT-10359

---
+++ cypress/e2e/consortia/inventory/instance/consortial-holdings-accordion-shared-instance-central.cy.js
@@ -0,0 +1,119 @@
+import uuid from 'uuid';
+import { ITEM_STATUS_NAMES } from '../../../../support/constants';
+import Affiliations from '../../../../support/dictionary/affiliations';
+import Permissions from '../../../../support/dictionary/permissions';
+import InventoryInstance from '../../../../support/fragments/inventory/inventoryInstance';
+import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
+import Locations from '../../../../support/fragments/settings/tenant/location-setup/locations';
+import ServicePoints from '../../../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import TopMenu from '../../../../support/fragments/topMenu';
+import Users from '../../../../support/fragments/users/users';
+import getRandomPostfix from '../../../../support/utils/stringTools';
+
+describe('Inventory', () => {
+  describe('Instance', () => {
+    const testData = {
+      instanceTitle: `C411636 Instance ${getRandomPostfix()}`,
+      itemBarcode: uuid(),
+    };
+
+    before('Create test data', () => {
+      cy.getAdminToken();
+      cy.getConsortiaId().then((consortiaId) => {
+        testData.consortiaId = consortiaId;
+      });
+      cy.setTenant(Affiliations.College);
+      cy.getCollegeAdminToken()
+        .then(() => {
+          cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
+            testData.instanceTypeId = instanceTypes[0].id;
+          });
+          cy.getHoldingTypes({ limit: 1 }).then((res) => {
+            testData.holdingTypeId = res[0].id;
+          });
+          const collegeLocationData = Locations.getDefaultLocation({
+            servicePointId: ServicePoints.getDefaultServicePoint().id,
+          }).location;
+          Locations.createViaApi(collegeLocationData).then((location) => {
+            testData.collegeLocation = location;
+          });
+          cy.getLoanTypes({ limit: 1 }).then((res) => {
+            testData.loanTypeId = res[0].id;
+          });
+          cy.getMaterialTypes({ limit: 1 }).then((res) => {
+            testData.materialTypeId = res.id;
+          });
+        })
+        .then(() => {
+          InventoryInstances.createFolioInstanceViaApi({
+            instance: {
+              instanceTypeId: testData.instanceTypeId,
+              title: testData.instanceTitle,
+            },
+            holdings: [
+              {
+                holdingsTypeId: testData.holdingTypeId,
+                permanentLocationId: testData.collegeLocation.id,
+              },
+            ],
+            items: [
+              {
+                barcode: testData.itemBarcode,
+                status: { name: ITEM_STATUS_NAMES.AVAILABLE },
+                permanentLoanType: { id: testData.loanTypeId },
+                materialType: { id: testData.materialTypeId },
+              },
+            ],
+          }).then((specialInstanceIds) => {
+            testData.testInstanceIds = specialInstanceIds;
+
+            InventoryInstance.shareInstanceViaApi(
+              specialInstanceIds.instanceId,
+              testData.consortiaId,
+              Affiliations.College,
+              Affiliations.Consortia,
+            );
+          });
+        });
+
+      cy.resetTenant();
+      cy.createTempUser([Permissions.inventoryAll.gui]).then((userProperties) => {
+        testData.user = userProperties;
+
+        cy.assignAffiliationToUser(Affiliations.College, testData.user.userId);
+        cy.setTenant(Affiliations.College);
+        cy.assignPermissionsToExistingUser(testData.user.userId, [Permissions.inventoryAll.gui]);
+
+        cy.login(testData.user.username, testData.user.password, {
+          path: TopMenu.inventoryPath,
+          waiter: InventoryInstances.waitContentLoading,
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.resetTenant();
+      cy.getAdminToken();
+      Users.deleteViaApi(testData.user.userId);
+      cy.setTenant(Affiliations.College);
+      InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(testData.itemBarcode);
+      Locations.deleteViaApi(testData.collegeLocation);
+    });
+
+    it(
+      'C411636 (CONSORTIA) Verify the Consortial holdings accordion details on Instance in Central Tenant (consortia) (folijet)',
+      { tags: ['extendedPathECS', 'folijet'] },
+      () => {
+        InventoryInstances.searchByTitle(testData.instanceTitle);
+        InventoryInstances.selectInstance();
+        InventoryInstance.waitLoading();
+        InventoryInstance.verifyConsortiaHoldingsAccordion(false);
+        InventoryInstance.expandConsortiaHoldings();
+        InventoryInstance.verifyMemberSubHoldingsAccordion(Affiliations.College);
+        InventoryInstance.expandMemberSubHoldings(Affiliations.College);
+        InventoryInstance.openHoldingsAccordion(testData.collegeLocation.name);
+        InventoryInstance.checkIsItemCreated(testData.itemBarcode);
+      },
+    );
+  });
+});
