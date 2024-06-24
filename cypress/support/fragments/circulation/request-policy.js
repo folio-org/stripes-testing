43c1152c3d273cd10dd344fd6e3ce8b85e827e77
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2024-06-24 12:16:44 +0300

    FAT-1391 new test

--- cypress/support/fragments/circulation/request-policy.js
+++ cypress/support/fragments/circulation/request-policy.js
@@ -1,6 +1,22 @@
 import uuid from 'uuid';
 import { getTestEntityValue } from '../../utils/stringTools';
 import { REQUEST_TYPES } from '../../constants';
+import {
+  Button,
+  MultiColumnListCell,
+  MultiColumnListRow,
+  Pane,
+  TextField,
+  TextArea,
+  including,
+  Link,
+} from '../../../../interactors';
+
+const actionsButton = Button('Actions');
+const actionsEditButton = Button({ id: 'dropdown-clickable-edit-item' });
+const nameTextField = TextField({ id: 'request_policy_name' });
+const descriptionTextField = TextArea({ id: 'request_policy_description' });
+const saveAndCloseButton = Button({ id: 'footer-save-entity' });
 
 export const defaultRequestPolicy = {
   requestTypes: [REQUEST_TYPES.HOLD],
@@ -10,6 +26,41 @@ export const defaultRequestPolicy = {
 };
 
 export default {
+  waitLoading() {
+    cy.expect(Pane('Request policies').exists());
+    cy.wait(1000);
+  },
+
+  selectRequestPolicy(requestPolicy) {
+    cy.do(Link(requestPolicy).click());
+  },
+
+  editRequestPolicy() {
+    cy.do(actionsButton.click());
+    cy.do(actionsEditButton.click());
+    cy.wait(1000);
+  },
+
+  setName(name) {
+    cy.do([
+      nameTextField.clear(),
+      nameTextField.fillIn(name),
+    ]);
+    cy.wait(1000);
+  },
+
+  setDescription(description) {
+    cy.do([
+      descriptionTextField.clear(),
+      descriptionTextField.fillIn(description),
+    ]);
+    cy.wait(1000);
+  },
+
+  save() {
+    cy.do(saveAndCloseButton.click());
+  },
+
   createViaApi(requestBody = defaultRequestPolicy) {
     return cy
       .okapiRequest({
