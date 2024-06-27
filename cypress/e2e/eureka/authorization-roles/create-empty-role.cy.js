3475ddd94a3722046c943d330462dbeb8ca6d569
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-06-27 13:30:14 +0500

    test stabilization

--- cypress/e2e/eureka/authorization-roles/create-empty-role.cy.js
+++ cypress/e2e/eureka/authorization-roles/create-empty-role.cy.js
@@ -32,7 +32,7 @@ describe('Eureka', () => {
       afterEach(() => {
         cy.getAdminToken();
         Users.deleteViaApi(testData.user.userId);
-        cy.deleteAuthorizationRoleApi(testData.roleId);
+        cy.deleteAuthorizationRoleApi(testData.roleId, true);
       });
 
       it(
