4adabd1cf42c633a356daeefb9dc75105a824a6c
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2024-06-28 17:50:39 +0500

    implemented test C466071

--- cypress/e2e/inventory/search/search-by-issn-field-is-case-insensitive.cy.js
+++ cypress/e2e/inventory/search/search-by-issn-field-is-case-insensitive.cy.js
@@ -13,27 +13,34 @@ describe('Inventory', () => {
     describe('Case-insensitive checks', () => {
       const testData = {
         defaultSearchOption: 'Keyword (title, contributor, identifier, HRID, UUID)',
-        isbnOption: 'ISBN',
+        issnOption: 'ISSN',
         identifierAllOption: 'Identifier (all)',
         allOption: 'All',
-        searchQueries: ['016037622X', '016037622x'],
-        searchResults: ['C466070 Instance 1, ISBN lower case', 'C466070 Instance 2, ISBN UPPER case'],
+        searchQueries: ['0040-782X', '0040-782x'],
+        searchResults: [
+          'C466071 (MSEARCH672 test ISSN upper case, record 1) Time.',
+          'C466071 (MSEARCH672 test ISSN lower case, record 2) Time.',
+          'C466071 (MSEARCH672 test invalid ISSN upper case, record 3) Time.',
+          'C466071 (MSEARCH672 test invalid ISSN lower case, record 4) Time.',
+          'C466071 (MSEARCH672 test linking ISSN upper case, record 5) Time.',
+          'C466071 (MSEARCH672 test linking ISSN lower case, record 6) Time.',
+        ],
       };
-  
+
       const marcFile = {
-        marc: 'marcBibFileForC466070.mrc',
-        fileName: `testMarcFileC466070.${getRandomPostfix()}.mrc`,
+        marc: 'marcBibFileForC466071.mrc',
+        fileName: `testMarcFileC466071.${getRandomPostfix()}.mrc`,
         jobProfileToRun: DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS,
         propertyName: 'instance',
       };
-  
+
       const createdRecordIDs = [];
-  
+
       before(() => {
         cy.getAdminToken();
         cy.createTempUser([Permissions.inventoryAll.gui]).then((userProperties) => {
           testData.user = userProperties;
-  
+
           DataImport.uploadFileViaApi(
             marcFile.marc,
             marcFile.fileName,
@@ -43,14 +50,14 @@ describe('Inventory', () => {
               createdRecordIDs.push(record[marcFile.propertyName].id);
             });
           });
-  
+
           cy.login(testData.user.username, testData.user.password, {
             path: TopMenu.inventoryPath,
             waiter: InventoryInstances.waitContentLoading,
           });
         });
       });
-  
+
       after(() => {
         cy.getAdminToken();
         createdRecordIDs.forEach((id) => {
@@ -58,9 +65,9 @@ describe('Inventory', () => {
         });
         Users.deleteViaApi(testData.user.userId);
       });
-  
+
       it(
-        'C466070 Search by "ISBN" field is case-insensitive (spitfire)',
+        'C466071 Search by "ISSN" field is case-insensitive (spitfire)',
         { tags: ['criticalPath', 'spitfire'] },
         () => {
           InventorySearchAndFilter.instanceTabIsDefault();
@@ -81,7 +88,7 @@ describe('Inventory', () => {
           });
           testData.searchQueries.forEach((query) => {
             InventorySearchAndFilter.resetAllAndVerifyNoResultsAppear();
-            InventorySearchAndFilter.selectSearchOptions(testData.isbnOption, '');
+            InventorySearchAndFilter.selectSearchOptions(testData.issnOption, '');
             InventorySearchAndFilter.executeSearch(query);
             testData.searchResults.forEach((expectedResult) => {
               InventorySearchAndFilter.verifySearchResult(expectedResult);
@@ -95,13 +102,13 @@ describe('Inventory', () => {
               InventorySearchAndFilter.verifySearchResult(expectedResult);
             });
           });
-  
+
           InventorySearchAndFilter.switchToHoldings();
           InventorySearchAndFilter.holdingsTabIsDefault();
           InventorySearchAndFilter.verifyResultPaneEmpty();
           InventorySearchAndFilter.verifySearchFieldIsEmpty();
           testData.searchQueries.forEach((query) => {
-            InventorySearchAndFilter.selectSearchOptions(testData.isbnOption, '');
+            InventorySearchAndFilter.selectSearchOptions(testData.issnOption, '');
             InventorySearchAndFilter.executeSearch(query);
             testData.searchResults.forEach((expectedResult) => {
               InventorySearchAndFilter.verifySearchResult(expectedResult);
@@ -112,7 +119,7 @@ describe('Inventory', () => {
           InventorySearchAndFilter.verifyResultPaneEmpty();
           InventorySearchAndFilter.verifySearchFieldIsEmpty();
           testData.searchQueries.forEach((query) => {
-            InventorySearchAndFilter.selectSearchOptions(testData.isbnOption, '');
+            InventorySearchAndFilter.selectSearchOptions(testData.issnOption, '');
             InventorySearchAndFilter.executeSearch(query);
             testData.searchResults.forEach((expectedResult) => {
               InventorySearchAndFilter.verifySearchResult(expectedResult);
