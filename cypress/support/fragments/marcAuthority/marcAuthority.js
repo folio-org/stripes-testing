be95ccdeb0d49181fa3820e33b20f831622d3ffd
Sviatlana Stsiapanava <152482600+sviatlana-stsiapanava@users.noreply.github.com>
2024-06-28 16:56:59 +0500

    Add the method addNewFieldAfterExistingByTag(), replace with it addNewField() method (#3852)

--- cypress/support/fragments/marcAuthority/marcAuthority.js
+++ cypress/support/fragments/marcAuthority/marcAuthority.js
@@ -148,6 +148,36 @@ export default {
     ]);
   },
 
+  addNewFieldAfterExistingByTag: (
+    previousFieldTag,
+    newFieldTag,
+    content,
+    indicator0 = '\\',
+    indicator1 = '\\',
+  ) => {
+    cy.do(
+      QuickMarcEditorRow({ tagValue: previousFieldTag }).perform((element) => {
+        const rowIndex = Number(element.getAttribute('data-row').match(/\d+/)[0]);
+
+        cy.do([
+          QuickMarcEditorRow({ index: rowIndex }).find(addFieldButton).click(),
+          QuickMarcEditorRow({ index: rowIndex + 1 })
+            .find(TextField({ name: `records[${rowIndex + 1}].tag` }))
+            .fillIn(newFieldTag),
+          QuickMarcEditorRow({ index: rowIndex + 1 })
+            .find(TextField({ name: `records[${rowIndex + 1}].indicators[0]` }))
+            .fillIn(indicator0),
+          QuickMarcEditorRow({ index: rowIndex + 1 })
+            .find(TextField({ name: `records[${rowIndex + 1}].indicators[1]` }))
+            .fillIn(indicator1),
+          QuickMarcEditorRow({ index: rowIndex + 1 })
+            .find(TextArea({ name: `records[${rowIndex + 1}].content` }))
+            .fillIn(content),
+        ]);
+      }),
+    );
+  },
+
   checkLinkingAuthority650: () => {
     cy.expect(buttonLink.exists());
     cy.expect(Callout('Field 650 has been linked to a MARC authority record.').exists());
