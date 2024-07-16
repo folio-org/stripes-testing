f8c9b218b53fe834fd830495671ff8f95417aa00
Tetiana_Paranich <tetiana_paranich@epam.com>
2024-07-16 19:06:52 +0300

    fixed tests

--- cypress/support/fragments/inventory/inventoryInstances.js
+++ cypress/support/fragments/inventory/inventoryInstances.js
@@ -1022,7 +1022,6 @@ export default {
   ) => {
     cy.do([actionsButton.click(), Button({ id: 'dropdown-clickable-import-record' }).click()]);
     cy.expect(singleRecordImportModal.exists());
-    cy.pause();
     cy.do(Select({ name: 'selectedJobProfileId' }).choose(profile));
     cy.wait(1000);
     cy.do([
