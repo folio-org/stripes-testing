92cdb0af8fddf49baed27621ca88e1a5a148d412
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-07-17 17:02:57 +0300

    FAT-15097-C503019 (#3935)
    * FAT-15097: test added
    
    * FAT-15097: workarounds added

---
+++ cypress/e2e/eureka/authorization-roles/selecting-capabilities-and-sets-edit.cy.js
@@ -0,0 +1,202 @@
+// import Users from '../../../support/fragments/users/users';
+import TopMenu from '../../../support/fragments/topMenu';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import AuthorizationRoles from '../../../support/fragments/settings/authorization-roles/authorizationRoles';
+
+describe('Eureka', () => {
+  describe('Settings', () => {
+    describe('Authorization roles', () => {
+      const testData = {
+        capabIds: [],
+        roleName: `Auto Role C503019 ${getRandomPostfix()}`,
+        roleDescription: `Description C503019 ${getRandomPostfix()}`,
+        updatedRoleName: `Auto Role C503019 ${getRandomPostfix()} UPD`,
+        applicationName: 'app-platform-complete',
+        capabilitySet: {
+          table: 'Data',
+          resource: 'Acquisitions-Units Memberships',
+          action: 'Manage',
+        },
+        capabilitiesInSet: [
+          {
+            table: 'Data',
+            resource: 'Acquisitions-Units Memberships Collection',
+            action: 'View',
+          },
+          {
+            table: 'Data',
+            resource: 'Acquisitions-Units Memberships Item',
+            action: 'View',
+          },
+          {
+            table: 'Data',
+            resource: 'Acquisitions-Units Memberships Item',
+            action: 'Edit',
+          },
+          {
+            table: 'Data',
+            resource: 'Acquisitions-Units Memberships Item',
+            action: 'Create',
+          },
+          {
+            table: 'Data',
+            resource: 'Acquisitions-Units Memberships Item',
+            action: 'Delete',
+          },
+        ],
+        additionalCapabilities: [
+          {
+            table: 'Data',
+            resource: 'Acquisition Piece Events',
+            action: 'View',
+          },
+          {
+            table: 'Procedural',
+            resource: 'UI-Receiving',
+            action: 'Execute',
+          },
+        ],
+        additionalCapabilitySets: [
+          {
+            table: 'Data',
+            resource: 'Calendar',
+            action: 'View',
+          },
+          {
+            table: 'Data',
+            resource: 'Licenses Contacts',
+            action: 'View',
+          },
+        ],
+        numberOfCapabilitiesInAdditionalSets: 5,
+      };
+
+      testData.capabilitySet.application = testData.applicationName;
+      testData.capabilitiesInSet.forEach((capability) => {
+        capability.application = testData.applicationName;
+      });
+      testData.additionalCapabilities.forEach((capability) => {
+        capability.application = testData.applicationName;
+      });
+      testData.additionalCapabilitySets.forEach((set) => {
+        set.application = testData.applicationName;
+      });
+
+      const capabilitiesInSetSelected = testData.capabilitiesInSet.filter(
+        (capab, index) => index <= 2,
+      );
+      const capabilitiesInSetUnselected = testData.capabilitiesInSet.filter(
+        (capab, index) => index > 2,
+      );
+
+      // TO DO: uncomment code for running under test user when evrk2 is rebuilt
+      // const capabSetsForTestUser = [
+      //   { type: 'Settings', resource: 'UI-Authorization-Roles Settings Admin', action: 'View' },
+      //   { type: 'Data', resource: 'Capabilities', action: 'Manage' },
+      //   { type: 'Data', resource: 'Role-Capability-Sets', action: 'Manage' },
+      // ];
+
+      before('Create user, data', () => {
+        // cy.createTempUser([]).then((createdUserProperties) => {
+        //   testData.user = createdUserProperties;
+        //   cy.assignCapabilitiesToExistingUser(testData.user.userId, [], capabSetsForTestUser);
+        // if (Cypress.env('runAsAdmin')) cy.updateRolesForUserApi(testData.user.userId, []);
+        cy.getAdminToken();
+        cy.createAuthorizationRoleApi(testData.roleName, testData.roleDescription).then((role) => {
+          testData.roleId = role.id;
+          capabilitiesInSetSelected.forEach((capability) => {
+            capability.type = capability.table;
+            cy.getCapabilityIdViaApi(capability).then((capabId) => {
+              testData.capabIds.push(capabId);
+            });
+          });
+          testData.additionalCapabilities.forEach((capability) => {
+            capability.type = capability.table;
+            cy.getCapabilityIdViaApi(capability).then((capabId) => {
+              testData.capabIds.push(capabId);
+            });
+          });
+        });
+      });
+      // });
+
+      before('Assign capabilities, login', () => {
+        cy.addCapabilitiesToNewRoleApi(testData.roleId, testData.capabIds);
+        // cy.login(testData.user.username, testData.user.password, {
+        //   path: TopMenu.settingsAuthorizationRoles,
+        //   waiter: AuthorizationRoles.waitContentLoading,
+        // });
+        cy.loginAsAdmin({
+          path: TopMenu.settingsAuthorizationRoles,
+          waiter: AuthorizationRoles.waitContentLoading,
+        });
+      });
+
+      after('Delete user, data', () => {
+        cy.getAdminToken();
+        // Users.deleteViaApi(testData.user.userId);
+        cy.deleteCapabilitiesFromRoleApi(testData.roleId);
+        cy.deleteAuthorizationRoleApi(testData.roleId);
+      });
+
+      it(
+        'C503019 Verify capabilities selected/deselected when selecting/deselecting a capability set when editing a role (eureka)',
+        { tags: ['criticalPath', 'eureka', 'eurekaPhase1'] },
+        () => {
+          AuthorizationRoles.searchRole(testData.roleName);
+          AuthorizationRoles.clickOnRoleName(testData.roleName);
+          AuthorizationRoles.openForEdit();
+
+          // TO DO: remove this workaround after https://folio-org.atlassian.net/browse/UIROLES-104 is fixed
+          AuthorizationRoles.closeRoleEditView();
+          AuthorizationRoles.verifyRoleViewPane(testData.roleName);
+          AuthorizationRoles.openForEdit();
+
+          AuthorizationRoles.fillRoleNameDescription(testData.updatedRoleName);
+          AuthorizationRoles.verifyCapabilitySetCheckboxChecked(testData.capabilitySet, false);
+          capabilitiesInSetSelected.forEach((capability) => {
+            AuthorizationRoles.verifyCapabilityCheckboxChecked(capability, true, true);
+          });
+          testData.additionalCapabilities.forEach((capability) => {
+            AuthorizationRoles.verifyCapabilityCheckboxChecked(capability, true, true);
+          });
+
+          AuthorizationRoles.selectCapabilitySetCheckbox(testData.capabilitySet);
+          testData.capabilitiesInSet.forEach((capability) => {
+            AuthorizationRoles.verifyCapabilityCheckboxCheckedAndDisabled(capability);
+          });
+          testData.additionalCapabilities.forEach((capability) => {
+            AuthorizationRoles.verifyCapabilityCheckboxChecked(capability, true, true);
+          });
+          testData.capabilitiesInSet.forEach((capability) => {
+            AuthorizationRoles.clickOnCheckedDisabledCheckbox(capability);
+          });
+
+          AuthorizationRoles.selectCapabilitySetCheckbox(testData.capabilitySet, false);
+          capabilitiesInSetSelected.forEach((capability) => {
+            AuthorizationRoles.verifyCapabilityCheckboxChecked(capability, true, true);
+          });
+          testData.additionalCapabilities.forEach((capability) => {
+            AuthorizationRoles.verifyCapabilityCheckboxChecked(capability, true, true);
+          });
+          capabilitiesInSetUnselected.forEach((capability) => {
+            AuthorizationRoles.verifyCapabilityCheckboxUncheckedAndEnabled(capability);
+          });
+          testData.additionalCapabilitySets.forEach((capabilitySet) => {
+            AuthorizationRoles.selectCapabilitySetCheckbox(capabilitySet);
+          });
+          AuthorizationRoles.clickSaveButton();
+
+          AuthorizationRoles.checkAfterSaveEdit(testData.roleName);
+          AuthorizationRoles.checkCapabilitySetsAccordionCounter('2');
+          AuthorizationRoles.checkCapabilitiesAccordionCounter(
+            capabilitiesInSetSelected.length +
+              testData.additionalCapabilities.length +
+              testData.numberOfCapabilitiesInAdditionalSets +
+              '',
+          );
+        },
+      );
+    });
+  });
+});
