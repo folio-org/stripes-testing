ee3b59323becc0ce244bc5bbc4dcbc0714965ffa
Tetiana_Paranich <tetiana_paranich@epam.com>
2024-07-23 21:10:26 +0300

    added test

---
+++ cypress/e2e/settings/data-import/no-error-appears-after-editing-invoice-field-mapping-profile.cy.js
@@ -0,0 +1,72 @@
+import {
+  BATCH_GROUP,
+  FOLIO_RECORD_TYPE,
+  PAYMENT_METHOD,
+  VENDOR_NAMES,
+} from '../../../support/constants';
+import { Permissions } from '../../../support/dictionary';
+import { FieldMappingProfiles as SettingsFieldMappingProfiles } from '../../../support/fragments/settings/dataImport';
+import FieldMappingProfileView from '../../../support/fragments/settings/dataImport/fieldMappingProfile/fieldMappingProfileView';
+import FieldMappingProfiles from '../../../support/fragments/settings/dataImport/fieldMappingProfile/fieldMappingProfiles';
+import NewFieldMappingProfile from '../../../support/fragments/settings/dataImport/fieldMappingProfile/newFieldMappingProfile';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix from '../../../support/utils/stringTools';
+
+describe('Data Import', () => {
+  describe('Settings', () => {
+    let user;
+    const profileForDuplicate = FieldMappingProfiles.mappingProfileForDuplicate.gobi;
+    const mappingProfile = {
+      name: `C491301 autoTestMappingProf.${getRandomPostfix()}`,
+      incomingRecordType: NewFieldMappingProfile.incomingRecordType.edifact,
+      typeValue: FOLIO_RECORD_TYPE.INVOICE,
+      description: '',
+      batchGroup: BATCH_GROUP.AMHERST,
+      organizationName: VENDOR_NAMES.GOBI,
+      paymentMethod: PAYMENT_METHOD.CASH,
+    };
+    const newMappingProfileName = `C491301 newAutoTestMappingProf.${getRandomPostfix()}`;
+
+    before('Create user and login', () => {
+      cy.createTempUser([Permissions.settingsDataImportEnabled.gui]).then((userProperties) => {
+        user = userProperties;
+
+        cy.login(userProperties.username, userProperties.password, {
+          path: SettingsMenu.mappingProfilePath,
+          waiter: FieldMappingProfiles.waitLoading,
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(user.userId);
+        SettingsFieldMappingProfiles.deleteMappingProfileByNameViaApi(newMappingProfileName);
+      });
+    });
+
+    it(
+      'C491301 Verify that no error appears after editing Invoice field mapping profile (folijet)',
+      { tags: ['criticalPath', 'folijet'] },
+      () => {
+        // create Field mapping profile
+        FieldMappingProfiles.waitLoading();
+        FieldMappingProfiles.createInvoiceMappingProfile(mappingProfile, profileForDuplicate);
+        FieldMappingProfiles.checkMappingProfilePresented(mappingProfile.name);
+
+        const FieldMappingProfileEditForm = FieldMappingProfileView.clickEditButton();
+        FieldMappingProfileEditForm.fillMappingProfileFields({
+          summary: { name: newMappingProfileName },
+        });
+        FieldMappingProfileEditForm.verifyFormView();
+        FieldMappingProfileEditForm.clickSaveAndCloseButton({
+          profileCreated: false,
+        });
+        FieldMappingProfileView.checkSummaryFieldsConditions([
+          { label: 'Name', conditions: { value: newMappingProfileName } },
+        ]);
+      },
+    );
+  });
+});
