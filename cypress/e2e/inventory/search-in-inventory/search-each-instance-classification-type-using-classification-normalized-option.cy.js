1cea9015f8d1d45c2a3d2d193701bcc434a55d0c
Sherzod-Kenjaev <114989418+Sherzod-Kenjaev@users.noreply.github.com>
2024-08-08 10:35:01 +0500

    implemented test C466145 (#4031)

---
+++ cypress/e2e/inventory/search-in-inventory/search-each-instance-classification-type-using-classification-normalized-option.cy.js
@@ -0,0 +1,186 @@
+import { Permissions } from '../../../support/dictionary';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import Users from '../../../support/fragments/users/users';
+import TopMenu from '../../../support/fragments/topMenu';
+import ClassificationIdentifierTypes from '../../../support/fragments/settings/inventory/instances/classificationIdentifierTypes';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import {
+  CLASSIFICATION_IDENTIFIER_TYPES,
+  DEFAULT_JOB_PROFILE_NAMES,
+} from '../../../support/constants';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+
+describe('Inventory', () => {
+  describe('Search in Inventory', () => {
+    const testData = {
+      folioInstances: [
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 1 - Additional Dewey',
+          classificationType: CLASSIFICATION_IDENTIFIER_TYPES.ADDITIONAL_DEWEY,
+          classificationValue: '598.099',
+        },
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 2 - Canadian Classification',
+          classificationType: CLASSIFICATION_IDENTIFIER_TYPES.CANADIAN_CLASSIFICATION,
+          classificationValue: 'HT154',
+        },
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 6 - LC (local)',
+          classificationType: CLASSIFICATION_IDENTIFIER_TYPES.DEWEY,
+          classificationValue: 'DD259.4 .B527 1973',
+        },
+        {
+          instanceTitle:
+            'C466145 Search by Classification Instance 7 - National Agricultural Library',
+          classificationType: CLASSIFICATION_IDENTIFIER_TYPES.GDC,
+          classificationValue: 'HD3492.H8',
+        },
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 9 - SUDOC',
+          classificationType: CLASSIFICATION_IDENTIFIER_TYPES.LC,
+          classificationValue: 'L37.s:Oc1/2/991',
+        },
+      ],
+      marcRecordsTitlesAndClassifications: [
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 3 - Dewey',
+          classificationValue: '598.0994',
+        },
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 4 - GDC',
+          classificationValue: 'HEU/G74.3C49',
+        },
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 5 - LC',
+          classificationValue: 'QS 11 .GA1 E53 2005',
+        },
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 8 - NLM',
+          classificationValue: 'SB945.A5',
+        },
+        {
+          instanceTitle: 'C466145 Search by Classification Instance 10 - UDC ',
+          classificationValue: '631.321:631.411.3',
+        },
+      ],
+      classificationOption: 'Classification, normalized',
+      localInstnaceClassificationValue: 'VP000321',
+      instanceTitleWithLocalClassification: 'C466145 Search by Classification Instance 11 - Local',
+    };
+    const marcFile = {
+      marc: 'marcBibFileForC466145.mrc',
+      fileName: `testMarcFileC466145.${getRandomPostfix()}.mrc`,
+      jobProfileToRun: DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS,
+      propertyName: 'instance',
+    };
+    const createdRecordIDs = [];
+    const localClassificationIdentifierType = {
+      name: `C466145 Classification identifier type ${getRandomPostfix()}`,
+      source: 'local',
+    };
+    let classificationIdentifierTypeId;
+    let user;
+    const search = (query, expectedResult) => {
+      InventorySearchAndFilter.selectSearchOption(testData.classificationOption);
+      InventorySearchAndFilter.executeSearch(query);
+      InventorySearchAndFilter.verifySearchResult(expectedResult);
+      InventorySearchAndFilter.resetAllAndVerifyNoResultsAppear();
+    };
+
+    before('Create user, test data', () => {
+      cy.getAdminToken();
+      cy.createTempUser([Permissions.uiInventoryViewInstances.gui]).then(
+        (createdUserProperties) => {
+          user = createdUserProperties;
+
+          DataImport.uploadFileViaApi(
+            marcFile.marc,
+            marcFile.fileName,
+            marcFile.jobProfileToRun,
+          ).then((response) => {
+            response.forEach((record) => {
+              createdRecordIDs.push(record[marcFile.propertyName].id);
+            });
+          });
+
+          ClassificationIdentifierTypes.createViaApi(localClassificationIdentifierType).then(
+            (response) => {
+              classificationIdentifierTypeId = response.body.id;
+            },
+          );
+
+          cy.getInstanceTypes({ limit: 1 })
+            .then((instanceTypes) => {
+              testData.instanceTypeId = instanceTypes[0].id;
+            })
+            .then(() => {
+              testData.folioInstances.forEach((folioInstance) => {
+                InventoryInstances.createFolioInstanceViaApi({
+                  instance: {
+                    instanceTypeId: testData.instanceTypeId,
+                    title: folioInstance.instanceTitle,
+                    classifications: [
+                      {
+                        classificationNumber: folioInstance.classificationValue,
+                        classificationTypeId: folioInstance.classificationType,
+                      },
+                    ],
+                  },
+                }).then((instance) => {
+                  createdRecordIDs.push(instance.instanceId);
+                });
+              });
+              InventoryInstances.createFolioInstanceViaApi({
+                instance: {
+                  instanceTypeId: testData.instanceTypeId,
+                  title: testData.instanceTitleWithLocalClassification,
+                  classifications: [
+                    {
+                      classificationNumber: testData.localInstnaceClassificationValue,
+                      classificationTypeId: classificationIdentifierTypeId,
+                    },
+                  ],
+                },
+              }).then((instance) => {
+                createdRecordIDs.push(instance.instanceId);
+              });
+            });
+
+          cy.login(user.username, user.password, {
+            path: TopMenu.inventoryPath,
+            waiter: InventoryInstances.waitContentLoading,
+          });
+          InventorySearchAndFilter.instanceTabIsDefault();
+        },
+      );
+    });
+
+    after('Delete user, test data', () => {
+      cy.getAdminToken();
+      ClassificationIdentifierTypes.deleteViaApi(classificationIdentifierTypeId);
+      createdRecordIDs.forEach((id) => {
+        InventoryInstance.deleteInstanceViaApi(id);
+      });
+      Users.deleteViaApi(user.userId);
+    });
+
+    it(
+      'C466145 Search for each Instance classification type using "Classification, normalized" search option (spitfire)',
+      { tags: ['criticalPath', 'spitfire'] },
+      () => {
+        testData.folioInstances.forEach((folioInstance) => {
+          search(folioInstance.classificationValue, folioInstance.instanceTitle);
+        });
+        testData.marcRecordsTitlesAndClassifications.forEach((marcInstance) => {
+          search(marcInstance.classificationValue, marcInstance.instanceTitle);
+        });
+        search(
+          testData.localInstnaceClassificationValue,
+          testData.instanceTitleWithLocalClassification,
+        );
+      },
+    );
+  });
+});
