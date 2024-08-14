6dd66abc7240be7ee8aa497e816876226d03f61a
IhorBohdan <ihor_bohdan@epam.com>
2024-08-14 22:00:39 +0300

    Implement FAT-15440

--- cypress/e2e/finance/funds/correct-financial-summary-values-when-encumbered-amount-exceeds-available-amount.cy.js
+++ cypress/e2e/finance/funds/correct-financial-summary-values-when-encumbered-amount-exceeds-available-amount.cy.js
@@ -12,30 +12,25 @@ import NewLocation from '../../../support/fragments/settings/tenant/locations/ne
 import ServicePoints from '../../../support/fragments/settings/tenant/servicePoints/servicePoints';
 import TopMenu from '../../../support/fragments/topMenu';
 import Users from '../../../support/fragments/users/users';
-import DateTools from '../../../support/utils/dateTools';
 import getRandomPostfix from '../../../support/utils/stringTools';
 import Budgets from '../../../support/fragments/finance/budgets/budgets';
 import {
   ACQUISITION_METHOD_NAMES_IN_PROFILE,
   INVOICE_STATUSES,
   ORDER_STATUSES,
-  ORDER_LINE_PAYMENT_STATUS,
-  RECEIPT_STATUS_VIEW,
 } from '../../../support/constants';
 import BasicOrderLine from '../../../support/fragments/orders/basicOrderLine';
 import MaterialTypes from '../../../support/fragments/settings/inventory/materialTypes';
+import FinanceHelp from '../../../support/fragments/finance/financeHelper';
+import BudgetDetails from '../../../support/fragments/finance/budgets/budgetDetails';
 
 describe('Finance: Funds', () => {
   const firstFiscalYear = { ...FiscalYears.defaultUiFiscalYear };
-  const secondFiscalYear = {
-    name: `autotest_year_${getRandomPostfix()}`,
-    code: DateTools.getRandomFiscalYearCode(2000, 9999),
-    periodStart: `${DateTools.getDayTomorrowDateForFiscalYear()}T00:00:00.000+00:00`,
-    periodEnd: `${DateTools.get2DaysAfterTomorrowDateForFiscalYear()}T00:00:00.000+00:00`,
-    description: `This is fiscal year created by E2E test automation script_${getRandomPostfix()}`,
-    series: 'FY',
+  const defaultLedger = {
+    ...Ledgers.defaultUiLedger,
+    restrictEncumbrance: false,
+    restrictExpenditures: false,
   };
-  const defaultLedger = { ...Ledgers.defaultUiLedger };
   const firstFund = { ...Funds.defaultUiFund };
   const secondFund = {
     name: `autotest_fund2_${getRandomPostfix()}`,
@@ -53,12 +48,14 @@ describe('Finance: Funds', () => {
   };
   const firstBudget = {
     ...Budgets.getDefaultBudget(),
-    allocated: 100,
+    allocated: 1000,
+    allowableEncumbrance: 100,
+    allowableExpenditure: 100,
+    allocationFrom: 500,
   };
   const organization = { ...NewOrganization.defaultUiOrganizations };
   let firstInvoice;
   let user;
-  let firstOrderNumber;
   let servicePointId;
   let location;
 
@@ -69,7 +66,6 @@ describe('Finance: Funds', () => {
       firstFiscalYear.id = firstFiscalYearResponse.id;
       firstBudget.fiscalYearId = firstFiscalYearResponse.id;
       defaultLedger.fiscalYearOneId = firstFiscalYear.id;
-      secondFiscalYear.code = firstFiscalYear.code.slice(0, -1) + '2';
       Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
         defaultLedger.id = ledgerResponse.id;
         firstFund.ledgerId = defaultLedger.id;
@@ -98,11 +94,11 @@ describe('Finance: Funds', () => {
                         const firstOrderLine = {
                           ...BasicOrderLine.defaultOrderLine,
                           cost: {
-                            listUnitPrice: 1.0,
+                            listUnitPrice: 1000,
                             currency: 'USD',
                             discountType: 'percentage',
                             quantityPhysical: 1,
-                            poLineEstimatedPrice: 1.0,
+                            poLineEstimatedPrice: 1000,
                           },
                           fundDistribution: [
                             { code: firstFund.code, fundId: firstFund.id, value: 100 },
@@ -118,43 +114,29 @@ describe('Finance: Funds', () => {
                             volumes: [],
                           },
                         };
-                        Orders.createOrderViaApi(firstOrder).then((firstOrderResponse) => {
-                          firstOrder.id = firstOrderResponse.id;
-                          firstOrderNumber = firstOrderResponse.poNumber;
-                          firstOrderLine.purchaseOrderId = firstOrderResponse.id;
 
-                          OrderLines.createOrderLineViaApi(firstOrderLine);
-                          Orders.updateOrderViaApi({
-                            ...firstOrderResponse,
-                            workflowStatus: ORDER_STATUSES.OPEN,
+                        Invoices.createInvoiceWithInvoiceLineViaApi({
+                          vendorId: organization.id,
+                          fiscalYearId: firstFiscalYear.id,
+                          fundDistributions: firstOrderLine.fundDistribution,
+                          accountingCode: organization.erpCode,
+                          releaseEncumbrance: true,
+                          subTotal: -100,
+                        }).then((invoiceRescponse) => {
+                          firstInvoice = invoiceRescponse;
+
+                          Invoices.changeInvoiceStatusViaApi({
+                            invoice: firstInvoice,
+                            status: INVOICE_STATUSES.APPROVED,
                           });
-                          Invoices.createInvoiceWithInvoiceLineViaApi({
-                            vendorId: organization.id,
-                            fiscalYearId: firstFiscalYear.id,
-                            poLineId: firstOrderLine.id,
-                            fundDistributions: firstOrderLine.fundDistribution,
-                            accountingCode: organization.erpCode,
-                            releaseEncumbrance: true,
-                            subTotal: 1,
-                          }).then((invoiceRescponse) => {
-                            firstInvoice = invoiceRescponse;
+                          Orders.createOrderViaApi(firstOrder).then((firstOrderResponse) => {
+                            firstOrder.id = firstOrderResponse.id;
+                            firstOrderLine.purchaseOrderId = firstOrderResponse.id;
 
-                            Invoices.changeInvoiceStatusViaApi({
-                              invoice: firstInvoice,
-                              status: INVOICE_STATUSES.PAID,
-                            }).then(() => {
-                              cy.wait(4000);
-                              Orders.updateOrderViaApi({
-                                ...firstOrderResponse,
-                                workflowStatus: ORDER_STATUSES.CLOSED,
-                                closeReason: { reason: 'Ceased', note: '' },
-                              });
-                              cy.wait(4000);
-
-                              Invoices.changeInvoiceStatusViaApi({
-                                invoice: firstInvoice,
-                                status: INVOICE_STATUSES.CANCELLED,
-                              });
+                            OrderLines.createOrderLineViaApi(firstOrderLine);
+                            Orders.updateOrderViaApi({
+                              ...firstOrderResponse,
+                              workflowStatus: ORDER_STATUSES.OPEN,
                             });
                           });
                         });
@@ -168,14 +150,11 @@ describe('Finance: Funds', () => {
         });
       });
     });
-    cy.createTempUser([
-      permissions.uiOrdersView.gui,
-      permissions.uiFinanceViewFundAndBudget.gui,
-    ]).then((userProperties) => {
+    cy.createTempUser([permissions.uiFinanceViewFundAndBudget.gui]).then((userProperties) => {
       user = userProperties;
       cy.login(userProperties.username, userProperties.password, {
-        path: TopMenu.ordersPath,
-        waiter: Orders.waitLoading,
+        path: TopMenu.fundPath,
+        waiter: Funds.waitLoading,
       });
     });
   });
@@ -189,20 +168,27 @@ describe('Finance: Funds', () => {
     'C496150 Correct "Financial summary" values when encumbered amount exceeds available amount (thunderjet)',
     { tags: ['criticalPath', 'thunderjet'] },
     () => {
-      Orders.searchByParameter('PO number', firstOrderNumber);
-      Orders.selectFromResultsList(firstOrderNumber);
-      OrderLines.selectPOLInOrder();
-      OrderLines.checkPOLReceiptStatus(RECEIPT_STATUS_VIEW.CANCELLED);
-      OrderLines.checkPaymentStatusInPOL(ORDER_LINE_PAYMENT_STATUS.AWAITING_PAYMENT);
-      OrderLines.openPageCurrentEncumbrance('$0.00');
-      Funds.varifyDetailsInTransaction(
-        firstFiscalYear.code,
-        '$0.00',
-        `${firstOrderNumber}-1`,
-        'Encumbrance',
-        `${firstFund.name} (${firstFund.code})`,
-      );
-      Funds.checkStatusInTransactionDetails('Released');
+      FinanceHelp.searchByName(firstFund.name);
+      Funds.selectFund(firstFund.name);
+      Funds.selectBudgetDetails();
+      BudgetDetails.checkBudgetDetails({
+        summary: [
+          { key: 'Initial allocation', value: '$1,000.00' },
+          { key: 'Increase in allocation', value: '$0.00' },
+          { key: 'Decrease in allocation', value: '$500.00' },
+          { key: 'Total allocated', value: '$500.00' },
+          { key: 'Net transfers', value: '$0.00' },
+          { key: 'Total funding', value: '$500.00' },
+          { key: 'Encumbered', value: '$1,000.00' },
+          { key: 'Awaiting payment', value: '-$100.00' },
+          { key: 'Expended', value: '$0.00' },
+          { key: 'Credited', value: '$0.00' },
+          { key: 'Unavailable', value: '$900.00' },
+          { key: 'Over encumbrance', value: '$400.00' },
+          { key: 'Over expended', value: '$0.00' },
+        ],
+        balance: { cash: '$500.00', available: '-$400.00' },
+      });
     },
   );
 });
