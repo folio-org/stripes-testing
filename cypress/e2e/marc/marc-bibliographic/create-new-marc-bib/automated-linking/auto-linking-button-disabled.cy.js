9ae14698704d2a73b17a482fa8bb57e4b965a58c
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2024-09-06 20:56:35 +0500

    Test updates that are affected by UIQM-631

--- cypress/e2e/marc/marc-bibliographic/create-new-marc-bib/automated-linking/auto-linking-button-disabled.cy.js
+++ cypress/e2e/marc/marc-bibliographic/create-new-marc-bib/automated-linking/auto-linking-button-disabled.cy.js
@@ -31,52 +31,45 @@ describe('MARC', () => {
             tag: '100',
             content: '$aC389484 Coates, Ta-Nehisi,$eauthor. $0 n2008001084',
           },
-          { rowIndex: 5, tag: '110', content: '$aC389484 Black Panther (MG) $0 no2006108277' },
+          { rowIndex: 5, tag: '240', content: '$aC389484 Black Panther $0 no2020024230' },
           {
             rowIndex: 6,
-            tag: '111',
-            content: '$aC389484 Pimedate Ööde Filmifestival $0 no2009176429',
-          },
-          { rowIndex: 7, tag: '130', content: '$aC389484 Marvel comics $0n 80026980' },
-          { rowIndex: 8, tag: '240', content: '$aC389484 Black Panther $0 no2020024230' },
-          {
-            rowIndex: 9,
             tag: '600',
             content: '$aC389484 Black Panther$c(Fictitious character)$2fast $0 n2016004081',
           },
-          { rowIndex: 10, tag: '610', content: '$aC389484 Black Panther $0 nb2009024488' },
-          { rowIndex: 11, tag: '611', content: '$aC389484 Panther Photographic $0n 82216757' },
-          { rowIndex: 12, tag: '630', content: '$aC389484 Black Panther $0 no2023006889' },
-          { rowIndex: 13, tag: '650', content: '$aC389484 Good and evil.$2fast $0 sh2009125989' },
-          { rowIndex: 14, tag: '651', content: '$aC389484 Africa.$2fast $0 sh 85001531' },
+          { rowIndex: 7, tag: '610', content: '$aC389484 Black Panther $0 nb2009024488' },
+          { rowIndex: 8, tag: '611', content: '$aC389484 Panther Photographic $0n 82216757' },
+          { rowIndex: 9, tag: '630', content: '$aC389484 Black Panther $0 no2023006889' },
+          { rowIndex: 10, tag: '650', content: '$aC389484 Good and evil.$2fast $0 sh2009125989' },
+          { rowIndex: 11, tag: '651', content: '$aC389484 Africa.$2fast $0 sh 85001531' },
           {
-            rowIndex: 15,
+            rowIndex: 12,
             tag: '655',
             content: '$aC389484 Comics (Graphic works)$2fast $0 gf2014026266',
           },
           {
-            rowIndex: 16,
+            rowIndex: 13,
             tag: '700',
             content:
               '$aC389484 Lee, Stan,$d1922-2018,$ecreator.$0 http://id.loc.gov/authorities/names/n83169267',
           },
-          { rowIndex: 17, tag: '710', content: '$aC389484 Robinson $0 no2008081921' },
-          { rowIndex: 18, tag: '711', content: '$aC389484 Delaware $0 n  84745425' },
-          { rowIndex: 19, tag: '730', content: '$aC389484 Gone T $0n 79066095' },
-          { rowIndex: 20, tag: '800', content: '$aC389484 Neilson, Donald $0 n 79023811' },
-          { rowIndex: 21, tag: '810', content: '$aC389484 Black Panther Party $0 n 80095585' },
+          { rowIndex: 14, tag: '710', content: '$aC389484 Robinson $0 no2008081921' },
+          { rowIndex: 15, tag: '711', content: '$aC389484 Delaware $0 n  84745425' },
+          { rowIndex: 16, tag: '730', content: '$aC389484 Gone T $0n 79066095' },
+          { rowIndex: 17, tag: '800', content: '$aC389484 Neilson, Donald $0 n 79023811' },
+          { rowIndex: 18, tag: '810', content: '$aC389484 Black Panther Party $0 n 80095585' },
           {
-            rowIndex: 22,
+            rowIndex: 19,
             tag: '811',
             content: '$aC389484 Stockholm International Film Festival $0 no2018125587',
           },
-          { rowIndex: 23, tag: '830', content: '$aC389484 Black Panther $0 no2018018754' },
+          { rowIndex: 20, tag: '830', content: '$aC389484 Black Panther $0 no2018018754' },
         ];
 
         let userData = {};
 
         const linkableFields = [
-          100, 110, 111, 130, 240, 600, 610, 611, 630, 650, 651, 655, 700, 710, 711, 730, 800, 810,
+          100, 240, 600, 610, 611, 630, 650, 651, 655, 700, 710, 711, 730, 800, 810,
           811, 830,
         ];
 
@@ -166,6 +159,8 @@ describe('MARC', () => {
               );
             });
             QuickMarcEditor.checkAbsenceOfLinkHeadingsButton();
+            QuickMarcEditor.pressSaveAndClose();
+            cy.wait(1500);
             QuickMarcEditor.pressSaveAndClose();
             QuickMarcEditor.checkAfterSaveAndClose();
             InventoryInstance.getId().then((id) => {
