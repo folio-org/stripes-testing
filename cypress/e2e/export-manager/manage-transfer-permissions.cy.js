cb4cd85a0d5887b2b6b6d8b25a8e0d53bd82b745
Tetiana_Paranich <tetiana_paranich@epam.com>
2024-09-11 13:31:08 +0300

    fixed Folijet tests

--- cypress/e2e/export-manager/manage-transfer-permissions.cy.js
+++ cypress/e2e/export-manager/manage-transfer-permissions.cy.js
@@ -27,7 +27,7 @@ describe('Export Manager', () => {
 
   it(
     'C350638 Verify permissions to manage transfer criteria and other transfer settings (bama) (TaaS)',
-    { tags: ['extendedPath', 'bama'] },
+    { tags: ['extendedPathBama', 'bama'] },
     () => {
       // #1 Go to Settings > Users > Fee/fine > Transfer criteria
       // Transfer criteria option is in the list of Fee/fine options
