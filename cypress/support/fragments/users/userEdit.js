5988576c2385b9071dc74e55718eb04520fbad71
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2024-09-18 14:54:31 +0300

    Fat 16581 c446007 (#4214)
    * added test
    
    * fixed C446093, C442796, C442795
    
    * added waiters

--- cypress/support/fragments/users/userEdit.js
+++ cypress/support/fragments/users/userEdit.js
@@ -18,11 +18,15 @@ import {
   Select,
   TextArea,
   TextField,
+  not,
+  ProfilePictureCard,
+  DropdownMenu,
 } from '../../../../interactors';
 import SelectUser from '../check-out-actions/selectUser';
 import TopMenu from '../topMenu';
 import defaultUser from './userDefaultObjects/defaultUser';
 
+const rootPane = Pane('Edit');
 const permissionsList = MultiColumnList({ id: '#list-permissions' });
 const saveAndCloseBtn = Button('Save & close');
 const actionsButton = Button('Actions');
@@ -30,6 +34,7 @@ const userDetailsPane = Pane({ id: 'pane-userdetails' });
 const editButton = Button('Edit');
 const externalSystemIdTextfield = TextField('External system ID');
 const selectPermissionsModal = Modal('Select Permissions');
+const deleteProfilePicturesModal = Modal({ header: 'Delete profile picture' });
 const permissionsAccordion = Accordion({ id: 'permissions' });
 const userInformationAccordion = Accordion('User information');
 const affiliationsAccordion = Accordion('Affiliations');
@@ -54,7 +59,11 @@ const resetAllButton = Button('Reset all');
 const selectRequestType = Select({ id: 'type' });
 const cancelButton = Button('Cancel');
 const userSearch = TextField('User search');
+const profilePictureCard = ProfilePictureCard({ alt: 'Profile picture' });
 let totalRows;
+const localFileButton = Button({ dataTestID: 'localFile' });
+const externalUrlButton = Button({ dataTestID: 'externalURL' });
+const deletePictureButton = Button({ dataTestID: 'delete' });
 
 // servicePointIds is array of ids
 const addServicePointsViaApi = (servicePointIds, userId, defaultServicePointId) => cy.okapiRequest({
@@ -73,6 +82,7 @@ export default {
 
   openEdit() {
     cy.do([userDetailsPane.find(actionsButton).click(), editButton.click()]);
+    cy.expect(rootPane.exists());
   },
 
   changeMiddleName(midName) {
@@ -167,19 +177,6 @@ export default {
     cy.do(selectPermissionsModal.find(saveAndCloseBtn).click());
   },
 
-  verifyPermissionDoesNotExist(permission) {
-    cy.do([addPermissionsButton.click(), userSearch.fillIn(permission)]);
-    cy.expect(userSearch.is({ value: permission }));
-    // wait is needed to avoid so fast robot clicks
-    cy.wait(1000);
-    cy.do(Button('Search').click());
-  },
-
-  verifyPermissionDoesNotExistInSelectPermissions(permission) {
-    this.searchForPermission(permission);
-    cy.expect(selectPermissionsModal.find(HTML('The list contains no items')).exists());
-  },
-
   cancelSelectPermissionsModal() {
     cy.do(selectPermissionsModal.find(cancelButton).click());
   },
@@ -268,18 +265,6 @@ export default {
     });
   },
 
-  verifySaveAndColseIsDisabled: (status) => {
-    cy.expect(saveAndCloseBtn.has({ disabled: status }));
-  },
-
-  verifyCancelIsDisable: (status) => {
-    cy.expect(cancelButton.has({ disabled: status }));
-  },
-
-  verifyUserInformation: (allContentToCheck) => {
-    return allContentToCheck.forEach((contentToCheck) => cy.expect(Section({ id: 'editUserInfo' }, including(contentToCheck)).exists()));
-  },
-
   cancelChanges() {
     cy.do([Button('Cancel').click(), Button('Close without saving').click()]);
   },
@@ -289,7 +274,9 @@ export default {
   },
 
   saveAndClose() {
+    cy.expect(saveAndCloseBtn.has({ disabled: false }));
     cy.do(saveAndCloseBtn.click());
+    cy.expect(rootPane.absent());
   },
 
   saveEditedUser() {
@@ -335,6 +322,24 @@ export default {
       });
     }),
 
+  addProfilePictureViaApi(userId, url) {
+    return cy
+      .okapiRequest({
+        method: 'GET',
+        path: `users/${userId}`,
+      })
+      .then((response) => {
+        response.body.personal.profilePictureLink = url;
+
+        cy.okapiRequest({
+          method: 'PUT',
+          path: `users/${userId}`,
+          body: response.body,
+          isDefaultSearchParamsRequired: false,
+        });
+      });
+  },
+
   updateExternalIdViaApi(user, externalSystemId) {
     cy.updateUser({
       ...user,
@@ -375,6 +380,107 @@ export default {
       customFieldsAccordion.find(TextArea({ label: customFieldName })).fillIn(customFieldText),
     ]);
     this.saveAndClose();
+  },
+
+  selectSingleSelectValue: ({ data }) => {
+    cy.do(Select({ label: data.fieldLabel }).choose(data.firstLabel));
+  },
+
+  chooseRequestType(requestType) {
+    cy.do(selectRequestType.choose(requestType));
+  },
+
+  enterValidValueToCreateViaUi: (userData) => {
+    return cy
+      .do([
+        TextField({ id: 'adduser_lastname' }).fillIn(userData.personal.lastName),
+        Select({ id: 'adduser_group' }).choose(userData.patronGroup),
+        TextField({ name: 'barcode' }).fillIn(userData.barcode),
+        TextField({ name: 'username' }).fillIn(userData.username),
+        TextField({ id: 'adduser_email' }).fillIn(userData.personal.email),
+        saveAndCloseBtn.click(),
+      ])
+      .then(() => {
+        cy.intercept('/users').as('user');
+        return cy.wait('@user', { timeout: 80000 }).then((xhr) => xhr.response.body.id);
+      });
+  },
+
+  resetAll() {
+    cy.do(resetAllButton.click());
+    permissionsList.perform((el) => {
+      el.invoke('attr', 'aria-rowcount').then((rowCount) => {
+        expect(rowCount).to.equal(totalRows);
+      });
+    });
+  },
+
+  addAddress(type = 'Home') {
+    cy.do([Button('Add address').click(), Select('Address Type*').choose(type)]);
+  },
+
+  editUsername(username) {
+    cy.do(TextField({ id: 'adduser_username' }).fillIn(username));
+  },
+
+  setPictureFromExternalUrl(url) {
+    cy.do(externalUrlButton.click({ force: true }));
+    cy.do(
+      cy.get('#external-image-url').clear(),
+      cy.get('#external-image-url').type(url),
+      cy.get('#save-external-link-btn').click(),
+    );
+    this.verifyProfilePictureIsPresent(url);
+  },
+
+  deleteProfilePicture(user) {
+    cy.do(deletePictureButton.click({ force: true }));
+    cy.expect([
+      deleteProfilePicturesModal.exists(),
+      deleteProfilePicturesModal
+        .find(
+          HTML(
+            including(
+              `You are deleting the profile picture for ${user.lastName}, ${user.firstName}`,
+            ),
+          ),
+        )
+        .exists(),
+      deleteProfilePicturesModal.find(Button('Yes')).exists(),
+      deleteProfilePicturesModal.find(Button('No')).exists(),
+    ]);
+    cy.do(deleteProfilePicturesModal.find(Button('Yes')).click());
+    cy.expect(deleteProfilePicturesModal.absent());
+  },
+
+  expandUpdateDropDown() {
+    cy.do(Button('Update').click());
+  },
+
+  // checking
+  verifyPermissionDoesNotExist(permission) {
+    cy.do([addPermissionsButton.click(), userSearch.fillIn(permission)]);
+    cy.expect(userSearch.is({ value: permission }));
+    // wait is needed to avoid so fast robot clicks
+    cy.wait(1000);
+    cy.do(Button('Search').click());
+  },
+
+  verifyPermissionDoesNotExistInSelectPermissions(permission) {
+    this.searchForPermission(permission);
+    cy.expect(selectPermissionsModal.find(HTML('The list contains no items')).exists());
+  },
+
+  verifySaveAndColseIsDisabled: (status) => {
+    cy.expect(saveAndCloseBtn.has({ disabled: status }));
+  },
+
+  verifyCancelIsDisable: (status) => {
+    cy.expect(cancelButton.has({ disabled: status }));
+  },
+
+  verifyUserInformation: (allContentToCheck) => {
+    return allContentToCheck.forEach((contentToCheck) => cy.expect(Section({ id: 'editUserInfo' }, including(contentToCheck)).exists()));
   },
 
   verifyTextFieldPresented(fieldData) {
@@ -447,35 +553,11 @@ export default {
     cy.expect(HTML(data.helpText).exists());
   },
 
-  selectSingleSelectValue: ({ data }) => {
-    cy.do(Select({ label: data.fieldLabel }).choose(data.firstLabel));
-  },
-
-  chooseRequestType(requestType) {
-    cy.do(selectRequestType.choose(requestType));
-  },
-
   verifyUserTypeItems() {
     cy.expect([
       selectRequestType.has({ content: including('Patron') }),
       selectRequestType.has({ content: including('Staff') }),
     ]);
-  },
-
-  enterValidValueToCreateViaUi: (userData) => {
-    return cy
-      .do([
-        TextField({ id: 'adduser_lastname' }).fillIn(userData.personal.lastName),
-        Select({ id: 'adduser_group' }).choose(userData.patronGroup),
-        TextField({ name: 'barcode' }).fillIn(userData.barcode),
-        TextField({ name: 'username' }).fillIn(userData.username),
-        TextField({ id: 'adduser_email' }).fillIn(userData.personal.email),
-        saveAndCloseBtn.click(),
-      ])
-      .then(() => {
-        cy.intercept('/users').as('user');
-        return cy.wait('@user', { timeout: 80000 }).then((xhr) => xhr.response.body.id);
-      });
   },
 
   verifyUserPermissionsAccordion() {
@@ -501,20 +583,34 @@ export default {
     });
   },
 
-  resetAll() {
-    cy.do(resetAllButton.click());
-    permissionsList.perform((el) => {
-      el.invoke('attr', 'aria-rowcount').then((rowCount) => {
-        expect(rowCount).to.equal(totalRows);
-      });
+  verifyProfileCardIsPresent() {
+    cy.expect(profilePictureCard.exists());
+  },
+
+  verifyProfilePictureIsPresent(url) {
+    cy.expect(profilePictureCard.has({ src: including(url) }));
+  },
+
+  verifyPlaceholderProfilePictureIsPresent() {
+    cy.expect(profilePictureCard.has({ src: including('/./img/placeholderThumbnail') }));
+  },
+
+  verifyLocalUploadButtonIsPresent() {
+    cy.expect(localFileButton.exists());
+  },
+
+  verifyPictureIsRemoved(url) {
+    cy.expect(profilePictureCard.has({ src: not(including(url)) }));
+  },
+
+  verifyButtonsStateForProfilePicture(buttonsToCheck) {
+    this.expandUpdateDropDown();
+    buttonsToCheck.forEach((button) => {
+      cy.expect(
+        DropdownMenu({ ariaLabel: 'profile picture action menu' })
+          .find(Button(button.value))
+          .exists(),
+      );
     });
-  },
-
-  addAddress(type = 'Home') {
-    cy.do([Button('Add address').click(), Select('Address Type*').choose(type)]);
-  },
-
-  editUsername(username) {
-    cy.do(TextField({ id: 'adduser_username' }).fillIn(username));
   },
 };
