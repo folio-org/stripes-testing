3d281ce299f726b0992e578d1d939a5b47a7be32
Tetiana_Paranich <tetiana_paranich@epam.com>
2024-09-18 13:50:16 +0300

    fixed C446093, C442796, C442795

---
+++ cypress/e2e/users/profile-pictures/pp-displays-on-user-edit-with-for-user-with-users-can-view-pp.cy.js
@@ -0,0 +1,73 @@
+import Permissions from '../../../support/dictionary/permissions';
+import TopMenu from '../../../support/fragments/topMenu';
+import UserEdit from '../../../support/fragments/users/userEdit';
+import Users from '../../../support/fragments/users/users';
+import UsersCard from '../../../support/fragments/users/usersCard';
+import UsersSearchPane from '../../../support/fragments/users/usersSearchPane';
+
+describe('Users', () => {
+  describe('Profile pictures', () => {
+    const testData = {
+      externalPictureUrl:
+        'https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg',
+    };
+
+    before('Create test data and login', () => {
+      cy.getAdminToken();
+      cy.getConfigurationsEntry().then((respBody) => {
+        const id = respBody.id;
+        respBody.enabled = true;
+
+        cy.updateConfigurationsEntry(id, respBody);
+      });
+
+      // create user B
+      cy.createTempUser().then((userProperties) => {
+        testData.userB = userProperties;
+        UserEdit.addProfilePictureViaApi(testData.userB.userId, testData.externalPictureUrl);
+      });
+
+      // create user C
+      cy.createTempUser().then((userProperties) => {
+        testData.userC = userProperties;
+      });
+
+      // create user A
+      cy.createTempUser([
+        Permissions.uiUserEdit.gui,
+        Permissions.uiUserViewProfilePictores.gui,
+      ]).then((userProperties) => {
+        testData.userA = userProperties;
+
+        cy.login(testData.userA.username, testData.userA.password, {
+          path: TopMenu.usersPath,
+          waiter: UsersSearchPane.waitLoading,
+        });
+      });
+    });
+
+    after('Deleting created entities', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(testData.userA.userId);
+      Users.deleteViaApi(testData.userB.userId);
+      Users.deleteViaApi(testData.userC.userId);
+    });
+
+    it(
+      'C442796 Verify that profile picture displays on user edit with for user with Users: Can view profile pictures (volaris)',
+      { tags: ['smoke', 'volaris'] },
+      () => {
+        UsersSearchPane.searchByUsername(testData.userB.username);
+        UsersCard.waitLoading();
+        UserEdit.openEdit();
+        UserEdit.verifyProfilePictureIsPresent(testData.externalPictureUrl);
+        UserEdit.cancelEdit();
+        UsersCard.waitLoading();
+
+        UsersSearchPane.searchByUsername(testData.userC.username);
+        UsersCard.waitLoading();
+        UserEdit.verifyPlaceholderProfilePictureIsPresent();
+      },
+    );
+  });
+});
