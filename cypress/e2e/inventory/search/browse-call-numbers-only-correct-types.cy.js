0ebc97bbdaaac46c23580b7943eae964c15d2699
Vadym Yeromichev <v.eremichev@gmail.com>
2024-09-23 11:35:45 +0300

    removed unused call number option in C405530

--- cypress/e2e/inventory/search/browse-call-numbers-only-correct-types.cy.js
+++ cypress/e2e/inventory/search/browse-call-numbers-only-correct-types.cy.js
@@ -265,11 +265,6 @@ describe('Inventory', () => {
         BrowseCallNumber.valueInResultTableIsHighlighted(callNumbers[6].lc);
         BrowseCallNumber.verifyCallNumbersNotFound(filterCNsExcluding('lc', 6));
 
-        InventorySearchAndFilter.selectBrowseOption(BROWSE_CALL_NUMBER_OPTIONS.LOCAL);
-        InventorySearchAndFilter.browseSearch(callNumbers[6].local);
-        BrowseCallNumber.valueInResultTableIsHighlighted(callNumbers[6].local);
-        BrowseCallNumber.verifyCallNumbersNotFound(filterCNsExcluding('local', 6));
-
         InventorySearchAndFilter.selectBrowseOption(BROWSE_CALL_NUMBER_OPTIONS.LIBRARY_OF_MEDICINE);
         InventorySearchAndFilter.browseSearch(callNumbers[2].nlm);
         BrowseCallNumber.valueInResultTableIsHighlighted(callNumbers[2].nlm);
