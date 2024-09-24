8c59cb8ee3c9fe15fff69badfb2e54a6853f1887
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2024-09-24 14:04:39 +0500

    Add console.log() for debugging purpose

--- cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-holdings-incorrectly-formatted-identifiers.cy.js
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-holdings-incorrectly-formatted-identifiers.cy.js
@@ -25,6 +25,7 @@ const getCalloutContent = (fileName) => {
 const checkResponse = (alias, fileName) => {
   cy.wait(alias).then((interception) => {
     cy.log(interception);
+    console.log(interception);
     expect(interception.response.body.linkToTriggeringCsvFile).to.include(fileName);
     expect(interception.response.body.errorMessage).to.eq(errorMessage);
   });
