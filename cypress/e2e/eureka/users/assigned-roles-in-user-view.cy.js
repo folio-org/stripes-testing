2b67621b75890d4a3cd06da4764e6f940e0838e4
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-10-03 15:09:27 +0500

    eureka test updates (capab set changes, optimization)

--- cypress/e2e/eureka/users/assigned-roles-in-user-view.cy.js
+++ cypress/e2e/eureka/users/assigned-roles-in-user-view.cy.js
@@ -85,9 +85,6 @@ describe('Eureka', () => {
 
     after('Delete roles, users', () => {
       cy.getAdminToken();
-      cy.deleteCapabilitiesFromRoleApi(testData.roleAId);
-      cy.deleteCapabilitiesFromRoleApi(testData.roleBId);
-      cy.deleteCapabilitySetsFromRoleApi(testData.roleAId);
       cy.deleteAuthorizationRoleApi(testData.roleAId);
       cy.deleteAuthorizationRoleApi(testData.roleBId);
       cy.deleteAuthorizationRoleApi(testData.role0Id);
