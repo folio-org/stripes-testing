8a2481974998bdcc215b46cfdd8f63c514157523
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-10-07 20:55:36 +0500

    FAT-16893: test added

---
+++ cypress/e2e/eureka/login/app-details-shown-in-settings.cy.js
@@ -0,0 +1,58 @@
+import Users from '../../../support/fragments/users/users';
+import TopMenuNavigation from '../../../support/fragments/topMenuNavigation';
+import { APPLICATION_NAMES } from '../../../support/constants';
+import SoftwareVersions, {
+  SETTINGS_SUBSECTION_ABOUT,
+} from '../../../support/fragments/settings/softwareVersions/software-versions';
+
+describe('Eureka', () => {
+  describe('Login', () => {
+    const modulesExpectedToBeMissing = ['inn-reach'];
+    const appIds = [];
+    const moduleIds = [];
+    const uiModuleIds = [];
+    let tempUser;
+
+    const capabsToAssign = [{ type: 'Settings', resource: 'Settings Enabled', action: 'View' }];
+
+    before('Create user, get data', () => {
+      cy.getAdminToken();
+      cy.getApplicationsForTenantApi(Cypress.env('OKAPI_TENANT'), false).then(({ body }) => {
+        body.applicationDescriptors.forEach((app) => {
+          appIds.push(app.id);
+          moduleIds.push(...app.modules.map((module) => module.id));
+          uiModuleIds.push(...app.uiModules.map((module) => module.id));
+        });
+      });
+      cy.createTempUser([]).then((createdUserProperties) => {
+        tempUser = createdUserProperties;
+        cy.assignCapabilitiesToExistingUser(tempUser.userId, capabsToAssign, []);
+      });
+    });
+
+    after('Delete user', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(tempUser.userId);
+    });
+
+    it(
+      'C431150 Details for all available applications shown in "Settings" / "Software versions" (eureka)',
+      { tags: ['smoke', 'eureka'] },
+      () => {
+        cy.login(tempUser.username, tempUser.password);
+        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.SETTINGS, SETTINGS_SUBSECTION_ABOUT);
+        SoftwareVersions.waitLoading();
+        SoftwareVersions.checkErrorText(modulesExpectedToBeMissing);
+        for (const appId of appIds) {
+          SoftwareVersions.verifyTextPresent(appId);
+        }
+        for (const moduleId of moduleIds) {
+          SoftwareVersions.verifyTextPresent(moduleId);
+        }
+        for (const uiModuleId of uiModuleIds) {
+          SoftwareVersions.verifyTextPresent(uiModuleId);
+        }
+      },
+    );
+  });
+});
