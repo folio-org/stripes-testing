134555b234ddda750b0cd15022d2eacee98cc057
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2024-10-10 12:34:15 +0500

    small update

--- cypress/e2e/inventory/classification-browse/browse-using-classification-all-option-and-select-exact-match-result.cy.js
+++ cypress/e2e/inventory/classification-browse/browse-using-classification-all-option-and-select-exact-match-result.cy.js
@@ -32,6 +32,16 @@ describe('Inventory', () => {
 
     const createdRecordIDs = [];
 
+    const verifySearchResult = () => {
+      BrowseClassifications.verifySearchResultsTable();
+      InventorySearchAndFilter.verifySearchResultIncludingValue(testData.searchQuery);
+      BrowseClassifications.verifyResultAndItsRow(5, testData.searchQuery);
+      BrowseClassifications.verifyValueInResultTableIsHighlighted(testData.searchQuery);
+      BrowseClassifications.verifyNumberOfTitlesInRow(5, '1');
+      BrowseClassifications.verifyRowExists(4);
+      BrowseClassifications.verifyRowExists(6);
+    };
+
     before(() => {
       cy.createTempUser([Permissions.moduleDataImportEnabled.gui]).then((userProperties) => {
         testData.preconditionUserId = userProperties.userId;
@@ -95,11 +105,7 @@ describe('Inventory', () => {
       () => {
         InventorySearchAndFilter.selectBrowseOption(testData.classificationOption);
         InventorySearchAndFilter.browseSearch(testData.searchQuery);
-        BrowseClassifications.verifySearchResultsTable();
-        InventorySearchAndFilter.verifySearchResultIncludingValue(testData.searchQuery);
-        BrowseClassifications.verifyResultAndItsRow(5, testData.searchQuery);
-        BrowseClassifications.verifyValueInResultTableIsHighlighted(testData.searchQuery);
-        BrowseClassifications.verifyNumberOfTitlesInRow(5, '1');
+        verifySearchResult();
         InventorySearchAndFilter.selectFoundItemFromBrowse(testData.searchQuery);
         InventorySearchAndFilter.verifySearchOptionAndQuery(
           'Query search',
@@ -108,11 +114,7 @@ describe('Inventory', () => {
         InventorySearchAndFilter.verifyInstanceDisplayed(testData.instanceTitle);
         InventoryInstances.checkSearchResultCount(/1 record found/);
         InventorySearchAndFilter.switchToBrowseTab();
-        BrowseClassifications.verifySearchResultsTable();
-        InventorySearchAndFilter.verifySearchResultIncludingValue(testData.searchQuery);
-        BrowseClassifications.verifyResultAndItsRow(5, testData.searchQuery);
-        BrowseClassifications.verifyValueInResultTableIsHighlighted(testData.searchQuery);
-        BrowseClassifications.verifyNumberOfTitlesInRow(5, '1');
+        verifySearchResult();
       },
     );
   });
--- cypress/e2e/inventory/classification-browse/browse-using-lib-of-congress-classification-option-and-select-exact-match-result.cy.js
+++ cypress/e2e/inventory/classification-browse/browse-using-lib-of-congress-classification-option-and-select-exact-match-result.cy.js
@@ -35,6 +35,16 @@ describe('Inventory', () => {
 
     const createdRecordIDs = [];
 
+    const verifySearchResult = () => {
+      BrowseClassifications.verifySearchResultsTable();
+      InventorySearchAndFilter.verifySearchResultIncludingValue(testData.searchQuery);
+      BrowseClassifications.verifyResultAndItsRow(5, testData.searchQuery);
+      BrowseClassifications.verifyValueInResultTableIsHighlighted(testData.searchQuery);
+      BrowseClassifications.verifyNumberOfTitlesInRow(5, '1');
+      BrowseClassifications.verifyRowExists(4);
+      BrowseClassifications.verifyRowExists(6);
+    };
+
     before(() => {
       cy.createTempUser([Permissions.moduleDataImportEnabled.gui]).then((userProperties) => {
         testData.preconditionUserId = userProperties.userId;
@@ -99,11 +109,7 @@ describe('Inventory', () => {
           testData.classificationOption,
         );
         InventorySearchAndFilter.browseSearch(testData.searchQuery);
-        BrowseClassifications.verifySearchResultsTable();
-        InventorySearchAndFilter.verifySearchResultIncludingValue(testData.searchQuery);
-        BrowseClassifications.verifyResultAndItsRow(5, testData.searchQuery);
-        BrowseClassifications.verifyValueInResultTableIsHighlighted(testData.searchQuery);
-        BrowseClassifications.verifyNumberOfTitlesInRow(5, '1');
+        verifySearchResult();
         InventorySearchAndFilter.selectFoundItemFromBrowse(testData.searchQuery);
         InventorySearchAndFilter.verifySearchOptionAndQuery(
           'Query search',
@@ -112,11 +118,7 @@ describe('Inventory', () => {
         InventorySearchAndFilter.verifyInstanceDisplayed(testData.instanceTitle);
         InventoryInstances.checkSearchResultCount(/1 record found/);
         InventorySearchAndFilter.switchToBrowseTab();
-        BrowseClassifications.verifySearchResultsTable();
-        InventorySearchAndFilter.verifySearchResultIncludingValue(testData.searchQuery);
-        BrowseClassifications.verifyResultAndItsRow(5, testData.searchQuery);
-        BrowseClassifications.verifyValueInResultTableIsHighlighted(testData.searchQuery);
-        BrowseClassifications.verifyNumberOfTitlesInRow(5, '1');
+        verifySearchResult();
       },
     );
   });
