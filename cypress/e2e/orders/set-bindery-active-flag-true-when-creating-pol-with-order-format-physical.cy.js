e34a271ff314e7e77c383d1360166b921c67ff86
IhorBohdan <ihor_bohdan@epam.com>
2024-10-11 10:10:57 +0300

    Implement FAT-16802

--- cypress/e2e/orders/set-bindery-active-flag-true-when-creating-pol-with-order-format-physical.cy.js
+++ cypress/e2e/orders/set-bindery-active-flag-true-when-creating-pol-with-order-format-physical.cy.js
@@ -1,5 +1,4 @@
 import permissions from '../../support/dictionary/permissions';
-import getRandomPostfix from '../../support/utils/stringTools';
 import FiscalYears from '../../support/fragments/finance/fiscalYears/fiscalYears';
 import TopMenu from '../../support/fragments/topMenu';
 import Ledgers from '../../support/fragments/finance/ledgers/ledgers';
@@ -13,22 +12,12 @@ import NewOrganization from '../../support/fragments/organizations/newOrganizati
 import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
 import NewLocation from '../../support/fragments/settings/tenant/locations/newLocation';
 import Budgets from '../../support/fragments/finance/budgets/budgets';
-import { ACQUISITION_METHOD_NAMES_IN_PROFILE, ORDER_STATUSES } from '../../support/constants';
-import BasicOrderLine from '../../support/fragments/orders/basicOrderLine';
-import MaterialTypes from '../../support/fragments/settings/inventory/materialTypes';
-import Receiving from '../../support/fragments/receiving/receiving';
+import { ORDER_FORMAT_NAMES } from '../../support/constants';
 
-describe('Receiving', () => {
+describe('Orders', () => {
   const firstFiscalYear = { ...FiscalYears.defaultUiFiscalYear };
   const defaultLedger = { ...Ledgers.defaultUiLedger };
   const firstFund = { ...Funds.defaultUiFund };
-  const secondFund = {
-    name: `autotest_fund2_${getRandomPostfix()}`,
-    code: getRandomPostfix(),
-    externalAccountNo: getRandomPostfix(),
-    fundStatus: 'Active',
-    description: `This is fund created by E2E test automation script_${getRandomPostfix()}`,
-  };
   const firstOrder = {
     ...NewOrder.getDefaultOngoingOrder,
     orderType: 'Ongoing',
@@ -42,15 +31,10 @@ describe('Receiving', () => {
     ...Budgets.getDefaultBudget(),
     allocated: 1000,
   };
-  const routingList1 = `routingList_1${getRandomPostfix()}`;
   let enterUser;
-  let firstUser;
-  let secondUser;
-  let thirdUser;
   let firstOrderNumber;
   let servicePointId;
   let location;
-  let orderLine;
 
   before(() => {
     cy.getAdminToken();
@@ -61,98 +45,22 @@ describe('Receiving', () => {
       Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
         defaultLedger.id = ledgerResponse.id;
         firstFund.ledgerId = defaultLedger.id;
-        secondFund.ledgerId = defaultLedger.id;
 
         Funds.createViaApi(firstFund).then((fundResponse) => {
           firstFund.id = fundResponse.fund.id;
           firstBudget.fundId = fundResponse.fund.id;
           Budgets.createViaApi(firstBudget);
-          cy.createTempUser([
-            permissions.uiOrdersEdit.gui,
-            permissions.uiReceivingViewEdit.gui,
-          ]).then((firstUserProperties) => {
-            firstUser = firstUserProperties;
-            cy.createTempUser([
-              permissions.uiOrdersEdit.gui,
-              permissions.uiReceivingViewEdit.gui,
-            ]).then((secondUserProperties) => {
-              secondUser = secondUserProperties;
-              cy.createTempUser([
-                permissions.uiOrdersEdit.gui,
-                permissions.uiReceivingViewEdit.gui,
-              ]).then((thirdUserProperties) => {
-                thirdUser = thirdUserProperties;
-                ServicePoints.getViaApi().then((servicePoint) => {
-                  servicePointId = servicePoint[0].id;
-                  NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then(
-                    (res) => {
-                      location = res;
 
-                      MaterialTypes.createMaterialTypeViaApi(
-                        MaterialTypes.getDefaultMaterialType(),
-                      ).then((mtypes) => {
-                        cy.getAcquisitionMethodsApi({
-                          query: `value="${ACQUISITION_METHOD_NAMES_IN_PROFILE.PURCHASE_AT_VENDOR_SYSTEM}"`,
-                        }).then((params) => {
-                          Organizations.createOrganizationViaApi(organization).then(
-                            (responseOrganizations) => {
-                              organization.id = responseOrganizations;
-                              firstOrder.vendor = organization.id;
-                              const firstOrderLine = {
-                                ...BasicOrderLine.defaultOrderLine,
-                                cost: {
-                                  listUnitPrice: 100.0,
-                                  currency: 'USD',
-                                  discountType: 'percentage',
-                                  quantityPhysical: 1,
-                                  poLineEstimatedPrice: 100.0,
-                                },
-                                fundDistribution: [
-                                  { code: firstFund.code, fundId: firstFund.id, value: 100 },
-                                ],
-                                locations: [
-                                  { locationId: location.id, quantity: 1, quantityPhysical: 1 },
-                                ],
-                                acquisitionMethod: params.body.acquisitionMethods[0].id,
-                                physical: {
-                                  createInventory: 'Instance, Holding, Item',
-                                  materialType: mtypes.body.id,
-                                  materialSupplier: responseOrganizations,
-                                  volumes: [],
-                                },
-                              };
-                              Orders.createOrderViaApi(firstOrder).then((firstOrderResponse) => {
-                                firstOrder.id = firstOrderResponse.id;
-                                firstOrderNumber = firstOrderResponse.poNumber;
-                                firstOrderLine.purchaseOrderId = firstOrderResponse.id;
-                                OrderLines.createOrderLineViaApi(firstOrderLine);
-                                Orders.updateOrderViaApi({
-                                  ...firstOrderResponse,
-                                  workflowStatus: ORDER_STATUSES.OPEN,
-                                });
-                                cy.loginAsAdmin({
-                                  path: TopMenu.ordersPath,
-                                  waiter: Orders.waitLoading,
-                                });
-                                Orders.searchByParameter('PO number', firstOrderNumber);
-                                Orders.selectFromResultsList(firstOrderNumber);
-                                OrderLines.selectPOLInOrder();
-                                OrderLines.openRoutingLists();
-                                OrderLines.addRoutingListExist();
-                                OrderLines.addRoutingList();
-                                OrderLines.fillInRoutingListInfo(routingList1);
-                                OrderLines.assignUser(firstUser.username);
-                                OrderLines.assignUser(secondUser.username);
-                                OrderLines.assignUser(thirdUser.username);
-                                OrderLines.saveRoutingList();
-                                OrderLines.backToEditingOrder();
-                              });
-                            },
-                          );
-                        });
-                      });
-                    },
-                  );
+          ServicePoints.getViaApi().then((servicePoint) => {
+            servicePointId = servicePoint[0].id;
+            NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then((res) => {
+              location = res;
+              Organizations.createOrganizationViaApi(organization).then((responseOrganizations) => {
+                organization.id = responseOrganizations;
+                firstOrder.vendor = organization.id;
+                Orders.createOrderViaApi(firstOrder).then((firstOrderResponse) => {
+                  firstOrder.id = firstOrderResponse.id;
+                  firstOrderNumber = firstOrderResponse.poNumber;
                 });
               });
             });
@@ -161,23 +69,19 @@ describe('Receiving', () => {
       });
     });
 
-    cy.createTempUser([
-      permissions.uiOrdersEdit.gui,
-      permissions.uiReceivingViewEditDelete.gui,
-    ]).then((userProperties) => {
-      enterUser = userProperties;
-      cy.login(userProperties.username, userProperties.password, {
-        path: TopMenu.ordersPath,
-        waiter: Orders.waitLoading,
-      });
-    });
+    cy.createTempUser([permissions.uiOrdersEdit.gui, permissions.uiOrdersCreate.gui]).then(
+      (userProperties) => {
+        enterUser = userProperties;
+        cy.login(userProperties.username, userProperties.password, {
+          path: TopMenu.ordersPath,
+          waiter: Orders.waitLoading,
+        });
+      },
+    );
   });
 
   after(() => {
     cy.getAdminToken();
-    Users.deleteViaApi(enterUser.userId);
-    Users.deleteViaApi(enterUser.userId);
-    Users.deleteViaApi(enterUser.userId);
     Users.deleteViaApi(enterUser.userId);
   });
 
@@ -187,16 +91,17 @@ describe('Receiving', () => {
     () => {
       Orders.searchByParameter('PO number', firstOrderNumber);
       Orders.selectFromResultsList(firstOrderNumber);
-      OrderLines.selectPOLInOrder();
-      OrderLines.openRoutingList(routingList1);
-      OrderLines.editRoutingList();
-      OrderLines.assignUser(enterUser.username);
-      OrderLines.unAssignUserFromRoutingList(firstUser.id);
-      OrderLines.checkUserIsAbsent(firstUser.username);
-      OrderLines.saveRoutingList();
-      OrderLines.backToEditingOrder();
-      Orders.receiveOrderViaActions();
-      Receiving.selectPOLInReceive(orderLine.titleOrPackage);
+      OrderLines.addPOLine();
+      OrderLines.selectRandomInstanceInTitleLookUP('*', 15);
+      OrderLines.binderyActivePOLineInfo(
+        firstFund,
+        ORDER_FORMAT_NAMES.PHYSICAL_RESOURCE,
+        '110',
+        '1',
+        '110',
+        location.name,
+      );
+      OrderLines.checkBinderyActiveStatus(true);
     },
   );
 });
