f3b6a75f69c4b415d2f951f7e041b287a11fd9ed
Tetiana_Paranich <tetiana_paranich@epam.com>
2024-10-12 21:29:48 +0300

    added test

--- cypress/support/fragments/settings/tenant/general/readingRoom.js
+++ cypress/support/fragments/settings/tenant/general/readingRoom.js
@@ -1,21 +1,32 @@
+import { HTML, including } from '@interactors/html';
 import {
   Button,
-  MultiColumnListHeader,
-  Pane,
-  Spinner,
-  MultiColumnListCell,
-  MultiColumnListRow,
   Checkbox,
+  EditableListRow,
+  Modal,
+  MultiColumnListCell,
+  MultiColumnListHeader,
+  MultiSelect,
+  Pane,
+  PaneSet,
+  Spinner,
+  TextField,
 } from '../../../../../../interactors';
 
 const readingRoomRecordCss = 'div[id=editList-reading-room-access-settings]';
 const settingsHeaderIconCss = 'a[id=ModuleMainHeading]';
 const tenantReadingRoomOptionCss = 'nav[data-test-nav-list] a[href$=tenant-settings]';
 const readingRoomPaneCss = 'nav[data-test-nav-list] a[href$=reading-room]';
+const readingRoomPane = PaneSet({ id: 'reading-room-access-settings' });
+const newButton = Button({ id: 'clickable-add-reading-room-access-settings' });
 
 const readingRoomName = 'Autotest_Room';
 const recordLoadRetries = 10;
 const recordLoadWait = 15000;
+
+function getEditableListRow(rowNumber) {
+  return EditableListRow({ index: +rowNumber.split('-')[1] });
+}
 
 export default {
   readingRoomName,
@@ -71,6 +82,66 @@ export default {
       path: `reading-room/${readingRoomId}`,
     });
   },
+  create(readingRoom) {
+    cy.wait(1000);
+    cy.do([
+      newButton.click(),
+      readingRoomPane.find(TextField({ placeholder: 'Room name' })).fillIn(readingRoom.name),
+    ]);
+    cy.wait(1000);
+    cy.do(
+      readingRoomPane
+        .find(MultiSelect({ id: 'rra-service-point' }))
+        .choose(readingRoom.servicePointName),
+    );
+    cy.wait(1000);
+    cy.do(readingRoomPane.find(Checkbox({ ariaLabel: 'Public 0' })).click());
+    cy.wait(2000);
+    cy.do(
+      readingRoomPane.find(Button({ id: 'clickable-save-reading-room-access-settings-0' })).click(),
+    );
+    cy.wait(2000);
+    cy.expect(readingRoomPane.find(HTML(including(readingRoom.name))).exists());
+  },
+  edit(name) {
+    cy.do(
+      MultiColumnListCell({ content: name }).perform((element) => {
+        const rowNumber = element.parentElement.parentElement.getAttribute('data-row-index');
+
+        cy.do(
+          getEditableListRow(rowNumber)
+            .find(Button({ icon: 'edit' }))
+            .click(),
+        );
+        cy.wait(1000);
+        cy.do(
+          getEditableListRow(rowNumber)
+            .find(Checkbox({ ariaLabel: `Public ${rowNumber.split('-')[1]}` }))
+            .click(),
+        );
+      }),
+    );
+    cy.do(Button('Save').click());
+    cy.expect(readingRoomPane.find(HTML(including(name))).exists());
+  },
+  delete(name) {
+    cy.do(
+      MultiColumnListCell({ content: name }).perform((element) => {
+        const rowNumber = element.parentElement.parentElement.getAttribute('data-row-index');
+        cy.do(
+          getEditableListRow(rowNumber)
+            .find(Button({ icon: 'trash' }))
+            .click(),
+        );
+      }),
+    );
+    cy.do(
+      Modal({ id: 'delete-controlled-vocab-entry-confirmation' })
+        .find(Button({ id: 'clickable-delete-controlled-vocab-entry-confirmation-confirm' }))
+        .click(),
+    );
+    cy.expect(readingRoomPane.find(HTML(including(name))).absent());
+  },
   verifyColumns() {
     cy.expect([
       MultiColumnListHeader('Room name*').exists(),
@@ -89,14 +160,18 @@ export default {
       Pane('Reading room access')
         .find(MultiColumnListCell({ content: roomName }))
         .perform((element) => {
-          const rowNumber = element.parentElement.parentElement.getAttribute('data-row-index');
+          const rowNumber = element.parentElement.parentElement
+            .getAttribute('data-row-index')
+            .split('-')[1];
 
-          cy.expect(
-            MultiColumnListRow({ rowIndexInParent: rowNumber })
-              .find(Checkbox({ ariaLabel: 'Public' }))
-              .is({ disabled: true }),
-          );
+          cy.get('div[class^="mclRowFormatterContainer-"]')
+            .eq(rowNumber)
+            .find('input[type="checkbox"]')
+            .should('not.have.prop', 'checked', true);
         }),
     );
+  },
+  verifyReadingRoomPaneExists() {
+    cy.expect(readingRoomPane.exists());
   },
 };
