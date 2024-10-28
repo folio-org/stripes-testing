e1cc4a496217ab04b57fba28b2047b918a41ee11
Yauhen Viazau <Yauhen_Viazau@epam.com>
2024-10-28 20:56:44 +0500

    FAT-17238: test added

---
+++ cypress/e2e/eureka/users/authn-update-user-with-without-keycloak-api.cy.js
@@ -0,0 +1,117 @@
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix from '../../../support/utils/stringTools';
+
+describe('Eureka', () => {
+  describe('Users', () => {
+    const randomPostfix = getRandomPostfix();
+    const userBodies = [];
+    const userIds = [];
+    const testData = {
+      oldPassword: 'password',
+      newPassword: 'MyComplicatedPassword123!',
+      invalidUsername: 'nonexistentusername123',
+      notFoundErrorMessage: 'Failed to find user by name: username = ',
+      noUsernameErrorMessage: 'User without username cannot be created in Keycloak',
+    };
+
+    before('Create data', () => {
+      cy.getAdminToken();
+      cy.getUserGroups().then(() => {
+        for (let i = 1; i < 5; i++) {
+          userBodies.push({
+            type: 'staff',
+            active: true,
+            username: `user${i}c594518${randomPostfix}`,
+            patronGroup: Cypress.env('userGroups')[0].id,
+            personal: {
+              lastName: `Last ${i} c594518${randomPostfix}`,
+              email: 'testuser@test.org',
+              preferredContactTypeId: '002',
+            },
+          });
+        }
+        delete userBodies[2].username;
+        userBodies.forEach((userBody, index) => {
+          if (index < 3) {
+            cy.createUserWithoutKeycloakInEurekaApi(userBody).then((userId) => {
+              userIds.push(userId);
+            });
+          } else {
+            Users.createViaApi(userBody).then((user) => {
+              userIds.push(user.id);
+            });
+          }
+        });
+      });
+    });
+
+    after('Delete data', () => {
+      cy.getAdminToken();
+      userIds.forEach((id) => {
+        Users.deleteViaApi(id);
+        Users.deleteViaApi(id, true);
+      });
+    });
+
+    it(
+      'C594518 Updating credentials using /authn/update for users with/without Keycloak record via API (eureka)',
+      { tags: ['criticalPath', 'eureka', 'C594518'] },
+      () => {
+        cy.then(() => {
+          cy.getAdminToken();
+          cy.updateCredentials(
+            userBodies[0].username,
+            testData.oldPassword,
+            testData.newPassword,
+          ).then(({ status }) => {
+            expect(status).to.eq(204);
+            cy.checkIfUserHasKeycloakApi(userIds[0]).then((response) => {
+              expect(response.status).to.eq(204);
+            });
+          });
+          cy.updateCredentials(
+            userBodies[1].username,
+            testData.oldPassword,
+            testData.newPassword,
+            userIds[1],
+          ).then(({ status }) => {
+            expect(status).to.eq(204);
+            cy.checkIfUserHasKeycloakApi(userIds[1]).then((response) => {
+              expect(response.status).to.eq(204);
+            });
+          });
+          cy.updateCredentials(
+            testData.invalidUsername,
+            testData.oldPassword,
+            testData.newPassword,
+          ).then(({ status, body }) => {
+            expect(status).to.eq(404);
+            expect(body.errors[0].message).to.include(
+              testData.notFoundErrorMessage + testData.invalidUsername,
+            );
+          });
+          cy.updateCredentials(
+            testData.invalidUsername,
+            testData.oldPassword,
+            testData.newPassword,
+            userIds[2],
+          ).then(({ status, body }) => {
+            expect(status).to.eq(500);
+            expect(body.errors[0].message).to.include(testData.noUsernameErrorMessage);
+          });
+          cy.updateCredentials(
+            userBodies[3].username,
+            testData.oldPassword,
+            testData.newPassword,
+            userIds[3],
+          ).then(({ status }) => {
+            expect(status).to.eq(204);
+          });
+        }).then(() => {
+          cy.login(userBodies[0].username, testData.newPassword);
+          cy.login(userBodies[3].username, testData.newPassword);
+        });
+      },
+    );
+  });
+});
