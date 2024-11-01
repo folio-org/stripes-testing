237ff4776b0614e75eb00d99f1eba154cfb7c69d
Vadim Eremichev <vadym_yeremichev@epam.com>
2024-11-01 10:45:31 +0200

    Revert "removed selector to avoid multiple interactors detection (#4427)" (#4430)
    This reverts commit 75fc569dbc1ba9d25d97be39c13db2f74440d076.

--- interactors/multi-column-list.js
+++ interactors/multi-column-list.js
@@ -10,7 +10,7 @@ export const InfoRow = HTML.extend('info-row').selector('[class^=row-]').locator
 });
 
 export const MultiColumnListRow = HTML.extend('multi column list row')
-  .selector('[class^=mclRowFormatterContainer-]')
+  .selector('[data-row-inner],[class^=mclRowFormatterContainer-]')
   .locator(content)
   .filters({
     content,
