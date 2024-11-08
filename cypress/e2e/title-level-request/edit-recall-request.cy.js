18b5eb880a6e0043b2388291e3eb01ad7ff5883e
Ostap Voitsekhovskyi <Ostap_Voitsekhovskyi@epam.com>
2024-11-08 08:51:00 +0200

    added retry logic for flaky tests (#4468)
    * added retry logic for flaky tests

--- cypress/e2e/title-level-request/edit-recall-request.cy.js
+++ cypress/e2e/title-level-request/edit-recall-request.cy.js
@@ -26,159 +26,170 @@ import UsersCard from '../../support/fragments/users/usersCard';
 import InteractorsTools from '../../support/utils/interactorsTools';
 import { getTestEntityValue } from '../../support/utils/stringTools';
 
-describe('Title Level Request', () => {
-  const testData = {
-    folioInstances: InventoryInstances.generateFolioInstances({ count: 2 }),
-    servicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
-  };
-  const patronGroup = {
-    name: getTestEntityValue('GroupCircLog'),
-  };
-  let firstItemBarcode;
-  let secondItemBarcode;
+describe(
+  'Title Level Request',
+  {
+    retries: {
+      runMode: 1,
+    },
+  },
+  () => {
+    let testData;
+    let patronGroup;
+    let firstItemBarcode;
+    let secondItemBarcode;
 
-  before('Create test data', () => {
-    cy.getAdminToken();
-    ServicePoints.createViaApi(testData.servicePoint);
-    testData.defaultLocation = Locations.getDefaultLocation({
-      servicePointId: testData.servicePoint.id,
-    }).location;
-    Locations.createViaApi(testData.defaultLocation).then((location) => {
-      InventoryInstances.createFolioInstancesViaApi({
-        folioInstances: testData.folioInstances,
-        location,
-      });
-    });
-    cy.getUsers({ limit: 1, query: '"barcode"="" and "active"="true"' }).then((users) => {
-      testData.requester = users[0];
-    });
-    firstItemBarcode = testData.folioInstances[0].barcodes[0];
-    secondItemBarcode = testData.folioInstances[1].barcodes[0];
-    PatronGroups.createViaApi(patronGroup.name).then((patronGroupResponse) => {
-      patronGroup.id = patronGroupResponse;
-    });
-    TitleLevelRequests.enableTLRViaApi();
+    beforeEach('Create test data', () => {
+      testData = {
+        folioInstances: InventoryInstances.generateFolioInstances({ count: 2 }),
+        servicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
+      };
+      patronGroup = {
+        name: getTestEntityValue('GroupCircLog'),
+      };
 
-    cy.createTempUser(
-      [
-        Permissions.checkoutAll.gui,
-        Permissions.uiRequestsAll.gui,
-        Permissions.inventoryAll.gui,
-        Permissions.circulationLogAll.gui,
-        Permissions.uiUsersView.gui,
-        Permissions.loansAll.gui,
-        Permissions.loansRenewOverride.gui,
-      ],
-      patronGroup.name,
-    ).then((user) => {
-      testData.user = user;
-      UserEdit.addServicePointViaApi(
-        testData.servicePoint.id,
-        testData.user.userId,
-        testData.servicePoint.id,
-      );
-
-      cy.login(testData.user.username, testData.user.password, {
-        path: TopMenu.checkOutPath,
-        waiter: Checkout.waitLoading,
-      });
-    });
-  });
-
-  after('Delete test data', () => {
-    cy.getAdminToken();
-    testData.folioInstances.forEach((item) => {
-      CheckInActions.checkinItemViaApi({
-        itemBarcode: item.barcodes[0],
+      cy.getAdminToken();
+      ServicePoints.createViaApi(testData.servicePoint);
+      testData.defaultLocation = Locations.getDefaultLocation({
         servicePointId: testData.servicePoint.id,
-        checkInDate: new Date().toISOString(),
+      }).location;
+      Locations.createViaApi(testData.defaultLocation).then((location) => {
+        InventoryInstances.createFolioInstancesViaApi({
+          folioInstances: testData.folioInstances,
+          location,
+        });
       });
-    });
-    UserEdit.changeServicePointPreferenceViaApi(testData.user.userId, [testData.servicePoint.id]);
-    ServicePoints.deleteViaApi(testData.servicePoint.id);
-    Users.deleteViaApi(testData.user.userId);
-    PatronGroups.deleteViaApi(patronGroup.id);
-    testData.folioInstances.forEach((item) => {
-      InventoryInstances.deleteInstanceViaApi({
-        instance: item,
-        servicePoint: testData.servicePoint,
-        shouldCheckIn: true,
+      cy.getUsers({ limit: 1, query: '"barcode"="" and "active"="true"' }).then((users) => {
+        testData.requester = users[0];
       });
-    });
-
-    Locations.deleteViaApi(testData.defaultLocation);
-  });
-
-  it(
-    'C380500 Editing recall request does not change recalled item (vega) (TaaS)',
-    {
-      tags: ['criticalPath', 'vega', 'shiftLeft', 'C380500'],
-    },
-    () => {
-      // Enter patron id or choose patron with "Patron look-up" function.
-      CheckOutActions.checkOutUser(testData.user.barcode);
-      // Enter barcode for one of the items (described in Preconditions).
-      CheckOutActions.checkOutItem(firstItemBarcode);
-      Checkout.verifyResultsInTheRow([firstItemBarcode]);
-      // Check out the second item from the instance described in Preconditions to another Patron.
-      CheckOutActions.checkOutItem(secondItemBarcode);
-      Checkout.verifyResultsInTheRow([secondItemBarcode]);
-
-      TopMenuNavigation.navigateToApp(APPLICATION_NAMES.REQUESTS);
-      Requests.waitLoading();
-      NewRequest.openNewRequestPane();
-      NewRequest.waitLoadingNewRequestPage(true);
-      cy.getInstance({
-        limit: 1,
-        expandAll: true,
-        query: `"id"=="${testData.folioInstances[0].instanceId}"`,
-      }).then((instance) => {
-        NewRequest.enterHridInfo(instance.hrid);
+      firstItemBarcode = testData.folioInstances[0].barcodes[0];
+      secondItemBarcode = testData.folioInstances[1].barcodes[0];
+      PatronGroups.createViaApi(patronGroup.name).then((patronGroupResponse) => {
+        patronGroup.id = patronGroupResponse;
       });
-      NewRequest.enterRequesterInfoWithRequestType(
-        {
-          requesterBarcode: testData.requester.barcode,
-          pickupServicePoint: testData.servicePoint.name,
-        },
-        REQUEST_TYPES.RECALL,
-      );
-      NewRequest.verifyRequestInformation(REQUEST_TYPES.HOLD);
-      NewRequest.saveRequestAndClose();
-      NewRequest.waitLoading();
-      RequestDetail.checkItemBarcode(testData.requester.barcode);
-      RequestDetail.checkRequestsCount('1');
+      TitleLevelRequests.enableTLRViaApi();
 
-      TopMenuNavigation.navigateToApp(APPLICATION_NAMES.CIRCULATION_LOG);
-      SearchPane.waitLoading();
-      SearchPane.searchByItemBarcode(firstItemBarcode);
-      SearchPane.searchByLoanType('Recall requested');
-      SearchPane.checkResultSearch({
-        itemBarcode: firstItemBarcode,
-        circAction: 'Recall requested',
-      });
-
-      TopMenuNavigation.navigateToApp(APPLICATION_NAMES.REQUESTS);
-      Requests.waitLoading();
-      EditRequest.findAndOpenCreatedRequest({
-        instanceTitle: testData.folioInstances[0].instanceTitle,
-      });
-      EditRequest.editPickupServicePoint();
-      InteractorsTools.checkCalloutMessage(including('Request has been successfully edited for'));
-      RequestDetail.openItemByBarcode(firstItemBarcode);
-      ItemRecordView.waitLoading();
-      ItemRecordView.openBorrowerPage();
-      UsersCard.viewCurrentLoans();
-      UserLoans.openLoanDetails(firstItemBarcode);
-      UserLoans.renewItem(firstItemBarcode, true);
-      RenewConfirmationModal.verifyRenewConfirmationModal(
+      cy.createTempUser(
         [
-          {
-            itemBarcode: firstItemBarcode,
-            status: 'Item not renewed:',
-          },
+          Permissions.checkoutAll.gui,
+          Permissions.uiRequestsAll.gui,
+          Permissions.inventoryAll.gui,
+          Permissions.circulationLogAll.gui,
+          Permissions.uiUsersView.gui,
+          Permissions.loansAll.gui,
+          Permissions.loansRenewOverride.gui,
         ],
-        true,
-      );
-    },
-  );
-});
+        patronGroup.name,
+      ).then((user) => {
+        testData.user = user;
+        UserEdit.addServicePointViaApi(
+          testData.servicePoint.id,
+          testData.user.userId,
+          testData.servicePoint.id,
+        );
+
+        cy.login(testData.user.username, testData.user.password, {
+          path: TopMenu.checkOutPath,
+          waiter: Checkout.waitLoading,
+        });
+      });
+    });
+
+    afterEach('Delete test data', () => {
+      cy.getAdminToken();
+      testData.folioInstances.forEach((item) => {
+        CheckInActions.checkinItemViaApi({
+          itemBarcode: item.barcodes[0],
+          servicePointId: testData.servicePoint.id,
+          checkInDate: new Date().toISOString(),
+        });
+      });
+      UserEdit.changeServicePointPreferenceViaApi(testData.user.userId, [testData.servicePoint.id]);
+      ServicePoints.deleteViaApi(testData.servicePoint.id);
+      Users.deleteViaApi(testData.user.userId);
+      PatronGroups.deleteViaApi(patronGroup.id);
+      testData.folioInstances.forEach((item) => {
+        InventoryInstances.deleteInstanceViaApi({
+          instance: item,
+          servicePoint: testData.servicePoint,
+          shouldCheckIn: true,
+        });
+      });
+
+      Locations.deleteViaApi(testData.defaultLocation);
+    });
+
+    it(
+      'C380500 Editing recall request does not change recalled item (vega) (TaaS)',
+      {
+        tags: ['criticalPath', 'vega', 'shiftLeft', 'C380500'],
+      },
+      () => {
+        // Enter patron id or choose patron with "Patron look-up" function.
+        CheckOutActions.checkOutUser(testData.user.barcode);
+        // Enter barcode for one of the items (described in Preconditions).
+        CheckOutActions.checkOutItem(firstItemBarcode);
+        Checkout.verifyResultsInTheRow([firstItemBarcode]);
+        // Check out the second item from the instance described in Preconditions to another Patron.
+        CheckOutActions.checkOutItem(secondItemBarcode);
+        Checkout.verifyResultsInTheRow([secondItemBarcode]);
+
+        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.REQUESTS);
+        Requests.waitLoading();
+        NewRequest.openNewRequestPane();
+        NewRequest.waitLoadingNewRequestPage(true);
+        cy.getInstance({
+          limit: 1,
+          expandAll: true,
+          query: `"id"=="${testData.folioInstances[0].instanceId}"`,
+        }).then((instance) => {
+          NewRequest.enterHridInfo(instance.hrid);
+        });
+        NewRequest.enterRequesterInfoWithRequestType(
+          {
+            requesterBarcode: testData.requester.barcode,
+            pickupServicePoint: testData.servicePoint.name,
+          },
+          REQUEST_TYPES.RECALL,
+        );
+        NewRequest.verifyRequestInformation(REQUEST_TYPES.HOLD);
+        NewRequest.saveRequestAndClose();
+        NewRequest.waitLoading();
+        RequestDetail.checkItemBarcode(testData.requester.barcode);
+        RequestDetail.checkRequestsCount('1');
+
+        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.CIRCULATION_LOG);
+        SearchPane.waitLoading();
+        SearchPane.searchByItemBarcode(firstItemBarcode);
+        SearchPane.searchByLoanType('Recall requested');
+        SearchPane.checkResultSearch({
+          itemBarcode: firstItemBarcode,
+          circAction: 'Recall requested',
+        });
+
+        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.REQUESTS);
+        Requests.waitLoading();
+        EditRequest.findAndOpenCreatedRequest({
+          instanceTitle: testData.folioInstances[0].instanceTitle,
+        });
+        EditRequest.editPickupServicePoint();
+        InteractorsTools.checkCalloutMessage(including('Request has been successfully edited for'));
+        RequestDetail.openItemByBarcode(firstItemBarcode);
+        ItemRecordView.waitLoading();
+        ItemRecordView.openBorrowerPage();
+        UsersCard.viewCurrentLoans();
+        UserLoans.openLoanDetails(firstItemBarcode);
+        UserLoans.renewItem(firstItemBarcode, true);
+        RenewConfirmationModal.verifyRenewConfirmationModal(
+          [
+            {
+              itemBarcode: firstItemBarcode,
+              status: 'Item not renewed:',
+            },
+          ],
+          true,
+        );
+      },
+    );
+  },
+);
