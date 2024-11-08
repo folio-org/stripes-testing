02cc9c026ec45a5c0c89ac700fb342884e0e2c74
Sherzod Kenjaev <sherzod_kenjaev@epam.com>
2024-11-08 21:04:03 +0500

    Test fix eureka

--- cypress/support/fragments/inventory/inventoryInstance.js
+++ cypress/support/fragments/inventory/inventoryInstance.js
@@ -1015,7 +1015,8 @@ export default {
     InventoryInstanceSelectInstanceModal.waitLoading();
     InventoryInstanceSelectInstanceModal.searchByHrId(newInstanceHrId);
     InventoryInstanceSelectInstanceModal.selectInstance();
-    cy.wait(3000);
+    // cypress clicks too fast
+    cy.wait(5000);
     InventoryInstancesMovement.move();
   },
 
