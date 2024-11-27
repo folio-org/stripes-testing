0616b50cd5d667ef18e22645aa6fe1222c0f7049
Kyrylo Brener <kbrener@ebsco.com>
2024-11-27 17:53:39 +0200

    FAT-17512. LDE User Journey added

--- cypress/support/fragments/linked-data/work.js
+++ cypress/support/fragments/linked-data/work.js
@@ -13,6 +13,20 @@ export default {
       .then((response) => response.body.content[0].id);
   },
 
+  getInstancesByTitle: (title) => {
+    return cy
+      .okapiRequest({
+        method: 'GET',
+        path: 'search/linked-data/works',
+        isDefaultSearchParamsRequired: false,
+        searchParams: {
+          limit: 1,
+          query: `"title"=="${title}"`,
+        },
+      })
+      .then((response) => response.body.content[0].instances);
+  },
+
   deleteById: (id) => {
     return cy.okapiRequest({
       method: 'DELETE',
