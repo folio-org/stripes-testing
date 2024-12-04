e7e39b1ddf3974cc50f8198a1f1f8ef1644aa51c
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2024-12-04 18:40:35 +0500

    Fix/update test cases

--- cypress/e2e/data-import/importing-marc-bib-files/delete-linked-field-which-controlled-by-marc-authority.cy.js
+++ cypress/e2e/data-import/importing-marc-bib-files/delete-linked-field-which-controlled-by-marc-authority.cy.js
@@ -297,7 +297,7 @@ describe('Data Import', () => {
         Logs.openFileDetails(nameForUpdatedMarcFile);
         Logs.verifyInstanceStatus(0, 3, RECORD_STATUSES.UPDATED);
 
-        TopMenuNavigation.openAppFromDropdown(APPLICATION_NAMES.INVENTORY);
+        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
         InventoryInstances.searchByTitle(createdAuthorityIDs[0]);
         InventoryInstances.selectInstance();
         InventoryInstance.editMarcBibliographicRecord();
