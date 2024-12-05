e2378ecc41f274dc5a98d1129e996f4679144871
Sviatlana Stsiapanava <152482600+sviatlana-stsiapanava@users.noreply.github.com>
2024-12-05 13:59:00 +0500

    Fix/udate tests (#4531)

--- cypress/e2e/consortia/marc/marc-bibliographic/edit-marc-bib/manual-linking/link-shared-marcBib-with-shared-marcAuth-in-central.cy.js
+++ cypress/e2e/consortia/marc/marc-bibliographic/edit-marc-bib/manual-linking/link-shared-marcBib-with-shared-marcAuth-in-central.cy.js
@@ -2,11 +2,12 @@ import Permissions from '../../../../../../support/dictionary/permissions';
 import Affiliations, { tenantNames } from '../../../../../../support/dictionary/affiliations';
 import Users from '../../../../../../support/fragments/users/users';
 import TopMenu from '../../../../../../support/fragments/topMenu';
+import TopMenuNavigation from '../../../../../../support/fragments/topMenuNavigation';
 import InventoryInstances from '../../../../../../support/fragments/inventory/inventoryInstances';
 import getRandomPostfix from '../../../../../../support/utils/stringTools';
 import InventoryInstance from '../../../../../../support/fragments/inventory/inventoryInstance';
 import DataImport from '../../../../../../support/fragments/data_import/dataImport';
-import { DEFAULT_JOB_PROFILE_NAMES } from '../../../../../../support/constants';
+import { APPLICATION_NAMES, DEFAULT_JOB_PROFILE_NAMES } from '../../../../../../support/constants';
 import QuickMarcEditor from '../../../../../../support/fragments/quickMarcEditor';
 import ConsortiumManager from '../../../../../../support/fragments/settings/consortium-manager/consortium-manager';
 import MarcAuthority from '../../../../../../support/fragments/marcAuthority/marcAuthority';
@@ -105,9 +106,7 @@ describe('MARC', () => {
               });
             })
             .then(() => {
-              cy.resetTenant();
-              cy.setTenant(Affiliations.College);
-              cy.visit(TopMenu.inventoryPath);
+              TopMenuNavigation.openAppFromDropdown(APPLICATION_NAMES.INVENTORY);
               ConsortiumManager.switchActiveAffiliation(tenantNames.central, tenantNames.college);
               InventoryInstances.waitContentLoading();
               ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.college);
