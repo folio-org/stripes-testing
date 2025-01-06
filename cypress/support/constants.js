f32f956ea1c9bff981b934a3ee46d6ac8044af8d
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2025-01-06 16:35:19 +0500

    Add C496148 tets case

--- cypress/support/constants.js
+++ cypress/support/constants.js
@@ -993,6 +993,8 @@ export const BULK_EDIT_TABLE_COLUMN_HEADERS = {
     PROVENANCE_NOTE: 'Provenance note',
     MEMBER: 'Member',
     SUPPRESS_FROM_DISCOVERY: 'Suppress from discovery',
+    PERMANENT_LOAN_TYPE: 'Permanent loan type',
+    TEMPORARY_LOAN_TYPE: 'Temporary loan type',
   },
 };
 
