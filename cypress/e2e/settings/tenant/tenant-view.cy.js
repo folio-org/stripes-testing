dc1f95a331ebb6edad55ef843f37a5f7778c2e6a
Sviatlana Stsiapanava <152482600+sviatlana-stsiapanava@users.noreply.github.com>
2025-01-15 17:32:50 +0500

    FAT-18025/C494356 (#4633)
    * Add the test case C494356
    
    * Refactor repetitive code
    
    * Stabilize test cases

--- cypress/e2e/settings/tenant/tenant-view.cy.js
+++ cypress/e2e/settings/tenant/tenant-view.cy.js
@@ -1,8 +1,13 @@
+import uuid from 'uuid';
 import { Permissions } from '../../../support/dictionary';
 import { Locations, ServicePoints } from '../../../support/fragments/settings/tenant';
 import TenantPane, { TENANTS } from '../../../support/fragments/settings/tenant/tenantPane';
 import TopMenuNavigation from '../../../support/fragments/topMenuNavigation';
 import Users from '../../../support/fragments/users/users';
+import Campuses from '../../../support/fragments/settings/tenant/location-setup/campuses';
+import Institutions from '../../../support/fragments/settings/tenant/location-setup/institutions';
+import Libraries from '../../../support/fragments/settings/tenant/location-setup/libraries';
+import getRandomPostfix from '../../../support/utils/stringTools';
 
 describe('Settings: Tenant', () => {
   const testData = {
@@ -11,11 +16,42 @@ describe('Settings: Tenant', () => {
   before('Create test data', () => {
     cy.getAdminToken();
     ServicePoints.createViaApi(testData.servicePoint);
-    const { institution, location } = Locations.getDefaultLocation({
-      servicePointId: testData.servicePoint.id,
+
+    testData.institution = Institutions.getDefaultInstitution({
+      id: uuid(),
+      name: `1_autotest_institution_${getRandomPostfix()}`,
     });
-    testData.institution = institution;
-    testData.location = location;
+    testData.campus = Campuses.getDefaultCampuse({
+      id: uuid(),
+      name: `1_autotest_campuse_${getRandomPostfix()}`,
+      institutionId: testData.institution.id,
+    });
+    testData.library = Libraries.getDefaultLibrary({
+      id: uuid(),
+      name: `1_autotest_library_${getRandomPostfix()}`,
+      campusId: testData.campus.id,
+    });
+    testData.location = {
+      id: uuid(),
+      isActive: true,
+      institutionId: testData.institution.id,
+      institutionName: testData.institution.name,
+      campusId: testData.campus.id,
+      campusName: testData.campus.name,
+      libraryId: testData.library.id,
+      libraryName: testData.library.name,
+      servicePointIds: [testData.servicePoint.id],
+      name: `1_autotest_location_name_${getRandomPostfix()}`,
+      code: `1_autotest_location_code_${getRandomPostfix()}`,
+      discoveryDisplayName: `1_autotest_name_${getRandomPostfix()}`,
+      primaryServicePoint: testData.servicePoint.id,
+    };
+
+    Institutions.createViaApi(testData.institution).then(() => {
+      Campuses.createViaApi(testData.campus).then(() => {
+        Libraries.createViaApi(testData.library);
+      });
+    });
     Locations.createViaApi(testData.location);
     cy.createTempUser([Permissions.settingsTenantView.gui]).then((userProperties) => {
       testData.user = userProperties;
@@ -39,15 +75,13 @@ describe('Settings: Tenant', () => {
     { tags: ['extendedPath', 'firebird', 'C409487'] },
     () => {
       cy.intercept('/location-units/institutions*', { locinsts: [testData.institution] });
-      // reload is needed because sometimes Location setup section is not displayed
-      cy.reload();
       // Select "Institutions" option on the "Location setup" subsection
-      const Institutions = TenantPane.selectTenant(TENANTS.INSTITUTIONS);
+      TenantPane.selectTenant(TENANTS.INSTITUTIONS);
       Institutions.checkNoActionButtons();
 
       // Select "Campuses" option on the "Location setup" subsection
       // Select any existing institution from the  "Select institution" dropdown
-      const Campuses = TenantPane.selectTenant(TENANTS.CAMPUSES);
+      TenantPane.selectTenant(TENANTS.CAMPUSES);
       Campuses.selectOption('Institution', {
         name: testData.location.institutionName,
         id: testData.location.institutionId,
@@ -56,7 +90,7 @@ describe('Settings: Tenant', () => {
 
       // Select "Libraries" option on the "Location setup" subsection
       // Select any existing institution from the  "Select institution" dropdown
-      const Libraries = TenantPane.selectTenant(TENANTS.LIBRARIES);
+      TenantPane.selectTenant(TENANTS.LIBRARIES);
       Libraries.selectOption('Institution', {
         name: testData.location.institutionName,
         id: testData.location.institutionId,
