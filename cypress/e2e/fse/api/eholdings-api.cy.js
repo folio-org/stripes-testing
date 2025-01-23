94643a802a68e57349bfefa0b6eb270f7ae8aa4a
kbrener-eis <165926280+kbrener-eis@users.noreply.github.com>
2025-01-23 18:53:19 +0200

    retries added for the LOC tests (#4664)

--- cypress/e2e/fse/api/eholdings-api.cy.js
+++ cypress/e2e/fse/api/eholdings-api.cy.js
@@ -1,4 +1,4 @@
-describe('fse-eholdings', () => {
+describe('fse-eholdings', { retries: { runMode: 1 } }, () => {
   beforeEach(() => {
     // hide sensitive data from the report
     cy.allure().logCommandSteps(false);
