91250ab593fd08ca1565210640dbb1faea65f4e0
Kyrylo Brener <kbrener@ebsco.com>
2025-02-03 14:41:25 +0200

    serials ui test fixed

--- cypress/support/fragments/serials/serials.js
+++ cypress/support/fragments/serials/serials.js
@@ -1,11 +1,6 @@
-import { Button } from '../../../../interactors';
-
-const newSerialsButton = Button({ id: 'clickable-new-serial' });
-
 export default {
   waitLoading: () => {
-    // checking button xpath since UI for serials doesn't have unique IDs for sections
-    cy.expect(newSerialsButton.exists());
-    cy.xpath("//div[@id='serials-management-module-display']").should('exist');
+    // checking xpath since UI for serials doesn't have unique IDs for sections
+    cy.xpath("//div[@id='serials-management-module-display']").should('be.visible');
   },
 };
