7355a38b0688ef280abf09f100b78bee378eb3ff
Kyrylo Brener <kbrener@ebsco.com>
2025-02-03 16:04:57 +0200

    more updates for serials ui

--- cypress/support/fragments/serials/serials.js
+++ cypress/support/fragments/serials/serials.js
@@ -1,6 +1,12 @@
+import { Button } from '../../../../interactors';
+
+const serialsFiltersButton = Button({ id: 'clickable-nav-serials' });
+const pieceSetsFiltersButton = Button({ id: 'clickable-nav-piece-sets' });
+
 export default {
-  waitLoading: () => {
-    // checking xpath since UI for serials doesn't have unique IDs for sections
-    cy.xpath("//div[@id='serials-management-module-display']").should('be.visible');
+  waitLoading() {
+    // checking button xpath since UI for serials doesn't have unique IDs for sections
+    cy.expect([serialsFiltersButton.exists(), pieceSetsFiltersButton.exists()]);
+    cy.xpath("//div[@id='serials-management-module-display']").should('exist');
   },
 };
