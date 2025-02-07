89f91b8650046a1a1377061303816ccc9a516ce0
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-02-07 22:13:47 +0500

    Eureka prepare refactor

--- cypress/e2e/eureka/aaaaa_eurekaPrepare.cy.js
+++ cypress/e2e/eureka/aaaaa_eurekaPrepare.cy.js
@@ -1,84 +1,57 @@
 import Affiliations from '../../support/dictionary/affiliations';
 
 describe('Eureka - Prepare for testing', () => {
-  it(
-    'Clear all cookies',
-    {
-      tags: ['clearCookies', 'eureka'],
-    },
-    () => {
-      // workaround for EUREKA-396 - otherwise tests may fail in consecutive runs for different tenants:
-      if (Cypress.env('eureka')) cy.clearCookies({ domain: null });
-    },
-  );
+  function assignAdminRole(affiliation, userId) {
+    cy.clearCookies({ domain: null });
+    cy.setTenant(affiliation);
+
+    if (affiliation === Affiliations.College) {
+      cy.getCollegeAdminToken();
+    } else if (affiliation === Affiliations.University) {
+      cy.getUniversityAdminToken();
+    }
+
+    const roleName = Cypress.env('systemRoleName');
+    cy.getUserRoleIdByNameApi(roleName).then((adminRoleId) => {
+      cy.getAuthorizationRolesForUserApi(userId).then((roles) => {
+        cy.log(
+          `ADMIN ROLE IN ${affiliation.toUpperCase()}: ` +
+            (roles.body.userRoles.length > 0 &&
+              roles.body.userRoles.map((entry) => entry.roleId).includes(adminRoleId)),
+        );
+
+        if (!roles.body.userRoles.length) {
+          cy.wait(10000);
+          cy.addRolesToNewUserApi(userId, [adminRoleId]);
+        } else if (!roles.body.userRoles.map((entry) => entry.roleId).includes(adminRoleId)) {
+          cy.wait(10000);
+          cy.updateRolesForUserApi(userId, [adminRoleId]);
+        }
+      });
+    });
+  }
+
+  it('Clear all cookies', { tags: ['clearCookies', 'eureka'] }, () => {
+    // workaround for EUREKA-396 - otherwise tests may fail in consecutive runs for different tenants:
+    if (Cypress.env('eureka')) cy.clearCookies({ domain: null });
+  });
 
   it(
     'Assign admin roles in Members to the Central admin',
-    {
-      tags: ['eureka', 'adminECS'],
-      retries: 3,
-    },
+    { tags: ['eureka', 'adminECS'], retries: 3 },
     () => {
       if (Cypress.env('eureka') && Cypress.env('ecsEnabled')) {
         cy.resetTenant();
         cy.getAdminToken();
         cy.getUserWithBlUsersByUsername(Cypress.env('diku_login')).then(({ body }) => {
-          // adding adminRole in College
-          cy.clearCookies({ domain: null });
-          cy.setTenant(Affiliations.College);
-          cy.getCollegeAdminToken();
-          cy.getUserRoleIdByNameApi(Cypress.env('systemRoleName')).then((collegeAdminRoleId) => {
-            cy.getAuthorizationRolesForUserApi(body.user.id).then((rolesCollege) => {
-              cy.log(
-                'ADMIN ROLE IN COLLEGE: ' +
-                  (rolesCollege.body.userRoles.length > 0 &&
-                    rolesCollege.body.userRoles
-                      .map((entry) => entry.roleId)
-                      .includes(collegeAdminRoleId)),
-              );
-              if (!rolesCollege.body.userRoles.length) {
-                cy.addRolesToNewUserApi(body.user.id, [collegeAdminRoleId]);
-              } else if (
-                !rolesCollege.body.userRoles
-                  .map((entry) => entry.roleId)
-                  .includes(collegeAdminRoleId)
-              ) {
-                cy.updateRolesForUserApi(body.user.id, [collegeAdminRoleId]);
-              }
-            });
-
-            // adding adminRole in University
-            cy.clearCookies({ domain: null });
-            cy.setTenant(Affiliations.University);
-            cy.getUniversityAdminToken();
-            cy.getUserRoleIdByNameApi(Cypress.env('systemRoleName')).then(
-              (universityAdminRoleId) => {
-                cy.getAuthorizationRolesForUserApi(body.user.id).then((rolesUniversity) => {
-                  cy.log(
-                    'ADMIN ROLE IN UNIVERSITY: ' +
-                      (rolesUniversity.body.userRoles.length > 0 &&
-                        rolesUniversity.body.userRoles
-                          .map((entry) => entry.roleId)
-                          .includes(universityAdminRoleId)),
-                  );
-                  if (!rolesUniversity.body.userRoles.length) {
-                    cy.wait(10000);
-                    cy.addRolesToNewUserApi(body.user.id, [universityAdminRoleId]);
-                  } else if (
-                    !rolesUniversity.body.userRoles
-                      .map((entry) => entry.roleId)
-                      .includes(universityAdminRoleId)
-                  ) {
-                    cy.wait(10000);
-                    cy.updateRolesForUserApi(body.user.id, [universityAdminRoleId]);
-                  }
-                });
-              },
-            );
-          });
+          assignAdminRole(Affiliations.College, body.user.id);
+          assignAdminRole(Affiliations.University, body.user.id);
           cy.resetTenant();
           cy.wait(10000);
         });
+
+        cy.resetTenant();
+        cy.wait(10000);
       }
     },
   );
