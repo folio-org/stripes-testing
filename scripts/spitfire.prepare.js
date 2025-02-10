2c6c29cc196d277ac72ed55819a36f0a2142a5c4
Vadym Yeromichev <v.eremichev@gmail.com>
2025-02-10 14:49:55 +0200

    small script optimization

--- scripts/spitfire.prepare.js
+++ scripts/spitfire.prepare.js
@@ -44,23 +44,16 @@ function checkResponse(response, message, validStatus = 200) {
 }
 
 async function getEholdingsKbs() {
-  const response = await axios({
-    url: '/eholdings/kb-credentials',
-  });
+  const response = await axios.get('/eholdings/kb-credentials');
   return response.data.data;
 }
 
 async function unassignEholdingsKbUsers() {
   const existingKbs = await getEholdingsKbs();
-  const usersResponse = await axios({
-    url: `/eholdings/kb-credentials/${existingKbs[0].id}/users`,
-  });
+  const usersResponse = await axios.get(`/eholdings/kb-credentials/${existingKbs[0].id}/users`);
   const users = usersResponse.data.data;
   for (const user of users) {
-    await axios({
-      method: 'DELETE',
-      url: `/eholdings/kb-credentials/${existingKbs[0].id}/users/${user.id}`,
-    });
+    await axios.delete(`/eholdings/kb-credentials/${existingKbs[0].id}/users/${user.id}`);
   }
 }
 
@@ -73,17 +66,13 @@ async function resetEholdingsKbs() {
     existingKbs[0].attributes.apiKey !== process.env.EHOLDINGS_KB_KEY
   ) {
     for (const kb of existingKbs) {
-      await axios({
-        method: 'DELETE',
-        url: `/eholdings/kb-credentials/${kb.id}`,
-      });
+      await axios.delete(`/eholdings/kb-credentials/${kb.id}`);
     }
     console.log('Configuring eHoldings KB');
-    await axios({
-      method: 'POST',
-      url: '/eholdings/kb-credentials',
-      headers: { 'Content-type': 'application/vnd.api+json' },
-      data: {
+
+    await axios.post(
+      '/eholdings/kb-credentials',
+      {
         data: {
           type: 'kbCredentials',
           attributes: {
@@ -94,15 +83,14 @@ async function resetEholdingsKbs() {
           },
         },
       },
-    });
+      { headers: { 'Content-type': 'application/vnd.api+json' } },
+    );
     await unassignEholdingsKbUsers();
   }
 }
 
 async function getFieldProtectionSettings() {
-  const response = await axios({
-    url: '/field-protection-settings/marc?limit=2000',
-  });
+  const response = await axios.get('/field-protection-settings/marc?limit=2000');
   return response.data.marcFieldProtectionSettings;
 }
 
@@ -111,26 +99,18 @@ async function resetFieldProtectionSettings() {
   const userSettings = existingSettings.filter((setting) => setting.source === 'USER');
   console.log('Removing user field protection settings');
   for (const setting of userSettings) {
-    await axios({
-      method: 'DELETE',
-      url: `/field-protection-settings/marc/${setting.id}`,
-    });
+    await axios.delete(`/field-protection-settings/marc/${setting.id}`);
   }
 }
 
 async function deleteItemsForHoldings(holdingsId) {
-  const itemsResponse = await axios({
-    url: `inventory/items-by-holdings-id?query=holdingsRecordId==${holdingsId}`,
-  });
+  const itemsResponse = await axios.get(
+    `inventory/items-by-holdings-id?query=holdingsRecordId==${holdingsId}`,
+  );
   const deletePromises = [];
   // eslint-disable-next-line guard-for-in
   for (const item of itemsResponse.data.items) {
-    deletePromises.push(
-      axios({
-        method: 'DELETE',
-        url: `inventory/items/${item.id}`,
-      }),
-    );
+    deletePromises.push(axios.delete(`inventory/items/${item.id}`));
   }
   const settledDeletePromises = await Promise.allSettled(deletePromises);
   settledDeletePromises.forEach((settledPromise) => {
@@ -140,19 +120,14 @@ async function deleteItemsForHoldings(holdingsId) {
 }
 
 async function deleteHoldingsForInstance(instanceId) {
-  const holdingsResponse = await axios({
-    url: `holdings-storage/holdings?limit=200&query=instanceId="${instanceId}"`,
-  });
+  const holdingsResponse = await axios(
+    `holdings-storage/holdings?limit=200&query=instanceId="${instanceId}"`,
+  );
   const deletePromises = [];
   // eslint-disable-next-line guard-for-in
   for (const holdings of holdingsResponse.data.holdingsRecords) {
     await deleteItemsForHoldings(holdings.id);
-    deletePromises.push(
-      await axios({
-        method: 'DELETE',
-        url: `holdings-storage/holdings/${holdings.id}`,
-      }),
-    );
+    deletePromises.push(await axios.delete(`holdings-storage/holdings/${holdings.id}`));
   }
   const settledDeletePromises = await Promise.allSettled(deletePromises);
   settledDeletePromises.forEach((settledPromise) => {
@@ -163,21 +138,15 @@ async function deleteHoldingsForInstance(instanceId) {
 
 async function removeAutotestInstances() {
   const batchSize = 15;
-  const autotestInstances = await axios({
-    url: '/search/instances?limit=500&query=(title all "autotest")',
-  });
+  const autotestInstances = await axios.get(
+    '/search/instances?limit=500&query=(title all "autotest")',
+  );
   console.log('Removing autotest instances');
   const deletePromises = [];
   const deletePromisesBatches = [];
   for (const instance of autotestInstances.data.instances) {
     await deleteHoldingsForInstance(instance.id);
-    deletePromises.push(
-      axios({
-        method: 'DELETE',
-        url: `/instance-storage/instances/${instance.id}`,
-        validateStatus: () => true,
-      }),
-    );
+    deletePromises.push(axios.delete(`/instance-storage/instances/${instance.id}`));
   }
   for (let i = 0; i < deletePromises.length; i += batchSize) {
     const batch = [];
@@ -195,45 +164,35 @@ async function removeAutotestInstances() {
 }
 
 async function removeAutotestAuthorities() {
-  const autotestAuthorities = await axios({
-    url: '/search/authorities?limit=500&query=(keyword="autotest" or keyword="auto" or keyword="C*")',
-  });
+  const autotestAuthorities = await axios.get(
+    '/search/authorities?limit=500&query=(keyword="autotest" or keyword="auto" or keyword="C*" and authRefType="Authorized")',
+  );
   console.log('Removing autotest authorities');
   let counter = 1;
   for (const authority of autotestAuthorities.data.authorities) {
     if (!(counter % 100)) await getToken();
     if (!(counter % 15)) await wait(1500);
-    await axios({
-      method: 'DELETE',
-      url: `/authority-storage/authorities/${authority.id}`,
-      validateStatus: () => true,
-    });
+    await axios.delete(`/authority-storage/authorities/${authority.id}`);
     counter++;
   }
   await getToken();
 }
 
 async function removeAuthoritiesWithSpecificSource(sourceFileId) {
-  const authoritiesWithSource = await axios({
-    url: `/search/authorities?limit=500&query=(sourceFileId==("${sourceFileId}"))`,
-  });
+  const authoritiesWithSource = await axios.get(
+    `/search/authorities?limit=500&query=(sourceFileId==("${sourceFileId}"))`,
+  );
   if (authoritiesWithSource.data.authorities.length <= 100) {
     for (const [index, authority] of authoritiesWithSource.data.authorities.entries()) {
       if (!((index + 1) % 15)) await wait(1500);
-      await axios({
-        method: 'DELETE',
-        url: `/authority-storage/authorities/${authority.id}`,
-        validateStatus: () => true,
-      });
+      await axios.delete(`/authority-storage/authorities/${authority.id}`);
     }
     await wait(500);
   }
 }
 
 async function removeUserAuthoritySourceFiles() {
-  const response = await axios({
-    url: '/authority-source-files?limit=200',
-  });
+  const response = await axios.get('/authority-source-files?limit=200');
   const userSourceFiles = response.data.authoritySourceFiles.filter(
     (sourceFile) => sourceFile.source === 'local',
   );
@@ -242,26 +201,20 @@ async function removeUserAuthoritySourceFiles() {
   for (const sourceFile of userSourceFiles) {
     if (!(counter % 15)) await wait(1500);
     await removeAuthoritiesWithSpecificSource(sourceFile.id);
-    await axios({
-      method: 'DELETE',
-      url: `/authority-source-files/${sourceFile.id}`,
-      validateStatus: () => true,
-    });
+    await axios.delete(`/authority-source-files/${sourceFile.id}`);
     counter++;
   }
 }
 
 async function uploadDefinitions(keyValue, fileName) {
-  const response = await axios.post(`${envData.apiBaseUrl}/data-import/uploadDefinitions`, {
+  const response = await axios.post('/data-import/uploadDefinitions', {
     fileDefinitions: [{ uiKey: keyValue, size: 2, name: fileName }],
   });
   return checkResponse(response, 'Uploading definition', 201).data;
 }
 
 async function uploadDefinitionWithId(uploadDefinitionId) {
-  const response = await axios.get(
-    `${envData.apiBaseUrl}/data-import/uploadDefinitions/${uploadDefinitionId}`,
-  );
+  const response = await axios.get(`/data-import/uploadDefinitions/${uploadDefinitionId}`);
   return checkResponse(response, 'Uploading definition with id', 200).data;
 }
 
@@ -272,7 +225,7 @@ async function processFileWithSplitFiles(
   jobProfileName,
 ) {
   const response = await axios.post(
-    `${envData.apiBaseUrl}/data-import/uploadDefinitions/${uploadDefinitionId}/processFiles?defaultMapping=false`,
+    `/data-import/uploadDefinitions/${uploadDefinitionId}/processFiles?defaultMapping=false`,
     {
       uploadDefinition,
       jobProfileInfo: {
@@ -286,12 +239,9 @@ async function processFileWithSplitFiles(
 }
 
 async function getUploadUrl(fileName) {
-  const response = await axios.get(
-    `${envData.apiBaseUrl}/data-import/uploadUrl?filename=${fileName}`,
-    {
-      headers: { 'Content-Type': 'application/octet-stream' },
-    },
-  );
+  const response = await axios.get(`/data-import/uploadUrl?filename=${fileName}`, {
+    headers: { 'Content-Type': 'application/octet-stream' },
+  });
   return checkResponse(response, 'Getting upload url', 200)?.data;
 }
 
@@ -310,7 +260,7 @@ async function getTag(uploadUrl, filePath) {
 }
 
 async function getJobProfilesViaApi(profileName) {
-  const response = await axios.get(`${envData.apiBaseUrl}/data-import-profiles/jobProfiles`, {
+  const response = await axios.get('/data-import-profiles/jobProfiles', {
     params: { query: `name="${profileName}"` },
   });
   return checkResponse(response, 'Getting job profile', 200).data;
@@ -319,13 +269,13 @@ async function getJobProfilesViaApi(profileName) {
 async function getParentJobExecutionId(sourcePath) {
   let attempts = 10;
   while (attempts--) {
-    const response = await axios.get(`${envData.apiBaseUrl}/metadata-provider/jobExecutions`, {
-      params: { limit: 10000, sortBy: 'started_date,desc' },
+    const response = await axios.get('/metadata-provider/jobExecutions', {
+      params: { limit: 10_000, sortBy: 'started_date,desc' },
     });
     checkResponse(response, `\tGetting parent job executions (${attempts} attempts left)`, 200);
     const jobExecution = response.data.jobExecutions.find((job) => job.sourcePath === sourcePath);
     if (jobExecution) return jobExecution.id;
-    await new Promise((res) => setTimeout(res, 5_000));
+    await wait(5_000);
   }
   throw new Error('Parent job execution not found');
 }
@@ -333,9 +283,7 @@ async function getParentJobExecutionId(sourcePath) {
 async function getJobStatus(jobExecutionId) {
   let attempts = 16;
   while (attempts--) {
-    const response = await axios.get(
-      `${envData.apiBaseUrl}/change-manager/jobExecutions/${jobExecutionId}`,
-    );
+    const response = await axios.get(`/change-manager/jobExecutions/${jobExecutionId}`);
     checkResponse(
       response,
       `\t\tGetting parent job execution status (${attempts} attempts left)`,
@@ -343,25 +291,20 @@ async function getJobStatus(jobExecutionId) {
     );
     const status = response.data;
     if (status.status === 'COMMITTED' && status.uiStatus === 'RUNNING_COMPLETE') return status;
-    await new Promise((res) => setTimeout(res, 5_000));
+    await wait(5_000);
   }
   throw new Error('Parent job execution status is not RUNNING_COMPLETE');
 }
 
 async function getChildJobExecutionId(jobExecutionId) {
-  const response = await axios.get(
-    `${envData.apiBaseUrl}/change-manager/jobExecutions/${jobExecutionId}/children`,
-  );
+  const response = await axios.get(`/change-manager/jobExecutions/${jobExecutionId}/children`);
   return checkResponse(response, '\tGetting child job execution id', 200).data.jobExecutions[0].id;
 }
 
 async function getRecordSourceId(jobExecutionId) {
-  const response = await axios.get(
-    `${envData.apiBaseUrl}/metadata-provider/jobLogEntries/${jobExecutionId}`,
-    {
-      params: { limit: 100, query: 'order=asc' },
-    },
-  );
+  const response = await axios.get(`/metadata-provider/jobLogEntries/${jobExecutionId}`, {
+    params: { limit: 100, query: 'order=asc' },
+  });
   return checkResponse(response, 'Getting job Log Entries', 200).data.entries;
 }
 
@@ -369,14 +312,14 @@ async function getCreatedRecordInfoWithSplitFiles(jobExecutionId, recordId) {
   let attempts = 10;
   while (attempts--) {
     const response = await axios.get(
-      `${envData.apiBaseUrl}/metadata-provider/jobLogEntries/${jobExecutionId}/records/${recordId}`,
+      `/metadata-provider/jobLogEntries/${jobExecutionId}/records/${recordId}`,
       {
         params: { limit: 100 },
       },
     );
     checkResponse(response, `Getting created record info (${attempts} attempts left)`, 200);
     if (response.status === 200) return response.data;
-    await new Promise((res) => setTimeout(res, 1_000));
+    await wait(1_000);
   }
   throw new Error('Record not found in job log entries');
 }
@@ -389,7 +332,7 @@ async function uploadDefinitionWithAssembleStorageFile(
   s3Etag,
 ) {
   const response = await axios.post(
-    `${envData.apiBaseUrl}/data-import/uploadDefinitions/${uploadId}/files/${fileId}/assembleStorageFile`,
+    `/data-import/uploadDefinitions/${uploadId}/files/${fileId}/assembleStorageFile`,
     {
       uploadId: s3UploadId,
       key: s3UploadKey,
@@ -441,19 +384,18 @@ async function uploadFileWithSplitFilesViaApi(filePathName, fileName, profileNam
 
 (async () => {
   await getToken();
+
   try {
-    console.error(
-      (await uploadFileWithSplitFilesViaApi(
-        'cypress/fixtures/marcFileForC375261.mrc',
-        'autotest-00.mrc',
-        'Default - Create SRS MARC Authority',
-      ))
-        ? '0'
-        : '1',
+    const filesUploadedStatus = await uploadFileWithSplitFilesViaApi(
+      'cypress/fixtures/marcFileForC375261.mrc',
+      'autotest-00.mrc',
+      'Default - Create SRS MARC Authority',
     );
-  } catch (e) {
-    console.error('1');
+    if (!filesUploadedStatus) process.exit(1);
+  } catch (error) {
+    process.exit(1);
   }
+
   await resetEholdingsKbs();
   await resetFieldProtectionSettings();
   await removeAutotestInstances();
