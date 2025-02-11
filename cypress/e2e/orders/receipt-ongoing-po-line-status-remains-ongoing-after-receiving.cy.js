480bcf7751b39fa89a5311818a93ed446eb0530b
IhorBohdan <ihor_bohdan@epam.com>
2025-02-11 13:36:12 +0200

    implement fat-18096

---
+++ cypress/e2e/orders/receipt-ongoing-po-line-status-remains-ongoing-after-receiving.cy.js
@@ -0,0 +1,164 @@
+import permissions from '../../support/dictionary/permissions';
+import FinanceHelp from '../../support/fragments/finance/financeHelper';
+import FiscalYears from '../../support/fragments/finance/fiscalYears/fiscalYears';
+import Funds from '../../support/fragments/finance/funds/funds';
+import Ledgers from '../../support/fragments/finance/ledgers/ledgers';
+import NewOrder from '../../support/fragments/orders/newOrder';
+import OrderLines from '../../support/fragments/orders/orderLines';
+import Orders from '../../support/fragments/orders/orders';
+import NewOrganization from '../../support/fragments/organizations/newOrganization';
+import Organizations from '../../support/fragments/organizations/organizations';
+import NewLocation from '../../support/fragments/settings/tenant/locations/newLocation';
+import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import TopMenu from '../../support/fragments/topMenu';
+import Users from '../../support/fragments/users/users';
+import DateTools from '../../support/utils/dateTools';
+import Budgets from '../../support/fragments/finance/budgets/budgets';
+import {
+  ACQUISITION_METHOD_NAMES_IN_PROFILE,
+  ORDER_STATUSES,
+} from '../../support/constants';
+import BasicOrderLine from '../../support/fragments/orders/basicOrderLine';
+import MaterialTypes from '../../support/fragments/settings/inventory/materialTypes';
+import Receiving from '../../support/fragments/receiving/receiving';
+import OrderLineDetails from '../../support/fragments/orders/orderLineDetails';
+
+describe('Orders', () => {
+  const firstFiscalYear = { ...FiscalYears.defaultUiFiscalYear };
+  const defaultLedger = { ...Ledgers.defaultUiLedger };
+  const defaultFund = { ...Funds.defaultUiFund };
+  const organization = { ...NewOrganization.defaultUiOrganizations };
+  const firstOrder = {
+    ...NewOrder.getDefaultOngoingOrder,
+    orderType: 'Ongoing',
+    ongoing: { isSubscription: false, manualRenewal: false },
+    approved: true,
+    reEncumber: true,
+  };
+  const firstBudget = {
+    ...Budgets.getDefaultBudget(),
+    allocated: 100,
+  };
+  const instanceTitle = `autotestTitle ${FinanceHelp.getRandomBarcode()}`;
+
+  let user;
+  let firstOrderNumber;
+  let servicePointId;
+  let location;
+
+  before(() => {
+    cy.getAdminToken();
+    // create first Fiscal Year and prepere 2 Funds for Rollover
+    FiscalYears.createViaApi(firstFiscalYear).then((firstFiscalYearResponse) => {
+      firstFiscalYear.id = firstFiscalYearResponse.id;
+      firstBudget.fiscalYearId = firstFiscalYearResponse.id;
+      defaultLedger.fiscalYearOneId = firstFiscalYear.id;
+      Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
+        defaultLedger.id = ledgerResponse.id;
+        defaultFund.ledgerId = defaultLedger.id;
+
+        Funds.createViaApi(defaultFund).then((fundResponse) => {
+          defaultFund.id = fundResponse.fund.id;
+          firstBudget.fundId = fundResponse.fund.id;
+          Budgets.createViaApi(firstBudget);
+          ServicePoints.getViaApi().then((servicePoint) => {
+            servicePointId = servicePoint[0].id;
+            NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePointId)).then((res) => {
+              location = res;
+
+              MaterialTypes.createMaterialTypeViaApi(MaterialTypes.getDefaultMaterialType()).then(
+                (mtypes) => {
+                  cy.getAcquisitionMethodsApi({
+                    query: `value="${ACQUISITION_METHOD_NAMES_IN_PROFILE.PURCHASE_AT_VENDOR_SYSTEM}"`,
+                  }).then((params) => {
+                    // Prepare 2 Open Orders for Rollover
+                    Organizations.createOrganizationViaApi(organization).then(
+                      (responseOrganizations) => {
+                        organization.id = responseOrganizations;
+                        firstOrder.vendor = organization.id;
+                        const firstOrderLine = {
+                          ...BasicOrderLine.defaultOrderLine,
+                          cost: {
+                            listUnitPrice: 10.0,
+                            currency: 'USD',
+                            discountType: 'percentage',
+                            quantityPhysical: 1,
+                            poLineEstimatedPrice: 10.0,
+                          },
+                          checkinItems: true,
+                          paymentStatus: 'Payment Not Required',
+                          fundDistribution: [
+                            { code: defaultFund.code, fundId: defaultFund.id, value: 100 },
+                          ],
+                          locations: [
+                            { locationId: location.id, quantity: 1, quantityPhysical: 1 },
+                          ],
+                          acquisitionMethod: params.body.acquisitionMethods[0].id,
+                          physical: {
+                            createInventory: 'Instance, Holding, Item',
+                            materialType: mtypes.body.id,
+                            materialSupplier: responseOrganizations,
+                            volumes: [],
+                          },
+                          titleOrPackage: instanceTitle,
+                        };
+                        Orders.createOrderViaApi(firstOrder).then((firstOrderResponse) => {
+                          firstOrder.id = firstOrderResponse.id;
+                          firstOrderLine.purchaseOrderId = firstOrderResponse.id;
+                          firstOrderNumber = firstOrderResponse.poNumber;
+                          OrderLines.createOrderLineViaApi(firstOrderLine);
+                          Orders.updateOrderViaApi({
+                            ...firstOrderResponse,
+                            workflowStatus: ORDER_STATUSES.OPEN,
+                          });
+                        });
+                      },
+                    );
+                  });
+                },
+              );
+            });
+          });
+        });
+      });
+    });
+    cy.createTempUser([
+      permissions.uiInventoryViewInstances.gui,
+      permissions.uiOrdersEdit.gui,
+      permissions.uiReceivingViewEditCreate.gui,
+    ]).then((userProperties) => {
+      user = userProperties;
+      cy.login(userProperties.username, userProperties.password, {
+        path: TopMenu.ordersPath,
+        waiter: Orders.waitLoading,
+      });
+    });
+  });
+
+  after(() => {
+    cy.getAdminToken();
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C423418 Receipt ongoing PO line status remains “Ongoing” after receiving (thunderjet)',
+    { tags: ['criticalPath', 'thunderjet'] },
+    () => {
+      Orders.searchByParameter('PO number', firstOrderNumber);
+      Orders.selectFromResultsList(firstOrderNumber);
+      OrderLines.selectPOLInOrder();
+      OrderLines.receiveOrderLinesViaActions();
+      Receiving.selectFromResultsList(instanceTitle);
+      Receiving.addPieceInActions();
+      Receiving.openDropDownInEditPieceModal();
+      Receiving.quickReceivePieceFromDropdown();
+      Receiving.clickOnPOLnumber(`${firstOrderNumber}-1`);
+      OrderLineDetails.checkOrderLineDetails({
+        purchaseOrderLineInformation: [
+          { key: 'Receipt status', value: 'Ongoing' },
+          { key: 'Payment status', value: 'Payment Not Required' },
+        ],
+      });
+    },
+  );
+});
