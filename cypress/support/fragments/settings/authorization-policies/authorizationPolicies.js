84094868389ba9d18da736e4d233024285b8a5b6
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-02-12 14:28:17 +0500

    FAT-18426: test added

--- cypress/support/fragments/settings/authorization-policies/authorizationPolicies.js
+++ cypress/support/fragments/settings/authorization-policies/authorizationPolicies.js
@@ -9,7 +9,10 @@ import {
   Spinner,
   and,
   PaneHeader,
+  MultiColumnList,
+  MultiColumnListCell,
 } from '../../../../../interactors';
+import { AUTHORIZATION_POLICIES_COLUMNS } from '../../../constants';
 
 const policiesPane = Pane('Authorization policies');
 const policiesSearchInputField = policiesPane.find(TextField('Search'));
@@ -18,10 +21,17 @@ const generalInformationAccordion = Accordion('General Information');
 const recordLastUpdatedHeader = generalInformationAccordion.find(
   Button(including('Record last updated:')),
 );
+const policySearchInputField = policiesPane.find(TextField({ testid: 'search-field' }));
+const policySearchButton = policiesPane.find(Button({ dataTestID: 'search-button' }));
+
+export const SETTINGS_SUBSECTION_AUTH_POLICIES = 'Authorization policies';
 
 export default {
   waitContentLoading: () => {
-    cy.expect(policiesPane.find(MultiColumnListHeader('Name')).exists());
+    Object.values(AUTHORIZATION_POLICIES_COLUMNS).forEach((columnName) => {
+      cy.expect(policiesPane.find(MultiColumnListHeader(columnName)).exists());
+    });
+    cy.expect([policySearchInputField.exists(), policySearchButton.exists()]);
   },
 
   clickOnPolicyName: (policyName) => {
@@ -33,6 +43,7 @@ export default {
   searchPolicy: (policyName) => {
     cy.expect(Spinner().absent());
     cy.do([policiesSearchInputField.fillIn(policyName), policiesSearchButton.click()]);
+    cy.wait(1000);
   },
 
   verifyPolicyViewPane: (policyName) => {
@@ -68,5 +79,29 @@ export default {
         ),
       }),
     ]);
+  },
+
+  verifyPoliciesCount: (count) => {
+    if (count === 0) cy.expect(policiesPane.find(MultiColumnList()).absent());
+    else cy.expect(policiesPane.find(MultiColumnList()).has({ rowCount: count }));
+  },
+
+  checkPolicyFound: (policyName, isFound = true) => {
+    const targetRow = policiesPane.find(HTML(policyName, { className: including('root') }));
+    if (isFound) cy.expect(targetRow.exists());
+    else cy.expect(targetRow.absent());
+  },
+
+  verifyPolicyRow: (policyName, policyDescription, updated, updatedBy) => {
+    cy.do(
+      policiesPane.find(MultiColumnListCell(policyName)).perform((element) => {
+        const rowNumber = +element.parentElement.getAttribute('data-row-inner');
+        cy.expect([
+          policiesPane.find(MultiColumnListCell(policyDescription, { row: rowNumber })).exists(),
+          policiesPane.find(MultiColumnListCell(updated, { row: rowNumber })).exists(),
+          policiesPane.find(MultiColumnListCell(including(updatedBy), { row: rowNumber })).exists(),
+        ]);
+      }),
+    );
   },
 };
