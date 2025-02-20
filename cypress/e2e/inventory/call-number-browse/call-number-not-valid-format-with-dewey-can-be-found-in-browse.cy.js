1b1d813295904689ed071208ada7739eb85d1223
Vadym Yeromichev <v.eremichev@gmail.com>
2025-02-20 14:29:50 +0200

    fixed test C451466

--- cypress/e2e/inventory/call-number-browse/call-number-not-valid-format-with-dewey-can-be-found-in-browse.cy.js
+++ cypress/e2e/inventory/call-number-browse/call-number-not-valid-format-with-dewey-can-be-found-in-browse.cy.js
@@ -7,6 +7,8 @@ import TopMenu from '../../../support/fragments/topMenu';
 import Users from '../../../support/fragments/users/users';
 import { getTestEntityValue, randomFourDigitNumber } from '../../../support/utils/stringTools';
 import { BROWSE_CALL_NUMBER_OPTIONS, ITEM_STATUS_NAMES } from '../../../support/constants';
+import { CallNumberBrowseSettings } from '../../../support/fragments/settings/inventory/instances/callNumberBrowse';
+import BrowseCallNumber from '../../../support/fragments/inventory/search/browseCallNumber';
 
 describe('Inventory', () => {
   describe('Call Number Browse', () => {
@@ -18,6 +20,17 @@ describe('Inventory', () => {
       barcodes: [`451466${randomFourDigitNumber()}`, `451466${randomFourDigitNumber()}`],
       userServicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
     };
+
+    const callNumberBrowseSettings = [
+      {
+        name: 'Dewey Decimal classification',
+        callNumberTypes: ['Dewey Decimal classification'],
+      },
+      {
+        name: 'Call numbers (all)',
+        callNumberTypes: [],
+      },
+    ];
 
     before('Create test data', () => {
       cy.getAdminToken().then(() => {
@@ -93,7 +106,9 @@ describe('Inventory', () => {
       });
       cy.createTempUser([Permissions.inventoryAll.gui]).then((userProperties) => {
         testData.user = userProperties;
-
+        callNumberBrowseSettings.forEach((setting) => {
+          CallNumberBrowseSettings.assignCallNumberTypesViaApi(setting);
+        });
         cy.login(testData.user.username, testData.user.password, {
           path: TopMenu.inventoryPath,
           waiter: InventoryInstances.waitContentLoading,
@@ -117,6 +132,7 @@ describe('Inventory', () => {
       { tags: ['criticalPath', 'spitfire', 'shiftLeft', 'C451466'] },
       () => {
         InventorySearchAndFilter.selectBrowseCallNumbers();
+        BrowseCallNumber.waitForCallNumberToAppear(testData.firstCallNumber);
         InventorySearchAndFilter.browseSearch(testData.firstCallNumber);
         InventorySearchAndFilter.verifyBrowseInventorySearchResults({
           records: [{ callNumber: testData.firstCallNumber }],
@@ -127,6 +143,7 @@ describe('Inventory', () => {
         InventorySearchAndFilter.checkBrowseSearchInputFieldContent('');
 
         InventorySearchAndFilter.selectBrowseCallNumbers();
+        BrowseCallNumber.waitForCallNumberToAppear(testData.secondCallNumber);
         InventorySearchAndFilter.browseSearch(testData.secondCallNumber);
         InventorySearchAndFilter.verifyBrowseInventorySearchResults({
           records: [{ callNumber: testData.secondCallNumber }],
