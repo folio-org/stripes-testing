f08f245e9d633b6c5668959dc552d858e72abdf6
sviatlana-stsiapanava <sviatlana_stsiapanava@epam.com>
2025-02-24 13:56:25 +0500

    Revert "Switch affiliations approach"
    This reverts commit f571c657c5dbc00113941e463d16d12fb3b0bfb0.

--- cypress/support/dictionary/affiliations.js
+++ cypress/support/dictionary/affiliations.js
@@ -1,52 +1,27 @@
-const currentEnv = Cypress.env('ECS_ENV_NAME');
-
 export default {
-  sprint: {
-    Consortia: 'cs00000int',
-    University: 'cs00000int_0005',
-    School: 'cs00000int_0003',
-    College: 'cs00000int_0001',
-  },
-  cypress: {
-    Consortia: 'consortium',
-    University: 'university',
-    College: 'college',
-  },
-}[currentEnv];
+  Consortia: 'cs00000int',
+  University: 'cs00000int_0005',
+  School: 'cs00000int_0003',
+  College: 'cs00000int_0001',
+};
 
 export const tenantNames = {
-  sprint: {
-    central: 'Central tenant',
-    college: 'Colleague tenant',
-    university: 'University tenant',
-    professional: 'Professional tenant',
-    school: 'School tenant',
-    special: 'Special tenant',
-  },
-  cypress: {
-    central: 'Consortium',
-    college: 'College',
-    university: 'University',
-    professional: 'University',
-  },
-}[currentEnv];
+  central: 'Central tenant',
+  college: 'Colleague tenant',
+  university: 'University tenant',
+  professional: 'Professional tenant',
+  school: 'School tenant',
+  special: 'Special tenant',
+};
 
 export const tenantCodes = {
-  sprint: {
-    central: 'CEN',
-    college: 'COL',
-    university: 'UNI',
-    professional: 'PROF',
-    school: 'SCHO',
-    special: 'SPE,',
-  },
-  cypress: {
-    central: 'MCO',
-    college: 'COL',
-    university: 'UNI',
-    professional: 'UNI',
-  },
-}[currentEnv];
+  central: 'CEN',
+  college: 'COL',
+  university: 'UNI',
+  professional: 'PROF',
+  school: 'SCHO',
+  special: 'SPE,',
+};
 
 export const tenantErrors = {
   code: 'The code must not be longer than 5 characters alphanumeric.',
