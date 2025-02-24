786f9dfe7dfc88891b8463af863e94f8841d726e
Sviatlana Stsiapanava <152482600+sviatlana-stsiapanava@users.noreply.github.com>
2025-02-24 16:32:32 +0500

    FAT -18878 Add the logic to get tenant names dependent on the environment. (#4769)
    * Add logic to get env dependable tenant names

--- cypress/support/dictionary/affiliations.js
+++ cypress/support/dictionary/affiliations.js
@@ -1,27 +1,52 @@
+const currentEnv = Cypress.env('ecs_env_name');
+
 export default {
-  Consortia: 'cs00000int',
-  University: 'cs00000int_0005',
-  School: 'cs00000int_0003',
-  College: 'cs00000int_0001',
-};
+  sprint: {
+    Consortia: 'cs00000int',
+    University: 'cs00000int_0005',
+    School: 'cs00000int_0003',
+    College: 'cs00000int_0001',
+  },
+  snapshot: {
+    Consortia: 'consortium',
+    University: 'university',
+    College: 'college',
+  },
+}[currentEnv];
 
 export const tenantNames = {
-  central: 'Central tenant',
-  college: 'Colleague tenant',
-  university: 'University tenant',
-  professional: 'Professional tenant',
-  school: 'School tenant',
-  special: 'Special tenant',
-};
+  sprint: {
+    central: 'Central tenant',
+    college: 'Colleague tenant',
+    university: 'University tenant',
+    professional: 'Professional tenant',
+    school: 'School tenant',
+    special: 'Special tenant',
+  },
+  snapshot: {
+    central: 'Consortium',
+    college: 'College',
+    university: 'University',
+    professional: 'University',
+  },
+}[currentEnv];
 
 export const tenantCodes = {
-  central: 'CEN',
-  college: 'COL',
-  university: 'UNI',
-  professional: 'PROF',
-  school: 'SCHO',
-  special: 'SPE,',
-};
+  sprint: {
+    central: 'CEN',
+    college: 'COL',
+    university: 'UNI',
+    professional: 'PROF',
+    school: 'SCHO',
+    special: 'SPE,',
+  },
+  snapshot: {
+    central: 'MCO',
+    college: 'COL',
+    university: 'UNI',
+    professional: 'UNI',
+  },
+}[currentEnv];
 
 export const tenantErrors = {
   code: 'The code must not be longer than 5 characters alphanumeric.',
