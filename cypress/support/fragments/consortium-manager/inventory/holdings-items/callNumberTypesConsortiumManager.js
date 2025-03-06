71a7f4620d6c77e31033e50ff04b13ef60ac4a90
Vadim Eremichev <vadym_yeremichev@epam.com>
2025-03-06 15:39:37 +0200

    Fat 18413 (#4822)
    * implemented test
    
    * implemented pre conditions to test
    
    * improves according to comments

---
+++ cypress/support/fragments/consortium-manager/inventory/holdings-items/callNumberTypesConsortiumManager.js
@@ -0,0 +1,100 @@
+import uuid from 'uuid';
+import { REQUEST_METHOD } from '../../../../constants';
+import { Button, MultiColumnListHeader } from '../../../../../../interactors';
+import ConsortiumManagerApp from '../../consortiumManagerApp';
+
+export const typeActions = {
+  edit: 'edit',
+  trash: 'trash',
+};
+
+const Elements = {
+  newButton: Button('+ New'),
+};
+
+const Actions = {
+  choose() {
+    ConsortiumManagerApp.chooseSecondMenuItem('Call number types');
+    cy.expect(Elements.newButton.is({ disabled: false }));
+    ['Name', 'Source', 'Last updated', 'Member libraries', 'Actions'].forEach((header) => {
+      cy.expect(MultiColumnListHeader(header).exists());
+    });
+  },
+};
+
+const Assertions = {};
+
+const API = {
+  createViaApiShared(type) {
+    const id = uuid();
+    return cy.getConsortiaId().then((consortiaId) => {
+      cy.okapiRequest({
+        method: REQUEST_METHOD.POST,
+        path: `consortia/${consortiaId}/sharing/settings`,
+        body: {
+          url: '/call-number-types',
+          settingId: id,
+          payload: {
+            id,
+            name: type.payload.name,
+            source: 'local',
+          },
+        },
+      }).then(() => {
+        type.url = '/call-number-types';
+        type.settingId = id;
+        return type;
+      });
+    });
+  },
+
+  createViaApiLocal(type, tenants) {
+    const id = uuid();
+    return cy.getConsortiaId().then((consortiaId) => {
+      cy.okapiRequest({
+        method: REQUEST_METHOD.POST,
+        path: `consortia/${consortiaId}/publications`,
+        body: {
+          method: REQUEST_METHOD.POST,
+          tenants: tenants.map((tenant) => tenant.toLowerCase()),
+          payload: {
+            id,
+            name: type.payload.name,
+            source: 'local',
+          },
+          url: '/call-number-types',
+        },
+      }).then(() => {
+        type.url = '/call-number-types';
+        type.tenants = tenants;
+        type.id = id;
+        return type;
+      });
+    });
+  },
+
+  deleteViaApi(type, allowFailure = true) {
+    cy.getConsortiaId().then((consortiaId) => {
+      cy.okapiRequest({
+        method: REQUEST_METHOD.DELETE,
+        path: `consortia/${consortiaId}/sharing/settings/${type.settingId}`,
+        body: type,
+        failOnStatusCode: allowFailure,
+      });
+    });
+  },
+  deleteViaApiLocal(id, allowFailure = true) {
+    cy.okapiRequest({
+      method: REQUEST_METHOD.DELETE,
+      path: `call-number-types/${id}`,
+      isDefaultSearchParamsRequired: false,
+      failOnStatusCode: allowFailure,
+    });
+  },
+};
+
+export default {
+  ...Actions,
+  ...Assertions,
+  ...API,
+};
--- cypress/support/fragments/inventory/inventorySearchAndFilter.js
+++ cypress/support/fragments/inventory/inventorySearchAndFilter.js
@@ -616,6 +616,7 @@ export default {
 
   clickResetAllButton() {
     cy.do(searchAndFilterSection.find(resetAllBtn).click());
+    cy.wait(1000);
   },
 
   clickNextPaginationButton() {
