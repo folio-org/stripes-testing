cd1572aee25b95eb6ac9ca75dc4bdced2be3d29d
Vadym Yeromichev <v.eremichev@gmail.com>
2025-04-03 10:05:54 +0300

    implemenetation of C627461

--- cypress/e2e/consortia/settings/inventory/call-number-browse.cy.js
+++ cypress/e2e/consortia/settings/inventory/call-number-browse.cy.js
@@ -1,6 +1,6 @@
 import { Permissions } from '../../../../support/dictionary';
 import Users from '../../../../support/fragments/users/users';
-import { CallNumberBrowse } from '../../../../support/fragments/settings/inventory/instances/callNumberBrowse';
+import { CallNumberBrowseSettings } from '../../../../support/fragments/settings/inventory/instances/callNumberBrowse';
 import Affiliations, { tenantNames } from '../../../../support/dictionary/affiliations';
 import ConsortiumManager from '../../../../support/fragments/settings/consortium-manager/consortium-manager';
 import SettingsPane from '../../../../support/fragments/settings/settingsPane';
@@ -15,9 +15,21 @@ describe('Inventory', () => {
         let user;
 
         const permissions = [Permissions.uiSettingsCallNumberBrowseView.gui];
+        const callNumberBrowseOptionCentral = 'Call numbers (all)';
+        const callNumberTypeCentral = 'Dewey Decimal classification';
+        const callNumberBrowseOptionMember = 'Library of Congress classification';
+        const callNumberTypeMember = 'Dewey Decimal classification';
 
         before('Create user, login', () => {
           cy.getAdminToken();
+          CallNumberBrowseSettings.assignCallNumberTypesViaApi({
+            name: callNumberBrowseOptionCentral,
+            callNumberTypes: [],
+          });
+          CallNumberBrowseSettings.assignCallNumberTypesViaApi({
+            name: callNumberBrowseOptionMember,
+            callNumberTypes: [],
+          });
           cy.createTempUser(permissions).then((userProperties) => {
             user = userProperties;
             cy.affiliateUserToTenant({
@@ -32,8 +44,8 @@ describe('Inventory', () => {
             });
             TopMenuNavigation.navigateToApp(APPLICATION_NAMES.SETTINGS, 'Inventory');
             ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
-            CallNumberBrowse.openCallNumberBrowse();
-            CallNumberBrowse.validateCallNumberBrowsePaneOpened();
+            CallNumberBrowseSettings.openCallNumberBrowse();
+            CallNumberBrowseSettings.validateCallNumberBrowsePaneOpened();
           });
         });
 
@@ -47,8 +59,70 @@ describe('Inventory', () => {
           'C627461 Edit "Call number browse" option from Central/Member tenants (spitfire)',
           { tags: ['criticalPathECS', 'spitfire', 'C627461'] },
           () => {
-            CallNumberBrowse.clickEditButtonForItem('Call numbers (all)');
-            CallNumberBrowse.validateSaveButtonIsDisabledForItem('Call numbers (all)');
+            CallNumberBrowseSettings.clickEditButtonForItem(callNumberBrowseOptionCentral);
+            CallNumberBrowseSettings.validateSaveButtonStatusForItem({
+              itemName: callNumberBrowseOptionCentral,
+              isEnabled: false,
+            });
+            CallNumberBrowseSettings.validateCancelButtonStatusForItem({
+              itemName: callNumberBrowseOptionCentral,
+              isEnabled: true,
+            });
+            CallNumberBrowseSettings.expandCallNumberTypeDropdownOption(
+              callNumberBrowseOptionCentral,
+            );
+            CallNumberBrowseSettings.validateCallNumberTypesDropdownExpanded(
+              callNumberBrowseOptionCentral,
+            );
+            cy.wait(500);
+            CallNumberBrowseSettings.selectCallNumberTypeDropdownOption(callNumberTypeCentral);
+            CallNumberBrowseSettings.validateCallNumberTypesDropdownExpanded(
+              callNumberBrowseOptionCentral,
+            );
+            CallNumberBrowseSettings.validateSaveButtonStatusForItem({
+              itemName: callNumberBrowseOptionCentral,
+              isEnabled: true,
+            });
+            CallNumberBrowseSettings.validateOptionSelectedInCallNumberTypesDropdown(
+              callNumberBrowseOptionCentral,
+              callNumberTypeCentral,
+            );
+
+            CallNumberBrowseSettings.clickSaveButtonForItem(callNumberBrowseOptionCentral);
+            CallNumberBrowseSettings.validateCallNumberUpdateMessageSuccess(
+              callNumberBrowseOptionCentral,
+            );
+            CallNumberBrowseSettings.validateEditButtonForItemExists(callNumberBrowseOptionCentral);
+            CallNumberBrowseSettings.validateCallNumberBrowseRowInTable(
+              callNumberBrowseOptionCentral,
+              callNumberTypeCentral,
+            );
+
+            ConsortiumManager.switchActiveAffiliation(tenantNames.central, tenantNames.college);
+            CallNumberBrowseSettings.openCallNumberBrowse();
+            CallNumberBrowseSettings.validateCallNumberBrowsePaneOpened();
+            CallNumberBrowseSettings.validateCallNumberBrowseRowInTable(
+              callNumberBrowseOptionCentral,
+              callNumberTypeCentral,
+            );
+            CallNumberBrowseSettings.setCallNumberTypeOptions(callNumberBrowseOptionMember, [
+              callNumberTypeMember,
+            ]);
+            CallNumberBrowseSettings.validateCallNumberUpdateMessageSuccess(
+              callNumberBrowseOptionMember,
+            );
+
+            ConsortiumManager.switchActiveAffiliation(tenantNames.college, tenantNames.central);
+            CallNumberBrowseSettings.openCallNumberBrowse();
+            CallNumberBrowseSettings.validateCallNumberBrowsePaneOpened();
+            CallNumberBrowseSettings.validateCallNumberBrowseRowInTable(
+              callNumberBrowseOptionCentral,
+              callNumberTypeCentral,
+            );
+            CallNumberBrowseSettings.validateCallNumberBrowseRowInTable(
+              callNumberBrowseOptionMember,
+              callNumberTypeMember,
+            );
           },
         );
       });
--- cypress/support/fragments/settings/inventory/instances/callNumberBrowse.js
+++ cypress/support/fragments/settings/inventory/instances/callNumberBrowse.js
@@ -4,8 +4,13 @@ import {
   PaneContent,
   NavListItem,
   MultiColumnListRow,
+  MultiColumnListCell,
+  MultiSelectOption,
+  MultiSelect,
+  MultiSelectMenu,
   including,
 } from '../../../../../../interactors';
+import InteractorsTools from '../../../../utils/interactorsTools';
 
 const sectionName = 'Call number browse';
 
@@ -22,13 +27,57 @@ const elements = {
       MultiColumnListRow({ innerHTML: including(callNumberName), isContainer: true }),
     );
   },
+  callNumberTypesDropdown: MultiSelect({
+    label: including('Call number types'),
+  }),
+};
+
+const Assertions = {
+  validateCallNumberBrowsePaneOpened() {
+    cy.expect(elements.callNumberBrowsePane.exists());
+  },
+  validateSaveButtonStatusForItem({ itemName, isEnabled }) {
+    const targetRow = elements.getTargetRowByName(itemName);
+    cy.expect(targetRow.find(elements.saveButton).has({ disabled: !isEnabled }));
+  },
+  validateCancelButtonStatusForItem({ itemName, isEnabled }) {
+    const targetRow = elements.getTargetRowByName(itemName);
+    cy.expect(targetRow.find(elements.cancelButton).has({ disabled: !isEnabled }));
+  },
+
+  validateEditButtonForItemExists(itemName) {
+    const targetRow = elements.getTargetRowByName(itemName);
+    cy.expect(targetRow.find(elements.editButton).exists());
+  },
+
+  validateOptionSelectedInCallNumberTypesDropdown(itemName, option) {
+    const selectedOptions = Array.isArray(option) ? option : [option];
+    const targetRow = elements.getTargetRowByName(itemName);
+    cy.expect(targetRow.find(MultiSelect({ selected: selectedOptions })).exists());
+  },
+  validateCallNumberTypesDropdownExpanded(itemName) {
+    const targetRow = elements.getTargetRowByName(itemName);
+    cy.expect(targetRow.find(elements.callNumberTypesDropdown).has({ open: true }));
+  },
+  validateCallNumberUpdateMessageSuccess(itemName) {
+    InteractorsTools.checkCalloutMessage(
+      `The call number browse type ${itemName} was successfully updated`,
+    );
+  },
+
+  validateCallNumberBrowseRowInTable(itemName, callNumberTypes) {
+    const targetRow = elements.getTargetRowByName(itemName);
+    cy.expect([
+      targetRow.find(MultiColumnListCell(itemName)).exists(),
+      targetRow.find(MultiColumnListCell(callNumberTypes)).exists(),
+      targetRow.find(elements.editButton).exists(),
+    ]);
+  },
 };
 
 const Actions = {
   openCallNumberBrowse() {
-    cy.intercept('/browse/config/instance-call-number').as('callNumberTypes');
     cy.do(elements.navigationPane.find(NavListItem(sectionName)).click());
-    cy.wait('@callNumberTypes', { timeout: 10_000 });
   },
 
   clickEditButtonForItem(optionName) {
@@ -39,19 +88,30 @@ const Actions = {
     const targetRow = elements.getTargetRowByName(optionName);
     cy.do(targetRow.find(elements.saveButton).click());
   },
-};
 
-const Assertions = {
-  validateCallNumberBrowsePaneOpened() {
-    cy.expect(elements.callNumberBrowsePane.exists());
-  },
-  validateSaveButtonIsDisabledForItem(optionName) {
+  expandCallNumberTypeDropdownOption(optionName) {
     const targetRow = elements.getTargetRowByName(optionName);
-    cy.expect(targetRow.find(elements.saveButton).isDisabled());
+    cy.do(targetRow.find(elements.callNumberTypesDropdown).toggle());
+    cy.expect(targetRow.find(elements.callNumberTypesDropdown).has({ open: true }));
+  },
+
+  selectCallNumberTypeDropdownOption(optionName) {
+    cy.do(MultiSelectMenu().find(MultiSelectOption(optionName)).click());
+  },
+
+  setCallNumberTypeOptions(itemName, optionNames) {
+    this.clickEditButtonForItem(itemName);
+    this.expandCallNumberTypeDropdownOption(itemName);
+    const optionsToSelect = Array.isArray(optionNames) ? optionNames : [optionNames];
+    optionsToSelect.forEach((optionName) => {
+      cy.wait(500);
+      this.selectCallNumberTypeDropdownOption(optionName);
+    });
+    this.clickSaveButtonForItem(itemName);
   },
 };
 
-export const CallNumberBrowse = {
+export const CallNumberBrowseSettings = {
   ...Actions,
   ...Assertions,
 };
