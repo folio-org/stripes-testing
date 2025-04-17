1a30725dd39f93e9c73048d207dcea565a678ff5
IhorBohdan <ihor_bohdan@epam.com>
2025-04-17 17:11:11 +0300

    Thunderjet updates

--- cypress/e2e/invoices/approve-invoice.cy.js
+++ cypress/e2e/invoices/approve-invoice.cy.js
@@ -1,7 +1,6 @@
 import permissions from '../../support/dictionary/permissions';
 import Helper from '../../support/fragments/finance/financeHelper';
 import Funds from '../../support/fragments/finance/funds/funds';
-import NewFund from '../../support/fragments/finance/funds/newFund';
 import Invoices from '../../support/fragments/invoices/invoices';
 import NewInvoice from '../../support/fragments/invoices/newInvoice';
 import NewInvoiceLine from '../../support/fragments/invoices/newInvoiceLine';
@@ -10,40 +9,62 @@ import Organizations from '../../support/fragments/organizations/organizations';
 import TopMenu from '../../support/fragments/topMenu';
 import TopMenuNavigation from '../../support/fragments/topMenuNavigation';
 import Users from '../../support/fragments/users/users';
-import DateTools from '../../support/utils/dateTools';
 import Approvals from '../../support/fragments/settings/invoices/approvals';
+import Ledgers from '../../support/fragments/finance/ledgers/ledgers';
+import Budgets from '../../support/fragments/finance/budgets/budgets';
+import FiscalYears from '../../support/fragments/finance/fiscalYears/fiscalYears';
 
 describe('Invoices', () => {
   const invoice = { ...NewInvoice.defaultUiInvoice };
   const vendorPrimaryAddress = { ...VendorAddress.vendorAddress };
   const invoiceLine = { ...NewInvoiceLine.defaultUiInvoiceLine };
-  const defaultFund = { ...NewFund.defaultFund };
+
+  const defaultFiscalYear = { ...FiscalYears.defaultUiFiscalYear };
+  const defaultLedger = { ...Ledgers.defaultUiLedger };
+  const defaultFund = { ...Funds.defaultUiFund };
+  const defaultBudget = {
+    ...Budgets.getDefaultBudget(),
+    allocated: 100,
+  };
   const subtotalValue = 100;
   let user;
 
   before(() => {
     cy.getAdminToken();
-    Organizations.getOrganizationViaApi({ query: `name=${invoice.vendorName}` }).then(
-      (organization) => {
-        invoice.accountingCode = organization.erpCode;
-        Object.assign(
-          vendorPrimaryAddress,
-          organization.addresses.find((address) => address.isPrimary === true),
-        );
-        cy.getBatchGroups().then((batchGroup) => {
-          invoice.batchGroup = batchGroup.name;
-          Funds.createFundViaUI(defaultFund).then(() => {
-            Funds.addBudget(100);
-            Funds.checkCreatedBudget(defaultFund.code, DateTools.getCurrentFiscalYearCode());
-            invoiceLine.subTotal = -subtotalValue;
-            TopMenuNavigation.openAppFromDropdown('Invoices');
-            Invoices.createDefaultInvoice(invoice, vendorPrimaryAddress);
-            Invoices.createInvoiceLine(invoiceLine);
-            Invoices.addFundDistributionToLine(invoiceLine, defaultFund);
-          });
+    FiscalYears.createViaApi(defaultFiscalYear).then((firstFiscalYearResponse) => {
+      defaultFiscalYear.id = firstFiscalYearResponse.id;
+      defaultBudget.fiscalYearId = firstFiscalYearResponse.id;
+      defaultLedger.fiscalYearOneId = defaultFiscalYear.id;
+      Ledgers.createViaApi(defaultLedger).then((ledgerResponse) => {
+        defaultLedger.id = ledgerResponse.id;
+        defaultFund.ledgerId = defaultLedger.id;
+
+        Funds.createViaApi(defaultFund).then((fundResponse) => {
+          defaultFund.id = fundResponse.fund.id;
+          defaultBudget.fundId = fundResponse.fund.id;
+          Budgets.createViaApi(defaultBudget);
+          Organizations.getOrganizationViaApi({ query: `name=${invoice.vendorName}` }).then(
+            (organization) => {
+              invoice.accountingCode = organization.erpCode;
+              Object.assign(
+                vendorPrimaryAddress,
+                organization.addresses.find((address) => address.isPrimary === true),
+              );
+              cy.getBatchGroups().then((batchGroup) => {
+                invoice.batchGroup = batchGroup.name;
+                invoiceLine.subTotal = -subtotalValue;
+                cy.loginAsAdmin();
+                TopMenuNavigation.openAppFromDropdown('Invoices');
+                Invoices.createDefaultInvoice(invoice, vendorPrimaryAddress);
+                Invoices.createInvoiceLine(invoiceLine);
+                Invoices.addFundDistributionToLine(invoiceLine, defaultFund);
+              });
+            },
+          );
         });
-      },
-    );
+      });
+    });
+
     cy.createTempUser([
       permissions.uiFinanceViewFundAndBudget.gui,
       permissions.viewEditCreateInvoiceInvoiceLine.gui,
@@ -75,11 +96,11 @@ describe('Invoices', () => {
       Helper.selectFundsNavigation();
       Helper.searchByName(defaultFund.name);
       Funds.selectFund(defaultFund.name);
-      Funds.openBudgetDetails(defaultFund.code, DateTools.getCurrentFiscalYearCode());
+      Funds.selectBudgetDetails();
       Funds.openTransactions();
       Funds.selectTransactionInList('Pending payment');
       Funds.varifyDetailsInTransaction(
-        DateTools.getCurrentFiscalYearCode(),
+        defaultFiscalYear.code,
         '$100.00',
         invoice.invoiceNumber,
         'Pending payment',
