69397121ed6255cd537450c0f3e00fc7ebb4de1b
Kyrylo Brener <kbrener@ebsco.com>
2025-05-06 12:19:17 +0300

    sso test updated

--- cypress/e2e/fse/ui/sso-ui.cy.js
+++ cypress/e2e/fse/ui/sso-ui.cy.js
@@ -1,9 +1,26 @@
 describe('fse-sso - UI', () => {
+  beforeEach(() => {
+    // hide sensitive data from the report
+    cy.allure().logCommandSteps(false);
+    cy.getUserToken(Cypress.env('diku_login'), Cypress.env('diku_password'));
+    cy.allure().logCommandSteps();
+  });
+
   it(
     `TC195393 - verify that SSO button is displayed for ${Cypress.env('OKAPI_HOST')}`,
-    { tags: ['fse', 'ui', 'sso'] },
+    { tags: ['fse', 'ui', 'sso', 'sanity'] },
     () => {
-      cy.checkSsoButton();
+      cy.checkSsoSupported().then((response) => {
+        cy.log('saml/check status: ' + response.status);
+        // verify sso button is present if supported on current tenant
+        if (response.status === 200 && response.body.active === true) {
+          cy.checkSsoButton();
+        } else {
+          cy.log(
+            `SSO button is not supported on the current tenant ${Cypress.env('OKAPI_TENANT')} or saml/check returned error`,
+          );
+        }
+      });
     },
   );
 });
