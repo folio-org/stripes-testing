4d9d81379200f4343656372373988dd02dc7a452
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-05-08 13:46:37 +0500

    Thunderjet QG fix 5/8

--- cypress/support/api/acq-units.js
+++ cypress/support/api/acq-units.js
@@ -1,6 +1,26 @@
+import uuid from 'uuid';
+
 Cypress.Commands.add('getAcqUnitsApi', (searchParams) => {
   return cy.okapiRequest({
     path: 'acquisitions-units/units',
     searchParams,
   });
 });
+
+Cypress.Commands.add(
+  'createAcqUnitApi',
+  (name, restrictions = { read: false, create: true, update: true, delete: true }) => {
+    return cy.okapiRequest({
+      method: 'POST',
+      path: 'acquisitions-units/units',
+      body: {
+        id: uuid(),
+        name,
+        protectRead: restrictions.read,
+        protectCreate: restrictions.create,
+        protectUpdate: restrictions.update,
+        protectDelete: restrictions.delete,
+      },
+    });
+  },
+);
