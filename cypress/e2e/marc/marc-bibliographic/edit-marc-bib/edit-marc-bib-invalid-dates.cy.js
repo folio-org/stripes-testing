cd180810ea4c0c25bc6b6f6242627c89e9d9ff58
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-05-28 21:56:34 +0500

    FAT-19931: test updated

--- cypress/e2e/marc/marc-bibliographic/edit-marc-bib/edit-marc-bib-invalid-dates.cy.js
+++ cypress/e2e/marc/marc-bibliographic/edit-marc-bib/edit-marc-bib-invalid-dates.cy.js
@@ -20,33 +20,48 @@ describe('MARC', () => {
         tag245: '245',
         tag008: '008',
         dateTypeOption: INVENTORY_008_FIELD_DTST_DROPDOWN.K,
-        dateTypeText: INSTANCE_DATE_TYPES.DISTRIBUTION,
+        dateTypeText: INSTANCE_DATE_TYPES.RANGE,
         dateLengthError: (dateNumber) => `Fail: Invalid Date ${dateNumber} field length, must be 4 characters`,
       };
       const datesData = [
-        { date1: '1999u', date2: '20b27', errors: [] },
+        { date1: '1999u', date2: '20b27', errors: [], close: false },
         {
           date1: '1',
           date2: '9',
           errors: [testData.dateLengthError(1), testData.dateLengthError(2)],
+          close: false,
         },
-        { date1: '12', date2: '199u', errors: [testData.dateLengthError(1)] },
-        { date1: '12b', date2: '199u', errors: [testData.dateLengthError(1)] },
-        { date1: '12b6', date2: '19', errors: [testData.dateLengthError(2)] },
-        { date1: '12b6', date2: '199', errors: [testData.dateLengthError(2)] },
-        { date1: '12b6', date2: '199u', errors: [] },
+        { date1: '12', date2: '199u', errors: [testData.dateLengthError(1)], close: true },
+        { date1: '12b', date2: '199u', errors: [testData.dateLengthError(1)], close: true },
+        { date1: '12b6', date2: '19', errors: [testData.dateLengthError(2)], close: true },
+        { date1: '12b6', date2: '199', errors: [testData.dateLengthError(2)], close: true },
+        { date1: '12b6', date2: '199u', errors: [], close: true },
       ];
       const createdRecordIDs = [];
       let user;
 
       function checkDateFields(date1, date2) {
-        QuickMarcEditor.verify008TextFields(INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DATE1, date1);
-        QuickMarcEditor.verify008TextFields(INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DATE2, date2);
+        QuickMarcEditor.verify008TextFields(
+          INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DATE1,
+          date1.slice(0, 4),
+        );
+        QuickMarcEditor.verify008TextFields(
+          INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DATE2,
+          date2.slice(0, 4),
+        );
       }
 
       function fillDateFields(date1, date2) {
-        QuickMarcEditor.update008TextFields(INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DATE1, date1);
-        QuickMarcEditor.update008TextFields(INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DATE2, date2);
+        QuickMarcEditor.update008TextFields(
+          INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DATE1,
+          date1,
+          true,
+        );
+        QuickMarcEditor.update008TextFields(
+          INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DATE2,
+          date2,
+          true,
+        );
         checkDateFields(date1, date2);
       }
 
@@ -89,6 +104,8 @@ describe('MARC', () => {
           InventoryInstance.editMarcBibliographicRecord();
           QuickMarcEditor.updateLDR06And07Positions();
           QuickMarcEditor.updateExistingField(testData.tag245, `$a ${testData.instanceTitle}`);
+          QuickMarcEditor.updateIndicatorValue(testData.tag245, '1', 0);
+          QuickMarcEditor.updateIndicatorValue(testData.tag245, '1', 1);
           QuickMarcEditor.selectFieldsDropdownOption(
             testData.tag008,
             INVENTORY_008_FIELD_DROPDOWNS_BOXES_NAMES.DTST,
@@ -102,12 +119,33 @@ describe('MARC', () => {
 
           datesData.forEach((dateData) => {
             fillDateFields(dateData.date1, dateData.date2);
+            if (dateData.errors.length) {
+              if (dateData.close) {
+                QuickMarcEditor.pressSaveAndClose();
+                dateData.errors.forEach((error) => {
+                  QuickMarcEditor.checkErrorMessage(3, error);
+                });
+                QuickMarcEditor.verifyValidationCallout(0, dateData.errors.length);
+              } else {
+                QuickMarcEditor.clickSaveAndKeepEditingButton();
+                dateData.errors.forEach((error) => {
+                  QuickMarcEditor.checkErrorMessage(3, error);
+                });
+                QuickMarcEditor.verifyValidationCallout(0, dateData.errors.length);
+              }
+              QuickMarcEditor.closeAllCallouts();
+            } else if (dateData.close) {
+              QuickMarcEditor.pressSaveAndClose();
+              QuickMarcEditor.checkAfterSaveAndClose();
+              InstanceRecordView.verifyInstanceIsOpened(testData.instanceTitle);
+              InstanceRecordView.verifyDates(dateData.date1, dateData.date2, testData.dateTypeText);
+            } else {
+              QuickMarcEditor.clickSaveAndKeepEditingButton();
+              QuickMarcEditor.checkAfterSaveAndKeepEditing();
+              checkDateFields(dateData.date1, dateData.date2);
+              cy.wait(2000);
+            }
           });
-
-          QuickMarcEditor.saveAndCloseWithValidationWarnings();
-          QuickMarcEditor.checkAfterSaveAndClose();
-          InstanceRecordView.verifyInstanceIsOpened(testData.instanceTitle);
-          InstanceRecordView.verifyDates(testData.date1, undefined, testData.dateTypeText);
         },
       );
     });
