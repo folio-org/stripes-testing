cdc00d3f818b08bd9630a278d025780486b6fb7f
Sviatlana Stsiapanava <sviatlana_stsiapanava@epam.com>
2025-05-28 19:05:40 +0300

    Improve methods

--- cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-holdings-with-source-marc.cy.js
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-holdings-with-source-marc.cy.js
@@ -242,7 +242,7 @@ describe('bulk-edit', () => {
             BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_UUID,
             uuid,
             BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.SUPPRESS_FROM_DISCOVERY,
-            'true',
+            true,
           );
         });
 
@@ -270,7 +270,7 @@ describe('bulk-edit', () => {
             BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_UUID,
             uuid,
             BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.SUPPRESS_FROM_DISCOVERY,
-            'true',
+            true,
           );
         });
 
--- cypress/e2e/consortia/bulk-edit/in-app/bulk-edit-in-app-items-suppress-from-discovery-in-central-tenant.cy.js
+++ cypress/e2e/consortia/bulk-edit/in-app/bulk-edit-in-app-items-suppress-from-discovery-in-central-tenant.cy.js
@@ -294,7 +294,7 @@ describe('Bulk-edit', () => {
                 BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_ITEMS.ITEM_UUID,
                 instance.itemUuid,
                 BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_ITEMS.SUPPRESS_FROM_DISCOVERY,
-                'true',
+                true,
               );
             });
 
@@ -318,7 +318,7 @@ describe('Bulk-edit', () => {
                 BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_ITEMS.ITEM_UUID,
                 instance.itemUuid,
                 BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_ITEMS.SUPPRESS_FROM_DISCOVERY,
-                'true',
+                true,
               );
             });
 
@@ -392,7 +392,7 @@ describe('Bulk-edit', () => {
                     BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_ITEMS.ITEM_UUID,
                     instance.itemUuid,
                     BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_ITEMS.SUPPRESS_FROM_DISCOVERY,
-                    'false',
+                    false,
                   );
                 });
 
@@ -416,7 +416,7 @@ describe('Bulk-edit', () => {
                     BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_ITEMS.ITEM_UUID,
                     instance.itemUuid,
                     BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_ITEMS.SUPPRESS_FROM_DISCOVERY,
-                    'false',
+                    false,
                   );
                 });
 
