30d93b3eb412c9250c432093b4a97c813a1e7efc
Tetiana_Paranich <tetiana_paranich@epam.com>
2025-06-11 19:30:49 +0300

    fixed C422064

--- cypress/e2e/data-import/log-details/record-title-is-present-in-log-details-when-job-profile-only-involves-holdings-and-items-without-instance-match-item.cy.js
+++ cypress/e2e/data-import/log-details/record-title-is-present-in-log-details-when-job-profile-only-involves-holdings-and-items-without-instance-match-item.cy.js
@@ -317,7 +317,6 @@ describe('Data Import', () => {
           const expectedUUID = InventorySearchAndFilter.getInstanceUUIDFromRequest(req);
 
           FileManager.createFile(`cypress/fixtures/${csvFileName}`, expectedUUID);
-          FileManager.createFile(`cypress/fixtures/${csvFileName}`, expectedUUID);
         });
 
         // download exported marc file
