2995d6edd4be8502360dfc8a863dcc13cc31bd38
Lisavetti <lizaemets0244@gmail.com>
2025-06-16 17:38:05 +0300

    fixed C367971, rewrote function for choosing and switching service points (thunderjet)

--- cypress/support/fragments/settings/tenant/servicePoints/switchServicePoint.js
+++ cypress/support/fragments/settings/tenant/servicePoints/switchServicePoint.js
@@ -16,34 +16,36 @@ const usersList = MultiColumnList({ id: 'list-users' });
 
 export default {
   switchServicePoint: (servicePoint) => {
-    TopMenuNavigation.openAppFromDropdown('Users');
-    usersSearchPane.searchByKeywords('admin');
-    cy.wait(4000);
-    cy.expect(usersResult.is({ empty: false }));
-    cy.do(usersList.find(Link('ADMINISTRATOR, Diku_admin')).click()); // need to paramrtrize
-    cy.wait(4000);
-    UserEdit.openEdit();
-    cy.wait(4000);
-    UserEdit.openServicePointsAccordion();
+    cy.getAdminSourceRecord().then((sourceRecord) => {
+      TopMenuNavigation.openAppFromDropdown('Users');
+      usersSearchPane.searchByKeywords('admin');
+      cy.wait(4000);
+      cy.expect(usersResult.is({ empty: false }));
+      cy.do(usersList.find(Link(sourceRecord)).click());
+      cy.wait(4000);
+      UserEdit.openEdit();
+      cy.wait(4000);
+      UserEdit.openServicePointsAccordion();
 
-    cy.get('section#servicePoints').then(($section) => {
-      const pointIsPresent =
-        $section.find(`li[data-test-service-point="${servicePoint.id}"]`).length > 0;
-      if (!pointIsPresent) {
-        UserEdit.addServicePoints(servicePoint.name);
-      }
-      UserEdit.selectPreferableServicePoint(servicePoint.name);
-      UserEdit.checkServicePoints(servicePoint.name);
-    });
+      cy.get('section#servicePoints').then(($section) => {
+        const pointIsPresent =
+          $section.find(`li[data-test-service-point="${servicePoint.id}"]`).length > 0;
+        if (!pointIsPresent) {
+          UserEdit.addServicePoints(servicePoint.name);
+        }
+        UserEdit.selectPreferableServicePoint(servicePoint.name);
+        UserEdit.checkServicePoints(servicePoint.name);
+      });
 
-    cy.contains('button', 'Save & close', { timeout: 5000 }).then(($btn) => {
-      const isDisabled = $btn.prop('disabled');
-      UserEdit.verifySaveAndCloseIsDisabled(isDisabled);
-      if (isDisabled) {
-        UserEdit.cancelEdit();
-      } else {
-        UserEdit.saveAndClose();
-      }
+      cy.contains('button', 'Save & close', { timeout: 5000 }).then(($btn) => {
+        const isDisabled = $btn.prop('disabled');
+        UserEdit.verifySaveAndCloseIsDisabled(isDisabled);
+        if (isDisabled) {
+          UserEdit.cancelEdit();
+        } else {
+          UserEdit.saveAndClose();
+        }
+      });
     });
   },
 
