a17fac44fb3160ab7dd5a3f322b84aafa6d3a487
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-07-03 14:49:59 +0500

    C380757 test tags added

--- cypress/e2e/check-roles-for-migrated-users.cy.js
+++ cypress/e2e/check-roles-for-migrated-users.cy.js
@@ -11,24 +11,28 @@ import UsersSearchPane from '../support/fragments/users/usersSearchPane';
 import UsersSearchResultsPane from '../support/fragments/users/usersSearchResultsPane';
 
 describe('Check roles for migrated users', () => {
-  it('Check roles for migrated users', { tags: ['checkRolesForMigrationUsers'] }, () => {
-    cy.loginAsAdmin();
-    TopMenuNavigation.openAppFromDropdown(APPLICATION_NAMES.USERS);
-    Users.waitLoading();
+  it(
+    'C805757 Check roles for migrated users (eureka)',
+    { tags: ['eurekaMigration', 'C805757', 'checkRolesForMigrationUsers'] },
+    () => {
+      cy.loginAsAdmin();
+      TopMenuNavigation.openAppFromDropdown(APPLICATION_NAMES.USERS);
+      Users.waitLoading();
 
-    migrationUsers.forEach((user) => {
-      UsersSearchPane.searchByKeywords(MigrationData.getUsername(user.caseId));
-      UsersCard.verifyUserLastFirstNameInCard(
-        MigrationData.getLastName(user.caseId),
-        MigrationData.getFirstName(user.caseId),
-      );
-      UsersCard.verifyUserRolesCounter('1');
-      UsersCard.clickUserRolesAccordion();
-      UsersCard.verifyUserRoleNames([`migration_permission_set_${user.caseId}`]);
-      UsersCard.verifyUserRolesRowsCount(1);
-      UsersCard.close();
-      UsersSearchPane.resetAllFilters();
-      UsersSearchResultsPane.verifySearchPaneIsEmpty();
-    });
-  });
+      migrationUsers.forEach((user) => {
+        UsersSearchPane.searchByKeywords(MigrationData.getUsername(user.caseId));
+        UsersCard.verifyUserLastFirstNameInCard(
+          MigrationData.getLastName(user.caseId),
+          MigrationData.getFirstName(user.caseId),
+        );
+        UsersCard.verifyUserRolesCounter('1');
+        UsersCard.clickUserRolesAccordion();
+        UsersCard.verifyUserRoleNames([`migration_permission_set_${user.caseId}`]);
+        UsersCard.verifyUserRolesRowsCount(1);
+        UsersCard.close();
+        UsersSearchPane.resetAllFilters();
+        UsersSearchResultsPane.verifySearchPaneIsEmpty();
+      });
+    },
+  );
 });
