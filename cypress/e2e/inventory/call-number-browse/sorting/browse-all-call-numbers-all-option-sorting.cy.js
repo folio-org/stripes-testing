7af28c2f96d0895fff999bcc5bb52f8ff9f3913d
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-07-09 16:18:56 +0500

    FAT-20342: test added

--- cypress/e2e/inventory/call-number-browse/sorting/browse-all-call-numbers-all-option-sorting.cy.js
+++ cypress/e2e/inventory/call-number-browse/sorting/browse-all-call-numbers-all-option-sorting.cy.js
@@ -16,133 +16,135 @@ import { CallNumberBrowseSettings } from '../../../../support/fragments/settings
 
 describe('Inventory', () => {
   describe('Call Number Browse', () => {
-    const randomPostfix = getRandomPostfix();
-    const randomDigits = randomFourDigitNumber();
-    const instanceTitle = `AT_C477566_FolioInstance_${randomPostfix}`;
-    const testData = { user: {} };
-    const callNumberBase = 'QD477 .M566';
-    const callNumberEndings = [
-      `${randomDigits}1`,
-      `${randomDigits}2`,
-      `${randomDigits}3`,
-      `${randomDigits}4`,
-      `${randomDigits}5`,
-      `${randomDigits}6`,
-      `${randomDigits}7`,
-      `${randomDigits}8`,
-    ];
-    const callNumberTypes = [
-      CALL_NUMBER_TYPE_NAMES.DEWAY_DECIMAL,
-      CALL_NUMBER_TYPE_NAMES.LIBRARY_OF_CONGRESS,
-      'Local',
-      CALL_NUMBER_TYPE_NAMES.LIBRARY_OF_MEDICINE,
-      CALL_NUMBER_TYPE_NAMES.OTHER_SCHEME,
-      CALL_NUMBER_TYPE_NAMES.SUDOC,
-      CALL_NUMBER_TYPE_NAMES.MOYS,
-      '', // No type selected
-    ];
-    let holdingsId;
-    let callNumberTypeIds = [];
-    let locationId;
-    let loanTypeId;
-    let materialTypeId;
-    let localTypeId;
+    describe('Sorting', () => {
+      const randomPostfix = getRandomPostfix();
+      const randomDigits = randomFourDigitNumber();
+      const instanceTitle = `AT_C477566_FolioInstance_${randomPostfix}`;
+      const testData = { user: {} };
+      const callNumberBase = 'QD477 .M566';
+      const callNumberEndings = [
+        `${randomDigits}1`,
+        `${randomDigits}2`,
+        `${randomDigits}3`,
+        `${randomDigits}4`,
+        `${randomDigits}5`,
+        `${randomDigits}6`,
+        `${randomDigits}7`,
+        `${randomDigits}8`,
+      ];
+      const callNumberTypes = [
+        CALL_NUMBER_TYPE_NAMES.DEWAY_DECIMAL,
+        CALL_NUMBER_TYPE_NAMES.LIBRARY_OF_CONGRESS,
+        'Local',
+        CALL_NUMBER_TYPE_NAMES.LIBRARY_OF_MEDICINE,
+        CALL_NUMBER_TYPE_NAMES.OTHER_SCHEME,
+        CALL_NUMBER_TYPE_NAMES.SUDOC,
+        CALL_NUMBER_TYPE_NAMES.MOYS,
+        '', // No type selected
+      ];
+      let holdingsId;
+      let callNumberTypeIds = [];
+      let locationId;
+      let loanTypeId;
+      let materialTypeId;
+      let localTypeId;
 
-    before('Create data and user', () => {
-      cy.getAdminToken();
-      InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C477566');
-      cy.then(() => {
-        CallNumberTypes.createCallNumberTypeViaApi({
-          name: `AT_C477566_Type_${randomPostfix}`,
-        }).then((id) => {
-          localTypeId = id;
-          CallNumberTypes.getCallNumberTypesViaAPI().then((res) => {
-            callNumberTypeIds = callNumberTypes.map((type) => {
-              if (!type) return undefined;
-              if (type === 'Local') {
-                return localTypeId;
-              }
-              const found = res.find((t) => t.name === type);
-              if (found) return found.id;
-              return undefined;
+      before('Create data and user', () => {
+        cy.getAdminToken();
+        InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C477566');
+        cy.then(() => {
+          CallNumberTypes.createCallNumberTypeViaApi({
+            name: `AT_C477566_Type_${randomPostfix}`,
+          }).then((id) => {
+            localTypeId = id;
+            CallNumberTypes.getCallNumberTypesViaAPI().then((res) => {
+              callNumberTypeIds = callNumberTypes.map((type) => {
+                if (!type) return undefined;
+                if (type === 'Local') {
+                  return localTypeId;
+                }
+                const found = res.find((t) => t.name === type);
+                if (found) return found.id;
+                return undefined;
+              });
             });
           });
-        });
 
-        cy.getLocations({ limit: 1, query: '(isActive=true and name<>"AT_*")' }).then((res) => {
-          locationId = res.id;
-        });
-        cy.getLoanTypes({ limit: 1, query: 'name<>"AT_*"' }).then((res) => {
-          loanTypeId = res[0].id;
-        });
-        cy.getMaterialTypes({ limit: 1, query: 'source=folio' }).then((res) => {
-          materialTypeId = res.id;
-        });
-      })
-        .then(() => {
-          const instanceData = InventoryInstances.generateFolioInstances({
-            instanceTitlePrefix: instanceTitle,
-            holdingsCount: 1,
-          })[0];
-          InventoryInstances.createFolioInstancesViaApi({
-            folioInstances: [instanceData],
-            location: { id: locationId },
-          }).then((createdInstance) => {
-            holdingsId = createdInstance.holdings[0].id;
+          cy.getLocations({ limit: 1, query: '(isActive=true and name<>"AT_*")' }).then((res) => {
+            locationId = res.id;
+          });
+          cy.getLoanTypes({ limit: 1, query: 'name<>"AT_*"' }).then((res) => {
+            loanTypeId = res[0].id;
+          });
+          cy.getMaterialTypes({ limit: 1, query: 'source=folio' }).then((res) => {
+            materialTypeId = res.id;
           });
         })
-        .then(() => {
-          cy.getAdminToken();
-          for (let i = 0; i < 8; i++) {
-            ItemRecordNew.createViaApi({
-              holdingsId,
-              materialTypeId,
-              permanentLoanTypeId: loanTypeId,
-              itemLevelCallNumber: `${callNumberBase} ${callNumberEndings[i]}`,
-              itemLevelCallNumberTypeId: callNumberTypeIds[i],
+          .then(() => {
+            const instanceData = InventoryInstances.generateFolioInstances({
+              instanceTitlePrefix: instanceTitle,
+              holdingsCount: 1,
+            })[0];
+            InventoryInstances.createFolioInstancesViaApi({
+              folioInstances: [instanceData],
+              location: { id: locationId },
+            }).then((createdInstance) => {
+              holdingsId = createdInstance.holdings[0].id;
             });
-          }
-          cy.createTempUser([Permissions.inventoryAll.gui]).then((userProps) => {
-            testData.user = userProps;
+          })
+          .then(() => {
+            cy.getAdminToken();
+            for (let i = 0; i < 8; i++) {
+              ItemRecordNew.createViaApi({
+                holdingsId,
+                materialTypeId,
+                permanentLoanTypeId: loanTypeId,
+                itemLevelCallNumber: `${callNumberBase} ${callNumberEndings[i]}`,
+                itemLevelCallNumberTypeId: callNumberTypeIds[i],
+              });
+            }
+            cy.createTempUser([Permissions.inventoryAll.gui]).then((userProps) => {
+              testData.user = userProps;
+            });
+            CallNumberBrowseSettings.assignCallNumberTypesViaApi({
+              name: BROWSE_CALL_NUMBER_OPTIONS.CALL_NUMBERS_ALL,
+              callNumberTypes: [],
+            });
           });
-          CallNumberBrowseSettings.assignCallNumberTypesViaApi({
-            name: BROWSE_CALL_NUMBER_OPTIONS.CALL_NUMBERS_ALL,
-            callNumberTypes: [],
-          });
-        });
-    });
-
-    after('Clean up', () => {
-      cy.getAdminToken();
-      Users.deleteViaApi(testData.user.userId);
-      InventoryInstances.deleteFullInstancesByTitleViaApi(instanceTitle);
-      CallNumberTypes.deleteLocalCallNumberTypeViaApi(localTypeId);
-      CallNumberBrowseSettings.assignCallNumberTypesViaApi({
-        name: BROWSE_CALL_NUMBER_OPTIONS.CALL_NUMBERS_ALL,
-        callNumberTypes: [],
       });
-    });
 
-    it(
-      'C477566 Similar call numbers with different types selected are sorted alphabetically when used "Call numbers (all)" browse option (case 2) (spitfire)',
-      { tags: ['criticalPath', 'spitfire', 'nonParallel', 'C477566'] },
-      () => {
-        const expectedRows = callNumberEndings.map((num) => `${callNumberBase} ${num}`);
-        cy.login(testData.user.username, testData.user.password);
-        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
-        InventoryInstances.waitContentLoading();
-        // Switch to Browse tab and select Call numbers (all)
-        InventorySearchAndFilter.switchToBrowseTab();
-        InventorySearchAndFilter.selectBrowseOption(BROWSE_CALL_NUMBER_OPTIONS.CALL_NUMBERS_ALL);
-        expectedRows.forEach((row) => {
-          BrowseCallNumber.waitForCallNumberToAppear(row);
+      after('Clean up', () => {
+        cy.getAdminToken();
+        Users.deleteViaApi(testData.user.userId);
+        InventoryInstances.deleteFullInstancesByTitleViaApi(instanceTitle);
+        CallNumberTypes.deleteLocalCallNumberTypeViaApi(localTypeId);
+        CallNumberBrowseSettings.assignCallNumberTypesViaApi({
+          name: BROWSE_CALL_NUMBER_OPTIONS.CALL_NUMBERS_ALL,
+          callNumberTypes: [],
         });
-        InventorySearchAndFilter.fillInBrowseSearch(expectedRows[0]);
-        InventorySearchAndFilter.clickSearch();
-        // Verify the order and bolding
-        BrowseCallNumber.valueInResultTableIsHighlighted(expectedRows[0]);
-        BrowseCallNumber.resultRowsIsInRequiredOder(expectedRows);
-      },
-    );
+      });
+
+      it(
+        'C477566 Similar call numbers with different types selected are sorted alphabetically when used "Call numbers (all)" browse option (case 2) (spitfire)',
+        { tags: ['criticalPath', 'spitfire', 'nonParallel', 'C477566'] },
+        () => {
+          const expectedRows = callNumberEndings.map((num) => `${callNumberBase} ${num}`);
+          cy.login(testData.user.username, testData.user.password);
+          TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
+          InventoryInstances.waitContentLoading();
+          // Switch to Browse tab and select Call numbers (all)
+          InventorySearchAndFilter.switchToBrowseTab();
+          InventorySearchAndFilter.selectBrowseOption(BROWSE_CALL_NUMBER_OPTIONS.CALL_NUMBERS_ALL);
+          expectedRows.forEach((row) => {
+            BrowseCallNumber.waitForCallNumberToAppear(row);
+          });
+          InventorySearchAndFilter.fillInBrowseSearch(expectedRows[0]);
+          InventorySearchAndFilter.clickSearch();
+          // Verify the order and bolding
+          BrowseCallNumber.valueInResultTableIsHighlighted(expectedRows[0]);
+          BrowseCallNumber.resultRowsIsInRequiredOder(expectedRows);
+        },
+      );
+    });
   });
 });
