9ac7385bdfa15b98e92ad04606526e2272973ca8
Lisavetti <lizaemets0244@gmail.com>
2025-07-10 17:33:40 +0300

    fixed C353592, С360116, С374188, С375110, С375111, C375283, C375959

--- cypress/support/fragments/finance/states.js
+++ cypress/support/fragments/finance/states.js
@@ -6,7 +6,7 @@ export default {
   transferCreatedSuccessfully: '(?:\\S+) was successfully transferred to the budget (?:\\S+)',
   negativeAmountConfirmation:
     'Completing this (?:transfer|allocation) will result in (?:\\S+) having a negative available amount. Are you sure you would like to complete this transaction?',
-  amountAllocatedSuccessfully: 'ui-finance.transaction.allocation.hasBeenCreated',
+  amountAllocatedSuccessfully: '(?:\\\\S+) was successfully allocated to the budget (?:\\\\S+)',
   rolloverTestStartedSuccessfully: '(?:\\S+) rollover test has started successfully',
   rolloverExportStartedSuccessfully: 'Export of budget results has been started',
 };
