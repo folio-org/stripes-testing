93c44e4af8e923e0935cf0d431b53d7cd624f1b3
Tetiana_Paranich <tetiana_paranich@epam.com>
2025-08-04 17:25:25 +0300

    added C519981

---
+++ cypress/e2e/inventory/instance/move-items-within-instance-action-for-instance-with-one-holding.cy.js
@@ -0,0 +1,95 @@
+import uuid from 'uuid';
+import { ITEM_STATUS_NAMES, LOCATION_NAMES } from '../../../support/constants';
+import Permissions from '../../../support/dictionary/permissions';
+import InstanceRecordView from '../../../support/fragments/inventory/instanceRecordView';
+import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+
+describe('Inventory', () => {
+  describe('Instance', () => {
+    const testData = {
+      permanentLocationUI: LOCATION_NAMES.ANNEX_UI,
+      itemBarcode: uuid(),
+    };
+
+    before('Create test data and login', () => {
+      cy.getAdminToken()
+        .then(() => {
+          cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
+            testData.instanceTypeId = instanceTypes[0].id;
+          });
+          cy.getHoldingTypes({ limit: 1 }).then((res) => {
+            testData.holdingTypeId = res[0].id;
+          });
+          cy.getLocations({ query: `name="${testData.permanentLocationUI}"` }).then((locations) => {
+            testData.locationsId = locations.id;
+          });
+          cy.getLoanTypes({ limit: 1 }).then((res) => {
+            testData.loanTypeId = res[0].id;
+          });
+          cy.getDefaultMaterialType().then((res) => {
+            testData.materialTypeId = res.id;
+          });
+        })
+        .then(() => {
+          InventoryInstances.createFolioInstanceViaApi({
+            instance: {
+              instanceTypeId: testData.instanceTypeId,
+              title: testData.instanceTitle,
+            },
+            holdings: [
+              {
+                holdingsTypeId: testData.holdingTypeId,
+                permanentLocationId: testData.locationsId,
+              },
+            ],
+            items: [
+              {
+                barcode: testData.itemBarcode,
+                status: { name: ITEM_STATUS_NAMES.AVAILABLE },
+                permanentLoanType: { id: testData.loanTypeId },
+                materialType: { id: testData.materialTypeId },
+              },
+            ],
+          }).then((specialInstanceIds) => {
+            testData.testInstanceIds = specialInstanceIds;
+          });
+        });
+
+      cy.createTempUser([Permissions.inventoryAll.gui, Permissions.uiInventoryMoveItems.gui]).then(
+        (userProperties) => {
+          testData.user = userProperties;
+
+          cy.login(testData.user.username, testData.user.password, {
+            path: TopMenu.inventoryPath,
+            waiter: InventoryInstances.waitContentLoading,
+          });
+        },
+      );
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(testData.user.userId);
+        InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(testData.itemBarcode);
+      });
+    });
+
+    it(
+      'C519981 Check "Move items within an Instance" action for Instance with one holding (folijet)',
+      { tags: ['extendedPath', 'folijet', 'C519981'] },
+      () => {
+        InventoryInstances.searchByTitle(testData.testInstanceIds.instanceId);
+        InventoryInstances.selectInstance();
+        InstanceRecordView.moveItemsWithinAnInstance();
+
+        const isEnubled = true;
+        InstanceRecordView.verifyMoveToButtonState(isEnubled);
+        InstanceRecordView.openHoldingsAccordion(testData.permanentLocationUI);
+        InstanceRecordView.verifyIsItemCreated(testData.itemBarcode);
+        InstanceRecordView.verifyMoveToButtonState(isEnubled);
+      },
+    );
+  });
+});
