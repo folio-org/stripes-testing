cde10314616cd1646ebc7eb6f644593481b11c10
Tetiana_Paranich <tetiana_paranich@epam.com>
2025-08-07 15:41:50 +0300

    fixed C422057

--- cypress/e2e/consortia/inventory/instance/sharing-of-imported-instance-via-isri.cy.js
+++ cypress/e2e/consortia/inventory/instance/sharing-of-imported-instance-via-isri.cy.js
@@ -7,83 +7,75 @@ import InventorySearchAndFilter from '../../../../support/fragments/inventory/in
 import ConsortiumManager from '../../../../support/fragments/settings/consortium-manager/consortium-manager';
 import Z3950TargetProfiles from '../../../../support/fragments/settings/inventory/integrations/z39.50TargetProfiles';
 import TopMenuNavigation from '../../../../support/fragments/topMenuNavigation';
-// import Users from '../../../../support/fragments/users/users';
+import Users from '../../../../support/fragments/users/users';
 
 describe('Inventory', () => {
   describe('Instance', () => {
     const testData = {
       oclcNumberForImport: '1234568',
       OCLCAuthentication: '100481406/PAOLF',
-      instanceTitle: '',
+      instanceTitle:
+        'Rincões dos frutos de ouro (tipos e cenarios do sul baiano) [por] Saboia Ribeiro.',
     };
 
     before('Create test data', () => {
       cy.getAdminToken();
-      cy.setTenant(Affiliations.College);
-      Z3950TargetProfiles.changeOclcWorldCatValueViaApi(testData.OCLCAuthentication);
-      cy.resetTenant();
-
-      cy.getAdminToken();
       cy.createTempUser([
         Permissions.inventoryAll.gui,
-        // Permissions.uiQuickMarcQuickMarcBibliographicEditorView.gui,
-        Permissions.settingsDataImportCanViewOnly.gui,
+        Permissions.consortiaInventoryShareLocalInstance.gui,
       ]).then((userProperties) => {
         testData.user = userProperties;
 
+        cy.assignAffiliationToUser(Affiliations.University, testData.user.userId);
+        cy.setTenant(Affiliations.University);
+        cy.assignPermissionsToExistingUser(testData.user.userId, [Permissions.inventoryAll.gui]);
+        cy.resetTenant();
+
         cy.assignAffiliationToUser(Affiliations.College, testData.user.userId);
         cy.setTenant(Affiliations.College);
+        Z3950TargetProfiles.changeOclcWorldCatValueViaApi(testData.OCLCAuthentication);
         cy.assignPermissionsToExistingUser(testData.user.userId, [
           Permissions.uiInventorySingleRecordImport.gui,
           Permissions.inventoryAll.gui,
-          // Permissions.uiQuickMarcQuickMarcBibliographicEditorView.gui,
-          // Permissions.consortiaInventoryShareLocalInstance.gui,
-          Permissions.settingsDataImportCanViewOnly.gui,
+          Permissions.consortiaInventoryShareLocalInstance.gui,
+          Permissions.settingsDataImportEnabled.gui,
         ]);
-        cy.resetTenant();
 
-        cy.assignAffiliationToUser(Affiliations.University, testData.user.userId);
-        cy.setTenant(Affiliations.University);
-        cy.assignPermissionsToExistingUser(testData.user.userId, [
-          Permissions.inventoryAll.gui,
-          // Permissions.uiQuickMarcQuickMarcBibliographicEditorView.gui,
-        ]);
-        cy.resetTenant();
-
-        // cy.login(testData.user.username, testData.user.password);
-        cy.loginAsAdmin();
+        cy.login(testData.user.username, testData.user.password);
         ConsortiumManager.switchActiveAffiliation(tenantNames.central, tenantNames.college);
-        // TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
-        TopMenuNavigation.openAppFromDropdown(APPLICATION_NAMES.INVENTORY);
-        InventoryInstances.waitContentLoading();
+        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
       });
     });
 
-    // after('Delete test data', () => {
-    //   cy.resetTenant();
-    //   cy.getAdminToken();
-    //   Users.deleteViaApi(testData.user.userId);
-    //   cy.withinTenant(Affiliations.College, () => {
-    //     cy.getInstance({ limit: 1, expandAll: true, query: `"hrid"=="${testData.instanceHRID}"` })
-    //       .then(
-    //         (instance) => {
-    //           InventoryInstance.deleteInstanceViaApi(instance.id);
-    //         },
-    //       );
-    //   });
-    // });
+    after('Delete test data', () => {
+      cy.resetTenant();
+      cy.getAdminToken();
+      Users.deleteViaApi(testData.user.userId);
+      cy.withinTenant(Affiliations.College, () => {
+        cy.getInstance({
+          limit: 1,
+          expandAll: true,
+          query: `"hrid"=="${testData.instanceHRID}"`,
+        }).then((instance) => {
+          InventoryInstance.deleteInstanceViaApi(instance.id);
+        });
+      });
+    });
 
     it(
       'C422057 (CONSORTIA) Verify the sharing of imported instance via ISRI (consortia) (folijet)',
       { tags: ['extendedPathECS', 'folijet', 'C422057'] },
       () => {
-        cy.pause();
+        InventoryInstances.waitContentLoading();
         InventoryInstances.importWithOclc(testData.oclcNumberForImport);
         InventoryInstance.waitLoading();
         InventoryInstance.verifyInstanceTitle(testData.instanceTitle);
         InventoryInstance.checkExpectedMARCSource();
         InventoryInstance.shareInstance();
-        cy.reload();
+        cy.waitForAuthRefresh(() => {
+          cy.reload();
+          InventoryInstance.waitLoading();
+        }, 20_000);
         InventoryInstance.verifyInstanceTitle(testData.instanceTitle);
         InventoryInstance.checkExpectedMARCSource();
         InventoryInstance.getAssignedHRID().then((initialInstanceHrId) => {
