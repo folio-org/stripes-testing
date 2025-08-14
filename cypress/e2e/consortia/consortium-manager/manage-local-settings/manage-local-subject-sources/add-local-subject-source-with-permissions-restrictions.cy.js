940fff4106f8e5e6580c9cf93d48d778348253a6
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-08-14 18:31:22 +0500

    FAT-21479-pt5: tests added (#5481)

---
+++ cypress/e2e/consortia/consortium-manager/manage-local-settings/manage-local-subject-sources/add-local-subject-source-with-permissions-restrictions.cy.js
@@ -0,0 +1,275 @@
+import moment from 'moment';
+import { APPLICATION_NAMES } from '../../../../../support/constants';
+import Affiliations, { tenantNames } from '../../../../../support/dictionary/affiliations';
+import ConsortiaControlledVocabularyPaneset, {
+  actionIcons,
+} from '../../../../../support/fragments/consortium-manager/consortiaControlledVocabularyPaneset';
+import ConsortiumManagerApp, {
+  settingsItems,
+  messages,
+} from '../../../../../support/fragments/consortium-manager/consortiumManagerApp';
+import SubjectSourcesConsortiumManager from '../../../../../support/fragments/consortium-manager/inventory/instances/subjectSourcesConsortiumManager';
+import ConfirmCreate from '../../../../../support/fragments/consortium-manager/modal/confirm-create';
+import SelectMembers from '../../../../../support/fragments/consortium-manager/modal/select-members';
+import TopMenuNavigation from '../../../../../support/fragments/topMenuNavigation';
+import Users from '../../../../../support/fragments/users/users';
+import getRandomPostfix from '../../../../../support/utils/stringTools';
+import ConsortiumManager from '../../../../../support/fragments/settings/consortium-manager/consortium-manager';
+import SubjectSources from '../../../../../support/fragments/settings/inventory/instances/subjectSources';
+import SettingsInventory, {
+  INVENTORY_SETTINGS_TABS,
+} from '../../../../../support/fragments/settings/inventory/settingsInventory';
+// import InteractorsTools from '../../../../../support/utils/interactorsTools';
+import Permissions from '../../../../../support/dictionary/permissions';
+import SettingsPane from '../../../../../support/fragments/settings/settingsPane';
+
+describe('Consortia', () => {
+  describe('Consortium manager', () => {
+    describe('Manage local settings', () => {
+      describe('Manage local Subject sources', () => {
+        let userA;
+        let userB;
+        const testData = {
+          sourceName: `AT_C594437_SubjectSource_${getRandomPostfix()}`,
+        };
+
+        const CONSTANTS = {
+          LOCAL_SOURCE: 'local',
+          SETTINGS_OPTION: 'subject source',
+        };
+
+        before('Create test data', () => {
+          cy.resetTenant();
+          cy.getAdminToken();
+
+          // Create User A in Central tenant
+          cy.createTempUser([
+            Permissions.consortiaSettingsConsortiumManagerEdit.gui,
+            Permissions.consortiaSettingsConsortiumManagerView.gui,
+            Permissions.uiSettingsSubjectSourceCreateEditDelete.gui,
+          ]).then((userProperties) => {
+            userA = userProperties;
+
+            // Assign User A affiliation to College (member-1) with Organizations view permission
+            cy.assignAffiliationToUser(Affiliations.College, userA.userId);
+            cy.setTenant(Affiliations.College);
+            cy.assignPermissionsToExistingUser(userA.userId, [Permissions.uiOrganizationsView.gui]);
+
+            // Assign User A affiliation to University (member-2) with Subject sources permission
+            cy.resetTenant();
+            cy.assignAffiliationToUser(Affiliations.University, userA.userId);
+            cy.setTenant(Affiliations.University);
+            cy.assignPermissionsToExistingUser(userA.userId, [
+              Permissions.uiSettingsSubjectSourceCreateEditDelete.gui,
+            ]);
+
+            // Create User B in Central tenant
+            cy.resetTenant();
+            cy.createTempUser([Permissions.uiSettingsSubjectSourceCreateEditDelete.gui]).then(
+              (userBProperties) => {
+                userB = userBProperties;
+
+                // Assign User B affiliations
+                cy.assignAffiliationToUser(Affiliations.College, userB.userId);
+                cy.setTenant(Affiliations.College);
+                cy.assignPermissionsToExistingUser(userB.userId, [
+                  Permissions.uiSettingsSubjectSourceCreateEditDelete.gui,
+                ]);
+
+                cy.resetTenant();
+                cy.assignAffiliationToUser(Affiliations.University, userB.userId);
+                cy.setTenant(Affiliations.University);
+                cy.assignPermissionsToExistingUser(userB.userId, [
+                  Permissions.uiSettingsSubjectSourceCreateEditDelete.gui,
+                ]);
+
+                cy.resetTenant();
+                cy.login(userA.username, userA.password);
+              },
+            );
+          });
+        });
+
+        after('Delete test data', () => {
+          cy.resetTenant();
+          cy.getAdminToken();
+          Users.deleteViaApi(userA.userId);
+          Users.deleteViaApi(userB.userId);
+          // Clean up subject sources from all tenants
+          [Affiliations.Consortia, Affiliations.College, Affiliations.University].forEach(
+            (tenant) => {
+              cy.setTenant(tenant);
+              SubjectSources.getSubjectSourcesViaApi({
+                query: `name="${testData.sourceName}"`,
+              }).then((subjectSources) => {
+                subjectSources.forEach((subjectSource) => {
+                  SubjectSources.deleteViaApi(subjectSource.id);
+                });
+              });
+              SubjectSources.getSubjectSourcesViaApi({
+                query: `name="${testData.editedSourceName}"`,
+              }).then((subjectSources) => {
+                subjectSources.forEach((subjectSource) => {
+                  SubjectSources.deleteViaApi(subjectSource.id);
+                });
+              });
+            },
+          );
+        });
+
+        it(
+          'C594437 Add local subject source via "Consortium manager" app with permissions and affiliations restrictions (consortia) (folijet)',
+          { tags: ['extendedPathECS', 'folijet', 'C594437'] },
+          () => {
+            // Step 1: Navigate to Consortium manager app and select all members
+            TopMenuNavigation.navigateToApp(APPLICATION_NAMES.CONSORTIUM_MANAGER);
+            ConsortiumManagerApp.verifyStatusOfConsortiumManager();
+            SelectMembers.selectAllMembers();
+            ConsortiumManagerApp.verifyStatusOfConsortiumManager(3);
+
+            // Step 2: Navigate to Inventory settings
+            ConsortiumManagerApp.chooseSettingsItem(settingsItems.inventory);
+
+            // Step 3: Navigate to Subject sources and verify permission error
+            SubjectSourcesConsortiumManager.choose();
+            ConsortiaControlledVocabularyPaneset.verifyNewButtonShown();
+
+            // Verify permission error toast message appears for member-1
+            // TO DO: uncomment after EUREKA-536 is done
+            // InteractorsTools.checkCalloutExists(messages.noPermission(tenantNames.college));
+
+            // Step 4: Create new subject source with permissions issue
+            ConsortiaControlledVocabularyPaneset.clickNew();
+            ConsortiaControlledVocabularyPaneset.verifyEditModeIsActive();
+            ConsortiaControlledVocabularyPaneset.verifyNewButtonDisabled();
+            ConsortiumManagerApp.verifySelectMembersButton(false);
+            // Verify Share checkbox is disabled due to permission restrictions
+            ConsortiaControlledVocabularyPaneset.verifyShareCheckboxState({
+              isEnabled: false,
+              isChecked: false,
+            });
+
+            // Step 5: Fill in name field
+            ConsortiaControlledVocabularyPaneset.fillInTextField({ name: testData.sourceName });
+
+            // Step 6: Try to save and verify error
+            // TO DO: uncomment after EUREKA-536 is done
+            // ConsortiaControlledVocabularyPaneset.clickSave();
+            // InteractorsTools.checkCalloutExists(messages.noPermission(tenantNames.college));
+            // ConsortiaControlledVocabularyPaneset.verifyFieldValidatorError({
+            //   name: 'Error on saving data',
+            // });
+            // ConsortiaControlledVocabularyPaneset.verifyEditModeIsActive();
+
+            // Step 7: Cancel creation
+            ConsortiaControlledVocabularyPaneset.clickCancel();
+            ConsortiaControlledVocabularyPaneset.verifyRecordIsNotInTheList(testData.sourceName);
+            ConsortiaControlledVocabularyPaneset.verifyNewButtonDisabled(false);
+            ConsortiumManagerApp.verifySelectMembersButton();
+
+            // Step 8: Open Select members modal
+            ConsortiumManagerApp.clickSelectMembers();
+            SelectMembers.verifyStatusOfSelectMembersModal(3, 3, true);
+
+            // Step 9: Unselect member-1 tenant (College)
+            SelectMembers.checkMember(tenantNames.college, false);
+            SelectMembers.verifyStatusOfSelectMembersModal(3, 2, false);
+
+            // Step 10: Save member selection
+            SelectMembers.saveAndClose();
+            ConsortiumManagerApp.verifyStatusOfConsortiumManager(2);
+
+            // Step 11: Create new subject source successfully
+            ConsortiaControlledVocabularyPaneset.clickNew();
+            ConsortiaControlledVocabularyPaneset.verifyEditModeIsActive();
+            ConsortiaControlledVocabularyPaneset.fillInTextField({ name: testData.sourceName });
+            ConsortiaControlledVocabularyPaneset.clickSave();
+
+            // Verify confirmation modal shows only Central and University
+            ConfirmCreate.waitLoadingConfirmCreate(testData.sourceName);
+
+            // Step 12: Click "Keep editing"
+            ConfirmCreate.clickKeepEditing();
+            ConsortiaControlledVocabularyPaneset.verifyEditModeIsActive();
+
+            // Step 13: Save again
+            ConsortiaControlledVocabularyPaneset.clickSave();
+            ConfirmCreate.waitLoadingConfirmCreate(testData.sourceName);
+
+            // Step 14: Confirm creation
+            ConfirmCreate.clickConfirm();
+            ConsortiumManagerApp.checkMessage(
+              messages.created(
+                testData.sourceName,
+                `${[tenantNames.central, tenantNames.university].sort().join(', ')}`,
+              ),
+            );
+
+            // Verify two rows created (Central and University)
+            ConsortiaControlledVocabularyPaneset.verifyRecordIsInTheList(
+              testData.sourceName,
+              tenantNames.central,
+              [
+                testData.sourceName,
+                '',
+                CONSTANTS.LOCAL_SOURCE,
+                `${moment().format('l')} by`,
+                tenantNames.central,
+              ],
+              [actionIcons.edit, actionIcons.trash],
+            );
+            ConsortiaControlledVocabularyPaneset.verifyRecordIsInTheList(
+              testData.sourceName,
+              tenantNames.university,
+              [
+                testData.sourceName,
+                '',
+                CONSTANTS.LOCAL_SOURCE,
+                `${moment().format('l')} by`,
+                tenantNames.university,
+              ],
+              [actionIcons.edit, actionIcons.trash],
+            );
+
+            // Step 15: Login as User B
+            cy.login(userB.username, userB.password);
+
+            // Step 16: Verify subject source in Central tenant Settings
+            TopMenuNavigation.navigateToApp(
+              APPLICATION_NAMES.SETTINGS,
+              APPLICATION_NAMES.INVENTORY,
+            );
+            SettingsPane.waitLoading();
+            SettingsInventory.selectSettingsTab(INVENTORY_SETTINGS_TABS.SUBJECT_SOURCES);
+            SubjectSources.waitLoading();
+            SubjectSources.verifySubjectSourceExists(
+              testData.sourceName,
+              CONSTANTS.LOCAL_SOURCE,
+              userA.lastName,
+              { actions: [actionIcons.edit, actionIcons.trash] },
+            );
+
+            // Step 17: Switch to College tenant and verify NOT present
+            ConsortiumManager.switchActiveAffiliation(tenantNames.central, tenantNames.college);
+            SettingsPane.waitLoading();
+            SettingsInventory.selectSettingsTab(INVENTORY_SETTINGS_TABS.SUBJECT_SOURCES);
+            SubjectSources.waitLoading();
+            SubjectSources.verifySubjectSourceAbsent(testData.sourceName);
+
+            // Step 18: Switch to University tenant and verify present
+            ConsortiumManager.switchActiveAffiliation(tenantNames.college, tenantNames.university);
+            SettingsPane.waitLoading();
+            SettingsInventory.selectSettingsTab(INVENTORY_SETTINGS_TABS.SUBJECT_SOURCES);
+            SubjectSources.waitLoading();
+            SubjectSources.verifySubjectSourceExists(
+              testData.sourceName,
+              CONSTANTS.LOCAL_SOURCE,
+              userA.lastName,
+              { actions: [actionIcons.edit, actionIcons.trash] },
+            );
+          },
+        );
+      });
+    });
+  });
+});
