f9a96aa81d2669e23fda5e75ec5251195cee60c0
Sviatlana Stsiapanava <sviatlana_stsiapanava@epam.com>
2025-08-15 19:01:36 +0300

    Move test to folder according TestRail structure

---
+++ cypress/e2e/consortia/bulk-edit/in-app/central-tenant/bulk-edit-in-app-verify-preview-of-record-matched-valid-instance-identifier.cy.js
@@ -0,0 +1,205 @@
+import permissions from '../../../../../support/dictionary/permissions';
+import BulkEditSearchPane from '../../../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import BulkEditActions from '../../../../../support/fragments/bulk-edit/bulk-edit-actions';
+import InventoryInstances from '../../../../../support/fragments/inventory/inventoryInstances';
+import TopMenu from '../../../../../support/fragments/topMenu';
+import Users from '../../../../../support/fragments/users/users';
+import FileManager from '../../../../../support/utils/fileManager';
+import getRandomPostfix from '../../../../../support/utils/stringTools';
+import ConsortiumManager from '../../../../../support/fragments/settings/consortium-manager/consortium-manager';
+import { tenantNames } from '../../../../../support/dictionary/affiliations';
+import { BULK_EDIT_TABLE_COLUMN_HEADERS } from '../../../../../support/constants';
+import BulkEditFiles from '../../../../../support/fragments/bulk-edit/bulk-edit-files';
+import ExportFile from '../../../../../support/fragments/data-export/exportFile';
+
+let user;
+let instanceTypeId;
+const firstInstance = {
+  title: `C476785 first instance-${getRandomPostfix()}`,
+};
+const secondInstance = {
+  title: `C476785 second instance-${getRandomPostfix()}`,
+};
+const recordIdentifier = 'Instance HRIDs';
+const errorText = 'Duplicate entry';
+const firstInstanceUUIDsFileName = `instanceUUIDs-${getRandomPostfix()}.csv`;
+const secondInstanceHRIDsFileName = `instanceHRIDs-${getRandomPostfix()}.csv`;
+const firstMatchedRecordsFileName = BulkEditFiles.getMatchedRecordsFileName(
+  firstInstanceUUIDsFileName,
+);
+const secondMatchedRecordsFileName = BulkEditFiles.getMatchedRecordsFileName(
+  secondInstanceHRIDsFileName,
+);
+const errorsFromMatchingFileName = BulkEditFiles.getErrorsFromMatchingFileName(
+  secondInstanceHRIDsFileName,
+);
+
+describe('Bulk-edit', () => {
+  describe('In-app approach', () => {
+    describe('Consortia', () => {
+      before('create test data', () => {
+        cy.getAdminToken();
+        cy.createTempUser([
+          permissions.bulkEditView.gui,
+          permissions.uiInventoryViewInstances.gui,
+        ]).then((userProperties) => {
+          user = userProperties;
+
+          cy.getInstanceTypes({ limit: 1 })
+            .then((instanceTypes) => {
+              instanceTypeId = instanceTypes[0].id;
+            })
+            .then(() => {
+              InventoryInstances.createFolioInstanceViaApi({
+                instance: {
+                  instanceTypeId,
+                  title: firstInstance.title,
+                },
+              });
+            })
+            .then((instanceData) => {
+              firstInstance.uuid = instanceData.instanceId;
+            })
+            .then(() => {
+              cy.getInstanceById(firstInstance.uuid).then((res) => {
+                firstInstance.hrid = res.hrid;
+              });
+            })
+            .then(() => {
+              InventoryInstances.createFolioInstanceViaApi({
+                instance: {
+                  instanceTypeId,
+                  title: secondInstance.title,
+                },
+              });
+            })
+            .then((instanceData) => {
+              secondInstance.uuid = instanceData.instanceId;
+            })
+            .then(() => {
+              cy.getInstanceById(secondInstance.uuid).then((res) => {
+                secondInstance.hrid = res.hrid;
+              });
+            })
+            .then(() => {
+              FileManager.createFile(
+                `cypress/fixtures/${firstInstanceUUIDsFileName}`,
+                firstInstance.uuid,
+              );
+              FileManager.createFile(
+                `cypress/fixtures/${secondInstanceHRIDsFileName}`,
+                `${secondInstance.hrid}\r\n${secondInstance.hrid}`,
+              );
+            });
+
+          cy.resetTenant();
+          cy.login(user.username, user.password, {
+            path: TopMenu.bulkEditPath,
+            waiter: BulkEditSearchPane.waitLoading,
+          });
+          ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
+        });
+      });
+
+      after('delete test data', () => {
+        cy.getAdminToken();
+        InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(firstInstance.uuid);
+        InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(secondInstance.uuid);
+        Users.deleteViaApi(user.userId);
+        FileManager.deleteFile(`cypress/fixtures/${firstInstanceUUIDsFileName}`);
+        FileManager.deleteFile(`cypress/fixtures/${secondInstanceHRIDsFileName}`);
+        FileManager.deleteFileFromDownloadsByMask(
+          firstMatchedRecordsFileName,
+          secondMatchedRecordsFileName,
+          errorsFromMatchingFileName,
+        );
+      });
+
+      it(
+        'C476785 Identifier - Verify "Preview of record matched" when uploading valid Instance identifiers in Central tenant (consortia) (firebird)',
+        { tags: ['smokeECS', 'firebird', 'C476785'] },
+        () => {
+          BulkEditSearchPane.verifyDragNDropRecordTypeIdentifierArea('Instance', 'Instance UUIDs');
+          BulkEditSearchPane.uploadFile(firstInstanceUUIDsFileName);
+          BulkEditSearchPane.waitFileUploading();
+          BulkEditSearchPane.verifyPaneTitleFileName(firstInstanceUUIDsFileName);
+          BulkEditSearchPane.verifyPaneRecordsCount('1 instance');
+          BulkEditSearchPane.verifyFileNameHeadLine(firstInstanceUUIDsFileName);
+          BulkEditSearchPane.verifyExactChangesUnderColumnsByIdentifierInResultsAccordion(
+            firstInstance.hrid,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_HRID,
+            firstInstance.hrid,
+          );
+          BulkEditSearchPane.verifyExactChangesUnderColumnsByIdentifierInResultsAccordion(
+            firstInstance.hrid,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.RESOURCE_TITLE,
+            firstInstance.title,
+          );
+          BulkEditSearchPane.verifyPreviousPaginationButtonDisabled();
+          BulkEditSearchPane.verifyNextPaginationButtonDisabled();
+          BulkEditSearchPane.verifyActionsAfterConductedCSVUploading(false);
+          BulkEditSearchPane.verifySearchColumnNameTextFieldExists();
+          BulkEditSearchPane.verifyCheckedCheckboxesPresentInTheTable();
+          BulkEditActions.openActions();
+          BulkEditActions.downloadMatchedResults();
+          BulkEditFiles.verifyValueInRowByUUID(
+            firstMatchedRecordsFileName,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_UUID,
+            firstInstance.uuid,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.RESOURCE_TITLE,
+            firstInstance.title,
+          );
+          BulkEditFiles.verifyValueInRowByUUID(
+            firstMatchedRecordsFileName,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_UUID,
+            firstInstance.uuid,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_HRID,
+            firstInstance.hrid,
+          );
+
+          BulkEditSearchPane.selectRecordIdentifier(recordIdentifier);
+          BulkEditSearchPane.verifyAfterChoosingIdentifier(recordIdentifier);
+          cy.wait(2000);
+          BulkEditSearchPane.uploadFile(secondInstanceHRIDsFileName);
+          BulkEditSearchPane.waitFileUploading();
+          BulkEditSearchPane.verifyPaneTitleFileName(secondInstanceHRIDsFileName);
+          BulkEditSearchPane.verifyPaneRecordsCount('1 instance');
+          BulkEditSearchPane.verifyFileNameHeadLine(secondInstanceHRIDsFileName);
+          BulkEditSearchPane.verifyExactChangesUnderColumnsByIdentifierInResultsAccordion(
+            secondInstance.hrid,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_HRID,
+            secondInstance.hrid,
+          );
+          BulkEditSearchPane.verifyExactChangesUnderColumnsByIdentifierInResultsAccordion(
+            secondInstance.hrid,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.RESOURCE_TITLE,
+            secondInstance.title,
+          );
+          BulkEditSearchPane.verifyPaginatorInErrorsAccordion(1);
+          BulkEditSearchPane.verifyErrorLabel(0, 1);
+          BulkEditSearchPane.verifyErrorByIdentifier(secondInstance.hrid, errorText, 'Warning');
+          BulkEditSearchPane.verifyShowWarningsCheckbox(true, true);
+          BulkEditActions.downloadMatchedResults();
+          BulkEditFiles.verifyValueInRowByUUID(
+            secondMatchedRecordsFileName,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_UUID,
+            secondInstance.uuid,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.RESOURCE_TITLE,
+            secondInstance.title,
+          );
+          BulkEditFiles.verifyValueInRowByUUID(
+            secondMatchedRecordsFileName,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_UUID,
+            secondInstance.uuid,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_INSTANCES.INSTANCE_HRID,
+            secondInstance.hrid,
+          );
+          BulkEditActions.downloadErrors();
+          ExportFile.verifyFileIncludes(errorsFromMatchingFileName, [
+            `WARNING,${secondInstance.hrid},${errorText}`,
+          ]);
+        },
+      );
+    });
+  });
+});
---
+++ cypress/e2e/consortia/bulk-edit/in-app/member-tenant/bulk-edit-in-app-holdings-electronic-access-clear-field-member-tenant.cy.js
@@ -0,0 +1,337 @@
+import permissions from '../../../../../support/dictionary/permissions';
+import BulkEditActions from '../../../../../support/fragments/bulk-edit/bulk-edit-actions';
+import BulkEditSearchPane from '../../../../../support/fragments/bulk-edit/bulk-edit-search-pane';
+import BulkEditFiles from '../../../../../support/fragments/bulk-edit/bulk-edit-files';
+import InventoryInstances from '../../../../../support/fragments/inventory/inventoryInstances';
+import TopMenu from '../../../../../support/fragments/topMenu';
+import Users from '../../../../../support/fragments/users/users';
+import FileManager from '../../../../../support/utils/fileManager';
+import getRandomPostfix from '../../../../../support/utils/stringTools';
+import InventorySearchAndFilter from '../../../../../support/fragments/inventory/inventorySearchAndFilter';
+import InventoryHoldings from '../../../../../support/fragments/inventory/holdings/inventoryHoldings';
+import HoldingsRecordView from '../../../../../support/fragments/inventory/holdingsRecordView';
+import UrlRelationship from '../../../../../support/fragments/settings/inventory/instance-holdings-item/urlRelationship';
+import Affiliations from '../../../../../support/dictionary/affiliations';
+import {
+  APPLICATION_NAMES,
+  BULK_EDIT_TABLE_COLUMN_HEADERS,
+} from '../../../../../support/constants';
+import TopMenuNavigation from '../../../../../support/fragments/topMenuNavigation';
+import UrlRelationshipConsortiumManager from '../../../../../support/fragments/consortium-manager/inventory/instances-holdings-items/urlRelationshipConsortiumManager';
+
+let user;
+let instanceTypeId;
+let locationId;
+let sourceId;
+let sharedUrlRelationshipData;
+const holdingIds = [];
+const holdingHrids = [];
+const folioInstance = {
+  title: `C566156 folio instance testBulkEdit_${getRandomPostfix()}`,
+};
+const marcInstance = {
+  title: `C566156 marc instance testBulkEdit_${getRandomPostfix()}`,
+};
+const sharedUrlRelationship = {
+  payload: {
+    name: `C566156 shared urlRelationship ${getRandomPostfix()}`,
+  },
+};
+const localUrlRelationship = {
+  name: `C566156 local urlRelationship ${getRandomPostfix()}`,
+};
+const instances = [folioInstance, marcInstance];
+const electronicAccessTableHeaders =
+  'URL relationshipURILink textMaterials specifiedURL public note';
+const electronicAccessTableHeadersInFile =
+  'URL relationship;URI;Link text;Materials specified;URL public note\n';
+const holdingUUIDsFileName = `holdingUUIdsFileName_${getRandomPostfix()}.csv`;
+const fileNames = BulkEditFiles.getAllDownloadedFileNames(holdingUUIDsFileName, true);
+const getRowsInCsvFileMatchingHrids = (csvFileData, hrids) => {
+  return csvFileData.filter((row) => {
+    return hrids.some((hrid) => {
+      return row[BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.HOLDINGS_HRID].includes(hrid);
+    });
+  });
+};
+
+describe('Bulk-edit', () => {
+  describe('In-app approach', () => {
+    describe('Consortia', () => {
+      before('create test data', () => {
+        cy.clearLocalStorage();
+        cy.getAdminToken();
+        // create shared url relationship in Central
+        UrlRelationshipConsortiumManager.createViaApi(sharedUrlRelationship)
+          .then((newUrlRelationship) => {
+            sharedUrlRelationshipData = newUrlRelationship;
+          })
+          .then((newUrlRelationship) => {
+            sharedUrlRelationshipData = newUrlRelationship;
+
+            cy.withinTenant(Affiliations.College, () => {
+              cy.createTempUser([
+                permissions.bulkEditEdit.gui,
+                permissions.uiInventoryViewCreateEditHoldings.gui,
+              ]).then((userProperties) => {
+                user = userProperties;
+
+                cy.getInstanceTypes({ limit: 1 }).then((instanceTypeData) => {
+                  instanceTypeId = instanceTypeData[0].id;
+                });
+                cy.getLocations({ query: 'name="DCB"' }).then((res) => {
+                  locationId = res.id;
+                });
+                InventoryHoldings.getHoldingsFolioSource().then((folioSource) => {
+                  sourceId = folioSource.id;
+                });
+
+                // create local url relationship in College
+                UrlRelationship.createViaApi({
+                  name: localUrlRelationship.name,
+                  source: 'local',
+                }).then((response) => {
+                  localUrlRelationship.id = response.id;
+                  // create holdings in College tenant
+                  instances.forEach((instance) => {
+                    InventoryInstances.createFolioInstanceViaApi({
+                      instance: {
+                        instanceTypeId,
+                        title: instance.title,
+                      },
+                    }).then((createdInstanceData) => {
+                      instance.id = createdInstanceData.instanceId;
+                      InventoryHoldings.createHoldingRecordViaApi({
+                        instanceId: instance.id,
+                        permanentLocationId: locationId,
+                        electronicAccess: [
+                          {
+                            linkText: 'College shared link text',
+                            materialsSpecification: 'College shared materials specified',
+                            publicNote: 'College shared url public note',
+                            uri: 'https://college-shared-uri.com',
+                            relationshipId: sharedUrlRelationshipData.settingId,
+                          },
+                          {
+                            linkText: 'College link text',
+                            materialsSpecification: 'College materials specified',
+                            publicNote: 'College url public note',
+                            uri: 'https://college-uri.com',
+                            relationshipId: localUrlRelationship.id,
+                          },
+                        ],
+                        sourceId,
+                      }).then((holding) => {
+                        holdingIds.push(holding.id);
+                        holdingHrids.push(holding.hrid);
+                      });
+                    });
+                    cy.wait(1000);
+                  });
+
+                  cy.then(() => {
+                    FileManager.createFile(
+                      `cypress/fixtures/${holdingUUIDsFileName}`,
+                      `${holdingIds.join('\n')}`,
+                    );
+                  });
+                });
+
+                cy.resetTenant();
+                cy.login(user.username, user.password, {
+                  path: TopMenu.bulkEditPath,
+                  waiter: BulkEditSearchPane.waitLoading,
+                });
+              });
+            });
+          });
+      });
+
+      after('delete test data', () => {
+        cy.resetTenant();
+        cy.getAdminToken();
+        UrlRelationshipConsortiumManager.deleteViaApi(sharedUrlRelationshipData);
+        cy.withinTenant(Affiliations.College, () => {
+          UrlRelationship.deleteViaApi(localUrlRelationship.id);
+          Users.deleteViaApi(user.userId);
+
+          instances.forEach((instance) => {
+            InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(instance.id);
+          });
+
+          FileManager.deleteFile(`cypress/fixtures/${holdingUUIDsFileName}`);
+          BulkEditFiles.deleteAllDownloadedFiles(fileNames);
+        });
+      });
+
+      it(
+        'C566156 Verify "Clear field" action for Holdings electronic access in Member tenant (consortia) (firebird)',
+        { tags: ['criticalPathECS', 'firebird', 'C566156'] },
+        () => {
+          // Step 1: Select record type and identifier
+          BulkEditSearchPane.verifyDragNDropRecordTypeIdentifierArea('Holdings', 'Holdings UUIDs');
+
+          // Step 2: Upload .csv file with Holdings UUIDs
+          BulkEditSearchPane.uploadFile(holdingUUIDsFileName);
+
+          // Step 3: Check the result of uploading the .csv file
+          BulkEditSearchPane.verifyPaneTitleFileName(holdingUUIDsFileName);
+          BulkEditSearchPane.verifyPaneRecordsCount(`${holdingIds.length} holdings`);
+          BulkEditSearchPane.verifyFileNameHeadLine(holdingUUIDsFileName);
+
+          holdingHrids.forEach((holdingHrid) => {
+            BulkEditSearchPane.verifyExactChangesUnderColumnsByIdentifierInResultsAccordion(
+              holdingHrid,
+              BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.HOLDINGS_HRID,
+              holdingHrid,
+            );
+          });
+
+          BulkEditSearchPane.verifyPreviousPaginationButtonDisabled();
+          BulkEditSearchPane.verifyNextPaginationButtonDisabled();
+
+          // Step 4: Show "Electronic access" column
+          BulkEditActions.openActions();
+          BulkEditSearchPane.changeShowColumnCheckbox(
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS,
+          );
+          BulkEditSearchPane.verifyCheckboxesInActionsDropdownMenuChecked(
+            true,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS,
+          );
+
+          const holdingsElectronicAccess = `${electronicAccessTableHeaders}${sharedUrlRelationship.payload.name}https://college-shared-uri.comCollege shared link textCollege shared materials specifiedCollege shared url public note${localUrlRelationship.name}https://college-uri.comCollege link textCollege materials specifiedCollege url public note`;
+
+          holdingHrids.forEach((holdingHrid) => {
+            BulkEditSearchPane.verifyExactChangesUnderColumnsByIdentifierInResultsAccordion(
+              holdingHrid,
+              BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS,
+              holdingsElectronicAccess,
+            );
+          });
+
+          // Step 5: Hide "Electronic access" column and download matched records
+          BulkEditSearchPane.changeShowColumnCheckbox(
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS,
+          );
+          BulkEditSearchPane.verifyCheckboxesInActionsDropdownMenuChecked(
+            false,
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS,
+          );
+          BulkEditSearchPane.verifyResultColumnTitlesDoNotIncludeTitles(
+            BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS,
+          );
+          BulkEditActions.openActions();
+          BulkEditActions.downloadMatchedResults();
+
+          const holdingsElectronicAccessInFile = `${electronicAccessTableHeadersInFile}${sharedUrlRelationship.payload.name};https://college-shared-uri.com;College shared link text;College shared materials specified;College shared url public note|${localUrlRelationship.name};https://college-uri.com;College link text;College materials specified;College url public note`;
+
+          FileManager.convertCsvToJson(fileNames.matchedRecordsCSV).then((csvFileData) => {
+            const holdingRows = getRowsInCsvFileMatchingHrids(csvFileData, holdingHrids);
+            holdingRows.forEach((holdingRow) => {
+              cy.expect(
+                holdingRow[BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS],
+              ).to.equal(holdingsElectronicAccessInFile);
+            });
+          });
+
+          // Step 6: Start in-app bulk edit
+          BulkEditActions.openInAppStartBulkEditFrom();
+          BulkEditActions.verifyBulkEditsAccordionExists();
+          BulkEditActions.verifyOptionsDropdown();
+          BulkEditActions.verifyRowIcons();
+          BulkEditActions.verifyCancelButtonDisabled(false);
+          BulkEditActions.verifyConfirmButtonDisabled(true);
+
+          // Steps 7-13: Add clear field actions for all electronic access fields
+          const fieldsToClear = [
+            'URL relationship',
+            'URI',
+            'Link text',
+            'Materials specified',
+            'URL public note',
+          ];
+
+          fieldsToClear.forEach((field, rowIndex) => {
+            // Step 7-13: Select option and clear field for each
+            BulkEditActions.selectOption(field, rowIndex);
+            BulkEditActions.selectAction('Clear field', rowIndex);
+            BulkEditActions.verifyConfirmButtonDisabled(false);
+            if (rowIndex < fieldsToClear.length - 1) {
+              BulkEditActions.addNewBulkEditFilterString();
+              BulkEditActions.verifyNewBulkEditRow(rowIndex + 1);
+              BulkEditActions.verifyConfirmButtonDisabled(true);
+            }
+          });
+
+          // Step 14: Confirm changes and verify preview
+          BulkEditActions.confirmChanges();
+          BulkEditActions.verifyMessageBannerInAreYouSureForm(holdingIds.length);
+
+          const updatedHoldingsElectronicAccess = electronicAccessTableHeaders;
+          const updatedHoldingsElectronicAccessInFile = `${electronicAccessTableHeadersInFile};;;;|;;;;`;
+
+          holdingHrids.forEach((holdingHrid) => {
+            BulkEditSearchPane.verifyExactChangesUnderColumnsByIdentifier(
+              holdingHrid,
+              BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS,
+              updatedHoldingsElectronicAccess,
+            );
+          });
+
+          BulkEditActions.verifyAreYouSureForm(holdingIds.length);
+          BulkEditSearchPane.verifyPreviousPaginationButtonInAreYouSureFormDisabled();
+          BulkEditSearchPane.verifyNextPaginationButtonInAreYouSureFormDisabled();
+
+          // Step 15: Download preview
+          BulkEditActions.downloadPreview();
+          FileManager.convertCsvToJson(fileNames.previewRecordsCSV).then((csvFileData) => {
+            const holdingRows = getRowsInCsvFileMatchingHrids(csvFileData, holdingHrids);
+            holdingRows.forEach((holdingRow) => {
+              cy.expect(
+                holdingRow[BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS],
+              ).to.equal(updatedHoldingsElectronicAccessInFile);
+            });
+          });
+
+          // Step 16: Commit changes and verify updated records
+          BulkEditActions.commitChanges();
+          BulkEditActions.verifySuccessBanner(holdingIds.length);
+
+          holdingHrids.forEach((holdingHrid) => {
+            BulkEditSearchPane.verifyExactChangesUnderColumnsByIdentifierInChangesAccordion(
+              holdingHrid,
+              BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS,
+              updatedHoldingsElectronicAccess,
+            );
+          });
+
+          // Step 17: Download changed records
+          BulkEditActions.openActions();
+          BulkEditActions.downloadChangedCSV();
+          FileManager.convertCsvToJson(fileNames.changedRecordsCSV).then((csvFileData) => {
+            const holdingRows = getRowsInCsvFileMatchingHrids(csvFileData, holdingHrids);
+            holdingRows.forEach((holdingRow) => {
+              cy.expect(
+                holdingRow[BULK_EDIT_TABLE_COLUMN_HEADERS.INVENTORY_HOLDINGS.ELECTRONIC_ACCESS],
+              ).to.equal(updatedHoldingsElectronicAccessInFile);
+            });
+          });
+
+          // Step 18: Verify changes in Inventory app
+          TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
+
+          instances.forEach((instance) => {
+            InventorySearchAndFilter.searchInstanceByTitle(instance.title);
+            InventorySearchAndFilter.selectViewHoldings();
+            HoldingsRecordView.waitLoading();
+            HoldingsRecordView.checkElectronicAccess('-', '-', '-', '-');
+            HoldingsRecordView.checkElectronicAccess('-', '-', '-', '-', 1);
+            HoldingsRecordView.close();
+            InventorySearchAndFilter.resetAll();
+          });
+        },
+      );
+    });
+  });
+});
