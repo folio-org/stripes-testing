c11bb3f8177190e956a728c8ed510498b0c07503
Denys Bohdan <denys_bohdan@epam.com>
2025-08-18 15:52:35 +0200

    FAT-21567 Update label for 38 position of MARC authority "008" field to "Mod Rec"

--- cypress/support/fragments/marcAuthority/marcAuthorities.js
+++ cypress/support/fragments/marcAuthority/marcAuthorities.js
@@ -136,7 +136,7 @@ const valid008FieldValues = {
   Lang: '\\',
   'Level Est': 'a',
   'Main use': 'a',
-  'Mod Rec Est': '\\',
+  'Mod Rec': '\\',
   'Numb Series': 'n',
   'Pers Name': 'a',
   RecUpd: 'a',
--- cypress/support/fragments/quickMarcEditor.js
+++ cypress/support/fragments/quickMarcEditor.js
@@ -1597,10 +1597,7 @@ export default {
       'ui-quick-marc.record.fixedField-Level Est-text',
       'Level of establishment',
     );
-    this.verifyDropdownHoverText(
-      'ui-quick-marc.record.fixedField-Mod Rec Est-text',
-      'Modified record',
-    );
+    this.verifyDropdownHoverText('ui-quick-marc.record.fixedField-Mod Rec-text', 'Modified record');
     this.verifyDropdownHoverText(
       'ui-quick-marc.record.fixedField-Source-text',
       'Cataloging source',
