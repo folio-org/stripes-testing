4d628558ec49d42fd8a517b9d99a41e7806ee993
Tetiana_Paranich <tetiana_paranich@epam.com>
2025-08-19 13:56:20 +0300

    marked test flaky

--- cypress/e2e/data-import/e2e/match-on-pol-and-update-items.cy.js
+++ cypress/e2e/data-import/e2e/match-on-pol-and-update-items.cy.js
@@ -302,7 +302,7 @@ describe('Data Import', () => {
 
     it(
       'C350590 Match on POL and update related Instance, Holdings, Item (folijet)',
-      { tags: ['smoke', 'folijet', 'C350590'] },
+      { tags: ['smokeFlaky', 'folijet', 'C350590'] },
       () => {
         // create the first PO with POL
         Orders.createOrderWithOrderLineViaApi(
