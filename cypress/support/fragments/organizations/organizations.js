96a89ea8cd4b4014dc181452427686ea7bc06e9f
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2025-08-20 19:00:54 +0300

    added ignore line

--- cypress/support/fragments/organizations/organizations.js
+++ cypress/support/fragments/organizations/organizations.js
@@ -875,6 +875,7 @@ export default {
   },
 
   selectInterfaceType: (interfaceType) => {
+    // eslint-disable-next-line cypress/no-force
     cy.get('select[name="type"]').select(interfaceType, { force: true });
   },
 
