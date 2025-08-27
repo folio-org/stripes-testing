b119461d8f06b1d2a96f47679b0b644348fc41a2
Tetiana_Paranich <tetiana_paranich@epam.com>
2025-08-27 15:44:38 +0300

    added tests

---
+++ cypress/e2e/consortia/inventory/instance/set-record-for-deletion-option-in-action-menu-for-shared-instance-on-central.cy.js
@@ -0,0 +1,60 @@
+import { Permissions } from '../../../../support/dictionary';
+import { tenantNames } from '../../../../support/dictionary/affiliations';
+import InstanceRecordView from '../../../../support/fragments/inventory/instanceRecordView';
+import InventoryInstance from '../../../../support/fragments/inventory/inventoryInstance';
+import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
+import SetRecordForDeletionModal from '../../../../support/fragments/inventory/modals/setRecordForDeletionModal';
+import ConsortiumManager from '../../../../support/fragments/settings/consortium-manager/consortium-manager';
+import TopMenu from '../../../../support/fragments/topMenu';
+import Users from '../../../../support/fragments/users/users';
+
+describe('Inventory', () => {
+  describe('Instance', () => {
+    const testData = {};
+
+    before('Create test data and login', () => {
+      cy.getAdminToken();
+      InventoryInstance.createInstanceViaApi().then(({ instanceData }) => {
+        testData.instance = instanceData;
+      });
+
+      cy.createTempUser([
+        Permissions.uiInventorySetRecordsForDeletion.gui,
+        Permissions.inventoryAll.gui,
+      ]).then((userProperties) => {
+        testData.user = userProperties;
+
+        cy.resetTenant();
+        cy.login(testData.user.username, testData.user.password, {
+          path: TopMenu.inventoryPath,
+          waiter: InventoryInstances.waitContentLoading,
+        });
+        ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
+        InventoryInstances.searchByTitle(testData.instance.instanceId);
+        InventoryInstances.selectInstance();
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(testData.user.userId);
+        InventoryInstance.deleteInstanceViaApi(testData.instance.instanceId);
+      });
+    });
+
+    it(
+      'C436835 (CONSORTIA) Check "Set record for deletion" option in Actions menu for Shared instance on Central tenant (folijet)',
+      { tags: ['extendedPathECS', 'folijet', 'C436835'] },
+      () => {
+        InstanceRecordView.waitLoading();
+        InstanceRecordView.clickActionsButton();
+        InstanceRecordView.setRecordForDeletion();
+        SetRecordForDeletionModal.waitLoading();
+        SetRecordForDeletionModal.verifyModalView(testData.instance.instanceTitle);
+        SetRecordForDeletionModal.clickCancel();
+        SetRecordForDeletionModal.isNotDisplayed();
+        InstanceRecordView.waitLoading();
+      },
+    );
+  });
+});
