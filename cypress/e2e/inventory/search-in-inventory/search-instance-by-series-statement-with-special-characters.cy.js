03f5e7ef3e7feea815d7bf4c3fd72d91449fa78e
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-08-28 12:29:58 +0500

    FAT-21559, pt5 (#5560)
    * FAT-21559-pt5: tests added
    
    * FAT-21599-pt5: C368039, C368041 added
    
    * FAT-21559-pt5: C369043 added

---
+++ cypress/e2e/inventory/search-in-inventory/search-instance-by-series-statement-with-special-characters.cy.js
@@ -0,0 +1,136 @@
+import InventoryInstances, {
+  searchInstancesOptions,
+} from '../../../support/fragments/inventory/inventoryInstances';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import { Permissions } from '../../../support/dictionary';
+import { randomFourDigitNumber } from '../../../support/utils/stringTools';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+
+describe('Inventory', () => {
+  describe('Search in Inventory', () => {
+    const randomDigits = `C368041${randomFourDigitNumber()}${randomFourDigitNumber()}`;
+    const keywordOption = searchInstancesOptions[0];
+    const instanceTitlePrefix = `AT_C368041_FolioInstance_${randomDigits}`;
+
+    const seriesStatements = [
+      `${randomDigits}Ultimate Matrix and collection discs 5, ${randomDigits}6`,
+      `${randomDigits}Ultimate & Matrix collection; discs 5-${randomDigits}6`,
+      `${randomDigits}Ultimate Matrix : collection discs 5, ${randomDigits}6`,
+      `"${randomDigits}Ultimate Matrix" / collection ; {discs 5, ${randomDigits}6}`,
+      `.${randomDigits}Ultimate - Matrix [collection] discs 5, ${randomDigits}6 !`,
+      `${randomDigits}Ultimate & Matrix : collection discs 5 / ${randomDigits}6, (new edition).`,
+      `${randomDigits}Ultimate Matrix collection discs 5, ${randomDigits}6`,
+      `${randomDigits}Ultimate&Matrix collection: discs 5/ ${randomDigits}6`,
+    ];
+
+    const searchTerms = [
+      {
+        value: `${randomDigits}Ultimate Matrix collection discs 5, ${randomDigits}6`,
+        expectedInstanceIndexes: [1, 2, 3, 4, 5, 6, 7, 8],
+      },
+      {
+        value: `${randomDigits}Ultimate & Matrix collection; discs 5-${randomDigits}6`,
+        expectedInstanceIndexes: [1, 2, 3, 4, 5, 6, 7, 8],
+      },
+      {
+        value: `${randomDigits}Ultimate Matrix : collection discs 5, ${randomDigits}6`,
+        expectedInstanceIndexes: [1, 2, 3, 4, 5, 6, 7, 8],
+      },
+      {
+        value: `${randomDigits}Ultimate Matrix / collection discs 5-${randomDigits}6`,
+        expectedInstanceIndexes: [1, 2, 3, 4, 5, 6, 7, 8],
+      },
+      {
+        value: `${randomDigits}Ultimate&Matrix collection: discs 5/ ${randomDigits}6`,
+        expectedInstanceIndexes: [1, 2, 3, 4, 5, 6, 7, 8],
+      },
+      {
+        value: `discs 5-${randomDigits}6 : matrix ${randomDigits}ultimate-collection`,
+        expectedInstanceIndexes: [1, 2, 3, 4, 5, 6, 7, 8],
+      },
+      {
+        value: `${randomDigits}Ultimate Matrix and collection discs 5, ${randomDigits}6`,
+        expectedInstanceIndexes: [1, 2, 6],
+      },
+      {
+        value: `${randomDigits}Ultimate & Matrix : collection discs 5 / ${randomDigits}6, (new edition).`,
+        expectedInstanceIndexes: [6],
+      },
+      {
+        value: `.${randomDigits}Ultimate - Matrix [collection] discs 5, ${randomDigits}6`,
+        expectedInstanceIndexes: [],
+      },
+    ];
+
+    const instances = seriesStatements.map((seriesStatement, index) => ({
+      title: `${instanceTitlePrefix}_${index + 1}`,
+      seriesStatement,
+    }));
+
+    let user;
+
+    before('Create test data', () => {
+      cy.getAdminToken();
+      InventoryInstances.deleteInstanceByTitleViaApi('AT_C368041_FolioInstance');
+
+      // Get required instance metadata
+      cy.getInstanceTypes({ limit: 1 }).then((instanceTypeData) => {
+        // Create all instances with specific series statements
+        instances.forEach((instance) => {
+          cy.createInstance({
+            instance: {
+              instanceTypeId: instanceTypeData[0].id,
+              title: instance.title,
+              series: [{ value: instance.seriesStatement }],
+            },
+          }).then((instanceId) => {
+            instance.id = instanceId;
+          });
+        });
+
+        cy.createTempUser([Permissions.inventoryAll.gui]).then((userProperties) => {
+          user = userProperties;
+          cy.login(user.username, user.password, {
+            path: TopMenu.inventoryPath,
+            waiter: InventorySearchAndFilter.waitLoading,
+          });
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(user.userId);
+      instances.forEach((instance) => {
+        InventoryInstance.deleteInstanceViaApi(instance.id);
+      });
+    });
+
+    it(
+      'C368041 Search for "Instance" by "Series statement" field with special characters using "Keyword" search option (spitfire)',
+      { tags: ['extendedPath', 'spitfire', 'C368041'] },
+      () => {
+        // Ensure we're on the Instance tab with Keyword search selected
+        InventorySearchAndFilter.instanceTabIsDefault();
+        InventorySearchAndFilter.verifyDefaultSearchOptionSelected(keywordOption);
+
+        searchTerms.forEach((term) => {
+          if (term.expectedInstanceIndexes.length) {
+            InventoryInstances.searchByTitle(term.value);
+            InventorySearchAndFilter.checkRowsCount(term.expectedInstanceIndexes.length);
+            term.expectedInstanceIndexes.forEach((index) => {
+              InventorySearchAndFilter.verifyInstanceDisplayed(`${instanceTitlePrefix}_${index}`);
+            });
+          } else {
+            InventoryInstances.searchByTitle(term.value, false);
+            InventorySearchAndFilter.verifyResultPaneEmpty();
+          }
+          InventorySearchAndFilter.clearSearchInputField();
+          InventorySearchAndFilter.verifyResultPaneEmpty();
+        });
+      },
+    );
+  });
+});
---
+++ cypress/e2e/inventory/search-in-inventory/search-item-by-uuid-keyword-and-item-uuid-options.cy.js
@@ -0,0 +1,99 @@
+import InventoryInstances, {
+  searchItemsOptions,
+} from '../../../support/fragments/inventory/inventoryInstances';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import ItemRecordView from '../../../support/fragments/inventory/item/itemRecordView';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import { Permissions } from '../../../support/dictionary';
+import getRandomPostfix from '../../../support/utils/stringTools';
+
+describe('Inventory', () => {
+  describe('Search in Inventory', () => {
+    const randomPostfix = getRandomPostfix();
+    const instanceTitlePrefix = `AT_C813025_FolioInstance_${randomPostfix}`;
+    const folioInstances = InventoryInstances.generateFolioInstances({
+      instanceTitlePrefix,
+      count: 3,
+      itemsCount: 1,
+    });
+    const searchOptions = {
+      keyword: searchItemsOptions[0],
+      itemUuid: searchItemsOptions[10],
+    };
+
+    let user;
+    const itemUuids = [];
+    const itemHrids = [];
+
+    before('Create test data', () => {
+      cy.getAdminToken();
+      cy.getLocations({
+        limit: 1,
+        query: '(isActive=true and name<>"*autotest*" and name<>"AT_*")',
+      }).then((location) => {
+        InventoryInstances.createFolioInstancesViaApi({
+          folioInstances,
+          location,
+        }).then(() => {
+          // Collect item UUIDs for testing
+          folioInstances.forEach((instance) => {
+            instance.items.forEach((item) => {
+              itemUuids.push(item.id);
+              itemHrids.push(item.hrid);
+            });
+          });
+        });
+      });
+
+      cy.createTempUser([Permissions.uiInventoryViewInstances.gui]).then((userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password, {
+          path: TopMenu.inventoryPath,
+          waiter: InventorySearchAndFilter.waitLoading,
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(user.userId);
+      InventoryInstances.deleteFullInstancesByTitleViaApi(instanceTitlePrefix);
+    });
+
+    it(
+      'C813025 Search for Item by UUID field using "Keyword" and "Item UUID" search options (spitfire)',
+      { tags: ['extendedPath', 'spitfire', 'C813025'] },
+      () => {
+        // Step 1: Search for item using item UUID with "Keyword" search option
+        InventorySearchAndFilter.switchToItem();
+        InventorySearchAndFilter.itemTabIsDefault();
+        InventorySearchAndFilter.verifyDefaultSearchOptionSelected(searchOptions.keyword);
+
+        InventoryInstances.searchByTitle(itemUuids[0]);
+        ItemRecordView.verifyHrid(itemHrids[0]);
+
+        // Step 2: Close item detail view and select "Item UUID" search option
+        ItemRecordView.closeDetailView();
+        InventorySearchAndFilter.waitLoading();
+        InventorySearchAndFilter.selectSearchOption(searchOptions.itemUuid);
+        InventorySearchAndFilter.verifyDefaultSearchOptionSelected(searchOptions.itemUuid);
+
+        // Step 3: Search for item using item UUID with "Item UUID" search option
+        InventoryInstances.searchByTitle(itemUuids[0]);
+        ItemRecordView.verifyHrid(itemHrids[0]);
+
+        // Step 4: Repeat test with additional item UUIDs
+        ItemRecordView.closeDetailView();
+        InventorySearchAndFilter.waitLoading();
+        InventoryInstances.searchByTitle(itemUuids[1]);
+        ItemRecordView.verifyHrid(itemHrids[1]);
+
+        ItemRecordView.closeDetailView();
+        InventorySearchAndFilter.waitLoading();
+        InventoryInstances.searchByTitle(itemUuids[2]);
+        ItemRecordView.verifyHrid(itemHrids[2]);
+      },
+    );
+  });
+});
