d5eda3c0c4d190761ac4b45a642993ba1db0f84f
Lisavetti <lizaemets0244@gmail.com>
2025-08-28 17:57:47 +0300

    implement C353532, C359169, C359169

---
+++ cypress/e2e/organizations/make-organization-not-a-vendor.cy.js
@@ -0,0 +1,45 @@
+import TopMenu from '../../support/fragments/topMenu';
+import Organizations from '../../support/fragments/organizations/organizations';
+import Permissions from '../../support/dictionary/permissions';
+import Users from '../../support/fragments/users/users';
+import NewOrganization from '../../support/fragments/organizations/newOrganization';
+
+describe('Organizations', () => {
+  const organization = {
+    ...NewOrganization.defaultUiOrganizations,
+    isVendor: true,
+  };
+  let user;
+
+  before('Create user and organization', () => {
+    cy.getAdminToken();
+    Organizations.createOrganizationViaApi(organization).then((organizationId) => {
+      organization.id = organizationId;
+    });
+    cy.createTempUser([Permissions.uiOrganizationsViewEdit.gui]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password, {
+        path: TopMenu.organizationsPath,
+        waiter: Organizations.waitLoading,
+      });
+    });
+  });
+
+  after('Delete test data', () => {
+    cy.getAdminToken();
+    Organizations.deleteOrganizationViaApi(organization.id);
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C359169 Make existing organization NOT a Vendor (thunderjet)',
+    { tags: ['extendedPath', 'thunderjet'] },
+    () => {
+      Organizations.searchByParameters('Name', organization.name);
+      Organizations.selectOrganization(organization.name);
+      Organizations.editOrganization();
+      Organizations.deselectVendor();
+      Organizations.varifySaveOrganizationCalloutMessage(organization);
+    },
+  );
+});
