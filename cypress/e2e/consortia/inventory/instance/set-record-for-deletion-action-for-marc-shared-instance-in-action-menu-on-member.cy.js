22b55f76e1c4ee976eeb6b500ec0bd6a7374bd16
Tetiana_Paranich <tetiana_paranich@epam.com>
2025-08-30 00:26:53 +0300

    added tests

---
+++ cypress/e2e/consortia/inventory/instance/set-record-for-deletion-action-for-marc-shared-instance-in-action-menu-on-member.cy.js
@@ -0,0 +1,83 @@
+import { DEFAULT_JOB_PROFILE_NAMES } from '../../../../support/constants';
+import { Permissions } from '../../../../support/dictionary';
+import Affiliations, { tenantNames } from '../../../../support/dictionary/affiliations';
+import DataImport from '../../../../support/fragments/data_import/dataImport';
+import InstanceRecordView from '../../../../support/fragments/inventory/instanceRecordView';
+import InventoryInstance from '../../../../support/fragments/inventory/inventoryInstance';
+import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
+import SetRecordForDeletionModal from '../../../../support/fragments/inventory/modals/setRecordForDeletionModal';
+import ConsortiumManager from '../../../../support/fragments/settings/consortium-manager/consortium-manager';
+import TopMenu from '../../../../support/fragments/topMenu';
+import Users from '../../../../support/fragments/users/users';
+import InteractorsTools from '../../../../support/utils/interactorsTools';
+import getRandomPostfix from '../../../../support/utils/stringTools';
+
+describe('Inventory', () => {
+  describe('Instance', () => {
+    const marcFile = {
+      marc: 'oneMarcBib.mrc',
+      marcFileName: `C446002 marcFileName${getRandomPostfix()}.mrc`,
+    };
+    const testData = {
+      instanceTitle:
+        'Anglo-Saxon manuscripts in microfiche facsimile Volume 25 Corpus Christi College, Cambridge II, MSS 12, 144, 162, 178, 188, 198, 265, 285, 322, 326, 449 microform A. N. Doane (editor and director), Matthew T. Hussey (associate editor), Phillip Pulsiano (founding editor)',
+    };
+
+    before('Create test data and login', () => {
+      cy.getAdminToken();
+      DataImport.uploadFileViaApi(
+        marcFile.marc,
+        marcFile.marcFileName,
+        DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS,
+      ).then((response) => {
+        testData.instanceId = response[0].instance.id;
+      });
+
+      cy.createTempUser([
+        Permissions.uiInventorySetRecordsForDeletion.gui,
+        Permissions.inventoryAll.gui,
+      ]).then((userProperties) => {
+        testData.user = userProperties;
+
+        cy.assignAffiliationToUser(Affiliations.College, testData.user.userId);
+        cy.setTenant(Affiliations.College);
+        cy.assignPermissionsToExistingUser(testData.user.userId, [Permissions.inventoryAll.gui]);
+
+        cy.resetTenant();
+        cy.login(testData.user.username, testData.user.password, {
+          path: TopMenu.inventoryPath,
+          waiter: InventoryInstances.waitContentLoading,
+        });
+        ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
+        ConsortiumManager.switchActiveAffiliation(tenantNames.central, tenantNames.college);
+        ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.college);
+        InventoryInstances.searchByTitle(testData.instanceId);
+        InventoryInstances.selectInstance();
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(testData.user.userId);
+        InventoryInstance.deleteInstanceViaApi(testData.instanceId);
+      });
+    });
+
+    it(
+      'C446002 (CONSORTIA) Check "Set record for deletion" action for MARC Shared Instance in Actions menu on Member tenant (folijet)',
+      { tags: ['extendedPathECS', 'folijet', 'C446002'] },
+      () => {
+        InstanceRecordView.waitLoading();
+        InstanceRecordView.clickActionsButton();
+        InstanceRecordView.setRecordForDeletion();
+        SetRecordForDeletionModal.waitLoading();
+        SetRecordForDeletionModal.verifyModalView(testData.instanceTitle);
+        SetRecordForDeletionModal.clickConfirm();
+        SetRecordForDeletionModal.isNotDisplayed();
+        InstanceRecordView.verifyInstanceIsSetForDeletionSuppressedFromDiscoveryStaffSuppressedWarning();
+        InstanceRecordView.waitLoading();
+        InteractorsTools.checkCalloutMessage(`${testData.instanceTitle} has been set for deletion`);
+      },
+    );
+  });
+});
