48680809c6fce86cf488e5159fb9983d7d0da736
Sviatlana Stsiapanava <sviatlana_stsiapanava@epam.com>
2025-09-01 10:09:20 +0300

    Add retry logic

--- cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-item-body-special-chars.cy.js
+++ cypress/e2e/bulk-edit/in-app/bulk-edit-in-app-item-body-special-chars.cy.js
@@ -17,133 +17,144 @@ import { APPLICATION_NAMES } from '../../../support/constants';
 let user;
 let noteTypeId;
 let servicePointId;
-const noteType = `Dew;ey Dec|im:al class & ${getRandomPostfix()}`;
-const itemNote = `Note with;special&characters) ${getRandomPostfix()}`;
-const itemHRIDsFileName = `validItemHRIDs_${getRandomPostfix()}.csv`;
-const item = {
-  instanceName: `AT_C368480_FolioInstance_${getRandomPostfix()}`,
-  barcode: getRandomPostfix(),
-};
+let noteType;
+let itemNote;
+let itemHRIDsFileName;
+let item;
 
-describe('Bulk-edit', () => {
-  describe('In-app approach', () => {
-    before('create test data', () => {
-      cy.createTempUser([
-        permissions.bulkEditView.gui,
-        permissions.bulkEditEdit.gui,
-        permissions.inventoryAll.gui,
-      ]).then((userProperties) => {
-        user = userProperties;
-        ServicePoints.getCircDesk1ServicePointViaApi()
-          .then((servicePoint) => {
-            servicePointId = servicePoint.id;
-          })
-          .then(() => {
-            UserEdit.addServicePointViaApi(servicePointId, user.userId, servicePointId);
+describe(
+  'Bulk-edit',
+  {
+    retries: {
+      runMode: 1,
+    },
+  },
+  () => {
+    describe('In-app approach', () => {
+      beforeEach('create test data', () => {
+        // Recreate variables with fresh values for each retry
+        noteType = `Dew;ey Dec|im:al class & ${getRandomPostfix()}`;
+        itemNote = `Note with;special&characters) ${getRandomPostfix()}`;
+        itemHRIDsFileName = `validItemHRIDs_${getRandomPostfix()}.csv`;
+        item = {
+          instanceName: `AT_C368480_FolioInstance_${getRandomPostfix()}`,
+          barcode: getRandomPostfix(),
+        };
+        cy.createTempUser([
+          permissions.bulkEditView.gui,
+          permissions.bulkEditEdit.gui,
+          permissions.inventoryAll.gui,
+        ]).then((userProperties) => {
+          user = userProperties;
+          ServicePoints.getCircDesk1ServicePointViaApi()
+            .then((servicePoint) => {
+              servicePointId = servicePoint.id;
+            })
+            .then(() => {
+              UserEdit.addServicePointViaApi(servicePointId, user.userId, servicePointId);
+            });
+
+          InventoryInstances.createInstanceViaApi(item.instanceName, item.barcode);
+
+          ItemNoteTypes.createItemNoteTypeViaApi(noteType).then((noteId) => {
+            noteTypeId = noteId;
+
+            cy.getItems({ limit: 1, expandAll: true, query: `"barcode"=="${item.barcode}"` }).then(
+              (itemData) => {
+                item.hrid = itemData.hrid;
+
+                // need to wait created item note type to be saved
+                cy.wait(10_000);
+
+                itemData.notes = [
+                  {
+                    itemNoteTypeId: noteTypeId,
+                    note: itemNote,
+                    staffOnly: false,
+                  },
+                ];
+
+                cy.updateItemViaApi(itemData);
+
+                FileManager.createFile(`cypress/fixtures/${itemHRIDsFileName}`, item.hrid);
+              },
+            );
           });
 
-        InventoryInstances.createInstanceViaApi(item.instanceName, item.barcode);
-
-        ItemNoteTypes.createItemNoteTypeViaApi(noteType).then((noteId) => {
-          noteTypeId = noteId;
-
-          cy.getItems({ limit: 1, expandAll: true, query: `"barcode"=="${item.barcode}"` }).then(
-            (itemData) => {
-              item.hrid = itemData.hrid;
-
-              // need to wait created item note type to be saved
-              cy.wait(3000);
-
-              itemData.notes = [
-                {
-                  itemNoteTypeId: noteTypeId,
-                  note: itemNote,
-                  staffOnly: false,
-                },
-              ];
-
-              cy.updateItemViaApi(itemData);
-
-              FileManager.createFile(`cypress/fixtures/${itemHRIDsFileName}`, item.hrid);
-            },
-          );
-        });
-
-        cy.login(user.username, user.password, {
-          path: TopMenu.bulkEditPath,
-          waiter: BulkEditSearchPane.waitLoading,
+          cy.login(user.username, user.password, {
+            path: TopMenu.bulkEditPath,
+            waiter: BulkEditSearchPane.waitLoading,
+          });
         });
       });
-    });
 
-    after('delete test data', () => {
-      cy.getAdminToken();
-      Users.deleteViaApi(user.userId);
-      InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.barcode);
-      FileManager.deleteFile(`cypress/fixtures/${itemHRIDsFileName}`);
-    });
-
-    it(
-      'C368480 Verify that there no errors during bulk editing if ITEMS body has special characters (firebird) (TaaS)',
-      { tags: ['extendedPath', 'firebird', 'C368480'] },
-      () => {
-        BulkEditSearchPane.checkItemsRadio();
-        BulkEditSearchPane.selectRecordIdentifier('Item HRIDs');
-        BulkEditSearchPane.uploadFile(itemHRIDsFileName);
-        BulkEditSearchPane.waitFileUploading();
-        BulkEditSearchPane.verifyMatchedResults(item.hrid);
-        BulkEditActions.openActions();
-        BulkEditSearchPane.changeShowColumnCheckboxIfNotYet(noteType, 'Item temporary location');
-        BulkEditSearchPane.verifySpecificItemsMatched(itemNote);
-
-        const location = 'Online';
-        BulkEditActions.openStartBulkEditForm();
-        BulkEditActions.replaceTemporaryLocation(location);
-        BulkEditActions.confirmChanges();
-        BulkEditActions.verifyAreYouSureForm(1, location);
-        BulkEditActions.commitChanges();
-        BulkEditSearchPane.waitFileUploading();
-        BulkEditSearchPane.verifyChangedResults(location);
-
-        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
-        InventorySearchAndFilter.switchToItem();
-        InventorySearchAndFilter.searchByParameter('Barcode', item.barcode);
-        ItemRecordView.waitLoading();
-        ItemRecordView.verifyTemporaryLocation(location);
-        ItemRecordView.checkItemNote(itemNote, 'No', noteType);
-
+      afterEach('delete test data', () => {
         cy.getAdminToken();
+        Users.deleteViaApi(user.userId);
+        InventoryInstances.deleteInstanceAndHoldingRecordAndAllItemsViaApi(item.barcode);
         ItemNoteTypes.deleteItemNoteTypeViaApi(noteTypeId);
+        FileManager.deleteFile(`cypress/fixtures/${itemHRIDsFileName}`);
+      });
 
-        cy.getToken(user.username, user.password);
-        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.BULK_EDIT);
-        BulkEditSearchPane.checkHoldingsRadio();
-        BulkEditSearchPane.checkItemsRadio();
-        BulkEditSearchPane.selectRecordIdentifier('Item HRIDs');
-        BulkEditSearchPane.uploadFile(itemHRIDsFileName);
-        BulkEditSearchPane.waitFileUploading();
-        BulkEditSearchPane.verifyMatchedResults(item.hrid);
-        BulkEditActions.openActions();
-        BulkEditSearchPane.changeShowColumnCheckboxIfNotYet('Note', 'Item temporary location');
-        BulkEditSearchPane.verifySpecificItemsMatched(item.hrid);
+      it(
+        'C368480 Verify that there no errors during bulk editing if ITEMS body has special characters (firebird) (TaaS)',
+        { tags: ['extendedPath', 'firebird', 'C368480'] },
+        () => {
+          BulkEditSearchPane.checkItemsRadio();
+          BulkEditSearchPane.selectRecordIdentifier('Item HRIDs');
+          BulkEditSearchPane.uploadFile(itemHRIDsFileName);
+          BulkEditSearchPane.waitFileUploading();
+          BulkEditSearchPane.verifyMatchedResults(item.hrid);
+          BulkEditActions.openActions();
+          BulkEditSearchPane.changeShowColumnCheckboxIfNotYet(noteType, 'Item temporary location');
+          BulkEditSearchPane.verifySpecificItemsMatched(itemNote);
 
-        const newLocation = 'Annex';
-        BulkEditActions.openStartBulkEditForm();
-        BulkEditActions.replaceTemporaryLocation(newLocation);
-        BulkEditActions.confirmChanges();
-        BulkEditActions.verifyAreYouSureForm(1, newLocation);
-        BulkEditActions.commitChanges();
-        BulkEditSearchPane.waitFileUploading();
-        BulkEditSearchPane.verifyChangedResults(newLocation);
+          const location = 'Online';
+          BulkEditActions.openStartBulkEditForm();
+          BulkEditActions.replaceTemporaryLocation(location);
+          BulkEditActions.confirmChanges();
+          BulkEditActions.verifyAreYouSureForm(1, location);
+          BulkEditActions.commitChanges();
+          BulkEditSearchPane.waitFileUploading();
+          BulkEditSearchPane.verifyChangedResults(location);
 
-        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
-        ItemRecordView.closeDetailView();
-        InventorySearchAndFilter.resetAll();
-        InventorySearchAndFilter.switchToItem();
-        InventorySearchAndFilter.searchByParameter('Barcode', item.barcode);
-        ItemRecordView.waitLoading();
-        ItemRecordView.verifyTemporaryLocation(newLocation);
-      },
-    );
-  });
-});
+          TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
+          InventorySearchAndFilter.switchToItem();
+          InventorySearchAndFilter.searchByParameter('Barcode', item.barcode);
+          ItemRecordView.waitLoading();
+          ItemRecordView.verifyTemporaryLocation(location);
+          ItemRecordView.checkItemNote(itemNote, 'No', noteType);
+
+          cy.getToken(user.username, user.password);
+          TopMenuNavigation.navigateToApp(APPLICATION_NAMES.BULK_EDIT);
+          BulkEditSearchPane.checkHoldingsRadio();
+          BulkEditSearchPane.checkItemsRadio();
+          BulkEditSearchPane.selectRecordIdentifier('Item HRIDs');
+          BulkEditSearchPane.uploadFile(itemHRIDsFileName);
+          BulkEditSearchPane.waitFileUploading();
+          BulkEditSearchPane.verifyMatchedResults(item.hrid);
+          BulkEditActions.openActions();
+          BulkEditSearchPane.changeShowColumnCheckboxIfNotYet('Note', 'Item temporary location');
+          BulkEditSearchPane.verifySpecificItemsMatched(item.hrid);
+
+          const newLocation = 'Annex';
+          BulkEditActions.openStartBulkEditForm();
+          BulkEditActions.replaceTemporaryLocation(newLocation);
+          BulkEditActions.confirmChanges();
+          BulkEditActions.verifyAreYouSureForm(1, newLocation);
+          BulkEditActions.commitChanges();
+          BulkEditSearchPane.waitFileUploading();
+          BulkEditSearchPane.verifyChangedResults(newLocation);
+
+          TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
+          ItemRecordView.closeDetailView();
+          InventorySearchAndFilter.resetAll();
+          InventorySearchAndFilter.switchToItem();
+          InventorySearchAndFilter.searchByParameter('Barcode', item.barcode);
+          ItemRecordView.waitLoading();
+          ItemRecordView.verifyTemporaryLocation(newLocation);
+        },
+      );
+    });
+  },
+);
