9065a8e4b969e4655432818ad2f8489591ff7bc2
Viktoriia Markus <viktoriia_markus@epam.com>
2025-09-02 14:44:10 +0300

    implement C423475 C423476 C423457 C423458

--- cypress/support/fragments/eholdings/eHoldingsNewCustomPackage.js
+++ cypress/support/fragments/eholdings/eHoldingsNewCustomPackage.js
@@ -1,4 +1,16 @@
-import { Button, HTML, TextField, including, Select, Pane } from '../../../../interactors';
+import { Button, HTML, TextField, including, Select, Pane, Modal } from '../../../../interactors';
+
+const nameField = TextField({ name: 'name' });
+const contentTypeSelect = Select({ name: 'contentType' });
+const saveAndCloseButton = Button({ type: 'submit' });
+const cancelButton = Button('Cancel');
+const closeIconButton = Button({ icon: 'times' });
+const unsavedChangesModal = Modal({ id: 'navigation-modal' });
+const unsavedChangesText = Modal().find(
+  HTML('Your changes have not been saved. Are you sure you want to leave this page?'),
+);
+const keepEditingButton = Modal().find(Button('Keep editing'));
+const continueWithoutSavingButton = Modal().find(Button('Continue without saving'));
 
 export default {
   waitLoading: () => {
@@ -6,18 +18,57 @@ export default {
   },
 
   fillInRequiredProperties: (packageName) => {
-    cy.do(TextField({ name: 'name' }).fillIn(packageName));
+    cy.do(nameField.fillIn(packageName));
   },
 
   chooseContentType: (contentTypeValue) => {
-    cy.do(Select({ name: 'contentType' }).choose(contentTypeValue));
+    cy.do(contentTypeSelect.choose(contentTypeValue));
   },
 
   saveAndClose: () => {
-    cy.do(Button({ type: 'submit' }).click());
+    cy.do(saveAndCloseButton.click());
   },
 
   checkPackageCreatedCallout(calloutMessage = 'Custom package created.') {
     cy.expect(HTML(including(calloutMessage)).exists());
+  },
+
+  verifyButtonsDisabled: () => {
+    cy.expect(cancelButton.has({ disabled: true }));
+    cy.expect(saveAndCloseButton.has({ disabled: true }));
+  },
+
+  verifyButtonsEnabled: () => {
+    cy.expect(cancelButton.has({ disabled: false }));
+    cy.expect(saveAndCloseButton.has({ disabled: false }));
+  },
+
+  cancelChanges: () => {
+    cy.expect(cancelButton.exists());
+    cy.do(cancelButton.click());
+  },
+
+  verifyUnsavedChangesModalExists: () => {
+    cy.expect(unsavedChangesModal.exists());
+    cy.expect(unsavedChangesText.exists());
+  },
+
+  clickKeepEditing: () => {
+    cy.expect(keepEditingButton.exists());
+    cy.do(keepEditingButton.click());
+  },
+
+  clickContinueWithoutSaving: () => {
+    cy.expect(continueWithoutSavingButton.exists());
+    cy.do(continueWithoutSavingButton.click());
+  },
+
+  closeEditingWindow: () => {
+    cy.expect(closeIconButton.exists());
+    cy.do(closeIconButton.click());
+  },
+
+  verifyNameFieldValue: (expectedValue) => {
+    cy.expect(nameField.has({ value: expectedValue }));
   },
 };
