e99de34bee468d72ce3132f92d8bc44b0a4109c3
Viktoriia Markus <viktoriia_markus@epam.com>
2025-09-02 15:19:14 +0300

    adjust tests

--- cypress/e2e/eholdings/package/cancel-changes-using-x-icon-in-custom-package.cy.js
+++ cypress/e2e/eholdings/package/cancel-changes-using-x-icon-in-custom-package.cy.js
@@ -14,7 +14,6 @@ describe('eHoldings', () => {
     };
 
     before('Creating user, logging in', () => {
-      cy.getAdminToken();
       cy.createTempUser([
         permissions.uieHoldingsTitlesPackagesCreateDelete.gui,
         permissions.uieHoldingsRecordsEdit.gui,
