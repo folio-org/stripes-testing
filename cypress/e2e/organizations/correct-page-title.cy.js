d595b9fc5cdad661d2a877b1b3d72cb34f6fa086
Lisavetti <lizaemets0244@gmail.com>
2025-09-04 17:31:56 +0300

    implement C434063, С434070, С451606

---
+++ cypress/e2e/organizations/correct-page-title.cy.js
@@ -0,0 +1,69 @@
+import permissions from '../../support/dictionary/permissions';
+import Organizations from '../../support/fragments/organizations/organizations';
+import TopMenu from '../../support/fragments/topMenu';
+import Users from '../../support/fragments/users/users';
+import newOrganization from '../../support/fragments/organizations/newOrganization';
+import getRandomPostfix from '../../support/utils/stringTools';
+
+describe('Organizations → Correct page title when Organization details pane is opened', () => {
+  let user;
+  const resetFiltersMessage = 'Choose a filter or enter a search query to show results.';
+  const organizations = [
+    {
+      ...newOrganization.defaultUiOrganizations,
+      name: `Test Organization 1 ${Date.now()}`,
+      code: `${getRandomPostfix()}_1`,
+    },
+    {
+      ...newOrganization.defaultUiOrganizations,
+      name: `Test Organization 2 ${Date.now()}`,
+      code: `${getRandomPostfix()}_2`,
+    },
+  ];
+
+  before('Create user and organizations', () => {
+    cy.getAdminToken();
+    organizations.forEach((org) => {
+      Organizations.createOrganizationViaApi(org).then((orgId) => {
+        org.id = orgId;
+      });
+    });
+    cy.createTempUser([permissions.uiOrganizationsView.gui]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password, {
+        path: TopMenu.organizationsPath,
+        waiter: Organizations.waitLoading,
+      });
+    });
+  });
+
+  after('Delete test data', () => {
+    cy.getAdminToken();
+    organizations.forEach((org) => {
+      Organizations.deleteOrganizationViaApi(org.id);
+    });
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C451606 Correct page title when Organization details pane is opened (thunderjet)',
+    { tags: ['extendedPath', 'thunderjet'] },
+    () => {
+      Organizations.verifyNoResultMessage(resetFiltersMessage);
+      Organizations.searchByParameters('Name', organizations[0].name);
+      Organizations.selectOrganization(organizations[0].name);
+      Organizations.closeDetailsPane();
+      Organizations.resetFilters();
+      Organizations.verifyNoResultMessage(resetFiltersMessage);
+      Organizations.selectActiveStatus();
+      Organizations.searchByParameters('Name', organizations[0].name);
+      Organizations.checkSearchResults(organizations[0]);
+      Organizations.selectOrganization(organizations[0].name);
+      Organizations.checkOrganizationInfo(organizations[0]);
+      Organizations.searchByParameters('Name', organizations[1].name);
+      Organizations.checkSearchResults(organizations[1]);
+      Organizations.selectOrganization(organizations[1].name);
+      Organizations.checkOrganizationInfo(organizations[1]);
+    },
+  );
+});
---
+++ cypress/e2e/organizations/integration-day-field-validation.cy.js
@@ -0,0 +1,107 @@
+import permissions from '../../support/dictionary/permissions';
+import Organizations from '../../support/fragments/organizations/organizations';
+import TopMenu from '../../support/fragments/topMenu';
+import Users from '../../support/fragments/users/users';
+import newOrganization from '../../support/fragments/organizations/newOrganization';
+import getRandomPostfix from '../../support/utils/stringTools';
+import DateTools from '../../support/utils/dateTools';
+
+describe('Organizations → Integration Day Field Validation', () => {
+  let user;
+  const organization = {
+    ...newOrganization.defaultUiOrganizations,
+    accounts: [
+      {
+        name: `autotest_name_${getRandomPostfix()}`,
+        accountNo: getRandomPostfix(),
+        description: 'This is my account description.',
+        paymentMethod: 'EFT',
+        accountStatus: 'Active',
+      },
+    ],
+  };
+  const integrationName = `IntegrationName${getRandomPostfix()}`;
+  const integartionDescription = 'Test Integation descripton1';
+  const vendorEDICode = getRandomPostfix();
+  const libraryEDICode = getRandomPostfix();
+  const UTCTime = DateTools.getUTCDateForScheduling();
+
+  const schedulingInfo1 = {
+    period: 'Monthly',
+    day: '12345',
+    time: DateTools.getUTCDateForScheduling(),
+  };
+  const schedulingInfo2 = {
+    period: 'Monthly',
+    day: '32',
+    time: DateTools.getUTCDateForScheduling(),
+  };
+  const schedulingInfo3 = {
+    period: 'Monthly',
+    day: '-12',
+    time: DateTools.getUTCDateForScheduling(),
+  };
+  const schedulingInfo4 = {
+    period: 'Monthly',
+    day: '23',
+    time: DateTools.getUTCDateForScheduling(),
+  };
+
+  before('Create user, organization, and integration', () => {
+    cy.loginAsAdmin({
+      path: TopMenu.organizationsPath,
+      waiter: Organizations.waitLoading,
+    });
+    Organizations.createOrganizationViaApi(organization).then((orgId) => {
+      organization.id = orgId;
+    });
+    Organizations.searchByParameters('Name', organization.name);
+    Organizations.selectOrganization(organization.name);
+    Organizations.addIntegration();
+    Organizations.fillIntegrationInformationWithoutSchedulingWithDifferentInformation(
+      integrationName,
+      integartionDescription,
+      vendorEDICode,
+      libraryEDICode,
+      organization.accounts[0].accountNo,
+      'Purchase',
+      UTCTime,
+    );
+    cy.wait(4000);
+    cy.createTempUser([permissions.uiOrganizationsViewEdit.gui]).then((userProperties) => {
+      user = userProperties;
+      cy.login(user.username, user.password, {
+        path: TopMenu.organizationsPath,
+        waiter: Organizations.waitLoading,
+      });
+    });
+  });
+
+  after('Delete test data', () => {
+    cy.getAdminToken();
+    Organizations.deleteOrganizationViaApi(organization.id);
+    Users.deleteViaApi(user.userId);
+  });
+
+  it(
+    'C434063 "Day" field on Integration edit page accepts only numbers less than "31" (thunderjet)',
+    { tags: ['extendedPath', 'thunderjet'] },
+    () => {
+      Organizations.searchByParameters('Name', organization.name);
+      Organizations.selectOrganization(organization.name);
+      Organizations.selectIntegration(integrationName);
+      Organizations.editIntegration();
+      Organizations.clickSchedulingEDICheckbox();
+      Organizations.fillScheduleInfo(schedulingInfo1);
+      Organizations.checkDayFieldError();
+      Organizations.fillScheduleInfo(schedulingInfo2);
+      Organizations.checkDayFieldError();
+      Organizations.fillScheduleInfo(schedulingInfo3);
+      Organizations.checkDayFieldError('Value must be greater than or equal to 1');
+      Organizations.fillScheduleInfo(schedulingInfo4);
+      Organizations.saveOrganization();
+      Organizations.closeIntegrationDetailsPane();
+      Organizations.checkIntegrationsAdd(integrationName, integartionDescription);
+    },
+  );
+});
