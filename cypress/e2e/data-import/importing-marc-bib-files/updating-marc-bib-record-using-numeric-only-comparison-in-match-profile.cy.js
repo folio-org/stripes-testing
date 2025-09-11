0354228d9f3c6dbba1a45d35ed67dae1a32bd247
Tetiana_Paranich <tetiana_paranich@epam.com>
2025-09-11 19:43:14 +0300

    added test

---
+++ cypress/e2e/data-import/importing-marc-bib-files/updating-marc-bib-record-using-numeric-only-comparison-in-match-profile.cy.js
@@ -0,0 +1,236 @@
+// import {
+//   ACCEPTED_DATA_TYPE_NAMES,
+//   ACTION_NAMES_IN_ACTION_PROFILE,
+//   APPLICATION_NAMES,
+//   DEFAULT_JOB_PROFILE_NAMES,
+//   EXISTING_RECORD_NAMES,
+//   FOLIO_RECORD_TYPE,
+//   JOB_STATUS_NAMES,
+//   RECORD_STATUSES,
+// } from '../../../support/constants';
+import { Permissions } from '../../../support/dictionary';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+// import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+// import NewJobProfile from '../../../support/fragments/data_import/job_profiles/newJobProfile';
+// import FileDetails from '../../../support/fragments/data_import/logs/fileDetails';
+// import Logs from '../../../support/fragments/data_import/logs/logs';
+// import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+// import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+// import {
+//   ActionProfiles as SettingsActionProfiles,
+//   FieldMappingProfiles as SettingsFieldMappingProfiles,
+//   JobProfiles as SettingsJobProfiles,
+//   MatchProfiles as SettingsMatchProfiles,
+// } from '../../../support/fragments/settings/dataImport';
+// import FieldMappingProfileView from '../../../support/fragments/settings/dataImport/fieldMappingProfile/fieldMappingProfileView';
+// import FieldMappingProfiles from '../../../support/fragments/settings/dataImport/fieldMappingProfile/fieldMappingProfiles';
+// import NewFieldMappingProfile from '../../../support/fragments/settings/dataImport/fieldMappingProfile/newFieldMappingProfile';
+// import MarcFieldProtection from '../../../support/fragments/settings/dataImport/marcFieldProtection';
+// import MatchProfiles from '../../../support/fragments/settings/dataImport/matchProfiles/matchProfiles';
+// import SettingsDataImport, {
+//   SETTINGS_TABS,
+// } from '../../../support/fragments/settings/dataImport/settingsDataImport';
+import TopMenu from '../../../support/fragments/topMenu';
+// import TopMenuNavigation from '../../../support/fragments/topMenuNavigation';
+// import Users from '../../../support/fragments/users/users';
+// import FileManager from '../../../support/utils/fileManager';
+// import GenerateIdentifierCode from '../../../support/utils/generateIdentifierCode';
+// import getRandomPostfix from '../../../support/utils/stringTools';
+
+describe('Data Import', () => {
+  describe('Importing MARC Bib files', () => {
+    let user;
+    // const randomIdentifierCode = GenerateIdentifierCode.getRandomIdentifierCode();
+    // const quantityOfItems = '1';
+    // const protectedFields = {
+    //   firstField: '020',
+    //   secondField: '514',
+    // };
+    // const protectedFieldIds = [];
+    // const filePathToUpload = 'marcBibFileForC380390.mrc';
+    // const jobProfileToRun = DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS;
+    // const fileNameForCreate = `C380390 autotestFile${getRandomPostfix()}.mrc`;
+    // const editedMarcFileName = `C380390 autotestFile${getRandomPostfix()}.mrc`;
+    // const fileNameForMatch = `C380390 autotestFile${getRandomPostfix()}.mrc`;
+    // const fileNameForUpdate = `C380390 autotestFile${getRandomPostfix()}.mrc`;
+    // const matchProfile = {
+    //   profileName: `C380390 ccn MARC match ${getRandomPostfix()}`,
+    //   incomingRecordFields: {
+    //     field: '035',
+    //     subfield: 'a',
+    //   },
+    //   qualifierType: 'Begins with',
+    //   qualifierValue: `${randomIdentifierCode}`,
+    //   existingRecordFields: {
+    //     field: '035',
+    //     subfield: 'a',
+    //   },
+    //   matchCriterion: 'Exactly matches',
+    //   existingRecordType: EXISTING_RECORD_NAMES.MARC_BIBLIOGRAPHIC,
+    // };
+    // const jobProfile = {
+    //   ...NewJobProfile.defaultJobProfile,
+    //   profileName: `C380390 Reject based on ccn.${getRandomPostfix()}`,
+    // };
+    // const mappingProfile = {
+    //   name: `C380390 New or update on ccn.${getRandomPostfix()}`,
+    //   typeValue: FOLIO_RECORD_TYPE.MARCBIBLIOGRAPHIC,
+    // };
+    // const actionProfile = {
+    //   name: `C380390 New or update on ccn.${getRandomPostfix()}`,
+    //   typeValue: FOLIO_RECORD_TYPE.MARCBIBLIOGRAPHIC,
+    //   action: ACTION_NAMES_IN_ACTION_PROFILE.UPDATE,
+    // };
+    // const jobProfileForUpdate = {
+    //   profileName: `C380390 New or update on ccn.${getRandomPostfix()}`,
+    //   acceptedType: ACCEPTED_DATA_TYPE_NAMES.MARC,
+    // };
+
+    before('Create test user and login', () => {
+      cy.createTempUser([
+        Permissions.settingsDataImportEnabled.gui,
+        Permissions.moduleDataImportEnabled.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+
+        cy.login(user.username, user.password, {
+          path: TopMenu.dataImportPath,
+          waiter: DataImport.waitLoading,
+        });
+        // MarcFieldProtection.createViaApi({
+        //   indicator1: '*',
+        //   indicator2: '*',
+        //   subfield: '*',
+        //   data: '*',
+        //   source: 'USER',
+        //   field: protectedFields.firstField,
+        // }).then((firstResp) => {
+        //   protectedFieldIds.push(firstResp.id);
+        // });
+        // MarcFieldProtection.createViaApi({
+        //   indicator1: '*',
+        //   indicator2: '*',
+        //   subfield: '*',
+        //   data: '*',
+        //   source: 'USER',
+        //   field: protectedFields.secondField,
+        // }).then((secondResp) => {
+        //   protectedFieldIds.push(secondResp.id);
+        // });
+      });
+    });
+
+    // after('Delete test data', () => {
+    //   // delete created files
+    //   FileManager.deleteFile(`cypress/fixtures/${editedMarcFileName}`);
+    //   cy.getAdminToken().then(() => {
+    //     SettingsJobProfiles.deleteJobProfileByNameViaApi(jobProfile.profileName);
+    //     SettingsJobProfiles.deleteJobProfileByNameViaApi(jobProfileForUpdate.profileName);
+    //     SettingsMatchProfiles.deleteMatchProfileByNameViaApi(matchProfile.profileName);
+    //     SettingsActionProfiles.deleteActionProfileByNameViaApi(actionProfile.name);
+    //     SettingsFieldMappingProfiles.deleteMappingProfileByNameViaApi(mappingProfile.name);
+    //     Users.deleteViaApi(user.userId);
+    //     protectedFieldIds.forEach((fieldId) => MarcFieldProtection.deleteViaApi(fieldId));
+    //     InventorySearchAndFilter.getInstancesByIdentifierViaApi(
+    //       `${randomIdentifierCode}00999523`,
+    //     ).then((instances) => {
+    //       if (instances) {
+    //         instances.forEach(({ id }) => {
+    //           InventoryInstance.deleteInstanceViaApi(id);
+    //         });
+    //       }
+    //     });
+    //   });
+    // });
+    // C347830
+    it(
+      'C651587 Check updating MARC Bib record using "Numeric only" comparison in match profile (folijet)',
+      { tags: ['criticalPath', 'folijet', 'C651587'] },
+      () => {
+        //     // change files for create instance using random identifier code
+        //     DataImport.editMarcFile(
+        //       filePathToUpload,
+        //       editedMarcFileName,
+        //       ['ccn'],
+        //       [randomIdentifierCode],
+        //     );
+        //     // upload a marc file
+        //     DataImport.verifyUploadState();
+        //     DataImport.uploadFile(editedMarcFileName, fileNameForCreate);
+        //     JobProfiles.waitFileIsUploaded();
+        //     JobProfiles.search(jobProfileToRun);
+        //     JobProfiles.runImportFile();
+        //     Logs.waitFileIsImported(fileNameForCreate);
+        //     Logs.checkJobStatus(fileNameForCreate, JOB_STATUS_NAMES.COMPLETED);
+        //     // create match profile
+        //     TopMenuNavigation.navigateToApp(APPLICATION_NAMES.SETTINGS, APPLICATION_NAMES.DATA_IMPORT);
+        //     SettingsDataImport.selectSettingsTab(SETTINGS_TABS.MATCH_PROFILES);
+        //     MatchProfiles.createMatchProfileWithQualifier(matchProfile);
+        //     MatchProfiles.checkMatchProfilePresented(matchProfile.profileName);
+        //     // create job profile
+        //     SettingsDataImport.selectSettingsTab(SETTINGS_TABS.JOB_PROFILES);
+        //     JobProfiles.createJobProfile(jobProfile);
+        //     NewJobProfile.linkMatchProfile(matchProfile.profileName);
+        //     NewJobProfile.linkActionProfileForNonMatches('Default - Create instance');
+        //     NewJobProfile.saveAndClose();
+        //     JobProfiles.checkJobProfilePresented(jobProfile.profileName);
+        //     // upload a marc file
+        //     TopMenuNavigation.navigateToApp(APPLICATION_NAMES.DATA_IMPORT);
+        //     DataImport.verifyUploadState();
+        //     DataImport.uploadFile(editedMarcFileName, fileNameForMatch);
+        //     JobProfiles.waitFileIsUploaded();
+        //     JobProfiles.search(jobProfile.profileName);
+        //     JobProfiles.runImportFile();
+        //     Logs.waitFileIsImported(fileNameForMatch);
+        //     Logs.checkJobStatus(fileNameForMatch, JOB_STATUS_NAMES.COMPLETED);
+        //     Logs.openFileDetails(fileNameForMatch);
+        //     FileDetails.checkSrsRecordQuantityInSummaryTable(quantityOfItems, 2);
+        //     FileDetails.checkInstanceQuantityInSummaryTable(quantityOfItems, 2);
+        //     [
+        //       FileDetails.columnNameInResultList.srsMarc,
+        //       FileDetails.columnNameInResultList.instance,
+        //     ].forEach((columnName) => {
+        //       FileDetails.checkStatusInColumn(RECORD_STATUSES.NO_ACTION, columnName);
+        //     });
+        //     // create mapping profile
+        //     TopMenuNavigation.navigateToApp(APPLICATION_NAMES.SETTINGS, APPLICATION_NAMES.DATA_IMPORT);
+        //     SettingsDataImport.selectSettingsTab(SETTINGS_TABS.FIELD_MAPPING_PROFILES);
+        //     FieldMappingProfiles.openNewMappingProfileForm();
+        //     NewFieldMappingProfile.fillMappingProfileForUpdatesMarc(mappingProfile);
+        //     NewFieldMappingProfile.markFieldForProtection(protectedFields.firstField);
+        //     NewFieldMappingProfile.markFieldForProtection(protectedFields.secondField);
+        //     NewFieldMappingProfile.save();
+        //     FieldMappingProfileView.closeViewMode(mappingProfile.name);
+        //     FieldMappingProfiles.checkMappingProfilePresented(mappingProfile.name);
+        //     // create action profile
+        //     SettingsDataImport.selectSettingsTab(SETTINGS_TABS.ACTION_PROFILES);
+        //     SettingsActionProfiles.create(actionProfile, mappingProfile.name);
+        //     SettingsActionProfiles.checkActionProfilePresented(actionProfile.name);
+        //     // create job profile
+        //     SettingsDataImport.selectSettingsTab(SETTINGS_TABS.JOB_PROFILES);
+        //     JobProfiles.createJobProfile(jobProfileForUpdate);
+        //     NewJobProfile.linkMatchAndActionProfiles(matchProfile.profileName, actionProfile.name);
+        //     NewJobProfile.linkActionProfileForNonMatches('Default - Create instance');
+        //     NewJobProfile.saveAndClose();
+        //     JobProfiles.checkJobProfilePresented(jobProfile.profileName);
+        //     // upload a marc file
+        //     TopMenuNavigation.navigateToApp(APPLICATION_NAMES.DATA_IMPORT);
+        //     FileDetails.close();
+        //     DataImport.verifyUploadState();
+        //     DataImport.uploadFile(editedMarcFileName, fileNameForUpdate);
+        //     JobProfiles.waitFileIsUploaded();
+        //     JobProfiles.search(jobProfileForUpdate.profileName);
+        //     JobProfiles.runImportFile();
+        //     Logs.waitFileIsImported(fileNameForUpdate);
+        //     Logs.checkJobStatus(fileNameForUpdate, JOB_STATUS_NAMES.COMPLETED);
+        //     Logs.openFileDetails(fileNameForUpdate);
+        //     [
+        //       FileDetails.columnNameInResultList.srsMarc,
+        //       FileDetails.columnNameInResultList.instance,
+        //     ].forEach((columnName) => {
+        //       FileDetails.checkStatusInColumn(RECORD_STATUSES.UPDATED, columnName);
+        //     });
+      },
+    );
+  });
+});
