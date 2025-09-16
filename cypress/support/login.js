e9b2032b267d5278a15d32161b7acd651e8cc4d6
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-09-16 14:11:29 +0500

    authRefresh parameter added to login command (#5677)
    * authRefresh parameter added to login command
    
    * login console.log removed

--- cypress/support/login.js
+++ cypress/support/login.js
@@ -16,48 +16,60 @@ Cypress.Commands.add(
   (
     username,
     password,
-    visitPath = { path: '/', waiter: () => cy.expect(Heading(including('Welcome')).exists()) },
+    {
+      path = '/',
+      waiter = () => cy.expect(Heading(including('Welcome')).exists()),
+      authRefresh = false,
+    } = {},
   ) => {
-    // We use a behind-the-scenes method of ensuring we are logged
-    // out, rather than using the UI, in accordance with the Best
-    // Practices guidance at
-    // https://docs.cypress.io/guides/references/best-practices.html#Organizing-Tests-Logging-In-Controlling-State
-    localforage.removeItem('okapiSess');
+    function loginFn() {
+      // We use a behind-the-scenes method of ensuring we are logged
+      // out, rather than using the UI, in accordance with the Best
+      // Practices guidance at
+      // https://docs.cypress.io/guides/references/best-practices.html#Organizing-Tests-Logging-In-Controlling-State
+      localforage.removeItem('okapiSess');
 
-    if (Cypress.env('eureka')) {
-      cy.logoutViaApi();
-      cy.clearCookies({ domain: null }).then(() => {
-        cy.visit(visitPath.path);
+      if (Cypress.env('eureka')) {
+        cy.logoutViaApi();
+        cy.clearCookies({ domain: null }).then(() => {
+          cy.visit(path);
 
-        cy.selectTenantIfDropdown();
+          cy.selectTenantIfDropdown();
 
+          cy.do([
+            TextInput('Username').fillIn(username),
+            TextInput('Password').fillIn(password),
+            Button({ name: 'login' }).click(),
+          ]);
+          waiter();
+        });
+      } else {
+        cy.visit(path);
+
+        // Todo: find the way to wrap interactor to cy chainable object
         cy.do([
-          TextInput('Username').fillIn(username),
-          TextInput('Password').fillIn(password),
-          Button({ name: 'login' }).click(),
+          TextField('Username').fillIn(username),
+          TextField('Password').fillIn(password),
+          Button('Log in').click(),
         ]);
-        visitPath.waiter();
-      });
-    } else {
-      cy.visit(visitPath.path);
+        // TODO: find the way how customize waiter timeout in case of interactors(cy.wrap may be)
+        // https://stackoverflow.com/questions/57464806/set-timeout-for-cypress-expect-assertion
+        // https://docs.cypress.io/api/commands/wrap#Requirements
 
-      // Todo: find the way to wrap interactor to cy chainable object
-      cy.do([
-        TextField('Username').fillIn(username),
-        TextField('Password').fillIn(password),
-        Button('Log in').click(),
-      ]);
-      // TODO: find the way how customize waiter timeout in case of interactors(cy.wrap may be)
-      // https://stackoverflow.com/questions/57464806/set-timeout-for-cypress-expect-assertion
-      // https://docs.cypress.io/api/commands/wrap#Requirements
-
-      visitPath.waiter();
-      // There seems to be a race condition here: sometimes there is
-      // re-render that happens so quickly that following actions like
-      //       cy.get('#app-list-item-clickable-courses-module').click()
-      // fail because the button becomes detached from the DOM after the
-      // get() but before the click().
+        waiter();
+        // There seems to be a race condition here: sometimes there is
+        // re-render that happens so quickly that following actions like
+        //       cy.get('#app-list-item-clickable-courses-module').click()
+        // fail because the button becomes detached from the DOM after the
+        // get() but before the click().
+      }
     }
+
+    if (authRefresh) {
+      cy.waitForAuthRefresh(() => {
+        loginFn();
+      }, 20_000);
+    } else loginFn();
   },
 );
 
