910cffbc1c14fc5016b538c1bd44e4cd081eae62
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-09-25 18:23:13 +0500

    C359240: adapted to Sunflower

--- cypress/e2e/marc/marc-bibliographic/derive-marc-bib/derive-bib-save-keep-editing.cy.js
+++ cypress/e2e/marc/marc-bibliographic/derive-marc-bib/derive-bib-save-keep-editing.cy.js
@@ -56,6 +56,7 @@ describe('MARC', () => {
             cy.login(userProperties.username, userProperties.password, {
               path: TopMenu.inventoryPath,
               waiter: InventoryInstances.waitContentLoading,
+              authRefresh: true,
             });
           });
         });
