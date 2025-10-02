46e7ff6303551bcdb1302cd164727b08ba7b5bf0
Yana Rusakova <yana_rusakova@epam.com>
2025-10-02 10:44:16 +0300

    FAT-20589 add C470

--- cypress/support/fragments/users/cancelFeeFine.js
+++ cypress/support/fragments/users/cancelFeeFine.js
@@ -1,6 +1,6 @@
 import { including } from '@interactors/html';
 
-import { Modal, TextArea, Button, Callout } from '../../../../interactors';
+import { Modal, TextArea, Button, Callout, HTML } from '../../../../interactors';
 
 const rootModal = Modal({ id: 'error-modal' });
 
@@ -17,10 +17,26 @@ export default {
     cy.expect(Callout(including('has been successfully cancelled as error for ')).exists());
   },
 
+  verifyCommentError: () => {
+    cy.do(rootModal.find(TextArea({ name: 'comment' })).fillIn(''));
+    cy.expect(rootModal.find(Button('Confirm')).has({ disabled: true }));
+    cy.expect(rootModal.find(HTML(including('Comment must be provided'))).exists());
+  },
+
   cancelFeeFineViaApi: (apiBody, feeFineId) => cy.okapiRequest({
     method: 'POST',
     path: `accounts/${feeFineId}/cancel`,
     body: apiBody,
     isDefaultSearchParamsRequired: false,
   }),
+
+  verifyModalContent: (amount, feeFineType) => {
+    cy.expect(rootModal.has({ header: 'Confirm fee/fine cancellation' }));
+    cy.expect(rootModal.find(HTML(including(amount))).exists());
+    cy.expect(rootModal.find(HTML(including(feeFineType))).exists());
+  },
+
+  clickBack: () => {
+    cy.do(rootModal.find(Button('Back')).click());
+  },
 };
