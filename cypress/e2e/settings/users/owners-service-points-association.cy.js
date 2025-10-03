9fde8048af1f180452e208325ce7226b48df806f
Dmytro-Melnyshyn <77053927+Dmytro-Melnyshyn@users.noreply.github.com>
2025-10-03 14:20:19 +0300

    FAT-21841-C503131-fix (#5795)

--- cypress/e2e/settings/users/owners-service-points-association.cy.js
+++ cypress/e2e/settings/users/owners-service-points-association.cy.js
@@ -2,15 +2,19 @@ import { Permissions } from '../../../support/dictionary';
 import UsersOwners from '../../../support/fragments/settings/users/usersOwners';
 import SettingsMenu from '../../../support/fragments/settingsMenu';
 import Users from '../../../support/fragments/users/users';
+import ServicePoints from '../../../support/fragments/settings/tenant/servicePoints/servicePoints';
 
 describe('Settings', () => {
   describe('Users', () => {
     const testData = {
       owners: [],
+      servicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
     };
 
     beforeEach('Create test data', () => {
       cy.getAdminToken().then(() => {
+        ServicePoints.createViaApi(testData.servicePoint);
+
         // Create 3 owners
         for (let i = 0; i < 3; i++) {
           UsersOwners.createViaApi(UsersOwners.getDefaultNewOwner()).then((owner) => {
@@ -39,6 +43,9 @@ describe('Settings', () => {
       if (testData.user?.userId) {
         Users.deleteViaApi(testData.user.userId);
       }
+      if (testData.servicePoint?.id) {
+        ServicePoints.deleteViaApi(testData.servicePoint.id);
+      }
     });
 
     it(
@@ -57,10 +64,10 @@ describe('Settings', () => {
         UsersOwners.verifySuccessfulCallout('updated');
 
         UsersOwners.startEditOwner(testData.owners[1].owner, { name: testData.owners[1].owner });
-        UsersOwners.selectMultipleServicePoints('Circ Desk 2');
+        UsersOwners.selectMultipleServicePoints(testData.servicePoint.name);
         UsersOwners.clickSaveBtn();
         UsersOwners.verifySuccessfulCallout('updated');
-        UsersOwners.verifyRemovedServicePoint(testData.owners[1].owner, 'Circ Desk 2');
+        UsersOwners.verifyRemovedServicePoint(testData.owners[1].owner, testData.servicePoint.name);
       },
     );
   });
