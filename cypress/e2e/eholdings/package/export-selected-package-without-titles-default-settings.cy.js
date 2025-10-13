7d5186bf2e12930fbe4991a1d73bcc91ae1ac885
Viktoriia Markus <viktoriia_markus@epam.com>
2025-10-13 17:06:40 +0300

    Implement: C356418 C356414 C356415 C356420

---
+++ cypress/e2e/eholdings/package/export-selected-package-without-titles-default-settings.cy.js
@@ -0,0 +1,98 @@
+import { Permissions } from '../../../support/dictionary';
+import ExportFile from '../../../support/fragments/data-export/exportFile';
+import EHoldingsPackageView from '../../../support/fragments/eholdings/eHoldingsPackageView';
+import EHoldingsPackages from '../../../support/fragments/eholdings/eHoldingsPackages';
+import EHoldingsPackagesSearch from '../../../support/fragments/eholdings/eHoldingsPackagesSearch';
+import EHoldingSearch from '../../../support/fragments/eholdings/eHoldingsSearch';
+import EHoldingsTitlesSearch from '../../../support/fragments/eholdings/eHoldingsTitlesSearch';
+import ExportSettingsModal from '../../../support/fragments/eholdings/modals/exportSettingsModal';
+import ExportManagerSearchPane from '../../../support/fragments/exportManager/exportManagerSearchPane';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import FileManager from '../../../support/utils/fileManager';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import TopMenuNavigation from '../../../support/fragments/topMenuNavigation';
+import {
+  APPLICATION_NAMES,
+  EHOLDINGS_PACKAGE_HEADERS,
+  EHOLDINGS_TITLE_HEADERS,
+} from '../../../support/constants';
+
+describe('eHoldings', () => {
+  describe('Package', () => {
+    const testData = {
+      searchQuery: 'A+ Lagomorph Journals',
+      selectedStatus: 'Selected',
+      titlesNumber: 0,
+      fileName: `C356414autoTestFile${getRandomPostfix()}.csv`,
+      fileMask: '*_package.csv',
+    };
+
+    before('Create user and login', () => {
+      cy.createTempUser([
+        Permissions.moduleeHoldingsEnabled.gui,
+        Permissions.uiAgreementsSearchAndView.gui,
+        Permissions.uiNotesItemView.gui,
+        Permissions.exportManagerAll.gui,
+      ]).then((userProperties) => {
+        testData.user = userProperties;
+        cy.login(userProperties.username, userProperties.password, {
+          path: TopMenu.eholdingsPath,
+          waiter: EHoldingsTitlesSearch.waitLoading,
+        });
+        EHoldingSearch.switchToPackages();
+      });
+    });
+
+    after('Delete user and artifacts', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(testData.user.userId);
+      FileManager.deleteFile(`cypress/fixtures/${testData.fileName}`);
+      FileManager.deleteFolder(Cypress.config('downloadsFolder'));
+    });
+
+    it(
+      'C356414 Export of selected "Package" without titles and with default settings (spitfire)',
+      { tags: ['extendedPath', 'spitfire', 'C356414'] },
+      () => {
+        EHoldingsPackagesSearch.byName(testData.searchQuery);
+        EHoldingsPackagesSearch.bySelectionStatus(testData.selectedStatus);
+        EHoldingsPackages.openPackageWithExpectedTitels(testData.titlesNumber);
+        EHoldingsPackageView.verifyPackageDetailViewIsOpened(
+          testData.searchQuery,
+          testData.titlesNumber,
+          testData.selectedStatus,
+        );
+
+        EHoldingsPackageView.openExportModal();
+
+        ExportSettingsModal.clickExportButton();
+
+        EHoldingsPackageView.getJobIDFromCalloutMessage().then((jobId) => {
+          TopMenuNavigation.navigateToApp(APPLICATION_NAMES.EXPORT_MANAGER);
+          ExportManagerSearchPane.searchByEHoldings();
+          ExportManagerSearchPane.verifyResult(jobId);
+          ExportManagerSearchPane.exportJobRecursively({ jobId });
+          ExportFile.downloadCSVFile(testData.fileName, testData.fileMask);
+
+          const expectedTokens = [
+            ...EHOLDINGS_PACKAGE_HEADERS,
+            ...EHOLDINGS_TITLE_HEADERS,
+            'Package Name',
+            'Package Id',
+            'Title Id',
+            'Title Name',
+            'Title Holdings Status',
+          ];
+
+          FileManager.verifyFile(
+            ExportManagerSearchPane.verifyExportedFileName,
+            testData.fileMask,
+            ExportManagerSearchPane.verifyContentOfExportFile,
+            expectedTokens,
+          );
+        });
+      },
+    );
+  });
+});
