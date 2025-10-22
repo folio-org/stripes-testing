ac61f5a77df4a4d095e4f018b6e0534ab56e9f4e
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2025-10-22 16:48:12 +0300

    Fat 10381 10382 10383 (#5836)
    * added tests C411716-C411721
    
    * added C411723
    
    * changed file
    
    * changed files
    
    * fixed test
    
    * deleted comment

--- scripts/get-automated-tests.js
+++ scripts/get-automated-tests.js
@@ -11,19 +11,17 @@ const testRailClient = createTestRailClient(testUsername, testPassword);
 const getTests = getTestRunResults.bind(null, testRailClient, runId);
 
 function printAutomatedTestsByTeams() {
-  getTests()
-    .then((tests) => {
-      console.log(`\nNumber of all tests in the #${runId} run: ${tests.length}\n`);
-      // custom_automation_type: 1 - Automated, 2 - Manual
-      const automatedTests = tests.filter((test) => test.custom_automation_type === 1);
-      console.log(`Number of automated tests in the #${runId} run: ${automatedTests.length}\n`);
-      // eslint-disable-next-line guard-for-in
-      for (const key in team) {
-        const count = automatedTests
-          .filter((test) => test.custom_dev_team === team[key]).length;
-        console.log(`${key}: ${count}`);
-      }
-    });
+  getTests().then((tests) => {
+    console.log(`\nNumber of all tests in the #${runId} run: ${tests.length}\n`);
+    // custom_automation_type: 1 - Automated, 2 - Manual
+    const automatedTests = tests.filter((test) => test.custom_automation_type === 1);
+    console.log(`Number of automated tests in the #${runId} run: ${automatedTests.length}\n`);
+    // eslint-disable-next-line guard-for-in
+    for (const key in team) {
+      const count = automatedTests.filter((test) => test.custom_dev_team === team[key]).length;
+      console.log(`${key}: ${count}`);
+    }
+  });
 }
 
 printAutomatedTestsByTeams();
