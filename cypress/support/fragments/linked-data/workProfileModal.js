7a0bb76f5b29615b526886fcd6b34a6d3aa54b7a
Kyrylo Brener <kbrener@ebsco.com>
2025-10-28 15:40:06 +0200

    updates for citation tests

--- cypress/support/fragments/linked-data/workProfileModal.js
+++ cypress/support/fragments/linked-data/workProfileModal.js
@@ -1,4 +1,17 @@
 export default {
+  waitLoading() {
+    cy.get('div[class="modal modal-choose-profile"]').should('be.visible');
+  },
+  checkOptionSelected(option) {
+    cy.get('div[class="modal modal-choose-profile"]').then((modal) => {
+      if (modal.is(':visible')) {
+        // verify selected option
+        cy.get('select#select-profile option:selected').should('have.text', option);
+      } else {
+        cy.log('Work profile modal is not displayed');
+      }
+    });
+  },
   selectDefaultOption() {
     cy.get('div[class="modal modal-choose-profile"]').then((modal) => {
       if (modal.is(':visible')) {
