ff53c82501ba197953a80c12e9db193c800ef6fc
Daria-Reznikova <daria_reznikova@epam.com>
2025-10-31 13:12:29 +0200

    FAT-22424: Implement C15854 (#6026)
    Co-authored-by: Ostap Voitsekhovskyi <Ostap_Voitsekhovskyi@epam.com>

---
+++ cypress/e2e/settings/orders/closing-purchase-order-reasons.cy.js
@@ -0,0 +1,41 @@
+import permissions from '../../../support/dictionary/permissions';
+import Users from '../../../support/fragments/users/users';
+import ClosingReasons from '../../../support/fragments/settings/orders/closingPurchaseOrderReasons';
+import settingsMenu from '../../../support/fragments/settingsMenu';
+
+describe('Settings (Orders) - Closing purchase order reasons', () => {
+  const { reason: closingReason } = { ...ClosingReasons.defaultClosingReason };
+  const closingReasonEdited = `${closingReason}-edited`;
+  let user;
+
+  before('Create user and login', () => {
+    cy.getAdminToken();
+    cy.createTempUser([permissions.uiSettingsOrdersCanViewAndEditAllSettings.gui]).then(
+      (userProps) => {
+        user = userProps;
+        cy.login(user.username, user.password, {
+          path: settingsMenu.ordersClosingPurchaseOrderReasonsPath,
+          waiter: ClosingReasons.waitLoading,
+        });
+      },
+    );
+  });
+
+  after('Delete test user', () => {
+    cy.getAdminToken().then(() => {
+      Users.deleteViaApi(user.userId);
+    });
+  });
+
+  it(
+    'C15854 Create, edit and delete closing purchase order reasons (thunderjet)',
+    { tags: ['extended', 'thunderjet', 'eurekaPhase1', 'C15854'] },
+    () => {
+      ClosingReasons.createClosingReason(closingReason);
+      ClosingReasons.editClosingReason(closingReason, closingReasonEdited);
+      ClosingReasons.deleteClosingReason(closingReasonEdited);
+      ClosingReasons.verifyClosingReasonAbsent(closingReasonEdited);
+      ClosingReasons.verifySystemClosingReasonNotEditable('Cancelled');
+    },
+  );
+});
