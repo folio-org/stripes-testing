d2debd6665d0ba6b60562fd5fe96d664d48b9011
Daria-Reznikova <daria_reznikova@epam.com>
2025-10-31 08:36:11 +0200

    Fat 22399 sunflower (#6013)
    * FAT-22399: Implement C347633 Sunflower

--- cypress/e2e/settings/orders/acquisition-method-create.cy.js
+++ cypress/e2e/settings/orders/acquisition-method-create.cy.js
@@ -3,13 +3,13 @@ import SettingsMenu from '../../../support/fragments/settingsMenu';
 
 describe('Orders', () => {
   describe('Settings (Orders)', () => {
-    const acquisitionMethod = { ...AcquisitionMethods.defaultAcquisitionMethod };
+    const { value: acquisitionMethodName } = { ...AcquisitionMethods.defaultAcquisitionMethod };
+    const newAcquisitionMethodName = `${acquisitionMethodName}-edited`;
 
     before(() => {
       cy.loginAsAdmin({
         path: SettingsMenu.acquisitionMethodsPath,
         waiter: AcquisitionMethods.waitLoading,
-        authRefresh: true,
       });
     });
 
@@ -18,10 +18,10 @@ describe('Orders', () => {
       { tags: ['criticalPath', 'thunderjet', 'eurekaPhase1'] },
       () => {
         AcquisitionMethods.newAcquisitionMethod();
-        AcquisitionMethods.fillAcquisitionMethodName(acquisitionMethod.value);
-        AcquisitionMethods.checkcreatedAM(acquisitionMethod.value);
-        AcquisitionMethods.editAcquisitionMethod(acquisitionMethod.value);
-        AcquisitionMethods.deleteAcquisitionMethod(acquisitionMethod.value);
+        AcquisitionMethods.fillAcquisitionMethodName(acquisitionMethodName);
+        AcquisitionMethods.checkcreatedAM(acquisitionMethodName);
+        AcquisitionMethods.editAcquisitionMethod(acquisitionMethodName, newAcquisitionMethodName);
+        AcquisitionMethods.deleteAcquisitionMethod(newAcquisitionMethodName);
       },
     );
   });
