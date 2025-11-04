fe685a7129180f667c7e3e8c4e71597c0dedfce5
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-11-04 16:14:20 +0500

    Sunflower FAT-22462 (#6042)
    * FAT-22462: tests added (#6040)
    
    * FAT-22462: describe block added

---
+++ cypress/e2e/eureka/consortia/login/login-url-redirect-consortia.cy.js
@@ -0,0 +1,73 @@
+import Users from '../../../../support/fragments/users/users';
+import TopMenu from '../../../../support/fragments/topMenu';
+import CapabilitiySets from '../../../../support/dictionary/capabilitySets';
+import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
+import CheckInActions from '../../../../support/fragments/check-in-actions/checkInActions';
+import Courses from '../../../../support/fragments/courses/courses';
+import MarcAuthorities from '../../../../support/fragments/marcAuthority/marcAuthorities';
+
+describe('Eureka', () => {
+  describe('Login', () => {
+    describe('Consortia', () => {
+      const capabSetsToAssign = [
+        CapabilitiySets.uiInventory,
+        CapabilitiySets.uiCheckin,
+        CapabilitiySets.uiCourses,
+        CapabilitiySets.uiMarcAuthoritiesAuthorityRecordView,
+      ];
+
+      let tempUser;
+
+      before('Create users, data', () => {
+        cy.getAdminToken();
+        cy.createTempUser([]).then((createdUserProperties) => {
+          tempUser = createdUserProperties;
+          cy.assignCapabilitiesToExistingUser(tempUser.userId, [], capabSetsToAssign);
+        });
+      });
+
+      after('Delete users, data', () => {
+        cy.resetTenant();
+        cy.getAdminToken();
+        Users.deleteViaApi(tempUser.userId);
+      });
+
+      it(
+        'C451520 Original URL restored after logging in to ECS environment (consortia) (eureka)',
+        { tags: ['smokeECS', 'eureka', 'C451520'] },
+        () => {
+          cy.login(tempUser.username, tempUser.password, {
+            path: TopMenu.inventoryPath,
+            waiter: InventoryInstances.waitContentLoading,
+            authRefresh: true,
+          });
+          cy.logout();
+
+          cy.login(tempUser.username, tempUser.password, {
+            path: TopMenu.checkInPath,
+            waiter: CheckInActions.verifyAccessDeniedModal,
+            authRefresh: true,
+          });
+          CheckInActions.closeAccessDeniedModal();
+          cy.logout();
+
+          cy.login(tempUser.username, tempUser.password, {
+            path: TopMenu.coursesPath,
+            waiter: Courses.waitLoading,
+            authRefresh: true,
+          });
+          cy.logout();
+
+          cy.login(tempUser.username, tempUser.password, {
+            path: TopMenu.marcAuthorities,
+            waiter: MarcAuthorities.waitLoading,
+            authRefresh: true,
+          });
+          cy.logout();
+
+          cy.login(tempUser.username, tempUser.password);
+        },
+      );
+    });
+  });
+});
