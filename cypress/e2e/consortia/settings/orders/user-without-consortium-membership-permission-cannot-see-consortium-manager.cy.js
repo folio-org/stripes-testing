e62b8f06cce83d53fc58dd1b8ac21497703b077d
Daria-Reznikova <daria_reznikova@epam.com>
2025-11-07 10:10:32 +0200

    Sunflower cherry-pick FAT-22429, FAT-22468, FAT-22439 (#6071)
    * FAT-22429, FAT-22468, FAT-22439 (#6055)
    
    * FAT-22439: cherry-pick Sunflower

---
+++ cypress/e2e/consortia/settings/orders/user-without-consortium-membership-permission-cannot-see-consortium-manager.cy.js
@@ -0,0 +1,47 @@
+import Permissions from '../../../../support/dictionary/permissions';
+import SettingsMenu from '../../../../support/fragments/settingsMenu';
+import TopMenu from '../../../../support/fragments/topMenu';
+import Users from '../../../../support/fragments/users/users';
+import settingsPane from '../../../../support/fragments/settings/settingsPane';
+
+describe('Consortia', () => {
+  describe('Settings', () => {
+    let user;
+
+    before('Create test data', () => {
+      cy.resetTenant();
+      cy.getAdminToken();
+      cy.createTempUser([
+        Permissions.uiSettingsOrdersCanViewAndEditAllSettings.gui,
+        Permissions.uiSettingsOrganizationsCanViewAndEditSettings.gui,
+        Permissions.settingsCircView.gui,
+        Permissions.uiSettingsAcquisitionUnitsViewEditCreateDelete.gui,
+        Permissions.uiSettingsFinanceViewEditCreateDeleter.gui,
+        Permissions.invoiceSettingsAll.gui,
+        Permissions.uiTenantSettingsSettingsLocation.gui,
+        Permissions.calendarEditCalendars.gui,
+      ]).then((userProperties) => {
+        user = userProperties;
+        cy.login(user.username, user.password, {
+          path: TopMenu.settingsPath,
+          waiter: settingsPane.waitSettingsPaneLoading,
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.resetTenant();
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(user.userId);
+      });
+    });
+
+    it(
+      'C380516 Users without "Settings (Consortia): Can manage consortium membership" permission can not see "Consortium manager" option in settings menu (ECS)',
+      { tags: ['criticalPathECS', 'consortia', 'thunderjet', 'C380516'] },
+      () => {
+        SettingsMenu.verifyConsortiumManagerOptionAbsent();
+      },
+    );
+  });
+});
