6728aa30abfe1184d0ff5cc36fbab900f7371fa5
Daria-Reznikova <daria_reznikova@epam.com>
2025-11-10 12:15:59 +0200

    FAT-22483, FAT-22440: Implemented C10947, updated C353609

--- cypress/support/fragments/settings/invoices/index.js
+++ cypress/support/fragments/settings/invoices/index.js
@@ -1,3 +1,4 @@
+export { default as Adjustments } from './adjustments';
 export { default as Approvals } from './approvals';
 export { default as BatchGroups } from './batchGroups';
 export { default as BatchGroupConfigurations } from './batchGroupConfigurations';
