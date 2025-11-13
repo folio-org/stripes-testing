fac78be3abde41a6587b3e8ea5b67165d1d6080f
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-11-13 11:40:12 +0500

    Spitfire minor fixes 11/12/25 (#6113)

--- cypress/e2e/marc/marc-authority/reporting-marc-authority/marc-autority-reporting-updated-heading.cy.js
+++ cypress/e2e/marc/marc-authority/reporting-marc-authority/marc-autority-reporting-updated-heading.cy.js
@@ -185,7 +185,7 @@ describe('MARC', () => {
             ExportManagerSearchPane.waitLoading();
             ExportManagerSearchPane.searchByAuthorityControl();
             ExportManagerSearchPane.verifyJobDataInResults(expectedJobData);
-            ExportManagerSearchPane.verifyResultAndClick(jobID);
+            ExportManagerSearchPane.openJobDetailView(jobID);
             ExportManagerSearchPane.verifyJobDataInDetailView(expectedJobDetails);
             ExportManagerSearchPane.downloadLastCreatedJob(item.response.body.name);
           });
