919b851c64b66afcba4309330aaab173c09537d0
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-11-18 13:47:25 +0500

    Sunflower FAT-22395-pt3 (#6141)
    * FAT-22395-pt3: tests added (#6011)
    
    * FAT-22395-pt3: tests adapted to Sunflower

---
+++ cypress/e2e/inventory/search-in-inventory/instance-advanced-search-not-supported-query-option.cy.js
@@ -0,0 +1,84 @@
+import { Permissions } from '../../../support/dictionary';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import InventoryInstances, {
+  searchInstancesOptions,
+} from '../../../support/fragments/inventory/inventoryInstances';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import { ADVANCED_SEARCH_MODIFIERS } from '../../../support/constants';
+import InteractorsTools from '../../../support/utils/interactorsTools';
+
+describe('Inventory', () => {
+  describe('Search in Inventory', () => {
+    const testData = {
+      user: {},
+      instanceTitle: `AT_C423707_FolioInstance_${getRandomPostfix()} The`,
+      initialSearchQuery: 'title all The',
+      newSearchQuery: 'The',
+      generatedAdvancedSearchQuery: 'keyword containsAll The',
+      advancedSearchOption: searchInstancesOptions.at(-1),
+      querySearchOption: searchInstancesOptions.at(-2),
+      keywordSearchOption: searchInstancesOptions[0],
+      advSearchModifier: ADVANCED_SEARCH_MODIFIERS.CONTAINS_ALL,
+    };
+
+    before('Create test data', () => {
+      cy.getAdminToken();
+
+      InventoryInstance.createInstanceViaApi({ instanceTitle: testData.instanceTitle }).then(
+        ({ instanceData }) => {
+          testData.instanceId = instanceData.instanceId;
+
+          cy.createTempUser([Permissions.inventoryAll.gui]).then((createdUserProperties) => {
+            testData.user = createdUserProperties;
+
+            cy.login(testData.user.username, testData.user.password, {
+              path: TopMenu.inventoryPath,
+              waiter: InventoryInstances.waitContentLoading,
+              authRefresh: true,
+            });
+            InventorySearchAndFilter.instanceTabIsDefault();
+          });
+        },
+      );
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(testData.user.userId);
+      InventoryInstance.deleteInstanceViaApi(testData.instanceId);
+    });
+
+    it(
+      'C423707 Instance | Using Advanced search with search query with not supported search option "Query search" in Inventory (spitfire)',
+      { tags: ['extendedPath', 'spitfire', 'C423707'] },
+      () => {
+        InventorySearchAndFilter.selectSearchOptions(
+          testData.querySearchOption,
+          testData.initialSearchQuery,
+        );
+        InventoryInstances.clickAdvSearchButton();
+        InventoryInstances.checkAdvSearchModalValues(
+          0,
+          testData.initialSearchQuery,
+          testData.advSearchModifier,
+          testData.keywordSearchOption,
+        );
+        InventoryInstances.fillAdvSearchRow(
+          0,
+          testData.newSearchQuery,
+          testData.advSearchModifier,
+          testData.keywordSearchOption,
+        );
+        InventoryInstances.clickSearchBtnInAdvSearchModal();
+        InventoryInstances.checkAdvSearchModalAbsence();
+        InventoryInstances.verifySelectedSearchOption(testData.advancedSearchOption);
+        InventorySearchAndFilter.checkSearchQueryText(testData.generatedAdvancedSearchQuery);
+        InventorySearchAndFilter.verifyResultListExists();
+        InteractorsTools.checkNoErrorCallouts();
+      },
+    );
+  });
+});
