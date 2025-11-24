3ac3744c6ab5208d5977658943b12d432bb687c2
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-11-24 12:59:37 +0500

    Sunflower FAT-22527-pt4 (#6163)
    * FAT-22527-pt4: tests added (#6161)
    
    * FAT-22527-pt4: tests adapted to Sunflower

---
+++ cypress/e2e/inventory/move-holdings-and-item/move-holdings-to-different-folio-instance-search-holdings-hrid.cy.js
@@ -0,0 +1,131 @@
+import InventoryInstances, {
+  searchHoldingsOptions,
+} from '../../../support/fragments/inventory/inventoryInstances';
+import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
+import TopMenu from '../../../support/fragments/topMenu';
+import { Permissions } from '../../../support/dictionary';
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import InventoryInstancesMovement from '../../../support/fragments/inventory/holdingsMove/inventoryInstancesMovement';
+import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
+import HoldingsRecordView from '../../../support/fragments/inventory/holdingsRecordView';
+
+describe('Inventory', () => {
+  describe('Move holdings and item', () => {
+    const randomPostfix = getRandomPostfix();
+    const instanceTitlePrefix = `AT_C366108_FolioInstance_${randomPostfix}`;
+    const holdingsHridOption = searchHoldingsOptions[7];
+    const instancesData = {
+      folioInstancesA: InventoryInstances.generateFolioInstances({
+        count: 1,
+        instanceTitlePrefix: `${instanceTitlePrefix} A`,
+        holdingsCount: 1,
+        itemsCount: 1,
+      }),
+      folioInstancesB: InventoryInstances.generateFolioInstances({
+        count: 1,
+        instanceTitlePrefix: `${instanceTitlePrefix} B`,
+        holdingsCount: 0,
+        itemsCount: 0,
+      }),
+    };
+
+    let user;
+    let location;
+    let holdingsHrid;
+    const instanceIds = [];
+
+    before('Create test data', () => {
+      cy.getAdminToken();
+      InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C366108');
+
+      cy.then(() => {
+        cy.getLocations({
+          limit: 1,
+          query: '(isActive=true and name<>"AT_*" and name<>"autotest*")',
+        }).then((res) => {
+          location = res;
+        });
+      })
+        .then(() => {
+          InventoryInstances.createFolioInstancesViaApi({
+            folioInstances: instancesData.folioInstancesA,
+            location,
+          });
+          InventoryInstances.createFolioInstancesViaApi({
+            folioInstances: instancesData.folioInstancesB,
+          });
+        })
+        .then(() => {
+          instanceIds.push(
+            instancesData.folioInstancesA[0].instanceId,
+            instancesData.folioInstancesB[0].instanceId,
+          );
+          cy.getHoldings({
+            limit: 1,
+            query: `"instanceId"="${instanceIds[0]}"`,
+          }).then((holdings) => {
+            holdingsHrid = holdings[0].hrid;
+          });
+
+          cy.createTempUser([
+            Permissions.inventoryAll.gui,
+            Permissions.uiInventoryMoveItems.gui,
+            Permissions.uiInventoryHoldingsMove.gui,
+          ]).then((userProperties) => {
+            user = userProperties;
+          });
+        });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      instanceIds.forEach((id) => {
+        InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(id);
+      });
+      Users.deleteViaApi(user.userId);
+    });
+
+    it(
+      'C366108 Verify that search by "Holdings HRID" for "Holdings" which moved from one "Instance" to another will return only one record. (spitfire)',
+      {
+        tags: ['extendedPath', 'spitfire', 'C366108'],
+      },
+      () => {
+        cy.login(user.username, user.password, {
+          path: TopMenu.inventoryPath,
+          waiter: InventoryInstances.waitContentLoading,
+          authRefresh: true,
+        });
+
+        InventoryInstances.searchByTitle(instanceIds[0]);
+        InventoryInstances.selectInstanceById(instanceIds[0]);
+        InventoryInstance.waitLoading();
+
+        InventoryInstance.moveHoldingsToAnotherInstanceByItemTitle(
+          location.name,
+          `${instanceTitlePrefix} B`,
+        );
+        InventoryInstancesMovement.checkHoldingsMoveSuccessCallout(1);
+        InventoryInstancesMovement.verifyHoldingsMoved(location.name, '1');
+        InventoryInstancesMovement.verifyHoldingsLocationInInstancePane(
+          location.name,
+          `${instanceTitlePrefix} B`,
+          true,
+        );
+
+        InventoryInstance.viewHoldings();
+        HoldingsRecordView.waitLoading();
+        HoldingsRecordView.checkHrId(holdingsHrid);
+
+        HoldingsRecordView.close();
+        InventoryInstance.waitLoading();
+
+        InventorySearchAndFilter.switchToHoldings();
+        InventorySearchAndFilter.searchByParameter(holdingsHridOption, holdingsHrid);
+        InventorySearchAndFilter.verifySearchResult(`${instanceTitlePrefix} B`);
+        InventorySearchAndFilter.checkRowsCount(1);
+      },
+    );
+  });
+});
--- cypress/support/fragments/settings/inventory/instance-holdings-item/statisticalCodes.js
+++ cypress/support/fragments/settings/inventory/instance-holdings-item/statisticalCodes.js
@@ -1,4 +1,14 @@
+import {
+  Pane,
+  Modal,
+  MultiColumnListHeader,
+  MultiColumnListRow,
+  MultiColumnListCell,
+  Button,
+  including,
+} from '../../../../../../interactors';
 import getRandomPostfix from '../../../../utils/stringTools';
+import InteractorsTools from '../../../../utils/interactorsTools';
 
 const defaultStatisticalCode = {
   source: 'local',
@@ -6,6 +16,26 @@ const defaultStatisticalCode = {
   name: `autotest_statistical_code_${getRandomPostfix()}`,
   statisticalCodeTypeId: '3abd6fc2-b3e4-4879-b1e1-78be41769fe3',
 };
+const rootPane = Pane('Statistical codes');
+const columnNames = [
+  'Statistical codes',
+  'Statistical code names',
+  'Statistical code types',
+  'Source',
+  'Last updated',
+  'Actions',
+];
+const ACTIONS = {
+  DELETE: 'trash',
+  EDIT: 'edit',
+};
+const deleteConfirmModal = Modal('Delete Statistical code');
+const deleteButton = Button('Delete');
+const deleteErrorModal = Modal('Cannot delete Statistical code');
+const deleteErrorModalMessage =
+  'This Statistical code cannot be deleted, as it is in use by one or more records.';
+const okayButton = Button('Okay');
+const deleteNotificationText = (code) => `The Statistical code ${code} was successfully deleted`;
 
 function getListOfStatisticalCodesNames() {
   const cells = [];
@@ -27,6 +57,13 @@ function getListOfStatisticalCodesNames() {
 }
 
 export default {
+  ACTIONS,
+  waitLoading() {
+    cy.expect(rootPane.exists());
+    columnNames.forEach((header) => {
+      cy.expect(rootPane.find(MultiColumnListHeader(header)).exists());
+    });
+  },
   getListOfStatisticalCodesNames,
   createViaApi(body = defaultStatisticalCode) {
     return cy
@@ -55,5 +92,42 @@ export default {
       const result = codesFromList.every((element2) => statusesFromInstance.some((element1) => element1.includes(element2)));
       expect(result).to.equal(true);
     });
+  },
+
+  clickActionIconForStatisticalCode(code, name, action) {
+    cy.do([
+      MultiColumnListRow({ content: including(`${code}${name}`), isContainer: false })
+        .find(MultiColumnListCell({ columnIndex: 5 }))
+        .find(Button({ icon: action }))
+        .click(),
+    ]);
+  },
+
+  confirmDeleteStatisticalCode() {
+    cy.do([deleteConfirmModal.find(deleteButton).click()]);
+    cy.expect(deleteConfirmModal.absent());
+  },
+
+  verifyDeleteErrorModal() {
+    cy.expect([
+      deleteErrorModal.exists(),
+      deleteErrorModal.has({ message: deleteErrorModalMessage }),
+      deleteErrorModal.find(okayButton).exists(),
+    ]);
+  },
+
+  dismissDeleteErrorModal() {
+    cy.do(deleteErrorModal.find(okayButton).click());
+    cy.expect(deleteErrorModal.absent());
+  },
+
+  verifyStatisticalCodeShown(code, name, isShown = true) {
+    const targetRow = MultiColumnListRow({ content: including(`${code}${name}`) });
+    if (isShown) cy.expect(targetRow.exists());
+    else cy.expect(targetRow.absent());
+  },
+
+  checkDeleteNotification(code) {
+    InteractorsTools.checkCalloutMessage(deleteNotificationText(code));
   },
 };
