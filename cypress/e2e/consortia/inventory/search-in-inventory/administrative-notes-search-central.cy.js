feae0d8ff139a0e34d108031889967d85cf81bd5
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-12-12 14:35:41 +0500

    FAT-22735-pt4: tests adapted to Sunflower

--- cypress/e2e/consortia/inventory/search-in-inventory/administrative-notes-search-central.cy.js
+++ cypress/e2e/consortia/inventory/search-in-inventory/administrative-notes-search-central.cy.js
@@ -160,6 +160,7 @@ describe('Inventory', () => {
             cy.login(user.username, user.password, {
               path: TopMenu.inventoryPath,
               waiter: InventoryInstances.waitContentLoading,
+              authRefresh: true,
             });
             ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
             InventorySearchAndFilter.selectSearchOption(notesSearchOption);
--- cypress/e2e/consortia/inventory/search-in-inventory/oclc-search-central.cy.js
+++ cypress/e2e/consortia/inventory/search-in-inventory/oclc-search-central.cy.js
@@ -176,6 +176,7 @@ describe('Inventory', () => {
             cy.login(user.username, user.password, {
               path: TopMenu.inventoryPath,
               waiter: InventoryInstances.waitContentLoading,
+              authRefresh: true,
             });
             ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
             InventorySearchAndFilter.selectSearchOption(searchOptions.identifierAll);
