3cdee42733a94e12b86711d5ff939f988ceabe42
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-12-12 14:10:11 +0500

    FAT-22735-pt5: C411635 added

---
+++ cypress/e2e/consortia/inventory/search-in-inventory/instance-hrid-search-central.cy.js
@@ -0,0 +1,197 @@
+import Permissions from '../../../../support/dictionary/permissions';
+import Affiliations, { tenantNames } from '../../../../support/dictionary/affiliations';
+import Users from '../../../../support/fragments/users/users';
+import TopMenu from '../../../../support/fragments/topMenu';
+import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
+import ConsortiumManager from '../../../../support/fragments/settings/consortium-manager/consortium-manager';
+import getRandomPostfix from '../../../../support/utils/stringTools';
+import InventorySearchAndFilter from '../../../../support/fragments/inventory/inventorySearchAndFilter';
+import { INSTANCE_SOURCE_NAMES } from '../../../../support/constants';
+import QuickMarcEditor from '../../../../support/fragments/quickMarcEditor';
+import InventoryHoldings from '../../../../support/fragments/inventory/holdings/inventoryHoldings';
+import NewLocation from '../../../../support/fragments/settings/tenant/locations/newLocation';
+import ServicePoints from '../../../../support/fragments/settings/tenant/servicePoints/servicePoints';
+
+describe('Inventory', () => {
+  describe('Search in Inventory', () => {
+    describe('Consortia', () => {
+      const randomPostfix = getRandomPostfix();
+      const instancePrefix = `AT_C411635_Instance_${randomPostfix}`;
+      const hridSearchOption = 'Instance HRID';
+      const instancesData = [
+        {
+          instanceSource: INSTANCE_SOURCE_NAMES.MARC,
+          affiliation: Affiliations.Consortia,
+          hasHoldings: false,
+        },
+        {
+          instanceSource: INSTANCE_SOURCE_NAMES.MARC,
+          affiliation: Affiliations.Consortia,
+          hasHoldings: true,
+        },
+        {
+          instanceSource: INSTANCE_SOURCE_NAMES.FOLIO,
+          affiliation: Affiliations.Consortia,
+          hasHoldings: false,
+        },
+        {
+          instanceSource: INSTANCE_SOURCE_NAMES.FOLIO,
+          affiliation: Affiliations.Consortia,
+          hasHoldings: true,
+        },
+        {
+          instanceSource: INSTANCE_SOURCE_NAMES.FOLIO,
+          affiliation: Affiliations.College,
+          hasHoldings: true,
+        },
+        {
+          instanceSource: INSTANCE_SOURCE_NAMES.MARC,
+          affiliation: Affiliations.College,
+          hasHoldings: true,
+        },
+      ];
+      const instanceTitles = Array.from(
+        { length: instancesData.length },
+        (_, i) => `${instancePrefix}_${i}`,
+      );
+      const sharedInstanceIndexes = instancesData
+        .map((item, index) => ({ item, index }))
+        .filter(({ item }) => item.affiliation === Affiliations.Consortia)
+        .map(({ index }) => index);
+      const localInstanceIndexes = instancesData
+        .map((item, index) => ({ item, index }))
+        .filter(({ item }) => item.affiliation !== Affiliations.Consortia)
+        .map(({ index }) => index);
+      let user;
+      let memberLocation;
+
+      before('Create user, data', () => {
+        cy.resetTenant();
+        cy.getAdminToken();
+        InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C411635');
+
+        cy.createTempUser([Permissions.uiInventoryViewInstances.gui])
+          .then((userProperties) => {
+            user = userProperties;
+
+            cy.setTenant(Affiliations.College);
+            InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C411635');
+          })
+          .then(() => {
+            cy.resetTenant();
+            cy.getInstanceTypes({ limit: 1, query: 'source=rdacontent' }).then((instanceTypes) => {
+              instancesData.forEach((instanceData, index) => {
+                cy.setTenant(instanceData.affiliation);
+
+                if (instanceData.instanceSource === INSTANCE_SOURCE_NAMES.FOLIO) {
+                  InventoryInstances.createFolioInstanceViaApi({
+                    instance: {
+                      instanceTypeId: instanceTypes[0].id,
+                      title: `${instanceTitles[index]}`,
+                    },
+                  }).then((createdInstanceData) => {
+                    instanceData.instanceId = createdInstanceData.instanceId;
+                  });
+                } else {
+                  const marcInstanceFields = [
+                    {
+                      tag: '008',
+                      content: QuickMarcEditor.defaultValid008Values,
+                    },
+                    {
+                      tag: '245',
+                      content: `$a ${instanceTitles[index]}`,
+                      indicators: ['1', '1'],
+                    },
+                  ];
+                  cy.createMarcBibliographicViaAPI(
+                    QuickMarcEditor.defaultValidLdr,
+                    marcInstanceFields,
+                  ).then((instanceId) => {
+                    instanceData.instanceId = instanceId;
+                  });
+                }
+              });
+            });
+          })
+          .then(() => {
+            cy.setTenant(Affiliations.College);
+            ServicePoints.getViaApi().then((servicePoint) => {
+              NewLocation.createViaApi(NewLocation.getDefaultLocation(servicePoint[0].id)).then(
+                (res) => {
+                  memberLocation = res;
+                },
+              );
+            });
+          })
+          .then(() => {
+            instancesData.forEach((instanceData) => {
+              cy.setTenant(instanceData.affiliation);
+              cy.getInstanceById(instanceData.instanceId).then((instance) => {
+                instanceData.instanceHrid = instance.hrid;
+
+                if (instanceData.hasHoldings) {
+                  cy.setTenant(Affiliations.College);
+                  InventoryHoldings.getHoldingsFolioSource().then((folioSource) => {
+                    InventoryHoldings.createHoldingRecordViaApi({
+                      instanceId: instanceData.instanceId,
+                      permanentLocationId: memberLocation.id,
+                      sourceId: folioSource.id,
+                    });
+                  });
+                }
+              });
+            });
+          })
+          .then(() => {
+            cy.resetTenant();
+            cy.login(user.username, user.password, {
+              path: TopMenu.inventoryPath,
+              waiter: InventoryInstances.waitContentLoading,
+            });
+            ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
+            InventorySearchAndFilter.selectSearchOption(hridSearchOption);
+          });
+      });
+
+      after('Delete user, data', () => {
+        cy.resetTenant();
+        cy.getAdminToken();
+        Users.deleteViaApi(user.userId);
+        InventoryInstances.deleteFullInstancesByTitleViaApi(instancePrefix);
+        cy.setTenant(Affiliations.College);
+        InventoryInstances.deleteFullInstancesByTitleViaApi(instancePrefix);
+        NewLocation.deleteInstitutionCampusLibraryLocationViaApi(
+          memberLocation.institutionId,
+          memberLocation.campusId,
+          memberLocation.libraryId,
+          memberLocation.id,
+        );
+      });
+
+      it(
+        'C411635 Search for Shared/Local records by "Instance HRID" search option from "Central" tenant (consortia) (spitfire)',
+        { tags: ['extendedPathECS', 'spitfire', 'C411635'] },
+        () => {
+          sharedInstanceIndexes.forEach((instanceIndex) => {
+            InventorySearchAndFilter.fillInSearchQuery(instancesData[instanceIndex].instanceHrid);
+            InventorySearchAndFilter.clickSearch();
+
+            InventorySearchAndFilter.verifySearchResult(instanceTitles[instanceIndex]);
+            InventorySearchAndFilter.checkRowsCount(1);
+          });
+
+          localInstanceIndexes.forEach((instanceIndex) => {
+            InventorySearchAndFilter.fillInSearchQuery(instancesData[instanceIndex].instanceHrid);
+            InventorySearchAndFilter.clickSearch();
+
+            InventorySearchAndFilter.verifyResultPaneEmpty({
+              noResultsFound: true,
+              searchQuery: instancesData[instanceIndex].instanceHrid,
+            });
+          });
+        },
+      );
+    });
+  });
+});
