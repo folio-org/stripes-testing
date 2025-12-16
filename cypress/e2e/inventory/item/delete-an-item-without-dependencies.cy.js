8011316cc3be62c205cb387e8e4e6538676a991f
Valery Pilko <Valery_Pilko@epam.com>
2025-12-16 20:58:07 +0400

    FAT-22806 added checks

--- cypress/e2e/inventory/item/delete-an-item-without-dependencies.cy.js
+++ cypress/e2e/inventory/item/delete-an-item-without-dependencies.cy.js
@@ -1,90 +1,104 @@
 import { ITEM_STATUS_NAMES } from '../../../support/constants';
+import { Permissions } from '../../../support/dictionary';
+import Users from '../../../support/fragments/users/users';
 import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
-import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
+import InventoryInstances, {
+  searchItemsOptions,
+} from '../../../support/fragments/inventory/inventoryInstances';
 import InventorySearchAndFilter from '../../../support/fragments/inventory/inventorySearchAndFilter';
 import ItemRecordView from '../../../support/fragments/inventory/item/itemRecordView';
 import ConfirmDeleteItemModal from '../../../support/fragments/inventory/modals/confirmDeleteItemModal';
-import Location from '../../../support/fragments/settings/tenant/locations/newLocation';
-import ServicePoints from '../../../support/fragments/settings/tenant/servicePoints/servicePoints';
 import TopMenu from '../../../support/fragments/topMenu';
 import generateItemBarcode from '../../../support/utils/generateItemBarcode';
 import getRandomPostfix from '../../../support/utils/stringTools';
 
 describe('Inventory', () => {
   describe('Item', () => {
-    const servicePoint = ServicePoints.getDefaultServicePointWithPickUpLocation();
-    const itemData = {
-      barcode: generateItemBarcode(),
-      instanceTitle: `C715 Instance${getRandomPostfix()}`,
+    const instanceTitlePrefix = `AT_C715_FolioInstance_${getRandomPostfix()}`;
+    const barcodeOption = searchItemsOptions[1];
+    const hridOption = searchItemsOptions[9];
+    const uuidOption = searchItemsOptions[10];
+    const testData = {
+      folioInstances: InventoryInstances.generateFolioInstances({
+        count: 1,
+        holdingsCount: 1,
+        itemsCount: 0,
+        instanceTitlePrefix,
+      }),
     };
-    let defaultLocation;
+
+    let user;
+    let location;
+    let materialType;
+    let loanType;
+    let holdingsRecordId;
+    let createdItem;
 
     before('Create test data and login', () => {
-      cy.getAdminToken()
-        .then(() => {
-          ServicePoints.createViaApi(servicePoint);
-          defaultLocation = Location.getDefaultLocation(servicePoint.id);
-          Location.createViaApi(defaultLocation);
-          cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
-            itemData.instanceTypeId = instanceTypes[0].id;
-          });
-          cy.getHoldingTypes({ limit: 1 }).then((res) => {
-            itemData.holdingTypeId = res[0].id;
-          });
-          cy.getLoanTypes({ limit: 1 }).then((res) => {
-            itemData.loanTypeId = res[0].id;
-          });
-          cy.getDefaultMaterialType().then((res) => {
-            itemData.materialTypeId = res.id;
-            itemData.materialTypeName = res.name;
-          });
-        })
-        .then(() => {
-          InventoryInstances.createFolioInstanceViaApi({
-            instance: {
-              instanceTypeId: itemData.instanceTypeId,
-              title: itemData.instanceTitle,
-            },
-            holdings: [
-              {
-                holdingsTypeId: itemData.holdingTypeId,
-                permanentLocationId: defaultLocation.id,
-              },
-            ],
-            items: [
-              {
-                barcode: itemData.barcode,
-                status: { name: ITEM_STATUS_NAMES.AVAILABLE },
-                permanentLoanType: { id: itemData.loanTypeId },
-                materialType: { id: itemData.materialTypeId },
-              },
-            ],
-          });
-        })
-        .then((specialInstanceIds) => {
-          itemData.testInstanceIds = specialInstanceIds;
-        })
-        .then(() => {
-          cy.loginAsAdmin({
-            path: TopMenu.inventoryPath,
-            waiter: InventoryInstances.waitContentLoading,
-          });
+      cy.getAdminToken();
+      InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C715_FolioInstance');
+
+      cy.then(() => {
+        cy.getLocations({
+          limit: 10,
+          query: '(isActive=true and name<>"AT_*") and name<>"autotest*"',
+        }).then(() => {
+          location = Cypress.env('locations')[0];
         });
+        cy.getLoanTypes({ limit: 1, query: 'name<>"AT_*"' }).then((loanTypes) => {
+          loanType = loanTypes[0];
+        });
+        cy.getMaterialTypes({ limit: 1, query: 'source=folio' }).then((res) => {
+          materialType = res;
+        });
+      })
+        .then(() => {
+          // Create instance with 1 holdings
+          InventoryInstances.createFolioInstancesViaApi({
+            folioInstances: testData.folioInstances,
+            location,
+          });
+          holdingsRecordId = testData.folioInstances[0].holdings[0].id;
+        })
+        .then(() => {
+          // Create item for holdings
+          return cy
+            .createItem({
+              barcode: generateItemBarcode(),
+              status: { name: ITEM_STATUS_NAMES.AVAILABLE },
+              holdingsRecordId,
+              materialType: { id: materialType.id },
+              permanentLoanType: { id: loanType.id },
+            })
+            .then((response) => {
+              const itemId = response.body.id;
+
+              return cy.getItems({ query: `id==${itemId}` }).then((itemData) => {
+                createdItem = itemData;
+              });
+            });
+        });
+
+      cy.createTempUser([Permissions.uiInventoryViewCreateEditDeleteItems.gui]).then(
+        (userProperties) => {
+          user = userProperties;
+
+          cy.waitForAuthRefresh(() => {
+            cy.login(user.username, user.password, {
+              path: TopMenu.inventoryPath,
+              waiter: InventoryInstances.waitContentLoading,
+            });
+          });
+        },
+      );
     });
 
     after('Delete test data', () => {
-      cy.getAdminToken().then(() => {
-        cy.deleteHoldingRecordViaApi(itemData.testInstanceIds.holdings[0].id);
-        InventoryInstance.deleteInstanceViaApi(itemData.testInstanceIds.instanceId);
-
-        Location.deleteInstitutionCampusLibraryLocationViaApi(
-          defaultLocation.institutionId,
-          defaultLocation.campusId,
-          defaultLocation.libraryId,
-          defaultLocation.id,
-        );
-        ServicePoints.deleteViaApi(servicePoint.id);
-      });
+      cy.getAdminToken();
+      InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(
+        testData.folioInstances[0].instanceId,
+      );
+      Users.deleteViaApi(user.userId);
     });
 
     it(
@@ -92,11 +106,20 @@ describe('Inventory', () => {
       { tags: ['extendedPath', 'folijet', 'C715', 'eurekaPhase1'] },
       () => {
         InventorySearchAndFilter.switchToItem();
-        InventorySearchAndFilter.searchByParameter('Barcode', itemData.barcode);
+        InventorySearchAndFilter.searchByParameter(barcodeOption, createdItem.barcode);
         ItemRecordView.waitLoading();
         ItemRecordView.clickDeleteButton();
         ConfirmDeleteItemModal.clickDeleteButton();
-        InventoryInstance.verifyItemBarcode(itemData.barcode, false);
+        InventoryInstance.verifyItemBarcode(createdItem.barcode, false);
+        InventorySearchAndFilter.resetAll();
+        InventorySearchAndFilter.searchByParameter(barcodeOption, createdItem.barcode);
+        InventorySearchAndFilter.verifyNoRecordsFound();
+        InventorySearchAndFilter.resetAll();
+        InventorySearchAndFilter.searchByParameter(hridOption, createdItem.hrid);
+        InventorySearchAndFilter.verifyNoRecordsFound();
+        InventorySearchAndFilter.resetAll();
+        InventorySearchAndFilter.searchByParameter(uuidOption, createdItem.id);
+        InventorySearchAndFilter.verifyNoRecordsFound();
       },
     );
   });
