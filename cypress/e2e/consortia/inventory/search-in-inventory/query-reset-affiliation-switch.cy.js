2fc6b39545eaad1aac8de6907513ea368ac7b2e6
Yauhen Viazau <Yauhen_Viazau@epam.com>
2025-12-19 18:00:48 +0500

    FAT-22781-pt3: tests adapted for Sunflower

--- cypress/e2e/consortia/inventory/search-in-inventory/query-reset-affiliation-switch.cy.js
+++ cypress/e2e/consortia/inventory/search-in-inventory/query-reset-affiliation-switch.cy.js
@@ -41,6 +41,7 @@ describe('Inventory', () => {
           cy.login(user.username, user.password, {
             path: TopMenu.inventoryPath,
             waiter: InventoryInstances.waitContentLoading,
+            authRefresh: true,
           });
           ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
           InventorySearchAndFilter.validateSearchTabIsDefault();
