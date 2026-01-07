84e65a3f9134df0a792a4330055700a59c9f5de6
Adeliona <adenosine382@gmail.com>
2026-01-07 16:09:00 +0200

    Added new tests C560 C469 C468 C451618

---
+++ cypress/e2e/checkout/checkout-loans-count-updates.cy.js
@@ -0,0 +1,105 @@
+import { Permissions } from '../../support/dictionary';
+import CheckInActions from '../../support/fragments/check-in-actions/checkInActions';
+import CheckOutActions from '../../support/fragments/check-out-actions/check-out-actions';
+import Checkout from '../../support/fragments/checkout/checkout';
+import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
+import Location from '../../support/fragments/settings/tenant/locations/newLocation';
+import ServicePoints from '../../support/fragments/settings/tenant/servicePoints/servicePoints';
+import TopMenu from '../../support/fragments/topMenu';
+import UserEdit from '../../support/fragments/users/userEdit';
+import Users from '../../support/fragments/users/users';
+
+describe('Check out', () => {
+  describe('Checkout loans count updates', () => {
+    const testData = {
+      folioInstances: InventoryInstances.generateFolioInstances({ count: 3 }),
+      servicePoint: ServicePoints.getDefaultServicePointWithPickUpLocation(),
+    };
+    let userData;
+
+    before('Create test data', () => {
+      cy.getAdminToken();
+      ServicePoints.createViaApi(testData.servicePoint);
+      testData.defaultLocation = Location.getDefaultLocation(testData.servicePoint.id);
+      Location.createViaApi(testData.defaultLocation).then((location) => {
+        InventoryInstances.createFolioInstancesViaApi({
+          folioInstances: testData.folioInstances,
+          location,
+        });
+      });
+
+      cy.createTempUser([
+        Permissions.checkoutCirculatingItems.gui,
+        Permissions.uiUsersViewLoans.gui,
+      ]).then((userProperties) => {
+        userData = userProperties;
+        userData.personal = {
+          lastname: userProperties.username,
+          firstName: userProperties.firstName,
+        };
+        UserEdit.addServicePointViaApi(
+          testData.servicePoint.id,
+          userData.userId,
+          testData.servicePoint.id,
+        );
+
+        cy.login(userData.username, userData.password, {
+          path: TopMenu.checkOutPath,
+          waiter: Checkout.waitLoading,
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      testData.folioInstances.forEach((instance) => {
+        CheckInActions.checkinItemViaApi({
+          itemBarcode: instance.barcodes[0],
+          servicePointId: testData.servicePoint.id,
+          checkInDate: new Date().toISOString(),
+        }).then(
+          () => {},
+          () => {},
+        );
+      });
+      testData.folioInstances.forEach((instance) => {
+        InventoryInstances.deleteInstanceViaApi({
+          instance,
+          servicePoint: testData.servicePoint,
+          shouldCheckIn: false,
+        });
+      });
+      Users.deleteViaApi(userData.userId);
+      ServicePoints.deleteViaApi(testData.servicePoint.id);
+      Location.deleteInstitutionCampusLibraryLocationViaApi(
+        testData.defaultLocation.institutionId,
+        testData.defaultLocation.campusId,
+        testData.defaultLocation.libraryId,
+        testData.defaultLocation.id,
+      );
+    });
+
+    it(
+      'C560 Find user with no open loans. Complete 3 checkouts for different items. Make sure that loans count updates properly after each checkout. (vega)',
+      { tags: ['smoke', 'vega', 'C560'] },
+      () => {
+        CheckOutActions.checkOutUser(userData.barcode);
+        CheckOutActions.waitForPatronSpinnerToDisappear();
+        CheckOutActions.checkUserInfo(userData);
+        CheckOutActions.verifyOpenLoansCount(0);
+
+        CheckOutActions.checkOutItem(testData.folioInstances[0].barcodes[0]);
+        CheckOutActions.verifyItemCheckedOut(testData.folioInstances[0].barcodes[0]);
+        CheckOutActions.verifyOpenLoansCount(1);
+
+        CheckOutActions.checkOutItem(testData.folioInstances[1].barcodes[0]);
+        CheckOutActions.verifyItemCheckedOut(testData.folioInstances[1].barcodes[0]);
+        CheckOutActions.verifyOpenLoansCount(2);
+
+        CheckOutActions.checkOutItem(testData.folioInstances[2].barcodes[0]);
+        CheckOutActions.verifyItemCheckedOut(testData.folioInstances[2].barcodes[0]);
+        CheckOutActions.verifyOpenLoansCount(3);
+      },
+    );
+  });
+});
