36179146de2cfb317fbed6bfc198001bfbccd152
Sviatlana Stsiapanava <sviatlana_stsiapanava@epam.com>
2026-01-08 13:39:49 +0300

    Add C375183 test case

--- cypress/e2e/oai-pmh/get-records/marc-and-folio-instances-retrieved-in-response.cy.js
+++ cypress/e2e/oai-pmh/get-records/marc-and-folio-instances-retrieved-in-response.cy.js
@@ -1,14 +1,21 @@
 import { Behavior } from '../../../support/fragments/settings/oai-pmh';
 import { BEHAVIOR_SETTINGS_OPTIONS_API } from '../../../support/fragments/settings/oai-pmh/behavior';
+import DataImport from '../../../support/fragments/data_import/dataImport';
+import getRandomPostfix from '../../../support/utils/stringTools';
+import { DEFAULT_JOB_PROFILE_NAMES } from '../../../support/constants';
 import InventoryInstance from '../../../support/fragments/inventory/inventoryInstance';
 import InventoryInstances from '../../../support/fragments/inventory/inventoryInstances';
 import OaiPmh from '../../../support/fragments/oai-pmh/oaiPmh';
-import DateTools from '../../../support/utils/dateTools';
-import getRandomPostfix from '../../../support/utils/stringTools';
 
+let marcInstanceId;
 let folioInstanceId;
+const marcFile = {
+  marc: 'marcBibFileForC375183.mrc',
+  fileNameImported: `testMarcFileC375183.${getRandomPostfix()}.mrc`,
+  jobProfileToRun: DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS,
+};
 const folioInstance = {
-  title: `AT_C375182_FolioInstance_${getRandomPostfix()}`,
+  title: `AT_C375183_FolioInstance_${getRandomPostfix()}`,
 };
 
 describe('OAI-PMH', () => {
@@ -17,10 +24,14 @@ describe('OAI-PMH', () => {
       cy.getAdminToken();
       Behavior.updateBehaviorConfigViaApi(
         BEHAVIOR_SETTINGS_OPTIONS_API.SUPPRESSED_RECORDS_PROCESSING.TRUE,
-        BEHAVIOR_SETTINGS_OPTIONS_API.RECORD_SOURCE.INVENTORY,
+        BEHAVIOR_SETTINGS_OPTIONS_API.RECORD_SOURCE.SOURCE_RECORD_STORAGE_AND_INVENTORY,
         BEHAVIOR_SETTINGS_OPTIONS_API.DELETED_RECORDS_SUPPORT.PERSISTENT,
         BEHAVIOR_SETTINGS_OPTIONS_API.ERRORS_PROCESSING.OK_200,
       );
+
+      DataImport.uploadFileViaApi(...Object.values(marcFile)).then((response) => {
+        marcInstanceId = response[0].instance.id;
+      });
 
       cy.getInstanceTypes({ limit: 1 }).then((instanceTypes) => {
         InventoryInstances.createFolioInstanceViaApi({
@@ -30,30 +41,45 @@ describe('OAI-PMH', () => {
           },
         }).then((createdInstanceData) => {
           folioInstanceId = createdInstanceData.instanceId;
-
-          cy.getInstanceById(folioInstanceId).then((instanceData) => {
-            folioInstance.hrid = instanceData.hrid;
-          });
         });
       });
     });
 
     after('delete test data', () => {
+      cy.getAdminToken();
+      InventoryInstance.deleteInstanceViaApi(marcInstanceId);
       InventoryInstance.deleteInstanceViaApi(folioInstanceId);
       Behavior.updateBehaviorConfigViaApi();
     });
 
     it(
-      'C375182 GetRecord: Verify that FOLIO instances are retrieved in response (marc21) (firebird)',
-      { tags: ['extendedPath', 'firebird', 'nonParallel', 'C375182'] },
+      'C375183 GetRecord: Verify that MARC + FOLIO instances are retrieved in responce (firebird)',
+      { tags: ['extendedPath', 'firebird', 'C375183', 'nonParallel'] },
       () => {
+        OaiPmh.getRecordRequest(marcInstanceId).then((response) => {
+          OaiPmh.verifyMarcField(
+            response,
+            marcInstanceId,
+            '999',
+            { ind1: 'f', ind2: 'f' },
+            { t: '0', i: marcInstanceId },
+          );
+          OaiPmh.verifyMarcField(
+            response,
+            marcInstanceId,
+            '245',
+            { ind1: '0', ind2: '4' },
+            { a: 'AT_C375183_MarcInstance' },
+          );
+        });
+
         OaiPmh.getRecordRequest(folioInstanceId).then((response) => {
-          OaiPmh.verifyMarcControlField(response, folioInstanceId, '001', folioInstance.hrid);
-          OaiPmh.verifyMarcControlField(
+          OaiPmh.verifyMarcField(
             response,
             folioInstanceId,
-            '008',
-            `${DateTools.getCurrentDateYYMMDD()}|||||||||||||||||       |||||und||`,
+            '999',
+            { ind1: 'f', ind2: 'f' },
+            { t: '0', i: folioInstanceId },
           );
           OaiPmh.verifyMarcField(
             response,
@@ -62,14 +88,6 @@ describe('OAI-PMH', () => {
             { ind1: '0', ind2: '0' },
             { a: folioInstance.title },
           );
-          OaiPmh.verifyMarcField(
-            response,
-            folioInstanceId,
-            '999',
-            { ind1: 'f', ind2: 'f' },
-            { i: folioInstanceId, t: '0' },
-          );
-          OaiPmh.verifyOaiPmhRecordHeader(response, folioInstanceId, false, true);
         });
       },
     );
