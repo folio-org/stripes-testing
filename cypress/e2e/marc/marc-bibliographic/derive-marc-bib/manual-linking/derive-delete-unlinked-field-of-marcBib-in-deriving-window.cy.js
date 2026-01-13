5f4c20f37a0bf62f227425d787bf0c5f3659e22a
Yauhen Viazau <Yauhen_Viazau@epam.com>
2026-01-13 13:44:24 +0500

    Spitfire fixes 1/13/2025

--- cypress/e2e/marc/marc-bibliographic/derive-marc-bib/manual-linking/derive-delete-unlinked-field-of-marcBib-in-deriving-window.cy.js
+++ cypress/e2e/marc/marc-bibliographic/derive-marc-bib/manual-linking/derive-delete-unlinked-field-of-marcBib-in-deriving-window.cy.js
@@ -137,14 +137,11 @@ describe('MARC', () => {
               QuickMarcEditor.checkAfterSaveAndClose();
             });
 
-            cy.waitForAuthRefresh(() => {
-              cy.login(testData.user.username, testData.user.password, {
-                path: TopMenu.inventoryPath,
-                waiter: InventoryInstances.waitContentLoading,
-              });
-              cy.reload();
-              InventoryInstances.waitContentLoading();
-            }, 20_000);
+            cy.login(testData.user.username, testData.user.password, {
+              path: TopMenu.inventoryPath,
+              waiter: InventoryInstances.waitContentLoading,
+              authRefresh: true,
+            });
           });
         });
 
