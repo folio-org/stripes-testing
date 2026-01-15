3820621b8d06f3a37ee5348a311022935c6ed664
Yauhen Viazau <Yauhen_Viazau@epam.com>
2026-01-15 12:46:42 +0500

    FAT-22963-pt3: tests adapted for Sunflower

--- cypress/e2e/consortia/inventory/search-in-inventory/filters/locations-facet-options-central.cy.js
+++ cypress/e2e/consortia/inventory/search-in-inventory/filters/locations-facet-options-central.cy.js
@@ -172,6 +172,7 @@ describe('Inventory', () => {
               cy.login(user.username, user.password, {
                 path: TopMenu.inventoryPath,
                 waiter: InventoryInstances.waitContentLoading,
+                authRefresh: true,
               });
               ConsortiumManager.checkCurrentTenantInTopMenu(tenantNames.central);
             });
