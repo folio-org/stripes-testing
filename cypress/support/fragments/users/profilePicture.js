bf494a9eb7f15fb9942ce1bde53ec6b113fe4b45
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2026-01-16 17:39:04 +0200

    add enableProfilePictures

---
+++ cypress/support/fragments/users/profilePicture.js
@@ -0,0 +1,13 @@
+/**
+ * Enables profile pictures functionality if it's currently disabled.
+ */
+export function enableProfilePictures() {
+  cy.getProfilePictureSetting().then((respBody) => {
+    const profilePictureConfig = respBody.settings[0];
+
+    if (!profilePictureConfig.value.enabled) {
+      profilePictureConfig.value.enabled = true;
+      cy.updateProfilePictureSetting(profilePictureConfig.id, profilePictureConfig);
+    }
+  });
+}
