40e37ddce2422baa4870b68376c818ee58c257c1
Sviatlana Stsiapanava <sviatlana_stsiapanava@epam.com>
2026-02-03 14:56:48 +0300

    Adapt C380470

---
+++ cypress/e2e/fse/dry-run/data-export/default-job-profile-sanity.cy.js
@@ -0,0 +1,32 @@
+import ExportJobProfiles from '../../../../support/fragments/data-export/exportJobProfile/exportJobProfiles';
+import ExportFieldMappingProfiles from '../../../../support/fragments/data-export/exportMappingProfile/exportFieldMappingProfiles';
+import SettingsPane from '../../../../support/fragments/settings/settingsPane';
+import TopMenu from '../../../../support/fragments/topMenu';
+import { parseSanityParameters } from '../../../../support/utils/users';
+
+const { user, memberTenant } = parseSanityParameters();
+
+describe('Data Export', () => {
+  before('login', () => {
+    cy.setTenant(memberTenant.id);
+    cy.getUserToken(user.username, user.password);
+
+    cy.allure().logCommandSteps(false);
+    cy.login(user.username, user.password, {
+      path: TopMenu.settingsPath,
+      waiter: SettingsPane.waitLoading,
+    });
+    cy.allure().logCommandSteps(true);
+  });
+
+  it(
+    'C380470 Verify that Default Data export profiles are present (firebird)',
+    { tags: ['dryRun', 'firebird', 'C380470'] },
+    () => {
+      ExportJobProfiles.goToJobProfilesTab();
+      ExportJobProfiles.verifyDefaultProfiles();
+      ExportFieldMappingProfiles.goToFieldMappingProfilesTab();
+      ExportFieldMappingProfiles.verifyDefaultProfiles();
+    },
+  );
+});
