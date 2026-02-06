2892fbff9f68378eb4e6574bec79208c90ce4e84
Yauhen Viazau <Yauhen_Viazau@epam.com>
2026-02-06 17:29:44 +0500

    FAT-23525-pt3: tests added (#6533)

--- cypress/support/fragments/orders/modals/selectInstanceModal.js
+++ cypress/support/fragments/orders/modals/selectInstanceModal.js
@@ -5,6 +5,7 @@ import {
   MultiColumnListCell,
   MultiColumnListHeader,
   MultiColumnListRow,
+  MultiSelectMenu,
   MultiSelectOption,
   TextField,
   including,
@@ -303,7 +304,7 @@ export default {
   },
 
   checkOptionsWithCountersExistInAccordion(accordionName) {
-    cy.do(Accordion(accordionName).find(MultiSelect()).open());
+    cy.do(selectInstanceModal.find(Accordion(accordionName)).find(MultiSelect()).open());
     cy.expect(MultiSelectOption({ text: matching(/.{1,}(\d{1,})/) }).exists());
   },
 
@@ -316,14 +317,10 @@ export default {
   },
 
   verifyMultiSelectFilterNumberOfOptions(accordionName, numberOfOptions) {
-    const multiSelect = Accordion(accordionName).find(MultiSelect());
+    const multiSelect = selectInstanceModal.find(Accordion(accordionName)).find(MultiSelect());
     cy.do(multiSelect.open());
     cy.wait(1000);
-    cy.get('[class^=multiSelectOption-]')
-      .filter(':visible')
-      .then((elements) => {
-        expect(elements.length).to.eq(numberOfOptions);
-      });
+    cy.expect(MultiSelectMenu({ optionCount: numberOfOptions }).exists());
   },
 
   selectMultiSelectFilterOption(accordionName, optionName) {
@@ -335,16 +332,12 @@ export default {
   },
 
   typeValueInMultiSelectFilterFieldAndCheck(accordionName, value, isFound = true, foundCount = 1) {
-    const multiSelect = Accordion(accordionName).find(MultiSelect());
+    const multiSelect = selectInstanceModal.find(Accordion(accordionName)).find(MultiSelect());
     cy.do(multiSelect.fillIn(value));
     cy.wait(1000);
     if (isFound) {
       cy.expect(MultiSelectOption(including(value)).exists());
-      cy.get('[class^=multiSelectOption-]')
-        .filter(':visible')
-        .then((elements) => {
-          expect(elements.length).to.eq(foundCount);
-        });
+      cy.expect(MultiSelectMenu({ optionCount: foundCount }).exists());
     } else cy.expect(MultiSelectOption(including(value)).absent());
   },
 
@@ -354,11 +347,27 @@ export default {
     fullValue,
     isFound = true,
   ) {
-    const multiSelect = Accordion(accordionName).find(MultiSelect());
+    const multiSelect = selectInstanceModal.find(Accordion(accordionName)).find(MultiSelect());
     cy.do(multiSelect.fillIn(notFullValue));
     cy.wait(1000);
     if (isFound) {
       cy.expect(MultiSelectOption(including(fullValue)).exists());
     } else cy.expect(MultiSelectOption(including(fullValue)).absent());
+  },
+
+  verifyMultiSelectFilterOptionCount(accordionName, optionName, expectedCount) {
+    const multiSelect = selectInstanceModal.find(Accordion(accordionName)).find(MultiSelect());
+    const escapedValue = optionName.replace(/[-.*+?^${}()|[\]\\]/g, '\\$&');
+    cy.do(multiSelect.open());
+    cy.expect(
+      MultiSelectOption(matching(new RegExp(`^${escapedValue}\\(\\d+\\)$`)), {
+        totalRecords: expectedCount,
+      }).exists(),
+    );
+  },
+
+  checkModalIncludesText(text) {
+    const value = text instanceof RegExp ? text : matching(text);
+    cy.expect(selectInstanceModal.find(HTML(value)).exists());
   },
 };
