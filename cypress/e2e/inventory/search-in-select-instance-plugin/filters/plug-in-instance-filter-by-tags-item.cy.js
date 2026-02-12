cc65979abc61541bc6863677eca98fd0f73c7014
Yauhen Viazau <Yauhen_Viazau@epam.com>
2026-02-12 12:06:29 +0500

    FAT-23653 pt1 (#6557)
    * FAT-23653-pt1: tests added
    
    * FAT-23653-pt1: C553057, C553058 added
    
    * FAT-23653-pt1: C476837 added

---
+++ cypress/e2e/inventory/search-in-select-instance-plugin/filters/plug-in-instance-filter-by-tags-item.cy.js
@@ -0,0 +1,220 @@
+import uuid from 'uuid';
+import Permissions from '../../../../support/dictionary/permissions';
+import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
+import InventorySearchAndFilter from '../../../../support/fragments/inventory/inventorySearchAndFilter';
+import TopMenu from '../../../../support/fragments/topMenu';
+import Users from '../../../../support/fragments/users/users';
+import getRandomPostfix, { getRandomLetters } from '../../../../support/utils/stringTools';
+import { NewOrder, Orders } from '../../../../support/fragments/orders';
+import { NewOrganization, Organizations } from '../../../../support/fragments/organizations';
+import OrderLineEditForm from '../../../../support/fragments/orders/orderLineEditForm';
+import OrderDetails from '../../../../support/fragments/orders/orderDetails';
+import SelectInstanceModal from '../../../../support/fragments/orders/modals/selectInstanceModal';
+import { ITEM_STATUS_NAMES } from '../../../../support/constants';
+
+describe('Inventory', () => {
+  describe('Search in "Select instance" plugin', () => {
+    describe('Filters', () => {
+      const numberOfRecords = 11;
+      const randomPostfix = getRandomPostfix();
+      const randomLetters = getRandomLetters(7);
+      const instanceTitlePrefix = `AT_C476837_FolioInstance_${randomPostfix}`;
+      const tagsAccordionName = 'Tags';
+      const tagPrefix = `at_c476837_tag_${randomLetters}`;
+      const organization = NewOrganization.getDefaultOrganization();
+      organization.name = `AT_C476837_Org_${randomPostfix}`;
+      const instanceTitles = Array.from(
+        { length: numberOfRecords },
+        (_, i) => `${instanceTitlePrefix}_${i}`,
+      );
+      const tagValues = Array.from({ length: numberOfRecords }, (_, i) => `${tagPrefix}_${i}`);
+
+      let instanceTypeId;
+      let order;
+      let user;
+      const createdTagIds = [];
+      let locationId;
+      let loanTypeId;
+      let materialTypeId;
+
+      before('Create users, data', () => {
+        cy.getAdminToken();
+
+        cy.then(() => {
+          InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C476837');
+
+          cy.getInstanceTypes({ limit: 1, query: 'source=rdacontent' }).then((types) => {
+            instanceTypeId = types[0].id;
+          });
+          cy.getLocations({ limit: 1, query: '(isActive=true and name<>"AT_*")' }).then((loc) => {
+            locationId = loc.id;
+          });
+          cy.getLoanTypes({ limit: 1, query: 'name<>"AT_*"' }).then((res) => {
+            loanTypeId = res[0].id;
+          });
+          cy.getMaterialTypes({ limit: 1, query: 'source=folio' }).then((matType) => {
+            materialTypeId = matType.id;
+          });
+          for (let i = 0; i < numberOfRecords; i++) {
+            cy.createTagApi({
+              id: uuid(),
+              description: tagValues[i],
+              label: tagValues[i],
+            }).then((tagId) => {
+              createdTagIds.push(tagId);
+            });
+          }
+          Organizations.createOrganizationViaApi(organization).then(() => {
+            const orderData = NewOrder.getDefaultOngoingOrder({
+              vendorId: organization.id,
+            });
+            Orders.createOrderViaApi(orderData).then((createdOrder) => {
+              order = createdOrder;
+            });
+          });
+        })
+          .then(() => {
+            for (let i = 0; i < numberOfRecords; i++) {
+              InventoryInstances.createFolioInstanceViaApi({
+                instance: {
+                  instanceTypeId,
+                  title: instanceTitles[i],
+                },
+                holdings: [
+                  {
+                    permanentLocationId: locationId,
+                  },
+                ],
+                items: [
+                  {
+                    status: { name: ITEM_STATUS_NAMES.AVAILABLE },
+                    permanentLoanType: { id: loanTypeId },
+                    materialType: { id: materialTypeId },
+                    tags: { tagList: [tagValues[i]] },
+                  },
+                ],
+              });
+            }
+          })
+          .then(() => {
+            cy.createTempUser([
+              Permissions.uiInventoryViewInstances.gui,
+              Permissions.uiOrdersCreate.gui,
+            ]).then((userProperties) => {
+              user = userProperties;
+
+              cy.login(user.username, user.password, {
+                path: TopMenu.ordersPath,
+                waiter: Orders.waitLoading,
+              });
+              Orders.selectOrderByPONumber(order.poNumber);
+              OrderDetails.selectAddPOLine();
+              OrderLineEditForm.clickTitleLookUpButton();
+              InventorySearchAndFilter.switchToItem();
+              InventorySearchAndFilter.itemTabIsDefault();
+            });
+          });
+      });
+
+      after('Delete test data', () => {
+        cy.getAdminToken();
+        InventoryInstances.deleteFullInstancesByTitleViaApi(instanceTitlePrefix);
+        Users.deleteViaApi(user.userId);
+        Organizations.deleteOrganizationViaApi(organization.id);
+        Orders.deleteOrderViaApi(order.id);
+        createdTagIds.forEach((tagId) => {
+          cy.deleteTagApi(tagId, true);
+        });
+      });
+
+      it(
+        'C476837 "Select Instance" plugin | Filter "Instance" records by "Tags" filter in "Item" segment (spitfire)',
+        { tags: ['extendedPath', 'spitfire', 'C476837'] },
+        () => {
+          InventorySearchAndFilter.verifyAccordionExistance(tagsAccordionName, true);
+          InventorySearchAndFilter.toggleAccordionByName(tagsAccordionName);
+          SelectInstanceModal.checkOptionsWithCountersExistInAccordion(tagsAccordionName);
+
+          SelectInstanceModal.searchByName(instanceTitlePrefix);
+          instanceTitles.forEach((title) => {
+            InventorySearchAndFilter.verifySearchResult(title);
+          });
+          InventorySearchAndFilter.verifyNumberOfSearchResults(instanceTitles.length);
+          tagValues.forEach((tagValue) => {
+            SelectInstanceModal.verifyOptionAvailableMultiselect(tagsAccordionName, tagValue);
+          });
+          SelectInstanceModal.verifyMultiSelectFilterNumberOfOptions(
+            tagsAccordionName,
+            tagValues.length,
+          );
+
+          SelectInstanceModal.selectMultiSelectFilterOption(tagsAccordionName, tagValues[0]);
+          InventorySearchAndFilter.verifyMultiSelectFilterOptionSelected(
+            tagsAccordionName,
+            tagValues[0],
+          );
+          SelectInstanceModal.verifyMultiSelectFilterOptionCount(
+            tagsAccordionName,
+            tagValues[0],
+            1,
+          );
+          InventorySearchAndFilter.verifyNumberOfSearchResults(1);
+          SelectInstanceModal.checkModalIncludesText(/^1 record found/);
+          InventorySearchAndFilter.verifySearchResult(instanceTitles[0]);
+
+          SelectInstanceModal.selectMultiSelectFilterOption(tagsAccordionName, tagValues[1]);
+          InventorySearchAndFilter.verifyMultiSelectFilterOptionSelected(
+            tagsAccordionName,
+            tagValues[0],
+          );
+          InventorySearchAndFilter.verifyMultiSelectFilterOptionSelected(
+            tagsAccordionName,
+            tagValues[1],
+          );
+          InventorySearchAndFilter.verifyNumberOfSearchResults(2);
+          InventorySearchAndFilter.verifySearchResult(instanceTitles[0]);
+          InventorySearchAndFilter.verifySearchResult(instanceTitles[1]);
+
+          SelectInstanceModal.selectMultiSelectFilterOption(tagsAccordionName, tagValues[0]);
+          InventorySearchAndFilter.verifyMultiSelectFilterOptionSelected(
+            tagsAccordionName,
+            tagValues[0],
+            false,
+          );
+          SelectInstanceModal.verifyMultiSelectFilterOptionCount(
+            tagsAccordionName,
+            tagValues[1],
+            1,
+          );
+          InventorySearchAndFilter.verifyNumberOfSearchResults(1);
+          SelectInstanceModal.checkModalIncludesText(/^1 record found/);
+          InventorySearchAndFilter.verifySearchResult(instanceTitles[1]);
+
+          InventorySearchAndFilter.clearFilter(tagsAccordionName);
+          InventorySearchAndFilter.verifyNumberOfSelectedOptionsInMultiSelectFilter(
+            tagsAccordionName,
+            0,
+          );
+          instanceTitles.forEach((title) => {
+            InventorySearchAndFilter.verifySearchResult(title);
+          });
+          InventorySearchAndFilter.verifyNumberOfSearchResults(instanceTitles.length);
+
+          SelectInstanceModal.typeValueInMultiSelectFilterFieldAndCheck(
+            tagsAccordionName,
+            tagValues[3],
+          );
+
+          SelectInstanceModal.selectMultiSelectFilterOption(tagsAccordionName, tagValues[3]);
+          SelectInstanceModal.typeNotFullValueInMultiSelectFilterFieldAndCheck(
+            tagsAccordionName,
+            '',
+            '',
+          );
+          InventorySearchAndFilter.verifyNumberOfSearchResults(1);
+          InventorySearchAndFilter.verifySearchResult(instanceTitles[3]);
+        },
+      );
+    });
+  });
+});
