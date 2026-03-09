7201b381c5e7c965020c18096b593d72ac9a0aa1
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2026-03-09 18:05:21 +0200

    added strong password

---
+++ cypress/support/utils/generateStrongPassword.js
@@ -0,0 +1,9 @@
+export default function getRandomStrongPassword(len = 25) {
+  const lettersUC = 'ABDEFGHKMNPQRSTWXZ!@#$%^&*()_+1234567980qwertyuiopasdfghjklzxcvbnm';
+  let str = '';
+  for (let i = 0; i < len; i++) {
+    const position = Math.floor(Math.random() * lettersUC.length);
+    str += lettersUC.substring(position, position + 1);
+  }
+  return str;
+}
