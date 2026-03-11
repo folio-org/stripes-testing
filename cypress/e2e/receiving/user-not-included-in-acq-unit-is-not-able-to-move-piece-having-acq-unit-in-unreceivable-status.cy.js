b8337820538aca47165cc399c1097b66407feb92
Nadia Ivashko <ivaskoned21@gmail.com>
2026-03-11 15:01:04 +0200

    Added test C436816

---
+++ cypress/e2e/receiving/user-not-included-in-acq-unit-is-not-able-to-move-piece-having-acq-unit-in-unreceivable-status.cy.js
@@ -0,0 +1,166 @@
+import uuid from 'uuid';
+import permissions from '../../support/dictionary/permissions';
+import NewOrder from '../../support/fragments/orders/newOrder';
+import OrderLines from '../../support/fragments/orders/orderLines';
+import Orders from '../../support/fragments/orders/orders';
+import NewOrganization from '../../support/fragments/organizations/newOrganization';
+import Organizations from '../../support/fragments/organizations/organizations';
+import Receiving from '../../support/fragments/receiving/receiving';
+import ReceivingDetails from '../../support/fragments/receiving/receivingDetails';
+import AcquisitionUnits from '../../support/fragments/settings/acquisitionUnits/acquisitionUnits';
+import TopMenu from '../../support/fragments/topMenu';
+import Users from '../../support/fragments/users/users';
+import BasicOrderLine from '../../support/fragments/orders/basicOrderLine';
+import InventoryInstances from '../../support/fragments/inventory/inventoryInstances';
+import { ACQUISITION_METHOD_NAMES_IN_PROFILE, ORDER_STATUSES } from '../../support/constants';
+import getRandomPostfix from '../../support/utils/stringTools';
+import EditPieceModal from '../../support/fragments/receiving/modals/editPieceModal';
+
+describe('Receiving', () => {
+  describe('Acquisition units', () => {
+    const organization = { ...NewOrganization.defaultUiOrganizations };
+    const randomPostfix = getRandomPostfix();
+    const instanceTitle = `C436816_Title_${randomPostfix}`;
+    const quantity = 1;
+    let order;
+    let orderLine;
+    let user;
+    let acquisitionUnit;
+    let location;
+
+    before('Create test data', () => {
+      cy.getAdminToken();
+
+      acquisitionUnit = AcquisitionUnits.getDefaultAcquisitionUnit({
+        protectRead: false,
+        protectUpdate: true,
+        protectCreate: false,
+        protectDelete: false,
+      });
+
+      AcquisitionUnits.createAcquisitionUnitViaApi(acquisitionUnit).then((auResponse) => {
+        acquisitionUnit.id = auResponse.id;
+
+        cy.createTempUser([
+          permissions.uiInventoryViewInstances.gui,
+          permissions.uiReceivingViewEditCreate.gui,
+        ]).then((userProperties) => {
+          user = userProperties;
+
+          InventoryInstances.getLocations({ limit: 1 }).then((locations) => {
+            location = locations[0];
+
+            Organizations.createOrganizationViaApi(organization).then((orgResponse) => {
+              organization.id = orgResponse;
+
+              cy.getDefaultMaterialType().then((materialType) => {
+                cy.getAcquisitionMethodsApi({
+                  query: `value="${ACQUISITION_METHOD_NAMES_IN_PROFILE.PURCHASE_AT_VENDOR_SYSTEM}"`,
+                }).then((acquisitionMethodResponse) => {
+                  const orderData = {
+                    ...NewOrder.getDefaultOrder({ vendorId: organization.id }),
+                    orderType: 'One-Time',
+                    approved: true,
+                  };
+
+                  Orders.createOrderViaApi(orderData).then((orderResponse) => {
+                    order = orderResponse;
+
+                    const orderLineData = {
+                      ...BasicOrderLine.defaultOrderLine,
+                      id: uuid(),
+                      purchaseOrderId: orderResponse.id,
+                      titleOrPackage: instanceTitle,
+                      checkinItems: false,
+                      cost: {
+                        listUnitPrice: 100.0,
+                        currency: 'USD',
+                        discountType: 'percentage',
+                        quantityPhysical: quantity,
+                        poLineEstimatedPrice: 100.0,
+                      },
+                      fundDistribution: [],
+                      locations: [
+                        {
+                          locationId: location.id,
+                          quantity,
+                          quantityPhysical: quantity,
+                        },
+                      ],
+                      acquisitionMethod: acquisitionMethodResponse.body.acquisitionMethods[0].id,
+                      physical: {
+                        createInventory: 'Instance, Holding, Item',
+                        materialType: materialType.id,
+                        materialSupplier: organization.id,
+                        volumes: [],
+                      },
+                    };
+
+                    OrderLines.createOrderLineViaApi(orderLineData).then((orderLineResponse) => {
+                      orderLine = orderLineResponse;
+
+                      Orders.updateOrderViaApi({
+                        ...orderResponse,
+                        workflowStatus: ORDER_STATUSES.OPEN,
+                      }).then(() => {
+                        cy.getAdminToken();
+                        Receiving.getTitleByPoLineIdViaApi(orderLine.id).then((titleData) => {
+                          Receiving.updateTitleViaApi({
+                            ...titleData,
+                            acqUnitIds: [acquisitionUnit.id],
+                          }).then(() => {
+                            cy.login(user.username, user.password, {
+                              path: TopMenu.receivingPath,
+                              waiter: Receiving.waitLoading,
+                            });
+                          });
+                        });
+                      });
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      Orders.deleteOrderViaApi(order.id);
+      Organizations.deleteOrganizationViaApi(organization.id);
+      Users.deleteViaApi(user.userId);
+      AcquisitionUnits.deleteAcquisitionUnitViaApi(acquisitionUnit.id);
+    });
+
+    it(
+      'C436816 User NOT included in Acq unit is not able to move a piece having Acq unit in "Unreceivable" status (thunderjet)',
+      { tags: ['extendedPath', 'thunderjet', 'C436816'] },
+      () => {
+        Receiving.searchByParameter({ parameter: 'Keyword', value: instanceTitle });
+        Receiving.selectFromResultsList(instanceTitle);
+        ReceivingDetails.waitLoading();
+        ReceivingDetails.verifyExpectedRecordsCount(1);
+
+        ReceivingDetails.expandTitleInformationAccordion();
+        ReceivingDetails.verifyAcquisitionUnitInTitleInformation(acquisitionUnit.name, true);
+
+        ReceivingDetails.openEditPieceModal({ row: 0, section: 'Expected' });
+        EditPieceModal.waitLoading();
+        EditPieceModal.verifySaveAndCloseButtonState({ disabled: true });
+
+        EditPieceModal.openActionsMenu();
+        EditPieceModal.verifyActionsMenuOptionsStates([
+          { option: 'Save and create another', disabled: true },
+          { option: 'Quick receive', disabled: true },
+          { option: 'Mark late', disabled: true },
+          { option: 'Send claim', disabled: true },
+          { option: 'Delay claim', disabled: true },
+          { option: 'Unreceivable', disabled: true },
+          { option: 'Delete', disabled: false },
+        ]);
+      },
+    );
+  });
+});
