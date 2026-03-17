afd75755e3df1f010c5ecfb601886172fceef696
Dmytro Melnyshyn <dmytro_melnyshyn@epam.com>
2026-03-17 14:50:56 +0200

    FAT-24013: C770456.

--- cypress/support/api/tenant.js
+++ cypress/support/api/tenant.js
@@ -68,6 +68,25 @@ Cypress.Commands.add('getTenantsApi', () => {
   });
 });
 
+Cypress.Commands.add('getTenantLocaleApi', () => {
+  return cy
+    .okapiRequest({
+      method: 'GET',
+      path: 'locale',
+      isDefaultSearchParamsRequired: false,
+    })
+    .then(({ body }) => body);
+});
+
+Cypress.Commands.add('setTenantLocaleApi', (localeBody) => {
+  return cy.okapiRequest({
+    method: 'PUT',
+    path: 'locale',
+    isDefaultSearchParamsRequired: false,
+    body: localeBody,
+  });
+});
+
 Cypress.Commands.add('setDefaultLocaleApi', () => {
   return cy.okapiRequest({
     method: 'PUT',
