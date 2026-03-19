f9a154e78c1183032d1187d839d9d7459e0a4a39
Valery Pilko <Valery_Pilko@epam.com>
2026-03-19 20:05:14 +0400

    FAT-24411 Implement C569546, C569553, C569533

---
+++ cypress/e2e/consortia/marc/marc-bibliographic/create-new-marcBib/create-marc-bib-with-valid-lccn-validation-enabled.cy.js
@@ -0,0 +1,79 @@
+import Permissions from '../../../../../support/dictionary/permissions';
+import InventoryInstance from '../../../../../support/fragments/inventory/inventoryInstance';
+import InventoryInstances from '../../../../../support/fragments/inventory/inventoryInstances';
+import QuickMarcEditor from '../../../../../support/fragments/quickMarcEditor';
+import TopMenu from '../../../../../support/fragments/topMenu';
+import Users from '../../../../../support/fragments/users/users';
+import getRandomPostfix from '../../../../../support/utils/stringTools';
+
+describe('MARC', () => {
+  describe('MARC Bibliographic', () => {
+    describe('Create MARC bib', () => {
+      describe('Consortia', () => {
+        const randomPostfix = getRandomPostfix();
+        const instanceTitle = `AT_C569553_SharedMarcBibInstance_${randomPostfix}`;
+        const lccnEntered = '$a 0000000340 $z t 003';
+        const lccnSaved = '$a   0000000340 $z t 003';
+        let user;
+
+        before('Create test data', () => {
+          cy.then(() => {
+            cy.resetTenant();
+            cy.getAdminToken();
+            InventoryInstances.deleteInstanceByTitleViaApi('AT_C569553');
+            cy.createTempUser([
+              Permissions.inventoryAll.gui,
+              Permissions.uiQuickMarcQuickMarcBibliographicEditorAll.gui,
+              Permissions.uiQuickMarcQuickMarcBibliographicEditorCreate.gui,
+            ]).then((createdUser) => {
+              user = createdUser;
+              InventoryInstances.toggleMarcBibLccnValidationRule({ enable: true });
+            });
+          }).then(() => {
+            cy.resetTenant();
+            cy.waitForAuthRefresh(() => {
+              cy.login(user.username, user.password, {
+                path: TopMenu.inventoryPath,
+                waiter: InventoryInstances.waitContentLoading,
+              });
+            }, 20_000);
+          });
+        });
+
+        after('Cleanup', () => {
+          cy.resetTenant();
+          cy.getAdminToken();
+          Users.deleteViaApi(user.userId);
+          InventoryInstances.deleteInstanceByTitleViaApi(instanceTitle);
+          InventoryInstances.toggleMarcBibLccnValidationRule({ enable: false });
+        });
+
+        it(
+          'C569553 Create a new MARC bib record with valid LCCN when "LCCN structure validation" is enabled (consortia) (spitfire)',
+          { tags: ['extendedPathECS', 'spitfire', 'nonParallel', 'C569553'] },
+          () => {
+            // Step 1: Click on "Actions" - "+ New MARC bibliographic record" option
+            InventoryInstance.newMarcBibRecord();
+
+            // Steps 2-3: Fill in LDR and 008 fields
+            QuickMarcEditor.updateLDR06And07Positions();
+
+            // Step 4: Fill in 245 field
+            QuickMarcEditor.updateExistingField('245', `$a ${instanceTitle}`);
+
+            // Step 5: Add "010" field with valid value in "$a" - 10 characters (digits with leading zero); fill "$z" with value in not valid format
+            QuickMarcEditor.addNewField('010', lccnEntered, 4);
+            QuickMarcEditor.checkContentByTag('010', lccnEntered);
+            QuickMarcEditor.pressSaveAndClose();
+            QuickMarcEditor.checkAfterSaveAndClose();
+
+            // Step 6: Click on the "Actions" button in the third pane >> Select "Edit MARC bibliographic record" option
+            InventoryInstance.editMarcBibliographicRecord();
+            QuickMarcEditor.waitLoading();
+            QuickMarcEditor.checkContentByTag('010', lccnSaved);
+          },
+        );
+      });
+    });
+  });
+});
