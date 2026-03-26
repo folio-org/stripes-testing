fb5f6c6a90fd569d6c312fb71b6ac727016f6f9e
Yauhen Viazau <Yauhen_Viazau@epam.com>
2026-03-26 14:49:50 +0500

    FAT-25455 pt1 (#6778)
    * FAT-25455-pt1: C957384, C808507 added
    
    * FAT-25455-pt1: tests added
    
    * duplicate test key issues resolved

--- cypress/e2e/inventory/item/re-order-items/ui-update-item-via-data-import-check-order.cy.js
+++ cypress/e2e/inventory/item/re-order-items/ui-update-item-via-data-import-check-order.cy.js
@@ -11,6 +11,7 @@ import {
   ActionProfiles as SettingsActionProfiles,
   FieldMappingProfiles as SettingsFieldMappingProfiles,
   JobProfiles as SettingsJobProfiles,
+  MatchProfiles as SettingsMatchProfiles,
 } from '../../../../support/fragments/settings/dataImport';
 import NewActionProfile from '../../../../support/fragments/settings/dataImport/actionProfiles/newActionProfile';
 import NewFieldMappingProfile from '../../../../support/fragments/settings/dataImport/fieldMappingProfile/newFieldMappingProfile';
@@ -20,55 +21,88 @@ import getRandomPostfix from '../../../../support/utils/stringTools';
 import ActionProfiles from '../../../../support/fragments/settings/dataImport/actionProfiles/actionProfiles';
 import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
 import InventoryItems from '../../../../support/fragments/inventory/item/inventoryItems';
+import NewMatchProfile from '../../../../support/fragments/settings/dataImport/matchProfiles/newMatchProfile';
 
 describe('Inventory', () => {
   describe('Item', () => {
     describe('Re-order item records', () => {
       let user;
-      const instanceTitle = 'AT_C808505_MarcBibInstance';
-      const filePath = 'marcBibC808505.mrc';
-      const nameMarcFileForImportCreate = `C808505 autotestFileForCreate${getRandomPostfix()}.mrc`;
+      const randomPostfix = getRandomPostfix();
+      const instanceTitle = 'AT_C808507_MarcBibInstance';
+      const filePath = 'marcBibFileC808507.mrc';
+      const nameMarcFileForImportCreate = `C808507 autotestFileForCreate_${randomPostfix}.mrc`;
+      const nameMarcFileForImportUpdate = `C808507 autotestFileForUpdate_${randomPostfix}.mrc`;
       const instanceActionProfileName = 'Default - Create instance';
+      const itemBarcode = 'c808507autotestitembarcode';
+      const newItemOrder = 3;
       // profiles for creating holdings, item
       const collectionOfProfilesForCreate = [
         {
           mappingProfile: {
-            name: `C808505 holdings mapping profile${getRandomPostfix()}`,
+            name: `C808507 holdings mapping profile_${randomPostfix}`,
             permanentLocation: null,
           },
           actionProfile: {
-            name: `C808505 holdings action profile${getRandomPostfix()}`,
+            name: `C808507 holdings action profile_${randomPostfix}`,
             action: 'CREATE',
             folioRecordType: EXISTING_RECORD_NAMES.HOLDINGS,
           },
         },
         {
           mappingProfile: {
-            name: `C808505 item mapping profile${getRandomPostfix()}`,
+            name: `C808507 item mapping profile_${randomPostfix}`,
             materialType: null,
             permanentLoanType: null,
             status: ITEM_STATUS_NAMES.AVAILABLE,
           },
           actionProfile: {
-            name: `C808505 item action profile${getRandomPostfix()}`,
+            name: `C808507 item action profile_${randomPostfix}`,
             action: 'CREATE',
             folioRecordType: EXISTING_RECORD_NAMES.ITEM,
           },
         },
       ];
-
+      // profiles for updating item
+      const collectionOfProfilesForUpdate = {
+        actionProfile: {
+          folioRecordType: EXISTING_RECORD_NAMES.ITEM,
+          name: `C808507 autotestActionItemUpdate_${randomPostfix}`,
+          action: 'UPDATE',
+        },
+        matchProfile: {
+          profileName: `C808507 autotestMatchItemUpdate_${randomPostfix}`,
+          incomingRecordFields: {
+            field: '949',
+            in1: ' ',
+            in2: ' ',
+            subfield: 'a',
+          },
+          recordType: EXISTING_RECORD_NAMES.MARC_BIBLIOGRAPHIC,
+          existingRecordType: EXISTING_RECORD_NAMES.ITEM,
+          existingMatchExpressionValue: 'item.barcode',
+        },
+      };
       const jobProfileForCreate = {
         ...NewJobProfile.defaultJobProfile,
-        profileName: `C808505 create job profile ${getRandomPostfix()}`,
+        profileName: `C808507 create job profile ${randomPostfix}`,
+        acceptedType: ACCEPTED_DATA_TYPE_NAMES.MARC,
+      };
+      const jobProfileForUpdate = {
+        ...NewJobProfile.defaultJobProfile,
+        profileName: `C808507 update job profile ${randomPostfix}`,
         acceptedType: ACCEPTED_DATA_TYPE_NAMES.MARC,
       };
 
       let instanceActionProfileId;
+      let mappingItemProfileId;
       let createdInstanceId;
       let createdHoldingsId;
+      let createdItemId;
 
       before('Create test data and login', () => {
         cy.getAdminToken();
+        InventoryInstances.deleteFullInstancesByTitleViaApi('C808507');
+
         cy.then(() => {
           cy.getLoanTypes({ limit: 1, query: 'name<>"AT_*"' }).then((res) => {
             collectionOfProfilesForCreate[1].mappingProfile.permanentLoanType = res[0].name;
@@ -103,9 +137,10 @@ describe('Inventory', () => {
               collectionOfProfilesForCreate[1].mappingProfile,
             )
               .then((mappingProfileResponse) => {
+                mappingItemProfileId = mappingProfileResponse.body.id;
                 NewActionProfile.createActionProfileViaApi(
                   collectionOfProfilesForCreate[1].actionProfile,
-                  mappingProfileResponse.body.id,
+                  mappingItemProfileId,
                 ).then((actionProfileResponse) => {
                   collectionOfProfilesForCreate[1].actionProfile.id = actionProfileResponse.body.id;
                 });
@@ -120,6 +155,22 @@ describe('Inventory', () => {
               });
           })
           .then(() => {
+            NewActionProfile.createActionProfileViaApi(
+              collectionOfProfilesForUpdate.actionProfile,
+              mappingItemProfileId,
+            ).then((actionProfileResponse) => {
+              NewMatchProfile.createMatchProfileWithIncomingAndExistingMatchExpressionViaApi(
+                collectionOfProfilesForUpdate.matchProfile,
+              ).then((matchProfileResponse) => {
+                NewJobProfile.createJobProfileWithLinkedMatchAndActionProfilesViaApi(
+                  jobProfileForUpdate.profileName,
+                  matchProfileResponse.body.id,
+                  actionProfileResponse.body.id,
+                );
+              });
+            });
+          })
+          .then(() => {
             cy.getAdminToken();
             cy.createTempUser([
               Permissions.moduleDataImportEnabled.gui,
@@ -129,6 +180,28 @@ describe('Inventory', () => {
             ]).then((userProperties) => {
               user = userProperties;
             });
+          })
+          .then(() => {
+            cy.getToken(user.username, user.password);
+
+            DataImport.uploadFileViaApi(
+              filePath,
+              nameMarcFileForImportCreate,
+              jobProfileForCreate.profileName,
+            ).then((response) => {
+              createdInstanceId = response[0].instance.id;
+              createdHoldingsId = response[0].holding.id;
+              createdItemId = response[0].item.id;
+
+              cy.getItems({ query: `id=="${createdItemId}"` }).then((item) => {
+                const updatedItemBody = {
+                  ...item,
+                  order: newItemOrder,
+                  barcode: itemBarcode,
+                };
+                InventoryItems.editItemViaApi(updatedItemBody);
+              });
+            });
           });
       });
 
@@ -137,6 +210,13 @@ describe('Inventory', () => {
           Users.deleteViaApi(user.userId);
           InventoryInstances.deleteFullInstancesByTitleViaApi(instanceTitle);
           SettingsJobProfiles.deleteJobProfileByNameViaApi(jobProfileForCreate.profileName);
+          SettingsJobProfiles.deleteJobProfileByNameViaApi(jobProfileForUpdate.profileName);
+          SettingsActionProfiles.deleteActionProfileByNameViaApi(
+            collectionOfProfilesForUpdate.actionProfile.name,
+          );
+          SettingsMatchProfiles.deleteMatchProfileByNameViaApi(
+            collectionOfProfilesForUpdate.matchProfile.profileName,
+          );
           collectionOfProfilesForCreate.forEach((profile) => {
             SettingsActionProfiles.deleteActionProfileByNameViaApi(profile.actionProfile.name);
             SettingsFieldMappingProfiles.deleteMappingProfileByNameViaApi(
@@ -147,34 +227,30 @@ describe('Inventory', () => {
       });
 
       it(
-        'C808505 Import "Item" record and check "order" field (spitfire)',
-        { tags: ['criticalPath', 'spitfire', 'C808505'] },
+        'C808507 Update "Item" record via "Data import" and check "order" field (spitfire)',
+        { tags: ['extendedPath', 'spitfire', 'C808507'] },
         () => {
-          cy.getToken(user.username, user.password);
-
           DataImport.uploadFileViaApi(
             filePath,
-            nameMarcFileForImportCreate,
-            jobProfileForCreate.profileName,
-          ).then((response) => {
-            createdInstanceId = response[0].instance.id;
-            createdHoldingsId = response[0].holding.id;
-
+            nameMarcFileForImportUpdate,
+            jobProfileForUpdate.profileName,
+          ).then(() => {
             cy.login(user.username, user.password, {
               path: TopMenu.inventoryPath,
               waiter: InventoryInstances.waitContentLoading,
-              authRefresh: true,
             });
 
             InventoryInstances.searchByTitle(createdInstanceId);
             InventoryInstances.selectInstanceById(createdInstanceId);
             InventoryInstance.waitLoading();
+            InventoryInstance.waitInstanceRecordViewOpened();
 
             InventoryInstance.openHoldingsAccordion('Holdings: ');
+            InventoryInstance.verifyItemBarcode(itemBarcode);
 
             InventoryItems.getItemsInHoldingsViaApi(createdHoldingsId).then((items) => {
-              expect(items).to.have.length(1);
-              expect(items[0].order).to.equal(1);
+              const orderValues = items.map((itm) => itm.order);
+              expect(orderValues).to.deep.equal([newItemOrder]);
             });
           });
         },
