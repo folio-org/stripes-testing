e1cbd038ad143b7ffd02f5cc5276e71b7955ea03
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2026-03-31 11:58:17 +0300

    updated test execution time

--- cypress.config.js
+++ cypress.config.js
@@ -12,7 +12,7 @@ const webpackPreprocessor = require('@cypress/webpack-batteries-included-preproc
 const testRailPlugin = require('cypress-testrail-simple/src/plugin');
 const flakyMarkerHandler = require('./scripts/report-portal/afterSpecHandler');
 
-let activeEnvironment = null;  // DO NOT SET ENV HERE, do it in ./environments.js file
+let activeEnvironment = null; // DO NOT SET ENV HERE, do it in ./environments.js file
 let environments = {};
 try {
   // eslint-disable-next-line global-require
