d3932ae4b004979029d3e8a42cc6c06e257ac767
Kyrylo Brener <kbrener@ebsco.com>
2026-04-01 14:35:01 +0300

    added more checks into sip2 configuration test

--- cypress/e2e/fse/api/configuration-api.cy.js
+++ cypress/e2e/fse/api/configuration-api.cy.js
@@ -17,8 +17,13 @@ describe('fse-configurations', { retries: { runMode: 1 } }, () => {
         isDefaultSearchParamsRequired: false,
       }).then((response) => {
         cy.expect(response.status).to.eq(200);
+        // check that sip2 configurations exist
         cy.expect(response.body.totalRecords).to.be.greaterThan(1);
         cy.expect(response.body).to.have.property('configs').that.is.an('array');
+        // check module in configuration entries
+        response.body.configs.forEach((entry) => {
+          cy.expect(entry.module).to.eq('edge-sip2');
+        });
       });
     },
   );
