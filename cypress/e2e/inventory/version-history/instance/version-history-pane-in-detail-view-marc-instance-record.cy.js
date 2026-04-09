30557f2c4aed036d2d9b1ce5f5f3da3ab5137111
Tetiana_Paranich <tetiana_paranich@epam.com>
2026-04-09 13:31:33 +0300

    added C651500

--- cypress/e2e/inventory/version-history/instance/version-history-pane-in-detail-view-marc-instance-record.cy.js
+++ cypress/e2e/inventory/version-history/instance/version-history-pane-in-detail-view-marc-instance-record.cy.js
@@ -1,45 +1,57 @@
-import { APPLICATION_NAMES } from '../../../../support/constants';
+import { APPLICATION_NAMES, DEFAULT_JOB_PROFILE_NAMES } from '../../../../support/constants';
 import CapabilitySets from '../../../../support/dictionary/capabilitySets';
+import DataImport from '../../../../support/fragments/data_import/dataImport';
 import InstanceRecordView from '../../../../support/fragments/inventory/instanceRecordView';
 import InventoryInstance from '../../../../support/fragments/inventory/inventoryInstance';
 import InventoryInstances from '../../../../support/fragments/inventory/inventoryInstances';
 import VersionHistorySection from '../../../../support/fragments/inventory/versionHistorySection';
 import TopMenuNavigation from '../../../../support/fragments/topMenuNavigation';
 import Users from '../../../../support/fragments/users/users';
-import DateTools from '../../../../support/utils/dateTools';
 import getRandomPostfix from '../../../../support/utils/stringTools';
 
 describe('Inventory', () => {
   describe('Version history', () => {
     describe('Instance', () => {
       const testData = {
-        resourceTitle: `C651477 autotestInstanceTitle${getRandomPostfix()}`,
-        date: DateTools.getFormattedDateWithSlashes({ date: new Date() }),
+        marcFilePath: 'oneMarcBib.mrc',
+        marcFileName: `C358136 autotestFileName${getRandomPostfix()}.mrc`,
+        jobProfileName: DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS,
       };
 
       before('Create test data', () => {
-        cy.createTempUser([]).then((userProperties) => {
-          testData.user = userProperties;
+        cy.getAdminToken();
+        DataImport.uploadFileViaApi(
+          testData.marcFilePath,
+          testData.marcFileName,
+          testData.jobProfileName,
+        ).then((response) => {
+          testData.instanceId = response[0].instance.id;
 
-          cy.assignCapabilitiesToExistingUser(
-            testData.user.userId,
-            [],
-            [CapabilitySets.uiInventory],
-          );
+          cy.createTempUser([]).then((userProperties) => {
+            testData.user = userProperties;
 
-          cy.getUserToken(testData.user.username, testData.user.password);
-          InventoryInstance.createInstanceViaApi().then(({ instanceData }) => {
-            testData.instance = instanceData;
+            cy.assignCapabilitiesToExistingUser(
+              testData.user.userId,
+              [],
+              [CapabilitySets.uiInventory],
+            );
 
-            cy.getInstanceById(testData.instance.instanceId).then((body) => {
-              body.title = testData.resourceTitle;
-              cy.updateInstance(body);
+            cy.getUserToken(testData.user.username, testData.user.password);
+            cy.getStatisticalCodes({
+              limit: 1,
+              query: 'code=="rmusic"',
+            }).then((code) => {
+              cy.getInstanceById(testData.instanceId).then((body) => {
+                body.statisticalCodeIds = [code[0].id];
+
+                cy.updateInstance(body);
+              });
             });
 
             cy.login(testData.user.username, testData.user.password);
             TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVENTORY);
             InventoryInstances.waitContentLoading();
-            InventoryInstances.searchByTitle(testData.instance.instanceId);
+            InventoryInstances.searchByTitle(testData.instanceId);
             InventoryInstances.selectInstance();
             InstanceRecordView.verifyInstanceRecordViewOpened();
           });
@@ -48,35 +60,32 @@ describe('Inventory', () => {
 
       after('Delete test data', () => {
         cy.getAdminToken();
-        InventoryInstance.deleteInstanceViaApi(testData.instance.instanceId);
+        InventoryInstance.deleteInstanceViaApi(testData.instanceId);
         Users.deleteViaApi(testData.user.userId);
       });
 
       it(
-        'C651477 Check "Version History" pane in the detail view of FOLIO Instance record (folijet)',
-        { tags: ['criticalPath', 'folijet', 'C651477'] },
+        'C651500 Check "Version History" pane in the detail view of MARC Instance record (folijet)',
+        { tags: ['criticalPath', 'folijet', 'C651500'] },
         () => {
-          const currentCardIndex = 0;
-          const numberOfVersions = 2;
-
+          InstanceRecordView.checkButtonsStateWhenVersionHistoryPaneClosed();
           InstanceRecordView.checkVersionHistoryButtonToolTipText();
           InstanceRecordView.clickVersionHistoryButton();
           InstanceRecordView.checkButtonsStateWhenVersionHistoryPaneIsOpen();
-          VersionHistorySection.verifyVersionHistoryPane(numberOfVersions);
           VersionHistorySection.verifyCurrentVersionCard({
             index: 0,
             firstName: testData.user.firstName,
             lastName: testData.user.lastName,
             isCurrent: true,
-            changes: ['Title (Edited)'],
+            changes: ['Statistical codes (Added)'],
           });
-          VersionHistorySection.openChangesForCard(currentCardIndex);
+          VersionHistorySection.openChangesForCard();
           VersionHistorySection.verifyChangesModal();
           VersionHistorySection.checkChangeInModal(
-            VersionHistorySection.fieldActions.EDITED,
-            'Title',
-            testData.instance.instanceTitle,
-            testData.resourceTitle,
+            VersionHistorySection.fieldActions.ADDED,
+            'Statistical codes',
+            'No value set-',
+            'Music sound recordings',
           );
           VersionHistorySection.closeChangesModal();
           VersionHistorySection.clickCloseButton();
