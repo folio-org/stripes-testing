852a906722992deee8d201b2ca26c81780971a24
Yauhen Viazau <Yauhen_Viazau@epam.com>
2026-04-10 11:56:16 +0500

    FAT-25492-pt2: tests added (#6871)

--- cypress/support/fragments/marcAuthority/marcAuthorities.js
+++ cypress/support/fragments/marcAuthority/marcAuthorities.js
@@ -1287,6 +1287,11 @@ export default {
     cy.do(thesaurusAccordion.clickHeader());
   },
 
+  verifyTypeOfHeadingAccordionAndClick: () => {
+    cy.expect(headingTypeAccordion.exists());
+    cy.do(headingTypeAccordion.clickHeader());
+  },
+
   chooseThesaurus: (thesaurusTypes) => {
     cy.do(
       MultiSelect({ ariaLabelledby: 'subjectHeadings-multiselect-label' }).select([
@@ -1727,7 +1732,10 @@ export default {
   },
 
   checkPreviousAndNextPaginationButtonsShown() {
-    cy.expect([previousButton.visible(), nextButton.visible()]);
+    cy.expect([
+      previousButton.has({ disabled: or(false, true) }),
+      nextButton.has({ disabled: or(false, true) }),
+    ]);
   },
 
   checkNextPaginationButtonEnabled(isEnabled = true) {
@@ -1754,7 +1762,7 @@ export default {
   verifyAllResultsHaveSource(sourceNames) {
     this.getResultsListByColumn(4).then((cellTexts) => {
       cellTexts.forEach((cellText) => {
-        expect(cellText).to.be.oneOf([...sourceNames]);
+        expect(cellText).to.be.oneOf(['', ...sourceNames]);
       });
     });
   },
