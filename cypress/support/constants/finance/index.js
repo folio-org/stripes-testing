f549afb2fd0ddd0cefe24310c4216cbc71527a75
Yury Saukou <yury_saukou@epam.com>
2026-04-14 17:37:16 +0400

    Split ACQ-related constants to separate files based on domain

---
+++ cypress/support/constants/finance/index.js
@@ -0,0 +1,7 @@
+export * from './budget';
+export * from './expense-class';
+export * from './fiscal-year';
+export * from './fund';
+export * from './ledger';
+export * from './rollover';
+export * from './transaction';
