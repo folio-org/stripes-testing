51d9eb7bd8e727be4f5e933c8aa4960a4db8428d
Yury Saukou <yury_saukou@epam.com>
2026-05-06 14:52:20 +0400

    FAT-24732 Implement e2e for C494012 (#7052)

--- cypress/support/constants/receiving/index.js
+++ cypress/support/constants/receiving/index.js
@@ -1,9 +1,2 @@
-export const RECEIVING_TITLE_ACCORDION_NAMES = {
-  TITLE_INFORMATION: 'Title information',
-  POL_DETAILS: 'POL details',
-  EXPECTED: 'Expected',
-  RECEIVED: 'Received',
-  ROUTING_LISTS: 'Routing lists',
-  UNRECEIVABLE: 'Unreceivable',
-  BOUND_ITEMS: 'Bound items',
-};
+export * from './piece';
+export * from './title';
