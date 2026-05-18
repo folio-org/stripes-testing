84a16a230c05fba2a231da16c1a34d56fc5c2d28
Yury Saukou <yury_saukou@epam.com>
2026-05-18 11:57:39 +0400

    FAT-24635 Implement e2e for C1332520 (#7136)
    * FAT-24635 Implement e2e for C1332520
    
    * resolve comments

--- cypress/support/utils/acquisitions.js
+++ cypress/support/utils/acquisitions.js
@@ -1,17 +1,27 @@
-import { FUND_DISTRIBUTION_TYPES } from '../constants';
+import { FUND_DISTRIBUTION_TYPES, INVALID_REFERENCE_MESSAGE } from '../constants';
 
-export const formatDateTime = (locale, date) => {
+export const formatIntlDateTime = (locale, date, config = {}) => {
   if (!date) return '';
 
   return new Intl.DateTimeFormat(locale.locale, {
     timeZone: locale.timezone,
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
+    ...config,
   }).format(new Date(date));
 };
+
+export const formatDateTime = (locale, date) => formatIntlDateTime(locale, date, {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+});
+
+export const formatDate = (locale, date) => formatIntlDateTime(locale, date, {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+});
 
 export function getMoneyMultiplier(currency) {
   const numberFormat = new Intl.NumberFormat(undefined, { style: 'currency', currency });
@@ -20,17 +30,48 @@ export function getMoneyMultiplier(currency) {
   return 10 ** maximumFractionDigits;
 }
 
-export const calculateFundAmount = (fundDistr, totalAmount, currency) => {
-  const fundDistributionValue = Number(fundDistr?.value || 0);
-  const fundDistributionType = fundDistr?.distributionType || FUND_DISTRIBUTION_TYPES.PERCENTAGE;
+export const calculateFundAmount = (distributionType, distributionValue, totalAmount, currency) => {
   const multiplier = getMoneyMultiplier(currency);
 
   const amount =
-    fundDistributionType === FUND_DISTRIBUTION_TYPES.PERCENTAGE
-      ? Math.round(fundDistributionValue * totalAmount * 100) / 10000
-      : fundDistributionValue;
+    distributionType.toLowerCase() === FUND_DISTRIBUTION_TYPES.PERCENTAGE.toLowerCase()
+      ? Math.round(distributionValue * totalAmount * 100) / 10000
+      : distributionValue;
 
   return Math.round(amount * multiplier) / multiplier;
 };
 
 export const chunk = (array, size) => array.reduce((acc, _, i) => (i % size ? acc : [...acc, array.slice(i, i + size)]), []);
+
+export const getExportFundDistributionFieldValue = (
+  fundDistribution,
+  currency,
+  totalAmount = 0,
+  expenseClassMap = new Map(),
+  invalidReferenceLabel = INVALID_REFERENCE_MESSAGE,
+) => {
+  return fundDistribution
+    ?.map((fund) => {
+      const distributionType = fund.distributionType;
+      const distributionValue = fund.value;
+      const expenseClassName = fund?.expenseClassId
+        ? (expenseClassMap.get(fund?.expenseClassId)?.name ?? invalidReferenceLabel)
+        : '';
+
+      return `"${fund.code || ''}""${expenseClassName}"
+      "${fund.value || '0'}${distributionType.toLowerCase() === FUND_DISTRIBUTION_TYPES.PERCENTAGE.toLowerCase() ? '%' : ''}"
+      "${calculateFundAmount(distributionType, distributionValue, totalAmount, currency)}"`;
+    })
+    .join(' | ')
+    .replaceAll(/\n\s+/g, '');
+};
+
+export const getExportAddressFieldValue = (
+  addressId,
+  addressMap,
+  invalidReferenceLabel = INVALID_REFERENCE_MESSAGE,
+) => {
+  return addressMap.get(addressId)
+    ? `"${addressMap.get(addressId).name}""${addressMap.get(addressId).address}"`
+    : invalidReferenceLabel;
+};
