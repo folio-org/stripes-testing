39ff3fcfd85897e390e769ec75ecc8fd7d106482
Dmytro-Melnyshyn <77053927+Dmytro-Melnyshyn@users.noreply.github.com>
2026-05-26 18:09:16 +0300

    FAT-20891: C584494 (#7174)

--- cypress/support/utils/users.js
+++ cypress/support/utils/users.js
@@ -13,3 +13,37 @@ export const getFullName = (user) => {
 
   return fullName;
 };
+
+export const buildPreRegistrationRecord = (
+  baseRecordName,
+  searchQuery,
+  { label, phone, mobilePhone, preferredEmailCommunication, isEmailVerified },
+) => {
+  const normalizedLabel = label.toLowerCase();
+  const addressLine0 = `${baseRecordName} ${label} Street`;
+
+  return {
+    status: 'TIER-2',
+    isEmailVerified,
+    generalInfo: {
+      firstName: `${baseRecordName}_${label}_First`,
+      lastName: `${baseRecordName}_${label}_Last`,
+      middleName: `${baseRecordName}_${label}_Middle`,
+      preferredFirstName: `${baseRecordName}_${label}_Preferred`,
+    },
+    addressInfo: {
+      addressLine0,
+      addressLine1: 'Suite 100',
+      city: 'Metropolis',
+      province: 'NY',
+      zip: '12345',
+      country: 'USA',
+    },
+    contactInfo: {
+      phone,
+      mobilePhone,
+      email: `${searchQuery}.${normalizedLabel}@example.com`,
+    },
+    preferredEmailCommunication,
+  };
+};
