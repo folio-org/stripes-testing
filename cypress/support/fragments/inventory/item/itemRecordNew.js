f34c1b0a89d485c2281198b49d786558cf7d9d4b
Tetiana Paranich <89065577+TetianaParanich@users.noreply.github.com>
2026-06-15 17:38:18 +0300

    added test (#7289)

--- cypress/support/fragments/inventory/item/itemRecordNew.js
+++ cypress/support/fragments/inventory/item/itemRecordNew.js
@@ -231,6 +231,15 @@ export default {
     }));
   },
 
+  deleteStatisticalCodeByName(statisticalCode) {
+    cy.contains(statisticalCode)
+      .should('be.visible')
+      .closest('[data-test-repeatable-field-list-item="true"]')
+      .find('button[data-test-repeatable-field-remove-item-button="true"]')
+      .click();
+    cy.contains(statisticalCode).should('not.exist');
+  },
+
   filterStatisticalCodeByName(name) {
     cy.do(statisticalCodeSelectionList.filter(name));
     cy.wait(1000);
