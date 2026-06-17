9cdcab7c76f503fb9c7cd530bda5e46823a8c33a
Sviatlana Stsiapanava <152482600+sviatlana-stsiapanava@users.noreply.github.com>
2026-06-17 17:06:51 +0300

    Merge pull request #7302 from folio-org/FAT-26441_1
    FAT-26441-C451497-C451493-C436809-C451495-C451494-C451492-C436810-C451496-C446042

---
+++ cypress/e2e/lists/query-builder/verify-holdings-statistical-code-names-field-displays.cy.js
@@ -0,0 +1,84 @@
+import Permissions from '../../../support/dictionary/permissions';
+import QueryModal, {
+  holdingsFieldValues,
+  enumOperators,
+  QUERY_OPERATIONS,
+} from '../../../support/fragments/bulk-edit/query-modal';
+import { Lists } from '../../../support/fragments/lists/lists';
+import TopMenu from '../../../support/fragments/topMenu';
+import Users from '../../../support/fragments/users/users';
+import getRandomPostfix from '../../../support/utils/stringTools';
+
+const listName = `AT_C451495_List_${getRandomPostfix()}`;
+const testData = {
+  statisticalCodes: [],
+};
+let user;
+
+describe('Lists', () => {
+  describe('Query Builder', () => {
+    before('Create test user and login', () => {
+      cy.getAdminToken();
+
+      // Get statistical codes with full names
+      cy.getStatisticalCodes({ limit: 2 }).then((codes) => {
+        cy.getStatisticalCodeTypes({ limit: 200 }).then((codeTypes) => {
+          codes.forEach((code) => {
+            const codeType = codeTypes.find((item) => item.id === code.statisticalCodeTypeId);
+            code.typeName = codeType.name;
+            code.fullName = `${code.typeName}: ${code.code} - ${code.name}`;
+            testData.statisticalCodes.push(code);
+          });
+        });
+      });
+
+      cy.createTempUser([Permissions.listsAll.gui, Permissions.inventoryAll.gui]).then(
+        (userProperties) => {
+          user = userProperties;
+
+          cy.login(user.username, user.password, {
+            path: TopMenu.listsPath,
+            waiter: Lists.waitLoading,
+          });
+        },
+      );
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(user.userId);
+    });
+
+    // Trillium
+    it.skip(
+      'C451495 [Holdings] Verify that the field "Holdings — Statistical code names" displays in the Query Builder (corsair)',
+      { tags: [] },
+      () => {
+        // Step 1: Create new list with Holdings record type
+        Lists.openNewListPane();
+        Lists.setName(listName);
+        Lists.selectRecordType(Lists.recordTypes.holdings);
+        Lists.verifySelectedOptionsInRecordTypeDropdown(Lists.recordTypes.holdings);
+        Lists.verifySaveButtonIsActive();
+        Lists.verifyCancelButtonIsActive();
+        Lists.buildQuery();
+        QueryModal.verify();
+
+        // Step 2: Click "Select field" dropdown in the "Field" column => search for the field 'Holdings — Statistical code names'
+        QueryModal.selectField(holdingsFieldValues.statisticalCodeNames);
+        QueryModal.verifySelectedField(holdingsFieldValues.statisticalCodeNames);
+
+        // Step 3: Select an available operator such as 'IN'
+        QueryModal.verifyOperatorsList(enumOperators);
+
+        // Step 4: Select any of the operators
+        QueryModal.selectOperator(QUERY_OPERATIONS.IN);
+        QueryModal.fillInValueMultiselect(testData.statisticalCodes[0].fullName, 0);
+        QueryModal.chooseFromValueMultiselect(testData.statisticalCodes[1].fullName, 0);
+        QueryModal.verifyQueryAreaContent(
+          `(holdings.statistical_code_names in [${testData.statisticalCodes[0].fullName}, ${testData.statisticalCodes[1].fullName}])`,
+        );
+      },
+    );
+  });
+});
