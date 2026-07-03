882092706e9bb3eee0dec713160d82baa0a0d08e
Yauhen Viazau <Yauhen_Viazau@epam.com>
2026-07-03 14:23:31 +0500

    FAT-27001-pt1: tests added

---
+++ cypress/e2e/settings/data-import/check-that-checkboxes-and-checkbox-actions-for-job-profile-are-suppressed.cy.js
@@ -0,0 +1,38 @@
+import { Permissions } from '../../../support/dictionary';
+import Users from '../../../support/fragments/users/users';
+import SettingsMenu from '../../../support/fragments/settingsMenu';
+import JobProfiles from '../../../support/fragments/data_import/job_profiles/jobProfiles';
+
+describe('Data Import', () => {
+  describe('Settings', () => {
+    let user;
+
+    before('Create test user and login', () => {
+      cy.createTempUser([Permissions.settingsDataImportEnabled.gui]).then((userProperties) => {
+        user = userProperties;
+
+        cy.login(userProperties.username, userProperties.password, {
+          path: SettingsMenu.jobProfilePath,
+          waiter: JobProfiles.waitLoadingList,
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken();
+      Users.deleteViaApi(user.userId);
+    });
+
+    it(
+      'C423390 Check that checkboxes and checkbox actions for Job profile are suppressed (folijet)',
+      { tags: ['extendedPath', 'folijet', 'C423390'] },
+      () => {
+        // Step 1: Verify checkboxes are NOT present in the Job profiles list
+        JobProfiles.verifyNoCheckboxesInList();
+
+        // Step 2: Click "Actions" button and verify suppressed options are absent
+        JobProfiles.verifyActionsMenuOptions();
+      },
+    );
+  });
+});
