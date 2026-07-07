1b8edfcd314313c0bf1843f3bb8dabe53ace3c3f
Daria <daria_reznikova@epam.com>
2026-07-07 17:21:40 +0300

    FAT-27071: Implemented C1045983 С1045984 С1045979 С1045980 (#7398)

--- cypress/support/constants/organizations/organization.js
+++ cypress/support/constants/organizations/organization.js
@@ -3,3 +3,20 @@ export const ORGANIZATION_STATUSES = {
   INACTIVE: 'Inactive',
   PENDING: 'Pending',
 };
+
+export const ORGANIZATION_SEARCH_OPTIONS = {
+  ALL: 'All',
+  NAME: 'Name',
+  CODE: 'Code',
+};
+
+export const ORGANIZATION_DETAILS_FIELDS = {
+  NAME: 'Name',
+  CODE: 'Code',
+  ORGANIZATION_STATUS: 'Organization status',
+  ACCOUNTING_CODE: 'Accounting code',
+  ACQUISITION_UNITS: 'Acquisition units',
+  DESCRIPTION: 'Description',
+  DONOR: 'Donor',
+  VENDOR: 'Vendor',
+};
