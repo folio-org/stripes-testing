e412122143411efd02343b2ea9b1d2d63144accd
Sviatlana Stsiapanava <sviatlana_stsiapanava@epam.com>
2026-07-27 18:08:02 +0300

    Add C889719, C68880 test cases

--- cypress/support/fragments/bulk-edit/query-modal.js
+++ cypress/support/fragments/bulk-edit/query-modal.js
@@ -75,6 +75,7 @@ const embeddedTableHeadersMap = {
     'Distribution type',
     'Value',
   ],
+  polLocations: ['Name', 'Code', 'Quantity electronic', 'Quantity physical'],
   userAddress: [
     'City',
     'Region',
@@ -309,6 +310,7 @@ export const transactionFieldValues = {
 export const organizationFieldValues = {
   code: 'Organization — Code',
   name: 'Organization — Name',
+  uuid: 'Organization — UUID',
 };
 export const purchaseOrderLinesFieldValues = {
   poNumber: 'PO — PO number',
@@ -318,6 +320,7 @@ export const purchaseOrderLinesFieldValues = {
   uuid: 'POL — UUID',
   costCurrency: 'POL — Cost currency',
   costPOLEstimatedPrice: 'POL — Cost PO line estimated price',
+  locationsCode: 'POL — Locations — Code',
   vendorOrgEdiType: 'Vendor org — EDI vendor type',
   vendorOrgName: 'Vendor org — Name',
 };
@@ -1251,6 +1254,8 @@ export default {
         ];
       case 'additionalCallNumbers':
         return [dataObj.callNumber, dataObj.prefix, dataObj.suffix, dataObj.type];
+      case 'polLocations':
+        return [dataObj.name, dataObj.code, dataObj.quantityElectronic, dataObj.quantityPhysical];
       default:
         throw new Error(`Unknown table type: ${tableType}`);
     }
@@ -1363,6 +1368,13 @@ export default {
       itemIdentifier,
       expectedAdditionalCallNumbers,
     );
+  },
+
+  verifyPOLLocationsEmbeddedTableInQueryModal(
+    polIdentifier,
+    expectedLocations, // Can be a single location object or array of objects, ex: { name: 'Main Library', code: 'KU/CC/DI/A', quantityElectronic: '0', quantityPhysical: '1' }
+  ) {
+    this.verifyEmbeddedTableInQueryModal('polLocations', polIdentifier, expectedLocations);
   },
 
   clickShowColumnsButton() {
