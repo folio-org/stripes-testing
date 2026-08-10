cabaec141472e434448c22c76b2172705c797fc3
Sviatlana Stsiapanava <sviatlana_stsiapanava@epam.com>
2026-08-10 18:18:57 +0300

    Add C1259785 test case

---
+++ cypress/support/constants/query-builder/purchaseOrderLinesFields.js
@@ -0,0 +1,12 @@
+export const PURCHASE_ORDER_LINES_FIELDS = {
+  POL: {
+    TAGS: 'POL — Tags',
+    CREATED_AT: 'POL — Created at',
+  },
+  PO: {
+    TAGS: 'PO — Tags',
+  },
+  VENDOR_ORG: {
+    TAGS: 'Vendor org — Tags',
+  },
+};
