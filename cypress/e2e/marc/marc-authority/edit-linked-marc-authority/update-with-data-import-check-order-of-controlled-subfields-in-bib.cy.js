207a2cc1d974bd9837bcc72d084aa84a64bcc925
Ryan Lee <rlee1@ebsco.com>
2026-08-10 19:11:01 -0700

    Passing test, some house cleaning still needed

--- cypress/e2e/marc/marc-authority/edit-linked-marc-authority/update-with-data-import-check-order-of-controlled-subfields-in-bib.cy.js
+++ cypress/e2e/marc/marc-authority/edit-linked-marc-authority/update-with-data-import-check-order-of-controlled-subfields-in-bib.cy.js
@@ -1,10 +1,10 @@
 import {
   ACCEPTED_DATA_TYPE_NAMES,
-  ACTION_NAMES_IN_ACTION_PROFILE,
   APPLICATION_NAMES,
   DEFAULT_JOB_PROFILE_NAMES,
   EXISTING_RECORD_NAMES,
   FOLIO_RECORD_TYPE,
+  RECORD_STATUSES,
 } from '../../../../support/constants';
 import SettingsDataImport, {
   SETTINGS_TABS,
@@ -17,13 +17,15 @@ import Users from '../../../../support/fragments/users/users';
 import getRandomPostfix from '../../../../support/utils/stringTools';
 
 import DataImport from '../../../../support/fragments/data_import/dataImport';
+import FileDetails from '../../../../support/fragments/data_import/logs/fileDetails';
 import InventoryInstance from '../../../../support/fragments/inventory/inventoryInstance';
 import JobProfiles from '../../../../support/fragments/data_import/job_profiles/jobProfiles';
 import Logs from '../../../../support/fragments/data_import/logs/logs';
 import MarcAuthorities from '../../../../support/fragments/marcAuthority/marcAuthorities';
 import MarcAuthority from '../../../../support/fragments/marcAuthority/marcAuthority';
-import NewJobProfile from '../../../../support/fragments/data_import/job_profiles/newJobProfile';
+import NewActionProfile from '../../../../support/fragments/settings/dataImport/actionProfiles/newActionProfile';
 import NewFieldMappingProfile from '../../../../support/fragments/settings/dataImport/fieldMappingProfile/newFieldMappingProfile';
+import NewJobProfile from '../../../../support/fragments/data_import/job_profiles/newJobProfile';
 import NewMatchProfile from '../../../../support/fragments/settings/dataImport/matchProfiles/newMatchProfile';
 import QuickMarcEditor from '../../../../support/fragments/quickMarcEditor';
 import {
@@ -44,14 +46,16 @@ const testData = {
   bibFileName: 'marcBibFileForC605928.mrc',
   bibUpdatedFileName: `testMarcFileUpd.${getRandomPostfix()}.mrc`,
   jobStatusCompleted: 'Completed',
+  keywordOption: 'Keyword',
   authorityAuthorized: 'Authorized',
   authorityHeadingType: 'Geographic Name',
   tag651: '651',
   tag151: '151',
-  field651_2: 29,
+  field651_2: 28,
   field651_2_ind1: '\\',
-  field651_2_ind2: '\\',
-  bib651Controlled: '$a United States',
+  field651_2_ind2: '0',
+  bib651Controlled: '$a United States of America', // TODO This should probably be the full uncontrolled...
+  // TODO consolidate and unify these, some repetition and different names for same things going on
   bib651Uri: 'http://id.loc.gov/authorities/subjects/sh85140220',
   bib651Uncontrolled1: '$x History $e Country $b States $e USA $y Civil War, 1861-1865 $x Cavalry operations.',
   bib651Uncontrolled2: '$8 number801 $1 URI1 $8 number802',
@@ -66,9 +70,9 @@ const mappingProfile = {
   name: `${baseProfileName} ${getRandomPostfix()}`,
 };
 const actionProfile = {
-  typeValue: FOLIO_RECORD_TYPE.MARCAUTHORITY,
+  folioRecordType: EXISTING_RECORD_NAMES.MARC_AUTHORITY,
   name: `${baseProfileName} ${getRandomPostfix()}`,
-  action: ACTION_NAMES_IN_ACTION_PROFILE.UPDATE,
+  action: 'UPDATE',
 };
 const matchProfile = {
   profileName: `${baseProfileName} ${getRandomPostfix()}`,
@@ -97,29 +101,26 @@ describe('MARC', () => {
     describe('Edit linked Authority record from Data Import', () => {
       before('Creating data', () => {
         cy.getAdminToken();
-        // create Match profile
-        NewMatchProfile.createMatchProfileWithIncomingAndExistingRecordsViaApi(matchProfile);
 
-        // create Field mapping profile
-        NewFieldMappingProfile.createMappingProfileForUpdateMarcAuthViaApi(mappingProfile);
-
-        // create Action profile and link it to Field mapping profile
-        TopMenuNavigation.openAppFromDropdown(APPLICATION_NAMES.SETTINGS);
-        SettingsDataImport.goToSettingsDataImport();
-        SettingsDataImport.selectSettingsTab(SETTINGS_TABS.ACTION_PROFILES);
-        SettingsActionProfiles.create(actionProfile, mappingProfile.name);
-        cy.wait(3000);
-
-        // create Job profile
-        SettingsDataImport.selectSettingsTab(SETTINGS_TABS.JOB_PROFILES);
-        JobProfiles.openNewJobProfileForm();
-        NewJobProfile.fillJobProfile(jobProfile);
-        NewJobProfile.linkMatchProfile(matchProfile.profileName);
-        NewJobProfile.linkActionProfileForMatches(actionProfile.name);
-        NewJobProfile.linkActionProfileForNonMatches(defaultActionProfileName);
-        // wait for the action profile to be linked
-        cy.wait(1000);
-        NewJobProfile.saveAndClose();
+        NewFieldMappingProfile.createMappingProfileForUpdateMarcAuthViaApi(mappingProfile)
+          .then((fmpResponse) => {
+            mappingProfile.id = fmpResponse.body.id;
+            return NewActionProfile.createActionProfileViaApi(actionProfile, mappingProfile.id);
+          })
+          .then((apResponse) => {
+            actionProfile.id = apResponse.body.id;
+            return NewMatchProfile.createMatchProfileWithIncomingAndExistingRecordsViaApi(
+              matchProfile,
+            );
+          })
+          .then((mpResponse) => {
+            matchProfile.id = mpResponse.body.id;
+            NewJobProfile.createJobProfileWithLinkedMatchAndActionProfilesViaApi(
+              jobProfile.profileName,
+              matchProfile.id,
+              actionProfile.id,
+            );
+          });
 
         // Upload authority
         DataImport.uploadFileViaApi(
@@ -127,36 +128,27 @@ describe('MARC', () => {
           testData.firstAuthUpdatedFileName,
           DEFAULT_JOB_PROFILE_NAMES.CREATE_AUTHORITY,
         ).then((response) => {
-          response.forEach((record) => {
-            testData.createdAuthorityID = record.authority.id;
+          testData.createdAuthorityID = response[0].authority.id;
+
+          // Upload instance
+          DataImport.uploadFileViaApi(
+            testData.bibFileName,
+            testData.bibUpdatedFileName,
+            DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS,
+          ).then((response) => {
+            testData.createdBibID = response[0].instance.id;
+
+            QuickMarcEditor.linkMarcRecordsViaApi({
+              bibId: testData.createdBibID,
+              authorityIds: [testData.createdAuthorityID],
+              bibFieldTags: [testData.tag651],
+              authorityFieldTags: [testData.tag151],
+              finalBibFieldContents: [`${testData.bib651Controlled} ${testData.bib651Uncontrolled1} ${testData.bib651Uncontrolled2}`],
+              bibFieldIndexes: [testData.field651_2],
+            });
+            MarcAuthorities.waitAuthorityLinked(testData.createdAuthorityID, 1);
           });
         });
-
-        // Upload instance
-        DataImport.uploadFileViaApi(
-          testData.bibFileName,
-          testData.bibUpdatedFileName,
-          DEFAULT_JOB_PROFILE_NAMES.CREATE_INSTANCE_AND_SRS,
-        ).then((response) => {
-          response.forEach((record) => {
-            testData.createdBibID = record.authority.id;
-          });
-        });
-
-        // Link bib to authority
-        // The finalBibFieldContents are not exactly correct after this operation since
-        // this method just appends $0 at the end. The first step in the spec will do an
-        // update that should then replace it with the correct order - what this test is
-        // ultimately checking for.
-        QuickMarcEditor.linkMarcRecordsViaApi({
-          bibId: testData.createdBibID,
-          authorityIds: [testData.createdAuthorityID],
-          bibFieldTags: [testData.tag651],
-          authorityFieldTags: [testData.tag151],
-          finalBibFieldContents: [`${testData.bib651Controlled} ${testData.bib651Uncontrolled1} ${testData.bib651Uncontrolled2}`],
-          bibFieldIndexes: [testData.bibField651_2],
-        });
-        MarcAuthorities.waitAuthorityLinked(testData.createdAuthorityID, 1);
 
         cy.createTempUser([
           Permissions.moduleDataImportEnabled.gui,
@@ -184,12 +176,12 @@ describe('MARC', () => {
         SettingsActionProfiles.deleteActionProfileByNameViaApi(actionProfile.name);
         SettingsFieldMappingProfiles.deleteMappingProfileByNameViaApi(mappingProfile.name);
         if (testData.createdAuthorityID) MarcAuthority.deleteViaAPI(testData.createdAuthorityID);
-        if (testData.createdBibID) InventoryInstance.deleteInstanceViaApi(testData.createdBibId);
+        if (testData.createdBibID) InventoryInstance.deleteInstanceViaApi(testData.createdBibID);
         if (testData.userProperties) Users.deleteViaApi(testData.userProperties.userId);
       });
 
       it(
-        'C605928 Check order of controlled subfields in MARC bib\'s field after update of linked "MARC authority" record via "Data import" (promin)',
+        'C605928 Check order of controlled subfields in MARC bib field after update of linked "MARC authority" record via "Data import" (promin)',
         { tags: ['extendedPath', 'promin', 'C605928'] },
         () => {
           // Upload authority update
@@ -203,13 +195,17 @@ describe('MARC', () => {
 
           // Review upload
           Logs.openFileDetails(testData.secondAuthUpdatedFileName);
-          Logs.verifyInstanceStatus(0, 2, RECORD_STATUSES.UPDATED);
-          Logs.verifyInstanceStatus(0, 6, RECORD_STATUSES.UPDATED);
+          [
+            FileDetails.columnNameInResultList.srsMarc,
+            FileDetails.columnNameInResultList.authority,
+          ].forEach((columnName) => {
+            FileDetails.checkStatusInColumn(RECORD_STATUSES.UPDATED, columnName);
+          });
 
           // Locate updated record and verify linking
           TopMenuNavigation.navigateToApp(APPLICATION_NAMES.MARC_AUTHORITY);
           MarcAuthorities.waitLoading();
-          MarcAuthorities.searchBy(keywordOption, testData.updatedAuthTitle);
+          MarcAuthorities.searchBy(testData.keywordOption, testData.updatedAuthTitle);
           MarcAuthorities.verifyResultsRowContent(testData.updatedAuthTitle, testData.authorityAuthorized, testData.authorityHeadingType);
           MarcAuthorities.verifyNumberOfTitlesForRowWithValue(testData.updatedAuthTitle, 1);
           
@@ -227,6 +223,9 @@ describe('MARC', () => {
           // Open editor and verify 2nd linked 651's controlled and uncontrolled input order
           InventoryInstance.editMarcBibliographicRecord();
           QuickMarcEditor.waitLoading();
+          // TODO testrail spec says uncontrolled should be something else - discrepancy because
+          // of error in testrail spec or error in setup? when it's originally set before the update,
+          // by me, effectively, is that affecting this outcome?
           QuickMarcEditor.verifyTagFieldAfterLinking(
             testData.field651_2,
             testData.tag651, 
