5c00cf2c9450a56236f6ef3f41689d3588db04c1
Daria <daria_reznikova@epam.com>
2026-08-17 15:36:57 +0300

    FAT-28324: Update C466244 (#7540)

--- cypress/e2e/orders/save-using-save-and-keep-editing-button-when-creating-new-order-and-order-line.cy.js
+++ cypress/e2e/orders/save-using-save-and-keep-editing-button-when-creating-new-order-and-order-line.cy.js
@@ -1,7 +1,11 @@
 import InventorySearchAndFilter from '../../support/fragments/inventory/inventorySearchAndFilter';
 import getRandomPostfix from '../../support/utils/stringTools';
 import { NewOrganization, Organizations } from '../../support/fragments/organizations';
-import { OpenOrder, OrderLinesLimit } from '../../support/fragments/settings/orders';
+import {
+  OpenOrder,
+  OrderLinesLimit,
+  SettingsOrders,
+} from '../../support/fragments/settings/orders';
 import { Permissions } from '../../support/dictionary';
 import TopMenu from '../../support/fragments/topMenu';
 import TopMenuNavigation from '../../support/fragments/topMenuNavigation';
@@ -66,6 +70,7 @@ describe('Orders', () => {
     cy.getAdminToken().then(() => {
       OrderLinesLimit.setPOLLimitViaApi(2);
       OpenOrder.setOpenOrderValue(false);
+      SettingsOrders.setUserCanEditPONumberViaApi(false);
       Organizations.createOrganizationViaApi(testData.organization).then((id) => {
         testData.organization.id = id;
       });
