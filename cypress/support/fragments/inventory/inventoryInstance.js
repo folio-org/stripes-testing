6d8e12f1ea1d0977c2f477b184e9c08410cb4151
Yauhen Viazau <Yauhen_Viazau@epam.com>
2026-08-21 22:12:53 +0500

    FAT-28020-pt3: tests added

--- cypress/support/fragments/inventory/inventoryInstance.js
+++ cypress/support/fragments/inventory/inventoryInstance.js
@@ -166,6 +166,7 @@ const importTypeSelect = Select({ name: 'externalIdentifierType' });
 const versionHistoryButton = Button({ icon: 'clock' });
 const instanceRelationshipAccordion = Accordion('Instance relationship');
 const heldbyAccordionName = 'Held by';
+const clipboardIcon = Button({ icon: 'clipboard' });
 
 const messages = {
   itemMovedSuccessfully: '1 item has been successfully moved.',
@@ -2078,12 +2079,22 @@ export default {
     cy.expect(targetEl.exists());
   },
 
+  verifyNoCopyIconForItemInHoldings(holdingsLocation, itemIndex = 0) {
+    const holdingSection = Accordion(including(holdingsLocation));
+    cy.expect(
+      holdingSection
+        .find(MultiColumnListCell({ columnIndex: 1, row: itemIndex }))
+        .find(clipboardIcon)
+        .absent(),
+    );
+  },
+
   copyItemBarcode(itemIndex = 0, holdingsLocation, whileMoving = false) {
     const holdingSection = Accordion(including(holdingsLocation));
     cy.do(
       holdingSection
         .find(MultiColumnListCell({ columnIndex: whileMoving ? 3 : 1, row: itemIndex }))
-        .find(Button({ icon: 'clipboard' }))
+        .find(clipboardIcon)
         .click(),
     );
 
