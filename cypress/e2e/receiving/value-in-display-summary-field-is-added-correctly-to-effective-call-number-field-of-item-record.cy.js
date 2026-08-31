6bfd650a33a74b443c4b23afc0ffba30354c390e
Daria-Reznikova <daria_reznikova@epam.com>
2026-08-31 13:46:06 +0300

    FAT-28446: Implemented C436895 C934299

--- cypress/e2e/receiving/value-in-display-summary-field-is-added-correctly-to-effective-call-number-field-of-item-record.cy.js
+++ cypress/e2e/receiving/value-in-display-summary-field-is-added-correctly-to-effective-call-number-field-of-item-record.cy.js
@@ -1,19 +1,20 @@
 import {
   ACQUISITION_METHOD_NAMES_IN_PROFILE,
+  HOLDING_RECEIVING_HISTORY,
+  INVENTORY_ITEMS,
+  ITEM_STATUS_NAMES,
   LOCATION_NAMES,
-  ORDER_SEARCH_OPTIONS,
   ORDER_STATUSES,
   RECEIVING_PIECE_FORM_FIELD_LABELS,
 } from '../../support/constants';
-import {
-  BasicOrderLine,
-  NewOrder,
-  OrderDetails,
-  OrderLines,
-  Orders,
-} from '../../support/fragments/orders';
+import { BasicOrderLine, NewOrder, OrderLines, Orders } from '../../support/fragments/orders';
 import EditPieceModal from '../../support/fragments/receiving/modals/editPieceModal';
 import HoldingsRecordView from '../../support/fragments/inventory/holdingsRecordView';
+import {
+  InstanceRecordView,
+  ItemRecordEdit,
+  ItemRecordView,
+} from '../../support/fragments/inventory';
 import InventoryInstance from '../../support/fragments/inventory/inventoryInstance';
 import getRandomPostfix from '../../support/utils/stringTools';
 import { NewOrganization, Organizations } from '../../support/fragments/organizations';
@@ -24,9 +25,31 @@ import TopMenu from '../../support/fragments/topMenu';
 import Users from '../../support/fragments/users/users';
 
 const PIECE_DETAILS_TO_FILL = {
-  enumeration: `Autotest_enumeration_${getRandomPostfix()}`,
-  chronology: `Autotest_chronology_${getRandomPostfix()}`,
+  displaySummary: `Autotest_display_summary_${getRandomPostfix()}`,
 };
+
+const ITEM_DETAILS_TO_FILL = {
+  callNumberPrefix: '1',
+  callNumber: '2',
+  callNumberSuffix: '3',
+  volume: '4',
+  enumeration: '5',
+  chronology: '6',
+  copyNumber: '7',
+};
+
+const UPDATED_DISPLAY_SUMMARY = `Item_1_${getRandomPostfix()}`;
+
+const EXPECTED_EFFECTIVE_CALL_NUMBER = [
+  ITEM_DETAILS_TO_FILL.callNumberPrefix,
+  ITEM_DETAILS_TO_FILL.callNumber,
+  ITEM_DETAILS_TO_FILL.callNumberSuffix,
+  UPDATED_DISPLAY_SUMMARY,
+  ITEM_DETAILS_TO_FILL.volume,
+  ITEM_DETAILS_TO_FILL.enumeration,
+  ITEM_DETAILS_TO_FILL.chronology,
+  ITEM_DETAILS_TO_FILL.copyNumber,
+].join(' ');
 
 describe('Receiving', () => {
   const testData = {
@@ -67,6 +90,7 @@ describe('Receiving', () => {
   const createOrderWithOrderLine = () => {
     return Orders.createOrderViaApi({
       ...NewOrder.getDefaultOrder({ vendorId: testData.organization.id }),
+      reEncumber: true,
     })
       .then((orderResponse) => {
         testData.order = orderResponse;
@@ -107,15 +131,15 @@ describe('Receiving', () => {
     });
 
     cy.createTempUser([
+      Permissions.uiReceivingViewEdit.gui,
       Permissions.uiInventoryViewInstances.gui,
-      Permissions.uiOrdersEdit.gui,
-      Permissions.uiReceivingViewEditCreate.gui,
+      Permissions.uiInventoryViewCreateEditItems.gui,
     ]).then((userProperties) => {
       testData.user = userProperties;
 
       cy.login(testData.user.username, testData.user.password, {
-        path: TopMenu.ordersPath,
-        waiter: Orders.waitLoading,
+        path: TopMenu.receivingPath,
+        waiter: Receiving.waitLoading,
       });
     });
   });
@@ -143,59 +167,90 @@ describe('Receiving', () => {
   });
 
   it(
-    'C388530 "Receiving history" accordion does not contain record\'s detail when "Display on holding" option is NOT active in "Quick receive" (thunderjet)',
-    { tags: ['extendedPath', 'thunderjet', 'C388530'] },
+    'C436895 Value in "Display summary" field is added correctly to "Effective call number" field of "Item" record with public display settings (thunderjet)',
+    { tags: ['criticalPath', 'thunderjet', 'C436895'] },
     () => {
-      // Step 1: Click "PO number" link and check order status
-      Orders.searchByParameter(ORDER_SEARCH_OPTIONS.PO_NUMBER, testData.order.poNumber);
-      Orders.selectFromResultsList(testData.order.poNumber);
-      OrderDetails.waitLoading();
-      OrderDetails.checkOrderStatus(ORDER_STATUSES.OPEN);
-
-      // Step 2: Open "Receiving" page via Actions -> Receive
-      Orders.receiveOrderViaActions();
+      // Step 1: Click on the PO line Title hyperlink
+      Receiving.searchByParameter({ value: testData.orderLine.titleOrPackage });
       Receiving.selectFromResultsList(testData.orderLine.titleOrPackage);
-
-      // Step 3: Click on the title, verify accordions on the details pane
       ReceivingDetails.waitLoading();
-      ReceivingDetails.checkReceivingDetails();
       ReceivingDetails.verifyExpectedRecordsCount(1);
 
-      // Step 4: Click on the record in "Expected" accordion, verify Edit piece form
+      // Step 2: Click on the piece record in "Expected" accordion
       Receiving.selectRecordInExpectedList();
       EditPieceModal.waitLoading();
-      EditPieceModal.verifyModalView();
-
-      // Step 5: Fill in Enumeration/Chronology, "Display on holding" stays unchecked
-      EditPieceModal.fillPieceDetails({
-        [RECEIVING_PIECE_FORM_FIELD_LABELS.ENUMERATION]: PIECE_DETAILS_TO_FILL.enumeration,
-        [RECEIVING_PIECE_FORM_FIELD_LABELS.CHRONOLOGY]: PIECE_DETAILS_TO_FILL.chronology,
-      });
-      EditPieceModal.verifyCheckboxState(
-        RECEIVING_PIECE_FORM_FIELD_LABELS.DISPLAY_ON_HOLDING,
+      EditPieceModal.verifyCheckboxPresent(
+        RECEIVING_PIECE_FORM_FIELD_LABELS.DISPLAY_TO_PUBLIC,
         false,
       );
 
-      // Step 6: Expand dropdown next to "Save & close" and select "Quick receive"
+      // Step 3: Fill "Display summary" and check "Display on holding"
+      EditPieceModal.fillPieceDetails({
+        [RECEIVING_PIECE_FORM_FIELD_LABELS.DISPLAY_SUMMARY]: PIECE_DETAILS_TO_FILL.displaySummary,
+      });
+      EditPieceModal.checkDisplayOnHoldingCheckbox();
+      EditPieceModal.verifyCheckboxState(
+        RECEIVING_PIECE_FORM_FIELD_LABELS.DISPLAY_ON_HOLDING,
+        true,
+      );
+      EditPieceModal.verifyCheckboxPresent(
+        RECEIVING_PIECE_FORM_FIELD_LABELS.DISPLAY_TO_PUBLIC,
+        true,
+      );
+
+      // Step 4: Check "Display to public"
+      EditPieceModal.checkDisplayToPublicCheckbox();
+      EditPieceModal.verifyCheckboxState(RECEIVING_PIECE_FORM_FIELD_LABELS.DISPLAY_TO_PUBLIC, true);
+
+      // Step 5: Quick receive the piece via dropdown next to "Save & close"
       EditPieceModal.openActionsMenu();
       Receiving.quickReceivePieceFromDropdown();
       ReceivingDetails.waitLoading();
       ReceivingDetails.verifyExpectedRecordsCount(0);
       ReceivingDetails.checkReceivedTableContent([
-        {
-          enumeration: PIECE_DETAILS_TO_FILL.enumeration,
-          chronology: PIECE_DETAILS_TO_FILL.chronology,
-        },
+        { displaySummary: PIECE_DETAILS_TO_FILL.displaySummary },
       ]);
 
-      // Step 7: Click Title link, navigate to Inventory
+      // Step 6: Click Title link, navigate to Inventory
       Receiving.selectInstanceInReceive(testData.orderLine.titleOrPackage);
+      InstanceRecordView.expandHoldings(testData.location.name);
 
-      // Step 8-9: Open holdings and verify "Receiving history" accordion does not contain the record
+      // Step 7: Click "View holdings" and verify "Receiving history" accordion
       InventoryInstance.viewHoldings();
       HoldingsRecordView.waitLoading();
       HoldingsRecordView.expandReceivingHistoryAccordion();
-      HoldingsRecordView.checkAbsentRecordInReceivingHistory();
+      HoldingsRecordView.checkReceivingHistoryValues({
+        displaySummary: PIECE_DETAILS_TO_FILL.displaySummary,
+        receiptDate: new Date().toLocaleDateString('en-US'),
+        publicDisplay: true,
+        source: HOLDING_RECEIVING_HISTORY.RECEIVING,
+      });
+
+      // Step 8: Close Holdings, open the item via "No barcode" link
+      HoldingsRecordView.close();
+      InventoryInstance.openHoldingItem({ name: testData.location.name });
+      ItemRecordView.verifyItemStatusInPane(ITEM_STATUS_NAMES.IN_PROCESS);
+      ItemRecordView.checkItemRecordDetails({
+        enumerationData: [
+          {
+            label: INVENTORY_ITEMS.DISPLAY_SUMMARY,
+            conditions: { value: PIECE_DETAILS_TO_FILL.displaySummary },
+          },
+        ],
+      });
+      ItemRecordView.verifyEffectiveCallNumber(PIECE_DETAILS_TO_FILL.displaySummary);
+
+      // Step 9-10: Open item edit form and fill in call number/enumeration data fields
+      ItemRecordView.openItemEditForm(testData.orderLine.titleOrPackage);
+      ItemRecordEdit.fillItemRecordFields(ITEM_DETAILS_TO_FILL);
+
+      // Step 11: Edit "Display summary" value
+      ItemRecordEdit.addDisplaySummary(UPDATED_DISPLAY_SUMMARY);
+
+      // Step 12: Save and verify the resulting "Effective call number"
+      ItemRecordEdit.saveAndClose({ itemSaved: true });
+      ItemRecordView.waitLoading();
+      ItemRecordView.verifyEffectiveCallNumber(EXPECTED_EFFECTIVE_CALL_NUMBER);
     },
   );
 });
