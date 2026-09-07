871a10932de47cd4d2f8a2cd5a8724f6277b6f8e
Ostap_Voitsekhovskyi <ostap_voitsekhovskyi@epam.com>
2026-09-07 11:40:05 +0300

    implemented new vega tests 3483 3484

--- cypress.config.js
+++ cypress.config.js
@@ -89,7 +89,7 @@ module.exports = defineConfig({
     openMode: 0,
   },
   numTestsKeptInMemory: 1,
-  watchForFileChanges: false,
+  watchForFileChanges: true,
   viewportWidth: 1920,
   viewportHeight: 1080,
   video: false,
