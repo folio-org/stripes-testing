46d21f39d23e8cba9e57541d5fbc488100978eec
Kyrylo Brener <kbrener@ebsco.com>
2026-09-10 13:44:58 +0300

    added mosaic UI config check for fse suite

--- cypress/support/fragments/settingsMenu.js
+++ cypress/support/fragments/settingsMenu.js
@@ -131,6 +131,8 @@ export default {
   tagsGeneralPath: 'settings/tags/general',
   // Software versions
   softwareVersionsPath: 'settings/about',
+  // Mosaic integration settings
+  mosaicSettingsConfigPath: 'settings/mosaic-settings/configuration-options',
   // Session locale path
   sessionLocalePath: 'settings/developer/locale',
   // Export manager settings
