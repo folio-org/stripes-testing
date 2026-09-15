01933dff270a6b7606586311bad0d7f0fd40ad39
Kyrylo Brener <kbrener@ebsco.com>
2026-09-15 19:23:22 +0300

    added fse sanity checks for marigold app

--- cypress/support/api/linkedDataEditor.js
+++ cypress/support/api/linkedDataEditor.js
@@ -1,3 +1,15 @@
+Cypress.Commands.add('getLinkedDataWorks', (query) => {
+  return cy.okapiRequest({
+    method: 'GET',
+    path: 'search/linked-data/works',
+    isDefaultSearchParamsRequired: false,
+    searchParams: {
+      limit: 1,
+      query,
+    },
+  });
+});
+
 Cypress.Commands.add('setPrefferedProfileForUser', () => {
   // id is hardcoded as 3 on purpose - API will automatically identify the user ID from the folioAccessToken cookie.
   const prefferedProfile = {
