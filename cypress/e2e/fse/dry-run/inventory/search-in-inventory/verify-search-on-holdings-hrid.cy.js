2d2cc6e9fcae4cd58dd704c551502831a123a9eb
Ostap Voitsekhovskyi <Ostap_Voitsekhovskyi@epam.com>
2026-09-22 15:14:17 +0300

    new DA tests added (#7717)

---
+++ cypress/e2e/fse/dry-run/inventory/search-in-inventory/verify-search-on-holdings-hrid.cy.js
@@ -0,0 +1,108 @@
+import InventoryInstances, {
+  searchHoldingsOptions,
+} from '../../../../../support/fragments/inventory/inventoryInstances';
+import InventorySearchAndFilter from '../../../../../support/fragments/inventory/inventorySearchAndFilter';
+import TopMenu from '../../../../../support/fragments/topMenu';
+import getRandomPostfix from '../../../../../support/utils/stringTools';
+
+const { user, memberTenant } = parseSanityParameters();
+
+describe('Inventory', () => {
+  describe('Search in Inventory', () => {
+    const randomPostfix = getRandomPostfix();
+    const instanceTitlePrefix = `AT_C9207_FolioInstance_${randomPostfix}`;
+    const testData = {
+      folioInstances: InventoryInstances.generateFolioInstances({
+        count: 3,
+        instanceTitlePrefix,
+        holdingsCount: 1,
+        itemsCount: 0,
+      }),
+      searchOption: searchHoldingsOptions[7], // Holdings HRID
+    };
+    testData.folioInstances.forEach((instance, index) => {
+      instance.instanceTitle = `${instanceTitlePrefix}_${index + 1}`;
+    });
+
+    let location;
+    const holdingsHRIDs = [];
+
+    before('Create test data', () => {
+      cy.setTenant(memberTenant.id);
+      cy.getUserToken(user.username, user.password, { log: false });
+      InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C9207_FolioInstance');
+
+      cy.then(() => {
+        // Get required reference data
+        cy.getLocations({
+          limit: 1,
+          query: '(isActive=true and name<>"AT_*" and name<>"autotest*")',
+        }).then((res) => {
+          location = res;
+        });
+      }).then(() => {
+        // Create 3 instances with 1 holdings each
+        InventoryInstances.createFolioInstancesViaApi({
+          folioInstances: testData.folioInstances,
+          location,
+        });
+
+        // Get Holdings HRIDs for search tests
+        testData.folioInstances.forEach((instance, index) => {
+          cy.getHoldings({
+            limit: 1,
+            query: `"instanceId"="${instance.instanceId}"`,
+          }).then((holdings) => {
+            holdingsHRIDs[index] = holdings[0].hrid;
+          });
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.setTenant(memberTenant.id);
+      cy.getUserToken(user.username, user.password, { log: false });
+      testData.folioInstances.forEach((instance) => {
+        InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(instance.instanceId);
+      });
+    });
+
+    it(
+      'C9207 Verify search on Holdings HRID (spitfire)',
+      { tags: ['dryRun', 'spitfire', 'C9207'] },
+      () => {
+        cy.waitForAuthRefresh(() => {
+          cy.login(user.username, user.password, {
+            path: TopMenu.inventoryPath,
+            waiter: InventoryInstances.waitContentLoading,
+          });
+        }, 20_000);
+        InventoryInstances.waitContentLoading();
+
+        // Step 1: Open the Inventory app. Select the holdings segment.
+        InventorySearchAndFilter.switchToHoldings();
+        InventorySearchAndFilter.holdingsTabIsDefault();
+        InventorySearchAndFilter.verifyResultPaneEmpty();
+
+        // Step 2: Click on the search options dropdown and select "Holdings HRID" search option
+        InventorySearchAndFilter.selectSearchOption(testData.searchOption);
+        InventorySearchAndFilter.verifyDefaultSearchOptionSelected(testData.searchOption);
+
+        // Step 3: Search for first holdings HRID
+        InventorySearchAndFilter.executeSearch(holdingsHRIDs[0]);
+        InventorySearchAndFilter.verifySearchResult(testData.folioInstances[0].instanceTitle);
+        InventorySearchAndFilter.checkRowsCount(1);
+
+        // Step 4: Repeat test with second Holdings HRID
+        InventorySearchAndFilter.executeSearch(holdingsHRIDs[1]);
+        InventorySearchAndFilter.verifySearchResult(testData.folioInstances[1].instanceTitle);
+        InventorySearchAndFilter.checkRowsCount(1);
+
+        // Repeat test with third Holdings HRID
+        InventorySearchAndFilter.executeSearch(holdingsHRIDs[2]);
+        InventorySearchAndFilter.verifySearchResult(testData.folioInstances[2].instanceTitle);
+        InventorySearchAndFilter.checkRowsCount(1);
+      },
+    );
+  });
+});
---
+++ cypress/e2e/fse/dry-run/inventory/search-in-inventory/verify-search-on-instance-hrid.cy.js
@@ -0,0 +1,105 @@
+import InventoryInstances, {
+  searchInstancesOptions,
+} from '../../../../../support/fragments/inventory/inventoryInstances';
+import InventorySearchAndFilter from '../../../../../support/fragments/inventory/inventorySearchAndFilter';
+import TopMenu from '../../../../../support/fragments/topMenu';
+import getRandomPostfix from '../../../../../support/utils/stringTools';
+
+const { user, memberTenant } = parseSanityParameters();
+
+describe('Inventory', () => {
+  describe('Search in Inventory', () => {
+    const randomPostfix = getRandomPostfix();
+    const instanceTitlePrefix = `AT_C9206_FolioInstance_${randomPostfix}`;
+    const testData = {
+      folioInstances: InventoryInstances.generateFolioInstances({
+        count: 3,
+        instanceTitlePrefix,
+        holdingsCount: 1,
+        itemsCount: 0,
+      }),
+      searchOption: searchInstancesOptions[13], // Instance HRID
+    };
+    testData.folioInstances.forEach((instance, index) => {
+      instance.instanceTitle = `${instanceTitlePrefix}_${index + 1}`;
+    });
+
+    let location;
+    const instanceHRIDs = [];
+
+    before('Create test data', () => {
+      cy.setTenant(memberTenant.id);
+      cy.getUserToken(user.username, user.password, { log: false });
+      InventoryInstances.deleteFullInstancesByTitleViaApi('AT_C9206_FolioInstance');
+
+      cy.then(() => {
+        // Get required reference data
+        cy.getLocations({
+          limit: 1,
+          query: '(isActive=true and name<>"AT_*" and name<>"autotest*")',
+        }).then((res) => {
+          location = res;
+        });
+      }).then(() => {
+        // Create 3 instances with 1 holdings each
+        InventoryInstances.createFolioInstancesViaApi({
+          folioInstances: testData.folioInstances,
+          location,
+        });
+
+        // Get Instance HRIDs for search tests
+        testData.folioInstances.forEach((instance, index) => {
+          cy.getInstanceHRID(instance.instanceId).then((instanceHRIDResponse) => {
+            instanceHRIDs[index] = instanceHRIDResponse;
+          });
+        });
+      });
+    });
+
+    after('Delete test data', () => {
+      cy.setTenant(memberTenant.id);
+      cy.getUserToken(user.username, user.password, { log: false });
+      testData.folioInstances.forEach((instance) => {
+        InventoryInstances.deleteInstanceAndItsHoldingsAndItemsViaApi(instance.instanceId);
+      });
+    });
+
+    it(
+      'C9206 Verify search on Instance HRID (spitfire)',
+      { tags: ['dryRun', 'spitfire', 'C9206'] },
+      () => {
+        cy.login(user.username, user.password, {
+          path: TopMenu.inventoryPath,
+          waiter: InventoryInstances.waitContentLoading,
+          authRefresh: true,
+        });
+
+        // Step 1: Open the Inventory app. Select the instance segment.
+        InventorySearchAndFilter.instanceTabIsDefault();
+        InventorySearchAndFilter.verifyResultPaneEmpty();
+
+        // Step 2: Click on the search options dropdown and select "Instance HRID" search option
+        InventorySearchAndFilter.selectSearchOption(testData.searchOption);
+        InventorySearchAndFilter.verifyDefaultSearchOptionSelected(testData.searchOption);
+
+        // Step 3: Search for first instance HRID
+        InventorySearchAndFilter.selectSearchOption(testData.searchOption);
+        InventorySearchAndFilter.executeSearch(instanceHRIDs[0]);
+        InventorySearchAndFilter.verifySearchResult(testData.folioInstances[0].instanceTitle);
+        InventorySearchAndFilter.checkRowsCount(1);
+
+        // Step 4: Repeat test with second Instance HRID
+        InventorySearchAndFilter.selectSearchOption(testData.searchOption);
+        InventorySearchAndFilter.executeSearch(instanceHRIDs[1]);
+        InventorySearchAndFilter.verifySearchResult(testData.folioInstances[1].instanceTitle);
+        InventorySearchAndFilter.checkRowsCount(1);
+
+        // Repeat test with third Instance HRID
+        InventorySearchAndFilter.selectSearchOption(testData.searchOption);
+        InventorySearchAndFilter.executeSearch(instanceHRIDs[2]);
+        InventorySearchAndFilter.verifySearchResult(testData.folioInstances[2].instanceTitle);
+        InventorySearchAndFilter.checkRowsCount(1);
+      },
+    );
+  });
+});
