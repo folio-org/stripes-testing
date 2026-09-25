7dcfdd37154d195bc83cc5b7c81bbac55547e9ef
Daria-Reznikova <daria_reznikova@epam.com>
2026-09-25 10:48:31 +0300

    FAT-29009: Implemented C1028980 C1028981 C1028984 C1028985 C1045390 C1045391 C1045392 C1045395 C1045396 C844253 C844259 C844263

---
+++ cypress/e2e/invoices/encumbrance-is-calculated-correctly-after-canceling-paid-invoice-when-another-paid-invoice-exists.cy.js
@@ -0,0 +1,237 @@
+import {
+  ACQUISITION_METHOD_NAMES_IN_PROFILE,
+  APPLICATION_NAMES,
+  ENCUMBRANCE_STATUSES,
+  FUND_DISTRIBUTION_TYPES,
+  INVOICE_STATUSES,
+  INVOICE_VIEW_FIELDS,
+  ORDER_STATUSES,
+  TRANSACTION_DETAIL_FIELDS,
+  TRANSACTION_TYPES,
+} from '../../support/constants';
+import Permissions from '../../support/dictionary/permissions';
+import { Budgets, TransactionDetails } from '../../support/fragments/finance';
+import { InvoiceLineDetails, Invoices, InvoiceView } from '../../support/fragments/invoices';
+import { BasicOrderLine, NewOrder, OrderLines, Orders } from '../../support/fragments/orders';
+import { NewOrganization, Organizations } from '../../support/fragments/organizations';
+import TopMenu from '../../support/fragments/topMenu';
+import TopMenuNavigation from '../../support/fragments/topMenuNavigation';
+import Users from '../../support/fragments/users/users';
+
+describe('Invoices', () => {
+  describe('Cancellation', () => {
+    const testData = {
+      organization: NewOrganization.getDefaultOrganization(),
+      fiscalYear: {},
+      fund: {},
+      acquisitionMethodId: null,
+      order: {},
+      orderLine: {},
+      invoices: {},
+      user: {},
+    };
+
+    const createBudgetWithFundLedgerAndFiscalYear = () => {
+      const { fiscalYear, fund } = Budgets.createBudgetWithFundLedgerAndFYViaApi({
+        budget: { allocated: 1000 },
+      });
+
+      testData.fiscalYear = fiscalYear;
+      testData.fund = fund;
+    };
+
+    const createOrganization = () => {
+      return Organizations.createOrganizationViaApi(testData.organization).then((id) => {
+        testData.organization.id = id;
+      });
+    };
+
+    const getAcquisitionMethodId = () => {
+      return cy
+        .getAcquisitionMethodsApi({
+          query: `value="${ACQUISITION_METHOD_NAMES_IN_PROFILE.PURCHASE_AT_VENDOR_SYSTEM}"`,
+        })
+        .then(({ body }) => {
+          testData.acquisitionMethodId = body.acquisitionMethods[0].id;
+        });
+    };
+
+    const createOpenOneTimeOrderWithLine = () => {
+      return getAcquisitionMethodId()
+        .then(() => {
+          return Orders.createOrderViaApi(
+            NewOrder.getDefaultOrder({ vendorId: testData.organization.id }),
+          );
+        })
+        .then((order) => {
+          testData.order = order;
+
+          return OrderLines.createOrderLineViaApi(
+            BasicOrderLine.getDefaultOrderLine({
+              purchaseOrderId: order.id,
+              acquisitionMethod: testData.acquisitionMethodId,
+              listUnitPrice: 100,
+              poLineEstimatedPrice: 100,
+              fundDistribution: [
+                {
+                  code: testData.fund.code,
+                  fundId: testData.fund.id,
+                  distributionType: FUND_DISTRIBUTION_TYPES.PERCENTAGE,
+                  value: 100,
+                },
+              ],
+            }),
+          );
+        })
+        .then((orderLine) => {
+          testData.orderLine = orderLine;
+
+          return Orders.updateOrderViaApi({
+            ...testData.order,
+            workflowStatus: ORDER_STATUSES.OPEN,
+          });
+        });
+    };
+
+    const createAndPayInvoice = ({ invoiceKey, subTotal, releaseEncumbrance }) => {
+      return OrderLines.getOrderLineByIdViaApi(testData.orderLine.id)
+        .then((orderLine) => {
+          return Invoices.createInvoiceWithInvoiceLineViaApi({
+            vendorId: testData.organization.id,
+            accountingCode: testData.organization.erpCode,
+            poLineId: orderLine.id,
+            fundDistributions: orderLine.fundDistribution,
+            invoiceStatus: INVOICE_STATUSES.OPEN,
+            subTotal,
+            releaseEncumbrance,
+            exportToAccounting: false,
+          });
+        })
+        .then((invoice) => {
+          testData.invoices[invoiceKey] = invoice;
+
+          return Invoices.changeInvoiceStatusViaApi({ invoice, status: INVOICE_STATUSES.PAID });
+        });
+    };
+
+    const createAndPayInvoices = () => {
+      return createAndPayInvoice({
+        invoiceKey: 'first',
+        subTotal: 10,
+        releaseEncumbrance: true,
+      }).then(() => {
+        return createAndPayInvoice({
+          invoiceKey: 'second',
+          subTotal: 150,
+          releaseEncumbrance: false,
+        });
+      });
+    };
+
+    const createUserAndLogin = () => {
+      return cy
+        .createTempUser([
+          Permissions.uiInvoicesCanViewInvoicesAndInvoiceLines.gui,
+          Permissions.uiOrdersView.gui,
+          Permissions.uiFinanceViewFundAndBudget.gui,
+          Permissions.uiInvoicesCancelInvoices.gui,
+          Permissions.uiFinanceUnreleaseEncumbrance.gui,
+        ])
+        .then((userProperties) => {
+          testData.user = userProperties;
+
+          cy.login(userProperties.username, userProperties.password, {
+            path: TopMenu.invoicesPath,
+            waiter: Invoices.waitLoading,
+          });
+        });
+    };
+
+    before('Create test data', () => {
+      cy.getAdminToken();
+
+      cy.then(createBudgetWithFundLedgerAndFiscalYear)
+        .then(createOrganization)
+        .then(createOpenOneTimeOrderWithLine)
+        .then(createAndPayInvoices)
+        .then(createUserAndLogin);
+    });
+
+    after('Delete test data', () => {
+      cy.getAdminToken().then(() => {
+        Users.deleteViaApi(testData.user.userId);
+        Organizations.deleteOrganizationViaApi(testData.organization.id);
+      });
+    });
+
+    it(
+      'C1028981 Encumbrance is calculated correctly after canceling a paid invoice (release encumbrance = false) when another paid invoice (release encumbrance = true) exists (thunderjet)',
+      { tags: ['criticalPath', 'thunderjet', 'C1028981'] },
+      () => {
+        // Step 1: Cancel Invoice #1
+        Invoices.searchByNumber(testData.invoices.first.vendorInvoiceNo);
+        Invoices.selectInvoice(testData.invoices.first.vendorInvoiceNo);
+        InvoiceView.cancelInvoice();
+        InvoiceView.checkInvoiceDetails({
+          title: testData.invoices.first.vendorInvoiceNo,
+          invoiceInformation: [
+            { key: INVOICE_VIEW_FIELDS.INVOICE_STATUS, value: INVOICE_STATUSES.CANCELLED },
+          ],
+        });
+
+        // Step 2: Open the encumbrance from the invoice line of Invoice #1
+        InvoiceView.selectInvoiceLine();
+        InvoiceLineDetails.checkFundDistibutionTableContent([
+          { name: testData.fund.name, currentEncumbrance: '$0.00' },
+        ]);
+        InvoiceLineDetails.openEncumbrancePane();
+        TransactionDetails.checkTransactionDetails({
+          information: [
+            { key: TRANSACTION_DETAIL_FIELDS.FISCAL_YEAR, value: testData.fiscalYear.code },
+            { key: TRANSACTION_DETAIL_FIELDS.AMOUNT, value: '$0.00' },
+            { key: TRANSACTION_DETAIL_FIELDS.SOURCE, value: testData.orderLine.poLineNumber },
+            { key: TRANSACTION_DETAIL_FIELDS.TYPE, value: TRANSACTION_TYPES.ENCUMBRANCE },
+            { key: TRANSACTION_DETAIL_FIELDS.FROM, value: testData.fund.name },
+            { key: TRANSACTION_DETAIL_FIELDS.INITIAL_ENCUMBRANCE, value: '$100.00' },
+            { key: TRANSACTION_DETAIL_FIELDS.AWAITING_PAYMENT, value: '$0.00' },
+            { key: TRANSACTION_DETAIL_FIELDS.EXPENDED, value: '$150.00' },
+            { key: TRANSACTION_DETAIL_FIELDS.STATUS, value: ENCUMBRANCE_STATUSES.UNRELEASED },
+          ],
+        });
+
+        // Step 3 is skipped: it is required only before MODINVOICE-628 is resolved
+
+        // Step 4: Cancel Invoice #2
+        TopMenuNavigation.navigateToApp(APPLICATION_NAMES.INVOICES);
+        Invoices.selectInvoiceByNumber(testData.invoices.second.vendorInvoiceNo);
+        InvoiceView.cancelInvoice();
+        InvoiceView.checkInvoiceDetails({
+          title: testData.invoices.second.vendorInvoiceNo,
+          invoiceInformation: [
+            { key: INVOICE_VIEW_FIELDS.INVOICE_STATUS, value: INVOICE_STATUSES.CANCELLED },
+          ],
+        });
+
+        // Step 5: Open the encumbrance from the invoice line of Invoice #2
+        InvoiceView.selectInvoiceLine();
+        InvoiceLineDetails.checkFundDistibutionTableContent([
+          { name: testData.fund.name, currentEncumbrance: '$100.00' },
+        ]);
+        InvoiceLineDetails.openEncumbrancePane();
+        TransactionDetails.checkTransactionDetails({
+          information: [
+            { key: TRANSACTION_DETAIL_FIELDS.FISCAL_YEAR, value: testData.fiscalYear.code },
+            { key: TRANSACTION_DETAIL_FIELDS.AMOUNT, value: '($100.00)' },
+            { key: TRANSACTION_DETAIL_FIELDS.SOURCE, value: testData.orderLine.poLineNumber },
+            { key: TRANSACTION_DETAIL_FIELDS.TYPE, value: TRANSACTION_TYPES.ENCUMBRANCE },
+            { key: TRANSACTION_DETAIL_FIELDS.FROM, value: testData.fund.name },
+            { key: TRANSACTION_DETAIL_FIELDS.INITIAL_ENCUMBRANCE, value: '$100.00' },
+            { key: TRANSACTION_DETAIL_FIELDS.AWAITING_PAYMENT, value: '$0.00' },
+            { key: TRANSACTION_DETAIL_FIELDS.EXPENDED, value: '$0.00' },
+            { key: TRANSACTION_DETAIL_FIELDS.STATUS, value: ENCUMBRANCE_STATUSES.UNRELEASED },
+          ],
+        });
+      },
+    );
+  });
+});
